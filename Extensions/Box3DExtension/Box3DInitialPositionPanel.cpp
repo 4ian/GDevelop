8dbe5d57f530a91330941afc142d6ebdba2feb24
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2010-03-07 20:34:03 +0000

    Added comments
    git-svn-id: svn://localhost@65 8062f311-0dae-4547-b526-b8ab9ac864a5

--- Extensions/Box3DExtension/Box3DInitialPositionPanel.cpp
+++ Extensions/Box3DExtension/Box3DInitialPositionPanel.cpp
@@ -1,3 +1,29 @@
+/**
+
+Game Develop - Box 3D Extension
+Copyright (c) 2008-2010 Florian Rival (Florian.Rival@gmail.com)
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+
+*/
+
 #if defined(GDE)
 #include "Box3DInitialPositionPanel.h"
 
@@ -36,7 +62,7 @@ Box3DInitialPositionPanel::Box3DInitialPositionPanel(wxWindow* parent,wxWindowID
 	wxFlexGridSizer* FlexGridSizer6;
 	wxStaticBoxSizer* StaticBoxSizer1;
 	wxFlexGridSizer* FlexGridSizer1;
-	
+
 	Create(parent, id, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("id"));
 	FlexGridSizer1 = new wxFlexGridSizer(0, 3, 0, 0);
 	StaticBoxSizer1 = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Angle"));
--- Extensions/Box3DExtension/Extension.cpp
+++ Extensions/Box3DExtension/Extension.cpp
@@ -1,18 +1,51 @@
+/**
+
+Game Develop - Box 3D Extension
+Copyright (c) 2008-2010 Florian Rival (Florian.Rival@gmail.com)
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+
+*/
+
 #include "GDL/ExtensionBase.h"
 #include "GDL/Version.h"
 #include "Box3DObject.h"
 #include <boost/version.hpp>
 
-class Box3DExtension : public ExtensionBase
+/**
+ * This class declare information about the extension.
+ */
+class Extension : public ExtensionBase
 {
     public:
-        Box3DExtension()
+
+        /**
+         * Constructor of an extension declares everything the extension contains : Objects, actions, conditions and expressions.
+         */
+        Extension()
         {
             DECLARE_THE_EXTENSION("Box3DObject",
                                   _("Objet Boîte 3D"),
                                   _("Extension permettant d'utiliser des objets boites en 3D."),
                                   "Compil Games",
-                                  "Freeware")
+                                  "zlib/libpng License ( Open Source )")
 
             DECLARE_OBJECT("Box3D",
                            _("Boite en 3D"),
@@ -270,10 +303,16 @@ class Box3DExtension : public ExtensionBase
 
             CompleteCompilationInformation();
         };
-        virtual ~Box3DExtension() {};
+        virtual ~Extension() {};
 
     protected:
     private:
+
+        /**
+         * This function is called by Game Develop so
+         * as to complete information about how the extension was compiled ( which libs... )
+         * -- Do not need to be modified. --
+         */
         void CompleteCompilationInformation()
         {
             #if defined(GDE)
@@ -306,10 +345,18 @@ class Box3DExtension : public ExtensionBase
         }
 };
 
+/**
+ * Used by Game Develop to create the extension class
+ * -- Do not need to be modified. --
+ */
 extern "C" ExtensionBase * CreateGDExtension() {
-    return new Box3DExtension;
+    return new Extension;
 }
 
+/**
+ * Used by Game Develop to destroy the extension class
+ * -- Do not need to be modified. --
+ */
 extern "C" void DestroyGDExtension(ExtensionBase * p) {
     delete p;
 }

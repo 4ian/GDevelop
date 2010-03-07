86654ac7978f9600f0d339dac4352c7a71595eaf
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2010-03-07 20:27:20 +0000

    Added comments
    git-svn-id: svn://localhost@63 8062f311-0dae-4547-b526-b8ab9ac864a5

--- Extensions/PrimitiveDrawing/Extension.cpp
+++ Extensions/PrimitiveDrawing/Extension.cpp
@@ -1,11 +1,44 @@
+/**
+
+Game Develop - Primitive Drawing Extension
+Copyright (c) 2008-2010 Florian Rival (Florian.Rival@gmail.com)
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+
+*/
+
 #include "GDL/ExtensionBase.h"
 #include "GDL/Version.h"
 #include "DrawerObject.h"
 #include <boost/version.hpp>
 
+/**
+ * This class declare information about the extension.
+ */
 class Extension : public ExtensionBase
 {
     public:
+
+        /**
+         * Constructor of an extension declares everything the extension contains : Objects, actions, conditions and expressions.
+         */
         Extension()
         {
             DECLARE_THE_EXTENSION("PrimitiveDrawing",
@@ -227,6 +260,12 @@ class Extension : public ExtensionBase
 
     protected:
     private:
+
+        /**
+         * This function is called by Game Develop so
+         * as to complete information about how the extension was compiled ( which libs... )
+         * -- Do not need to be modified. --
+         */
         void CompleteCompilationInformation()
         {
             #if defined(GDE)
@@ -259,10 +298,18 @@ class Extension : public ExtensionBase
         }
 };
 
+/**
+ * Used by Game Develop to create the extension class
+ * -- Do not need to be modified. --
+ */
 extern "C" ExtensionBase * CreateGDExtension() {
     return new Extension;
 }
 
+/**
+ * Used by Game Develop to destroy the extension class
+ * -- Do not need to be modified. --
+ */
 extern "C" void DestroyGDExtension(ExtensionBase * p) {
     delete p;
 }

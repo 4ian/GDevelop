13473d53240fd51c7eb169ef764db92929e60df9
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2010-03-23 21:08:40 +0000

    Make sure cameras size are correct, even if they use default size.
    git-svn-id: svn://localhost@74 8062f311-0dae-4547-b526-b8ab9ac864a5

--- GDL/GDL/Version.h
+++ GDL/GDL/Version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "21";
+	static const char DATE[] = "23";
 	static const char MONTH[] = "03";
 	static const char YEAR[] = "2010";
 	static const double UBUNTU_VERSION_STYLE = 10.03;
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 3;
-	static const long BUILD = 8890;
-	static const long REVISION = 44757;
+	static const long BUILD = 8892;
+	static const long REVISION = 44771;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 19061;
-	#define RC_FILEVERSION 1,3,8890,44757
-	#define RC_FILEVERSION_STRING "1, 3, 8890, 44757\0"
-	static const char FULLVERSION_STRING[] = "1.3.8890.44757";
+	static const long BUILDS_COUNT = 19066;
+	#define RC_FILEVERSION 1,3,8892,44771
+	#define RC_FILEVERSION_STRING "1, 3, 8892, 44771\0"
+	static const char FULLVERSION_STRING[] = "1.3.8892.44771";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

dafbeeb46492af0d71531f32007203f3d4852b9f
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2010-07-07 17:22:32 +0000

    Corrected ActSetWindowSize ( don't change game windowHeight and windowWidth, which would be better called defaultCameraHeight/Width ). Added icon to window extension expressions.
    git-svn-id: svn://localhost@166 8062f311-0dae-4547-b526-b8ab9ac864a5

--- GDL/GDL/Version.h
+++ GDL/GDL/Version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "05";
+	static const char DATE[] = "07";
 	static const char MONTH[] = "07";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.07";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 4;
-	static const long BUILD = 9626;
-	static const long REVISION = 48473;
+	static const long BUILD = 9627;
+	static const long REVISION = 48482;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 20036;
-	#define RC_FILEVERSION 1,4,9626,48473
-	#define RC_FILEVERSION_STRING "1, 4, 9626, 48473\0"
-	static const char FULLVERSION_STRING[] = "1.4.9626.48473";
+	static const long BUILDS_COUNT = 20041;
+	#define RC_FILEVERSION 1,4,9627,48482
+	#define RC_FILEVERSION_STRING "1, 4, 9627, 48482\0"
+	static const char FULLVERSION_STRING[] = "1.4.9627.48482";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
--- GDL/GDL/aWindow.cpp
+++ GDL/GDL/aWindow.cpp
@@ -40,17 +40,17 @@ bool ActSetFullScreen( RuntimeScene & scene, ObjectsConcerned & objectsConcerned
 bool ActSetWindowSize( RuntimeScene & scene, ObjectsConcerned & objectsConcerned, const Instruction & action )
 {
     #ifdef GDP
-    scene.game->windowWidth = action.GetParameter( 0 ).GetAsMathExpressionResult(scene, objectsConcerned);
-    scene.game->windowHeight = action.GetParameter( 1 ).GetAsMathExpressionResult(scene, objectsConcerned);
+    int windowWidth = action.GetParameter( 0 ).GetAsMathExpressionResult(scene, objectsConcerned);
+    int windowHeight = action.GetParameter( 1 ).GetAsMathExpressionResult(scene, objectsConcerned);
 
     if ( scene.game->fullscreen )
     {
-        scene.renderWindow->Create( sf::VideoMode( scene.game->windowWidth, scene.game->windowHeight, 32 ), scene.title, sf::Style::Close | sf::Style::Fullscreen );
+        scene.renderWindow->Create( sf::VideoMode( windowWidth, windowHeight, 32 ), scene.title, sf::Style::Close | sf::Style::Fullscreen );
         scene.ChangeRenderWindow(scene.renderWindow);
     }
     else
     {
-        scene.renderWindow->Create( sf::VideoMode( scene.game->windowWidth, scene.game->windowHeight, 32 ), scene.title, sf::Style::Close );
+        scene.renderWindow->Create( sf::VideoMode( windowWidth, windowHeight, 32 ), scene.title, sf::Style::Close );
         scene.ChangeRenderWindow(scene.renderWindow);
     }
     #endif

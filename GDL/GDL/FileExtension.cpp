9ce54cd771f27e0662f94de36fb62ccf91e9fcdf
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2010-08-20 15:39:40 +0000

    Reorganized file actions/conditions.
    git-svn-id: svn://localhost@217 8062f311-0dae-4547-b526-b8ab9ac864a5

--- GDL/GDL/FileExtension.cpp
+++ GDL/GDL/FileExtension.cpp
@@ -24,10 +24,10 @@ FileExtension::FileExtension()
     DECLARE_END_CONDITION()
 
     DECLARE_CONDITION("GroupExists",
-                   _("Un groupe existe dans un fichier"),
+                   _("Existance d'un groupe"),
                    _("Teste si le groupe ( de la forme /Racine/Exemple/1 ) existe dans le fichier.\nAttention ! Les espaces sont interdits dans le nom des groupes."),
                    _("Le groupe _PARAM1_ existe dans le fichier _PARAM0_"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/conditions/fichier24.png",
                    "res/conditions/fichier.png",
                    &CondGroupExists);
@@ -41,7 +41,7 @@ FileExtension::FileExtension()
                    _("Charger un fichier en mémoire"),
                    _("Cette action charge le fichier XML en mémoire, pour permettre de lire et écrire dedans.\nIl est possible de lire et d'écrire dans un fichier XML sans passer par cette action, mais les performances seront moindres."),
                    _("Charger le fichier XML _PARAM0_ en mémoire"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/fichier24.png",
                    "res/actions/fichier.png",
                    &ActLoadFile);
@@ -54,7 +54,7 @@ FileExtension::FileExtension()
                    _("Fermer un fichier chargé en mémoire"),
                    _("Cette action ferme le fichier XML chargé précédemment en mémoire, en enregistrant les modifications apportées à celui ci."),
                    _("Fermer le fichier XML _PARAM0_ chargé en mémoire"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/fichier24.png",
                    "res/actions/fichier.png",
                    &ActUnloadFile);
@@ -64,10 +64,10 @@ FileExtension::FileExtension()
     DECLARE_END_ACTION()
 
     DECLARE_ACTION("EcrireFichierExp",
-                   _("Ecrire un nombre dans un fichier"),
+                   _("Ecrire une valeur"),
                    _("Cette action écrit le résultat de l'expression dans le fichier, dans le groupe indiqué.\nIndiquez la structure du groupe en séparant les éléments par des /. (Exemple : Racine/MonPersonnage/X)\nAttention ! Les espaces sont interdits dans le nom des groupes."),
                    _("Ecrire _PARAM2_ dans _PARAM1_ du fichier _PARAM0_"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/fichier24.png",
                    "res/actions/fichier.png",
                    &ActEcrireFichierExp);
@@ -79,10 +79,10 @@ FileExtension::FileExtension()
     DECLARE_END_ACTION()
 
     DECLARE_ACTION("EcrireFichierTxt",
-                   _("Ecrire un texte dans un fichier"),
+                   _("Ecrire un texte"),
                    _("Cette action écrit le texte dans le fichier, dans le groupe indiqué.\nIndiquez la structure du groupe en séparant les éléments par des /. (Exemple : Racine/MonPersonnage/X)\nAttention ! Les espaces sont interdits dans le nom des groupes."),
                    _("Ecrire _PARAM2_ dans _PARAM1_ du fichier _PARAM0_"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/fichier24.png",
                    "res/actions/fichier.png",
                    &ActEcrireFichierTxt);
@@ -94,10 +94,10 @@ FileExtension::FileExtension()
     DECLARE_END_ACTION()
 
     DECLARE_ACTION("LireFichierExp",
-                   _("Lire une valeur depuis un fichier"),
+                   _("Lire une valeur"),
                    _("Cette action lit la valeur du groupe indiqué et\nla stocke dans une variable de la scène.\nIndiquez la structure du groupe en séparant les éléments par des /. (Exemple : Racine/Niveau/Actuel)\nAttention ! Les espaces sont interdits dans le nom des groupes."),
                    _("Lire le groupe _PARAM1_ du fichier _PARAM0_ et stocker la valeur dans _PARAM2_"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/fichier24.png",
                    "res/actions/fichier.png",
                    &ActLireFichierExp);
@@ -109,10 +109,10 @@ FileExtension::FileExtension()
     DECLARE_END_ACTION()
 
     DECLARE_ACTION("LireFichierTxt",
-                   _("Lire un texte depuis un fichier"),
+                   _("Lire un texte"),
                    _("Cette action lit le texte du groupe indiqué et\nle stocke dans une variable de la scène.\nIndiquez la structure du groupe en séparant les éléments par des /. (Exemple : Racine/Niveau/Actuel)\nAttention ! Les espaces sont interdits dans le nom des groupes."),
                    _("Lire le groupe _PARAM1_ du fichier _PARAM0_ et stocker le texte dans _PARAM2_"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/fichier24.png",
                    "res/actions/fichier.png",
                    &ActLireFichierTxt);
@@ -137,10 +137,10 @@ FileExtension::FileExtension()
     DECLARE_END_ACTION()
 
     DECLARE_ACTION("DeleteGroupFichier",
-                   _("Supprimer un groupe dans un fichier"),
-                   _("Cette action supprime le groupe indiqué du fichier.\nIndiquez la structure du groupe en séparant les éléments par des /. (Exemple : Racine/Niveau/Actuel)"),
+                   _("Supprimer un groupe"),
+                   _("Cette action supprime le groupe indiqué du fichier XML.\nIndiquez la structure du groupe en séparant les éléments par des /. (Exemple : Racine/Niveau/Actuel)"),
                    _("Supprimer le groupe _PARAM1_ du fichier _PARAM0_"),
-                   _("Fichiers"),
+                   _("Fichiers XML"),
                    "res/actions/delete24.png",
                    "res/actions/delete.png",
                    &ActDeleteGroupFichier);

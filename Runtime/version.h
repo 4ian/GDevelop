3669fb87661b39fff2c41a64bdaf0cfba770fa56
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2010-11-13 12:11:48 +0000

    Updated to use latest SFML libraries filenames. Added Linux - Debug target.
    git-svn-id: svn://localhost@292 8062f311-0dae-4547-b526-b8ab9ac864a5

--- Runtime/version.h
+++ Runtime/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "17";
-	static const char MONTH[] = "08";
+	static const char DATE[] = "27";
+	static const char MONTH[] = "10";
 	static const char YEAR[] = "2010";
-	static const char UBUNTU_VERSION_STYLE[] = "10.08";
+	static const char UBUNTU_VERSION_STYLE[] = "10.10";
 	
 	//Software Status
 	static const char STATUS[] = "Alpha";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 0;
-	static const long BUILD = 163;
-	static const long REVISION = 845;
+	static const long BUILD = 168;
+	static const long REVISION = 875;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 806;
-	#define RC_FILEVERSION 1,0,163,845
-	#define RC_FILEVERSION_STRING "1, 0, 163, 845\0"
-	static const char FULLVERSION_STRING[] = "1.0.163.845";
+	static const long BUILDS_COUNT = 822;
+	#define RC_FILEVERSION 1,0,168,875
+	#define RC_FILEVERSION_STRING "1, 0, 168, 875\0"
+	static const char FULLVERSION_STRING[] = "1.0.168.875";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

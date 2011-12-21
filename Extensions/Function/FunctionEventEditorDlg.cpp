1b26e610a10886353b47bb9c4d1a372c245705f7
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2011-12-21 13:14:14 +0000

    Added non versioned files.
    git-svn-id: svn://localhost@597 8062f311-0dae-4547-b526-b8ab9ac864a5

---
+++ Extensions/Function/FunctionEventEditorDlg.cpp
@@ -0,0 +1,123 @@
+#if defined(GD_IDE_ONLY)
+
+#include "FunctionEventEditorDlg.h"
+
+//(*InternalHeaders(FunctionEventEditorDlg)
+#include <wx/bitmap.h>
+#include <wx/settings.h>
+#include <wx/intl.h>
+#include <wx/image.h>
+#include <wx/string.h>
+//*)
+#include "GDL/CommonTools.h"
+#include "FunctionEvent.h"
+
+//(*IdInit(FunctionEventEditorDlg)
+const long FunctionEventEditorDlg::ID_STATICBITMAP3 = wxNewId();
+const long FunctionEventEditorDlg::ID_STATICTEXT3 = wxNewId();
+const long FunctionEventEditorDlg::ID_PANEL1 = wxNewId();
+const long FunctionEventEditorDlg::ID_STATICLINE2 = wxNewId();
+const long FunctionEventEditorDlg::ID_STATICTEXT2 = wxNewId();
+const long FunctionEventEditorDlg::ID_TEXTCTRL2 = wxNewId();
+const long FunctionEventEditorDlg::ID_RADIOBOX1 = wxNewId();
+const long FunctionEventEditorDlg::ID_STATICLINE1 = wxNewId();
+const long FunctionEventEditorDlg::ID_BUTTON1 = wxNewId();
+const long FunctionEventEditorDlg::ID_BUTTON2 = wxNewId();
+//*)
+
+BEGIN_EVENT_TABLE(FunctionEventEditorDlg,wxDialog)
+	//(*EventTable(FunctionEventEditorDlg)
+	//*)
+END_EVENT_TABLE()
+
+FunctionEventEditorDlg::FunctionEventEditorDlg(wxWindow* parent, FunctionEvent & event_, Game & game_, Scene & scene_) :
+eventEdited(event_),
+game(game_),
+scene(scene_)
+{
+	//(*Initialize(FunctionEventEditorDlg)
+	wxFlexGridSizer* FlexGridSizer3;
+	wxFlexGridSizer* FlexGridSizer5;
+	wxFlexGridSizer* FlexGridSizer2;
+	wxFlexGridSizer* FlexGridSizer7;
+	wxFlexGridSizer* FlexGridSizer6;
+	wxFlexGridSizer* FlexGridSizer1;
+	wxFlexGridSizer* FlexGridSizer17;
+
+	Create(parent, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("wxID_ANY"));
+	FlexGridSizer1 = new wxFlexGridSizer(0, 1, 0, 0);
+	FlexGridSizer17 = new wxFlexGridSizer(0, 1, 0, 0);
+	FlexGridSizer17->AddGrowableCol(0);
+	Panel1 = new wxPanel(this, ID_PANEL1, wxDefaultPosition, wxSize(420,54), wxTAB_TRAVERSAL, _T("ID_PANEL1"));
+	Panel1->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW));
+	FlexGridSizer6 = new wxFlexGridSizer(0, 3, 0, 0);
+	StaticBitmap3 = new wxStaticBitmap(Panel1, ID_STATICBITMAP3, wxBitmap(wxImage(_T("res/function24.png"))), wxDefaultPosition, wxDefaultSize, wxNO_BORDER, _T("ID_STATICBITMAP3"));
+	FlexGridSizer6->Add(StaticBitmap3, 1, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5);
+	StaticText3 = new wxStaticText(Panel1, ID_STATICTEXT3, _("La fonction et ses sous évènements ne sera executée\nque lorsqu\'elle aura été lancée par une action ou une expression."), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE, _T("ID_STATICTEXT3"));
+	FlexGridSizer6->Add(StaticText3, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
+	Panel1->SetSizer(FlexGridSizer6);
+	FlexGridSizer6->SetSizeHints(Panel1);
+	FlexGridSizer17->Add(Panel1, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	StaticLine2 = new wxStaticLine(this, ID_STATICLINE2, wxDefaultPosition, wxSize(10,-1), wxLI_HORIZONTAL, _T("ID_STATICLINE2"));
+	FlexGridSizer17->Add(StaticLine2, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	FlexGridSizer1->Add(FlexGridSizer17, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	FlexGridSizer5 = new wxFlexGridSizer(0, 1, 0, 0);
+	FlexGridSizer5->AddGrowableCol(0);
+	StaticText2 = new wxStaticText(this, ID_STATICTEXT2, _("Nom de la fonction ( Doit être unique ) :"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
+	FlexGridSizer5->Add(StaticText2, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
+	FlexGridSizer7 = new wxFlexGridSizer(0, 3, 0, 0);
+	FlexGridSizer7->AddGrowableCol(0);
+	nameEdit = new wxTextCtrl(this, ID_TEXTCTRL2, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL2"));
+	FlexGridSizer7->Add(nameEdit, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	FlexGridSizer5->Add(FlexGridSizer7, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	FlexGridSizer1->Add(FlexGridSizer5, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	FlexGridSizer2 = new wxFlexGridSizer(0, 1, 0, 0);
+	FlexGridSizer2->AddGrowableCol(0);
+	wxString __wxRadioBoxChoices_1[2] =
+	{
+		_("Utiliser les objets concernés du contexte qui appelle la fonction"),
+		_("Oublier les objets déjà concernés.")
+	};
+	callerContextChoice = new wxRadioBox(this, ID_RADIOBOX1, _("Lors de l\'appel à la fonction"), wxDefaultPosition, wxDefaultSize, 2, __wxRadioBoxChoices_1, 1, 0, wxDefaultValidator, _T("ID_RADIOBOX1"));
+	FlexGridSizer2->Add(callerContextChoice, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	FlexGridSizer1->Add(FlexGridSizer2, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	StaticLine1 = new wxStaticLine(this, ID_STATICLINE1, wxDefaultPosition, wxSize(10,-1), wxLI_HORIZONTAL, _T("ID_STATICLINE1"));
+	FlexGridSizer1->Add(StaticLine1, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 0);
+	FlexGridSizer3 = new wxFlexGridSizer(0, 3, 0, 0);
+	okBt = new wxButton(this, ID_BUTTON1, _("Ok"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
+	FlexGridSizer3->Add(okBt, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	cancelBt = new wxButton(this, ID_BUTTON2, _("Annuler"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
+	FlexGridSizer3->Add(cancelBt, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	FlexGridSizer1->Add(FlexGridSizer3, 1, wxALL|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 0);
+	SetSizer(FlexGridSizer1);
+	FlexGridSizer1->Fit(this);
+	FlexGridSizer1->SetSizeHints(this);
+
+	Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FunctionEventEditorDlg::OnokBtClick);
+	Connect(ID_BUTTON2,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FunctionEventEditorDlg::OncancelBtClick);
+	//*)
+
+	callerContextChoice->SetSelection(eventEdited.UseCallerContext() ? 0 : 1);
+	nameEdit->SetValue(eventEdited.GetName());
+}
+
+FunctionEventEditorDlg::~FunctionEventEditorDlg()
+{
+	//(*Destroy(FunctionEventEditorDlg)
+	//*)
+}
+
+void FunctionEventEditorDlg::OnokBtClick(wxCommandEvent& event)
+{
+    eventEdited.SetUseCallerContext(callerContextChoice->GetSelection() == 0);
+    eventEdited.SetName(ToString(nameEdit->GetValue()));
+
+    EndModal(1);
+}
+
+void FunctionEventEditorDlg::OncancelBtClick(wxCommandEvent& event)
+{
+    EndModal(0);
+}
+
+#endif
---
+++ Extensions/Function/FunctionEventEditorDlg.h
@@ -0,0 +1,70 @@
+#if defined(GD_IDE_ONLY)
+
+#ifndef FUNCTIONEVENTEDITORDLG_H
+#define FUNCTIONEVENTEDITORDLG_H
+
+//(*Headers(FunctionEventEditorDlg)
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/radiobox.h>
+#include <wx/textctrl.h>
+#include <wx/statline.h>
+#include <wx/panel.h>
+#include <wx/statbmp.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
+//*)
+#include "FunctionEvent.h"
+
+class FunctionEventEditorDlg: public wxDialog
+{
+	public:
+
+		FunctionEventEditorDlg(wxWindow* parent, FunctionEvent & event_, Game & game_, Scene & scene_);
+		virtual ~FunctionEventEditorDlg();
+
+		//(*Declarations(FunctionEventEditorDlg)
+		wxStaticText* StaticText2;
+		wxRadioBox* callerContextChoice;
+		wxPanel* Panel1;
+		wxStaticText* StaticText3;
+		wxButton* cancelBt;
+		wxStaticLine* StaticLine2;
+		wxTextCtrl* nameEdit;
+		wxStaticLine* StaticLine1;
+		wxButton* okBt;
+		wxStaticBitmap* StaticBitmap3;
+		//*)
+
+		FunctionEvent & eventEdited;
+
+	protected:
+
+		//(*Identifiers(FunctionEventEditorDlg)
+		static const long ID_STATICBITMAP3;
+		static const long ID_STATICTEXT3;
+		static const long ID_PANEL1;
+		static const long ID_STATICLINE2;
+		static const long ID_STATICTEXT2;
+		static const long ID_TEXTCTRL2;
+		static const long ID_RADIOBOX1;
+		static const long ID_STATICLINE1;
+		static const long ID_BUTTON1;
+		static const long ID_BUTTON2;
+		//*)
+
+	private:
+
+		//(*Handlers(FunctionEventEditorDlg)
+		void OncancelBtClick(wxCommandEvent& event);
+		void OnokBtClick(wxCommandEvent& event);
+		//*)
+
+		Game & game;
+		Scene & scene;
+
+		DECLARE_EVENT_TABLE()
+};
+
+#endif
+#endif

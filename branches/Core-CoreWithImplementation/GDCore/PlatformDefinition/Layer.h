36fb5cc709a3a5e8e1f9dd870d78765bd1799809
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2013-04-05 18:06:42 +0000

    Moved EditLayerDialog to GDCore. Moved implementation of Layer to GDCore.
    git-svn-id: svn://localhost@936 8062f311-0dae-4547-b526-b8ab9ac864a5

--- branches/Core-CoreWithImplementation/GDCore/PlatformDefinition/Layer.h
+++ branches/Core-CoreWithImplementation/GDCore/PlatformDefinition/Layer.h
@@ -5,6 +5,9 @@
 #ifndef GDCORE_LAYER_H
 #define GDCORE_LAYER_H
 #include <string>
+#include <vector>
+namespace gd { class Camera; }
+class TiXmlElement;
 
 namespace gd
 {
@@ -18,33 +21,141 @@ namespace gd
 class GD_CORE_API Layer
 {
 public:
-    Layer() {};
+    Layer();
     virtual ~Layer() {};
 
     /**
-     * Must change the name of the layout with the name passed as parameter.
+     * Change layer name
      */
-    virtual void SetName(const std::string & name) =0;
+    virtual void SetName(const std::string & name_) { name = name_; }
 
     /**
-     * Must return the name of the layout.
+     * Get layer name
      */
-    virtual const std::string & GetName() const =0;
+    virtual const std::string & GetName() const { return name; }
 
     /**
-     * Must change the visibility of the layer
+     * Change if layer is displayed or not
      */
-    virtual void SetVisibility(bool isVisible_) =0;
+    virtual void SetVisibility(bool isVisible_) { isVisible = isVisible_; }
 
     /**
-     * Must return true if the layer is visible
+     * True if layer will be displayed.
      */
-    virtual bool GetVisibility() const =0;
+    virtual bool GetVisibility() const { return isVisible; }
 
     /**
-     * Called when the user double click on a layer in a layer editor.
+     * Change the number of cameras inside the layer.
      */
-    virtual void EditLayer() =0;
+    void SetCameraCount(unsigned int n);
+
+    /**
+     * Get cameras count.
+     */
+    inline unsigned int GetCameraCount() const { return cameras.size(); };
+
+    /**
+     * Return a reference to a camera
+     */
+    inline const Camera & GetCamera(unsigned int n) const { if ( n >= GetCameraCount() ) return badCamera; return cameras[n]; }
+
+    /**
+     * Return a reference to a camera
+     */
+    inline Camera & GetCamera(unsigned int n) { if ( n >= GetCameraCount() ) return badCamera; return cameras[n]; }
+
+    /**
+     * Delete a specific camera.
+     */
+    inline void DeleteCamera(unsigned int n) { if ( n >= GetCameraCount() ) return; cameras.erase(cameras.begin()+n); }
+
+    /**
+     * Add an already existing camera.
+     */
+    inline void AddCamera(const Camera & camera) { cameras.push_back(camera); };
+
+    #if defined(GD_IDE_ONLY)
+    /**
+     * Display a window to edit the layer
+     */
+    virtual void EditLayer();
+
+    /**
+     * Save to xml
+     */
+    virtual void SaveToXml(TiXmlElement * element) const;
+    #endif
+
+    /**
+     * Load from xml
+     */
+    virtual void LoadFromXml(const TiXmlElement * element);
+
+private:
+
+    std::string name; ///< The name of the layer
+    bool isVisible; ///< True if the layer is visible
+    std::vector < gd::Camera > cameras; ///< The camera displayed by the layer
+
+    static gd::Camera badCamera;
+};
+
+/**
+ * \brief Functor testing layer name
+ *
+ * \see gd::Layer
+ */
+struct LayerHasName : public std::binary_function<gd::Layer, std::string, bool> {
+    bool operator()(const Layer & layer, const std::string & name) const { return layer.GetName() == name; }
+};
+
+
+/**
+ * \brief A camera is used to render a specific area of a scene.
+ *
+ * \see gd::Layout
+ * \ingroup PlatformDefinition
+ */
+class GD_CORE_API Camera
+{
+public:
+    Camera();
+    virtual ~Camera() {};
+
+    /**
+     * Change the viewport, i.e the area of the window where the camera will be displayed
+     * The coordinates must be between 0 and 1.
+     */
+    void SetViewport(float x1_, float y1_, float x2_, float y2_) { x1 = x1_; x2 = x2_; y1 = y1_; y2 = y2_; };
+    float GetViewportX1() const { return x1; };
+    float GetViewportY1() const { return y1; };
+    float GetViewportX2() const { return x2; };
+    float GetViewportY2() const { return y2; };
+
+    /**
+     * Change the size of the rendered area of the scene.
+     */
+    void SetSize(float width_, float height_) { width = width_; height = height_; };
+    float GetWidth() const { return width; };
+    float GetHeight() const { return height; };
+
+    void SetUseDefaultSize(bool useDefaultSize = true) { defaultSize = useDefaultSize; };
+    bool UseDefaultSize() const {return defaultSize;}
+
+    void SetUseDefaultViewport(bool useDefaultViewport = true) { defaultViewport = useDefaultViewport; };
+    bool UseDefaultViewport() const {return defaultViewport;}
+
+private:
+
+    bool defaultSize; ///< True if the camera use the default window size
+    bool defaultViewport; ///< True if the camera use the default viewport size
+
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float width; ///< The width of the rendered area
+    float height; ///< The height of the rendered area
 };
 
 }

24a5bb0182566add1d7919de98e981f88ebdf0e1
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2013-04-07 19:42:27 +0000

    Moved AutomatismsSharedData to GDCore.
    git-svn-id: svn://localhost@941 8062f311-0dae-4547-b526-b8ab9ac864a5

---
+++ branches/Core-CoreWithImplementation/GDCore/PlatformDefinition/AutomatismsSharedData.cpp
@@ -0,0 +1,16 @@
+/** \file
+ *  Game Develop
+ *  2008-2013 Florian Rival (Florian.Rival@gmail.com)
+ */
+
+#include "GDCore/PlatformDefinition/AutomatismsSharedData.h"
+
+namespace gd
+{
+
+AutomatismsSharedData::AutomatismsSharedData(std::string typeName) :
+    type(typeName)
+{
+}
+
+}
---
+++ branches/Core-CoreWithImplementation/GDCore/PlatformDefinition/AutomatismsSharedData.h
@@ -0,0 +1,77 @@
+/** \file
+ *  Game Develop
+ *  2008-2013 Florian Rival (Florian.Rival@gmail.com)
+ */
+
+#ifndef AUTOMATISMSSHAREDDATA_H
+#define AUTOMATISMSSHAREDDATA_H
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+class AutomatismsRuntimeSharedData;
+class TiXmlElement;
+
+namespace gd
+{
+
+/**
+ * \brief Base class for defining automatisms shared datas.
+ *
+ * Automatisms can use shared datas, as if they were extending the Scene members.
+ * Inherit from this class, and redefine Clone and CreateRuntimeSharedDatas.
+ *
+ * \ingroup GameEngine
+ */
+class GD_CORE_API AutomatismsSharedData
+{
+public:
+    AutomatismsSharedData(std::string typeName);
+    virtual ~AutomatismsSharedData() {};
+    virtual boost::shared_ptr<gd::AutomatismsSharedData> Clone() const { return boost::shared_ptr<gd::AutomatismsSharedData>(new AutomatismsSharedData(*this));}
+
+    /**
+     * Change the name identifying the automatism. Change also AutomatismId.
+     */
+    void SetName(std::string name_) { name = name_; };
+
+    /**
+     * Return the name identifying the automatism
+     */
+    std::string GetName() { return name; }
+
+    /**
+     * Return the name identifying the type of the automatism
+     */
+    std::string GetTypeName() { return type; }
+
+    //TODO : GD C++ Platform specific code :
+    /**
+     * Create Runtime equivalent of the shared datas.
+     * Derived class have to redefine this so as to create an appropriate
+     * object containing runtime shared datas.
+     */
+    virtual boost::shared_ptr<AutomatismsRuntimeSharedData> CreateRuntimeSharedDatas()
+    {
+        return boost::shared_ptr<AutomatismsRuntimeSharedData>();
+    }
+
+    #if defined(GD_IDE_ONLY)
+    /**
+     * Save AutomatismsSharedData to XML
+     */
+    virtual void SaveToXml(TiXmlElement * eventElem) const {}
+    #endif
+
+    /**
+     * Load AutomatismsSharedData from XML
+     */
+    virtual void LoadFromXml(const TiXmlElement * eventElem) {}
+
+private:
+    std::string name;
+    std::string type; ///< The type indicate of which type is the automatism.
+};
+
+}
+
+#endif // AUTOMATISMSSHAREDDATA_H

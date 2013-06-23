536bf4b915ed7c005d4a2df0d1682a1e50361418
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2013-06-23 20:26:56 +0000

    Removed useless commented code in TwoListsTest. Deactivated the use of HSHG: SoldierJS is performing better without using it ( On my computer, with Chrome 27 : 55/60 fps except for the beginning when it decreased to 30 for one second ).
    git-svn-id: svn://localhost@1061 8062f311-0dae-4547-b526-b8ab9ac864a5

--- GDJS/Runtime/objecttools.js
+++ GDJS/Runtime/objecttools.js
@@ -100,88 +100,20 @@ gdjs.evtTools.object.TwoListsTest = function(func, objectsLists1, objectsLists2,
     }
     
     return isTrue;
-    
-    /*
-    var objects1 = [];
-    var objects2 = [];
-    var objects1Values = objectsLists1.values();
-    var objects2Values = objectsLists2.values();
-
-    //Check if we're dealing with the same lists of objects
-    var objects1Keys = objectsLists1.keys();
-    var objects2Keys = objectsLists2.keys();
-    var sameObjectLists = objects1Keys.length === objects2Keys.length;
-    if ( sameObjectLists ) {
-        for( var i = 0, len = objects1Keys.length;i<len;++i) {
-            if ( objects1Keys[i] !== objects2Keys[i] ) {
-                sameObjectLists = false;
-                break;
-            }
-        }
-    }
-
-    //Prepare list of objects to iterate over.
-    //And remove these objects from the original tables.
-    for(var i = 0, len = objects1Values.length;i<len;++i) {
-        objects1.push.apply(objects1, objects1Values[i]);
-        objects1Values[i].length = 0; //Be sure not to lose the reference to the original array
-    }
-
-    if (sameObjectLists)
-        objects2 = objects1.slice(0);
-    else
-    {
-        for(var i = 0, len = objects2Values.length;i<len;++i) {
-            objects2.push.apply(objects2, objects2Values[i]);
-            objects2Values[i].length = 0; //Be sure not to lose the reference to the original array
-        }
-    }
-
-    //Iterate over the lists to do the test for each pair of objects.
-    var isTrue = false;
-    for(var i = 0, len1 = objects1.length;i<len1;++i) {
-
-        var atLeastOneObject = false;
-        for(var j = (!sameObjectLists ? 0 : i+1), len2 = objects2.length;j<len2;++j) {
-
-            if ( sameObjectLists || (objects1[i].id != objects2[j].id) ) {
-
-                if ( func(objects1[i], objects2[j], extraParam) ) {
-                    if ( !inverted ) {
-                        isTrue = true;
-
-                        //Pick the objects
-                        var objList = objectsLists1.get(objects1[i].getName());
-                        if ( objList.indexOf(objects1[i]) == -1) objList.push(objects1[i]);
-
-                        objList = objectsLists2.get(objects2[j].getName());
-                        if ( objList.indexOf(objects2[j]) == -1) objList.push(objects2[j]);
-                    }
-
-                    atLeastOneObject = true;
-                }
-
-            }
-
-        }
-        if ( !atLeastOneObject && inverted ) { //The object is not overlapping any other object.
-            isTrue = true;
-
-            //We are sure that objects1[i] is not already in the list.
-            //(As we are iterating over objects1 and only objects1 are added )
-            objectsLists1.get(objects1[i].getName()).push(objects1[i]);
-        }
-
-    }
-
-    return isTrue;*/
 }
 
 gdjs.evtTools.object.hitBoxesCollisionTest = function( objectsLists1, objectsLists2, inverted, runtimeScene) {
 
-    if ( inverted )
-        return gdjs.evtTools.object.TwoListsTest(gdjs.runtimeObject.collisionTest,
-                                                     objectsLists1, objectsLists2, inverted);
+    //if ( inverted ) ( See below why it is commented )
+    return gdjs.evtTools.object.TwoListsTest(gdjs.runtimeObject.collisionTest,
+                                                 objectsLists1, objectsLists2, inverted);
+                                                 
+    //Using HSHG ( code below ) is *less* efficient in games like SoldierJS
+    //but more efficient when no collision is really involved ( For example, testing a collision
+    //with lots of objects which are very far ).
+    //For now, use of HSHG is deactivated ( TwoListsTest is always called above ) : The bottleneck
+    //must come from the fact that HSHG performs collision test on all instances, and not on the
+    //picked one. ( getPotentialCollidingObjects method is using all the objects of the scene. ) 
 
     var objects1 = [];
     var objects2 = [];

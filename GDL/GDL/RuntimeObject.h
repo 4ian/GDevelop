bd00cbd3213fd5f371bde56e9fb2223894aee76c
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2013-06-28 16:29:54 +0000

    Improved a bit documentation.
    git-svn-id: svn://localhost@1086 8062f311-0dae-4547-b526-b8ab9ac864a5

--- GDL/GDL/RuntimeObject.h
+++ GDL/GDL/RuntimeObject.h
@@ -22,13 +22,13 @@ class RuntimeScene;
  *
  * Games don't directly use this class: Extensions can provide object by deriving from this class, and redefining functions:
  * - An important function is RuntimeObject::Draw. It is called to render the object on the scene. This function take in parameter a reference to the target where render the object.
- * - RuntimeObjects must be able to return their size, by redefining RuntimeObject::GetWidth and RuntimeObject::GetHeight
- * - RuntimeObjects must be able to return the position where they have precisely drawn ( for example, Sprite can draw image not exactly at the position of the object, if the origin point was moved ). They must also be able to return the position of their center. See RuntimeObject::GetDrawableX, RuntimeObject::GetDrawableY and RuntimeObject::GetCenterX, RuntimeObject::GetCenterY.
- * - If objects need to load ressources ( for example textures at the loading ), it must be done inside the constructor creating the object from a gd::Object.
+ * - RuntimeObject must be able to return their size, by redefining RuntimeObject::GetWidth and RuntimeObject::GetHeight
+ * - RuntimeObject must be able to return the position where they have precisely drawn ( for example, Sprite can draw its image not exactly at the position of the object, if the origin point was moved ). It must also be able to return the position of their center. See RuntimeObject::GetDrawableX, RuntimeObject::GetDrawableY and RuntimeObject::GetCenterX, RuntimeObject::GetCenterY.
+ * - If objects need to load resources ( for example textures at the loading ), it must be done inside the constructor creating the object from a gd::Object.
  * - When objects are placed at the start of the scene, scenes call RuntimeObject::ExtraInitializationFromInitialInstance, passing a gd::InitialInstance object in parameter ( containing information like the position where place the object ). Note that common information were already changed ( Position, angle, layer... ). You just need to setup the object with the information related to your object.
  * - Finally, objects can expose debugging features: RuntimeObject::GetPropertyForDebugger, RuntimeObject::ChangeProperty and RuntimeObject::GetNumberOfProperties
  *
- * See also gd::Object which is used to store the initial objects before they are instancied on the scene.
+ * See also gd::Object which is used to store the initial objects before they are put on the scene.
  *
  * \see gd::Object
  *
@@ -39,7 +39,8 @@ class GD_API RuntimeObject
 {
 public:
     /**
-     * Construct a RuntimeObject from an object.
+     * \brief Construct a RuntimeObject from an object.
+     *
      * The default implementation already takes care of setting common properties
      * ( name, type, automatisms... ). Be sure to call the original constructor if you redefine it:
      * \code
@@ -53,21 +54,22 @@ public:
     RuntimeObject(RuntimeScene & scene, const gd::Object & object);
 
     /**
-     * Copy constructor. Calls Init().
+     * \brief Copy constructor. Calls Init().
      */
     RuntimeObject(const RuntimeObject & object) { Init(object); };
 
     /**
-     * Assignment operator. Calls Init().
+     * \brief Assignment operator. Calls Init().
      */
     RuntimeObject& operator=(const RuntimeObject & object) {if( (this) != &object ) Init(object); return *this; }
 
-    /** Default destructor
+    /** \brief Default destructor
      */
     virtual ~RuntimeObject();
 
     /**
-     * Must return a pointer to a copy of the object. A such method is needed to do polymorphic copies.
+     * \brief Must return a pointer to a copy of the object. A such method is needed to do polymorphic copies.
+     *
      * Just redefine this method in your derived object class like this:
      * \code
      * return new MyRuntimeObject(*this);
@@ -76,14 +78,15 @@ public:
     virtual RuntimeObject * Clone() const { return new RuntimeObject(*this);}
 
     /**
-     * Called by RuntimeScene when creating the RuntimeObject from an initial instance.
-     * The RuntimeScene already takes care of setting common properties ( Position, Angle... )
-     * according to the InitialInstance.
+     * \brief Called by RuntimeScene when creating the RuntimeObject from an initial instance.
+     *
+     * \note The RuntimeScene already takes care of setting common properties ( Position, Angle... )
+     * according to the InitialInstance. You only need to initialize extra properties specific to your object.
      */
     virtual bool ExtraInitializationFromInitialInstance(const gd::InitialInstance & position) {return true;}
 
     /**
-     * Draw the object.
+     * \brief Draw the object.
      * \param renderTarget The SFML Rendertarget where object must be drawn.
      */
     virtual bool Draw(sf::RenderTarget & renderTarget) {return true;};
@@ -94,12 +97,12 @@ public:
     ///@{
 
     /**
-     * Provide access to the gd::VariablesContainer member containing the layout variables
+     * \brief Provide access to the gd::VariablesContainer member containing the object variables.
      */
     inline const gd::VariablesContainer & GetVariables() const { return objectVariables; }
 
     /**
-     * Provide access to the gd::VariablesContainer member containing the layout variables
+     * \brief Provide access to the gd::VariablesContainer member containing the object variables.
      */
     inline gd::VariablesContainer & GetVariables() { return objectVariables; }
 
@@ -111,12 +114,12 @@ public:
      */
     ///@{
     /**
-     * Call each automatism before events
+     * \brief Call each automatism so that they do their work before events
      */
     void DoAutomatismsPreEvents(RuntimeScene & scene);
 
     /**
-     * Call each automatism after events
+     * \brief Call each automatism so that they do their work after the events were runn.
      */
     void DoAutomatismsPostEvents(RuntimeScene & scene);
 
@@ -131,125 +134,128 @@ public:
     gd::Automatism* GetAutomatismRawPointer(const std::string & name) const;
 
     /**
-     * Return true if the object has the automatism with the specified name.
+     * \brief Return true if the object has the automatism with the specified name.
      */
     virtual bool HasAutomatismNamed(const std::string & name) const { return automatisms.find(name) != automatisms.end(); };
     ///@}
 
     /**
-     * Get the name of the object
+     * \brief Get the name of the object
      */
     inline const std::string & GetName() const { return name; };
 
     /**
-     * Get the type of the object
+     * \brief Get the type of the object
      */
     inline const std::string & GetType() const { return type; };
 
     /**
-     * Query the Z order of the object
+     * \brief Query the Z order of the object
      */
     inline int GetZOrder() const { return zOrder; }
 
     /**
-     * Change the Z order of the object
+     * \brief Change the Z order of the object
      */
     inline void SetZOrder(int zOrder_ ) { zOrder = zOrder_; }
 
     /**
-     * Return if the object is hidden or not
+     * \brief Return if the object is hidden or not
      */
     inline bool IsHidden() const {return hidden;};
 
     /**
-     * Return if the object is visible ( not hidden )
+     * \brief Return if the object is visible ( not hidden )
      */
     inline bool IsVisible() const {return !hidden;};
 
     /**
-     * Hide/Show the object
+     * \brief Hide/Show the object
      */
     inline void SetHidden(bool hide = true) {hidden = hide;};
 
     /**
-     * Change the layer of the object
+     * \brief Change the layer of the object
      */
     inline void SetLayer(const std::string & layer_) { layer = layer_;}
 
     /**
-     * Get the layer of the object
+     * \brief Get the layer of the object
      */
     inline const std::string & GetLayer() const { return layer; }
 
     /**
-     * Return true if object's layer is layer_.
+     * \brief Check if the object is on a layer.
      */
     inline bool IsOnLayer(const std::string & layer_) const { return layer == layer_; }
 
     /**
-     * Get object hitbox(es)
-     *
+     * \brief Get the object hitbox(es)
      * \note Default implementation returns a basic bounding box, according to the object width/height and angle.
      */
     virtual std::vector<Polygon2d> GetHitBoxes() const;
 
     /**
-     * Called at each frame so as to update internal object's things using time ( Such as animation for a sprite ).
+     * \brief Called at each frame so as to update internal object's things using time ( Such as animation for a sprite ).
+     * \note The default implementation does nothing.
      */
     virtual void UpdateTime(float timeElapsed) {};
 
     /**
-     * Get the width of the object
+     * \brief Get the width of the object
      */
     virtual float GetWidth() const {return 0;};
 
     /**
-     * Get the height of the object
+     * \brief Get the height of the object
      */
     virtual float GetHeight() const {return 0;};
 
     /**
-     * Set the new width of the object.
+     * \brief Set the new width of the object.
+     *
      * The width can be not changed if the object doesn't want/is not designed to.
      */
     virtual void SetWidth(float ) {};
 
     /**
-     * Set the new height of the object.
+     * \brief Set the new height of the object.
+     *
      * The height can be not changed if the object doesn't want/is not designed to.
      */
     virtual void SetHeight(float ) {};
 
     /**
-     * Set the new angle of the object.
+     * \brief Get the angle of the object, in degrees.
+     *
      * The angle can be not changed if the object doesn't want/is not designed to.
      */
     virtual bool SetAngle(float ) {return false;};
 
     /**
-     * Get the angle of the object
+     * \brief Get the angle of the object, in degrees.
      */
     virtual float GetAngle() const {return 0;};
 
     /**
-     * Get X Position of the object.
+     * \brief Get the X coordinate of the object in the layout.
      */
     inline float GetX() const { return X; }
 
     /**
-     * Get Y Position of the object.
+     * \brief Get the Y coordinate of the object in the layout.
      */
     inline float GetY() const { return Y; }
 
     /**
-     * Change X position. Call OnPositionChanged() to allow
-     * objects do special things if they want.
+     * \brief Change X position of the object.
+     * \note This method cannot be redefined: Redefine OnPositionChanged() to do extra work if needed.
      */
     void SetX(float x_) { X = x_; OnPositionChanged(); }
 
     /**
-     * Change Y position. Call OnPositionChanged() to allow
-     * objects do special things if they want.
+     * \brief Change Y position of the object.
+     * \note This method cannot be redefined: Redefine OnPositionChanged() to do extra work if needed.
      */
     void SetY(float y_) { Y = y_; OnPositionChanged(); }
 
@@ -260,26 +266,28 @@ public:
     virtual void OnPositionChanged() {};
 
     /**
-     * Get the real X position where is renderer the object.
-     * Used by the IDE to draw selection box for instance.
+     * \brief Get the real X position where is renderer the object.
+     *
+     * Most of the time, this will return the same value as GetX().<br>
+     * However, some objects may allow the origin point to be moved: In this case, you need to redefine this method.
      */
     virtual float GetDrawableX() const {return GetX();};
 
     /**
-     * Get the real Y position where is renderer the object.
-     * Used by the IDE to draw selection box for instance.
+     * \brief Get the real Y position where is renderer the object.
+     *
+     * Most of the time, this will return the same value as GetY().<br>
+     * However, some objects may allow the origin point to be moved: In this case, you need to redefine this method.
      */
     virtual float GetDrawableY() const {return GetY();};
 
     /**
-     * Get the X position of the center.
-     * Used by actions that move object for instance.
+     * \brief Get the X position of the center, relative to the position returned by GetDrawableX().
      */
     virtual float GetCenterX() const {return GetWidth()/2;};
 
     /**
-     * Get the Y position of the center.
-     * Used by actions that move object for instance.
+     * \brief Get the Y position of the center, relative to the position returned by GetDrawableY().
      */
     virtual float GetCenterY() const {return GetHeight()/2;};
 
@@ -305,7 +313,7 @@ public:
     ///@}
 
     /**
-     * Delete all forces applied to the object
+     * \brief Delete all forces applied to the object
      */
     bool ClearForce();
 
@@ -315,7 +323,8 @@ public:
      */
     ///@{
     /**
-     * Called by the debugger so as to get a property value and name
+     * \brief Called by the debugger so as to get a property value and name.
+     *
      * Implementation example:
      * \code
      * if      ( propertyNb == 0 ) {name = _("MyObjectProperty");      value = ToString(GetSomeProperty());}
@@ -325,7 +334,7 @@ public:
     virtual void GetPropertyForDebugger (unsigned int propertyNb, std::string & name, std::string & value) const;
 
     /**
-     * Called by the debugger so as to update a property
+     * \brief Called by the debugger so as to update a property
      * \return true if property was changed, false if it not possible.
      *
      * Implementation example:
@@ -337,7 +346,7 @@ public:
     virtual bool ChangeProperty(unsigned int propertyNb, std::string newValue);
 
     /**
-     * Must return the number of available properties for the debugger.
+     * \brief Must return the number of available properties for the debugger.
      */
     virtual unsigned int GetNumberOfProperties() const;
     ///@}
@@ -396,8 +405,8 @@ protected:
     std::vector < Force >                                   forces; ///< Forces applied to the object
 
     /**
-     * Initialize object using another object. Used by copy-ctor and assign-op.
-     * Don't forget to update me if members were changed !
+     * \brief Initialize object using another object. Used by copy-ctor and assign-op.
+     * \warning Don't forget to update me if members were changed !
      */
     void Init(const RuntimeObject & object);
 };

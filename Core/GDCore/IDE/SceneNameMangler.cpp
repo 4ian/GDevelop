19ef6bebb39251f9f89a2a310cf6f2ea2d1e8b42
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2013-09-24 19:27:27 +0000

    Disallowed figures as first characters of a mangled scene name ( Fixing issue with GDJS ).
    git-svn-id: svn://localhost@1182 8062f311-0dae-4547-b526-b8ab9ac864a5

--- Core/GDCore/IDE/SceneNameMangler.cpp
+++ Core/GDCore/IDE/SceneNameMangler.cpp
@@ -13,11 +13,14 @@ namespace gd
 std::string SceneNameMangler::GetMangledSceneName(const std::string & originalSceneName)
 {
     std::string partiallyMangledName = originalSceneName;
-    static const std::string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    static const std::string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static const std::string allowedExceptFirst = "0123456789";
 
-    for (size_t i = 0;i<partiallyMangledName.size();++i) //Replace all unallowed letter by an underscore and the ascii number of the letter
+    for (size_t i = 0;i<partiallyMangledName.size();++i) //Replace all unallowed letters by an underscore and the ascii number of the letter
     {
-        if ( allowedCharacters.find_first_of(partiallyMangledName[i]) == std::string::npos)
+        if ( allowedCharacters.find_first_of(partiallyMangledName[i]) == std::string::npos &&
+             (allowedExceptFirst.find_first_of(partiallyMangledName[i]) == std::string::npos ||
+              i == 0) ) //Also disallow some characters to be in first position
         {
             char unallowedChar = partiallyMangledName[i];
             partiallyMangledName.replace(i, 1, "_"+ToString(int(unallowedChar)));
--- Core/GDCore/IDE/SceneNameMangler.h
+++ Core/GDCore/IDE/SceneNameMangler.h
@@ -20,7 +20,9 @@ class GD_CORE_API SceneNameMangler
 public:
 
     /**
-     * Mangle the name of a scene, replacing all characters that are not 0-9, a-z, A-Z by "_"+AsciiCodeOfTheCharacter.
+     * \brief Mangle the name of a scene, replacing all characters that are not 0-9, a-z or A-Z 
+     * by "_"+AsciiCodeOfTheCharacter. The first character must be a letter, otherwise it is also
+     * replaced in the same manner.
      */
     static std::string GetMangledSceneName(const std::string & originalSceneName);
 

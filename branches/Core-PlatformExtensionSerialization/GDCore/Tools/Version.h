7f8dd984e2d0fed801670a0d01660a0c9345d154
Florian <Florian@8062f311-0dae-4547-b526-b8ab9ac864a5>
2013-11-02 15:33:15 +0000

    Version bump.
    git-svn-id: svn://localhost@1212 8062f311-0dae-4547-b526-b8ab9ac864a5

--- branches/Core-PlatformExtensionSerialization/GDCore/Tools/Version.h
+++ branches/Core-PlatformExtensionSerialization/GDCore/Tools/Version.h
@@ -4,8 +4,8 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char GDCore_DATE[] = "7";
-	static const char GDCore_MONTH[] = "10";
+	static const char GDCore_DATE[] = "2";
+	static const char GDCore_MONTH[] = "11";
 	static const char GDCore_YEAR[] = "2013";
 	
 	//Software Status
@@ -15,13 +15,13 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long GDCore_MAJOR = 3;
 	static const long GDCore_MINOR = 2;
-	static const long GDCore_BUILD = 63;
+	static const long GDCore_BUILD = 64;
 	static const long GDCore_REVISION = 0;
 	
 	//Miscellaneous Version Types
-	#define GDCore_RC_FILEVERSION 3,2,63,0
-	#define GDCore_RC_FILEVERSION_STRING "3, 2, 63, 0\0"
-	static const char GDCore_FULLVERSION_STRING[] = "3.2.63.0";
+	#define GDCore_RC_FILEVERSION 3,2,64,0
+	#define GDCore_RC_FILEVERSION_STRING "3, 2, 64, 0\0"
+	static const char GDCore_FULLVERSION_STRING[] = "3.2.64.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long GDCore_BUILD_HISTORY = 0;

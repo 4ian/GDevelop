14b696d7091614a249a8f50959581bec99634d03
Florian Rival <Florian.rival@gmail.com>
2014-06-30 21:52:01 +0200

    Removed useless files.

--- Core/GDCore/Bindings/Embind.h
+++
@@ -1,8 +0,0 @@
-/*
- * Game Develop Core
- * Copyright 2008-2014 Florian Rival (Florian.Rival@gmail.com). All rights reserved.
- * This project is released under the GNU Lesser General Public License.
- */
-
-#if defined(EMSCRIPTEN)
-#endif

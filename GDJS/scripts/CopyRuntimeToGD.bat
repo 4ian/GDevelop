7eb4c26c5327fd7d0ad4a70a582bd1c6f850baed
Florian Rival <Florian.rival@gmail.com>
2014-08-14 21:45:55 +0200

    Updated Windows scripts integrated to the build process. Fix forces in GDJS.

--- GDJS/scripts/CopyRuntimeToGD.bat
+++ GDJS/scripts/CopyRuntimeToGD.bat
@@ -1,19 +1,26 @@
-::This script copies the runtimes files ( i.e: the javascript files located into the Runtime
-::folder and in the Extensions folder ) to the Game Develop folder ( See below )
+::This script copies the runtimes files (i.e: the javascript files located into the Runtime
+::folder and in the Extensions folder) to the Game Develop folder.
 
 @echo off
+cd /d %~dp0
+
+echo Copying GDJS and extensions runtime files (*.js) to output directory...
 set destDir="..\..\Binaries\Output\RelWithDebInfo_Windows\JsPlatform"
-xcopy ..\Runtime\* %destDir%\Runtime\* /S /E  /D /Y
-xcopy ..\..\Extensions\*.js %destDir%\Runtime\Extensions\*.js /S /E  /D /Y /EXCLUDE:FilesExcludedFromCopy
+xcopy ..\Runtime\* %destDir%\Runtime\* /S /E  /D /Y /Q
+xcopy ..\..\Extensions\*.js %destDir%\Runtime\Extensions\*.js /S /E  /D /Y /Q /EXCLUDE:FilesExcludedFromCopy
 set destDir="..\..\Binaries\Output\Release_Windows\JsPlatform"
-xcopy ..\Runtime\* %destDir%\Runtime\* /S /E  /D /Y
-xcopy ..\..\Extensions\*.js %destDir%\Runtime\Extensions\*.js /S /E  /D /Y /EXCLUDE:FilesExcludedFromCopy
+xcopy ..\Runtime\* %destDir%\Runtime\* /S /E  /D /Y /Q
+xcopy ..\..\Extensions\*.js %destDir%\Runtime\Extensions\*.js /S /E  /D /Y /Q /EXCLUDE:FilesExcludedFromCopy
 set destDir="..\..\Binaries\Output\Debug_Windows\JsPlatform"
-xcopy ..\Runtime\* %destDir%\Runtime\* /S /E  /D /Y
-xcopy ..\..\Extensions\*.js %destDir%\Runtime\Extensions\*.js /S /E  /D /Y /EXCLUDE:FilesExcludedFromCopy
+xcopy ..\Runtime\* %destDir%\Runtime\* /S /E  /D /Y /Q
+xcopy ..\..\Extensions\*.js %destDir%\Runtime\Extensions\*.js /S /E  /D /Y /Q /EXCLUDE:FilesExcludedFromCopy
 
 ::You may want to change this or add new similar lines to copy the runtime and the extensions
 ::to your Game Develop directory.
 set destDir="C:\Users\Florian\AppData\Local\Temp\GDTemporaries\JSPreview"
-xcopy ..\Runtime\*.js %destDir%\*.js /S /E  /D /Y
-xcopy ..\..\Extensions\*.js %destDir%\Extensions\*.js /S /E  /D /Y /EXCLUDE:FilesExcludedFromCopy
+IF EXIST %destDir% (
+	xcopy ..\Runtime\*.js %destDir%\*.js /S /E  /D /Y /Q
+	xcopy ..\..\Extensions\*.js %destDir%\Extensions\*.js /S /E  /D /Y /EXCLUDE:FilesExcludedFromCopy /Q
+)
+
+echo Done.

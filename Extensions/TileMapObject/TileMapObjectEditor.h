a0af4086d9b091710c6821317c4f16b1ec4df000
victorlevasseur <victorlevasseur52@gmail.com>
2014-09-02 22:07:48 +0200

    Add/Update license text

--- Extensions/TileMapObject/TileMapObjectEditor.h
+++ Extensions/TileMapObject/TileMapObjectEditor.h
@@ -1,8 +1,7 @@
 /**
 
-Game Develop - Tiled Sprite Extension
-Copyright (c) 2012 Victor Levasseur (victorlevasseur01@orange.fr)
-Copyright (c) 2013-2014 Florian Rival (Florian.Rival@gmail.com)
+Game Develop - Tile Map Extension
+Copyright (c) 2014 Victor Levasseur (victorlevasseur52@gmail.com)
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages

b7e002b3eaba69d37b7733310f03f31c22a36d24
Florian Rival <Florian.Rival@gmail.com>
2014-10-26 15:08:14 +0100

    Ensure versioned files of Release_Windows are copied to other targets directory when building IDE

--- scripts/CopyWindowsToLinuxReleaseFiles.sh
+++ scripts/CopyWindowsToLinuxReleaseFiles.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 #This script copy all (versioned) files from Release_Windows into Release_Linux.
 
-echo "Copying versioned files from Release_Windows to Release_Linux..."
+#Get the destination, or copy by default to Release_Linux directory
+DESTINATION=Release_Linux
+if [ ! $# -eq 0 ]; then
+	DESTINATION=$1
+fi
+
+echo "Copying versioned files from Release_Windows to '$DESTINATION'..."
 
 cd ../Binaries/Output/Release_Windows
 git archive --format tar.gz --output ../allRuntimeFiles.tar master
 cd ..
-tar -xzf allRuntimeFiles.tar -C Release_Linux
+tar -xzf allRuntimeFiles.tar -C "$DESTINATION"
 rm allRuntimeFiles.tar
 cd ../../scripts/
 

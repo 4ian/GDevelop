a289f05a4c0f7d411d109bf8501e8fbb86a18f36
Florian Rival <Florian.rival@gmail.com>
2015-01-26 22:29:30 +0100

    Ensure SFML dependencies dlls and wxWidgets dlls are copied when building for Windows.

--- IDE/scripts/copyWindowsLibraries.bat
+++ IDE/scripts/copyWindowsLibraries.bat
@@ -1,3 +1,14 @@
+::SFML
 echo "Copying SFML files to " %1 "..."
-xcopy "%2"\*.dll "%1"\*.dll /D /Y /Q
-xcopy "%2"\*.a "%1"\*.a /D /Y /Q
+echo xcopy "%2"\SFML\lib\*.dll "%1"\*.dll
+xcopy "%2"\SFML\lib\*.dll "%1"\*.dll /D /Y /Q
+xcopy "%2"\SFML\lib\*.a "%1"\*.a /D /Y /Q
+
+::SFML dependencies
+set SFML_DIR=../../ExtLibs/SFML
+xcopy "%SFML_DIR%"\extlibs\bin\x86\*.dll "%1"\*.dll /D /Y /Q
+
+::wxWidgets
+echo "Copying wxWidgets files to " %1 "..."
+set WX_LIB_DIR=../../ExtLibs/wxwidgets
+xcopy "%WX_LIB_DIR%"\lib\gcc_dll\*.dll "%1"\*.dll /D /Y /Q

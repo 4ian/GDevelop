c9cb5faa092c3fd7be04911ea9caf40b13f9a5da
victorlevasseur <victorlevasseur52@gmail.com>
2015-03-29 20:06:36 +0200

    Add GD_T as a gettext keyword when generating pot file

--- scripts/ExtractTranslations.sh
+++ scripts/ExtractTranslations.sh
@@ -14,6 +14,6 @@ find ../Core/GDCore -name '*.cpp' >> /tmp/listfile.txt
 find ../Core/GDCore -name '*.h' -o -name "*.hpp" >> /tmp/listfile.txt
 
 echo "Generating .POT file..."
-xgettext --from-code iso-8859-1  -o source.pot --c++ --no-wrap -f /tmp/listfile.txt -k_
+xgettext --from-code iso-8859-1  -o source.pot --c++ --keyword=GD_T --no-wrap -f /tmp/listfile.txt -k_
 
 echo "source.pot file generated and ready to be sent to Crowdin or used in a translation software like PoEdit."

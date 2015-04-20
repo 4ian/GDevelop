cc07b135cb5ff6fc274ef4c8e7d527a1a9c71aaf
Florian Rival <Florian.Rival@gmail.com>
2015-04-20 15:59:34 +1200

    Version bump

--- Core/GDCore/Tools/Version.h
+++ Core/GDCore/Tools/Version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char GDCore_DATE[] = "3";
+	static const char GDCore_DATE[] = "20";
 	static const char GDCore_MONTH[] = "04";
 	static const char GDCore_YEAR[] = "2015";
 
@@ -15,12 +15,12 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long GDCore_MAJOR = 3;
 	static const long GDCore_MINOR = 6;
-	static const long GDCore_BUILD = 79;
+	static const long GDCore_BUILD = 80;
 	static const long GDCore_REVISION = 0;
 
 	//Miscellaneous Version Types
-	#define GDCore_RC_FILEVERSION 3,6,79,0
-	#define GDCore_RC_FILEVERSION_STRING "3, 6, 79, 0\0"
-	static const char GDCore_FULLVERSION_STRING[] = "3.6.79.0";
+	#define GDCore_RC_FILEVERSION 3,6,80,0
+	#define GDCore_RC_FILEVERSION_STRING "3, 6, 80, 0\0"
+	static const char GDCore_FULLVERSION_STRING[] = "3.6.80.0";
 }
 #endif //GDCORE_VERSION_H

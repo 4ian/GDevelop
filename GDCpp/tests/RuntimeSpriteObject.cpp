17dc3df0c1b864ff18aae67eb7cdc947ddef1d76
Florian Rival <Florian.Rival@gmail.com>
2015-06-02 17:04:55 +1200

    Add a few tests for Sprite objects

---
+++ GDCpp/tests/RuntimeSpriteObject.cpp
@@ -0,0 +1,49 @@
+/*
+ * GDevelop C++ Platform
+ * Copyright 2008-2015 Florian Rival (Florian.Rival@gmail.com). All rights reserved.
+ * This project is released under the MIT License.
+ */
+/**
+ * @file Tests covering common features of GDevelop C++ Platform.
+ */
+#include "catch.hpp"
+#include "GDCore/CommonTools.h"
+#include "GDCore/PlatformDefinition/ClassWithObjects.h"
+#include "GDCore/PlatformDefinition/Layout.h"
+#include "GDCore/PlatformDefinition/Object.h"
+#include "GDCore/BuiltinExtensions/SpriteExtension/SpriteObject.h"
+#include "GDCore/BuiltinExtensions/SpriteExtension/Animation.h"
+#include "GDCpp/RuntimeScene.h"
+#include "GDCpp/RuntimeGame.h"
+#include "GDCpp/RuntimeObject.h"
+#include "GDCpp/ObjectsListsTools.h"
+#include "GDCpp/RuntimeSpriteObject.h"
+
+TEST_CASE( "RuntimeSpriteObject", "[game-engine]" ) {
+	RuntimeGame game;
+	RuntimeScene scene(NULL, &game);
+
+	gd::SpriteObject obj1("SpriteObject");
+	gd::Animation anim;
+	gd::Sprite sprite;
+	sprite.SetImageName("Image.png");
+	anim.SetDirectionsCount(1);
+	anim.GetDirection(0).AddSprite(sprite);
+	obj1.AddAnimation(anim);
+
+	RuntimeSpriteObject object(scene, obj1);
+	SECTION("Scaling") {
+		REQUIRE(object.GetScaleX() == 1);
+		object.FlipX(true);
+		REQUIRE(object.GetScaleX() == 1);
+		object.SetScaleX(0.42);
+		REQUIRE(object.GetScaleX() == 0.42f);
+		object.FlipX(false);
+		REQUIRE(object.GetScaleX() == 0.42f);
+	}
+	SECTION("Angle") {
+		REQUIRE(object.GetAngle() == 0);
+		object.SetAngle(42);
+		REQUIRE(object.GetAngle() == 42);
+	}
+}
---
+++ GDJS/tests/tests/spriteruntimeobject.js
@@ -0,0 +1,21 @@
+/**
+ * Common tests for gdjs game engine.
+ * See README.md for more information.
+ */
+
+describe('gdjs.SpriteRuntimeObject', function() {
+	var runtimeGame = new gdjs.RuntimeGame({variables: [], properties: {windowWidth: 800, windowHeight: 600}});
+	var runtimeScene = new gdjs.RuntimeScene(runtimeGame, null);
+
+	it('should handle scaling properly', function(){
+		var object = new gdjs.SpriteRuntimeObject(runtimeScene, {name: "obj1", type: "", automatisms: []});
+
+		expect(object.getScaleX()).to.be(1);
+		object.flipX(true);
+		expect(object.getScaleX()).to.be(1);
+		object.setScaleX(0.42);
+		expect(object.getScaleX()).to.be(0.42);
+		object.flipX(false);
+		expect(object.getScaleX()).to.be(0.42);
+	});
+});

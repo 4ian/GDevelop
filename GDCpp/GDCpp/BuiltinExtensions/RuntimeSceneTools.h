3a64b7ef30c747640c6af7f00431cd00cd8d5fc6
Florian Rival <Florian.Rival@gmail.com>
2015-08-03 21:41:45 +1200

    Fix typo in ReplaceScene function prototype name

--- GDCpp/GDCpp/BuiltinExtensions/RuntimeSceneTools.h
+++ GDCpp/GDCpp/BuiltinExtensions/RuntimeSceneTools.h
@@ -36,7 +36,7 @@ void GD_API StopGame(RuntimeScene & scene);
 /**
  * Only used internally by GD events generated code.
  */
-void GD_API ChangeScene(RuntimeScene & scene, std::string newSceneName);
+void GD_API ReplaceScene(RuntimeScene & scene, std::string newSceneName);
 
 /**
  * Only used internally by GD events generated code.

200b0fb80038ab4f66457c5c26cb8403707830d0
Florian Rival <Florian.Rival@gmail.com>
2015-08-06 15:50:16 +1200

    Add TileMap object to translations catalog

--- scripts/ExtractTranslations.bat
+++ scripts/ExtractTranslations.bat
@@ -7,8 +7,8 @@ dir ..\GDCpp\GDCpp\*.cpp /L /B /S >> %TEMP%\listfile.txt
 dir ..\GDCpp\GDCpp\*.h /L /B /S >> %TEMP%\listfile.txt
 dir ..\GDJS\GDJS\*.cpp /L /B /S >> %TEMP%\listfile.txt
 dir ..\GDJS\GDJS\*.h /L /B /S >> %TEMP%\listfile.txt
-dir ..\Extensions\*.cpp /L /B /S |find /v /i "\TileMapObject\"  >> %TEMP%\listfile.txt
-dir ..\Extensions\*.h /L /B /S |find /v /i "\TileMapObject\"  >> %TEMP%\listfile.txt
+dir ..\Extensions\*.cpp /L /B /S  >> %TEMP%\listfile.txt
+dir ..\Extensions\*.h /L /B /S  >> %TEMP%\listfile.txt
 dir ..\Core\GDCore\*.cpp /L /B /S >> %TEMP%\listfile.txt
 dir ..\Core\GDCore\*.h /L /B /S >> %TEMP%\listfile.txt
 

da4dc223f73cc1017f5a73449170c43a733f9009
Victor Levasseur <victorlevasseur52@gmail.com>
2016-04-09 13:32:48 +0200

    Fix box2d.js crash when changing scenes
    (use an updated version of box2d.js)

--- Extensions/PhysicsBehavior/physicsruntimebehavior.js
+++ Extensions/PhysicsBehavior/physicsruntimebehavior.js
@@ -26,49 +26,44 @@ gdjs.PhysicsSharedData = function(runtimeScene, sharedData)
     this.staticBody = this.world.CreateBody(new Box2D.b2BodyDef());
 
     //...and prepare contact listeners
-    this.contactListener = new Box2D.b2ContactListener();
-    Box2D.customizeVTable(this.contactListener, [{
-        original: Box2D.b2ContactListener.prototype.BeginContact,
-        replacement:
-            function (thsPtr, contactPtr) {
-                var contact = Box2D.wrapPointer( contactPtr, Box2D.b2Contact );
+	this.contactListener = new Box2D.JSContactListener();
+    this.contactListener.BeginContact = function (contactPtr) {
+        var contact = Box2D.wrapPointer( contactPtr, Box2D.b2Contact );
 
-                if ( contact.GetFixtureA().GetBody() == null ||
-                     contact.GetFixtureB().GetBody() == null )
-                    return;
+        if ( contact.GetFixtureA().GetBody() == null ||
+             contact.GetFixtureB().GetBody() == null )
+            return;
 
-                var behaviorA = contact.GetFixtureA().GetBody().gdjsAssociatedBehavior,
-                behaviorB = contact.GetFixtureB().GetBody().gdjsAssociatedBehavior;
+        var behaviorA = contact.GetFixtureA().GetBody().gdjsAssociatedBehavior,
+        behaviorB = contact.GetFixtureB().GetBody().gdjsAssociatedBehavior;
 
-                behaviorA.currentContacts.push(behaviorB);
-                behaviorB.currentContacts.push(behaviorA);
-            }
-    }]);
+        behaviorA.currentContacts.push(behaviorB);
+        behaviorB.currentContacts.push(behaviorA);
+    };
 
-    Box2D.customizeVTable(this.contactListener, [{
-        original: Box2D.b2ContactListener.prototype.EndContact,
-        replacement:
-            function (thsPtr, contactPtr) {
-                var contact = Box2D.wrapPointer( contactPtr, Box2D.b2Contact );
+    this.contactListener.EndContact = function (contactPtr) {
+        var contact = Box2D.wrapPointer( contactPtr, Box2D.b2Contact );
 
-                if ( contact.GetFixtureA().GetBody() == null ||
-                     contact.GetFixtureB().GetBody() == null )
-                    return;
+        if ( contact.GetFixtureA().GetBody() == null ||
+             contact.GetFixtureB().GetBody() == null )
+            return;
 
-                if ( contact.GetFixtureA().GetBody() === null ||
-                    contact.GetFixtureB().GetBody() === null )
-                return;
+        if ( contact.GetFixtureA().GetBody() === null ||
+            contact.GetFixtureB().GetBody() === null )
+        return;
 
-                var behaviorA = contact.GetFixtureA().GetBody().gdjsAssociatedBehavior,
-                behaviorB = contact.GetFixtureB().GetBody().gdjsAssociatedBehavior;
+        var behaviorA = contact.GetFixtureA().GetBody().gdjsAssociatedBehavior,
+        behaviorB = contact.GetFixtureB().GetBody().gdjsAssociatedBehavior;
 
-                var i = behaviorA.currentContacts.indexOf(behaviorB);
-                if ( i !== -1 ) behaviorA.currentContacts.remove(i);
+        var i = behaviorA.currentContacts.indexOf(behaviorB);
+        if ( i !== -1 ) behaviorA.currentContacts.remove(i);
 
-                i = behaviorB.currentContacts.indexOf(behaviorA);
-                if ( i !== -1 ) behaviorB.currentContacts.remove(i);
-            }
-    }]);
+        i = behaviorB.currentContacts.indexOf(behaviorA);
+        if ( i !== -1 ) behaviorB.currentContacts.remove(i);
+    };
+
+	this.contactListener.PreSolve = function() {};
+	this.contactListener.PostSolve = function() {};
 
 	this.world.SetContactListener(this.contactListener);
 };

13d5069bd321ed26f3f93118f17231c52fba1e56
Victor Levasseur <victorlevasseur52@gmail.com>
2016-04-11 19:36:21 +0200

    Update Application.mk for the lastest NDK

--- GDCpp/Runtime/Android/jni/Application.mk
+++ GDCpp/Runtime/Android/jni/Application.mk
@@ -1,6 +1,5 @@
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-9
 APP_STL := c++_shared
-# APP_ABI := armeabi-v7a
-APP_ABI := x86
+APP_ABI := armeabi armeabi-v7a x86
 APP_MODULES := sfml-activity sfml-example

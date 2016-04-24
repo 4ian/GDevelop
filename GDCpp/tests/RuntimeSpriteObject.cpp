6820a3eb10ae01309f2629c7fa760c4713b6f8cf
Florian Rival <Florian.Rival@gmail.com>
2016-04-24 17:06:08 +0200

    Add test for named animations of RuntimeSpriteObject

--- GDCpp/tests/RuntimeSpriteObject.cpp
+++ GDCpp/tests/RuntimeSpriteObject.cpp
@@ -24,12 +24,24 @@ TEST_CASE( "RuntimeSpriteObject", "[game-engine]" ) {
 	RuntimeScene scene(NULL, &game);
 
 	gd::SpriteObject obj1("SpriteObject");
-	gd::Animation anim;
-	gd::Sprite sprite;
-	sprite.SetImageName("Image.png");
-	anim.SetDirectionsCount(1);
-	anim.GetDirection(0).AddSprite(sprite);
-	obj1.AddAnimation(anim);
+	{
+		gd::Animation anim;
+		anim.SetName("First animation");
+		gd::Sprite sprite;
+		sprite.SetImageName("Image.png");
+		anim.SetDirectionsCount(1);
+		anim.GetDirection(0).AddSprite(sprite);
+		obj1.AddAnimation(anim);
+	}
+	{
+		gd::Animation anim;
+		anim.SetName("Second animation");
+		obj1.AddAnimation(anim);
+	}
+	{
+		gd::Animation anim;
+		obj1.AddAnimation(anim);
+	}
 
 	RuntimeSpriteObject object(scene, obj1);
 	SECTION("Scaling") {
@@ -47,5 +59,31 @@ TEST_CASE( "RuntimeSpriteObject", "[game-engine]" ) {
 		REQUIRE(object.GetAngle() == 0);
 		object.SetAngle(42);
 		REQUIRE(object.GetAngle() == 42);
+	}
+	SECTION("Animations") {
+		REQUIRE(object.GetCurrentAnimation() == 0);
+		REQUIRE(object.GetCurrentAnimationName() == "First animation");
+		REQUIRE(object.IsCurrentAnimationName("First animation") == true);
+
+		object.SetCurrentAnimation("Second animation");
+		REQUIRE(object.GetCurrentAnimation() == 1);
+		REQUIRE(object.GetCurrentAnimationName() == "Second animation");
+
+		SECTION("It keeps the same animation when using an invalid/empty name") {
+			object.SetCurrentAnimation("");
+			REQUIRE(object.GetCurrentAnimation() == 1);
+			object.SetCurrentAnimation("Invalid name");
+			REQUIRE(object.GetCurrentAnimation() == 1);
+		}
+
+		SECTION("It can change animation using animation index") {
+			object.SetCurrentAnimation(2);
+			REQUIRE(object.GetCurrentAnimation() == 2);
+			REQUIRE(object.GetCurrentAnimationName() == "");
+
+			object.SetCurrentAnimation(0);
+			REQUIRE(object.GetCurrentAnimation() == 0);
+			REQUIRE(object.GetCurrentAnimationName() == "First animation");
+		}
 	}
 }

dba2d55193eb95e19555296e9a0ea823bb6ac67a
Victor Levasseur <victorlevasseur52@gmail.com>
2016-11-11 12:49:09 +0100

    Fix again for resource.rc

--- Version/GenerateVersion.cmake
+++ Version/GenerateVersion.cmake
@@ -28,17 +28,25 @@ if(GIT_FOUND)
 		GD_VERSION_RC
 		"${GD_VERSION_STR}")
 
-	if(NOT ("${ORIGINAL_CONTENT}" STREQUAL "#define GD_VERSION_STRING \"${GD_VERSION_STR}\"\n#define GD_VERSION_RC ${GD_VERSION_RC}\n#define GD_DATE_STRING __DATE__"))
+	string(REGEX REPLACE
+		"([0-9]*)\\.([0-9]*)\\.([0-9]*)-([0-9]*)-.*"
+		"\\1, \\2, \\3, \\4"
+		GD_VERSION_RC_STR
+		"${GD_VERSION_STR}")
+
+	set(NEW_CONTENT "#define GD_VERSION_STRING \"${GD_VERSION_STR}\"\n#define GD_VERSION_RC ${GD_VERSION_RC}\n#define GD_VERSION_RC_STRING \"${GD_VERSION_RC_STR}\\0\"\n#define GD_DATE_STRING __DATE__")
+
+	if(NOT ("${ORIGINAL_CONTENT}" STREQUAL "${NEW_CONTENT}"))
 		# Write only the version file if different from the previous one
 		message(STATUS "Updating VersionPriv.h header to version ${GD_VERSION_STR}.")
 		file(WRITE
 			"${VERSIONPRIV_PATH}"
-			"#define GD_VERSION_STRING \"${GD_VERSION_STR}\"\n#define GD_VERSION_RC ${GD_VERSION_RC}\n#define GD_DATE_STRING __DATE__")
+			"${NEW_CONTENT}")
 	else()
 		message(STATUS "VersionPriv.h already up-to-date.")
 	endif()
 else()
 	file(WRITE
 		"${VERSIONPRIV_PATH}"
-		"#define GD_VERSION_STRING \"0.0.0-0-unknown\"\n#define GD_VERSION_RC 0, 0, 0, 0\n#define GD_DATE_STRING __DATE__")
+	"#define GD_VERSION_STRING \"0.0.0-0-unknown\"\n#define GD_VERSION_RC 0, 0, 0, 0\n#define GD_VERSION_RC_STRING \"0, 0, 0, 0\\0\"\n#define GD_DATE_STRING __DATE__")
 endif()

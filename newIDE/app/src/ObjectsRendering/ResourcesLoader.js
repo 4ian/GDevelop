6efdabdb3614f5701a2d6d1c3c608e1bbd7f7002
Florian Rival <Florian.Rival@gmail.com>
2017-03-03 21:29:28 +0100

    [WIP] Add cache for filenames of resources in newIDE

--- newIDE/app/src/ObjectsRendering/ResourcesLoader.js
+++ newIDE/app/src/ObjectsRendering/ResourcesLoader.js
@@ -6,48 +6,80 @@ const path = optionalRequire('path');
 const loadedFontFamilies = {
 };
 
+class FilenamesCache {
+  constructor() {
+    this.projectCache = {};
+  }
+
+  _getProjectCache(project) {
+    const cache = this.projectCache[project.ptr];
+    if (!cache) {
+      return this.projectCache[project.ptr] = {};
+    }
+
+    return cache;
+  }
+
+  getSystemFilename(project, filename) {
+    const cache = this._getProjectCache(project);
+    return cache[filename];
+  }
+
+  cacheSystemFilename(project, filename, systemFilename) {
+    const cache = this._getProjectCache(project);
+    return cache[filename] = systemFilename;
+  }
+}
+
 export default class ResourceLoader {
-    static _getSystemFullFilename(project, filename) {
-      if (electron) { // Support local filesystem with Electron
-        const file = project.getProjectFile();
-        const projectPath = path.dirname(file);
-        const resourceAbsolutePath = path.resolve(projectPath, filename);
-        console.log("Loading", resourceAbsolutePath);
-        return 'file://' + resourceAbsolutePath;
-      }
+  static _cache = new FilenamesCache();
 
-      return filename;
+  static _getSystemFullFilename(project, filename) {
+    const cachedSystemFilename =
+      ResourceLoader._cache.getSystemFilename(project, filename);
+    if (cachedSystemFilename) return cachedSystemFilename;
+
+    if (electron) { // Support local filesystem with Electron
+      const file = project.getProjectFile();
+      const projectPath = path.dirname(file);
+      const resourceAbsolutePath = path.resolve(projectPath, filename);
+
+      console.log("Loading", resourceAbsolutePath);
+      return this._cache.cacheSystemFilename(project, filename, 'file://' + resourceAbsolutePath);
     }
 
-    static get(project, resourceName) {
-      if (project.getResourcesManager().hasResource(resourceName)) {
-        const resourceRelativePath =
-          project.getResourcesManager().getResource(resourceName).getFile();
-        return ResourceLoader._getSystemFullFilename(project, resourceRelativePath);
-      }
+    return this._cache.cacheSystemFilename(project, filename, filename);
+  }
 
-      return resourceName;
+  static get(project, resourceName) {
+    if (project.getResourcesManager().hasResource(resourceName)) {
+      const resourceRelativePath =
+        project.getResourcesManager().getResource(resourceName).getFile();
+      return ResourceLoader._getSystemFullFilename(project, resourceRelativePath);
     }
 
-    static getFontFamily(project, fontFilename) {
-      if (!fontFilename) return "";
+    return resourceName;
+  }
 
-      if (loadedFontFamilies[fontFilename]) {
-        return loadedFontFamilies[fontFilename];
-      }
+  static getFontFamily(project, fontFilename) {
+    if (!fontFilename) return "";
 
-      const fontFamily = slug(fontFilename);
-      const fontFaceDeclaration =
-        "@font-face { font-family: '" + fontFamily + "';" +
-        "src: url(" + ResourceLoader._getSystemFullFilename(project, fontFilename) + "); }";
-      const declaration =
-        "<style type='text/css'>" +
-        fontFaceDeclaration +
-        "</style>";
-
-      document.querySelector('head').innerHTML += declaration;
-      return loadedFontFamilies[fontFilename] = fontFamily;
+    if (loadedFontFamilies[fontFilename]) {
+      return loadedFontFamilies[fontFilename];
     }
 
-    static getInvalidImageURL() { return "res/error48.png" }
+    const fontFamily = slug(fontFilename);
+    const fontFaceDeclaration =
+      "@font-face { font-family: '" + fontFamily + "';" +
+      "src: url(" + ResourceLoader._getSystemFullFilename(project, fontFilename) + "); }";
+    const declaration =
+      "<style type='text/css'>" +
+      fontFaceDeclaration +
+      "</style>";
+
+    document.querySelector('head').innerHTML += declaration;
+    return loadedFontFamilies[fontFilename] = fontFamily;
+  }
+
+  static getInvalidImageURL() { return "res/error48.png" }
 };

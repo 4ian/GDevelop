f76a5311432862ee65cc450f22e82141d66bec1c
Florian Rival <Florian.Rival@gmail.com>
2017-05-20 16:46:03 +0200

    Suppress a warning

--- newIDE/app/src/MainFrame/StartPage.js
+++ newIDE/app/src/MainFrame/StartPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 import BaseEditor from './BaseEditor'

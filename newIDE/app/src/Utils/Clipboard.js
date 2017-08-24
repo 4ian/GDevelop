f320f34b190415c64e7a76ccac3ed1329be930eb
Florian Rival <Florian.Rival@gmail.com>
2017-08-25 00:18:40 +0200

    Implement internal, in memory Clipboard for browsers

--- newIDE/app/src/Utils/Clipboard.js
+++ newIDE/app/src/Utils/Clipboard.js
@@ -2,41 +2,49 @@ import optionalRequire from './OptionalRequire.js';
 const electron = optionalRequire('electron');
 const electronClipboard = electron ? electron.clipboard : null;
 
+let internalClipboard = "";
+
 export default class Clipboard {
   static set(kind, object) {
+    const text = JSON.stringify({
+      '000kind': kind, /// 000 to ensure the key is written first.
+      content: object,
+    });
+
     if (electronClipboard) {
-      electronClipboard.writeText(
-        JSON.stringify({
-          '000kind': kind, /// 000 to ensure the key is written first.
-          content: object,
-        })
-      );
+      electronClipboard.writeText(text);
+    } else {
+      internalClipboard = text;
     }
   }
 
   static has(kind) {
+    let text = "";
     if (electronClipboard) {
-      const text = electronClipboard.readText();
-      return text.indexOf(kind) === 12; /// 12 is the position of '000kind' value
+      text = electronClipboard.readText();
+    } else {
+      text = internalClipboard;
     }
 
-    return false;
+    return text.indexOf(kind) === 12; /// 12 is the position of '000kind' value
   }
 
   static get(kind) {
     if (!Clipboard.has(kind)) return null;
 
+    let text = "";
     if (electronClipboard) {
-      const text = electronClipboard.readText();
-      try {
-        const parsedText = JSON.parse(text);
-        return parsedText.content;
-      } catch (e) {
-        console.warn('The clipboard content is not valid JSON');
-        return null;
-      }
+      text = electronClipboard.readText();
+    } else {
+      text = internalClipboard;
     }
 
-    return null;
+    try {
+      const parsedText = JSON.parse(text);
+      return parsedText.content;
+    } catch (e) {
+      console.warn('The clipboard content is not valid JSON');
+      return null;
+    }
   }
 }

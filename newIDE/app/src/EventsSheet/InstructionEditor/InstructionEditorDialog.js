69d85f9db22d41de282f927f61b9f26ffe567ab1
Todor Imreorov <blurymind@gmail.com>
2017-10-11 00:26:47 +0100

    fix regression
    it seems that uncommenting that line created a regression bug where in some corner cases the page would refresh..
    So instead of uncommenting it, I am setting it to false- that seems to fix it.

--- newIDE/app/src/EventsSheet/InstructionEditor/InstructionEditorDialog.js
+++ newIDE/app/src/EventsSheet/InstructionEditor/InstructionEditorDialog.js
@@ -35,7 +35,7 @@ export default class InstructionEditorDialog extends React.Component {
       <FlatButton
         label="Ok"
         primary={true}
-        //keyboardFocused={true}
+        keyboardFocused={false}
         onTouchTap={this.props.onSubmit}
       />,
     ];

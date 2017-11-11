b03bf86a35226eaefe6637d8a6b71d46ba6f2040
Florian Rival <Florian.Rival@gmail.com>
2017-11-11 18:33:49 +0100

    Add error messages when an expression is invalid in ExpressionField or StringField in newIDE

--- newIDE/app/src/EventsSheet/InstructionEditor/ParameterFields/ExpressionField.js
+++ newIDE/app/src/EventsSheet/InstructionEditor/ParameterFields/ExpressionField.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
+const gd = global.gd;
 
 const styles = {
   input: {
@@ -8,9 +9,28 @@ const styles = {
 };
 
 export default class ExpressionField extends Component {
+  state = { errorText: null };
+
   focus() {
     if (this._field) this._field.focus();
   }
+
+  getError = () => {
+    const { project, layout } = this.props;
+
+    const callbacks = new gd.CallbacksForExpressionCorrectnessTesting(project, layout);
+    const parser = new gd.ExpressionParser(this.props.value);
+    const isValid = parser.parseMathExpression(project.getCurrentPlatform(), project, layout, callbacks);
+    const error = parser.getFirstError();
+    parser.delete();
+    callbacks.delete();
+
+    return isValid ? null : error;
+  }
+
+  doValidation = () => {
+    this.setState({ errorText: this.getError() });
+  };
 
   render() {
     const { parameterMetadata } = this.props;
@@ -26,6 +46,8 @@ export default class ExpressionField extends Component {
         onChange={(e, text) => this.props.onChange(text)}
         ref={field => (this._field = field)}
         fullWidth
+        errorText={this.state.errorText}
+        onBlur={this.doValidation}
       />
     );
   }

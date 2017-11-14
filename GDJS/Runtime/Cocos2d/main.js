98d970de30b09039d2b1129def0fabd39bd3bf85
Florian Rival <Florian.Rival@gmail.com>
2017-11-14 23:32:49 +0100

    Add polyfill for self and patch XMLHttpRequest.send for Cocos2d-JS 3.15 and below

--- GDJS/Runtime/Cocos2d/main.js
+++ GDJS/Runtime/Cocos2d/main.js
@@ -1,3 +1,24 @@
+// "Polyfill" self which is not defined in Cocos2d-JS (v3.10)
+// and used by some library like Shopify SDK.
+if (typeof self === 'undefined') {
+    console.log("Add polyfill for 'self'");
+    self = window;
+}
+
+// Patch XMLHttpRequest.send as Cocos2d-JS v3.15 and below are not supporting
+// passing null or undefined as parameter, even if it is compliant to do so.
+if (typeof XMLHttpRequest !== 'undefined') {
+    console.log("Patching XMLHttpRequest for Cocos2d-JS v3.15 and below");
+    var originalSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function(body) {
+        console.log("(Using patched XMLHttpRequest.send)");
+        if (body === null || body === undefined) {
+            return originalSend.call(this);
+        }
+        originalSend.apply(this, arguments);
+    }
+}
+
 cc.game.onStart = function(){
     if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
         document.body.removeChild(document.getElementById("cocosLoading"));

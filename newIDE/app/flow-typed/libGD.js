0ac3e79156ca72a660a1b107e147db1cd00bfc52
Florian Rival <Florian.Rival@gmail.com>
2017-12-16 14:24:25 +0100

    Fix flow typing in newIDE and update flow to latest version

---
+++ newIDE/app/flow-typed/libGD.js
@@ -0,0 +1,10 @@
+// @flow
+
+//TODO: These types could be generated from GDevelop.js instead of being
+//manually written here.
+declare type gdProject = Object;
+declare type gdLayout = Object;
+declare type gdExternalLayout = Object;
+
+//Represents all objects that have serializeTo and unserializeFrom methods.
+declare type gdSerializable = Object;

dc3d89093768f4fd871b949831344ccdb866f482
Florian Rival <Florian.Rival@gmail.com>
2017-12-23 23:14:02 +0100

    Add comment explaining how to create a new theme

--- newIDE/app/src/UI/Theme/index.js
+++ newIDE/app/src/UI/Theme/index.js
@@ -1,6 +1,10 @@
 import DarkTheme from './DarkTheme';
 import DefaultTheme from './DefaultTheme';
 
+// To add a new theme:
+// * copy the folder of an existing one (DefaultTheme for example),
+// * import it at the top of the file
+// * add it below:
 export const themes = {
   'GDevelop default': DefaultTheme,
   Dark: DarkTheme,

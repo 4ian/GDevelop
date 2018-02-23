1c1860370a0acde73a665ba82049863d44beb8db
Florian Rival <Florian.Rival@gmail.com>
2018-02-23 15:24:35 +0100

    Prevent pinch-to-zoom on exported games/preview and set black address bar

--- newIDE/app/src/Export/BrowserExporters/GDJSindex.html.js
+++ newIDE/app/src/Export/BrowserExporters/GDJSindex.html.js
@@ -1,5 +1,12 @@
 //TODO: Download the file then read it instead of hardcoding it.
+
 export default `<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <meta name="theme-color" content="#000000" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
 	<style>
 		body {

4ccbc1b95857d8eb723ab51e04258236aa52165c
Florian Rival <Florian.Rival@gmail.com>
2018-03-30 22:01:21 +0200

    [WIP] Add preview of collision mask polygons

--- newIDE/app/src/ObjectEditor/Editors/SpriteEditor/CollisionMasksEditor/CollisionMasksPreview.js
+++ newIDE/app/src/ObjectEditor/Editors/SpriteEditor/CollisionMasksEditor/CollisionMasksPreview.js
@@ -1,40 +1,54 @@
 // @flow
 import React from 'react';
 import { mapVector } from '../../../../Utils/MapFor';
+import Measure from 'react-measure';
 
 const styles = {
   container: {
     position: 'relative',
   },
+  svg: {
+    width: '100%',
+    height: '100%',
+  },
 };
 
 type Props = {|
   polygons: gdVectorPolygon2d,
+  imageWidth: number,
+  imageHeight: number,
 |};
 
 export default class CollisionMasksPreview extends React.Component<
   Props,
   void
 > {
+
   render() {
     const { polygons } = this.props;
 
     return (
-      <div style={styles.container}>
-        <svg>
-          {mapVector(polygons, polygon => {
-            const vertices = polygon.getVertices();
-            return mapVector(vertices, vertex => (
-              <circle
-                cx={vertex.get_x()}
-                cy={vertex.get_y()}
-                r="3"
-                fill="blue"
-              />
-            ));
-          })}
-        </svg>
-      </div>
+      <svg
+        width={this.props.imageWidth}
+        height={this.props.imageHeight}
+        style={styles.svg}
+      >
+        {mapVector(polygons, (polygon, i) => {
+          const vertices = polygon.getVertices();
+          return (
+            <polygon
+              fill="rgba(255,0,0,0.2)"
+              stroke="rgba(255,0,0,0.5)"
+              strokeWidth={1}
+              fileRule="evenodd"
+              points={mapVector(
+                vertices,
+                (vertex, j) => `${vertex.get_x()},${vertex.get_y()}`
+              ).join(' ')}
+            />
+          );
+        })}
+      </svg>
     );
   }
 }

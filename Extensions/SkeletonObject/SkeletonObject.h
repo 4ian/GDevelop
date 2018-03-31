50746b3c756aaf3d0d63fcd4d376f23f0663eb53
Lizard-13 <thelizard-13@hotmail.com>
2018-03-30 23:25:11 -0300

    Still trying to fix Travis CI

--- Extensions/SkeletonObject/SkeletonObject.h
+++ Extensions/SkeletonObject/SkeletonObject.h
@@ -23,20 +23,29 @@ public:
     virtual ~SkeletonObject() {};
     virtual std::unique_ptr<gd::Object> Clone() const { return gd::make_unique<SkeletonObject>(*this); }
 
-    #if !defined(GD_NO_WX_GUI)
+    #if defined(GD_IDE_ONLY)
     static void LoadEdittimeIcon();
+
+    #if !defined(GD_NO_WX_GUI)
     bool GenerateThumbnail(const gd::Project & project, wxBitmap & thumbnail) const override;
+    #endif
+
     void DrawInitialInstance(gd::InitialInstance & instance, sf::RenderTarget & renderTarget, gd::Project & project, gd::Layout & layout) override;
     sf::Vector2f GetInitialInstanceOrigin(gd::InitialInstance & instance, gd::Project & project, gd::Layout & layout) const override;
+    
+    #if !defined(GD_NO_WX_GUI)
     void UpdateSize(gd::Project & project);
     #endif
 
     std::map<gd::String, gd::PropertyDescriptor> GetProperties(gd::Project & project) const override;
     bool UpdateProperty(const gd::String & name, const gd::String & value, gd::Project & project) override;
+    #endif
 
 private:
     void DoUnserializeFrom(gd::Project & project, const gd::SerializerElement & element) override;
+    #if defined(GD_IDE_ONLY)
     void DoSerializeTo(gd::SerializerElement & element) const override;
+    #endif
 
     gd::String skeletalDataFilename;
     gd::String rootArmatureName;
@@ -44,8 +53,8 @@ private:
     gd::String textureName;
     gd::String apiName;
     bool debugPolygons;
-
-    #if !defined(GD_NO_WX_GUI)
+    
+    #if defined(GD_IDE_ONLY)
     static sf::Texture edittimeIconImage;
     static sf::Sprite edittimeIcon;
     sf::Vector2f originalSize;

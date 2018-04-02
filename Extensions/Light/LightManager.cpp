80e7a6010a1bdd128e7272074888f51a1e3e8a4f
Florian Rival <florian.rival@gmail.com>
2018-04-02 21:49:29 +0200

    Add missing std::endl in Light_Manager sometimes creating conflicts with FileStream.
    * Other files should be audited for missing std::endl.
    * FileStream should be protected against this.

--- Extensions/Light/LightManager.cpp
+++ Extensions/Light/LightManager.cpp
@@ -4,11 +4,11 @@
 Light_Manager::Light_Manager() :
     commonBlurEffectLoaded(false)
 {
-    std::cout << "Creating Light Manager";
+    std::cout << "Creating Light Manager" << std::endl;
 }
 Light_Manager::~Light_Manager()
 {
-    std::cout << "Destroying Light Manager";
+    std::cout << "Destroying Light Manager" << std::endl;
 }
 
 

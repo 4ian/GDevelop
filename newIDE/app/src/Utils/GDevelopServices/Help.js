1c9497dc920976c46078c0a71577df13b556417a
Florian Rival <Florian.Rival@gmail.com>
2018-04-13 23:45:26 +0200

    Remove useless file

--- newIDE/app/src/Utils/GDevelopServices/Help.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import axios from 'axios';
-
-export const listAllTutorials = (): Promise<any> => {
-  return axios
-    .get('http://wiki.compilgames.net/doku.php/gdevelop5/tutorials')
-    .then(response => response.data);
-};

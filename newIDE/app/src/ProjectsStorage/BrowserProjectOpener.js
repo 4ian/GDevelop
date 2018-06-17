a70f5df3868759540de9590e28d1aea345adbcc7
Florian Rival <Florian.Rival@gmail.com>
2018-06-17 14:33:50 +0100

    Add/update 9 examples

--- newIDE/app/src/ProjectsStorage/BrowserProjectOpener.js
+++ newIDE/app/src/ProjectsStorage/BrowserProjectOpener.js
@@ -1,10 +1,12 @@
 import advancedShapeBasedPainter from '../fixtures/advanced-shape-based-painter/advanced-shape-based-painter.json';
 import animationSpeedScale from '../fixtures/animation-speed-scale/animation-speed-scale.json';
 import asteroids from '../fixtures/asteroids/asteroids.json';
+import basicAiWithPathfinding from '../fixtures/basic-ai-with-pathfinding/basic-ai-with-pathfinding.json';
 import basicArtificialIntelligence from '../fixtures/basic-artificial-intelligence/basic-artificial-intelligence.json';
 import basicTopdownCarDriving from '../fixtures/basic-topdown-car-driving/basic-topdown-car-driving.json';
 import brakeout from '../fixtures/brakeout/brakeout.json';
 import buttons from '../fixtures/buttons/buttons.json';
+import carPhysics from '../fixtures/car-physics/car-physics.json';
 import changePositionOfObject from '../fixtures/change-position-of-object/change-position-of-object.json';
 import changeScaleOfSprites from '../fixtures/change-scale-of-sprites/change-scale-of-sprites.json';
 import changeSpriteAnimation from '../fixtures/change-sprite-animation/change-sprite-animation.json';
@@ -12,6 +14,7 @@ import changeSpriteColor from '../fixtures/change-sprite-color/change-sprite-col
 import createObjectWithMouseclick from '../fixtures/create-object-with-mouseclick/create-object-with-mouseclick.json';
 import customizeKeysWithLastpressedkey from '../fixtures/customize-keys-with-lastpressedkey/customize-keys-with-lastpressedkey.json';
 import dragCameraWithMouse from '../fixtures/drag-camera-with-mouse/drag-camera-with-mouse.json';
+import findDiagonals from '../fixtures/find-diagonals/find-diagonals.json';
 import infiniteScrollingBackground from '../fixtures/infinite-scrolling-background/infinite-scrolling-background.json';
 import instanceTimer from '../fixtures/instance-timer/instance-timer.json';
 import inventorySystem from '../fixtures/inventory-system/inventory-system.json';
@@ -19,9 +22,11 @@ import keyboardPractice from '../fixtures/keyboard-practice/keyboard-practice.js
 import magnet from '../fixtures/magnet/magnet.json';
 import manipulateTextObject from '../fixtures/manipulate-text-object/manipulate-text-object.json';
 import moveCameraToPosition from '../fixtures/move-camera-to-position/move-camera-to-position.json';
+import moveObjectInCircle from '../fixtures/move-object-in-circle/move-object-in-circle.json';
 import moveObjectTowardPosition from '../fixtures/move-object-toward-position/move-object-toward-position.json';
 import moveObjectWithPhysics from '../fixtures/move-object-with-physics/move-object-with-physics.json';
 import multitouch from '../fixtures/multitouch/multitouch.json';
+import objectGravity from '../fixtures/object-gravity/object-gravity.json';
 import objectSelection from '../fixtures/object-selection/object-selection.json';
 import openUrlInBrowser from '../fixtures/open-url-in-browser/open-url-in-browser.json';
 import parallax from '../fixtures/parallax/parallax.json';
@@ -40,11 +45,14 @@ import rotateTowardMouse from '../fixtures/rotate-toward-mouse/rotate-toward-mou
 import rotateTowardPosition from '../fixtures/rotate-toward-position/rotate-toward-position.json';
 import rotateWithKeypress from '../fixtures/rotate-with-keypress/rotate-with-keypress.json';
 import saveLoad from '../fixtures/save-load/save-load.json';
+import shootBulletInParabola from '../fixtures/shoot-bullet-in-parabola/shoot-bullet-in-parabola.json';
 import shootBullets from '../fixtures/shoot-bullets/shoot-bullets.json';
 import shootingBulletsExplanation from '../fixtures/shooting-bullets-explanation/shooting-bullets-explanation.json';
+import snapObjectToGrid from '../fixtures/snap-object-to-grid/snap-object-to-grid.json';
 import spaceShooter from '../fixtures/space-shooter/space-shooter.json';
 import splashScreen from '../fixtures/splash-screen/splash-screen.json';
 import textEntryObject from '../fixtures/text-entry-object/text-entry-object.json';
+import textFadeInOut from '../fixtures/text-fade-in-out/text-fade-in-out.json';
 import toggleMusicPlaySound from '../fixtures/toggle-music-play-sound/toggle-music-play-sound.json';
 import typeOnTextEffect from '../fixtures/type-on-text-effect/type-on-text-effect.json';
 import zDepth from '../fixtures/z-depth/z-depth.json';
@@ -58,6 +66,8 @@ export default class BrowserProjectOpener {
       return Promise.resolve(animationSpeedScale);
     } else if (url === 'internal://asteroids') {
       return Promise.resolve(asteroids);
+    } else if (url === 'internal://basic-ai-with-pathfinding') {
+      return Promise.resolve(basicAiWithPathfinding);
     } else if (url === 'internal://basic-artificial-intelligence') {
       return Promise.resolve(basicArtificialIntelligence);
     } else if (url === 'internal://basic-topdown-car-driving') {
@@ -66,6 +76,8 @@ export default class BrowserProjectOpener {
       return Promise.resolve(brakeout);
     } else if (url === 'internal://buttons') {
       return Promise.resolve(buttons);
+    } else if (url === 'internal://car-physics') {
+      return Promise.resolve(carPhysics);
     } else if (url === 'internal://change-position-of-object') {
       return Promise.resolve(changePositionOfObject);
     } else if (url === 'internal://change-scale-of-sprites') {
@@ -80,6 +92,8 @@ export default class BrowserProjectOpener {
       return Promise.resolve(customizeKeysWithLastpressedkey);
     } else if (url === 'internal://drag-camera-with-mouse') {
       return Promise.resolve(dragCameraWithMouse);
+    } else if (url === 'internal://find-diagonals') {
+      return Promise.resolve(findDiagonals);
     } else if (url === 'internal://infinite-scrolling-background') {
       return Promise.resolve(infiniteScrollingBackground);
     } else if (url === 'internal://instance-timer') {
@@ -94,12 +108,16 @@ export default class BrowserProjectOpener {
       return Promise.resolve(manipulateTextObject);
     } else if (url === 'internal://move-camera-to-position') {
       return Promise.resolve(moveCameraToPosition);
+    } else if (url === 'internal://move-object-in-circle') {
+      return Promise.resolve(moveObjectInCircle);
     } else if (url === 'internal://move-object-toward-position') {
       return Promise.resolve(moveObjectTowardPosition);
     } else if (url === 'internal://move-object-with-physics') {
       return Promise.resolve(moveObjectWithPhysics);
     } else if (url === 'internal://multitouch') {
       return Promise.resolve(multitouch);
+    } else if (url === 'internal://object-gravity') {
+      return Promise.resolve(objectGravity);
     } else if (url === 'internal://object-selection') {
       return Promise.resolve(objectSelection);
     } else if (url === 'internal://open-url-in-browser') {
@@ -136,16 +154,22 @@ export default class BrowserProjectOpener {
       return Promise.resolve(rotateWithKeypress);
     } else if (url === 'internal://save-load') {
       return Promise.resolve(saveLoad);
+    } else if (url === 'internal://shoot-bullet-in-parabola') {
+      return Promise.resolve(shootBulletInParabola);
     } else if (url === 'internal://shoot-bullets') {
       return Promise.resolve(shootBullets);
     } else if (url === 'internal://shooting-bullets-explanation') {
       return Promise.resolve(shootingBulletsExplanation);
+    } else if (url === 'internal://snap-object-to-grid') {
+      return Promise.resolve(snapObjectToGrid);
     } else if (url === 'internal://space-shooter') {
       return Promise.resolve(spaceShooter);
     } else if (url === 'internal://splash-screen') {
       return Promise.resolve(splashScreen);
     } else if (url === 'internal://text-entry-object') {
       return Promise.resolve(textEntryObject);
+    } else if (url === 'internal://text-fade-in-out') {
+      return Promise.resolve(textFadeInOut);
     } else if (url === 'internal://toggle-music-play-sound') {
       return Promise.resolve(toggleMusicPlaySound);
     } else if (url === 'internal://type-on-text-effect') {

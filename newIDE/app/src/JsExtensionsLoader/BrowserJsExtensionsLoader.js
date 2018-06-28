8e0783e45f7622b900fad47d481e4508d2695e2a
Florian Rival <Florian.Rival@gmail.com>
2018-06-29 00:09:46 +0100

    Load JsExtensions.js in BrowserJsExtensionsLoader

--- newIDE/app/src/JsExtensionsLoader/BrowserJsExtensionsLoader.js
+++ newIDE/app/src/JsExtensionsLoader/BrowserJsExtensionsLoader.js
@@ -1,8 +1,30 @@
 // @flow
-import { type JsExtensionsLoader } from '.';
+import {
+  type JsExtensionsLoader,
+  type ExtensionLoadingResult,
+  loadExtension,
+} from '.';
+const gd = global.gd;
 
+const jsExtensions = [
+  {
+    name: 'FacebookInstantGames',
+    extensionModule: require('GDJS-for-web-app-only/Runtime/Extensions/FacebookInstantGames/JsExtension.js'),
+  },
+];
+
+/**
+ * Loader that load all JS extensions required in this file.
+ * Extensions are usually auto-discovered when declared in Extensions/xxx/JsExtension.js, but not
+ * for the web-app where everything must be bundled.
+ */
 export default class BrowserJsExtensionsLoader implements JsExtensionsLoader {
   loadAllExtensions(): Promise<Array<ExtensionLoadingResult>> {
-    return Promise.resolve([]);
+    return Promise.resolve(
+      jsExtensions.map(({ name, extensionModule }) => ({
+        extensionModulePath: 'internal-extension://' + name,
+        result: loadExtension(gd.JsPlatform.get(), extensionModule),
+      }))
+    );
   }
 }

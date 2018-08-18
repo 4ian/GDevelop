7ae838bff4e449fd958f579e38885a9d32e99e7d
Florian Rival <Florian.Rival@gmail.com>
2018-08-18 19:50:21 +0200

    Fix docsearch by directly using algoliasearch and formatting results with ListItems

--- newIDE/app/src/HelpFinder/DocSearchArea.js
+++ newIDE/app/src/HelpFinder/DocSearchArea.js
@@ -3,8 +3,10 @@ import * as React from 'react';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 import Window from '../Utils/Window';
-import './DocSearchOverrides.css';
+import { List, ListItem } from 'material-ui/List';
 import { Column } from '../UI/Grid';
+import algoliasearch from 'algoliasearch/lite';
+import debounce from 'lodash/debounce';
 
 const styles = {
   dropdownMenuContainer: {
@@ -17,33 +19,108 @@ const styles = {
   },
 };
 
-export default class DocSearchArea extends React.Component<*, *> {
-  componentDidMount() {
-    if (!global.docsearch) {
-      console.warn("docsearch not found - DocSearchArea won't work");
-      return;
-    }
+type Props = {|
+  onChange: string => void,
+  value: string,
+|};
 
-    try {
-      global.docsearch({
-        apiKey: '69c73fc1a710bb79543d4b91f6b81c08',
-        indexName: 'gdevelop',
-        inputSelector: '#help-finder-search-bar',
-        debug: true, // Set debug to true if you want to inspect the dropdown
-        handleSelected: (input, event, suggestion) => {
-          Window.openExternalURL(suggestion.url);
-          this.props.onChange('');
-        },
-        autocompleteOptions: {
-          appendTo: true,
-          hint: false,
-          dropdownMenuContainer: '#help-finder-dropdown-menu-container',
-        },
-      });
-    } catch (e) {
-      console.error('Error while loading docsearch:', e);
+type AlgoliaResult = {|
+  content: string,
+  url: string,
+  hierarchy: {|
+    lvl0: ?string,
+    lvl1: ?string,
+    lvl2: ?string,
+    lvl3: ?string,
+    lvl4: ?string,
+    lvl5: ?string,
+    lvl6: ?string,
+  |},
+  objectID: string,
+|};
+
+type State = {|
+  results: ?Array<AlgoliaResult>,
+|};
+
+const indexName = 'gdevelop';
+const appId = 'BH4D9OD16A';
+const apiKey = '69c73fc1a710bb79543d4b91f6b81c08';
+const algoliaOptions = { hitsPerPage: 5 };
+
+export default class DocSearchArea extends React.Component<Props, State> {
+  client: algoliasearch = algoliasearch(appId, apiKey, algoliaOptions);
+  state = {
+    results: null,
+  };
+
+  _handleSearchTextChange = (searchText: string) => {
+    this.props.onChange(searchText);
+  };
+
+  _launchSearchRequest = debounce(() => {
+    if (this.props.value) {
+      this.client
+        .search([
+          {
+            indexName: indexName,
+            query: this.props.value,
+            params: algoliaOptions,
+          },
+        ])
+        .then(data => {
+          let hits = data.results[0].hits;
+          console.log(hits);
+
+          this.setState({
+            results: hits,
+          });
+        });
+    }
+  }, 200);
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.value !== this.props.value && this.props.value) {
+      this._launchSearchRequest();
     }
   }
+
+  _renderResult = (result: AlgoliaResult) => {
+    const primaryText =
+      result.hierarchy.lvl0 ||
+      result.hierarchy.lvl1 ||
+      result.hierarchy.lvl2 ||
+      result.hierarchy.lvl3 ||
+      result.hierarchy.lvl4 ||
+      result.hierarchy.lvl5 ||
+      result.hierarchy.lvl6;
+
+    const secondaryText = [
+      result.hierarchy.lvl0,
+      result.hierarchy.lvl1,
+      result.hierarchy.lvl2,
+      result.hierarchy.lvl3,
+      result.hierarchy.lvl4,
+      result.hierarchy.lvl5,
+      result.hierarchy.lvl6,
+    ]
+      .filter(text => !!text)
+      .filter(text => text !== primaryText)
+      .join(' - ')
+      .replace(/&quot;/g, '"');
+
+    return (
+      <ListItem
+        key={result.objectID}
+        primaryText={primaryText}
+        secondaryText={result.content || secondaryText}
+        secondaryTextLines={2}
+        onClick={() => {
+          Window.openExternalURL(result.url);
+        }}
+      />
+    );
+  };
 
   render() {
     return (
@@ -63,7 +140,11 @@ export default class DocSearchArea extends React.Component<*, *> {
             visibility: !this.props.value ? 'hidden' : undefined,
           }}
         />
-        {!this.props.value && (
+        {this.state.results ? (
+          <List>
+            {this.state.results.map(result => this._renderResult(result))}
+          </List>
+        ) : (
           <React.Fragment>
             <p>Examples:</p>
             <Column expand>
@@ -75,15 +156,15 @@ export default class DocSearchArea extends React.Component<*, *> {
                 ...<br />
               </p>
             </Column>
-            <p style={styles.poweredByText}>
-              This search is powered by{' '}
-              <FlatButton
-                onClick={() => Window.openExternalURL('http://algolia.com/')}
-                label="Algolia"
-              />
-            </p>
           </React.Fragment>
         )}
+        <p style={styles.poweredByText}>
+          This search is powered by{' '}
+          <FlatButton
+            onClick={() => Window.openExternalURL('http://algolia.com/')}
+            label="Algolia"
+          />
+        </p>
       </div>
     );
   }

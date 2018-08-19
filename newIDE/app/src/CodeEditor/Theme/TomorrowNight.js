feec79d9de6ecb65d7d8609264b9640d650d7b08
Florian Rival <Florian.Rival@gmail.com>
2018-08-19 15:25:17 +0200

    Allow to choose a custom theme for the Monaco code editor

---
+++ newIDE/app/src/CodeEditor/Theme/TomorrowNight.js
@@ -0,0 +1,187 @@
+const themeData = {
+  base: 'vs-dark',
+  inherit: true,
+  rules: [
+    {
+      foreground: '969896',
+      token: 'comment',
+    },
+    {
+      foreground: 'ced1cf',
+      token: 'keyword.operator.class',
+    },
+    {
+      foreground: 'ced1cf',
+      token: 'constant.other',
+    },
+    {
+      foreground: 'ced1cf',
+      token: 'source.php.embedded.line',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'variable',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'support.other.variable',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'string.other.link',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'string.regexp',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'entity.name.tag',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'entity.other.attribute-name',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'meta.tag',
+    },
+    {
+      foreground: 'cc6666',
+      token: 'declaration.tag',
+    },
+    {
+      foreground: 'de935f',
+      token: 'constant.numeric',
+    },
+    {
+      foreground: 'de935f',
+      token: 'constant.language',
+    },
+    {
+      foreground: 'de935f',
+      token: 'support.constant',
+    },
+    {
+      foreground: 'de935f',
+      token: 'constant.character',
+    },
+    {
+      foreground: 'de935f',
+      token: 'variable.parameter',
+    },
+    {
+      foreground: 'de935f',
+      token: 'punctuation.section.embedded',
+    },
+    {
+      foreground: 'de935f',
+      token: 'keyword.other.unit',
+    },
+    {
+      foreground: 'f0c674',
+      token: 'entity.name.class',
+    },
+    {
+      foreground: 'f0c674',
+      token: 'entity.name.type.class',
+    },
+    {
+      foreground: 'f0c674',
+      token: 'support.type',
+    },
+    {
+      foreground: 'f0c674',
+      token: 'support.class',
+    },
+    {
+      foreground: 'b5bd68',
+      token: 'string',
+    },
+    {
+      foreground: 'b5bd68',
+      token: 'constant.other.symbol',
+    },
+    {
+      foreground: 'b5bd68',
+      token: 'entity.other.inherited-class',
+    },
+    {
+      foreground: 'b5bd68',
+      token: 'markup.heading',
+    },
+    {
+      foreground: '8abeb7',
+      token: 'keyword.operator',
+    },
+    {
+      foreground: '8abeb7',
+      token: 'constant.other.color',
+    },
+    {
+      foreground: '81a2be',
+      token: 'entity.name.function',
+    },
+    {
+      foreground: '81a2be',
+      token: 'meta.function-call',
+    },
+    {
+      foreground: '81a2be',
+      token: 'support.function',
+    },
+    {
+      foreground: '81a2be',
+      token: 'keyword.other.special-method',
+    },
+    {
+      foreground: '81a2be',
+      token: 'meta.block-level',
+    },
+    {
+      foreground: 'b294bb',
+      token: 'keyword',
+    },
+    {
+      foreground: 'b294bb',
+      token: 'storage',
+    },
+    {
+      foreground: 'b294bb',
+      token: 'storage.type',
+    },
+    {
+      foreground: 'b294bb',
+      token: 'entity.name.tag.css',
+    },
+    {
+      foreground: 'ced2cf',
+      background: 'df5f5f',
+      token: 'invalid',
+    },
+    {
+      foreground: 'ced2cf',
+      background: '82a3bf',
+      token: 'meta.separator',
+    },
+    {
+      foreground: 'ced2cf',
+      background: 'b798bf',
+      token: 'invalid.deprecated',
+    },
+  ],
+  colors: {
+    'editor.foreground': '#C5C8C6',
+    'editor.background': '#1D1F21',
+    'editor.selectionBackground': '#373B41',
+    'editor.lineHighlightBackground': '#282A2E',
+    'editorCursor.foreground': '#AEAFAD',
+    'editorWhitespace.foreground': '#4B4E55',
+  },
+};
+
+export default {
+  name: 'Tomorrow-Night',
+  themeName: 'tomorrow-night',
+  themeData,
+};
--- newIDE/app/src/MainFrame/Preferences/PreferencesContext.js
+++ newIDE/app/src/MainFrame/Preferences/PreferencesContext.js
@@ -3,11 +3,13 @@ import createReactContext, { type Context } from 'create-react-context';
 export type PreferencesValues = {|
   autoDownloadUpdates: boolean,
   themeName: string,
+  codeEditorThemeName: string,
 |};
 
 export type Preferences = {|
   values: PreferencesValues,
   setThemeName: (themeName: string) => void,
+  setCodeEditorThemeName: (codeEditorThemeName: string) => void,
   setAutoDownloadUpdates: (enabled: boolean) => void,
   checkUpdates: (forceDownload?: boolean) => void,
 |};
@@ -16,8 +18,10 @@ export const initialPreferences = {
   values: {
     autoDownloadUpdates: true,
     themeName: 'GDevelop default',
+    codeEditorThemeName: 'vs-dark',
   },
   setThemeName: () => {},
+  setCodeEditorThemeName: () => {},
   setAutoDownloadUpdates: () => {},
   checkUpdates: () => {},
 };

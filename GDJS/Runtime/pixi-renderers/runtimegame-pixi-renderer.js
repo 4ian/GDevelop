e014447d2cf13019e4f4e5aedbd045d3234288d2
Bouh <bouhvivez@gmail.com>
2018-08-20 10:43:56 +0200

    Update comment
    for parameter 'preserveDrawingBuffer'

--- GDJS/Runtime/pixi-renderers/runtimegame-pixi-renderer.js
+++ GDJS/Runtime/pixi-renderers/runtimegame-pixi-renderer.js
@@ -41,7 +41,7 @@ gdjs.RuntimeGamePixiRenderer.prototype.createStandardCanvas = function(canvasAre
     PIXI.glCore.VertexArrayObject.FORCE_NATIVE = true;
 
     //Create the renderer and setup the rendering area
-    //"transparent: true" is needed to avoid flickering issues on some mobile phones (see #566)
+    //"preserveDrawingBuffer: true" is needed to avoid flickering and background issues on some mobile phones (see #585 #572 #566 #463)
     this._pixiRenderer = PIXI.autoDetectRenderer(this._game.getDefaultWidth(), this._game.getDefaultHeight(), {preserveDrawingBuffer: true} );
     canvasArea.style["position"] = "absolute";
     canvasArea.appendChild(this._pixiRenderer.view); // add the renderer view element to the DOM

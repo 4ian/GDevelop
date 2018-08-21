bda4a790b9af2cf4f78c449321d1589956707912
Wend1go <Matthias83@gmx.de>
2018-08-21 18:32:10 +0200

    Add orientation sensor extension with examples (#587)

---
+++ Extensions/DeviceSensors/devicesensortools.js
@@ -0,0 +1,87 @@
+/**
+ * @memberof gdjs.evtTools
+ * @class devicesensors
+ * @static
+ * @private
+ */
+
+gdjs.evtTools.devicesensors = {
+  orientation: {
+    _isActive: 0,
+    _absolute: 0,
+    _alpha: 0,
+    _beta: 0,
+    _gamma: 0
+  }
+};
+
+gdjs.evtTools.devicesensors.orientation._activateOrientationListener = function() {
+  window.addEventListener("deviceorientation", gdjs.evtTools.devicesensors.orientation._handleOrientation, true);
+  gdjs.evtTools.devicesensors.orientation._isActive = 1;
+}
+
+gdjs.evtTools.devicesensors.orientation._deactivateOrientationListener = function() {
+  window.removeEventListener('deviceorientation', gdjs.evtTools.devicesensors.orientation._handleOrientation, true);
+  gdjs.evtTools.devicesensors.orientation._isActive = 0;
+}
+
+gdjs.evtTools.devicesensors.orientation._handleOrientation = function(event) {
+  gdjs.evtTools.devicesensors.orientation._absolute = event.absolute ? Math.round(event.absolute) : 0;
+  gdjs.evtTools.devicesensors.orientation._alpha = event.alpha ? Math.round(event.alpha) : 0;
+  gdjs.evtTools.devicesensors.orientation._beta = event.beta ? Math.round(event.beta) : 0;
+  gdjs.evtTools.devicesensors.orientation._gamma = event.gamma ? Math.round(event.gamma) : 0;
+}
+
+/**
+ * Activate the orientation sensor
+ */
+gdjs.evtTools.devicesensors.orientation.activateOrientationSensor = function() {
+  gdjs.evtTools.devicesensors.orientation._activateOrientationListener();
+}
+
+/**
+ * Deactivate the orientation sensor
+ */
+gdjs.evtTools.devicesensors.orientation.deactivateOrientationSensor = function() {
+  gdjs.evtTools.devicesensors.orientation._deactivateOrientationListener();
+}
+
+/**
+ * Check if the orientation sensor is currently active
+ * @return {number} The activation state of the orientation sensor (0=false/1=true)
+ */
+gdjs.evtTools.devicesensors.orientation.isActive = function() {
+  return gdjs.evtTools.devicesensors.orientation._isActive;
+}
+
+/**
+ * Get the value of the device orientations absolute as a number
+ * @return {number} The device orientation's absolute value
+ */
+gdjs.evtTools.devicesensors.orientation.getOrientationAbsolute = function() {
+  return gdjs.evtTools.devicesensors.orientation._absolute;
+};
+
+/**
+ * Get the value of the device orientations alpha as a number
+ * @return {number} The device orientation's alpha value
+ */
+gdjs.evtTools.devicesensors.orientation.getOrientationAlpha = function() {
+  return gdjs.evtTools.devicesensors.orientation._alpha;
+};
+
+/**
+ * Get the value of the device orientations beta as a number
+ * @return {number} The device orientation's beta value
+ */
+gdjs.evtTools.devicesensors.orientation.getOrientationBeta = function() {
+  return gdjs.evtTools.devicesensors.orientation._beta;
+};
+
+/**
+ * Get the value of the device orientations gamma as a number
+ * @return {number} The device orientation's gamma value
+ */
+gdjs.evtTools.devicesensors.orientation.getOrientationGamma = function() {
+  return gdjs.evtTools.devicesensors.orientation._gamma;
+};

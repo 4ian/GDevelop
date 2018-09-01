c116db3cea32e9952b22a6c40e93ee4c710a9846
Matthias Meike <matthiasmeike@gmx.de>
2018-09-01 12:10:25 +0200

    Add vibration extension for mobile devices

---
+++ Extensions/DeviceVibration/devicevibrationtools.js
@@ -0,0 +1,37 @@
+/**
+ * @memberof gdjs
+ * @class deviceVibration
+ * @static
+ * @private
+ */
+
+gdjs.deviceVibration = {};
+
+/**
+ * Vibrate the mobile device.
+ * @param {number} duration Value in milliseconds.
+ */
+gdjs.deviceVibration.vibrationStart = function(duration) {
+  navigator.vibrate([duration]);
+}
+
+/**
+ * Vibrate the mobile device in a pattern.
+ * You can add multiple comma separated values where every second determines the silence between vibrations.
+ * Example: "200,1000,500" (200ms vibration, 1sec silense, 500ms vibration)
+ * @param {string} intervals Comma separated list of values (in ms).
+ */
+gdjs.deviceVibration.vibrationPatternStart = function(intervals) {
+  const pattern = '^[0-9]+(,[0-9]+)*$'
+
+  if (intervals.match(pattern)){
+    navigator.vibrate(intervals.split(","));
+  }
+}
+
+/**
+ * Stop the current vibration on the mobile device.
+ */
+gdjs.deviceVibration.vibrationStop = function() {
+  navigator.vibrate([]);
+}

ed84e2847029bf88a39930ec5d203dcfc74d4486
Lizard-13 <thelizard-13@hotmail.com>
2018-09-22 17:56:57 -0300

    Set wrapping false by default

--- Extensions/TextObject/textruntimeobject.js
+++ Extensions/TextObject/textruntimeobject.js
@@ -20,8 +20,8 @@ gdjs.TextRuntimeObject = function(runtimeScene, objectData)
     this._italic = objectData.italic;
     this._underlined = objectData.underlined;
     this._color = [objectData.color.r, objectData.color.g, objectData.color.b];
-    this._wrapping = objectData.wrapping;
-    this._wrappingWidth = objectData.wrappingWidth;
+    this._wrapping = false;
+    this._wrappingWidth = 1;
 
     this._str = objectData.string;
 

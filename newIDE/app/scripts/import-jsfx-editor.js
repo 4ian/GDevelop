c5554d05fd970aeb26b39262ae8a9f38c1e7cda2
Florian Rival <Florian.Rival@gmail.com>
2018-10-20 20:29:21 +0100

    Fix import-jsfx-editor and add jsfx sources to .gitignore

--- newIDE/app/scripts/import-jsfx-editor.js
+++ newIDE/app/scripts/import-jsfx-editor.js
@@ -3,18 +3,12 @@ var shell = require('shelljs');
 var source = '../node_modules/loov-jsfx';
 
 var success = true;
-success &= shell.mkdir('-p', '../public/External/jsfx');
-success &= shell.cp(
-  '-Rf',
-  source,
-  '../public/External/jsfx'
-);
-if (success) {
+success &= !shell.mkdir('-p', '../public/External/jsfx').stderr;
+success &= !shell.cp('-Rf', source, '../public/External/jsfx').stderr;
+if (!success) {
   shell.echo(
     `❌ Error(s) occurred while copying Jsfx Editor sources from node_modules/loov-jsfx`
   );
 } else {
-  shell.echo(
-    '✅ Sources of Jsfx Editor properly copied in public folder'
-  );
+  shell.echo('✅ Sources of Jsfx Editor properly copied in public folder');
 }

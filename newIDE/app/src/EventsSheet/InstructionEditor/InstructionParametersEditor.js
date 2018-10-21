1a3d2e0e46e18bce94a649738692b622ff558aad
Todor Imreorov <blurymind@gmail.com>
2018-10-21 20:14:54 +0100

    remove focus on mount to stop menu popping up
    this is a redo pull of https://github.com/4ian/GDevelop/pull/675

--- newIDE/app/src/EventsSheet/InstructionEditor/InstructionParametersEditor.js
+++ newIDE/app/src/EventsSheet/InstructionEditor/InstructionParametersEditor.js
@@ -49,7 +49,6 @@ type Props = {|
   objectsContainer: gdObjectsContainer,
   instruction: gdInstruction,
   isCondition: boolean,
-  focusOnMount?: boolean,
   resourceSources: Array<ResourceSource>,
   onChooseResource: ChooseResourceFunction,
   resourceExternalEditors: Array<ResourceExternalEditor>,
@@ -62,14 +61,6 @@ export default class InstructionParametersEditor extends React.Component<
   State
 > {
   _firstVisibleField: ?any = {};
-
-  componentDidMount() {
-    if (this.props.focusOnMount) {
-      setTimeout(() => {
-        this.focus();
-      }, 300); // Let the time to the dialog that is potentially containing the InstructionParametersEditor to finish its transition.
-    }
-  }
 
   focus() {
     // Verify that there is a field to focus.

187b0b4b1bb504710b6bf15a0789355ddc8098a2
Lizard-13 <thelizard-13@hotmail.com>
2018-10-23 23:00:23 -0300

    Add banners and interstitials support

--- Extensions/AdMob/JsExtension.js
+++ Extensions/AdMob/JsExtension.js
@@ -23,6 +23,224 @@ module.exports = {
       "MIT"
     );
 
+    // Banner
+    extension
+      .addCondition(
+        "BannerLoading",
+        t("Banner loading"),
+        t("Check if a banner is currently loading."),
+        t("Banner is loading"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.isBannerLoading");
+
+    extension
+      .addCondition(
+        "BannerReady",
+        t("Banner ready"),
+        t("Check if a banner is ready to be displayed."),
+        t("Banner is ready"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.isBannerReady");
+
+    extension
+      .addCondition(
+        "BannerShowing",
+        t("Banner showing"),
+        t("Check if there is a banner being displayed."),
+        t("Banner is showing"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.isBannerShowing");
+
+    extension
+      .addCondition(
+        "BannerExists",
+        t("Banner exists"),
+        t("Check if there is a banner in memory (visible or hidden)."),
+        t("Banner exists"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.existBanner");
+
+    extension
+      .addAction(
+        "LoadBanner",
+        t("Load banner"),
+        t(
+          "Start loading a banner, you can display it automatically when finish loading.\nIf test mode is set to true a test banner will be displayed."
+        ),
+        t(
+          "Load banner (at top: _PARAM2_, overlap: _PARAM3_, show on load: _PARAM4_, test mode: _PARAM5_)"
+        ),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .addParameter("string", t("Android banner ID"), "", false)
+      .addParameter("string", t("iOS banner ID"), "", false)
+      .addParameter(
+        "yesorno",
+        t("Display at top? (bottom otherwise)"),
+        "",
+        false
+      )
+      .setDefaultValue("false")
+      .addParameter("yesorno", t("Overlap webview?"), "", false)
+      .setDefaultValue("true")
+      .addParameter("yesorno", t("Display on load complete?"), "", false)
+      .setDefaultValue("true")
+      .addParameter("yesorno", t("Test mode?"), "", false)
+      .setDefaultValue("true")
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.loadBanner");
+
+    extension
+      .addAction(
+        "ShowBanner",
+        t("Show banner"),
+        t("Show the banner, will work only when the banner is fully loaded."),
+        t("Show banner"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.showBanner");
+
+    extension
+      .addAction(
+        "HideBanner",
+        t("Hide banner"),
+        t(
+          "Hide the banner. You can show it again with the corresponding action."
+        ),
+        t("Hide banner"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.hideBanner");
+
+    extension
+      .addAction(
+        "RemoveBanner",
+        t("Remove banner"),
+        t(
+          "Remove the banner. You have to load another banner to show it again."
+        ),
+        t("Remove banner"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.removeBanner");
+
+    // Interstitial
+    extension
+      .addCondition(
+        "InterstitialLoading",
+        t("Interstitial loading"),
+        t("Check if an interstitial is currently loading."),
+        t("Interstitial is loading"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.isInterstitialLoading");
+
+    extension
+      .addCondition(
+        "InterstitialReady",
+        t("Interstitial ready"),
+        t("Check if an interstitial is ready to be displayed."),
+        t("Interstitial is ready"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.isInterstitialReady");
+
+    extension
+      .addCondition(
+        "InterstitialShowing",
+        t("Interstitial showing"),
+        t("Check if there is an interstitial being displayed."),
+        t("Interstitial is showing"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.isInterstitialShowing");
+
+    extension
+      .addAction(
+        "LoadInterstitial",
+        t("Load interstitial"),
+        t(
+          "Start loading an interstitial, you can display it automatically when finish loading.\nIf test mode is set to true a test interstitial will be displayed."
+        ),
+        t("Load interstitial (show on load: _PARAM2_, test mode: _PARAM3_)"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .addParameter("string", t("Android interstitial ID"), "", false)
+      .addParameter("string", t("iOS interstitial ID"), "", false)
+      .addParameter("yesorno", t("Display on load complete?"), "", false)
+      .setDefaultValue("true")
+      .addParameter("yesorno", t("Test mode?"), "", false)
+      .setDefaultValue("true")
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.loadInterstitial");
+
+    extension
+      .addAction(
+        "ShowInterstitial",
+        t("Show interstitial"),
+        t(
+          "Show the interstitial, will work only when the interstitial is fully loaded."
+        ),
+        t("Show interstitial"),
+        t("AdMob"),
+        "JsPlatform/Extensions/admobicon24.png",
+        "JsPlatform/Extensions/admobicon16.png"
+      )
+      .getCodeExtraInformation()
+      .setIncludeFile("Extensions/AdMob/admobtools.js")
+      .setFunctionName("gdjs.adMob.showInterstitial");
+
+    // Reward video
     extension
       .addCondition(
         "VideoLoading",
@@ -55,7 +273,7 @@ module.exports = {
       .addCondition(
         "VideoShowing",
         t("Video showing"),
-        t("Check if there is a video being displayed."),
+        t("Check if there is a reward video being displayed."),
         t("Reward video is showing"),
         t("AdMob"),
         "JsPlatform/Extensions/admobicon24.png",

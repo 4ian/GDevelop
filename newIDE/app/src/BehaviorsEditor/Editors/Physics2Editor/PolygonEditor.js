8a07e9b6c54abd8d9d1927174014e5474e2321ee
Franco Maciel <thelizard-13@hotmail.com>
2019-01-06 17:26:43 +0000

    Prevent crash with repeated and aligned vertices in Physics2 behavior (#850)

--- newIDE/app/src/BehaviorsEditor/Editors/Physics2Editor/PolygonEditor.js
+++ newIDE/app/src/BehaviorsEditor/Editors/Physics2Editor/PolygonEditor.js
@@ -30,33 +30,51 @@ type Props = {|
 export default class PolygonEditor extends React.Component<Props> {
   _isPolygonConvex(vertices: Array<Vertex>) {
     // Get edges
-    var edges = [];
-    var v1 = null;
-    var v2 = null;
-    for (var i = 0; i < vertices.length; i++) {
+    let edges = [];
+    let v1 = null;
+    let v2 = null;
+    for (let i = 0; i < vertices.length; i++) {
       v1 = vertices[i];
       if (i + 1 >= vertices.length) v2 = vertices[0];
       else v2 = vertices[i + 1];
       edges.push({ x: v2.x - v1.x, y: v2.y - v1.y });
     }
+
     // Check convexity
     if (edges.length < 3) return false;
 
     const zProductIsPositive =
       edges[0].x * edges[0 + 1].y - edges[0].y * edges[0 + 1].x > 0;
 
-    for (i = 1; i < edges.length - 1; ++i) {
-      var zCrossProduct =
+    for (let i = 1; i < edges.length - 1; ++i) {
+      let zCrossProduct =
         edges[i].x * edges[i + 1].y - edges[i].y * edges[i + 1].x;
-      var zCrossProductIsPositive = zCrossProduct > 0;
+      let zCrossProductIsPositive = zCrossProduct > 0;
       if (zCrossProductIsPositive !== zProductIsPositive) return false;
     }
 
-    var lastZCrossProduct =
+    let lastZCrossProduct =
       edges[edges.length - 1].x * edges[0].y -
       edges[edges.length - 1].y * edges[0].x;
-    var lastZCrossProductIsPositive = lastZCrossProduct > 0;
+    let lastZCrossProductIsPositive = lastZCrossProduct > 0;
     if (lastZCrossProductIsPositive !== zProductIsPositive) return false;
+
+    // Check for repeated vertices (would crash Box2D during the game)
+    for (let i = 0; i < vertices.length - 1; ++i) {
+      for (let j = i + 1; j < vertices.length; ++j) {
+        if (vertices[i].x === vertices[j].x && vertices[i].y === vertices[j].y)
+          return false;
+      }
+    }
+
+    // Check if all vertices are aligned (would crash Box2D during the game)
+    let alignedX = true;
+    let alignedY = true;
+    for (let i = 0; i < vertices.length - 1; ++i) {
+      if (vertices[i].x !== vertices[i + 1].x) alignedX = false;
+      if (vertices[i].y !== vertices[i + 1].y) alignedY = false;
+    }
+    if (alignedX || alignedY) return false;
 
     return true;
   }

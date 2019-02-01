f8d558688698dede3c5a9a4ed25eec894ace3471
Todor Imreorov <blurymind@gmail.com>
2019-02-01 22:15:09 +0000

    remove redundant or check

--- newIDE/app/src/VariablesList/index.js
+++ newIDE/app/src/VariablesList/index.js
@@ -286,7 +286,7 @@ export default class VariablesList extends React.Component<Props, State> {
             style={styles.emptyExplanation}
             messageStyle={styles.emptyExplanationMessage}
           >
-            {this.props.emptyExplanationMessage || ''}
+            {this.props.emptyExplanationMessage}
           </EmptyMessage>
           <EmptyMessage
             style={styles.emptyExplanation}

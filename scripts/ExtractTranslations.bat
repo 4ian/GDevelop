b4bd225f785bcaff0b9d297055feeb4a613343d6
Florian Rival <Florian.Rival@gmail.com>
2019-02-10 18:10:44 +0000

    Exclude GDCpp messages from translations (as only used in old IDE)

--- scripts/ExtractTranslations.bat
+++ scripts/ExtractTranslations.bat
@@ -1,9 +1,7 @@
 @echo off
 echo Listing all GDCore, GDCpp, GDJS and Extensions sources files to translate...
 
-dir ..\GDCpp\GDCpp\*.cpp /L /B /S |find /v /i "\Dialogs\" > %TEMP%\listfile.txt
-dir ..\GDCpp\GDCpp\*.h /L /B /S |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
-dir ..\GDJS\GDJS\*.cpp /L /B /S |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
+dir ..\GDJS\GDJS\*.cpp /L /B /S |find /v /i "\Dialogs\" > %TEMP%\listfile.txt
 dir ..\GDJS\GDJS\*.h /L /B /S |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
 dir ..\Extensions\*.cpp /L /B /S  |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
 dir ..\Extensions\*.h /L /B /S  |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
@@ -13,6 +11,11 @@ REM Don't include old IDE anymore in the translations.
 REM newIDE translations are generated separately (see newIDE/app/scripts)
 REM dir ..\IDE\*.cpp /L /b /s  |find /v /i "\wxstedit\"  >> %TEMP%\listfile.txt
 REM dir ..\IDE\*.h /L /b /s  |find /v /i "\wxstedit\"  >> %TEMP%\listfile.txt
+
+REM Don't include GDCpp anymore in the translations, as these translations were
+REM for the old IDE (GDevelop 4).
+REM dir ..\GDCpp\GDCpp\*.cpp /L /B /S |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
+REM dir ..\GDCpp\GDCpp\*.h /L /B /S |find /v /i "\Dialogs\" >> %TEMP%\listfile.txt
 
 echo Generating .POT file...
 xgettext --from-code utf-8  -o gdcore-gdcpp-gdjs-extensions-messages.pot --c++ --keyword=GD_T --no-wrap -f %TEMP%\listfile.txt -k_
--- scripts/ExtractTranslations.sh
+++ scripts/ExtractTranslations.sh
@@ -2,19 +2,22 @@
 #to update the strings to be translated.
 echo "ℹ️ Listing all GDCore, GDCpp, GDJS and Extensions sources files to translate..."
 
-find ../GDCpp/GDCpp/ -name '*.cpp' | grep -v '/Dialogs/' | grep -v '/IDE/' > /tmp/listfile.txt
-find ../GDCpp/GDCpp/ -name '*.h' -o -name "*.hpp" | grep -v '/Dialogs/' | grep -v '/IDE/' >> /tmp/listfile.txt
-find ../GDJS/GDJS/ -name '*.cpp' | grep -v '/Dialogs/' >> /tmp/listfile.txt
+find ../GDJS/GDJS/ -name '*.cpp' | grep -v '/Dialogs/' > /tmp/listfile.txt
 find ../GDJS/GDJS/ -name '*.h' -o -name "*.hpp" | grep -v '/Dialogs/' >> /tmp/listfile.txt
 find ../Extensions/ -name '*.cpp' | grep -v '/Dialogs/' >> /tmp/listfile.txt
 find ../Extensions/ -name '*.h' -o -name "*.hpp" | grep -v '/Dialogs/' >> /tmp/listfile.txt
 find ../Extensions/ -name '*.js' | grep -v 'box2d.js' >> /tmp/listfile.txt
 find ../Core/GDCore -name '*.cpp' | grep -v '/Dialogs/' >> /tmp/listfile.txt
 find ../Core/GDCore -name '*.h' -o -name "*.hpp" | grep -v '/Dialogs/' >> /tmp/listfile.txt
-# Don't include old IDE anymore in the translations.
+# Don't include old IDE (GDevelop 4) anymore in the translations.
 # newIDE translations are generated separately (see newIDE/app/scripts)
 # find ../IDE -name '*.cpp' | grep -v '/wxstedit/' >> /tmp/listfile.txt
 # find ../IDE -name '*.h' -o -name "*.hpp" | grep -v '/wxstedit/' >> /tmp/listfile.txt
+
+# Don't include GDCpp anymore in the translations, as these translations were
+# for the old IDE (GDevelop 4).
+# find ../GDCpp/GDCpp/ -name '*.cpp' >> /tmp/listfile.txt
+# find ../GDCpp/GDCpp/ -name '*.h' -o -name "*.hpp" >> /tmp/listfile.txt
 
 echo "ℹ️ Generating .POT file..."
 if type xgettext 2>/dev/null; then

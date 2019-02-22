ec0b93455765b7c890ce7550f0841e1f8b2319ac
Florian Rival <Florian.Rival@gmail.com>
2019-02-22 20:20:13 +0000

    Add VideoResource

--- newIDE/app/src/EventsSheet/ParameterRenderingService.js
+++ newIDE/app/src/EventsSheet/ParameterRenderingService.js
@@ -16,6 +16,7 @@ import GlobalVariableField from './ParameterFields/GlobalVariableField';
 import ObjectVariableField from './ParameterFields/ObjectVariableField';
 import LayerField from './ParameterFields/LayerField';
 import AudioResourceField from './ParameterFields/AudioResourceField';
+import VideoResourceField from './ParameterFields/VideoResourceField';
 import ColorExpressionField from './ParameterFields/ColorExpressionField';
 import ForceMultiplierField, {
   renderForceMultiplierString,
@@ -42,6 +43,7 @@ const components = {
   file: DefaultField, //TODO
   musicfile: AudioResourceField,
   soundfile: AudioResourceField,
+  videoResource: VideoResourceField,
   color: ColorExpressionField,
   police: DefaultField, //TODO
   joyaxis: DefaultField, //TODO

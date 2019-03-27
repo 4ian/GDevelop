1f00a8622f3d13b37d611e707179611eab4b2704
Bouh <bouh.vivez@gmail.com>
2019-03-27 23:01:35 +0100

    Restart video only if paused before

--- Extensions/Video/videoruntimeobject.js
+++ Extensions/Video/videoruntimeobject.js
@@ -175,7 +175,7 @@ gdjs.VideoRuntimeObject.prototype._clamp = function(val, min, max) {
  * @param {number} volume The new volume.
  */
 gdjs.VideoRuntimeObject.prototype.setVolume = function(volume) {
-  this._volume = this._clamp(this._normalize(volume, 0, 100), 0, 1)*100;
+  this._volume = this._clamp(this._normalize(volume, 0, 100), 0, 1) * 100;
   this._renderer.updateVolume();
 };
 
@@ -255,10 +255,14 @@ gdjs.VideoRuntimeObject.prototype.getPlaybackSpeed = function() {
 gdjs.RuntimeScene.gdjsCallbackRuntimeScenePaused = function(runtimeScene) {
   for (var instances in runtimeScene._instances.items) {
     for (var object in runtimeScene._instances.items[instances]) {
-    
       var obj = runtimeScene._instances.items[instances][object];
-      if (obj.type == "Video::VideoObject") {
+      if (
+        obj.type == "Video::VideoObject" &&
+        !obj._renderer._pixiObject._texture.baseTexture.source.paused &&
+        !obj._renderer._pixiObject._texture.baseTexture.source.ended
+      ) {
         obj._renderer._pixiObject._texture.baseTexture.source.pause();
+        obj._pausedAsScenePaused = true;
       }
     }
   }
@@ -268,9 +272,8 @@ gdjs.RuntimeScene.gdjsCallbackRuntimeScenePaused = function(runtimeScene) {
 gdjs.RuntimeScene.gdjsCallbackRuntimeSceneResumed = function(runtimeScene) {
   for (var instances in runtimeScene._instances.items) {
     for (var object in runtimeScene._instances.items[instances]) {
-    
       var obj = runtimeScene._instances.items[instances][object];
-      if (obj.type == "Video::VideoObject") {
+      if (obj.type == "Video::VideoObject" && obj._pausedAsScenePaused) {
         obj._renderer._pixiObject._texture.baseTexture.source.play();
       }
     }

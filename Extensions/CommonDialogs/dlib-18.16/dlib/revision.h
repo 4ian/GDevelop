73d9604107155a795c38dbb65e0cf6ca19478cc0
Florian Rival <Florian.Rival@gmail.com>
2019-04-20 23:29:52 +0100

    [WIP] Remove all GDevelop 4 extensions not used in GDevelop 5

--- Extensions/CommonDialogs/dlib-18.16/dlib/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef DLIB_REVISION_H
-// Version:  18.16
-// Date:     Wed Jun 3 20:15:28 EDT 2015
-// Mercurial Revision ID:  42a25c606cf9
-#define DLIB_MAJOR_VERSION  18
-#define DLIB_MINOR_VERSION  16
-#endif
--- Extensions/Light/LightManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "LightManager.h"
-#include <iostream>
-
-Light_Manager::Light_Manager() : commonBlurEffectLoaded(false) {
-  std::cout << "Creating Light Manager" << std::endl;
-}
-Light_Manager::~Light_Manager() {
-  std::cout << "Destroying Light Manager" << std::endl;
-}

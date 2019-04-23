f3a86e93fdcb8e4157775a6e90028cda5a4a6107
Florian Rival <Florian.Rival@gmail.com>
2019-04-23 17:19:10 +0100

    Don't error if libGD.js can't be downloaded, but a local one exists
    This will happen when developers create commits on top of master

--- newIDE/app/scripts/import-libGD.js
+++ newIDE/app/scripts/import-libGD.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 var sourceFile = '../../../Binaries/Output/libGD.js/Release/libGD.js';
 var destinationTestDirectory = '../node_modules/libGD.js-for-tests-only';
+var alreadyHasLibGdJs = shell.test('-f', '../public/libGD.js') && shell.test('-f', destinationTestDirectory + '/index.js');
 
 if (shell.mkdir('-p', destinationTestDirectory).stderr) {
   shell.echo('❌ Error while creating node_modules folder for libGD.js');
@@ -38,13 +39,25 @@ if (shell.test('-f', sourceFile)) {
     shell.exit(1);
     return;
   }
-  var hash = (hashShellString.stdout || 'Unknown-hash').trim();
+  var hash = (hashShellString.stdout || 'unknown-hash').trim();
 
   var file = fs.createWriteStream('../public/libGD.js');
   https.get(
     `https://s3.amazonaws.com/gdevelop-gdevelop.js/${hash}/libGD.js`,
     function(response) {
       if (response.statusCode === 403) {
+        if (alreadyHasLibGdJs) {
+          shell.echo(
+            `ℹ️ Can't download libGD.js (forbidden access for hash=${hash}).`
+          );
+          shell.echo(
+            `ℹ️ As you have already a version of libGD.js, assuming you can go ahead with the existing one (this is normal if you're working on the editor only and made some commits).`
+          );
+
+          shell.exit(0);
+          return;
+        }
+
         shell.echo(
           `❌ Can't download libGD.js (forbidden access for hash=${hash}).`
         );

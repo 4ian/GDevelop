5bfb6369db276a5dd472dd9ff3d509038746bfab
Florian Rival <Florian.Rival@gmail.com>
2019-05-31 19:51:12 +0100

    Fix firebase import warning by only importing app and auth

--- newIDE/app/src/Utils/GDevelopServices/Authentification.js
+++ newIDE/app/src/Utils/GDevelopServices/Authentification.js
@@ -1,5 +1,6 @@
 // @flow
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 import { GDevelopFirebaseConfig } from './ApiConfigs';
 
 export type Profile = {

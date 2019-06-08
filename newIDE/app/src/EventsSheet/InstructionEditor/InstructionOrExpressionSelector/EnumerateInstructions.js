5188fed3e8d4b8ac79ea152817c56290a137540c
Florian Rival <Florian.Rival@gmail.com>
2019-06-08 18:40:41 +0100

    [WIP] Add new InstructionEditorDialog

--- newIDE/app/src/EventsSheet/InstructionEditor/InstructionOrExpressionSelector/EnumerateInstructions.js
+++ newIDE/app/src/EventsSheet/InstructionEditor/InstructionOrExpressionSelector/EnumerateInstructions.js
@@ -112,3 +112,172 @@ export const enumerateInstructions = (
 
   return allInstructions;
 };
+
+/**
+ * List all the instructions that can be used for the given object,
+ * in the given context. This includes instructions for the behaviors
+ * attached to the object.
+ */
+export const enumerateObjectInstructions = (
+  isCondition: boolean,
+  globalObjectsContainer: gdObjectsContainer,
+  objectsContainer: gdObjectsContainer,
+  objectName: string
+): Array<InstructionOrExpressionInformation> => {
+  let allInstructions = [];
+
+  const objectType: string = gd.getTypeOfObject(
+    globalObjectsContainer,
+    objectsContainer,
+    objectName,
+    true
+  );
+  const objectBehaviorNames: Array<string> = gd
+    .getBehaviorsOfObject(
+      globalObjectsContainer,
+      objectsContainer,
+      objectName,
+      true
+    )
+    .toJSArray();
+  const objectBehaviorTypes = new Set(
+    objectBehaviorNames.map(behaviorName =>
+      gd.getTypeOfBehavior(
+        globalObjectsContainer,
+        objectsContainer,
+        behaviorName
+      )
+    )
+  );
+
+  const allExtensions = gd
+    .asPlatform(gd.JsPlatform.get())
+    .getAllPlatformExtensions();
+  for (let i = 0; i < allExtensions.size(); ++i) {
+    const extension = allExtensions.get(i);
+    const hasObjectType =
+      extension
+        .getExtensionObjectsTypes()
+        .toJSArray()
+        .indexOf(objectType) !== -1;
+    const hasBaseObjectType =
+      extension
+        .getExtensionObjectsTypes()
+        .toJSArray()
+        .indexOf('' /* An empty string means the base object */) !== -1;
+    const behaviorTypes = extension
+      .getBehaviorsTypes()
+      .toJSArray()
+      .filter(behaviorType => objectBehaviorTypes.has(behaviorType));
+
+    if (!hasObjectType && !hasBaseObjectType && behaviorTypes.length === 0) {
+      continue;
+    }
+
+    // const prefix =
+    //   extension.getName() === 'BuiltinObject'
+    //     ? 'Common ' +
+    //       (isCondition ? 'conditions' : 'actions') +
+    //       ' for all objects' +
+    //       GROUP_DELIMITER
+    //     : '';
+    const prefix = '';
+
+    //Free instructions
+    // allInstructions = [
+    //   ...allInstructions,
+    //   ...enumerateExtensionInstructions(
+    //     prefix,
+    //     isCondition ? extension.getAllConditions() : extension.getAllActions()
+    //   ),
+    // ];
+
+    //Objects instructions:
+    if (hasObjectType) {
+      allInstructions = [
+        ...allInstructions,
+        ...enumerateExtensionInstructions(
+          prefix,
+          isCondition
+            ? extension.getAllConditionsForObject(objectType)
+            : extension.getAllActionsForObject(objectType)
+        ),
+      ];
+    }
+
+    if (hasBaseObjectType) {
+      allInstructions = [
+        ...allInstructions,
+        ...enumerateExtensionInstructions(
+          prefix,
+          isCondition
+            ? extension.getAllConditionsForObject(
+                '' /* An empty string means the base object */
+              )
+            : extension.getAllActionsForObject(
+                '' /* An empty string means the base object */
+              )
+        ),
+      ];
+    }
+
+    //Behaviors instructions:
+    // eslint-disable-next-line
+    behaviorTypes.forEach(behaviorType => {
+      allInstructions = [
+        ...allInstructions,
+        ...enumerateExtensionInstructions(
+          prefix,
+          isCondition
+            ? extension.getAllConditionsForBehavior(behaviorType)
+            : extension.getAllActionsForBehavior(behaviorType)
+        ),
+      ];
+    });
+  }
+
+  return allInstructions;
+};
+
+/**
+ * Enumerate all the instructions that are not directly tied
+ * to an object.
+ *
+ * @param {*} isCondition
+ */
+export const enumerateFreeInstructions = (
+  isCondition: boolean
+): Array<InstructionOrExpressionInformation> => {
+  let allFreeInstructions = [];
+
+  const allExtensions = gd
+    .asPlatform(gd.JsPlatform.get())
+    .getAllPlatformExtensions();
+  for (let i = 0; i < allExtensions.size(); ++i) {
+    const extension = allExtensions.get(i);
+    const allObjectsTypes = extension.getExtensionObjectsTypes();
+    const allBehaviorsTypes = extension.getBehaviorsTypes();
+
+    let prefix = '';
+    if (allObjectsTypes.size() > 0 || allBehaviorsTypes.size() > 0) {
+      prefix =
+        extension.getName() === 'BuiltinObject'
+          ? 'Common ' +
+            (isCondition ? 'conditions' : 'actions') +
+            ' for all objects'
+          : extension.getFullName();
+      prefix += GROUP_DELIMITER;
+    }
+
+    //Free instructions
+    allFreeInstructions = [
+      ...allFreeInstructions,
+      ...enumerateExtensionInstructions(
+        prefix,
+        isCondition ? extension.getAllConditions() : extension.getAllActions()
+      ),
+    ];
+  }
+
+  return allFreeInstructions;
+};
---
+++ newIDE/app/src/EventsSheet/InstructionEditor/NewInstructionEditorDialog.js
@@ -0,0 +1,245 @@
+// @flow
+import { Trans } from '@lingui/macro';
+
+import * as React from 'react';
+import Dialog from '../../UI/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import {
+  type ResourceSource,
+  type ChooseResourceFunction,
+} from '../../ResourcesList/ResourceSource.flow';
+import { type ResourceExternalEditor } from '../../ResourcesList/ResourceExternalEditor.flow';
+import InstructionParametersEditor from './InstructionParametersEditor';
+import InstructionOrObjectSelector from './InstructionOrObjectSelector';
+import { Column, Line } from '../../UI/Grid';
+import {
+  createTree,
+  type InstructionOrExpressionTreeNode,
+} from './InstructionOrExpressionSelector/CreateTree';
+import { type InstructionOrExpressionInformation } from './InstructionOrExpressionSelector/InstructionOrExpressionInformation.flow.js';
+import InstructionOrExpressionSelector from './InstructionOrExpressionSelector';
+import { enumerateObjectInstructions } from './InstructionOrExpressionSelector/EnumerateInstructions';
+import HelpButton from '../../UI/HelpButton';
+import ScrollView from '../../UI/ScrollView';
+const gd = global.gd;
+
+const styles = {
+  dialogContent: {
+    width: 'calc(100% - 16px)',
+    maxWidth: 'none',
+  },
+  dialogBody: {
+    padding: 0,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+};
+
+type Props = {|
+  project: gdProject,
+  layout: ?gdLayout,
+  globalObjectsContainer: gdObjectsContainer,
+  objectsContainer: gdObjectsContainer,
+  instruction: gdInstruction,
+  isCondition: boolean,
+  resourceSources: Array<ResourceSource>,
+  onChooseResource: ChooseResourceFunction,
+  resourceExternalEditors: Array<ResourceExternalEditor>,
+  style?: Object,
+  isNewInstruction: boolean,
+  onCancel: () => void,
+  onSubmit: () => void,
+  open: boolean,
+  openInstructionOrExpression: (
+    extension: gdPlatformExtension,
+    type: string
+  ) => void,
+|};
+type State = {|
+  chosenObjectName: ?string,
+  chosenObjectInstructionsInfo: ?Array<InstructionOrExpressionInformation>,
+  chosenObjectInstructionsInfoTree: ?InstructionOrExpressionTreeNode,
+|};
+
+export default class InstructionEditorDialog extends React.Component<
+  Props,
+  State
+> {
+  state = {
+    chosenObjectName: null,
+    chosenObjectInstructionsInfo: null,
+    chosenObjectInstructionsInfoTree: null,
+  };
+  _instructionParametersEditor: ?InstructionParametersEditor;
+
+  chooseInstruction = (type: string) => {
+    const { instruction } = this.props;
+    instruction.setType(type);
+    this.forceUpdate(() => {
+      if (this._instructionParametersEditor) {
+        this._instructionParametersEditor.focus();
+      }
+    });
+  };
+
+  chooseObject = (objectName: string) => {
+    const {
+      globalObjectsContainer,
+      objectsContainer,
+      isCondition,
+    } = this.props;
+
+    const chosenObjectInstructionsInfo = enumerateObjectInstructions(
+      isCondition,
+      globalObjectsContainer,
+      objectsContainer,
+      objectName
+    );
+    this.setState({
+      chosenObjectName: objectName,
+      chosenObjectInstructionsInfo,
+      chosenObjectInstructionsInfoTree: createTree(
+        chosenObjectInstructionsInfo
+      ),
+    });
+  };
+
+  // TOOD: This was copied from InstructionParametersEditor. Move this to a helper
+  // or pass it down.
+  _getInstructionMetadata = (): ?gdInstructionMetadata => {
+    const { instruction, isCondition, project } = this.props;
+    const type = instruction.getType();
+    if (!type) return null;
+
+    return isCondition
+      ? gd.MetadataProvider.getConditionMetadata(
+          project.getCurrentPlatform(),
+          type
+        )
+      : gd.MetadataProvider.getActionMetadata(
+          project.getCurrentPlatform(),
+          type
+        );
+  };
+
+  render() {
+    const {
+      project,
+      layout,
+      globalObjectsContainer,
+      objectsContainer,
+      onCancel,
+      onSubmit,
+      open,
+      instruction,
+      isCondition,
+    } = this.props;
+    const {
+      chosenObjectName,
+      chosenObjectInstructionsInfo,
+      chosenObjectInstructionsInfoTree,
+    } = this.state;
+    const instructionType = instruction.getType();
+
+    const instructionMetadata = this._getInstructionMetadata();
+    const instructionHelpPage = instructionMetadata
+      ? instructionMetadata.getHelpPath()
+      : undefined;
+
+    return (
+      <Dialog
+        actions={[
+          <FlatButton
+            label={<Trans>Cancel</Trans>}
+            primary={false}
+            onClick={onCancel}
+            key="cancel"
+          />,
+          <FlatButton
+            label={<Trans>Ok</Trans>}
+            primary={true}
+            keyboardFocused={false}
+            disabled={!instructionType}
+            onClick={onSubmit}
+            key="ok"
+          />,
+        ]}
+        secondaryActions={[
+          <HelpButton
+            key="help"
+            helpPagePath={instructionHelpPage || '/events'}
+            label={
+              !instructionHelpPage ? (
+                <Trans>Help</Trans>
+              ) : this.props.isCondition ? (
+                <Trans>Help for this condition</Trans>
+              ) : (
+                <Trans>Help for this action</Trans>
+              )
+            }
+          />,
+        ]}
+        open={open}
+        onRequestClose={onCancel}
+        contentStyle={styles.dialogContent}
+        bodyStyle={styles.dialogBody}
+      >
+        {!instructionType && !chosenObjectName && (
+          <Column expand noMargin>
+            <InstructionOrObjectSelector
+              style={{ flex: 1, display: 'flex', flexDirection: 'column' }} // TODO
+              project={project}
+              globalObjectsContainer={globalObjectsContainer}
+              objectsContainer={objectsContainer}
+              isCondition={isCondition}
+              selectedType={instructionType}
+              onChooseInstruction={this.chooseInstruction}
+              onChooseObject={this.chooseObject}
+              focusOnMount={!instructionType}
+            />
+          </Column>
+        )}
+        {(instructionType || chosenObjectName) && (
+          <Line expand noMargin>
+            {chosenObjectName &&
+              chosenObjectInstructionsInfoTree &&
+              chosenObjectInstructionsInfo && (
+                <ScrollView>
+                  <InstructionOrExpressionSelector
+                    instructionsInfo={chosenObjectInstructionsInfo}
+                    instructionsInfoTree={chosenObjectInstructionsInfoTree}
+                    iconSize={24}
+                    onChoose={this.chooseInstruction}
+                    selectedType={instructionType}
+                    useSubheaders
+                  />
+                </ScrollView>
+              )}
+            <Column expand>
+              <InstructionParametersEditor
+                project={project}
+                layout={layout}
+                globalObjectsContainer={globalObjectsContainer}
+                objectsContainer={objectsContainer}
+                objectName={chosenObjectName}
+                isCondition={isCondition}
+                instruction={instruction}
+                resourceSources={this.props.resourceSources}
+                onChooseResource={this.props.onChooseResource}
+                resourceExternalEditors={this.props.resourceExternalEditors}
+                openInstructionOrExpression={
+                  this.props.openInstructionOrExpression
+                }
+                ref={instructionParametersEditor =>
+                  (this._instructionParametersEditor = instructionParametersEditor)
+                }
+                focusOnMount={instruction.getType()}
+                noHelpButton
+              />
+            </Column>
+          </Line>
+        )}
+      </Dialog>
+    );
+  }
+}

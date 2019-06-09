6448c6a78a9e96eabb126223194611f43f12414c
Wend1go <Matthias83@gmx.de>
2019-06-09 22:20:30 +0200

    Add scale actions/conditions for Text objects (#1092)

--- Extensions/TextObject/textruntimeobject.js
+++ Extensions/TextObject/textruntimeobject.js
@@ -23,6 +23,8 @@ gdjs.TextRuntimeObject = function(runtimeScene, objectData)
     this.opacity = 255;
     this._wrapping = false;
     this._wrappingWidth = 1;
+    this._scaleX = 1;
+    this._scaleY = 1;
 
     this._str = objectData.string;
 
@@ -186,6 +188,55 @@ gdjs.TextRuntimeObject.prototype.getWidth = function() {
  */
 gdjs.TextRuntimeObject.prototype.getHeight = function() {
     return this._renderer.getHeight();
+};
+
+/**
+ * Get scale of the text.
+ */
+gdjs.TextRuntimeObject.prototype.getScale = function() {
+    return (Math.abs(this._scaleX)+Math.abs(this._scaleY))/2.0; 
+};
+
+/**
+ * Get y-scale of the text.
+ */
+gdjs.TextRuntimeObject.prototype.getScaleX = function() {
+    return this._renderer.getScaleX();
+};
+
+/**
+ * Get x-scale of the text.
+ */
+gdjs.TextRuntimeObject.prototype.getScaleY = function() {
+    return this._renderer.getScaleY();
+};
+
+/**
+ * Set the text object scale.
+ * @param {number} newScale The new scale for the text object.
+ */
+gdjs.TextRuntimeObject.prototype.setScale = function(newScale) {
+    this._scaleX = newScale;
+    this._scaleY = newScale;
+    this._renderer.setScale(newScale);
+};
+
+/**
+ * Set the text object x-scale.
+ * @param {number} newScale The new x-scale for the text object.
+ */
+gdjs.TextRuntimeObject.prototype.setScaleX = function(newScale) {
+    this._scaleX = newScale;
+    this._renderer.setScaleX(newScale);
+};
+
+/**
+ * Set the text object y-scale.
+ * @param {number} newScale The new y-scale for the text object.
+ */
+gdjs.TextRuntimeObject.prototype.setScaleY = function(newScale) {
+    this._scaleY = newScale;
+    this._renderer.setScaleY(newScale);
 };
 
 /**

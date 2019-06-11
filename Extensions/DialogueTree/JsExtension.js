b80bf3fd773cab281268b7b3e419bec03e197be7
Todor Imreorov <blurymind@gmail.com>
2019-06-11 20:35:01 +0100

    add options selection

--- Extensions/DialogueTree/JsExtension.js
+++ Extensions/DialogueTree/JsExtension.js
@@ -25,10 +25,10 @@ module.exports = {
 
 		extension
 			.addAction(
-				'LoadDialogueFromVariable',
-				_('LoadDialogue'),
-				_('Load a dialogue data object - Yarn json format, storedin a scene variable'),
-				_('Load Dialogue data from scene variable: _PARAM1_ and start from _PARAM2_'),
+				'LoadDialogueFromSceneVariable',
+				_('Load Dialogue Tree from a Scene Variable'),
+				_('Load a dialogue data object - Yarn json format, stored in a scene variable'),
+				_('Load Dialogue data from Scene Variable: _PARAM1_ and begin from _PARAM2_ Node'),
 				_('Dialogue Tree'),
 				'JsPlatform/Extensions/yarn24.png',
 				'JsPlatform/Extensions/yarn32.png'
@@ -36,14 +36,15 @@ module.exports = {
 			.addCodeOnlyParameter('currentScene', '')
 			.addParameter('scenevar', _('Scene variable that holds the Yarn Json data'))
 			.addParameter('string', _('Dialogue node'))
+			.setDefaultValue('Start')
 			.getCodeExtraInformation()
 			.setIncludeFile('Extensions/DialogueTree/dialoguetools.js')
 			.addIncludeFile('Extensions/DialogueTree/bondage.min.js')
-			.setFunctionName('gdjs.dialoguetree.load');
+			.setFunctionName('gdjs.dialoguetree.loadFromSceneVar');
 
 		extension
 			.addAction(
-				'AdvanceToNextDialogueLine',
+				'AdvanceToNextLine',
 				_('Advance to the Next Dialogue Line'),
 				_('Advance to the Next dialogue line'),
 				_('Advance to the Next Dialogue Line'),
@@ -51,13 +52,71 @@ module.exports = {
 				'JsPlatform/Extensions/yarn24.png',
 				'JsPlatform/Extensions/yarn32.png'
 			)
-			.addCodeOnlyParameter('currentScene', '')
+			// .addCodeOnlyParameter('currentScene', '')
 			.getCodeExtraInformation()
 			.setFunctionName('gdjs.dialoguetree.advanceDialogue');
 
 		extension
+			.addAction(
+				'ConfirmSelectOption',
+				_('Confirm Selected Option'),
+				_('Confirm Selected Option'),
+				_('Confirm Selected Option'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			// .addCodeOnlyParameter('currentScene', '')
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.confirmSelectOption');
+
+		extension
+			.addAction(
+				'SelectNextOption',
+				_('Select Next Option'),
+				_('Select Next Option (add 1 to selected option index)'),
+				_('Select Next Option'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			// .addCodeOnlyParameter('currentScene', '')
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.selectNextOption');
+
+		extension
+			.addAction(
+				'SelectPreviousOption',
+				_('Select Previous Option'),
+				_('Select Previous Option (subtract 1 from selected option index)'),
+				_('Select Previous Option'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			// .addCodeOnlyParameter('currentScene', '')
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.selectPreviousOption');
+
+		extension
+			.addAction(
+				'SelectOption',
+				_('Select Option by Index'),
+				_('Select Option by Index'),
+				_('Select Option by Index'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			// .addCodeOnlyParameter('currentScene', '')
+			.addParameter('expression', _('Option index number'))
+			.setDefaultValue('0')
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.selectPreviousOption');
+
+		extension
 			.addStrExpression(
-				'GetDialogueLineText',
+				'GetLineText',
 				_('Get the current dialogue line text'),
 				_('This returns the current dialogue line text'),
 				_('Dialogue Tree'),
@@ -65,19 +124,56 @@ module.exports = {
 				'JsPlatform/Extensions/yarn32.png'
 			)
 			.getCodeExtraInformation()
-			.setFunctionName('gdjs.dialoguetree.getDialogueLineText');
+			.setFunctionName('gdjs.dialoguetree.getLineText');
 
 		extension
 			.addStrExpression(
-				'GetDialogueLineType',
+				'GetLineType',
 				_('Get the current dialogue line type as a text'),
-				_('This returns the current dialogue line text'),
+				_('Get the current dialogue line type as a text'),
 				_('Dialogue Tree'),
 				'JsPlatform/Extensions/yarn24.png',
 				'JsPlatform/Extensions/yarn32.png'
 			)
 			.getCodeExtraInformation()
-			.setFunctionName('gdjs.dialoguetree.getDialogueLineType');
+			.setFunctionName('gdjs.dialoguetree.getLineType');
+
+		extension
+			.addExpression(
+				'GetOptionsCount',
+				_('Get the number of options in an Options line type'),
+				_('Get the number of options in an Options line type'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.lineOptionsCount');
+
+		extension
+			.addStrExpression(
+				'GetOption',
+				_('Get the text of an option from an Options line type'),
+				_('Get the text of an option from an Options line type'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			.addParameter('expression', _('Option Index Number'))
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.getLineOption');
+
+		extension
+			.addExpression(
+				'GetSelectedOptionIndex',
+				_('Get the index of the currently selected option'),
+				_('Get the index of the currently selected option'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.getSelectOption');
 
 		extension
 			.addCondition(
@@ -90,7 +186,7 @@ module.exports = {
 				'JsPlatform/Extensions/yarn32.png'
 			)
 			.getCodeExtraInformation()
-			.setFunctionName('gdjs.dialoguetree.dialogueLineTypeIsText');
+			.setFunctionName('gdjs.dialoguetree.lineTypeIsText');
 
 		extension
 			.addCondition(
@@ -103,7 +199,7 @@ module.exports = {
 				'JsPlatform/Extensions/yarn32.png'
 			)
 			.getCodeExtraInformation()
-			.setFunctionName('gdjs.dialoguetree.dialogueLineTypeIsCommand');
+			.setFunctionName('gdjs.dialoguetree.lineTypeIsCommand');
 
 		extension
 			.addCondition(
@@ -116,7 +212,7 @@ module.exports = {
 				'JsPlatform/Extensions/yarn32.png'
 			)
 			.getCodeExtraInformation()
-			.setFunctionName('gdjs.dialoguetree.dialogueLineTypeIsOptions');
+			.setFunctionName('gdjs.dialoguetree.lineTypeIsOptions');
 
 		extension
 			.addCondition(
@@ -129,7 +225,20 @@ module.exports = {
 				'JsPlatform/Extensions/yarn32.png'
 			)
 			.getCodeExtraInformation()
-			.setFunctionName('gdjs.dialoguetree.isDialogueRunning');
+			.setFunctionName('gdjs.dialoguetree.isRunning');
+
+		extension
+			.addCondition(
+				'Selected Option has Changed',
+				_('Selected Option has Changed'),
+				_('Selected Option has Changed'),
+				_('Selected Option has Changed'),
+				_('Dialogue Tree'),
+				'JsPlatform/Extensions/yarn24.png',
+				'JsPlatform/Extensions/yarn32.png'
+			)
+			.getCodeExtraInformation()
+			.setFunctionName('gdjs.dialoguetree.selectedOptionHasUpdated');
 		//finally return it
 		return extension;
 	},
--- Extensions/DialogueTree/dialoguetools.js
+++ Extensions/DialogueTree/dialoguetools.js
@@ -12,10 +12,13 @@ gdjs.dialoguetree.runner = new bondage.Runner();
  * Save a screenshot of the game.
  * @param {string} savepath The path where to save the screenshot
  */
-gdjs.dialoguetree.load = function(runtimeScene, sceneVar, startDialogueNode) {
+gdjs.dialoguetree.loadFromSceneVar = function(runtimeScene, sceneVar, startDialogueNode) {
 	this.runner = gdjs.dialoguetree.runner; //TODO needs to be initiated globally once, outside of any methods
 	this.yarnData = JSON.parse(sceneVar.getAsString());
 	this.runner.load(this.yarnData);
+	this.optionsCount = 0;
+	this.options = [];
+	this.selectOption = -1;
 
 	if (startDialogueNode) {
 		this.dialogueIsRunning = true;
@@ -24,11 +27,20 @@ gdjs.dialoguetree.load = function(runtimeScene, sceneVar, startDialogueNode) {
 	}
 };
 
-gdjs.dialoguetree.isDialogueRunning = function() {
+gdjs.dialoguetree.isRunning = function() {
 	return this.dialogueIsRunning;
 };
 
-gdjs.dialoguetree.getDialogueLineText = function() {
+gdjs.dialoguetree.selectedOptionHasUpdated = function() {
+	if (this.selectedOptionUpdated) {
+		console.log('UPDATED!');
+		this.selectedOptionUpdated = false;
+		return true;
+	}
+	return this.selectedOptionUpdated;
+};
+
+gdjs.dialoguetree.getLineText = function() {
 	return this.dialogueData.text
 		? this.dialogueData.text
 		: this.dialogueData.options
@@ -36,32 +48,92 @@ gdjs.dialoguetree.getDialogueLineText = function() {
 		: '';
 };
 
-gdjs.dialoguetree.dialogueLineOptions = function() {
-	return this.dialogueData.options;
+gdjs.dialoguetree._normalizedOptionIndex = function(optionIndex) {
+	if (optionIndex >= this.options.length) optionIndex = this.options.length - 1;
+	if (optionIndex < 0) optionIndex = 0;
+	return optionIndex;
 };
 
-gdjs.dialoguetree.dialogueLineOptionsCount = function() {
-	return this.dialogueData.options.length;
+gdjs.dialoguetree._cycledOptionIndex = function(optionIndex) {
+	if (optionIndex >= this.options.length) optionIndex = 0;
+	if (optionIndex < 0) optionIndex = this.options.length - 1;
+	return optionIndex;
 };
 
-gdjs.dialoguetree.dialogueLineTypeIsText = function() {
+gdjs.dialoguetree.getLineOption = function(optionIndex) {
+	if (!this.options.length) return [];
+	optionIndex = gdjs.dialoguetree._normalizedOptionIndex(optionIndex);
+	return this.options[optionIndex];
+};
+
+gdjs.dialoguetree.lineOptionsCount = function() {
+	if (this.options.length) {
+		// console.log(this.optionsCount);
+		return this.optionsCount;
+	}
+};
+
+gdjs.dialoguetree.confirmSelectOption = function() {
+	if (this.dialogueData.select) {
+		this.dialogueData.select(this.selectOption);
+		gdjs.dialoguetree.advanceDialogue();
+	}
+};
+
+gdjs.dialoguetree.selectNextOption = function() {
+	if (this.dialogueData.select) {
+		this.selectOption += 1;
+		this.selectOption = gdjs.dialoguetree._cycledOptionIndex(this.selectOption);
+		this.selectedOptionUpdated = true;
+	}
+};
+
+gdjs.dialoguetree.selectPreviousOption = function() {
+	if (this.dialogueData.select) {
+		this.selectOption -= 1;
+		this.selectOption = gdjs.dialoguetree._cycledOptionIndex(this.selectOption);
+		this.selectedOptionUpdated = true;
+	}
+};
+
+gdjs.dialoguetree.selectOption = function(optionIndex) {
+	if (this.dialogueData.select) {
+		this.selectOption = gdjs.dialoguetree._normalizedOptionIndex(this.selectOption);
+		this.selectedOptionUpdated = true;
+	}
+};
+
+gdjs.dialoguetree.getSelectOption = function() {
+	if (this.dialogueData.select) {
+		console.log(this.selectOption);
+		return this.selectOption;
+	}
+};
+
+gdjs.dialoguetree.lineTypeIsText = function() {
 	return this.dialogueData instanceof bondage.TextResult;
 };
-gdjs.dialoguetree.dialogueLineTypeIsOptions = function() {
+gdjs.dialoguetree.lineTypeIsOptions = function() {
 	return this.dialogueData instanceof bondage.OptionsResult;
 };
-gdjs.dialoguetree.dialogueLineTypeIsCommand = function() {
+gdjs.dialoguetree.lineTypeIsCommand = function() {
 	return this.dialogueData instanceof bondage.CommandResult;
 };
 
 gdjs.dialoguetree.advanceDialogue = function() {
 	this.dialogueData = this.dialogue.next().value;
+	this.optionsCount = 0;
+	this.selectedOptionUpdated = false;
 	console.log(this.dialogueData);
-	if (gdjs.dialoguetree.dialogueLineTypeIsText()) {
+	if (gdjs.dialoguetree.lineTypeIsText()) {
 		this.dialogueDataType = 'text';
-	} else if (gdjs.dialoguetree.dialogueLineTypeIsOptions()) {
+	} else if (gdjs.dialoguetree.lineTypeIsOptions()) {
 		this.dialogueDataType = 'options';
-	} else if (gdjs.dialoguetree.dialogueLineTypeIsCommand()) {
+		this.optionsCount = this.dialogueData.options.length;
+		this.options = this.dialogueData.options;
+		this.selectOption = 0;
+		this.selectedOptionUpdated = true;
+	} else if (gdjs.dialoguetree.lineTypeIsCommand()) {
 		this.dialogueDataType = 'command';
 	} else {
 		this.dialogueDataType = 'unknown';
@@ -69,7 +141,7 @@ gdjs.dialoguetree.advanceDialogue = function() {
 	if (!this.dialogueData) this.dialogueIsRunning = false;
 };
 
-gdjs.dialoguetree.getDialogueLineType = function() {
+gdjs.dialoguetree.getLineType = function() {
 	return this.dialogueDataType;
 };
 
@@ -78,4 +150,8 @@ gdjs.dialoguetree.getDialogueLineType = function() {
 
 //get title(expr)
 
+// has title been visited before, how many times has title been visited
+
 // check if it was visited before (how many times?)
+
+//Load/save visited, all variables -state

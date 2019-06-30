45070d33239b2f58911c4193a819cad2ae3892ef
Florian Rival <Florian.Rival@gmail.com>
2019-06-30 14:37:17 +0100

    Add support for choosing json resource in IDE

--- newIDE/app/src/ResourcesList/ResourceSource.flow.js
+++ newIDE/app/src/ResourcesList/ResourceSource.flow.js
@@ -1,7 +1,9 @@
 // @flow
 import * as React from 'react';
 
-export type ResourceKind = 'image' | 'audio' | 'font' | 'video';
+// These are all the kind of resources that can be found in
+// Core/GDCore/Project/ResourcesManager.h
+export type ResourceKind = 'image' | 'audio' | 'font' | 'video' | 'json';
 
 export type ResourceSource = {
   name: string,

aa0fce5e952408a2febd4d65899d96e01fcb1458
blurymind <blurymind@gmail.com>
2019-07-20 19:23:59 +0100

    add save and close button

--- newIDE/app/public/external/yarn/yarn-main.js
+++ newIDE/app/public/external/yarn/yarn-main.js
@@ -7,112 +7,43 @@ const remote = electron.remote;
 
 let yarn = null;
 
+const saveAndClose = () => {
+  yarn.data.trySaveCurrent(() => remote.getCurrentWindow().close());
+
+  // todo - send to GD the path of the currently edited file - so if it was changed - gd can adopt to the change
+};
+
 const closeWindow = () => {
   remote.getCurrentWindow().close();
 };
 
 const editorFrameEl = document.getElementById('yarn-frame');
-// editorFrameEl.addEventListener('load', function() {
-//   // alert('myframe is loaded');
-//   ipcRenderer.send('yarn-ready');
-// }, true)
-
-
-
 window.addEventListener('yarnReady', e => {
-  yarn = e.mainCtrl;
-  console.log(e);
+  yarn = e;
+  yarn.app.fs = fs;
   ipcRenderer.send('yarn-ready');
 });
-// Trigger the load of Jfxr manually, to ensure the event listener "jfxrReady" is registered already
 editorFrameEl.src = 'yarn-editor/app/index.html';
 
-// Called to load a sound. Should be called after the window is fully loaded.
-let receivedData
+// Called to load yarn data. Should be called after the window is fully loaded.
+let receivedData;
 ipcRenderer.on('yarn-open', (event, receivedOptions) => {
-  // console.log('ready!',receivedOptions);
+  console.log('ready!', receivedOptions);
   if (!yarn) return;
-  // console.log(yarn);
-  yarn.editingPath(receivedOptions.resourcePath);
-	yarn.editingType('json');
-  yarn.loadData(receivedOptions.yarnData, 'json', true)
+  yarn.data.editingPath(receivedOptions.resourcePath);
+  yarn.data.editingType('json');
+  yarn.data.loadData(receivedOptions.externalEditorData, 'json', true);
   receivedData = receivedOptions;
 
-  
+  //inject custom save+close button
+  const saveToGdButton = yarn.document
+    .getElementsByClassName('menu')[0]
+    .cloneNode(true);
+  saveToGdButton.onclick = () => saveAndClose();
+  yarn.document
+    .getElementsByClassName('app-menu')[0]
+    .appendChild(saveToGdButton);
+  saveToGdButton.childNodes[1].firstChild.data = 'Save & close';
+
+  console.log(yarn.data);
 });
-
-// const loadMetaData = externalEditorData => {
-//   if ('jfxr' in externalEditorData) {
-//     jfxr.getSound().parse(externalEditorData.jfxr.data);
-//   } else {
-//     jfxr.applyPreset(jfxr.presets[1])
-//   }
-// }
-
-// const saveSoundEffect = pathEditor => {
-//   const externalEditorData = {
-//     data: jfxr.getSound().serialize(),
-//     name: pathEditor.state.name
-//   }
-
-//   jfxr.synth.run().then(data => {
-//     var blob = new Blob([data.toWavBytes()], {
-//       type: 'audio/wav'
-//     })
-//     var fileReader = new FileReader()
-//     fileReader.onload = function () {
-//       fs.writeFileSync(pathEditor.state.fullPath, Buffer(new Uint8Array(this.result)))
-//       ipcRenderer.send('jfxr-changes-saved', pathEditor.state.fullPath, externalEditorData)
-//       closeWindow()
-//     }
-//     fileReader.readAsArrayBuffer(blob)
-//   })
-// }
-
-// // Gain access to JFXR controller by using the signal that the JFXR author kindly provided.
-// // It gets fired upon loading of jfxr in the iframe element
-// const editorFrameEl = document.getElementById('jfxr-frame')
-// window.addEventListener('jfxrReady', (e) => {
-//   jfxr = e.mainCtrl;
-//   ipcRenderer.send('jfxr-ready');
-// });
-// // Trigger the load of Jfxr manually, to ensure the event listener "jfxrReady" is registered already
-// editorFrameEl.src = 'jfxr-editor/index.html'
-
-// // Called to load a sound. Should be called after the window is fully loaded.
-// ipcRenderer.on('jfxr-open', (event, receivedOptions) => {
-//   loadMetaData(receivedOptions.externalEditorData);
-
-//   // Load a custom save file(s) header
-//   const pathEditorHeaderDiv = document.getElementById('path-editor-header');
-//   const headerStyle = {
-//     saveFolderLabel: 'float: left;margin-left: 2px; font-size:15px;margin-top: 10px;color:aqua',
-//     nameInput: 'font-family:"Courier New";height:27px;width:90px;float:left;margin-left: 2px;padding:4px;margin-top: 4px;font-size:15px;border: 2px solid #e5cd50;border-radius: 3px;background-color:black; color: #e5cd50;',
-//     saveButton: 'float:right;margin-left:2px;margin-right:4px;border: 2px solid white;border-radius: 1px;margin-top: 5px;background-color:white;',
-//     cancelButton: 'float:right;margin-right:2px;border: 2px solid white;border-radius: 1px;margin-top: 5px;background-color:white;',
-//     setFolderButton: 'float:right;margin-left:2px;margin-right:4px;border: 2px solid white;border-radius: 1px;margin-top: 5px;background-color:white;',
-//     fileExistsLabel: 'height:27px;color:blue;float: left;margin-left: 2px;margin-top: 10px; font-size:15px;'
-//   };
-//   createPathEditorHeader({
-//     parentElement: pathEditorHeaderDiv,
-//     editorContentDocument: document,
-//     onSaveToGd: saveSoundEffect,
-//     onCancelChanges: closeWindow,
-//     projectPath: receivedOptions.projectPath,
-//     initialResourcePath: receivedOptions.resourcePath,
-//     extension: '.wav',
-//     headerStyle
-//   });
-//   // Disable google analytics from collecting personal information
-//   editorFrameEl.contentWindow.ga('set', 'allowAdFeatures', false);
-//   // Alter the interface of the external editor
-//   const editorContentDocument = editorFrameEl.contentDocument;
-//   editorContentDocument.getElementsByClassName('github')[0].remove();
-//   // Disable inside iframe links - they break the embedding
-//   editorContentDocument.getElementsByClassName('titlepane column-left')[0].childNodes[0].onclick = () => {
-//     return false
-//   };
-//   editorContentDocument.getElementsByClassName('titlepane column-left')[0].childNodes[1].onclick = () => {
-//     return false
-//   };
-// })

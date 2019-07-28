e7dec09802167e3fcd8138a6992b7967b21b1190
Florian Rival <Florian.Rival@gmail.com>
2019-07-28 14:08:35 +0100

    Fix GDevelop.js tests

--- GDevelop.js/__tests__/Serializer.js
+++ GDevelop.js/__tests__/Serializer.js
@@ -65,7 +65,7 @@ describe('libGD.js object serialization', function() {
       obj.delete();
 
       expect(jsonObject).toBe(
-        '{"bold": false,"italic": false,"name": "testObject","smoothed": true,"tags": "inventory, player","type": "TextObject::Text","underlined": false,"variables": [],"behaviors": [],"string": "Text of the object, with 官话 characters","font": "","characterSize": 20,"color": {"b": 255,"g": 255,"r": 255}}'
+        '{"bold": false,"italic": false,"name": "testObject","smoothed": true,"tags": "inventory, player","type": "TextObject::Text","underlined": false,"variables": [],"behaviors": [],"string": "Text of the object, with 官话 characters","font": "","characterSize": 20,"color": {"b": 0,"g": 0,"r": 0}}'
       );
     });
   });

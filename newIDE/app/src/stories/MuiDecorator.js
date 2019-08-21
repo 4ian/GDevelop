2c2ed1c85aa93255c2b303ffdab326aa3c80e8ee
Florian Rival <Florian.Rival@gmail.com>
2019-08-20 23:19:23 -0700

    Rename MuiThemeProvider to V0MuiThemeProvider

--- newIDE/app/src/stories/MuiDecorator.js
+++ newIDE/app/src/stories/MuiDecorator.js
@@ -1,11 +1,13 @@
 // @flow
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import V0MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import defaultTheme from '../UI/Theme/DefaultTheme';
 import { type StoryDecorator } from '@storybook/react';
 
 const muiDecorator: StoryDecorator = (story, context) => (
-  <MuiThemeProvider muiTheme={defaultTheme}>{story(context)}</MuiThemeProvider>
+  <V0MuiThemeProvider muiTheme={defaultTheme}>
+    {story(context)}
+  </V0MuiThemeProvider>
 );
 
 export default muiDecorator;

4fd76989070c9a4d2d730bce531baf807d2439a7
Florian Rival <Florian.Rival@gmail.com>
2019-08-22 21:55:33 +0200

    Improve typing for Dialog

--- newIDE/app/src/UI/Dialog/index.js
+++ newIDE/app/src/UI/Dialog/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// @flow
+import * as React from 'react';
 import Dialog from 'material-ui/Dialog';
 
 const styles = {
@@ -16,11 +17,44 @@ const styles = {
   },
 };
 
+// We support a subset of the props supported by Material-UI v0.x Dialog
+// They should be self descriptive - refer to Material UI docs otherwise.
+type Props = {|
+  open?: boolean,
+  noMargin?: boolean,
+  title?: React.Node,
+  actions?: React.Node,
+  secondaryActions?: React.Node,
+
+  onRequestClose?: () => void,
+
+  modal?: boolean, // Force the user to use one of the actions in the Dialog. Clicking outside the Dialog will not trigger the onRequestClose.
+
+  children: React.Node, // The content of the dialog
+
+  // Style:
+  contentStyle?: {|
+    maxWidth?: string | number,
+    width?: string,
+  |},
+  titleStyle?: {| padding?: 0 |},
+  bodyStyle?: {|
+    padding?: 0,
+    display?: 'flex',
+    flexDirection?: 'row',
+    overflowX?: 'hidden',
+  |},
+
+  // Positioning:
+  autoScrollBodyContent?: boolean,
+  repositionOnUpdate?: boolean,
+|};
+
 /**
  * A enhanced material-ui Dialog that can have optional secondary actions
  * and no margins if required.
  */
-export default props => {
+export default (props: Props) => {
   const { secondaryActions, actions, noMargin, title, ...otherProps } = props;
   const dialogActions = secondaryActions
     ? [

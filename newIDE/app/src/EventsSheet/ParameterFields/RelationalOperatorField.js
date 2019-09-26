15069a4363da0c2c348780ea2aaf9757c1de4d89
Florian Rival <Florian.Rival@gmail.com>
2019-09-26 23:39:45 +0100

    Show operators as invalid in the events sheet if not chosen or wrong value entered

--- newIDE/app/src/EventsSheet/ParameterFields/RelationalOperatorField.js
+++ newIDE/app/src/EventsSheet/ParameterFields/RelationalOperatorField.js
@@ -1,9 +1,14 @@
+// @flow
+import { Trans } from '@lingui/macro';
 import { t } from '@lingui/macro';
 import React, { Component } from 'react';
+import { type ParameterInlineRendererProps } from './ParameterInlineRenderer.flow';
+import { type ParameterFieldProps } from './ParameterFieldCommons';
 import SelectField from '../../UI/SelectField';
 import SelectOption from '../../UI/SelectOption';
 
-export default class RelationalOperatorField extends Component {
+export default class RelationalOperatorField extends Component<ParameterFieldProps> {
+  _field: ?SelectField;
   focus() {
     if (this._field && this._field.focus) this._field.focus();
   }
@@ -17,10 +22,9 @@ export default class RelationalOperatorField extends Component {
     return (
       <SelectField
         fullWidth
-        floatingLabelFixed
         floatingLabelText={description}
         value={this.props.value}
-        onChange={(e, i, value) => this.props.onChange(value)}
+        onChange={(e, i, value: string) => this.props.onChange(value)}
         ref={field => (this._field = field)}
         hintText={t`Choose an operator`}
       >
@@ -40,7 +44,27 @@ export default class RelationalOperatorField extends Component {
 
 export const renderInlineRelationalOperator = ({
   value,
+  InvalidParameterValue,
 }: ParameterInlineRendererProps) => {
+  if (!value) {
+    return (
+      <InvalidParameterValue isEmpty>
+        <Trans>Choose an operator</Trans>
+      </InvalidParameterValue>
+    );
+  }
+
+  if (
+    value !== '=' &&
+    value !== '<' &&
+    value !== '>' &&
+    value !== '<=' &&
+    value !== '>=' &&
+    value !== '!='
+  ) {
+    return <InvalidParameterValue>{value}</InvalidParameterValue>;
+  }
+
   if (value === '<=') return '\u2264';
   if (value === '>=') return '\u2265';
   else if (value === '!=') return '\u2260';

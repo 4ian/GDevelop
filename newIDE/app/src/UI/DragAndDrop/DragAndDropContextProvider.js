4d8db625a761cf4a4fb792e31e144ba3ed95f104
Florian Rival <Florian.Rival@gmail.com>
2019-10-01 12:58:33 +0100

    Use react-dnd-multi-backend to automatically support touch screens for drag'n'drop

--- newIDE/app/src/UI/DragAndDrop/DragAndDropContextProvider.js
+++ newIDE/app/src/UI/DragAndDrop/DragAndDropContextProvider.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import HTML5Backend from 'react-dnd-html5-backend';
-import TouchBackend from 'react-dnd-touch-backend';
+import MultiBackend from 'react-dnd-multi-backend';
+import HTML5toTouch from 'react-dnd-multi-backend/lib/HTML5toTouch';
 import { DragDropContext } from 'react-dnd';
 
 class DragAndDropContextProvider extends Component {
@@ -9,4 +9,14 @@ class DragAndDropContextProvider extends Component {
   }
 }
 
-export default DragDropContext(HTML5Backend)(DragAndDropContextProvider);
+/**
+ * A react-dnd provider that automatically switch to react-dnd-touch-backend
+ * when a touch event is recognized (react-dnd-html5-backend won't work on
+ * touch devices like phones).
+ *
+ * When doing the switch from HTML5 backend to Touch backend, the existing events
+ * are passed to the new backend. Unsure if this is necessary in GDevelop case.
+ */
+export default DragDropContext(MultiBackend(HTML5toTouch))(
+  DragAndDropContextProvider
+);

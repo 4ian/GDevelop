910b1ea1a8a774f7eccba9d70a30a4bef2e3c8b3
blurymind <blurymind@gmail.com>
2019-10-14 23:03:42 +0100

    cleanup

--- newIDE/app/src/Utils/ColorTransformer.js
+++ newIDE/app/src/Utils/ColorTransformer.js
@@ -16,10 +16,9 @@ export const rgbToHexNumber = (r: number, g: number, b: number) =>
  * Convert a hex color value to an rgb object value.
  */
 export const getRGBColorFromHex = (hex: string) => {
-  let c;
   if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    c = hex.substring(1).split('');
-    if (c.length == 3) {
+    let c = hex.substring(1).split('');
+    if (c.length === 3) {
       c = [c[0], c[0], c[1], c[1], c[2], c[2]];
     }
     c = '0x' + c.join('');

2e84b2e6484c8eeaa33e41480aa6a6279e22278e
Florian Rival <Florian.rival@gmail.com>
2019-10-29 19:30:29 +0100

    Fix typo

--- Core/GDCore/Extensions/Builtin/FileExtension.cpp
+++ Core/GDCore/Extensions/Builtin/FileExtension.cpp
@@ -115,7 +115,7 @@ void GD_CORE_API BuiltinExtensionsImplementer::ImplementsFileExtension(
       .AddAction(
           "LireFichierTxt",
           _("Read a text"),
-          _("Read the text saved in the specified element and store it in a scene"
+          _("Read the text saved in the specified element and store it in a scene "
             "variable.\nSpecify the structure leading to the element using / "
             "(example : Root/Level/Current)\nSpaces are forbidden in element "
             "names."),

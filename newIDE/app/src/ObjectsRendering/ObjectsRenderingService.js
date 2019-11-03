b0fd36daffdb65d81294dc90ffbfc8fcc64754ae
Florian Rival <Florian.Rival@gmail.com>
2019-11-03 22:40:31 +0000

    Add registerModule and requireModule to ObjectsRenderingService, to bundle third party libraries

--- newIDE/app/src/ObjectsRendering/ObjectsRenderingService.js
+++ newIDE/app/src/ObjectsRendering/ObjectsRenderingService.js
@@ -12,7 +12,11 @@ import PixiResourcesLoader from './PixiResourcesLoader';
 import ResourcesLoader from '../ResourcesLoader';
 import RenderedInstance from './Renderers/RenderedInstance';
 import * as PIXI from 'pixi.js';
+import optionalRequire from '../Utils/OptionalRequire';
+const path = optionalRequire('path');
 const gd = global.gd;
+
+const requirableModules = {};
 
 /**
  * A service containing functions that are called to render instances
@@ -93,6 +97,33 @@ export default {
 
     console.info(`Properly registered renderer for object "${objectType}".`);
     this.renderers[objectType] = renderer;
+  },
+  /**
+   * Register a module that can be then required using `requireModule`.
+   * This is necessary for the web-app, as all files must be bundled.
+   */
+  registerModule: function(requirePath: string, module: any) {
+    requirableModules[requirePath] = module;
+  },
+  /**
+   * Require a module, that was either registered using `registerModule` (i.e: on the web-app), or from
+   * the specified path (if `optionalRequire` can find the file, i.e: on the electron app).
+   */
+  requireModule: function(requireBasePath: string, requirePath: string): ?any {
+    // On Electron, where modules can be required at runtime from files, require the
+    // file, relative to the base path.
+    if (path) {
+      const module = optionalRequire(path.join(requireBasePath, requirePath));
+      if (module) return module;
+    }
+
+    // On the web-app, modules need to be registered manually.
+    if (requirableModules[requirePath]) return requirableModules[requirePath];
+
+    console.error(
+      `Unable to find module "${requirePath}". Are you sure you registered it using ObjectsRenderingService.registerModule? This is mandatory for the web-app to have the file bundled.`
+    );
+    return null;
   },
   gd, // Expose gd so that it can be used by renderers
   PIXI, // Expose PIXI so that it can be used by renderers

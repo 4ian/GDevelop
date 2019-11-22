2edb2f8f80b164ad840655b7c84a5d000d0ecf94
blurymind <blurymind@gmail.com>
2019-11-22 14:51:55 +0000

    Remember debuger state, so we dont have to open it every time

--- newIDE/app/src/Export/LocalExporters/LocalPreviewLauncher/index.js
+++ newIDE/app/src/Export/LocalExporters/LocalPreviewLauncher/index.js
@@ -28,6 +28,7 @@ type State = {
   networkPreviewPort: ?number,
   networkPreviewError: ?any,
   previewGamePath: ?string,
+  debugToolsOpen: boolean,
   previewBrowserWindowConfig: ?{
     width: number,
     height: number,
@@ -49,6 +50,7 @@ export default class LocalPreviewLauncher extends React.Component<
     networkPreviewPort: null,
     networkPreviewError: null,
     previewGamePath: null,
+    debugToolsOpen: true,
     previewBrowserWindowConfig: null,
   };
   _subscriptionChecker: ?SubscriptionChecker = null;
@@ -63,6 +65,13 @@ export default class LocalPreviewLauncher extends React.Component<
 
     const win = new BrowserWindow(this.state.previewBrowserWindowConfig);
     win.loadURL(`file://${this.state.previewGamePath}/index.html`);
+    win.webContents.on('devtools-opened', () => {
+      this.setState({ debugToolsOpen: true });
+    });
+    win.webContents.on('devtools-closed', () => {
+      this.setState({ debugToolsOpen: false });
+    });
+    if (this.state.debugToolsOpen) win.openDevTools();
   };
 
   _openPreviewWindow = (

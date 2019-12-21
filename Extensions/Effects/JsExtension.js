935ebad37869f3c15f2a602ace286bf07bdd4d9b
Bouh <bouh.vivez@gmail.com>
2019-12-21 22:37:46 +0100

    Prettier

--- Extensions/Effects/JsExtension.js
+++ Extensions/Effects/JsExtension.js
@@ -14,11 +14,11 @@ module.exports = {
   createExtension: function(_, gd) {
     const extension = new gd.PlatformExtension();
     extension.setExtensionInformation(
-      'Effects',
-      'Effects',
-      'Contains various effects to be used in games.',
-      'Various contributors from PixiJS, PixiJS filters and GDevelop',
-      'MIT'
+      "Effects",
+      "Effects",
+      "Contains various effects to be used in games.",
+      "Various contributors from PixiJS, PixiJS filters and GDevelop",
+      "MIT"
     );
 
     // ℹ️ You can declare an effect here. Please order the effects by alphabetical order.
@@ -27,125 +27,125 @@ module.exports = {
     // and remove all the files and declaration of effects, or take a look at ExampleJsExtension).
 
     const blackAndWhiteEffect = extension
-      .addEffect('BlackAndWhite')
-      .setFullName(_('Black and White'))
-      .setDescription(_('Alter the colors to make the image black and white'))
-      .addIncludeFile('Extensions/Effects/black-and-white-pixi-filter.js');
+      .addEffect("BlackAndWhite")
+      .setFullName(_("Black and White"))
+      .setDescription(_("Alter the colors to make the image black and white"))
+      .addIncludeFile("Extensions/Effects/black-and-white-pixi-filter.js");
     const blackAndWhiteProperties = blackAndWhiteEffect.getProperties();
     blackAndWhiteProperties.set(
-      'opacity',
-      new gd.PropertyDescriptor(/* defaultValue= */ '1')
-        .setLabel(_('Opacity (between 0 and 1)'))
-        .setType('number')
+      "opacity",
+      new gd.PropertyDescriptor(/* defaultValue= */ "1")
+        .setLabel(_("Opacity (between 0 and 1)"))
+        .setType("number")
     );
 
     const blurEffect = extension
-      .addEffect('Blur')
-      .setFullName(_('Blur'))
-      .setDescription(_('Blur the rendered image'))
-      .addIncludeFile('Extensions/Effects/blur-pixi-filter.js');
+      .addEffect("Blur")
+      .setFullName(_("Blur"))
+      .setDescription(_("Blur the rendered image"))
+      .addIncludeFile("Extensions/Effects/blur-pixi-filter.js");
     const blurProperties = blurEffect.getProperties();
     blurProperties.set(
-      'blur',
-      new gd.PropertyDescriptor(/* defaultValue= */ '8')
-        .setLabel(_('Blur intensity'))
-        .setType('number')
+      "blur",
+      new gd.PropertyDescriptor(/* defaultValue= */ "8")
+        .setLabel(_("Blur intensity"))
+        .setType("number")
     );
     blurProperties.set(
-      'quality',
-      new gd.PropertyDescriptor(/* defaultValue= */ '1')
+      "quality",
+      new gd.PropertyDescriptor(/* defaultValue= */ "1")
         .setLabel(
           _(
-            'Number of render passes. An high value will cause lags/poor performance.'
+            "Number of render passes. An high value will cause lags/poor performance."
           )
         )
-        .setType('number')
+        .setType("number")
     );
     blurProperties.set(
-      'resolution',
-      new gd.PropertyDescriptor(/* defaultValue= */ '2')
-        .setLabel(_('Resolution'))
-        .setType('number')
+      "resolution",
+      new gd.PropertyDescriptor(/* defaultValue= */ "2")
+        .setLabel(_("Resolution"))
+        .setType("number")
     );
     blurProperties.set(
-      'resolution',
-      new gd.PropertyDescriptor(/* defaultValue= */ '2')
-        .setLabel(_('Kernel size (one of these values: 5, 7, 9, 11, 13, 15)'))
-        .setType('number')
+      "resolution",
+      new gd.PropertyDescriptor(/* defaultValue= */ "2")
+        .setLabel(_("Kernel size (one of these values: 5, 7, 9, 11, 13, 15)"))
+        .setType("number")
     );
 
     const brightnessEffect = extension
-      .addEffect('Brightness')
-      .setFullName(_('Brightness'))
-      .setDescription(_('Make the image brighter'))
-      .addIncludeFile('Extensions/Effects/brightness-pixi-filter.js');
+      .addEffect("Brightness")
+      .setFullName(_("Brightness"))
+      .setDescription(_("Make the image brighter"))
+      .addIncludeFile("Extensions/Effects/brightness-pixi-filter.js");
     const brightnessProperties = brightnessEffect.getProperties();
     brightnessProperties.set(
-      'brightness',
-      new gd.PropertyDescriptor(/* defaultValue= */ '0.8')
-        .setLabel(_('Brightness (between 0 and 1)'))
-        .setType('number')
+      "brightness",
+      new gd.PropertyDescriptor(/* defaultValue= */ "0.8")
+        .setLabel(_("Brightness (between 0 and 1)"))
+        .setType("number")
     );
 
     const lightNightEffect = extension
-      .addEffect('LightNight')
-      .setFullName(_('Light Night'))
-      .setDescription(_('Alter the colors to simulate night'))
-      .addIncludeFile('Extensions/Effects/light-night-pixi-filter.js');
+      .addEffect("LightNight")
+      .setFullName(_("Light Night"))
+      .setDescription(_("Alter the colors to simulate night"))
+      .addIncludeFile("Extensions/Effects/light-night-pixi-filter.js");
     const lightNightProperties = lightNightEffect.getProperties();
     lightNightProperties.set(
-      'opacity',
-      new gd.PropertyDescriptor(/* defaultValue= */ '1')
-        .setLabel(_('Opacity (between 0 and 1)'))
-        .setType('number')
+      "opacity",
+      new gd.PropertyDescriptor(/* defaultValue= */ "1")
+        .setLabel(_("Opacity (between 0 and 1)"))
+        .setType("number")
     );
 
     const nightEffect = extension
-      .addEffect('Night')
-      .setFullName(_('Dark Night'))
-      .setDescription(_('Alter the colors to simulate a dark night'))
-      .addIncludeFile('Extensions/Effects/night-pixi-filter.js');
+      .addEffect("Night")
+      .setFullName(_("Dark Night"))
+      .setDescription(_("Alter the colors to simulate a dark night"))
+      .addIncludeFile("Extensions/Effects/night-pixi-filter.js");
     const nightProperties = nightEffect.getProperties();
     nightProperties.set(
-      'intensity',
-      new gd.PropertyDescriptor(/* defaultValue= */ '0.5')
-        .setLabel(_('Intensity (between 0 and 1)'))
-        .setType('number')
+      "intensity",
+      new gd.PropertyDescriptor(/* defaultValue= */ "0.5")
+        .setLabel(_("Intensity (between 0 and 1)"))
+        .setType("number")
     );
     nightProperties.set(
-      'opacity',
-      new gd.PropertyDescriptor(/* defaultValue= */ '0.5')
-        .setLabel(_('Opacity (between 0 and 1)'))
-        .setType('number')
+      "opacity",
+      new gd.PropertyDescriptor(/* defaultValue= */ "0.5")
+        .setLabel(_("Opacity (between 0 and 1)"))
+        .setType("number")
     );
 
     const noiseEffect = extension
-      .addEffect('Noise')
-      .setFullName(_('Noise'))
-      .setDescription(_('Add some noise on the rendered image'))
-      .addIncludeFile('Extensions/Effects/noise-pixi-filter.js');
+      .addEffect("Noise")
+      .setFullName(_("Noise"))
+      .setDescription(_("Add some noise on the rendered image"))
+      .addIncludeFile("Extensions/Effects/noise-pixi-filter.js");
     const noiseProperties = noiseEffect.getProperties();
     noiseProperties.set(
-      'noise',
-      new gd.PropertyDescriptor(/* defaultValue= */ '0.5')
-        .setLabel(_('Noise intensity (between 0 and 1)'))
-        .setType('number')
+      "noise",
+      new gd.PropertyDescriptor(/* defaultValue= */ "0.5")
+        .setLabel(_("Noise intensity (between 0 and 1)"))
+        .setType("number")
     );
 
     const sepiaEffect = extension
-      .addEffect('Sepia')
-      .setFullName(_('Sepia'))
-      .setDescription(_('Alter the colors to sepia'))
-      .addIncludeFile('Extensions/Effects/sepia-pixi-filter.js');
+      .addEffect("Sepia")
+      .setFullName(_("Sepia"))
+      .setDescription(_("Alter the colors to sepia"))
+      .addIncludeFile("Extensions/Effects/sepia-pixi-filter.js");
     const sepiaProperties = sepiaEffect.getProperties();
     sepiaProperties.set(
-      'opacity',
-      new gd.PropertyDescriptor(/* defaultValue= */ '1')
-        .setLabel(_('Opacity (between 0 and 1)'))
-        .setType('number')
+      "opacity",
+      new gd.PropertyDescriptor(/* defaultValue= */ "1")
+        .setLabel(_("Opacity (between 0 and 1)"))
+        .setType("number")
     );
 
-     const displacementEffect = extension
+    const displacementEffect = extension
       .addEffect("Displacement")
       .setFullName(_("Displacement"))
       .setDescription(_("Add Displacement effect"))
@@ -155,7 +155,7 @@ module.exports = {
       "displacementMapImage",
       new gd.PropertyDescriptor("")
         .setType("resource")
-        .addExtraInfo('image')
+        .addExtraInfo("image")
         .setLabel(_("Displacement map image for the effect"))
     );
     displacementProperties.set(
@@ -173,5 +173,5 @@ module.exports = {
   },
   runExtensionSanityTests: function(gd, extension) {
     return [];
-  },
+  }
 };

d5b25987aa2e896c5e8eb33c51bd541c8bc72e4f
Florian Rival <Florian.rival@gmail.com>
2019-12-26 19:40:14 +0100

    Update crt-pixi-filter.js

--- Extensions/Effects/crt-pixi-filter.js
+++ Extensions/Effects/crt-pixi-filter.js
@@ -1,43 +1,34 @@
 gdjs.PixiFiltersTools.registerFilterCreator('Crt', {
   makePIXIFilter: function(layer, effectData) {
-    var crtFilter = new PIXI.filters.CRTFilter();
+    var crtFilter = new PIXI.filters.CRTFilter({ seed: Math.random() });
 
     return crtFilter;
   },
   update: function(filter, layer) {
-     if (filter.animated) {
+    if (filter.animated) {
       filter.time += layer.getElapsedTime() / 1000;
-      filter.seed = Math.random();
     }
   },
   updateDoubleParameter: function(filter, parameterName, value) {
     if (parameterName === 'lineWidth') {
       filter.lineWidth = value;
-    }
-    else if (parameterName === 'lineContrast') {
+    } else if (parameterName === 'lineContrast') {
       filter.lineContrast = value;
-    }
-    else if (parameterName === 'noise') {
+    } else if (parameterName === 'noise') {
       filter.noise = value;
-    }
-    else if (parameterName === 'curvature') {
+    } else if (parameterName === 'curvature') {
       filter.curvature = value;
-    }
-    else if (parameterName === 'noiseSize') {
+    } else if (parameterName === 'noiseSize') {
       filter.noiseSize = value;
-    }
-    else if (parameterName === 'vignetting') {
+    } else if (parameterName === 'vignetting') {
       filter.vignetting = value;
-    }
-    else if (parameterName === 'vignettingAlpha') {
+    } else if (parameterName === 'vignettingAlpha') {
       filter.vignettingAlpha = value;
-    }
-    else if (parameterName === 'vignettingBlur') {
+    } else if (parameterName === 'vignettingBlur') {
       filter.vignettingBlur = value;
     }
   },
-  updateStringParameter: function(filter, parameterName, value) {
-  },
+  updateStringParameter: function(filter, parameterName, value) {},
   updateBooleanParameter: function(filter, parameterName, value) {
     if (parameterName === 'verticalLine') {
       filter.verticalLine = value;

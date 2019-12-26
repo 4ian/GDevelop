6857f285cb9ae4c565c4a4ebfa1cf54d87a5c3bf
Florian Rival <Florian.rival@gmail.com>
2019-12-26 19:41:20 +0100

    Fix godray

--- Extensions/Effects/godray-pixi-filter.js
+++ Extensions/Effects/godray-pixi-filter.js
@@ -5,32 +5,26 @@ gdjs.PixiFiltersTools.registerFilterCreator('Godray', {
     return godrayFilter;
   },
   update: function(filter, layer) {
-     if (filter.animated) {
+    if (filter.animated) {
       filter.time += layer.getElapsedTime() / 1000;
     }
   },
   updateDoubleParameter: function(filter, parameterName, value) {
-    else if (parameterName === 'lacunarity') {
+    if (parameterName === 'lacunarity') {
       filter.lacunarity = value;
-    }
-    else if (parameterName === 'angle') {
+    } else if (parameterName === 'angle') {
       filter.angle = value;
-    }
-    else if (parameterName === 'gain') {
+    } else if (parameterName === 'gain') {
       filter.gain = value;
-    }
-    else if (parameterName === 'light') {
+    } else if (parameterName === 'light') {
       filter.light = value;
-    }
-    else if (parameterName === 'x') {
+    } else if (parameterName === 'x') {
       filter.x = value;
-    }
-    else if (parameterName === 'y') {
+    } else if (parameterName === 'y') {
       filter.y = value;
     }
   },
-  updateStringParameter: function(filter, parameterName, value) {
-  },
+  updateStringParameter: function(filter, parameterName, value) {},
   updateBooleanParameter: function(filter, parameterName, value) {
     if (parameterName === 'parallel') {
       filter.parallel = value;

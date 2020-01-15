44dbbd713881b6a3d1b938197d8bb10396d52492
Florian Rival <Florian.Rival@gmail.com>
2020-01-15 23:14:33 +0000

    Add notarization for macOS

---
+++ newIDE/electron-app/scripts/electron-builder-after-sign.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appleId = process.env.APPLEID;
+  const appleIdPassword = process.env.APPLEIDPASS;
+  const appBundleId = 'com.gdevelop-app.ide';
+  const appName = context.packager.appInfo.productFilename;
+  const appPath = path.join(appOutDir, `${appName}.app`);
+
+  // Bail out if credentials not present
+  if (!appleId) {
+    console.info(`APPLEID is not defined, skipping macOS notarization...`);
+    return;
+  }
+
+  // Sanity checks
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
+  if (!appleIdPassword) {
+    throw new Error(`APPLEIDPASS environment variable is not defined`);
+  }
+
+  // Launch notarization
+  console.info(
+    `Notarizing ${appBundleId} found at ${appPath} for Apple ID=${appleId}...`
+  );
+
+  try {
+    await notarize({
+      appBundleId,
+      appPath,
+      appleId,
+      appleIdPassword,
+    });
+
+    console.info(`Done notarizing ${appBundleId}.`);
+  } catch (error) {
+    console.error(`Error during notarization: `, error);
+    throw new Error(`Error during notarization: ${error}`);
+  }
+};

e4af64b69da5478b09f435aa22cef0363650d854
Florian Rival <Florian.rival@gmail.com>
2020-02-10 20:03:29 +0000

    Update Hashtable comments

--- GDJS/Runtime/libs/jshashtable.js
+++ GDJS/Runtime/libs/jshashtable.js
@@ -1,6 +1,6 @@
 // @ts-check
 /**
- * A generic map (key values) container.
+ * A generic map (key-value) container.
  *
  * Mostly used for storing lists of objects for
  * GDevelop generated events.
@@ -9,13 +9,19 @@
  */
 function Hashtable()
 {
-    /** @type {Object} */
+    /**
+     * The content of the Hashtable. Prefer using methods rather
+     * than accessing this internal object, unless you need to iterate
+     * on the values.
+     * @type {Object.<string, any>}
+     */
     this.items = {};
 }
 
 /**
  * Construct a Hashtable from a JS object.
- * @param {Object} items The content of the Hashtable.
+ *
+ * @param {Object.<string, any>} items The content of the Hashtable.
  * @returns {Hashtable} The new hashtable.
  * @static
  */
@@ -27,37 +33,42 @@ Hashtable.newFrom = function(items) {
 
 /**
  * Add a key-value pair to the Hashtable.
+ * If a value already exists for this key, it is overwritten.
+ *
  * @memberof Hashtable
- * @param key The key for the value.
- * @param value The value for the key.
+ * @param {string} key The key.
+ * @param {any} value The value to associate to the key.
  */
 Hashtable.prototype.put = function(key, value) {
     this.items[key] = value;
 }
 
 /**
- * Get a value corresponding to a key.
+ * Get a value corresponding to a key, or undefined if not found.
+ *
  * @memberof Hashtable
- * @param key The key for the value.
+ * @param {string} key The key associated to the value.
  */
 Hashtable.prototype.get = function(key) {
     return this.items[key];
 }
 
 /**
- * Verify if a key exists.
+ * Verify if a key exists in the Hashtable.
+ *
  * @memberof Hashtable
- * @param key A key.
- * @returns {boolean} Does the key exists?
+ * @param {string} key The key to search in the Hashtable.
+ * @returns {boolean} true if the key exists.
  */
 Hashtable.prototype.containsKey = function(key) {
     return this.items.hasOwnProperty(key);
 }
 
 /**
- * Remove the key-value pair with the key passed.
+ * Remove the value associated to the specified key.
+ *
  * @memberof Hashtable
- * @param key The key for the value.
+ * @param {string} key The key to remove.
  */
 Hashtable.prototype.remove = function(key) {
     delete this.items[key];
@@ -65,8 +76,9 @@ Hashtable.prototype.remove = function(key) {
 
 /**
  * Get the first key of the Hashtable.
+ *
  * @memberof Hashtable
- * @returns The first key of the Hashtable.
+ * @returns {?string} The first key of the Hashtable, or undefined if empty.
  */
 Hashtable.prototype.firstKey = function() {
     for (var k in this.items) {
@@ -79,9 +91,10 @@ Hashtable.prototype.firstKey = function() {
 }
 
 /**
- * Push all the keys of the Hashtable to an Array. Warning: Will clear the Array.
+ * Dump all the keys of the Hashtable to an array (which is cleared first).
+ *
  * @memberof Hashtable
- * @param {Array} result The Array where the result gets pushed.
+ * @param {Array<string>} result The Array where the result gets pushed.
  */
 Hashtable.prototype.keys = function(result) {
     result.length = 0;
@@ -93,9 +106,10 @@ Hashtable.prototype.keys = function(result) {
 }
 
 /**
- * Push all the values of the Hashtable to an Array. Warning: Will clear the Array.
+ * Dump all the values of the Hashtable to an array (which is cleared first).
+ *
  * @memberof Hashtable
- * @param {Array} result The Array where the result gets pushed.
+ * @param {Array<any>} result The Array where the results get pushed.
  */
 Hashtable.prototype.values = function(result) {
     result.length = 0;
@@ -108,6 +122,7 @@ Hashtable.prototype.values = function(result) {
 
 /**
  * Clear the Hashtable.
+ *
  * @memberof Hashtable
  */
 Hashtable.prototype.clear = function() {

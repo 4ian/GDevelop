0fa598899528e8d029b396ace9ec86647f26480f
Florian Rival <Florian.Rival@gmail.com>
2020-02-10 23:28:54 +0000

    Fix broken scripts since update to Emscripten 1.39.6

--- newIDE/app/scripts/update-fixtures-from-resources-examples.js
+++ newIDE/app/scripts/update-fixtures-from-resources-examples.js
@@ -3,7 +3,7 @@
  * from the examples in resources/examples. All resource paths are updated to be URLs,
  * using the same base URL (specified below in the script).
  */
-const gd = require('../public/libGD.js')();
+const initializeGDevelopJs = require('../public/libGD.js');
 const {
   readProjectFile,
   loadSerializedProject,
@@ -16,32 +16,16 @@ const fs = require('fs');
 // The base URL where all resources of web-app examples are stored.
 const baseUrl = 'https://resources.gdevelop-app.com/examples';
 
-const updateResources = (project, baseUrl) => {
-  const worker = new gd.ArbitraryResourceWorkerJS();
-  worker.exposeImage = file => {
-    // Don't do anything
-    return file;
-  };
-  worker.exposeShader = shader => {
-    // Don't do anything
-    return shader;
-  };
-  worker.exposeFile = file => {
-    if (file.length === 0) return '';
-
-    console.log('Updating resource: ', file);
-    return baseUrl + '/' + file;
-  };
-
-  project.exposeResources(worker);
-};
-
 const writeInternalExampleFilesJsFile = exampleNames => {
   let importsCode = [];
   let internalFilesObjectCode = [];
   exampleNames.forEach((exampleName, index) => {
-    importsCode.push(`import exampleFile${index} from '../../fixtures/${exampleName}/${exampleName}.json';`);
-    internalFilesObjectCode.push(`  'example://${exampleName}': exampleFile${index},`);
+    importsCode.push(
+      `import exampleFile${index} from '../../fixtures/${exampleName}/${exampleName}.json';`
+    );
+    internalFilesObjectCode.push(
+      `  'example://${exampleName}': exampleFile${index},`
+    );
   });
 
   const content = [
@@ -73,54 +57,76 @@ const writeInternalExampleFilesJsFile = exampleNames => {
   });
 };
 
-const noopTranslationFunction = str => str;
-const extensionsLoader = makeExtensionsLoader({ gd, filterExamples: false });
-extensionsLoader
-  .loadAllExtensions(noopTranslationFunction)
-  .then(loadingResults => {
-    console.info('Loaded extensions', loadingResults);
+initializeGDevelopJs().then(gd => {
+  const updateResources = (project, baseUrl) => {
+    const worker = new gd.ArbitraryResourceWorkerJS();
+    worker.exposeImage = file => {
+      // Don't do anything
+      return file;
+    };
+    worker.exposeShader = shader => {
+      // Don't do anything
+      return shader;
+    };
+    worker.exposeFile = file => {
+      if (file.length === 0) return '';
 
-    return getExampleNames();
-  })
-  .then(exampleNames =>
-    Promise.all(
-      exampleNames.map(exampleName => {
-        return readProjectFile(
-          `../resources/examples/${exampleName}/${exampleName}.json`
-        )
-          .then(projectObject => {
-            console.log(`Example "${exampleName}" loaded.`);
-            const project = loadSerializedProject(gd, projectObject);
-            updateResources(project, baseUrl + '/' + exampleName);
+      console.log('Updating resource: ', file);
+      return baseUrl + '/' + file;
+    };
 
-            return new Promise(resolve => {
-              fs.mkdir(`../src/fixtures/${exampleName}`, () => {
-                writeProjectJSONFile(
-                  gd,
-                  project,
-                  `../src/fixtures/${exampleName}/${exampleName}.json`
-                );
+    project.exposeResources(worker);
+  };
 
-                console.log(`Update of "${exampleName}" done.`);
-                resolve();
+  const noopTranslationFunction = str => str;
+
+  makeExtensionsLoader({ gd, filterExamples: false })
+    .loadAllExtensions(noopTranslationFunction)
+    .then(loadingResults => {
+      console.info('Loaded extensions', loadingResults);
+
+      return getExampleNames();
+    })
+    .then(exampleNames =>
+      Promise.all(
+        exampleNames.map(exampleName => {
+          return readProjectFile(
+            `../resources/examples/${exampleName}/${exampleName}.json`
+          )
+            .then(projectObject => {
+              console.log(`Example "${exampleName}" loaded.`);
+              const project = loadSerializedProject(gd, projectObject);
+              updateResources(project, baseUrl + '/' + exampleName);
+
+              return new Promise(resolve => {
+                fs.mkdir(`../src/fixtures/${exampleName}`, () => {
+                  writeProjectJSONFile(
+                    gd,
+                    project,
+                    `../src/fixtures/${exampleName}/${exampleName}.json`
+                  );
+
+                  console.log(`Update of "${exampleName}" done.`);
+                  resolve();
+                });
               });
+            })
+            .catch(error => {
+              console.error(`❌ Error caught for ${exampleName}:`, error);
             });
-          })
-          .catch(error => {
-            console.error(`❌ Error caught for ${exampleName}:`, error);
-          });
-      })
-    )
-      .then(() => writeInternalExampleFilesJsFile(exampleNames))
-      .then(
-        () => {
-          console.error(`✅ InternalExampleFiles.js written.`);
-        },
-        error => {
-          console.error(
-            `❌ Error caught while writing InternalExampleFiles.js:`,
-            error
-          );
-        }
+        })
       )
-  );
+        .then(() => writeInternalExampleFilesJsFile(exampleNames))
+        .then(
+          () => {
+            console.error(`✅ InternalExampleFiles.js written.`);
+          },
+          error => {
+            console.error(
+              `❌ Error caught while writing InternalExampleFiles.js:`,
+              error
+            );
+          }
+        )
+    );
+});

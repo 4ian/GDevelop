b2aab6b0350d8a4c1894184b29083d0c016c1fb1
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2020-02-10 16:56:24 +0100

    Add Complete JSDoc to Hashtable

--- GDJS/Runtime/libs/jshashtable.js
+++ GDJS/Runtime/libs/jshashtable.js
@@ -4,17 +4,19 @@
  * Mostly used for storing lists of objects for
  * GDevelop generated events.
  *
- * @class Hashtable
+ * @constructor
+ * @template K,V
  */
 function Hashtable()
 {
+    /** @type {Object<K,V>} */
     this.items = {};
 }
 
 /**
  * Construct a Hashtable from a JS object.
- * @param {Object} items The content of the Hashtable
- * @returns {Hashtable} The new hashtable
+ * @param {Object} items The content of the Hashtable.
+ * @returns {Hashtable} The new hashtable.
  * @static
  */
 Hashtable.newFrom = function(items) {
@@ -23,22 +25,49 @@ Hashtable.newFrom = function(items) {
     return hashtable;
 }
 
+/**
+ * Add a key-value pair to the Hashtable.
+ * @memberof Hashtable
+ * @param {K} key The key for the value.
+ * @param {V} value The value for the key.
+ */
 Hashtable.prototype.put = function(key, value) {
     this.items[key] = value;
 }
 
+/**
+ * Get a value corresponding to a key.
+ * @memberof Hashtable
+ * @param {K} key The key for the value.
+ */
 Hashtable.prototype.get = function(key) {
     return this.items[key];
 }
 
+/**
+ * Verify if a key exists.
+ * @memberof Hashtable
+ * @param {K} key A key.
+ * @returns {boolean} Does the key exists?
+ */
 Hashtable.prototype.containsKey = function(key) {
     return this.items.hasOwnProperty(key);
 }
 
+/**
+ * Remove the key-value pair with the key passed.
+ * @memberof Hashtable
+ * @param {K} key The key for the value.
+ */
 Hashtable.prototype.remove = function(key) {
     delete this.items[key];
 }
 
+/**
+ * Get the first key of the Hashtable.
+ * @memberof Hashtable
+ * @returns {K} The first key of the Hashtable.
+ */
 Hashtable.prototype.firstKey = function() {
     for (var k in this.items) {
         if (this.items.hasOwnProperty(k)) {
@@ -49,6 +78,11 @@ Hashtable.prototype.firstKey = function() {
     return undefined;
 }
 
+/**
+ * Push all the keys of the Hashtable to an Array. Warning: Will clear the Array.
+ * @memberof Hashtable
+ * @param {Array} result The Array where the result gets pushed.
+ */
 Hashtable.prototype.keys = function(result) {
     result.length = 0;
     for (var k in this.items) {
@@ -58,6 +92,11 @@ Hashtable.prototype.keys = function(result) {
     }
 }
 
+/**
+ * Push all the values of the Hashtable to an Array. Warning: Will clear the Array.
+ * @memberof Hashtable
+ * @param {Array} result The Array where the result gets pushed.
+ */
 Hashtable.prototype.values = function(result) {
     result.length = 0;
     for (var k in this.items) {
@@ -67,6 +106,10 @@ Hashtable.prototype.values = function(result) {
     }
 }
 
+/**
+ * Clear the Hashtable.
+ * @memberof Hashtable
+ */
 Hashtable.prototype.clear = function() {
     for (var k in this.items) {
         if (this.items.hasOwnProperty(k)) {

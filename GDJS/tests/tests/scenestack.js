e95a336dd2a561cc1608d3920dcfc6577b3155c9
Florian Rival <Florian.Rival@gmail.com>
2020-02-12 19:30:30 +0000

    Add new GDJS callbacks: first scene loaded, pre/post events

--- GDJS/tests/tests/scenestack.js
+++ GDJS/tests/tests/scenestack.js
@@ -24,15 +24,20 @@ describe('gdjs.SceneStack', function() {
       },
     ],
   });
-  var sceneStack = new gdjs.SceneStack(runtimeGame);
+  // @ts-ignore - access to a private member for testing purposes.
+  var sceneStack = runtimeGame._sceneStack;
 
   it('should support pushing, replacing and popping scenes', function() {
     // Set up some global callbacks
+    var firstLoadedScene = null;
     var lastLoadedScene = null;
     var lastUnloadedScene = null;
     var lastPausedScene = null;
     var lastResumedScene = null;
 
+    gdjs.registerFirstRuntimeSceneLoadedCallback(function(runtimeScene) {
+      firstLoadedScene = runtimeScene;
+    });
     gdjs.registerRuntimeSceneLoadedCallback(function(runtimeScene) {
       lastLoadedScene = runtimeScene;
     });
@@ -48,44 +53,58 @@ describe('gdjs.SceneStack', function() {
 
     // Test the stack
     expect(sceneStack.pop()).to.be(null);
+    expect(lastLoadedScene).to.be(null);
+    expect(firstLoadedScene).to.be(null);
+    expect(sceneStack.wasFirstSceneLoaded()).to.be(false);
 
     var scene1 = sceneStack.push('Scene 1');
     expect(lastLoadedScene).to.be(scene1);
+    expect(firstLoadedScene).to.be(scene1);
+    expect(sceneStack.wasFirstSceneLoaded()).to.be(true);
 
     var scene2 = sceneStack.push('Scene 2');
     expect(lastPausedScene).to.be(scene1);
     expect(lastLoadedScene).to.be(scene2);
+    expect(firstLoadedScene).to.be(scene1); // Not changed
 
     var scene3 = sceneStack.push('Scene 1');
     expect(lastPausedScene).to.be(scene2);
     expect(lastLoadedScene).to.be(scene3);
+    expect(firstLoadedScene).to.be(scene1); // Not changed
 
     var scene4 = sceneStack.replace('Scene 1');
     expect(lastPausedScene).to.be(scene2); // Not changed
     expect(lastUnloadedScene).to.be(scene3);
     expect(lastLoadedScene).to.be(scene4);
+    expect(firstLoadedScene).to.be(scene1); // Not changed
 
     expect(sceneStack.pop()).to.be(scene4);
     expect(lastUnloadedScene).to.be(scene4);
     expect(lastResumedScene).to.be(scene2);
     expect(lastPausedScene).to.be(scene2); // Not changed
+    expect(firstLoadedScene).to.be(scene1); // Not changed
 
     expect(sceneStack.pop()).to.be(scene2);
     expect(lastUnloadedScene).to.be(scene2);
     expect(lastResumedScene).to.be(scene1);
     expect(lastPausedScene).to.be(scene2); // Not changed
+    expect(firstLoadedScene).to.be(scene1); // Not changed
 
     var scene5 = sceneStack.replace('Scene 2', true);
     expect(lastLoadedScene).to.be(scene5);
     expect(lastUnloadedScene).to.be(scene1);
     expect(lastPausedScene).to.be(scene2); // Not changed
     expect(lastResumedScene).to.be(scene1); // Not changed
+    expect(firstLoadedScene).to.be(scene1); // Not changed
 
     expect(sceneStack.pop()).to.be(null);
     expect(lastLoadedScene).to.be(scene5); // Not changed
     expect(lastUnloadedScene).to.be(scene1); // Not changed
     expect(lastPausedScene).to.be(scene2); // Not changed
     expect(lastResumedScene).to.be(scene1); // Not changed
+    expect(firstLoadedScene).to.be(scene1); // Not changed
+
+    expect(sceneStack.wasFirstSceneLoaded()).to.be(true);
 
     // Remove all the global callbacks
     gdjs.clearGlobalCallbacks();

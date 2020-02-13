c0520ed668803e0fba9428d360ebaa8c603bcbbc
Bouh <bouh.vivez@gmail.com>
2020-02-14 00:56:50 +0100

    Improve support of contextMenu
    Now, when buildMenuTemplate is pass, it has the priority on simple onClick (onClick is ignored)
    And buildMenuTemplateRight is added for contextMenu on right click.

--- newIDE/app/src/UI/Menu/ElementWithMenu.js
+++ newIDE/app/src/UI/Menu/ElementWithMenu.js
@@ -5,7 +5,9 @@ import ContextMenu from './ContextMenu';
 
 type Props = {|
   element: React$Element<any>,
+  onClick?: () => void,
   buildMenuTemplate: () => Array<any>,
+  buildMenuTemplateRight: () => Array<any>,
 |};
 
 type State = {||};
@@ -13,37 +15,67 @@ type State = {||};
 /**
  * Wrap an element and display a menu when `onClick` prop is called on the element.
  */
+
 export default class ElementWithMenu extends React.Component<Props, State> {
   _contextMenu: ?ContextMenu;
   _wrappedElement: ?any;
 
-  open = (event: any) => {
-    const { _contextMenu } = this;
-    if (!_contextMenu) return;
+  open = (element: any) => {
+    if (!this.props.buildMenuTemplate) {
+      this.props.element.props.onClick();
+    } else {
+      const { _contextMenu } = this;
+      if (!_contextMenu) return;
+
+      const node = ReactDOM.findDOMNode(this._wrappedElement);
+      if (node instanceof HTMLElement) {
+        const dimensions = node.getBoundingClientRect();
+
+        _contextMenu.open(
+          Math.round(dimensions.left + dimensions.width / 2),
+          Math.round(dimensions.top + dimensions.height)
+        );
+      }
+    }
+  };
+
+  openRight = () => {
+    const { _contextMenuR } = this;
+    if (!_contextMenuR) return;
 
     const node = ReactDOM.findDOMNode(this._wrappedElement);
     if (node instanceof HTMLElement) {
       const dimensions = node.getBoundingClientRect();
 
-      _contextMenu.open(
-        Math.round(dimensions.left),
+      _contextMenuR.open(
+        Math.round(dimensions.left + dimensions.width / 2),
         Math.round(dimensions.top + dimensions.height)
       );
     }
   };
 
   render() {
-    const { element, buildMenuTemplate } = this.props;
+    const { element, buildMenuTemplate, buildMenuTemplateRight } = this.props;
+
     return (
       <React.Fragment>
         {React.cloneElement(element, {
           onClick: this.open,
+          onContextMenu: this.openRight,
           ref: wrappedElement => (this._wrappedElement = wrappedElement),
         })}
-        <ContextMenu
-          ref={contextMenu => (this._contextMenu = contextMenu)}
-          buildMenuTemplate={buildMenuTemplate}
-        />
+        {buildMenuTemplateRight && (
+          <ContextMenu
+            ref={contextMenuR => (this._contextMenuR = contextMenuR)}
+            buildMenuTemplate={buildMenuTemplateRight}
+          />
+        )}
+        {buildMenuTemplate && (
+          <ContextMenu
+            ref={contextMenu => (this._contextMenu = contextMenu)}
+            buildMenuTemplate={buildMenuTemplate}
+          />
+        )}
       </React.Fragment>
     );
   }

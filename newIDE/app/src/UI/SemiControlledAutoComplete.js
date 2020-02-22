6c667ab88a4e2a3bc48f03751b34219a24851962
HarsimranVirk <harsimranvirk99@gmail.com>
2020-02-23 01:04:31 +0530

    Added functinality to render Icons

--- newIDE/app/src/UI/SemiControlledAutoComplete.js
+++ newIDE/app/src/UI/SemiControlledAutoComplete.js
@@ -11,8 +11,7 @@ import { computeTextFieldStyleProps } from './TextField';
 import { MarkdownText } from './MarkdownText';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from '@material-ui/core/styles';
-
-
+import MenuItem from '@material-ui/core/MenuItem';
 
 export type DataSource = Array<
   | {|
@@ -26,21 +25,22 @@ export type DataSource = Array<
     |}
 >;
 
-const styles = makeStyles({
+// Styles for the TextField and input
+const useStyles = makeStyles({
+  root: {
+    flexWrap: 'wrap',
+    '& .MuiAutocomplete-inputRoot .MuiAutocomplete-input': {
+      flexGrow: 1,
+      width: 'auto',
+    },
+  },
+});
+
+const styles = {
   container: {
     position: 'relative',
   },
-  root: {
-    flexWrap: "wrap",
-    '& .MuiAutocomplete-inputRoot .MuiAutocomplete-input': {
-      flexGrow: 1,
-      width: "auto",
-    }
-  }
-});
-
-
-
+};
 
 type Props = {|
   value: string,
@@ -67,67 +67,110 @@ type Props = {|
 
 type State = {|
   inputValue: string | null,
-|}
+|};
 
+// filters the option list
 const filterFunction = (option, inputValue) => {
-  const lowercaseInputValue = inputValue.toLowerCase();
-  if(!option.text) return true;
-  if(option.type === 'separator') return true;
-  return option.text.toLowerCase().indexOf(lowercaseInputValue) !== -1;
-}
+  if (typeof inputValue !== 'undefined') {
+    const lowercaseInputValue = inputValue.toLowerCase();
+    if (!option.text) return true;
+    if (option.type === 'separator') return true;
+    return option.text.toLowerCase().indexOf(lowercaseInputValue) !== -1;
+  } else {
+    return false;
+  }
+};
 
+const renderMenu = option => {
+  if (option.type !== 'seperator') {
+    return (
+      <MenuItem>
+        {option.renderIcon && (
+          <ListItemIcon>{option.renderIcon()}</ListItemIcon>
+        )}
+        {option.value}
+      </MenuItem>
+    );
+  } else {
+    return <Divider />;
+  }
+};
 
 export default function SemiControlledAutoComplete(props) {
+  const [state, setState] = React.useState({ inputValue: props.value });
 
-    const [state, setState] = React.useState({ inputValue: null });
-  
-    const currentInputValue =
-          state.inputValue !== null ? state.inputValue : props.value;
-    
-    const helperText = props.helperMarkdownText ? (
-          <MarkdownText source={props.helperMarkdownText} />
-          ) : null;
+  const helperText = props.helperMarkdownText ? (
+    <MarkdownText source={props.helperMarkdownText} />
+  ) : null;
 
-    const classes = styles();
+  const classes = useStyles();
 
-    
-    return(
-          <div
-          style={{
-              ...styles.container,
-              flexGrow: props.fullWidth ? 1 : undefined,
-          }}>
-                  <Autocomplete
-                    freeSolo
-                    disableClearable
-                    options={props.dataSource
-                                  .filter(option => filterFunction(option, currentInputValue))
-                                  .map(option => option.value)}
-                    renderInput={params => (
-                      <TextField {...params}
-                        classes={{root: classes.root}}
-                        label={props.floatingLabelText}
-                        error={!!props.errorText}
-                        helperText={props.helperText || helperText || props.errorText}
-                        disabled={props.disabled}
-                        style={props.textFieldStyle}
+  // props.dataSource
+  // .filter(option => filterFunction(option, currentInputValue))
+  // .map(option => option.value)
 
-                        // Todo: Implement i18n for placeholder, couldn't find a workaround. 
-                        placeholder={props.hintText}
-                        variant="filled" />
-                      )}
-                    inputValue={currentInputValue}
-                    onInputChange={(event, inputValue, reason) => setState({inputValue})}
-                    onChange={(event, value) => {
-                        const data = props.dataSource
-                                          .filter(option => option.value === value);
-                        if(data.length !== 0){
-                          if(data[0].onClick){
-                          data[0].onClick();
-                        }                        
-                      }
-                      props.onChange(value)}}
-                  />
-            </div>
-    )
+  const handleChange = (event, value) => {
+    if (typeof value !== 'string') {
+      const data = props.dataSource.filter(
+        option => option.value === value.value
+      );
+      if (data.length !== 0) {
+        if (data[0].onClick) {
+          data[0].onClick();
+        }
+      }
+      props.onChange(value.value);
+    } else {
+      setState({ inputValue: value });
+      props.onChange(state.inputValue);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        ...styles.container,
+        flexGrow: props.fullWidth ? 1 : undefined,
+      }}
+    >
+      <Autocomplete
+        freeSolo
+        disableClearable
+        disableListWrap
+        options={props.dataSource.filter(option =>
+          filterFunction(option, state.inputValue)
+        )}
+        getOptionLabel={option => {
+          if (typeof option !== 'string') {
+            return option.value;
+          } else {
+            return '';
+          }
+        }}
+        renderOption={(option, state) => renderMenu(option)}
+        renderInput={params => (
+          <TextField
+            {...params}
+            classes={{ root: classes.root }}
+            label={props.floatingLabelText}
+            error={!!props.errorText}
+            helperText={props.helperText || helperText || props.errorText}
+            disabled={props.disabled}
+            style={props.textFieldStyle}
+            onChange={event => {
+              setState({ inputValue: event.target.value });
+            }}
+            // Todo: Implement i18n for placeholder, couldn't find a workaround.
+            placeholder={props.hintText}
+            variant="filled"
+          />
+        )}
+        inputValue={state.inputValue}
+        onInputChange={(event, inputValue, reason) => {
+          setState({ inputValue });
+        }}
+        onChange={(event, value) => handleChange(event, value)}
+      />
+    </div>
+  );
 }

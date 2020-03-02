e186d9c0e849acc467447b20ff0cdb4e34f14fbc
HarsimranVirk <harsimranvirk99@gmail.com>
2020-03-02 13:57:20 +0530

    replaced maxHeight with useMaxHeight

--- newIDE/app/src/UI/Grid.js
+++ newIDE/app/src/UI/Grid.js
@@ -36,7 +36,7 @@ export const Column = props => (
       alignItems: props.alignItems || 'stretch',
       justifyContent: props.justifyContent,
       flex: props.expand ? 1 : undefined,
-      useMaxHeight: props.useMaxHeight ? '100%' : undefined,
+      maxHeight: props.useMaxHeight ? '100%' : undefined,
     }}
   >
     {props.children}

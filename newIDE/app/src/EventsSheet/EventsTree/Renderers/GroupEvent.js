867ea6c8ce9b4fd844aded54f525c581263d3183
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-03 06:30:54 +0530

    Fix keybindings in GroupEvent

--- newIDE/app/src/EventsSheet/EventsTree/Renderers/GroupEvent.js
+++ newIDE/app/src/EventsSheet/EventsTree/Renderers/GroupEvent.js
@@ -1,41 +1,44 @@
 // @flow
-import { t } from '@lingui/macro';
+import { t } from "@lingui/macro";
 
-import * as React from 'react';
-import classNames from 'classnames';
-import TextField from '../../../UI/TextField';
+import * as React from "react";
+import classNames from "classnames";
+import TextField from "../../../UI/TextField";
 import {
   largeSelectedArea,
   largeSelectableArea,
   selectableArea,
-  disabledText,
-} from '../ClassNames';
-import { type EventRendererProps } from './EventRenderer';
+  disabledText
+} from "../ClassNames";
+import { type EventRendererProps } from "./EventRenderer";
+
 const gd = global.gd;
 
 const styles = {
   container: {
     height: 40,
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: 5,
-    overflow: 'hidden',
+    overflow: "hidden"
   },
   title: {
     fontSize: 18,
-  },
+    width: "100%"
+  }
 };
 
 export default class GroupEvent extends React.Component<EventRendererProps, *> {
   state = {
     editing: false,
+    prevValue: ""
   };
   _textField: ?TextField = null;
 
   edit = () => {
     this.setState(
       {
-        editing: true,
+        editing: true
       },
       () => {
         if (this._textField) this._textField.focus();
@@ -45,7 +48,7 @@ export default class GroupEvent extends React.Component<EventRendererProps, *> {
 
   endEditing = () => {
     this.setState({
-      editing: false,
+      editing: false
     });
   };
 
@@ -56,19 +59,24 @@ export default class GroupEvent extends React.Component<EventRendererProps, *> {
       g = groupEvent.getBackgroundColorG(),
       b = groupEvent.getBackgroundColorB();
 
-    const textColor = (r + g + b) / 3 > 200 ? 'black' : 'white';
+    const textColor = (r + g + b) / 3 > 200 ? "black" : "white";
 
     return (
       <div
         className={classNames({
           [largeSelectableArea]: true,
-          [largeSelectedArea]: this.props.selected,
+          [largeSelectedArea]: this.props.selected
         })}
         style={{
           ...styles.container,
-          backgroundColor: `rgb(${r}, ${g}, ${b})`,
+          backgroundColor: `rgb(${r}, ${g}, ${b})`
         }}
-        onClick={this.edit}
+        onClick={()=>{
+          this.setState({
+            prevValue: groupEvent.getName()
+          });
+          this.edit();
+        }}
       >
         {this.state.editing ? (
           <TextField
@@ -83,23 +91,34 @@ export default class GroupEvent extends React.Component<EventRendererProps, *> {
             style={styles.title}
             inputStyle={{
               color: textColor,
-              WebkitTextFillColor: textColor,
+              WebkitTextFillColor: textColor
             }}
             underlineFocusStyle={{
-              borderColor: textColor,
+              borderColor: textColor
             }}
             fullWidth
             id="group-title"
+            onKeyUp={(e) => {
+              if (e.key === "Escape") {
+                groupEvent.setName(this.state.prevValue);
+                this.endEditing();
+              }
+            }}
+            onKeyPress={(e) => {
+              if (e.charCode === 13 && !e.shiftKey) {
+                this.endEditing();
+              }
+            }}
           />
         ) : (
           <span
             className={classNames({
               [selectableArea]: true,
-              [disabledText]: this.props.disabled,
+              [disabledText]: this.props.disabled
             })}
             style={{ ...styles.title, color: textColor }}
           >
-            {groupEvent.getName() || '<Enter group name>'}
+            {groupEvent.getName() || "<Enter group name>"}
           </span>
         )}
       </div>

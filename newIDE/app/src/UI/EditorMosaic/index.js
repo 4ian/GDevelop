e64fb20ac3d7446448cf915efe9eba01e45d8132
HarsimranVirk <harsimranvirk99@gmail.com>
2020-03-10 23:58:19 +0530

    Added type Descriptor, Changed Trans to t everywhere EditorMosaic is used.

--- newIDE/app/src/UI/EditorMosaic/index.js
+++ newIDE/app/src/UI/EditorMosaic/index.js
@@ -8,6 +8,7 @@ import {
 } from 'react-mosaic-component';
 import CloseButton from './CloseButton';
 import ThemeConsumer from '../Theme/ThemeConsumer';
+import { type MessageDescriptor } from '../../Utils/i18n/MessageDescriptor.flow';
 
 // EditorMosaic default styling:
 import 'react-mosaic-component/react-mosaic-component.css';
@@ -17,7 +18,7 @@ export type Editor = {|
   type: 'primary' | 'secondary',
   renderEditor: () => React.Node,
   noTitleBar?: boolean,
-  title?: React.Node,
+  title?: MessageDescriptor,
   toolbarControls?: Array<React.Node>,
 |};
 

7da4c2f85819f74313ac3f52875b558da11d367a
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-11 12:17:30 +0530

    Update DialogWithHeaderFooter to use Dialog component

--- newIDE/app/src/UI/DialogWithHeaderFooter/index.js
+++ newIDE/app/src/UI/DialogWithHeaderFooter/index.js
@@ -1,34 +1,9 @@
 // @flow
 import * as React from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { ResponsiveWindowMeasurer } from '../Reponsive/ResponsiveWindowMeasurer';
+import Dialog from '../Dialog';
 import ScrollView from '../ScrollView';
 
 const styles = {
-  defaultBody: {
-    overflowX: 'hidden',
-  },
-  noMarginBody: {
-    padding: 0,
-    overflowX: 'hidden',
-  },
-  flexRowBody: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  flexBody: {
-    display: 'flex',
-  },
-  actionsContainerWithSecondaryActions: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  noTitleMargin: {
-    padding: 0,
-  },
   headerContainerStyle: {
     maxHeight: 80,
     overflow: 'auto',
@@ -94,85 +69,42 @@ export default (props: Props) => {
     headerContainerStyle,
     mainContainerStyle,
     footerContainerStyle,
-    flexRowBody,
     flexBody,
     noTitleMargin,
   } = props;
-  const dialogActions = secondaryActions ? (
-    <React.Fragment>
-      <div key="secondary-actions">{secondaryActions}</div>
-      <div key="actions">{actions}</div>
-    </React.Fragment>
-  ) : (
-    actions
-  );
 
   return (
-    <ResponsiveWindowMeasurer>
-      {size => (
-        <Dialog
-          open={open}
-          onClose={onRequestClose}
-          fullWidth
-          fullScreen={size === 'small'}
-          maxWidth={maxWidth !== undefined ? maxWidth : 'md'}
-          disableBackdropClick
+    <Dialog
+      title={title}
+      secondaryActions={secondaryActions}
+      actions={actions}
+      open={open}
+      onRequestClose={onRequestClose}
+      maxWidth={maxWidth}
+      noMargin={noMargin}
+      flexRowBody={false}
+      flexBody={flexBody}
+      noTitleMargin={noTitleMargin}
+    >
+      {header && (
+        <ScrollView
+          style={{ ...styles.headerContainerStyle, ...headerContainerStyle }}
         >
-          {title && (
-            <DialogTitle
-              style={noTitleMargin ? styles.noTitleMargin : undefined}
-            >
-              {title}
-            </DialogTitle>
-          )}
-          <DialogContent
-            style={{
-              ...(noMargin ? styles.noMarginBody : styles.defaultBody),
-              ...(flexRowBody ? styles.flexRowBody : {}),
-              ...(flexBody ? styles.flexBody : {}),
-              overflow: 'hidden',
-            }}
-          >
-            {header && (
-              <ScrollView
-                style={{
-                  ...styles.headerContainerStyle,
-                  ...headerContainerStyle,
-                }}
-              >
-                {header}
-              </ScrollView>
-            )}
-            <ScrollView
-              style={{
-                ...styles.mainContainerStyle,
-                ...mainContainerStyle,
-              }}
-            >
-              {children}
-            </ScrollView>
-            {footer && (
-              <ScrollView
-                style={{
-                  ...styles.footerContainerStyle,
-                  ...footerContainerStyle,
-                }}
-              >
-                {footer}
-              </ScrollView>
-            )}
-          </DialogContent>
-          <DialogActions
-            style={
-              secondaryActions
-                ? styles.actionsContainerWithSecondaryActions
-                : undefined
-            }
-          >
-            {dialogActions}
-          </DialogActions>
-        </Dialog>
+          {header}
+        </ScrollView>
       )}
-    </ResponsiveWindowMeasurer>
+      <ScrollView
+        style={{ ...styles.mainContainerStyle, ...mainContainerStyle }}
+      >
+        {children}
+      </ScrollView>
+      {footer && (
+        <ScrollView
+          style={{ ...styles.footerContainerStyle, ...footerContainerStyle }}
+        >
+          {footer}
+        </ScrollView>
+      )}
+    </Dialog>
   );
 };

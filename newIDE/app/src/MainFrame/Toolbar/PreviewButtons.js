1af6c71ceb1d5c05459ab4d5133097d1f3b91238
Florian Rival <Florian.rival@gmail.com>
2020-03-18 21:22:48 +0100

    Update button visibility and labels
    * Don't show the first button and separator if no first scene for preview chosen 
    * Also use a functional component

--- newIDE/app/src/MainFrame/Toolbar/PreviewButtons.js
+++ newIDE/app/src/MainFrame/Toolbar/PreviewButtons.js
@@ -27,97 +27,92 @@ type Props = {|
   previewButtonSettings: PreviewButtonSettings,
 |};
 
-export default class PreviewButtons extends React.Component<
-  Props,
-  PreviewButtonSettings
-> {
-  render() {
-    const {
-      onPreview,
-      onNetworkPreview,
-      onOpenDebugger,
-      showNetworkPreviewButton,
-      showPreviewButton,
-    } = this.props;
+export default function PreviewButtons({
+  onPreview,
+  onNetworkPreview,
+  onOpenDebugger,
+  showNetworkPreviewButton,
+  showPreviewButton,
+  previewButtonSettings,
+}: Props) {
+  const {
+    isPreviewFirstSceneOverriden,
+    previewFirstSceneName,
+    useSceneAsPreviewFirstScene,
+    togglePreviewFirstSceneOverride,
+  } = previewButtonSettings;
 
-    const {
-      isPreviewFirstSceneOverriden,
-      previewFirstSceneName,
-      useSceneAsPreviewFirstScene,
-      togglePreviewFirstSceneOverride,
-    } = this.props.previewButtonSettings;
+  const canOverridePreviewFirstScene =
+    useSceneAsPreviewFirstScene && togglePreviewFirstSceneOverride;
+  const previewIcon = (
+    <ToolbarIcon
+      onClick={onPreview}
+      src={
+        isPreviewFirstSceneOverriden
+          ? 'res/ribbon_default/previewOverride32.png'
+          : 'res/ribbon_default/preview32.png'
+      }
+      tooltip={
+        !canOverridePreviewFirstScene
+          ? t`Launch a preview of the scene`
+          : isPreviewFirstSceneOverriden
+          ? t`Preview is overridden, right click for more`
+          : t`Launch a preview of the scene, right click for more`
+      }
+    />
+  );
 
-    const previewIcon = (
-      <ToolbarIcon
-        onClick={onPreview}
-        src={
-          isPreviewFirstSceneOverriden
-            ? 'res/ribbon_default/previewOverride32.png'
-            : 'res/ribbon_default/preview32.png'
-        }
-        tooltip={
-          isPreviewFirstSceneOverriden
-            ? t`Preview is overridden, right click for more`
-            : t`Launch a preview of the scene, right click for more`
-        }
-      />
-    );
-
-    return (
-      <React.Fragment>
-        {showPreviewButton &&
-          (!useSceneAsPreviewFirstScene || !togglePreviewFirstSceneOverride ? (
-            previewIcon
-          ) : (
-            <ElementWithMenu
-              element={previewIcon}
-              openMenuWithSecondaryClick
-              buildMenuTemplate={() => [
-                {
-                  type: 'checkbox',
-                  label: previewFirstSceneName
-                    ? 'Use scene ' + previewFirstSceneName + ' for preview'
-                    : 'Use this scene for preview',
-                  checked: isPreviewFirstSceneOverriden,
-                  click: () => {
-                    togglePreviewFirstSceneOverride();
-                    if (!previewFirstSceneName) {
-                      // eslint-disable-next-line
-                      useSceneAsPreviewFirstScene();
-                    }
-                  },
-                },
-                { type: 'separator' },
-                {
-                  label: 'Always use this scene to start the previews',
-                  // eslint-disable-next-line
-                  click: () => useSceneAsPreviewFirstScene(),
-                },
-              ]}
-            />
-          ))}
-        {showNetworkPreviewButton && (
+  return (
+    <React.Fragment>
+      {showPreviewButton &&
+        (!canOverridePreviewFirstScene ? (
+          previewIcon
+        ) : (
           <ElementWithMenu
-            element={
-              <ToolbarIcon
-                src="res/ribbon_default/bug32.png"
-                tooltip={t`Advanced preview options (debugger, network preview...)`}
-              />
-            }
+            element={previewIcon}
+            openMenuWithSecondaryClick
             buildMenuTemplate={() => [
+              ...(previewFirstSceneName
+                ? [
+                    {
+                      type: 'checkbox',
+                      label:
+                        'Start all previews from scene ' +
+                        previewFirstSceneName,
+                      checked: isPreviewFirstSceneOverriden,
+                      click: togglePreviewFirstSceneOverride,
+                    },
+                    { type: 'separator' },
+                  ]
+                : []),
               {
-                label: 'Network preview (Preview over WiFi/LAN)',
-                click: () => onNetworkPreview(),
-              },
-              { type: 'separator' },
-              {
-                label: 'Preview with debugger and performance profiler',
-                click: () => onOpenDebugger(),
+                label: 'Use this scene to start all previews',
+                click: useSceneAsPreviewFirstScene,
               },
             ]}
           />
-        )}
-      </React.Fragment>
-    );
-  }
+        ))}
+      {showNetworkPreviewButton && (
+        <ElementWithMenu
+          element={
+            <ToolbarIcon
+              src="res/ribbon_default/bug32.png"
+              tooltip={t`Advanced preview options (debugger, network preview...)`}
+            />
+          }
+          buildMenuTemplate={() => [
+            {
+              label: 'Network preview (Preview over WiFi/LAN)',
+              click: () => onNetworkPreview(),
+            },
+            { type: 'separator' },
+            {
+              label: 'Preview with debugger and performance profiler',
+              click: () => onOpenDebugger(),
+            },
+          ]}
+        />
+      )}
+    </React.Fragment>
+  );
 }

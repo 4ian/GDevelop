b51c1db4b4e767c336484eed48bdec8c58f08847
HarsimranVirk <harsimranvirk99@gmail.com>
2020-04-10 17:08:15 +0530

    Quick fix for two scrollbars in project manager, search bar is always shown

--- newIDE/app/src/ProjectManager/index.js
+++ newIDE/app/src/ProjectManager/index.js
@@ -54,6 +54,7 @@ const styles = {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
+    overflowY: 'hidden',
   },
   list: {
     flex: 1,
@@ -1056,6 +1057,7 @@ export default class ProjectManager extends React.Component<Props, State> {
           value={searchText}
           onRequestSearch={this._onRequestSearch}
           onChange={this._onSearchChange}
+          elevation={24}
         />
         {this.state.projectVariablesEditorOpen && (
           <VariablesEditorDialog

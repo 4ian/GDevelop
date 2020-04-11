877b6829005ce2651c00a7aa2fa344ba3b67701f
Bouh <bouh.vivez@gmail.com>
2020-04-11 16:37:45 +0200

    2 spaces instead tabs

--- GDJS/Runtime/events-tools/commontools.js
+++ GDJS/Runtime/events-tools/commontools.js
@@ -16,7 +16,7 @@ gdjs.evtTools.common = gdjs.evtTools.common || {};
  * @private
  */
 gdjs.evtTools.common.getVariableNumber = function(variable) {
-	return variable.getAsNumber();
+  return variable.getAsNumber();
 };
 
 /**
@@ -24,50 +24,50 @@ gdjs.evtTools.common.getVariableNumber = function(variable) {
  * @private
  */
 gdjs.evtTools.common.getVariableString = function(variable) {
-	return variable.getAsString();
+  return variable.getAsString();
 };
 
 /**
  * @private
  */
 gdjs.evtTools.common.sceneVariableExists = function(runtimeScene, variableName) {
-	return runtimeScene.getVariables().has(variableName);
+  return runtimeScene.getVariables().has(variableName);
 };
 
 /**
  * @private
  */
 gdjs.evtTools.common.globalVariableExists = function(runtimeScene, variableName) {
-	return runtimeScene.getGame().getVariables().has(variableName);
+  return runtimeScene.getGame().getVariables().has(variableName);
 };
 
 /**
  * @private
  */
 gdjs.evtTools.common.variableChildExists = function(variable, childName) {
-	return variable.hasChild(childName);
+  return variable.hasChild(childName);
 };
 
 /**
  * @private
  */
 gdjs.evtTools.common.variableRemoveChild = function(variable, childName) {
-	return variable.removeChild(childName);
+  return variable.removeChild(childName);
 };
 
 /**
  * @private
  */
 gdjs.evtTools.common.variableClearChildren = function(variable) {
-	variable.clearChildren();
+  variable.clearChildren();
 };
 
 /**
  * @private
  */
 gdjs.evtTools.common.getVariableChildCount = function(variable) {
-	if (variable.isStructure() == false) return 0;
-	return Object.keys(variable.getAllChildren()).length;
+  if (variable.isStructure() == false) return 0;
+  return Object.keys(variable.getAllChildren()).length;
 };
 
 /**
@@ -75,7 +75,7 @@ gdjs.evtTools.common.getVariableChildCount = function(variable) {
  * @private
  */
 gdjs.evtTools.common.toNumber = function(str) {
-	return parseFloat(str);
+  return parseFloat(str);
 };
 
 /**
@@ -83,9 +83,9 @@ gdjs.evtTools.common.toNumber = function(str) {
  * @private
  */
 gdjs.evtTools.common.toString = function(num) {
-	//Using String literal is fastest than using toString according to
-	//http://jsperf.com/number-to-string/2 and http://jsben.ch/#/ghQYR
-	return '' + num;
+  //Using String literal is fastest than using toString according to
+  //http://jsperf.com/number-to-string/2 and http://jsben.ch/#/ghQYR
+  return '' + num;
 };
 
 /**
@@ -93,7 +93,7 @@ gdjs.evtTools.common.toString = function(num) {
  * @private
  */
 gdjs.evtTools.common.logicalNegation = function(bool) {
-	return !bool;
+  return !bool;
 };
 
 /**
@@ -104,7 +104,7 @@ gdjs.evtTools.common.logicalNegation = function(bool) {
  * @returns {number} The new value.
  */
 gdjs.evtTools.common.clamp = function(x, min, max) {
-	return Math.min(Math.max(x, min), max);
+  return Math.min(Math.max(x, min), max);
 };
 
 /**
@@ -113,9 +113,9 @@ gdjs.evtTools.common.clamp = function(x, min, max) {
  * @returns {number} The hyperbolic arc-cosine for the value.
  */
 gdjs.evtTools.common.acosh = function(arg) {
-	// http://kevin.vanzonneveld.net
-	// +   original by: Onno Marsman
-	return Math.log(arg + Math.sqrt(arg * arg - 1));
+  // http://kevin.vanzonneveld.net
+  // +   original by: Onno Marsman
+  return Math.log(arg + Math.sqrt(arg * arg - 1));
 };
 
 /**
@@ -124,9 +124,9 @@ gdjs.evtTools.common.acosh = function(arg) {
  * @returns {number} The hyperbolic arcsine for the value.
  */
 gdjs.evtTools.common.asinh = function(arg) {
-	// http://kevin.vanzonneveld.net
-	// +   original by: Onno Marsman
-	return Math.log(arg + Math.sqrt(arg * arg + 1));
+  // http://kevin.vanzonneveld.net
+  // +   original by: Onno Marsman
+  return Math.log(arg + Math.sqrt(arg * arg + 1));
 };
 
 /**
@@ -135,9 +135,9 @@ gdjs.evtTools.common.asinh = function(arg) {
  * @returns {number} The hyperbolic arctangent for the value.
  */
 gdjs.evtTools.common.atanh = function(arg) {
-	// http://kevin.vanzonneveld.net
-	// +   original by: Onno Marsman
-	return 0.5 * Math.log((1 + arg) / (1 - arg));
+  // http://kevin.vanzonneveld.net
+  // +   original by: Onno Marsman
+  return 0.5 * Math.log((1 + arg) / (1 - arg));
 };
 
 /**
@@ -146,7 +146,7 @@ gdjs.evtTools.common.atanh = function(arg) {
  * @returns {number} The hyperbolic cosine for the value.
  */
 gdjs.evtTools.common.cosh = function(arg) {
-	return (Math.exp(arg) + Math.exp(-arg)) / 2;
+  return (Math.exp(arg) + Math.exp(-arg)) / 2;
 };
 
 /**
@@ -155,7 +155,7 @@ gdjs.evtTools.common.cosh = function(arg) {
  * @returns {number} The hyperbolic sine for the value.
  */
 gdjs.evtTools.common.sinh = function(arg) {
-	return (Math.exp(arg) - Math.exp(-arg)) / 2;
+  return (Math.exp(arg) - Math.exp(-arg)) / 2;
 };
 
 /**
@@ -164,7 +164,7 @@ gdjs.evtTools.common.sinh = function(arg) {
  * @returns {number} The hyperbolic tangent for the value.
  */
 gdjs.evtTools.common.tanh = function(arg) {
-	return (Math.exp(arg) - Math.exp(-arg)) / (Math.exp(arg) + Math.exp(-arg));
+  return (Math.exp(arg) - Math.exp(-arg)) / (Math.exp(arg) + Math.exp(-arg));
 };
 
 /**
@@ -173,7 +173,7 @@ gdjs.evtTools.common.tanh = function(arg) {
  * @returns {number} The cotangent for the value.
  */
 gdjs.evtTools.common.cot = function(arg) {
-	return 1 / Math.tan(arg);
+  return 1 / Math.tan(arg);
 };
 
 /**
@@ -182,7 +182,7 @@ gdjs.evtTools.common.cot = function(arg) {
  * @returns {number} The cosecant for the value.
  */
 gdjs.evtTools.common.csc = function(arg) {
-	return 1 / Math.sin(arg);
+  return 1 / Math.sin(arg);
 };
 
 /**
@@ -191,7 +191,7 @@ gdjs.evtTools.common.csc = function(arg) {
  * @returns {number} The secant for the value.
  */
 gdjs.evtTools.common.sec = function(arg) {
-	return 1 / Math.cos(arg);
+  return 1 / Math.cos(arg);
 };
 
 /**
@@ -200,7 +200,7 @@ gdjs.evtTools.common.sec = function(arg) {
  * @returns {number} The base-10 logarithm for the value.
  */
 gdjs.evtTools.common.log10 = function(arg) {
-	return Math.log(arg) / Math.LN10;
+  return Math.log(arg) / Math.LN10;
 };
 
 /**
@@ -209,7 +209,7 @@ gdjs.evtTools.common.log10 = function(arg) {
  * @returns {number} The base-2 logarithm for the value.
  */
 gdjs.evtTools.common.log2 = function(arg) {
-	return Math.log(arg) / Math.LN2;
+  return Math.log(arg) / Math.LN2;
 };
 
 /**
@@ -218,9 +218,9 @@ gdjs.evtTools.common.log2 = function(arg) {
  * @returns {number} Return the sign for the value (1,-1 or 0).
  */
 gdjs.evtTools.common.sign = function(arg) {
-	if (arg === 0) return 0;
+  if (arg === 0) return 0;
 
-	return arg > 0 ? +1 : -1;
+  return arg > 0 ? +1 : -1;
 };
 
 /**
@@ -229,7 +229,7 @@ gdjs.evtTools.common.sign = function(arg) {
  * @returns {number} Return the cube root for the value.
  */
 gdjs.evtTools.common.cbrt = function(x) {
-	return Math.pow(x, 1 / 3);
+  return Math.pow(x, 1 / 3);
 };
 
 /**
@@ -239,7 +239,7 @@ gdjs.evtTools.common.cbrt = function(x) {
  * @returns {number} Return the nth root for the value.
  */
 gdjs.evtTools.common.nthroot = function(x, n) {
-	return Math.pow(x, 1 / n);
+  return Math.pow(x, 1 / n);
 };
 
 /**
@@ -249,7 +249,7 @@ gdjs.evtTools.common.nthroot = function(x, n) {
  * @returns {number} Return the remainder for the values.
  */
 gdjs.evtTools.common.mod = function(x, y) {
-	return x - y * Math.floor(x / y);
+  return x - y * Math.floor(x / y);
 };
 
 /**
@@ -259,7 +259,7 @@ gdjs.evtTools.common.mod = function(x, y) {
  * @returns {number} Return the difference of the angles.
  */
 gdjs.evtTools.common.angleDifference = function(angle1, angle2) {
-	return gdjs.evtTools.common.mod(gdjs.evtTools.common.mod(angle1 - angle2, 360.0) + 180.0, 360.0) - 180.0;
+  return gdjs.evtTools.common.mod(gdjs.evtTools.common.mod(angle1 - angle2, 360.0) + 180.0, 360.0) - 180.0;
 };
 
 /**
@@ -270,7 +270,7 @@ gdjs.evtTools.common.angleDifference = function(angle1, angle2) {
  * @returns {number} Return the difference of the angles.
  */
 gdjs.evtTools.common.lerp = function(a, b, x) {
-	return a + (b - a) * x;
+  return a + (b - a) * x;
 };
 
 /**
@@ -279,5 +279,5 @@ gdjs.evtTools.common.lerp = function(a, b, x) {
  * @returns {number} Return the value without decimals.
  */
 gdjs.evtTools.common.trunc = function(x) {
-	return x | 0;
+  return x | 0;
 };

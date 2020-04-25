5db65aaf4521b16ae18e4e6a0293c8dee81a5168
gautamv95 <gautamverma.bits@gmail.com>
2020-04-26 02:15:28 +0530

    Add UnsavedChangesDialog Component

---
+++ newIDE/app/src/UI/UnsavedChangesDialog.js
@@ -0,0 +1,51 @@
+// @flow
+import * as React from 'react';
+import Dialog from './Dialog';
+import Text from './Text';
+import FlatButton from './FlatButton';
+import { Trans } from '@lingui/macro';
+
+type Props = {|
+  onRequestClose: () => void,
+  onCancel: () => void,
+|};
+
+export default class UnsavedChangesDialog extends React.Component<
+  Props,
+  State
+> {
+  render() {
+    const { onCancel, onRequestClose } = this.props;
+    const actions = [
+      <FlatButton
+        key="cancel"
+        label={<Trans>Cancel</Trans>}
+        primary={false}
+        onClick={onRequestClose}
+      />,
+      <FlatButton
+        key="ok"
+        label={<Trans>Yes</Trans>}
+        primary={true}
+        keyboardFocused={false}
+        onClick={onCancel}
+      />,
+    ];
+    return (
+      <Dialog
+        title={<Trans>Confirm Quit</Trans>}
+        onRequestClose={onRequestClose}
+        maxWidth="xs"
+        actions={actions}
+        open
+      >
+        <Text>
+          <Trans>Are you sure want to quit?</Trans>
+        </Text>
+        <Text style={{ marginTop: -10 }}>
+          <Trans>Any unsaved changes will be lost...</Trans>
+        </Text>
+      </Dialog>
+    );
+  }
+}

d85f2bad93343b30d53f5daa4bbf90b9b5f33d82
arthuro555 <arthur.pacaud@hotmail.fr>
2020-04-27 14:52:20 +0200

    Add callback after firebase initialized

--- Extensions/Firebase/firebasetools/C_firebasetools.js
+++ Extensions/Firebase/firebasetools/C_firebasetools.js
@@ -1,13 +1,19 @@
 /**
- * Firebase Tools
+ * Firebase Tools Collection
  * @author arthuro555
  */
+
 
 /**
  * Firebase Event Tools
  * @namespace
  */
 gdjs.evtTools.firebase = {};
+
+/**
+ * An array of callbacks to call when the app gets initialized
+ */
+gdjs.evtTools.firebase.onAppCreated = []
 
 /** Callback to initialize the Firebase SDK */
 gdjs.registerFirstRuntimeSceneLoadedCallback(function(runtimeScene) {
@@ -18,4 +24,5 @@ gdjs.registerFirstRuntimeSceneLoadedCallback(function(runtimeScene) {
         console.error("The Firebase configuration is invalid!");
     }
     firebase.initializeApp(firebaseConfig);
+    for (let func of gdjs.evtTools.firebase.onAppCreated) func();
 })

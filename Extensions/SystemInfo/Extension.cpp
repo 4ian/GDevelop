413607528ae434e98c432357d889ecd8e56de2b7
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2020-05-08 15:47:41 +0200

    Update Extension.cpp
    Removed plus as unsupported

--- Extensions/SystemInfo/Extension.cpp
+++ Extensions/SystemInfo/Extension.cpp
@@ -48,8 +48,7 @@ void DeclareSystemInfoExtension(gd::PlatformExtension& extension) {
           "IsPreview",
           _("Is the game running in a preview"),
           _(
-            "Check if the game is currently being previewed. " +
-            "Can be used to enable a Debug mode"
+            "Check if the game is currently being previewed. Can be used to enable a Debug mode"
           ),
           _("The game is a preview"),
           _("System information"),

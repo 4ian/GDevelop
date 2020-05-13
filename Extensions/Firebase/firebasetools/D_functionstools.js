f647642b50857e97e7a3f1b66c6302551ef9c131
arthuro555 <arthur.pacaud@hotmail.fr>
2020-05-13 11:31:45 +0200

    Mark optional arguments in functiontools

--- Extensions/Firebase/firebasetools/D_functionstools.js
+++ Extensions/Firebase/firebasetools/D_functionstools.js
@@ -13,9 +13,9 @@ gdjs.evtTools.firebase.functions = {
     /**
      * Call an http function.
      * @param {string} httpFunctionName - The name of the function to call
-     * @param {string} parameters - Parameters for the function either as a JSON object or a string.
-     * @param {gdjs.Variable} callbackValueVariable - The variable where to store the result.
-     * @param {gdjs.Variable} callbackStateVariable - The variable where to store if the operration was successful.
+     * @param {string} [parameters] - Parameters for the function either as a JSON object or a string.
+     * @param {gdjs.Variable} [callbackValueVariable] - The variable where to store the result.
+     * @param {gdjs.Variable} [callbackStateVariable] - The variable where to store if the operration was successful.
      */
     call(httpFunctionName, parameters, callbackValueVariable, callbackStateVariable) {
         let param;

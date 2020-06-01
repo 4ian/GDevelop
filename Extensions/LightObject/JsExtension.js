5f5becdd2e95cd6ffa299d19c00d73715040de61
HarsimranVirk <harsimranvirk99@gmail.com>
2020-06-01 19:59:37 +0530

    Added hard coded mesh

--- Extensions/LightObject/JsExtension.js
+++ Extensions/LightObject/JsExtension.js
@@ -1,31 +1,33 @@
 module.exports = {
-    createExtension: function (_, gd) {
-        const extension = new gd.PlatformExtension();
-        extension.setExtensionInformation(
-            'Lighting',
-            _('PIXI Lights'),
-            _('Lights using PIXI'),
-            'Harsimran Virk',
-            'MIT'
-        );
+  createExtension: function (_, gd) {
+    const extension = new gd.PlatformExtension();
+    extension.setExtensionInformation(
+      'Lighting',
+      _('PIXI Lights'),
+      _('Lights using PIXI'),
+      'Harsimran Virk',
+      'MIT'
+    );
 
-        var lightObject = new gd.ObjectJsImplementation();
+    var lightObject = new gd.ObjectJsImplementation();
 
-        const object = extension
-        .addObject(
-            'LightObject',
-            _('Light Object for testing'),
-            _('This is an experimental light object'),
-            'CppPlatform/Extensions/topdownmovementicon.png',
-            lightObject
-        )
-        .setIncludeFile('Extensions/LightObject/lightruntimeobject.js')
-        .addIncludeFile(
-            'Extensions/LightObject/lightruntimeobject-pixi-renderer.js'
-        );
+    const object = extension
+      .addObject(
+        'LightObject',
+        _('Light Object for testing'),
+        _('This is an experimental light object'),
+        'CppPlatform/Extensions/topdownmovementicon.png',
+        lightObject
+      )
+      .setIncludeFile('Extensions/LightObject/lightruntimeobject.js')
+      .addIncludeFile(
+        'Extensions/LightObject/lightruntimeobject-pixi-renderer.js'
+      );
 
-        return extension;  
-    },
+    return extension;
+  },
 
-    runExtensionSanityTests: function(gd, extension) { return []; }
-}
+  runExtensionSanityTests: function (gd, extension) {
+    return [];
+  },
+};

b345b83ddcb97c4d92d2e6cd6afe1b791329474b
HarsimranVirk <harsimranvirk99@gmail.com>
2020-06-01 19:59:37 +0530

    Added hard coded mesh

--- Extensions/LightObject/lightruntimeobject.js
+++ Extensions/LightObject/lightruntimeobject.js
@@ -1,23 +1,22 @@
 gdjs.LightRuntimeObject = function (runtimeScene, lightObjectData) {
-    gdjs.RuntimeObject.call(this, runtimeScene, lightObjectData);
+  gdjs.RuntimeObject.call(this, runtimeScene, lightObjectData);
 
-    if (this._renderer)
-        gdjs.LightRuntimeObjectRenderer.call(this._renderer, this, runtimeScene);
-    else
-        /** @type {gdjs.TextRuntimeObjectRenderer} */
-        this._renderer = new gdjs.LightRuntimeObjectRenderer(this, runtimeScene);
+  if (this._renderer)
+    gdjs.LightRuntimeObjectRenderer.call(this._renderer, this, runtimeScene);
+  /** @type {gdjs.TextRuntimeObjectRenderer} */ 
+  else this._renderer = new gdjs.LightRuntimeObjectRenderer(this, runtimeScene);
 
-    // *ALWAYS* call `this.onCreated()` at the very end of your object constructor.
-    this.onCreated();
-} 
-
-gdjs.LightRuntimeObject.prototype = Object.create( gdjs.RuntimeObject.prototype );
-gdjs.registerObject("LightObject::Light", gdjs.LightRuntimeObject);
-
-gdjs.LightRuntimeObject.prototype.getRendererObject = function() {
-    return this._renderer.getRendererObject();
+  // *ALWAYS* call `this.onCreated()` at the very end of your object constructor.
+  this.onCreated();
 };
 
-gdjs.LightRuntimeObject.prototype.update = function() {
-    //this._renderer.ensureUpToDate();
+gdjs.LightRuntimeObject.prototype = Object.create(gdjs.RuntimeObject.prototype);
+gdjs.registerObject('Lighting::LightObject', gdjs.LightRuntimeObject);
+
+gdjs.LightRuntimeObject.prototype.getRendererObject = function () {
+  return this._renderer.getRendererObject();
+};
+
+gdjs.LightRuntimeObject.prototype.update = function () {
+  //this._renderer.ensureUpToDate();
 };

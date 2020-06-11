a1e48d9d32aeddc69452c2730edb1d95c9eae6ec
arthuro555 <arthur.pacaud@hotmail.fr>
2020-06-11 16:01:28 +0200

    Correct usage of UsedExtensions

--- newIDE/app/src/ProjectManager/EventFunctionExtensionNameVerifier.js
+++ newIDE/app/src/ProjectManager/EventFunctionExtensionNameVerifier.js
@@ -3,8 +3,9 @@ export function isExtensionNameTaken(
   name: string,
   project: gdProject
 ): boolean {
-  for (let extensionName of project.getUsedExtensions().toJSArray()) {
-    if (extensionName === name) return true;
+  const allExtensions = project.getCurrentPlatform().getAllPlatformExtensions();
+  for (let i = 0; i < allExtensions.size(); i++) {
+    if (allExtensions.at(i).getName() === name) return true;
   }
   return project.hasEventsFunctionsExtensionNamed(name);
 }

095aea93e3f924b445d295f3c58d76f94c18e117
Nilay Majorwar <nilaymajorwar@gmail.com>
2020-07-11 00:27:29 +0530

    Add toolbar commands for events editor

--- newIDE/app/src/EventsSheet/Toolbar.js
+++ newIDE/app/src/EventsSheet/Toolbar.js
@@ -5,6 +5,7 @@ import { ToolbarGroup } from '../UI/Toolbar';
 import ToolbarSeparator from '../UI/ToolbarSeparator';
 import ToolbarIcon from '../UI/ToolbarIcon';
 import ElementWithMenu from '../UI/Menu/ElementWithMenu';
+import ToolbarCommands from './ToolbarCommands';
 import { type EventMetadata } from './EnumerateEventsMetadata';
 
 type Props = {|
@@ -27,74 +28,77 @@ type Props = {|
 export class Toolbar extends PureComponent<Props> {
   render() {
     return (
-      <ToolbarGroup lastChild>
-        <ToolbarIcon
-          onClick={this.props.onAddStandardEvent}
-          src="res/ribbon_default/eventadd32.png"
-          tooltip={t`Add a new empty event`}
-        />
-        <ToolbarIcon
-          onClick={this.props.onAddSubEvent}
-          src="res/ribbon_default/subeventadd32.png"
-          disabled={!this.props.canAddSubEvent}
-          tooltip={t`Add a sub-event to the selected event`}
-        />
-        <ToolbarIcon
-          onClick={this.props.onAddCommentEvent}
-          src="res/ribbon_default/commentaireadd32.png"
-          tooltip={t`Add a comment`}
-        />
-        <ElementWithMenu
-          element={
-            <ToolbarIcon
-              src="res/ribbon_default/add32.png"
-              tooltip={t`Choose and add an event`}
-            />
-          }
-          buildMenuTemplate={() =>
-            this.props.allEventsMetadata.map(metadata => {
-              return {
-                label: metadata.fullName,
-                click: () => this.props.onAddEvent(metadata.type),
-              };
-            })
-          }
-        />
-        <ToolbarSeparator />
-        <ToolbarIcon
-          onClick={this.props.onRemove}
-          src="res/ribbon_default/deleteselected32.png"
-          disabled={!this.props.canRemove}
-          tooltip={t`Delete the selected event(s)`}
-        />
-        <ToolbarIcon
-          onClick={this.props.undo}
-          src="res/ribbon_default/undo32.png"
-          disabled={!this.props.canUndo}
-          tooltip={t`Undo the last changes`}
-        />
-        <ToolbarIcon
-          onClick={this.props.redo}
-          src="res/ribbon_default/redo32.png"
-          disabled={!this.props.canRedo}
-          tooltip={t`Redo the last changes`}
-        />
-        <ToolbarSeparator />
-        <ToolbarIcon
-          onClick={() => this.props.onToggleSearchPanel()}
-          src="res/ribbon_default/search32.png"
-          tooltip={t`Search in events`}
-          acceleratorString={'CmdOrCtrl+F'}
-        />
-        {this.props.onOpenSettings && <ToolbarSeparator />}
-        {this.props.onOpenSettings && (
+      <>
+        <ToolbarCommands {...this.props} />
+        <ToolbarGroup lastChild>
           <ToolbarIcon
-            onClick={this.props.onOpenSettings}
-            src="res/ribbon_default/pref32.png"
-            tooltip={t`Open settings`}
+            onClick={this.props.onAddStandardEvent}
+            src="res/ribbon_default/eventadd32.png"
+            tooltip={t`Add a new empty event`}
           />
-        )}
-      </ToolbarGroup>
+          <ToolbarIcon
+            onClick={this.props.onAddSubEvent}
+            src="res/ribbon_default/subeventadd32.png"
+            disabled={!this.props.canAddSubEvent}
+            tooltip={t`Add a sub-event to the selected event`}
+          />
+          <ToolbarIcon
+            onClick={this.props.onAddCommentEvent}
+            src="res/ribbon_default/commentaireadd32.png"
+            tooltip={t`Add a comment`}
+          />
+          <ElementWithMenu
+            element={
+              <ToolbarIcon
+                src="res/ribbon_default/add32.png"
+                tooltip={t`Choose and add an event`}
+              />
+            }
+            buildMenuTemplate={() =>
+              this.props.allEventsMetadata.map(metadata => {
+                return {
+                  label: metadata.fullName,
+                  click: () => this.props.onAddEvent(metadata.type),
+                };
+              })
+            }
+          />
+          <ToolbarSeparator />
+          <ToolbarIcon
+            onClick={this.props.onRemove}
+            src="res/ribbon_default/deleteselected32.png"
+            disabled={!this.props.canRemove}
+            tooltip={t`Delete the selected event(s)`}
+          />
+          <ToolbarIcon
+            onClick={this.props.undo}
+            src="res/ribbon_default/undo32.png"
+            disabled={!this.props.canUndo}
+            tooltip={t`Undo the last changes`}
+          />
+          <ToolbarIcon
+            onClick={this.props.redo}
+            src="res/ribbon_default/redo32.png"
+            disabled={!this.props.canRedo}
+            tooltip={t`Redo the last changes`}
+          />
+          <ToolbarSeparator />
+          <ToolbarIcon
+            onClick={() => this.props.onToggleSearchPanel()}
+            src="res/ribbon_default/search32.png"
+            tooltip={t`Search in events`}
+            acceleratorString={'CmdOrCtrl+F'}
+          />
+          {this.props.onOpenSettings && <ToolbarSeparator />}
+          {this.props.onOpenSettings && (
+            <ToolbarIcon
+              onClick={this.props.onOpenSettings}
+              src="res/ribbon_default/pref32.png"
+              tooltip={t`Open settings`}
+            />
+          )}
+        </ToolbarGroup>
+      </>
     );
   }
 }
---
+++ newIDE/app/src/EventsSheet/ToolbarCommands.js
@@ -0,0 +1,105 @@
+// @flow
+import * as React from 'react';
+import { t } from '@lingui/macro';
+import {
+  useCommand,
+  useCommandWithOptions,
+} from '../CommandPalette/CommandHooks';
+import { type EventMetadata } from './EnumerateEventsMetadata';
+
+const addEmptyEventCommandText = t`Add a new empty event`;
+const addSubEventCommandText = t`Add a sub-event to the selected event`;
+const addCommentCommandText = t`Add a comment`;
+const chooseAndAddEventCommandText = t`Choose and add an event...`;
+const undoCommandText = t`Undo the last changes`;
+const redoCommandText = t`Redo the last changes`;
+const deleteSelectionCommandText = t`Delete the selected event(s)`;
+const searchEventsCommandText = t`Search in events`;
+const openSettingsCommandText = t`Open settings`;
+
+type Props = {
+  onAddStandardEvent: () => void,
+  onAddSubEvent: () => void,
+  canAddSubEvent: boolean,
+  onAddCommentEvent: () => void,
+  allEventsMetadata: Array<EventMetadata>,
+  onAddEvent: (eventType: string) => void,
+  onRemove: () => void,
+  canRemove: boolean,
+  undo: () => void,
+  canUndo: boolean,
+  redo: () => void,
+  canRedo: boolean,
+  onToggleSearchPanel: () => void,
+  onOpenSettings?: ?() => void,
+};
+
+const ToolbarCommands = (props: Props) => {
+  const { onAddEvent } = props;
+
+  useCommand('ADD_STANDARD EVENT', {
+    displayText: addEmptyEventCommandText,
+    enabled: true,
+    handler: props.onAddStandardEvent,
+  });
+
+  useCommand('ADD_SUBEVENT', {
+    displayText: addSubEventCommandText,
+    enabled: props.canAddSubEvent,
+    handler: props.onAddSubEvent,
+  });
+
+  useCommand('ADD_COMMENT_EVENT', {
+    displayText: addCommentCommandText,
+    enabled: true,
+    handler: props.onAddCommentEvent,
+  });
+
+  useCommandWithOptions('CHOOSE_AND_ADD_EVENT', {
+    displayText: chooseAndAddEventCommandText,
+    enabled: true,
+    generateOptions: React.useCallback(
+      () =>
+        props.allEventsMetadata.map(metadata => ({
+          text: metadata.fullName,
+          handler: () => onAddEvent(metadata.type),
+          value: metadata,
+        })),
+      [props.allEventsMetadata, onAddEvent]
+    ),
+  });
+
+  useCommand('DELETE_SELECTION', {
+    displayText: deleteSelectionCommandText,
+    enabled: props.canRemove,
+    handler: props.onRemove,
+  });
+
+  useCommand('UNDO', {
+    displayText: undoCommandText,
+    enabled: props.canUndo,
+    handler: props.undo,
+  });
+
+  useCommand('REDO', {
+    displayText: redoCommandText,
+    enabled: props.canRedo,
+    handler: props.redo,
+  });
+
+  useCommand('SEARCH_EVENTS', {
+    displayText: searchEventsCommandText,
+    enabled: true,
+    handler: props.onToggleSearchPanel,
+  });
+
+  useCommand('OPEN_SETTINGS', {
+    displayText: openSettingsCommandText,
+    enabled: !!props.onOpenSettings,
+    handler: props.onOpenSettings || (() => {}),
+  });
+
+  return null;
+};
+
+export default ToolbarCommands;

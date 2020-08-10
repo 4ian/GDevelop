7bc3dcab87ea66adac6422e1a84549f12fd88217
Nilay Majorwar <nilaymajorwar@gmail.com>
2020-08-10 15:56:28 +0530

    Add some more reserved shortcuts

--- newIDE/app/src/KeyboardShortcuts/ReservedShortcuts.js
+++ newIDE/app/src/KeyboardShortcuts/ReservedShortcuts.js
@@ -7,6 +7,14 @@ const reservedShortcuts = [
   'CmdOrCtrl+KeyC', // Copy
   'CmdOrCtrl+KeyV', // Paste
   'Delete', // Delete
+  'NumpadAdd', // Scene: Zoom in
+  'CmdOrCtrl+Equal', // Scene: Zoom in
+  'NumpadSubtract', // Scene: Zoom out
+  'CmdOrCtrl+Minus', // Scene: Zoom out
+
+  // TODO: Following shortcuts correspond to actions
+  // that have overlapping commands
+  'CmdOrCtrl+KeyF', // Events: Search
 ];
 
 export default reservedShortcuts;

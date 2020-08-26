89047498adf8e94b423fee0af94159d92a8384cd
Bouh <bouh.vivez@gmail.com>
2020-08-26 18:12:48 +0200

    ObjectGroupList

--- newIDE/app/src/ObjectGroupsList/GroupRow.js
+++ newIDE/app/src/ObjectGroupsList/GroupRow.js
@@ -1,3 +1,6 @@
+import { I18n } from '@lingui/react';
+import { t } from '@lingui/macro';
+
 import React from 'react';
 import { ListItem } from '../UI/List';
 import TextField, {
@@ -72,42 +75,46 @@ export default class GroupRow extends React.Component<Props, {||}> {
     return (
       <ThemeConsumer>
         {muiTheme => (
-          <ListItem
-            style={{
-              borderBottom: `1px solid ${muiTheme.listItem.separatorColor}`,
-              ...style,
-            }}
-            primaryText={label}
-            displayMenuButton
-            buildMenuTemplate={() => [
-              {
-                label: 'Edit group',
-                click: () => this.props.onEdit(group),
-              },
-              {
-                label: 'Set as Global group',
-                enabled: !this.props.isGlobalGroup,
-                click: () => this.props.onSetAsGlobalGroup(),
-                visible: this.props.canSetAsGlobalGroup !== false,
-              },
-              {
-                label: 'Rename',
-                enabled: !!this.props.onEdit,
-                click: () => this.props.onEditName(),
-              },
-              {
-                label: 'Delete',
-                enabled: !!this.props.onEdit,
-                click: () => this.props.onDelete(),
-              },
-            ]}
-            onClick={() => {
-              // It's essential to discard clicks when editing the name,
-              // to avoid weird opening of an editor when clicking on the
-              // text field.
-              if (!this.props.editingName) this.props.onEdit(group);
-            }}
-          />
+          <I18n>
+            {({ i18n }) => (
+              <ListItem
+                style={{
+                  borderBottom: `1px solid ${muiTheme.listItem.separatorColor}`,
+                  ...style,
+                }}
+                primaryText={label}
+                displayMenuButton
+                buildMenuTemplate={() => [
+                  {
+                    label: i18n._(t`Edit group`),
+                    click: () => this.props.onEdit(group),
+                  },
+                  {
+                    label: i18n._(t`Set as Global group`),
+                    enabled: !this.props.isGlobalGroup,
+                    click: () => this.props.onSetAsGlobalGroup(),
+                    visible: this.props.canSetAsGlobalGroup !== false,
+                  },
+                  {
+                    label: i18n._(t`Rename`),
+                    enabled: !!this.props.onEdit,
+                    click: () => this.props.onEditName(),
+                  },
+                  {
+                    label: i18n._(t`Delete`),
+                    enabled: !!this.props.onEdit,
+                    click: () => this.props.onDelete(),
+                  },
+                ]}
+                onClick={() => {
+                  // It's essential to discard clicks when editing the name,
+                  // to avoid weird opening of an editor when clicking on the
+                  // text field.
+                  if (!this.props.editingName) this.props.onEdit(group);
+                }}
+              />
+            )}
+          </I18n>
         )}
       </ThemeConsumer>
     );

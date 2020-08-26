a447a87697ca842d8b132fbf18c32aca8495b2fc
Bouh <bouh.vivez@gmail.com>
2020-08-26 18:46:22 +0200

    SceneEditor/Toolbar

--- newIDE/app/src/SceneEditor/Toolbar.js
+++ newIDE/app/src/SceneEditor/Toolbar.js
@@ -1,5 +1,7 @@
 // @flow
+import { I18n } from '@lingui/react';
 import { t } from '@lingui/macro';
+
 import React, { PureComponent } from 'react';
 import { ToolbarGroup } from '../UI/Toolbar';
 import ToolbarSeparator from '../UI/ToolbarSeparator';
@@ -54,116 +56,120 @@ export class Toolbar extends PureComponent<Props> {
             this.props.instancesSelection.getSelectedInstances().length !== 0
           }
         />
-        <ToolbarGroup lastChild>
-          <ToolbarIcon
-            onClick={this.props.openObjectsList}
-            src="res/ribbon_default/objects64.png"
-            tooltip={t`Open the objects editor`}
-          />
-          <ToolbarIcon
-            onClick={this.props.openObjectGroupsList}
-            src={'res/ribbon_default/objectsgroups64.png'}
-            tooltip={t`Open the objects groups editor`}
-          />
-          <ToolbarIcon
-            onClick={this.props.openProperties}
-            src="res/ribbon_default/editprop32.png"
-            tooltip={t`Open the properties panel`}
-          />
-          <ToolbarIcon
-            onClick={this.props.toggleInstancesList}
-            src="res/ribbon_default/ObjectsPositionsList32.png"
-            tooltip={t`Open the list of instances`}
-          />
-          <ToolbarIcon
-            onClick={this.props.toggleLayersList}
-            src="res/ribbon_default/layers32.png"
-            tooltip={t`Open the layers editor`}
-          />
-          <ToolbarSeparator />
-          <ToolbarIcon
-            onClick={this.props.undo}
-            src="res/ribbon_default/undo32.png"
-            disabled={!this.props.canUndo}
-            tooltip={t`Undo the last changes`}
-          />
-          <ToolbarIcon
-            onClick={this.props.redo}
-            src="res/ribbon_default/redo32.png"
-            disabled={!this.props.canRedo}
-            tooltip={t`Redo the last changes`}
-          />
-          <ToolbarSeparator />
-          <ToolbarIcon
-            onClick={this.props.deleteSelection}
-            src="res/ribbon_default/deleteselected32.png"
-            disabled={
-              !this.props.instancesSelection.getSelectedInstances().length
-            }
-            tooltip={t`Delete the selected instances from the scene`}
-          />
-          <ToolbarSeparator />
-          <ElementWithMenu
-            element={
+        <I18n>
+          {({ i18n }) => (
+            <ToolbarGroup lastChild>
               <ToolbarIcon
-                src="res/ribbon_default/grid32.png"
-                tooltip={t`Toggle/edit grid`}
+                onClick={this.props.openObjectsList}
+                src="res/ribbon_default/objects64.png"
+                tooltip={t`Open the objects editor`}
               />
-            }
-            buildMenuTemplate={() => [
-              {
-                type: 'checkbox',
-                label: 'Show Mask',
-                checked: this.props.isWindowMaskShown(),
-                click: () => this.props.toggleWindowMask(),
-              },
-              {
-                type: 'checkbox',
-                label: 'Show grid',
-                checked: this.props.isGridShown(),
-                click: () => this.props.toggleGrid(),
-              },
-              { type: 'separator' },
-              {
-                label: 'Setup grid',
-                click: () => this.props.openSetupGrid(),
-              },
-            ]}
-          />
-          <ElementWithMenu
-            element={
               <ToolbarIcon
-                src="res/ribbon_default/zoom32.png"
-                tooltip={t`Change editor zoom`}
+                onClick={this.props.openObjectGroupsList}
+                src={'res/ribbon_default/objectsgroups64.png'}
+                tooltip={t`Open the objects groups editor`}
               />
-            }
-            buildMenuTemplate={() => [
-              {
-                label: 'Zoom in',
-                click: this.props.zoomIn,
-                accelerator: 'CmdOrCtrl+numadd',
-              },
-              {
-                label: 'Zoom out',
-                click: this.props.zoomOut,
-                accelerator: 'CmdOrCtrl+numsub',
-              },
-              {
-                label: 'Center View',
-                click: () => this.props.centerView(),
-              },
-              { type: 'separator' },
-              { label: '5%', click: () => this.props.setZoomFactor(0.05) },
-              { label: '10%', click: () => this.props.setZoomFactor(0.1) },
-              { label: '25%', click: () => this.props.setZoomFactor(0.25) },
-              { label: '50%', click: () => this.props.setZoomFactor(0.5) },
-              { label: '100%', click: () => this.props.setZoomFactor(1.0) },
-              { label: '150%', click: () => this.props.setZoomFactor(1.5) },
-              { label: '200%', click: () => this.props.setZoomFactor(2.0) },
-              { label: '400%', click: () => this.props.setZoomFactor(4.0) },
-            ]}
-          />
-        </ToolbarGroup>
+              <ToolbarIcon
+                onClick={this.props.openProperties}
+                src="res/ribbon_default/editprop32.png"
+                tooltip={t`Open the properties panel`}
+              />
+              <ToolbarIcon
+                onClick={this.props.toggleInstancesList}
+                src="res/ribbon_default/ObjectsPositionsList32.png"
+                tooltip={t`Open the list of instances`}
+              />
+              <ToolbarIcon
+                onClick={this.props.toggleLayersList}
+                src="res/ribbon_default/layers32.png"
+                tooltip={t`Open the layers editor`}
+              />
+              <ToolbarSeparator />
+              <ToolbarIcon
+                onClick={this.props.undo}
+                src="res/ribbon_default/undo32.png"
+                disabled={!this.props.canUndo}
+                tooltip={t`Undo the last changes`}
+              />
+              <ToolbarIcon
+                onClick={this.props.redo}
+                src="res/ribbon_default/redo32.png"
+                disabled={!this.props.canRedo}
+                tooltip={t`Redo the last changes`}
+              />
+              <ToolbarSeparator />
+              <ToolbarIcon
+                onClick={this.props.deleteSelection}
+                src="res/ribbon_default/deleteselected32.png"
+                disabled={
+                  !this.props.instancesSelection.getSelectedInstances().length
+                }
+                tooltip={t`Delete the selected instances from the scene`}
+              />
+              <ToolbarSeparator />
+              <ElementWithMenu
+                element={
+                  <ToolbarIcon
+                    src="res/ribbon_default/grid32.png"
+                    tooltip={t`Toggle/edit grid`}
+                  />
+                }
+                buildMenuTemplate={() => [
+                  {
+                    type: 'checkbox',
+                    label: i18n._(t`Show Mask`),
+                    checked: this.props.isWindowMaskShown(),
+                    click: () => this.props.toggleWindowMask(),
+                  },
+                  {
+                    type: 'checkbox',
+                    label: i18n._(t`Show grid`),
+                    checked: this.props.isGridShown(),
+                    click: () => this.props.toggleGrid(),
+                  },
+                  { type: 'separator' },
+                  {
+                    label: i18n._(t`Setup grid`),
+                    click: () => this.props.openSetupGrid(),
+                  },
+                ]}
+              />
+              <ElementWithMenu
+                element={
+                  <ToolbarIcon
+                    src="res/ribbon_default/zoom32.png"
+                    tooltip={t`Change editor zoom`}
+                  />
+                }
+                buildMenuTemplate={() => [
+                  {
+                    label: i18n._(t`Zoom in`),
+                    click: this.props.zoomIn,
+                    accelerator: 'CmdOrCtrl+numadd',
+                  },
+                  {
+                    label: i18n._(t`Zoom out`),
+                    click: this.props.zoomOut,
+                    accelerator: 'CmdOrCtrl+numsub',
+                  },
+                  {
+                    label: i18n._(t`Center View`),
+                    click: () => this.props.centerView(),
+                  },
+                  { type: 'separator' },
+                  { label: '5%', click: () => this.props.setZoomFactor(0.05) },
+                  { label: '10%', click: () => this.props.setZoomFactor(0.1) },
+                  { label: '25%', click: () => this.props.setZoomFactor(0.25) },
+                  { label: '50%', click: () => this.props.setZoomFactor(0.5) },
+                  { label: '100%', click: () => this.props.setZoomFactor(1.0) },
+                  { label: '150%', click: () => this.props.setZoomFactor(1.5) },
+                  { label: '200%', click: () => this.props.setZoomFactor(2.0) },
+                  { label: '400%', click: () => this.props.setZoomFactor(4.0) },
+                ]}
+              />
+            </ToolbarGroup>
+          )}
+        </I18n>
       </>
     );
   }

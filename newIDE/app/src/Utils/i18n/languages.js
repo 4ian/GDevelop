7f470df1650ddc2e0ccac678b9ac1c2f8d032a80
denim2x <denim2x@cyberdude.com>
2020-08-27 18:55:21 +0300

    Add lingui adapter to i18n module in Utils
    Signed-off-by: denim2x <denim2x@cyberdude.com>

---
+++ newIDE/app/src/Utils/i18n/languages.js
@@ -0,0 +1,24 @@
+export const rtl = new Set([
+  /******* Hebrew scripts *******/
+  'he'/* Hebrew */,
+  'ji'/* Yiddish */,
+  'yi'/* Yiddish */,
+  
+  /******* Arabic scripts *******/
+  'ar'/* Arabic */,
+  'az'/* Azerbaijani */,
+  'jv'/* Javanese */,
+  'ks'/* Kashmiri */,
+  'kk'/* Kazakh */,
+  'ku'/* Kurdish */,
+  'ms'/* Malay */,
+  'ml'/* Malayalam */,
+  'ps'/* Pashto */,
+  'fa'/* Persian */,
+  'pa'/* Punjabi */,
+  'sd'/* Sindhi */,
+  'so'/* Somali */,
+  'tk'/* Turkmen */,
+  'ug'/* Uighur */,
+  'ur'/* Urdu */,
+]);

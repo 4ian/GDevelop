7fe9ffcbed863fab4419d3e72e607a001c0b9e8a
Florian Rival <Florian.rival@gmail.com>
2020-08-30 18:38:58 +0100

    Update bitmaptextruntimeobject-pixi-renderer.js

--- Extensions/BitmapText/bitmaptextruntimeobject-pixi-renderer.js
+++ Extensions/BitmapText/bitmaptextruntimeobject-pixi-renderer.js
@@ -6,53 +6,30 @@
  * @param {gdjs.BitmapTextRuntimeObject} runtimeObject The object to render
  * @param {gdjs.RuntimeScene} runtimeScene The gdjs.RuntimeScene in which the object is
  */
-gdjs.BitmapTextRuntimeObjectPixiRenderer = function(
+gdjs.BitmapTextRuntimeObjectPixiRenderer = function (
   runtimeObject,
   runtimeScene
 ) {
   this._object = runtimeObject;
 
-  const style = new PIXI.TextStyle();
-  style.fontFamily = runtimeScene
+  // Set up the object to track the font we're using.
+  this._bitmapFontStyle = new PIXI.TextStyle();
+  this._bitmapFontStyle.fontFamily = runtimeScene
     .getGame()
     .getFontManager()
-    .getFontFamily(runtimeObject._fontFamily);
-  style.fontSize = runtimeObject._fontSize;
-  style.wordWrap = runtimeObject._wordWrap;
-  style.wordWrapWidth= runtimeObject._wrappingWidth;
-  style.fill = runtimeObject._color;
-
-  const slugFontName =
-    style.fontFamily + '-' + style.fontSize + '-' + style.fill + '-bitmapFont';
-
-  if (!PIXI.BitmapFont.available[slugFontName]) {
-    PIXI.BitmapFont.from(slugFontName, style, {
-      chars: PIXI.BitmapFont.ASCII,
-      textureWidth: 1024,
-    });
-  }
+    .getFontFamily(runtimeObject._fontResourceName);
+  this._bitmapFontStyle.fontSize = runtimeObject._fontSize;
+  this._bitmapFontStyle.fill = runtimeObject._fontColor;
 
   // Load (or reset) the text
   if (this._pixiObject === undefined) {
     this._pixiObject = new PIXI.BitmapText('BitmapText object', {
-      fontName: slugFontName,
+      fontName: this._ensureFontAvailableAndGetFontName(),
     });
-
-    this._pixiObject.align = runtimeObject._align;
-
-    this.style = style;
-    this.constructorSlugFontName = slugFontName;
   } else {
-    this.updateColor();
-    this.updateAlignment();
-    this.updateFontFamily();
+    this.updateFont();
     this.updateFontSize();
-    this.updateWordWrap();
-    this.updateWrappingWidth();
   }
-
-  // update BitmapText with new styles
-  this._pixiObject.updateText();
 
   runtimeScene
     .getLayer('')
@@ -60,88 +37,104 @@ gdjs.BitmapTextRuntimeObjectPixiRenderer = function(
     .addRendererObject(this._pixiObject, runtimeObject.getZOrder());
 
   // Set the anchor in the center, so that the object rotates around
-  // its center
+  // its center.
   this._pixiObject.anchor.x = 0.5;
   this._pixiObject.anchor.y = 0.5;
 
+  this.updateAlignment();
   this.updateTextContent();
   this.updatePosition();
   this.updateAngle();
   this.updateOpacity();
+  this.updateWrappingWidth();
 };
 
 gdjs.BitmapTextRuntimeObjectRenderer = gdjs.BitmapTextRuntimeObjectPixiRenderer;
 
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.getRendererObject = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.getRendererObject = function () {
   return this._pixiObject;
 };
 
-// REVIEW comparé le wrap avec celui de l'IDE
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateWordWrap = function() {
-  this._pixiObject.style.wordWrap = this._object._wordWrap;
-  this._pixiObject.dirty = true;
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype._ensureFontAvailableAndGetFontName = function () {
+  const slugFontName =
+    this._bitmapFontStyle.fontFamily +
+    '-' +
+    this._bitmapFontStyle.fontSize +
+    '-' +
+    this._bitmapFontStyle.fill +
+    '-bitmapFont';
+
+  // Load the font if it's not available yet.
+  if (!PIXI.BitmapFont.available[slugFontName]) {
+    console.info('Generating font "' + slugFontName + '" for BitmapText.');
+    PIXI.BitmapFont.from(slugFontName, this._bitmapFontStyle, {
+      chars: PIXI.BitmapFont.ASCII,
+      textureWidth: 1024,
+    });
+  }
+
+  // TODO: find a way to unload the BitmapFont that are not used anymore, otherwise
+  // we risk filling up the memory with useless BitmapFont - especially when the user
+  // plays with the color/size.
+
+  return slugFontName;
 };
 
-// REVIEW comparé le wrap avec celui de l'IDE
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateWrappingWidth = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateColor = function () {
+  this._bitmapFontStyle.fill = this._object._fontColor;
+  this._pixiObject.fontName = this._ensureFontAvailableAndGetFontName();
+};
+
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateFont = function () {
+  this._bitmapFontStyle.fontFamily = this._object._runtimeScene
+    .getGame()
+    .getFontManager()
+    .getFontFamily(this._object._fontResourceName);
+  this._pixiObject.fontName = this._ensureFontAvailableAndGetFontName();
+};
+
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateFontSize = function () {
+  this._bitmapFontStyle.fontSize = this._object._fontSize;
+  this._pixiObject.fontSize = this._object._fontSize;
+  this._pixiObject.fontName = this._ensureFontAvailableAndGetFontName();
+};
+
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateWrappingWidth = function () {
   if (this._object._wordWrap) {
     this._pixiObject.maxWidth = this._object._wrappingWidth;
   } else {
     this._pixiObject.maxWidth = 0;
   }
-  this._pixiObject.dirty = true;
+  this.updatePosition();
 };
 
-// REVIEW  text - seem OK
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateTextContent = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateTextContent = function () {
   this._pixiObject.text = this._object._text;
   this.updatePosition();
 };
 
-// REVIEW fill - seem OK
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateColor = function() {
-  this.style.fill = this._object._color;
-  this._pixiObject.dirty = true;
-};
-
-// REVIEW align - seem OK
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateAlignment = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateAlignment = function () {
   this._pixiObject.align = this._object._align;
 };
 
-// REVIEW fontFamily - seem OK
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateFontFamily = function() {
-  this.style.fontFamily = this._object._runtimeScene
-    .getGame()
-    .getFontManager()
-    .getFontFamily(this._object._fontFamily);
-  this._pixiObject.dirty = true;
-};
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateFontSize = function() {
-  this.style.fontSize = this._object._fontSize;
-  this._pixiObject.fontSize = this._object._fontSize;
-  this._pixiObject.dirty = true;
-};
-
-// FIXME position et placement correspondent pas à l'IDE
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updatePosition = function() {
-  this._pixiObject.position.x = this._object.x + this._pixiObject.width / 2;
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updatePosition = function () {
+  // Note: use `textWidth` as `width` seems unreliable.
+  this._pixiObject.position.x = this._object.x + this._pixiObject.textWidth / 2;
   this._pixiObject.position.y = this._object.y + this._pixiObject.height / 2;
 };
 
-// REVIEW angle
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateAngle = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateAngle = function () {
   this._pixiObject.rotation = gdjs.toRad(this._object.angle);
 };
 
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateOpacity = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.updateOpacity = function () {
   this._pixiObject.alpha = this._object._opacity / 255;
 };
 
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.getWidth = function() {
-  return this._pixiObject.width;
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.getWidth = function () {
+  return this._pixiObject.textWidth;
 };
 
-gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.getHeight = function() {
+gdjs.BitmapTextRuntimeObjectPixiRenderer.prototype.getHeight = function () {
   return this._pixiObject.height;
 };

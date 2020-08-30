4a56815e614e6e6f16dcda770a450a71b150dc0a
Florian Rival <Florian.rival@gmail.com>
2020-08-30 18:39:23 +0100

    Update bitmaptextruntimeobject.js

--- Extensions/BitmapText/bitmaptextruntimeobject.js
+++ Extensions/BitmapText/bitmaptextruntimeobject.js
@@ -2,11 +2,10 @@
  * @typedef {Object} BitmapTextObjectDataType Base parameters for {@link gdjs.BitmapTextRuntimeObject}
  * @property {Object} content The base parameters of the BitmapText
  * @property {number} content.opacity The opacity of the BitmapText
- * @property {boolean} content.visible Deprecated - Is the text visible?
  * @property {string} content.text Content of the text
- * @property {string} content.color The color of the text
- * @property {string} content.fontFamily The font of the text
- * @property {number} content.fontSize The size of the text
+ * @property {string} content.fontColor The color of the font of the text
+ * @property {string} content.fontResourceName The font of the text
+ * @property {number} content.fontSize The size of the font of the text
  * @property {boolean} content.wordWrap Activate word wrap if set to true
  * @property {('left'|'center'|'right')} content.align Alignment of the text: "left", "center" or "right"
  *
@@ -14,28 +13,29 @@
  */
 
 /**
- * Displays a rich text using BBCode markup (allowing to set parts of the text as bold, italic, use different colors and shadows).
+ * Displays a text using a "Bitmap Font", which is more performant than a traditional text,
+ * with the disadvantage of being less flexible (a font must be generated in memory for each font, font
+ * size and color) and only supports ASCII characters.
+ *
  * @memberof gdjs
  * @class BitmapTextRuntimeObject
  * @extends RuntimeObject
  * @param {gdjs.RuntimeScene} runtimeScene The {@link gdjs.RuntimeScene} the object belongs to
  * @param {BitmapTextObjectData} objectData The object data used to initialize the object
  */
-gdjs.BitmapTextRuntimeObject = function(runtimeScene, objectData) {
+gdjs.BitmapTextRuntimeObject = function (runtimeScene, objectData) {
   gdjs.RuntimeObject.call(this, runtimeScene, objectData);
 
   /** @type {number} */
-  this._opacity = parseFloat(objectData.content.opacity);
-  // parseFloat should not be required, but GDevelop 5.0 beta 92 and below were storing it as a string.
+  this._opacity = objectData.content.opacity;
   /** @type {string} */
   this._text = objectData.content.text;
   /** @type {string} */
-  this._color = objectData.content.color;
+  this._fontColor = objectData.content.fontColor;
   /** @type {string} */
-  this._fontFamily = objectData.content.fontFamily;
+  this._fontResourceName = objectData.content.fontResourceName;
   /** @type {number} */
-  this._fontSize = parseFloat(objectData.content.fontSize);
-  // parseFloat should not be required, but GDevelop 5.0 beta 92 and below were storing it as a string.
+  this._fontSize = objectData.content.fontSize;
   /** @type {boolean} */
   this._wordWrap = objectData.content.wordWrap;
   /** @type {number} */
@@ -55,10 +55,6 @@ gdjs.BitmapTextRuntimeObject = function(runtimeScene, objectData) {
       runtimeScene
     );
 
-  // While this should rather be exposed as a property for all objects, honor the "visible"
-  // property that is specific to this object.
-  this.hidden = !objectData.content.visible;
-
   // *ALWAYS* call `this.onCreated()` at the very end of your object constructor.
   this.onCreated();
 };
@@ -68,7 +64,7 @@ gdjs.BitmapTextRuntimeObject.prototype = Object.create(
 );
 gdjs.registerObject('BitmapText::BitmapText', gdjs.BitmapTextRuntimeObject);
 
-gdjs.BitmapTextRuntimeObject.prototype.getRendererObject = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getRendererObject = function () {
   return this._renderer.getRendererObject();
 };
 
@@ -76,31 +72,29 @@ gdjs.BitmapTextRuntimeObject.prototype.getRendererObject = function() {
  * @param {BitmapTextObjectDataType} oldObjectData
  * @param {BitmapTextObjectDataType} newObjectData
  */
-gdjs.BitmapTextRuntimeObject.prototype.updateFromObjectData = function(
+gdjs.BitmapTextRuntimeObject.prototype.updateFromObjectData = function (
   oldObjectData,
   newObjectData
 ) {
   if (oldObjectData.content.opacity !== newObjectData.content.opacity) {
     this.setOpacity(newObjectData.content.opacity);
   }
-  if (oldObjectData.content.visible !== newObjectData.content.visible) {
-    this.hide(!newObjectData.content.visible);
-  }
   if (oldObjectData.content.text !== newObjectData.content.text) {
-    this.setBitmapText(newObjectData.content.text);
+    this.setText(newObjectData.content.text);
   }
-  if (oldObjectData.content.color !== newObjectData.content.color) {
-    this._color = newObjectData.content.color;
+  if (oldObjectData.content.fontColor !== newObjectData.content.fontColor) {
+    this._fontColor = newObjectData.content.fontColor;
     this._renderer.updateColor();
   }
-
-  if (oldObjectData.content.fontFamily !== newObjectData.content.fontFamily) {
-    this.setFontFamily(newObjectData.content.fontFamily);
+  if (
+    oldObjectData.content.fontResourceName !==
+    newObjectData.content.fontResourceName
+  ) {
+    this.setFontFamily(newObjectData.content.fontResourceName);
   }
   if (oldObjectData.content.fontSize !== newObjectData.content.fontSize) {
     this.setFontSize(newObjectData.content.fontSize);
   }
-
   if (oldObjectData.content.wordWrap !== newObjectData.content.wordWrap) {
     this.setWordWrap(newObjectData.content.wordWrap);
   }
@@ -115,40 +109,38 @@ gdjs.BitmapTextRuntimeObject.prototype.updateFromObjectData = function(
  * Initialize the extra parameters that could be set for an instance.
  * @private
  */
-gdjs.BitmapTextRuntimeObject.prototype.extraInitializationFromInitialInstance = function(
+gdjs.BitmapTextRuntimeObject.prototype.extraInitializationFromInitialInstance = function (
   initialInstanceData
 ) {
   if (initialInstanceData.customSize) {
     this.setWrappingWidth(initialInstanceData.width);
-  } else {
-    this.setWrappingWidth(250); // This value is the default wrapping width of the runtime object.
   }
-  this.setX(initialInstanceData.x);
-  this.setY(initialInstanceData.y);
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.onDestroyFromScene = function(
+gdjs.BitmapTextRuntimeObject.prototype.onDestroyFromScene = function (
   runtimeScene
 ) {
+  // TODO: mark the font as not used anymore in the "BitmapFont manager".
+
   gdjs.RuntimeObject.prototype.onDestroyFromScene.call(this, runtimeScene);
 };
 
 /**
  * Set the text to display.
  */
-gdjs.BitmapTextRuntimeObject.prototype.setBitmapText = function(text) {
+gdjs.BitmapTextRuntimeObject.prototype.setText = function (text) {
   this._text = text;
-  this._renderer.updateText();
+  this._renderer.updateTextContent();
 };
 
 /**
  * Get the text displayed by the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.getBitmapText = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getText = function () {
   return this._text;
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.setColor = function(rgbColorString) {
+gdjs.BitmapTextRuntimeObject.prototype.setColor = function (rgbColorString) {
   const splitValue = rgbColorString.split(';');
   if (splitValue.length !== 3) return;
   const hexColor =
@@ -158,38 +150,40 @@ gdjs.BitmapTextRuntimeObject.prototype.setColor = function(rgbColorString) {
       parseInt(splitValue[1], 0),
       parseInt(splitValue[2], 0)
     );
-  this._color = hexColor;
+  this._fontColor = hexColor;
   this._renderer.updateColor();
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.getColor = function() {
-  return this._color;
+gdjs.BitmapTextRuntimeObject.prototype.getColor = function () {
+  return this._fontColor;
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.setFontSize = function(fontSize) {
+gdjs.BitmapTextRuntimeObject.prototype.setFontSize = function (fontSize) {
   this._fontSize = fontSize;
   this._renderer.updateFontSize();
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.getFontSize = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getFontSize = function () {
   return this._fontSize;
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.setFontFamily = function(fontFamily) {
-  this._fontFamily = fontFamily;
-  this._renderer.updateFontFamily();
+gdjs.BitmapTextRuntimeObject.prototype.setFontResourceName = function (
+  fontResourceName
+) {
+  this._fontResourceName = fontResourceName;
+  this._renderer.updateFont();
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.getFontFamily = function() {
-  return this._fontFamily;
+gdjs.BitmapTextRuntimeObject.prototype.getFontResourceName = function () {
+  return this._fontResourceName;
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.setAlignment = function(align) {
+gdjs.BitmapTextRuntimeObject.prototype.setAlignment = function (align) {
   this._align = align;
   this._renderer.updateAlignment();
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.getAlignment = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getAlignment = function () {
   return this._align;
 };
 
@@ -197,7 +191,7 @@ gdjs.BitmapTextRuntimeObject.prototype.getAlignment = function() {
  * Set object position on X axis.
  * @param {number} x The new position X of the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.setX = function(x) {
+gdjs.BitmapTextRuntimeObject.prototype.setX = function (x) {
   gdjs.RuntimeObject.prototype.setX.call(this, x);
   this._renderer.updatePosition();
 };
@@ -206,7 +200,7 @@ gdjs.BitmapTextRuntimeObject.prototype.setX = function(x) {
  * Set object position on Y axis.
  * @param {number} y The new position Y of the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.setY = function(y) {
+gdjs.BitmapTextRuntimeObject.prototype.setY = function (y) {
   gdjs.RuntimeObject.prototype.setY.call(this, y);
   this._renderer.updatePosition();
 };
@@ -215,7 +209,7 @@ gdjs.BitmapTextRuntimeObject.prototype.setY = function(y) {
  * Set the angle of the object.
  * @param {number} angle The new angle of the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.setAngle = function(angle) {
+gdjs.BitmapTextRuntimeObject.prototype.setAngle = function (angle) {
   gdjs.RuntimeObject.prototype.setAngle.call(this, angle);
   this._renderer.updateAngle();
 };
@@ -224,7 +218,7 @@ gdjs.BitmapTextRuntimeObject.prototype.setAngle = function(angle) {
  * Set object opacity.
  * @param {number} opacity The new opacity of the object (0-255).
  */
-gdjs.BitmapTextRuntimeObject.prototype.setOpacity = function(opacity) {
+gdjs.BitmapTextRuntimeObject.prototype.setOpacity = function (opacity) {
   this._opacity = opacity;
   this._renderer.updateOpacity();
 };
@@ -232,7 +226,7 @@ gdjs.BitmapTextRuntimeObject.prototype.setOpacity = function(opacity) {
 /**
  * Get object opacity.
  */
-gdjs.BitmapTextRuntimeObject.prototype.getOpacity = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getOpacity = function () {
   return this._opacity;
 };
 
@@ -240,7 +234,7 @@ gdjs.BitmapTextRuntimeObject.prototype.getOpacity = function() {
  * Set the width.
  * @param {number} width The new width in pixels.
  */
-gdjs.BitmapTextRuntimeObject.prototype.setWrappingWidth = function(width) {
+gdjs.BitmapTextRuntimeObject.prototype.setWrappingWidth = function (width) {
   this._wrappingWidth = width;
   this._renderer.updateWrappingWidth();
 };
@@ -248,29 +242,29 @@ gdjs.BitmapTextRuntimeObject.prototype.setWrappingWidth = function(width) {
 /**
  * Get the wrapping width of the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.getWrappingWidth = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getWrappingWidth = function () {
   return this._wrappingWidth;
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.setWordWrap = function(wordWrap) {
+gdjs.BitmapTextRuntimeObject.prototype.setWordWrap = function (wordWrap) {
   this._wordWrap = wordWrap;
-  this._renderer.updateWordWrap();
+  this._renderer.updateWrappingWidth();
 };
 
-gdjs.BitmapTextRuntimeObject.prototype.getWordWrap = function(wordWrap) {
+gdjs.BitmapTextRuntimeObject.prototype.getWordWrap = function (wordWrap) {
   return this._wordWrap;
 };
 
 /**
  * Get the width of the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.getWidth = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getWidth = function () {
   return this._renderer.getWidth();
 };
 
 /**
  * Get the height of the object.
  */
-gdjs.BitmapTextRuntimeObject.prototype.getHeight = function() {
+gdjs.BitmapTextRuntimeObject.prototype.getHeight = function () {
   return this._renderer.getHeight();
 };

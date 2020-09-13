ef1bd161c80554390ff5d86408decd0ce73e05b1
arthuro555 <arthur.pacaud@hotmail.fr>
2020-09-13 19:51:54 +0200

    Remove capitalisation mistakes

--- Extensions/Firebase/C_firebasetools/D_analyticstools.js
+++ Extensions/Firebase/C_firebasetools/D_analyticstools.js
@@ -11,9 +11,9 @@
 gdjs.evtTools.firebase.analytics = {};
 
 /**
- * Logs an Event/Conversion for that user on the Analytics.
- * @param {string} eventName The Event being triggered.
- * @param {string} [eventData] Additional Data for the event.
+ * Logs an event/conversion for that user on the analytics.
+ * @param {string} eventName The event being triggered.
+ * @param {string | Object} [eventData] Additional data for the event.
  */
 gdjs.evtTools.firebase.analytics.log = function (eventName, eventData) {
   let analytics = gdjs.evtTools.firebase.analytics._analyticsInstance;
@@ -29,9 +29,9 @@ gdjs.evtTools.firebase.analytics.log = function (eventName, eventData) {
 };
 
 /**
- * Sets the User ID (The name under wich the user will appear on the analytics).
- * Should be Unique if possible.
- * @param {string | number} newUID The new UserID.
+ * Sets the User ID (the name under wich the user will appear on the analytics).
+ * Should be unique if possible.
+ * @param {string | number} newUID The new User ID.
  */
 gdjs.evtTools.firebase.analytics.setUserID = function (newUID) {
   let analytics = gdjs.evtTools.firebase.analytics._analyticsInstance;
@@ -41,7 +41,7 @@ gdjs.evtTools.firebase.analytics.setUserID = function (newUID) {
 /**
  * Set an user's property.
  * @param {string} propertyName The property's name.
- * @param {string} [propertyData] The data associated to the property.
+ * @param {string | Object} [propertyData] The data associated to the property.
  */
 gdjs.evtTools.firebase.analytics.setProperty = function (
   propertyName,
@@ -62,7 +62,7 @@ gdjs.registerFirstRuntimeSceneLoadedCallback(function () {
   gdjs.evtTools.firebase.analytics._analyticsInstance = firebase.analytics();
 });
 
-/** Callback For Setting the analytics current View to the current Scene */
+/** Callback for setting the analytics current view to the current scene */
 gdjs.registerRuntimeSceneLoadedCallback(function (runtimeScene) {
   if (runtimeScene.getGame()._analyticsInstance)
     runtimeScene

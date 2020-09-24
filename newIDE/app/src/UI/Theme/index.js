8a5351b0d9f2d7b062c296da5cb5540d9a9fd4a9
denim2x <denim2x@cyberdude.com>
2020-09-24 11:52:12 +0300

    Add RTL functionality to module `Theme`
    Refactor `getTheme()`: use `convertToRtl()` to obtain new `muiTheme` instance based on language directionality

--- newIDE/app/src/UI/Theme/index.js
+++ newIDE/app/src/UI/Theme/index.js
@@ -1,9 +1,12 @@
 // @flow
+import { isLtr } from '../../Utils/i18n/RtlLanguages';
+import DefaultTheme, {
+  themeName as defaultThemeName,
+  type Theme,
+} from './DefaultTheme';
 import DarkTheme from './DarkTheme';
-import DefaultTheme from './DefaultTheme';
 import NordTheme from './NordTheme';
 import SolarizedDarkTheme from './SolarizedDarkTheme';
-import { type Theme } from './DefaultTheme';
 import './Global.css';
 
 // To add a new theme:
@@ -11,13 +14,31 @@ import './Global.css';
 // * import it at the top of the file
 // * add it below:
 export const themes = {
-  'GDevelop default': DefaultTheme,
+  [defaultThemeName]: DefaultTheme,
   Dark: DarkTheme,
   Nord: NordTheme,
   'Solarized Dark': SolarizedDarkTheme,
 };
 
-export const getTheme = (themeName: string): Theme =>
-  themes[themeName] || themes['GDevelop default'];
+export function getTheme({ themeName, language }: {| themeName: string, language: string |}): Theme {
+  // Get `Theme` associated to `themeName`
+  let theme = themes[themeName];
+
+  // If unavailable, give notice in the console and revert to default `Theme`
+  if (!theme) {
+    console.warn(`Theme '${themeName}' is unavailable; '${defaultThemeName}' is used`);
+    theme = themes[defaultThemeName];
+  }
+
+  const { gdevelopTheme, muiTheme } = theme;
+  return { gdevelopTheme, muiTheme: convertToRtl(muiTheme, language) };  
+};
 
 export type GDevelopTheme = $PropertyType<Theme, 'gdevelopTheme'>;
+
+type MuiTheme = $Exact<typeof DefaultTheme.muiTheme>;
+
+function convertToRtl(muiTheme: MuiTheme, language: string): MuiTheme {
+  const ltr = isLtr(language);
+  return { ...muiTheme, direction: ltr ? 'unset' : 'rtl' };
+}

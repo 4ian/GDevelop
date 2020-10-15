4cb9509a4551f0d6630c326416453b6519d6bc31
arthuro555 <arthur.pacaud@hotmail.fr>
2020-10-15 13:58:06 +0200

    Rerender ResponsiveWindowMeasurer on window resize

--- newIDE/app/src/UI/Reponsive/ResponsiveWindowMeasurer.js
+++ newIDE/app/src/UI/Reponsive/ResponsiveWindowMeasurer.js
@@ -7,12 +7,30 @@ type Props = {|
   children: (width: WidthType) => React.Node,
 |};
 
+const useForceUpdate = () => {
+  const [value, setValue] = React.useState(true);
+  return () => setValue(value => !value);
+}
+
 /**
  * Pass the proper size to the children according to the window size.
  * This consider a window to be "small" if *both* the width and height
  * are small.
  */
 export const ResponsiveWindowMeasurer = ({ children }: Props) => {
+  const forceUpdate = useForceUpdate();
+
+  React.useEffect(() => {
+    // Use timeouts to only rerender when the resize is finished.
+    let timeout;
+    const listener = window.addEventListener("resize", () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(forceUpdate, 200);
+    });
+
+    return () => window.removeEventListener("resize", listener);
+  })
+
   if (typeof window === 'undefined') {
     return children('medium');
   }

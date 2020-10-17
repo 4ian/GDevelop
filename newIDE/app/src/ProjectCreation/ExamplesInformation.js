b9be2852e77085101182005e17fe929098031863
Bouh <bouh.vivez@gmail.com>
2020-10-17 15:57:37 +0200

    Generate Fixtures

--- newIDE/app/src/ProjectCreation/ExamplesInformation.js
+++ newIDE/app/src/ProjectCreation/ExamplesInformation.js
@@ -309,7 +309,7 @@ module.exports = {
     ]
   },
   "bbtext-object-demo": {
-    "description": "This shows how to change the bbcode text of a BBText object displayed on the screen.\n",
+    "description": "This shows how to change the bbcode text of a BBText object displayed on the screen.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -419,7 +419,7 @@ module.exports = {
     ]
   },
   "board-walk-with-raycast": {
-    "description": "Example showing how to walk on a tile-based arbitrary board using the Raycast condition.\n",
+    "description": "Example showing how to walk on a tile-based arbitrary board using the Raycast condition.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -542,7 +542,7 @@ module.exports = {
     ]
   },
   "breakout": {
-    "description": "The classic Breakout arcade game adapted in GDevelop. Read the tutorial about it to learn how to make a game like this.\n",
+    "description": "The classic Breakout arcade game adapted in GDevelop. Read the tutorial about it to learn how to make a game like this.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -620,7 +620,7 @@ module.exports = {
     ]
   },
   "cannon-ball-with-physics": {
-    "description": "An example showing how to launch a ball from a cannon when using the Physics engine.\n",
+    "description": "An example showing how to launch a ball from a cannon when using the Physics engine.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -892,7 +892,7 @@ module.exports = {
     ]
   },
   "count-down-timer": {
-    "description": "Show how to use a variable as a timer and display it on screen like a bomb count down timer.\n",
+    "description": "Show how to use a variable as a timer and display it on screen like a bomb count down timer.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -934,7 +934,7 @@ module.exports = {
     ]
   },
   "create-random-object": {
-    "description": "Show how to create a object randomly picked in a list.\n",
+    "description": "Show how to create a object randomly picked in a list.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -976,7 +976,7 @@ module.exports = {
     ]
   },
   "custom-mouse-pointer": {
-    "description": "Learn how to replace the mouse cursor by a custom image.\n",
+    "description": "Learn how to replace the mouse cursor by a custom image.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -1113,7 +1113,7 @@ module.exports = {
     ]
   },
   "dialogue-tree-with-yarn": {
-    "description": "A platformer showing a simple interactive story with dialogues like in RPG games, using the Yarn dialogue tree extension.\n",
+    "description": "A platformer showing a simple interactive story with dialogues like in RPG games, using the Yarn dialogue tree extension.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -1428,7 +1428,7 @@ module.exports = {
     ]
   },
   "filesystem-create-directory": {
-    "description": "Show how to ask the user for a folder name and create it on the disk.\n",
+    "description": "Show how to ask the user for a folder name and create it on the disk.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -1502,7 +1502,7 @@ module.exports = {
     ]
   },
   "flappy-bird": {
-    "description": "A clone of the challenging and popular Flappy Bird game, where you control a bird which has to avoid pipes.\n",
+    "description": "A clone of the challenging and popular Flappy Bird game, where you control a bird which has to avoid pipes.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -1571,7 +1571,7 @@ module.exports = {
     ]
   },
   "game-feel-demo": {
-    "description": "A demo showing how to enhance the \"game feel\" of your project: VFX, shot trail, ambient sounds and SFX, screenshake, wobble...\n",
+    "description": "A demo showing how to enhance the \"game feel\" of your project: VFX, shot trail, ambient sounds and SFX, screenshake, wobble...\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -1681,7 +1681,7 @@ module.exports = {
     ]
   },
   "geometry-monster": {
-    "description": "A hyper casual endless game where you have to collect shapes and avoid bombs, with a progressively increasing difficulty.\n",
+    "description": "A hyper casual endless game where you have to collect shapes and avoid bombs, with a progressively increasing difficulty.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -2045,7 +2045,7 @@ module.exports = {
     ]
   },
   "level-editor": {
-    "description": "Example showing a basic level editor, where you can save and load positions of objects put on the scene during the game. Also shows how to use simple functions.\n",
+    "description": "Example showing a basic level editor, where you can save and load positions of objects put on the scene during the game. Also shows how to use simple functions.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -2106,7 +2106,7 @@ module.exports = {
     ]
   },
   "level-selection-unlock": {
-    "description": "Show how to save user progress and progressively unlock levels.\n",
+    "description": "Show how to save user progress and progressively unlock levels.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -2139,7 +2139,7 @@ module.exports = {
     ]
   },
   "lights": {
-    "description": "Example showing the usage of multiple light objects, setting other objects as light obstacles and using a slight blur effect to soften shadows.\n",
+    "description": "Example showing the usage of multiple light objects, setting other objects as light obstacles and using a slight blur effect to soften shadows.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -2251,7 +2251,7 @@ module.exports = {
     ]
   },
   "menu-with-functions-and-text-effects": {
-    "description": "How to implement the same menu across multiple scenes using functions and text effects for hover/active states.\n",
+    "description": "How to implement the same menu across multiple scenes using functions and text effects for hover/active states.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -2305,7 +2305,7 @@ module.exports = {
     ]
   },
   "move-object-back-and-forth": {
-    "description": "Use events to move objects back and forth vertically or horizontally.\n",
+    "description": "Use events to move objects back and forth vertically or horizontally.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -2438,7 +2438,7 @@ module.exports = {
     ]
   },
   "multi-language-menu": {
-    "description": "Show how to translate a menu (titles, buttons) in multiple languages and add an option for the player to choose the language.\n",
+    "description": "Show how to translate a menu (titles, buttons) in multiple languages and add an option for the player to choose the language.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -2471,7 +2471,7 @@ module.exports = {
     ]
   },
   "multiplayer-platformer-with-gamepads": {
-    "description": "Example showing how to use the Gamepads extension to read the state of gamepads (or controllers), for 2 players.\n",
+    "description": "Example showing how to use the Gamepads extension to read the state of gamepads (or controllers), for 2 players.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -2714,7 +2714,7 @@ module.exports = {
     ]
   },
   "pairs": {
-    "description": "Find all matching pairs of cards in this relaxing game. Use tweens to create smooth, natural animations with a few events.\n",
+    "description": "Find all matching pairs of cards in this relaxing game. Use tweens to create smooth, natural animations with a few events.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -2875,7 +2875,7 @@ module.exports = {
     ]
   },
   "particle-effects-demo": {
-    "description": "A demo of various high quality particle effects (fire, magic, snow, rune spell...) that you can try and use in your game.\n",
+    "description": "A demo of various high quality particle effects (fire, magic, snow, rune spell...) that you can try and use in your game.\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -3308,10 +3308,6 @@ module.exports = {
       {
         "fullName": "Features for all objects",
         "name": "BuiltinObject"
-      },
-      {
-        "fullName": "System information",
-        "name": "SystemInfo"
       },
       {
         "fullName": "Audio",
@@ -4002,7 +3998,7 @@ module.exports = {
     ]
   },
   "space-shooter": {
-    "description": "A beautiful, retro side-scrolling shooter where you must defeat incoming enemies with your mecha transforming spaceship. Huge boss included!\n",
+    "description": "A beautiful, retro side-scrolling shooter where you must defeat incoming enemies with your mecha transforming spaceship. Huge boss included!\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",
@@ -4101,7 +4097,7 @@ module.exports = {
     ]
   },
   "tabbed-menu-with-layers": {
-    "description": "Example showing how to create a menu with multiple tabs.\n",
+    "description": "Example showing how to create a menu with multiple tabs.\r\n",
     "usedExtensions": [
       {
         "fullName": "Text object",
@@ -4370,7 +4366,7 @@ module.exports = {
     ]
   },
   "unlock-items": {
-    "description": "Show how to save the items unlocked by the user and load them later (when the game is launched again for example, or when starting a saved game).\n",
+    "description": "Show how to save the items unlocked by the user and load them later (when the game is launched again for example, or when starting a saved game).\r\n",
     "usedExtensions": [
       {
         "fullName": "Sprite",

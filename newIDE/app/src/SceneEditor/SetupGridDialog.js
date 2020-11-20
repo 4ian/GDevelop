346eed37795313d667080953331d13d9eec6fc63
Florian Rival <Florian.Rival@gmail.com>
2020-11-20 09:10:36 +0000

    Improve UI of the dialog to edit the grid of the scene editor
    Don't show in changelog

--- newIDE/app/src/SceneEditor/SetupGridDialog.js
+++ newIDE/app/src/SceneEditor/SetupGridDialog.js
@@ -2,7 +2,7 @@ import { Trans } from '@lingui/macro';
 import React, { Component } from 'react';
 import FlatButton from '../UI/FlatButton';
 import TextField from '../UI/TextField';
-import { ResponsiveLineStackLayout } from '../UI/Layout';
+import { ColumnStackLayout, ResponsiveLineStackLayout } from '../UI/Layout';
 import Dialog from '../UI/Dialog';
 import ColorField from '../UI/ColorField';
 
@@ -43,56 +43,66 @@ export default class SetupGridDialog extends Component {
 
     return (
       <Dialog
+        title={<Trans>Edit Grid Options</Trans>}
         actions={actions}
         cannotBeDismissed={true}
         open={this.props.open}
         onRequestClose={this.props.onCancel}
+        maxWidth="sm"
+        noMargin
       >
-        <ResponsiveLineStackLayout>
-          <ColorField
-            floatingLabelText={<Trans>Line color</Trans>}
-            color={this.state.gridColor}
-            onChangeComplete={color => {
-              this.setState({ gridColor: color.rgb });
-            }}
-          />
-        </ResponsiveLineStackLayout>
-        <ResponsiveLineStackLayout>
-          <TextField
-            floatingLabelText={<Trans>Cell width (in pixels)</Trans>}
-            type="number"
-            value={this.state.gridWidth}
-            onChange={(e, value) =>
-              this.setState({ gridWidth: parseInt(value, 10) })
-            }
-          />
-          <TextField
-            floatingLabelText={<Trans>Cell height (in pixels)</Trans>}
-            type="number"
-            value={this.state.gridHeight}
-            onChange={(e, value) =>
-              this.setState({ gridHeight: parseInt(value, 10) })
-            }
-          />
-        </ResponsiveLineStackLayout>
-        <ResponsiveLineStackLayout>
-          <TextField
-            floatingLabelText={<Trans>X offset (in pixels)</Trans>}
-            type="number"
-            value={this.state.gridOffsetX}
-            onChange={(e, value) =>
-              this.setState({ gridOffsetX: parseInt(value, 10) })
-            }
-          />
-          <TextField
-            floatingLabelText={<Trans>Y offset (in pixels)</Trans>}
-            type="number"
-            value={this.state.gridOffsetY}
-            onChange={(e, value) =>
-              this.setState({ gridOffsetY: parseInt(value, 10) })
-            }
-          />
-        </ResponsiveLineStackLayout>
+        <ColumnStackLayout>
+          <ResponsiveLineStackLayout noMargin expand>
+            <ColorField
+              floatingLabelText={<Trans>Line color</Trans>}
+              fullWidth
+              color={this.state.gridColor}
+              onChangeComplete={color => {
+                this.setState({ gridColor: color.rgb });
+              }}
+            />
+          </ResponsiveLineStackLayout>
+          <ResponsiveLineStackLayout noMargin expand>
+            <TextField
+              floatingLabelText={<Trans>Cell width (in pixels)</Trans>}
+              fullWidth
+              type="number"
+              value={this.state.gridWidth}
+              onChange={(e, value) =>
+                this.setState({ gridWidth: parseInt(value, 10) })
+              }
+            />
+            <TextField
+              floatingLabelText={<Trans>Cell height (in pixels)</Trans>}
+              fullWidth
+              type="number"
+              value={this.state.gridHeight}
+              onChange={(e, value) =>
+                this.setState({ gridHeight: parseInt(value, 10) })
+              }
+            />
+          </ResponsiveLineStackLayout>
+          <ResponsiveLineStackLayout noMargin expand>
+            <TextField
+              floatingLabelText={<Trans>X offset (in pixels)</Trans>}
+              fullWidth
+              type="number"
+              value={this.state.gridOffsetX}
+              onChange={(e, value) =>
+                this.setState({ gridOffsetX: parseInt(value, 10) })
+              }
+            />
+            <TextField
+              floatingLabelText={<Trans>Y offset (in pixels)</Trans>}
+              fullWidth
+              type="number"
+              value={this.state.gridOffsetY}
+              onChange={(e, value) =>
+                this.setState({ gridOffsetY: parseInt(value, 10) })
+              }
+            />
+          </ResponsiveLineStackLayout>
+        </ColumnStackLayout>
       </Dialog>
     );
   }

f650a6aa9cf5d123f1e5fe632a2523f2ac2faaaf
Florian Rival <Florian.Rival@gmail.com>
2020-11-22 23:15:24 +0000

    Add support for registering a game and viewing its recorded metrics

--- newIDE/app/src/Profile/LoginDialog.js
+++ newIDE/app/src/Profile/LoginDialog.js
@@ -16,6 +16,7 @@ import LeftLoader from '../UI/LeftLoader';
 import Text from '../UI/Text';
 import { getEmailErrorText, getPasswordErrorText } from './CreateAccountDialog';
 import AlertMessage from '../UI/AlertMessage';
+import { ColumnStackLayout } from '../UI/Layout';
 
 type Props = {|
   onClose: () => void,
@@ -103,7 +104,7 @@ export default class LoginDialog extends Component<Props, State> {
         cannotBeDismissed={true}
         open
       >
-        <Column noMargin>
+        <ColumnStackLayout noMargin>
           <AlertMessage
             kind="info"
             renderRightButton={() => (
@@ -118,6 +119,7 @@ export default class LoginDialog extends Component<Props, State> {
             <Trans>Don't have an account yet?</Trans>
           </AlertMessage>
           <TextField
+            autoFocus
             value={this.state.form.email}
             floatingLabelText={<Trans>Email</Trans>}
             errorText={getEmailErrorText(error)}
@@ -146,7 +148,7 @@ export default class LoginDialog extends Component<Props, State> {
               });
             }}
           />
-        </Column>
+        </ColumnStackLayout>
         <Dialog
           cannotBeDismissed={true}
           open={resetPasswordDialogOpen}

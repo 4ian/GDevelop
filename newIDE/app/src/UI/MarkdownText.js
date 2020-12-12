f500196ab22643fa911d1d18a76e0204eaeb9a83
Florian Rival <Florian.Rival@gmail.com>
2020-12-12 17:20:38 +0100

    Fix bad links in extension descriptions that would reload GDevelop

--- newIDE/app/src/UI/MarkdownText.js
+++ newIDE/app/src/UI/MarkdownText.js
@@ -8,11 +8,22 @@ import classNames from 'classnames';
 const makeMarkdownCustomRenderers = (isStandaloneText: boolean) => ({
   // Ensure link are opened in a new page
   root: props => (isStandaloneText ? <div {...props} /> : <span {...props} />),
-  link: props => (
-    <a href={props.href} target="_blank" rel="noopener noreferrer">
-      {props.children}
-    </a>
-  ),
+  link: props =>
+    props.href ? (
+      <a href={props.href} target="_blank" rel="noopener noreferrer">
+        {props.children}
+      </a>
+    ) : (
+      props.children
+    ),
+  linkReference: props =>
+    props.href ? (
+      <a href={props.href} target="_blank" rel="noopener noreferrer">
+        {props.children}
+      </a>
+    ) : (
+      props.children
+    ),
   // Add paragraphs only if we explictly opt in.
   paragraph: props =>
     isStandaloneText ? <p>{props.children}</p> : props.children,

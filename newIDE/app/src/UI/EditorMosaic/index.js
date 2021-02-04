f2a1069b4b090c332fee19d857cc194bd73df4e4
INNOVATIVEGAMER <55017197+INNOVATIVEGAMER@users.noreply.github.com>
2021-02-04 18:45:12 +0530

    Fix flow types

--- newIDE/app/src/UI/EditorMosaic/index.js
+++ newIDE/app/src/UI/EditorMosaic/index.js
@@ -48,7 +48,7 @@ const getPathOfNode = (
   mosaicNode: ?EditorMosaicNode
 ): Array<?string> => {
   let path = [];
-  const hasPath = node => {
+  const hasPath = (node: ?EditorMosaicNode) => {
     if (!node) return;
 
     if (typeof node.first === 'string') {
@@ -65,12 +65,12 @@ const getPathOfNode = (
       }
     }
 
-    if (typeof node.first !== 'string') {
+    if (node.first && typeof node.first !== 'string') {
       path.push('first');
       if (hasPath(node.first)) return true;
     }
 
-    if (typeof node.second !== 'string') {
+    if (node.second && typeof node.second !== 'string') {
       path.push('second');
       if (hasPath(node.second)) return true;
     }

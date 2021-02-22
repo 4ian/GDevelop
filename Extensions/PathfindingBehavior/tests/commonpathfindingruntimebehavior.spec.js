2623b99a43ef07473312b457f737c300124ad81d
Davy Hélard <davy.helard@gmail.com>
2021-02-22 21:10:47 +0100

    Format

--- Extensions/PathfindingBehavior/tests/commonpathfindingruntimebehavior.spec.js
+++ Extensions/PathfindingBehavior/tests/commonpathfindingruntimebehavior.spec.js
@@ -1,5 +1,4 @@
-var commonpathfindingruntimebehavior = {
-
+var commonpathfindingtest = {
   createScene: () => {
     const runtimeGame = new gdjs.RuntimeGame({
       variables: [],
@@ -19,7 +18,7 @@ var commonpathfindingruntimebehavior = {
     };
     return runtimeScene;
   },
-  
+
   addPlayer: (runtimeScene, collisionMethod, allowDiagonals) => {
     const player = new gdjs.RuntimeObject(runtimeScene, {
       name: 'player',
@@ -49,7 +48,7 @@ var commonpathfindingruntimebehavior = {
     runtimeScene.addObject(player);
     return player;
   },
-  
+
   addObstacle: (runtimeScene) => {
     var obstacle = new gdjs.RuntimeObject(runtimeScene, {
       name: 'obstacle',
@@ -71,118 +70,135 @@ var commonpathfindingruntimebehavior = {
     runtimeScene.addObject(obstacle);
     return obstacle;
   },
-  
-  doTests: (
-    collisionMethod,
-    allowDiagonals
-  ) => {
+
+  doTests: (collisionMethod, allowDiagonals) => {
     const pathFindingName = 'auto1';
-  
+
     it('can find a path without any obstacle at all', function () {
-      const runtimeScene = commonpathfindingruntimebehavior.createScene();
-      const player = commonpathfindingruntimebehavior.addPlayer(runtimeScene, collisionMethod, allowDiagonals);
-  
+      const runtimeScene = commonpathfindingtest.createScene();
+      const player = commonpathfindingtest.addPlayer(
+        runtimeScene,
+        collisionMethod,
+        allowDiagonals
+      );
+
       player.setPosition(480, 300);
       player.getBehavior(pathFindingName).moveTo(runtimeScene, 720, 300);
       expect(player.getBehavior(pathFindingName).pathFound()).to.be(true);
       expect(player.getBehavior(pathFindingName).getNodeCount()).to.be(13);
     });
-  
+
     it('can find a path without any obstacle in the way', function () {
-      const runtimeScene = commonpathfindingruntimebehavior.createScene();
-      const player = commonpathfindingruntimebehavior.addPlayer(runtimeScene, collisionMethod, allowDiagonals);
-      const obstacle = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-  
+      const runtimeScene = commonpathfindingtest.createScene();
+      const player = commonpathfindingtest.addPlayer(
+        runtimeScene,
+        collisionMethod,
+        allowDiagonals
+      );
+      const obstacle = commonpathfindingtest.addObstacle(runtimeScene);
+
       obstacle.setPosition(100, 100);
       // To ensure obstacles are registered.
       runtimeScene.renderAndStep(1000 / 60);
-  
+
       player.setPosition(480, 300);
       player.getBehavior(pathFindingName).moveTo(runtimeScene, 720, 300);
       expect(player.getBehavior(pathFindingName).pathFound()).to.be(true);
       expect(player.getBehavior(pathFindingName).getNodeCount()).to.be(13);
     });
-  
-  
+
     it("mustn't find a path to the obstacle inside", function () {
-      const runtimeScene = commonpathfindingruntimebehavior.createScene();
-      const player = commonpathfindingruntimebehavior.addPlayer(runtimeScene, collisionMethod, allowDiagonals);
-      const obstacle = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-  
+      const runtimeScene = commonpathfindingtest.createScene();
+      const player = commonpathfindingtest.addPlayer(
+        runtimeScene,
+        collisionMethod,
+        allowDiagonals
+      );
+      const obstacle = commonpathfindingtest.addObstacle(runtimeScene);
+
       obstacle.setPosition(600, 300);
       // To ensure obstacles are registered.
       runtimeScene.renderAndStep(1000 / 60);
-  
+
       player.setPosition(480, 300);
       player.getBehavior(pathFindingName).moveTo(runtimeScene, 650, 350);
       expect(player.getBehavior(pathFindingName).pathFound()).to.be(false);
     });
-  
+
     it('can find a path with an obstacle in the way', function () {
-      const runtimeScene = commonpathfindingruntimebehavior.createScene();
-      const player = commonpathfindingruntimebehavior.addPlayer(runtimeScene, collisionMethod, allowDiagonals);
-      const obstacle = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-  
+      const runtimeScene = commonpathfindingtest.createScene();
+      const player = commonpathfindingtest.addPlayer(
+        runtimeScene,
+        collisionMethod,
+        allowDiagonals
+      );
+      const obstacle = commonpathfindingtest.addObstacle(runtimeScene);
+
       obstacle.setPosition(600, 300);
       // To ensure obstacles are registered.
       runtimeScene.renderAndStep(1000 / 60);
-  
+
       player.setPosition(480, 300);
       player.getBehavior(pathFindingName).moveTo(runtimeScene, 720, 300);
       expect(player.getBehavior(pathFindingName).pathFound()).to.be(true);
-      expect(player.getBehavior(pathFindingName).getNodeCount()).to.be.above(13);
+      expect(player.getBehavior(pathFindingName).getNodeCount()).to.be.above(
+        13
+      );
     });
-  
-  
+
     it('can find a path between 2 obstacles', function () {
-      const runtimeScene = commonpathfindingruntimebehavior.createScene();
-      const player = commonpathfindingruntimebehavior.addPlayer(runtimeScene, collisionMethod, allowDiagonals);
-      const obstacleTop = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-      const obstacleBottom = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-  
+      const runtimeScene = commonpathfindingtest.createScene();
+      const player = commonpathfindingtest.addPlayer(
+        runtimeScene,
+        collisionMethod,
+        allowDiagonals
+      );
+      const obstacleTop = commonpathfindingtest.addObstacle(runtimeScene);
+      const obstacleBottom = commonpathfindingtest.addObstacle(runtimeScene);
+
       obstacleTop.setPosition(600, 180);
       obstacleBottom.setPosition(600, 420);
       // To ensure obstacles are registered.
       runtimeScene.renderAndStep(1000 / 60);
-  
+
       player.setPosition(480, 300);
       player.getBehavior(pathFindingName).moveTo(runtimeScene, 720, 300);
       expect(player.getBehavior(pathFindingName).pathFound()).to.be(true);
       expect(player.getBehavior(pathFindingName).getNodeCount()).to.be(13);
     });
-  
-  
+
     it("mustn't find a path to a closed room", function () {
-      const runtimeScene = commonpathfindingruntimebehavior.createScene();
-      const player = commonpathfindingruntimebehavior.addPlayer(runtimeScene, collisionMethod, allowDiagonals);
-      const obstacleTop = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-      const obstacleBottom = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-      const obstacleLeft = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-      const obstacleRight = commonpathfindingruntimebehavior.addObstacle(runtimeScene);
-  
+      const runtimeScene = commonpathfindingtest.createScene();
+      const player = commonpathfindingtest.addPlayer(
+        runtimeScene,
+        collisionMethod,
+        allowDiagonals
+      );
+      const obstacleTop = commonpathfindingtest.addObstacle(runtimeScene);
+      const obstacleBottom = commonpathfindingtest.addObstacle(runtimeScene);
+      const obstacleLeft = commonpathfindingtest.addObstacle(runtimeScene);
+      const obstacleRight = commonpathfindingtest.addObstacle(runtimeScene);
+
       obstacleTop.setPosition(600, 180);
       obstacleBottom.setPosition(600, 420);
       obstacleLeft.setPosition(480, 300);
       obstacleRight.setPosition(720, 300);
       // To ensure obstacles are registered.
       runtimeScene.renderAndStep(1000 / 60);
-  
+
       player.setPosition(360, 300);
       player.getBehavior(pathFindingName).moveTo(runtimeScene, 600, 300);
       expect(player.getBehavior(pathFindingName).pathFound()).to.be(false);
     });
-  }
-}
+  },
+};
 // tests cases where every collisionMethod has the same behavior.
 describe('gdjs.PathfindingRuntimeBehavior', function () {
   ['Legacy'].forEach((collisionMethod) => {
     describe(`(collisionMethod: ${collisionMethod}, `, function () {
       [false, true].forEach((allowDiagonals) => {
         describe(`(allowDiagonals: ${allowDiagonals})`, function () {
-          commonpathfindingruntimebehavior.doTests(
-            collisionMethod,
-            allowDiagonals
-          );
+          commonpathfindingtest.doTests(collisionMethod, allowDiagonals);
         });
       });
     });

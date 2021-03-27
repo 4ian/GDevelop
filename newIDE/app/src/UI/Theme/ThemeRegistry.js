a6e718c93a003d60d53902c1001b36464e0c7ae9
Nilay Majorwar <nilaymajorwar@gmail.com>
2021-03-28 00:21:27 +0530

    Add One Dark theme for workbench and editor

--- newIDE/app/src/UI/Theme/ThemeRegistry.js
+++ newIDE/app/src/UI/Theme/ThemeRegistry.js
@@ -3,11 +3,13 @@ import DefaultTheme from './DefaultTheme';
 import DarkTheme from './DarkTheme';
 import NordTheme from './NordTheme';
 import SolarizedDarkTheme from './SolarizedDarkTheme';
+import OneDarkTheme from './OneDarkTheme';
 
 /*eslint no-useless-computed-key: "off"*/
 export const themes = {
-  ['GDevelop default']: DefaultTheme,
-  ['Dark']: DarkTheme,
-  ['Nord']: NordTheme,
-  ['Solarized Dark']: SolarizedDarkTheme,
+    ['GDevelop default']: DefaultTheme,
+    ['Dark']: DarkTheme,
+    ['Nord']: NordTheme,
+    ['Solarized Dark']: SolarizedDarkTheme,
+    ['One Dark']: OneDarkTheme,
 };

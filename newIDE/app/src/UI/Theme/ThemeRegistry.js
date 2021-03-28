e1423e0b907e101e3722ead9292376a2e2b8b891
Nilay Majorwar <nilaymajorwar@gmail.com>
2021-03-28 19:58:56 +0530

    Add One Dark theme (for the editor and for JavaScript code blocks) (#2476)

--- newIDE/app/src/UI/Theme/ThemeRegistry.js
+++ newIDE/app/src/UI/Theme/ThemeRegistry.js
@@ -3,6 +3,7 @@ import DefaultTheme from './DefaultTheme';
 import DarkTheme from './DarkTheme';
 import NordTheme from './NordTheme';
 import SolarizedDarkTheme from './SolarizedDarkTheme';
+import OneDarkTheme from './OneDarkTheme';
 
 /*eslint no-useless-computed-key: "off"*/
 export const themes = {
@@ -10,4 +11,5 @@ export const themes = {
   ['Dark']: DarkTheme,
   ['Nord']: NordTheme,
   ['Solarized Dark']: SolarizedDarkTheme,
+  ['One Dark']: OneDarkTheme,
 };

4981f32382b2710b92cb9e6d7c397395f92a24f9
Florian Rival <Florian.rival@gmail.com>
2021-03-29 23:00:08 +0100

    Fix naming and docs

--- Extensions/DebuggerTools/debuggertools.ts
+++ Extensions/DebuggerTools/debuggertools.ts
@@ -14,18 +14,22 @@ namespace gdjs {
       };
 
       /**
-       * Draw collisions hitboxes and points
+       * Enable or disable the debug draw.
        * @param runtimeScene - The current scene.
+       * @param enableDebugDraw - true to enable the debug draw, false to disable it.
+       * @param showHiddenInstances - true to apply the debug draw to hidden objects.
+       * @param showPointsNames - true to show point names.
+       * @param showCustomPoints - true to show custom points of Sprite objects.
        */
       export const enableDebugDraw = function (
         runtimeScene: gdjs.RuntimeScene,
-        enableDebugDrawRenderedObjects: boolean,
+        enableDebugDraw: boolean,
         showHiddenInstances: boolean,
         showPointsNames: boolean,
         showCustomPoints: boolean
       ) {
         runtimeScene.enableDebugDraw(
-          enableDebugDrawRenderedObjects,
+          enableDebugDraw,
           showHiddenInstances,
           showPointsNames,
           showCustomPoints

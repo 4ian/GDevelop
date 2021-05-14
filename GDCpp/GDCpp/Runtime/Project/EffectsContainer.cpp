39cc5f1ffbe0e34dfb08e6caf16c127fce4a39cc
Florian Rival <Florian.rival@gmail.com>
2021-05-14 09:42:39 +0000

    Add files to fix GDCpp compilation on Travis

---
+++ GDCpp/GDCpp/Runtime/Project/EffectsContainer.cpp
@@ -0,0 +1,9 @@
+/*
+ * GDevelop C++ Platform
+ * Copyright 2008-2016 Florian Rival (Florian.Rival@gmail.com). All rights
+ * reserved. This project is released under the MIT License.
+ */
+
+#if !defined(GD_IDE_ONLY)
+#include "GDCore/Project/EffectsContainer.cpp"
+#endif

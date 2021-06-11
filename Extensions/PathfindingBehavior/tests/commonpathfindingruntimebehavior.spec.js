dee255915ec9e4a9b343900c69b9c98c1dc57659
Davy Hélard <davy.helard@gmail.com>
2021-06-12 01:19:45 +0200

    Add collision methods in the path finding behavior

--- Extensions/PathfindingBehavior/tests/commonpathfindingruntimebehavior.spec.js
+++ Extensions/PathfindingBehavior/tests/commonpathfindingruntimebehavior.spec.js
@@ -188,7 +188,7 @@ describe('gdjs.PathfindingRuntimeBehavior', function () {
     });
   };
 
-  ['Legacy'].forEach((collisionMethod) => {
+  ['Legacy', 'AABB', 'HitBoxes'].forEach((collisionMethod) => {
     describe(`(collisionMethod: ${collisionMethod}, `, function () {
       [false, true].forEach((allowDiagonals) => {
         describe(`(allowDiagonals: ${allowDiagonals})`, function () {

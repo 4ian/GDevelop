13b62a06eb881ea23fd5568fac35e34d2df0aadc
Florian Rival <Florian.Rival@gmail.com>
2021-07-22 15:09:58 +0100

    Add option to allow building Android App Bundles that can be uploaded if you already published APKs with Play App Signing enabled for your game * If you already opted in for "Play App Signing" in the Play Developer Console and published some APKs like this, you need to enable the new "Old upload key" option, in the Signing Option, before *each* packaging for Android. * Read this [page to learn more](http://wiki.compilgames.net/doku.php/gdevelop5/publishing/android_and_ios/play-store/upgrading-from-apk-to-aab#done_you_can_now_upload_your_aab) and have step by step details of what to do to update your game. * If you publish a new game with Android App Bundles, there is nothing to do!

--- newIDE/app/src/Export/GenericExporters/OnlineCordovaExport.js
+++ newIDE/app/src/Export/GenericExporters/OnlineCordovaExport.js
@@ -7,9 +7,14 @@ import { type TargetName } from '../../Utils/GDevelopServices/Build';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
+import Dialog from '../../UI/Dialog';
+import FlatButton from '../../UI/FlatButton';
+import HelpButton from '../../UI/HelpButton';
 
 export type ExportState = {|
   targets: Array<TargetName>,
+  keystore: 'old' | 'new',
+  signingDialogOpen: boolean,
 |};
 
 type HeaderProps = {|
@@ -40,6 +45,7 @@ export const SetupExportHeader = ({
         onChange={event => {
           const targetName = event.target.value;
           updateExportState(prevExportState => ({
+            ...prevExportState,
             targets: [targetName],
           }));
         }}
@@ -61,6 +67,93 @@ export const SetupExportHeader = ({
           }
         />
       </RadioGroup>
+      <Line noMargin justifyContent="flex-end">
+        <FlatButton
+          label={<Trans>Signing options</Trans>}
+          onClick={() => {
+            updateExportState(prevExportState => ({
+              ...prevExportState,
+              signingDialogOpen: true,
+            }));
+          }}
+          disabled={exportState.targets[0] !== 'androidAppBundle'}
+        />
+      </Line>
+      {exportState.signingDialogOpen && (
+        <Dialog
+          actions={[
+            <FlatButton
+              key="close"
+              label={<Trans>Close</Trans>}
+              primary
+              keyboardFocused
+              onClick={() => {
+                updateExportState(prevExportState => ({
+                  ...prevExportState,
+                  signingDialogOpen: false,
+                }));
+              }}
+            />,
+          ]}
+          secondaryActions={[
+            <HelpButton
+              helpPagePath="/publishing/android_and_ios/play-store/upgrading-from-apk-to-aab"
+              key="help"
+            />,
+          ]}
+          open
+          title={<Trans>Signing options</Trans>}
+          onRequestClose={() => {
+            updateExportState(prevExportState => ({
+              ...prevExportState,
+              signingDialogOpen: false,
+            }));
+          }}
+          maxWidth="sm"
+        >
+          <Text>
+            <Trans>
+              Choose the upload key to use to identify your Android App Bundle.
+              In most cases you don't need to change this. Use the "Old upload
+              key" if you used to publish your game as an APK and you activated
+              Play App Signing before switching to Android App Bundle.
+            </Trans>
+          </Text>
+          <RadioGroup
+            name="signing-keystore"
+            value={exportState.keystore}
+            onChange={event => {
+              const keystore = event.target.value;
+              updateExportState(prevExportState => ({
+                ...prevExportState,
+                keystore,
+              }));
+            }}
+          >
+            <FormControlLabel
+              value={'new'}
+              control={<Radio color="primary" />}
+              label={<Trans>New upload key (recommended)</Trans>}
+            />
+            <FormControlLabel
+              value={'old'}
+              control={<Radio color="primary" />}
+              label={
+                <Trans>
+                  Old upload key (only if you used to publish your game as an
+                  APK and already activated Play App Signing)
+                </Trans>
+              }
+            />
+            <FormControlLabel
+              value={'custom'}
+              control={<Radio color="primary" />}
+              label={<Trans>Custom upload key (not available yet)</Trans>}
+              disabled
+            />
+          </RadioGroup>
+        </Dialog>
+      )}
     </Column>
   );
 };

a3f5dd9cdc1f685b73ddabb25a674ee6a3552f16
Davy Hélard <davy.helard@gmail.com>
2021-08-30 10:08:46 +0200

    Test on the condition method instead of an internal method.

--- Extensions/LinkedObjects/tests/linkedobjects.spec.js
+++ Extensions/LinkedObjects/tests/linkedobjects.spec.js
@@ -1,3 +1,4 @@
+// @ts-check
 describe('gdjs.LinksManager', function () {
   var runtimeGame = new gdjs.RuntimeGame({
     variables: [],
@@ -35,23 +36,35 @@ describe('gdjs.LinksManager', function () {
   });
 
   var object2A = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
+    name: 'obj2',
     type: '',
     behaviors: [],
     effects: [],
   });
   var object2B = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
+    name: 'obj2',
     type: '',
     behaviors: [],
     effects: [],
   });
   var object2C = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
+    name: 'obj2',
     type: '',
     behaviors: [],
     effects: [],
   });
+
+  var pickObjectsLinkedTo = (object) => {
+    const objectsLists = new Hashtable();
+    objectsLists.put('obj1', [object1A, object1B, object1C]);
+    objectsLists.put('obj2', [object2A, object2B, object2C]);
+    const pickedSomething = gdjs.evtTools.linkedObjects.pickObjectsLinkedTo(
+      runtimeScene,
+      objectsLists,
+      object
+    );
+    return pickedSomething ? objectsLists : false;
+  };
 
   runtimeScene.addObject(object1A);
   runtimeScene.addObject(object1B);
@@ -64,43 +77,50 @@ describe('gdjs.LinksManager', function () {
   it('can link two objects', function () {
     manager.linkObjects(object1A, object2A);
 
-    var linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object2A);
+    let objectsLists = pickObjectsLinkedTo(object1A);
+    expect(objectsLists.get('obj1').length).to.be(3);
+    expect(objectsLists.get('obj2').length).to.be(1);
+    expect(objectsLists.get('obj2')[0]).to.be(object2A);
 
-    linkedObjects = manager.getObjectsLinkedWith(object2A);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object1A);
+    objectsLists = pickObjectsLinkedTo(object2A);
+    expect(objectsLists.get('obj1').length).to.be(1);
+    expect(objectsLists.get('obj1')[0]).to.be(object1A);
+    expect(objectsLists.get('obj2').length).to.be(3);
   });
   it('can link more objects', function () {
     manager.linkObjects(object1A, object2A); //Including the same objects as before
     manager.linkObjects(object1A, object2B);
     manager.linkObjects(object1A, object2C);
 
-    var linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(3);
+    let objectsLists = pickObjectsLinkedTo(object1A);
+    expect(objectsLists.get('obj1').length).to.be(3);
+    expect(objectsLists.get('obj2').length).to.be(3);
 
-    linkedObjects = manager.getObjectsLinkedWith(object2C);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object1A);
+    objectsLists = pickObjectsLinkedTo(object2C);
+    expect(objectsLists.get('obj1').length).to.be(1);
+    expect(objectsLists.get('obj1')[0]).to.be(object1A);
+    expect(objectsLists.get('obj2').length).to.be(3);
   });
   it('supports removing links', function () {
     manager.removeLinkBetween(object1A, object2B);
-    var linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(2);
+
+    let objectsLists = pickObjectsLinkedTo(object1A);
+    expect(objectsLists.get('obj1').length).to.be(3);
+    expect(objectsLists.get('obj2').length).to.be(2);
 
     manager.linkObjects(object2B, object2C);
     manager.removeAllLinksOf(object1A);
     manager.removeAllLinksOf(object1A);
 
-    linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(0);
+    objectsLists = pickObjectsLinkedTo(object1A);
+    expect(objectsLists).to.be(false);
 
-    linkedObjects = manager.getObjectsLinkedWith(object2A);
-    expect(linkedObjects.length).to.be(0);
+    objectsLists = pickObjectsLinkedTo(object2A);
+    expect(objectsLists).to.be(false);
 
-    linkedObjects = manager.getObjectsLinkedWith(object2C);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object2B);
+    objectsLists = pickObjectsLinkedTo(object2C);
+    expect(objectsLists.get('obj1').length).to.be(0);
+    expect(objectsLists.get('obj2').length).to.be(1);
+    expect(objectsLists.get('obj2')[0]).to.be(object2B);
   });
 });

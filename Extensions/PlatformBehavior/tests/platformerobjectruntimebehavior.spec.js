0d53d0726532dea3b2b2d69180b97918e1c26405
Davy Hélard <davy.helard@gmail.com>
2021-09-04 21:18:08 +0200

    No longer skip the test case 'must not move when on the floor at startup'.

--- Extensions/PlatformBehavior/tests/platformerobjectruntimebehavior.spec.js
+++ Extensions/PlatformBehavior/tests/platformerobjectruntimebehavior.spec.js
@@ -250,7 +250,7 @@ describe('gdjs.PlatformerObjectRuntimeBehavior', function () {
       }
     });
 
-    it.skip('must not move when on the floor at startup', function () {
+    it('must not move when on the floor at startup', function () {
       object.setPosition(0, platform.getY() - object.getHeight());
 
       for (let i = 0; i < 10; ++i) {

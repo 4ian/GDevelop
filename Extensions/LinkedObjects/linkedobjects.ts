f0f2330137725ab58c02c75034b50a3da4917f00
Davy Hélard <davy.helard@gmail.com>
2021-09-09 00:47:01 +0200

    WIP: [LinkedObjects] Objects can be picked from event functions.

--- Extensions/LinkedObjects/linkedobjects.ts
+++ Extensions/LinkedObjects/linkedobjects.ts
@@ -7,7 +7,7 @@ namespace gdjs {
    * Manages the links between objects.
    */
   export class LinksManager {
-    private links: { [objectId: number]: gdjs.RuntimeObject[] } = {};
+    private links: { [objectId: number]: Hashtable<gdjs.RuntimeObject[]> } = {};
 
     /**
      * Get the links manager of a scene.
@@ -23,35 +23,56 @@ namespace gdjs {
       return runtimeScene.linkedObjectsManager;
     }
 
-    getObjectsLinkedWith(objA: gdjs.RuntimeObject) {
+    _getObjectsLinkedWith(objA: gdjs.RuntimeObject) {
       if (!this.links.hasOwnProperty(objA.id)) {
-        this.links[objA.id] = [];
+        this.links[objA.id] = new Hashtable<gdjs.RuntimeObject[]>();
       }
       return this.links[objA.id];
     }
 
     linkObjects(objA: gdjs.RuntimeObject, objB: gdjs.RuntimeObject) {
-      const objALinkedObjects = this.getObjectsLinkedWith(objA);
+      const objALinkedObjectMap = this._getObjectsLinkedWith(objA);
+      if (!objALinkedObjectMap.containsKey(objB.getName())) {
+        objALinkedObjectMap.put(objB.getName(), []);
+      }
+      const objALinkedObjects = objALinkedObjectMap.get(objB.getName());
       if (objALinkedObjects.indexOf(objB) === -1) {
         objALinkedObjects.push(objB);
       }
-      const objBLinkedObjects = this.getObjectsLinkedWith(objB);
+      const objBLinkedObjectMap = this._getObjectsLinkedWith(objB);
+      if (!objBLinkedObjectMap.containsKey(objA.getName())) {
+        objBLinkedObjectMap.put(objA.getName(), []);
+      }
+      const objBLinkedObjects = objBLinkedObjectMap.get(objA.getName());
       if (objBLinkedObjects.indexOf(objA) === -1) {
         objBLinkedObjects.push(objA);
       }
     }
 
     removeAllLinksOf(obj: gdjs.RuntimeObject) {
-      const objLinkedObjects = this.getObjectsLinkedWith(obj);
-      for (let i = 0; i < objLinkedObjects.length; i++) {
-        if (this.links.hasOwnProperty(objLinkedObjects[i].id)) {
-          const otherObjList = this.links[objLinkedObjects[i].id];
-          const index = otherObjList.indexOf(obj);
-          if (index !== -1) {
-            otherObjList.splice(index, 1);
+      // Remove the other side of the links
+      const linkedObjectMap = this._getObjectsLinkedWith(obj);
+      for (const linkedObjectName in linkedObjectMap.items) {
+        if (linkedObjectMap.containsKey(linkedObjectName)) {
+          const linkedObjects = linkedObjectMap.get(linkedObjectName);
+
+          for (let i = 0; i < linkedObjects.length; i++) {
+            // This is the object on the other side of the link
+            // We find obj in its list of linked objects and remove it.
+            const linkedObject = linkedObjects[i];
+            if (this.links.hasOwnProperty(linkedObject.id)) {
+              const otherObjList = this.links[linkedObject.id].get(
+                obj.getName()
+              );
+              const index = otherObjList.indexOf(obj);
+              if (index !== -1) {
+                otherObjList.splice(index, 1);
+              }
+            }
           }
         }
       }
+      // Remove the links on obj side
       if (this.links.hasOwnProperty(obj.id)) {
         delete this.links[obj.id];
       }
@@ -59,17 +80,23 @@ namespace gdjs {
 
     removeLinkBetween(objA: gdjs.RuntimeObject, objB: gdjs.RuntimeObject) {
       if (this.links.hasOwnProperty(objA.id)) {
-        const list = this.links[objA.id];
-        const index = list.indexOf(objB);
-        if (index !== -1) {
-          list.splice(index, 1);
+        const map = this.links[objA.id];
+        if (map.containsKey(objB.getName())) {
+          const list = map.get(objB.getName());
+          const index = list.indexOf(objB);
+          if (index !== -1) {
+            list.splice(index, 1);
+          }
         }
       }
       if (this.links.hasOwnProperty(objB.id)) {
-        const list = this.links[objB.id];
-        const index = list.indexOf(objA);
-        if (index !== -1) {
-          list.splice(index, 1);
+        const map = this.links[objB.id];
+        if (map.containsKey(objA.getName())) {
+          const list = map.get(objA.getName());
+          const index = list.indexOf(objA);
+          if (index !== -1) {
+            list.splice(index, 1);
+          }
         }
       }
     }
@@ -112,11 +139,18 @@ namespace gdjs {
         LinksManager.getManager(runtimeScene).removeAllLinksOf(objA);
       };
 
-      export const _objectIsInList = function (
-        obj: gdjs.RuntimeObject,
-        linkedObjects: gdjs.RuntimeObject[]
+      export const quickPickObjectsLinkedTo = function (
+        runtimeScene: gdjs.RuntimeScene,
+        eventsFunctionContext: EventsFunctionContext | undefined,
+        objectsLists: Hashtable<gdjs.RuntimeObject[]>,
+        obj: gdjs.RuntimeObject
       ) {
-        return linkedObjects.indexOf(obj) !== -1;
+        return doPickObjectsLinkedTo(
+          runtimeScene,
+          objectsLists,
+          obj,
+          !!eventsFunctionContext
+        );
       };
 
       export const pickObjectsLinkedTo = function (
@@ -124,18 +158,106 @@ namespace gdjs {
         objectsLists: Hashtable<gdjs.RuntimeObject[]>,
         obj: gdjs.RuntimeObject
       ) {
+        return doPickObjectsLinkedTo(
+          runtimeScene,
+          objectsLists,
+          obj,
+          // Can't know if it's called from an event function or not
+          // Object names will have to be checked
+          true
+        );
+      };
+
+      const doPickObjectsLinkedTo = function (
+        runtimeScene: gdjs.RuntimeScene,
+        objectsLists: Hashtable<gdjs.RuntimeObject[]>,
+        obj: gdjs.RuntimeObject,
+        isEventsFunction: boolean
+      ) {
         if (obj === null) {
           return false;
         }
-        const linkedObjects = LinksManager.getManager(
+        const linkedObjectMap = LinksManager.getManager(
           runtimeScene
-        ).getObjectsLinkedWith(obj);
-        return gdjs.evtTools.object.pickObjectsIf(
-          gdjs.evtTools.linkedObjects._objectIsInList,
-          objectsLists,
-          false,
-          linkedObjects
-        );
+        )._getObjectsLinkedWith(obj);
+
+        let pickedSomething = false;
+        for (const contextObjectName in objectsLists.items) {
+          if (objectsLists.containsKey(contextObjectName)) {
+            const pickedObjects = objectsLists.items[contextObjectName];
+
+            if (pickedObjects.length === 0) {
+              continue;
+            }
+
+            // Find the object names in the scene
+            const temporaryObjectNames = gdjs.staticArray2(
+              gdjs.evtTools.linkedObjects.pickObjectsLinkedTo
+            );
+            temporaryObjectNames.length = 0;
+            if (isEventsFunction) {
+              // For functions, objects may be a merged group
+              for (const pickedObject of pickedObjects) {
+                if (temporaryObjectNames.indexOf(pickedObject.getName()) < 0) {
+                  temporaryObjectNames.push(pickedObject.getName());
+                }
+              }
+            } else {
+              temporaryObjectNames.push(contextObjectName);
+            }
+
+            let objectCount = 0;
+            let linkedObjectExists = false;
+            for (const objectName of temporaryObjectNames) {
+              objectCount += runtimeScene.getObjects(objectName).length;
+              linkedObjectExists =
+                linkedObjectExists || linkedObjectMap.containsKey(objectName);
+            }
+
+            if (linkedObjectExists) {
+              if (pickedObjects.length === objectCount) {
+                // All the objects were picked, there is no need to make an intersection.
+                pickedObjects.length = 0;
+                for (const objectName of temporaryObjectNames) {
+                  if (linkedObjectMap.containsKey(objectName)) {
+                    const linkedObjects = linkedObjectMap.get(objectName);
+
+                    pickedSomething =
+                      pickedSomething || linkedObjects.length > 0;
+                    pickedObjects.push.apply(pickedObjects, linkedObjects);
+                  }
+                }
+              } else {
+                const temporaryObjects = gdjs.staticArray(
+                  gdjs.evtTools.linkedObjects.pickObjectsLinkedTo
+                );
+                temporaryObjects.length = 0;
+
+                for (const objectName of temporaryObjectNames) {
+                  if (linkedObjectMap.containsKey(objectName)) {
+                    const linkedObjects = linkedObjectMap.get(objectName);
+
+                    for (const otherObject of linkedObjects) {
+                      if (pickedObjects.indexOf(otherObject) >= 0) {
+                        temporaryObjects.push(otherObject);
+                      }
+                    }
+                  }
+                }
+                pickedSomething =
+                  pickedSomething || temporaryObjects.length > 0;
+                pickedObjects.length = 0;
+                pickedObjects.push.apply(pickedObjects, temporaryObjects);
+                temporaryObjects.length = 0;
+              }
+            } else {
+              // No object is linked for this name
+              pickedObjects.length = 0;
+            }
+            temporaryObjectNames.length = 0;
+          }
+        }
+        return pickedSomething;
       };
     }
   }
--- Extensions/LinkedObjects/tests/linkedobjects.spec.js
+++ Extensions/LinkedObjects/tests/linkedobjects.spec.js
@@ -1,106 +1,179 @@
+// @ts-check
 describe('gdjs.LinksManager', function () {
-  var runtimeGame = new gdjs.RuntimeGame({
-    variables: [],
-    properties: { windowWidth: 800, windowHeight: 600 },
-    resources: { resources: [] },
-  });
-  var runtimeScene = new gdjs.RuntimeScene(runtimeGame);
-  runtimeScene.loadFromScene({
-    layers: [{ name: '', visibility: true, effects: [] }],
-    variables: [],
-    behaviorsSharedData: [],
-    objects: [],
-    instances: [],
-  });
+  [true, false].forEach((legacy) => {
+    describe(legacy ? '(legacy)' : '(quick)', function () {
+      const runtimeGame = new gdjs.RuntimeGame({
+        variables: [],
+        // @ts-ignore - missing properties.
+        properties: { windowWidth: 800, windowHeight: 600 },
+        resources: { resources: [] },
+      });
+      const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
+      runtimeScene.loadFromScene({
+        // @ts-ignore - missing properties.
+        layers: [{ name: '', visibility: true, effects: [] }],
+        variables: [],
+        behaviorsSharedData: [],
+        objects: [],
+        instances: [],
+      });
 
-  var manager = gdjs.LinksManager.getManager(runtimeScene);
+      const manager = gdjs.LinksManager.getManager(runtimeScene);
 
-  var object1A = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
-    type: '',
-    behaviors: [],
-    effects: [],
-  });
-  var object1B = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
-    type: '',
-    behaviors: [],
-    effects: [],
-  });
-  var object1C = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
-    type: '',
-    behaviors: [],
-    effects: [],
-  });
+      // @ts-ignore - missing properties.
+      const object1A = new gdjs.RuntimeObject(runtimeScene, {
+        name: 'obj1',
+        type: '',
+        behaviors: [],
+        effects: [],
+      });
+      // @ts-ignore - missing properties.
+      const object1B = new gdjs.RuntimeObject(runtimeScene, {
+        name: 'obj1',
+        type: '',
+        behaviors: [],
+        effects: [],
+      });
+      // @ts-ignore - missing properties.
+      const object1C = new gdjs.RuntimeObject(runtimeScene, {
+        name: 'obj1',
+        type: '',
+        behaviors: [],
+        effects: [],
+      });
 
-  var object2A = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
-    type: '',
-    behaviors: [],
-    effects: [],
-  });
-  var object2B = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
-    type: '',
-    behaviors: [],
-    effects: [],
-  });
-  var object2C = new gdjs.RuntimeObject(runtimeScene, {
-    name: 'obj1',
-    type: '',
-    behaviors: [],
-    effects: [],
-  });
+      // @ts-ignore - missing properties.
+      const object2A = new gdjs.RuntimeObject(runtimeScene, {
+        name: 'obj2',
+        type: '',
+        behaviors: [],
+        effects: [],
+      });
 
-  runtimeScene.addObject(object1A);
-  runtimeScene.addObject(object1B);
-  runtimeScene.addObject(object1C);
+      // @ts-ignore - missing properties.
+      const object2B = new gdjs.RuntimeObject(runtimeScene, {
+        name: 'obj2',
+        type: '',
+        behaviors: [],
+        effects: [],
+      });
+      // @ts-ignore - missing properties.
+      const object2C = new gdjs.RuntimeObject(runtimeScene, {
+        name: 'obj2',
+        type: '',
+        behaviors: [],
+        effects: [],
+      });
 
-  runtimeScene.addObject(object2A);
-  runtimeScene.addObject(object2B);
-  runtimeScene.addObject(object2C);
+      runtimeScene.addObject(object1A);
+      runtimeScene.addObject(object1B);
+      runtimeScene.addObject(object1C);
 
-  it('can link two objects', function () {
-    manager.linkObjects(object1A, object2A);
+      runtimeScene.addObject(object2A);
+      runtimeScene.addObject(object2B);
+      runtimeScene.addObject(object2C);
 
-    var linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object2A);
+      const pickObjectsLinkedTo = (object) => {
+        const objectsLists = new Hashtable();
+        objectsLists.put('obj1', [object1A, object1B, object1C]);
+        objectsLists.put('obj2', [object2A, object2B, object2C]);
+        let pickedSomething = false;
+        if (legacy) {
+          pickedSomething = gdjs.evtTools.linkedObjects.pickObjectsLinkedTo(
+            runtimeScene,
+            objectsLists,
+            object
+          );
+        } else {
+          pickedSomething = gdjs.evtTools.linkedObjects.quickPickObjectsLinkedTo(
+            runtimeScene,
+            // Called from a scene, no event function context.
+            undefined,
+            objectsLists,
+            object
+          );
+        }
+        return { pickedSomething, objectsLists };
+      };
 
-    linkedObjects = manager.getObjectsLinkedWith(object2A);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object1A);
-  });
-  it('can link more objects', function () {
-    manager.linkObjects(object1A, object2A); //Including the same objects as before
-    manager.linkObjects(object1A, object2B);
-    manager.linkObjects(object1A, object2C);
-
-    var linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(3);
-
-    linkedObjects = manager.getObjectsLinkedWith(object2C);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object1A);
-  });
-  it('supports removing links', function () {
-    manager.removeLinkBetween(object1A, object2B);
-    var linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(2);
-
-    manager.linkObjects(object2B, object2C);
-    manager.removeAllLinksOf(object1A);
-    manager.removeAllLinksOf(object1A);
-
-    linkedObjects = manager.getObjectsLinkedWith(object1A);
-    expect(linkedObjects.length).to.be(0);
-
-    linkedObjects = manager.getObjectsLinkedWith(object2A);
-    expect(linkedObjects.length).to.be(0);
-
-    linkedObjects = manager.getObjectsLinkedWith(object2C);
-    expect(linkedObjects.length).to.be(1);
-    expect(linkedObjects[0]).to.be(object2B);
+      it('can link two objects', function () {
+        manager.linkObjects(object1A, object2A);
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object1A
+          );
+          expect(pickedSomething).to.be(true);
+          expect(objectsLists.get('obj1').length).to.be(0);
+          expect(objectsLists.get('obj2').length).to.be(1);
+          expect(objectsLists.get('obj2')[0]).to.be(object2A);
+        }
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object2A
+          );
+          expect(pickedSomething).to.be(true);
+          expect(objectsLists.get('obj1').length).to.be(1);
+          expect(objectsLists.get('obj1')[0]).to.be(object1A);
+          expect(objectsLists.get('obj2').length).to.be(0);
+        }
+      });
+      it('can link more objects', function () {
+        manager.linkObjects(object1A, object2A); //Including the same objects as before
+        manager.linkObjects(object1A, object2B);
+        manager.linkObjects(object1A, object2C);
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object1A
+          );
+          expect(pickedSomething).to.be(true);
+          expect(objectsLists.get('obj1').length).to.be(0);
+          expect(objectsLists.get('obj2').length).to.be(3);
+        }
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object2C
+          );
+          expect(pickedSomething).to.be(true);
+          expect(objectsLists.get('obj1').length).to.be(1);
+          expect(objectsLists.get('obj1')[0]).to.be(object1A);
+          expect(objectsLists.get('obj2').length).to.be(0);
+        }
+      });
+      it('supports removing links', function () {
+        manager.removeLinkBetween(object1A, object2B);
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object1A
+          );
+          expect(pickedSomething).to.be(true);
+          expect(objectsLists.get('obj1').length).to.be(0);
+          expect(objectsLists.get('obj2').length).to.be(2);
+        }
+        manager.linkObjects(object2B, object2C);
+        manager.removeAllLinksOf(object1A);
+        manager.removeAllLinksOf(object1A);
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object1A
+          );
+          expect(pickedSomething).to.be(false);
+        }
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object2A
+          );
+          expect(pickedSomething).to.be(false);
+        }
+        {
+          const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+            object2C
+          );
+          expect(pickedSomething).to.be(true);
+          expect(objectsLists.get('obj1').length).to.be(0);
+          expect(objectsLists.get('obj2').length).to.be(1);
+          expect(objectsLists.get('obj2')[0]).to.be(object2B);
+        }
+      });
+    });
   });
 });

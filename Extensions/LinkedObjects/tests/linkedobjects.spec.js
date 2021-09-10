603a0437f42778637befe7b082b7bc59daf9d430
Davy Hélard <davy.helard@gmail.com>
2021-09-10 03:16:53 +0200

    Add tests with groups.

--- Extensions/LinkedObjects/tests/linkedobjects.spec.js
+++ Extensions/LinkedObjects/tests/linkedobjects.spec.js
@@ -4,6 +4,7 @@ describe('gdjs.LinksManager', function () {
     caseName,
     object1Names,
     object2Names,
+    object3Names,
     eventsFunctionContext,
     legacy
   ) => {
@@ -70,10 +71,39 @@ describe('gdjs.LinksManager', function () {
       effects: [],
     });
 
-    const pickObjectsLinkedTo = (object) => {
+    // @ts-ignore - missing properties.
+    const object3A = new gdjs.RuntimeObject(runtimeScene, {
+      name: object3Names[0],
+      type: '',
+      behaviors: [],
+      effects: [],
+    });
+
+    // @ts-ignore - missing properties.
+    const object3B = new gdjs.RuntimeObject(runtimeScene, {
+      name: object3Names[1],
+      type: '',
+      behaviors: [],
+      effects: [],
+    });
+    // @ts-ignore - missing properties.
+    const object3C = new gdjs.RuntimeObject(runtimeScene, {
+      name: object3Names[2],
+      type: '',
+      behaviors: [],
+      effects: [],
+    });
+
+    /**
+     * @param {gdjs.RuntimeObject} object
+     * @param {{objectName: string, picked: gdjs.RuntimeObject[]}[]} pickedObjects
+     * @returns a selection of picked objects
+     */
+    const pickObjectsLinkedTo = (object, pickedObjects) => {
       const objectsLists = new Hashtable();
-      objectsLists.put('obj1', [object1A, object1B, object1C]);
-      objectsLists.put('obj2', [object2A, object2B, object2C]);
+      for (const pickedObject of pickedObjects) {
+        objectsLists.put(pickedObject.objectName, pickedObject.picked);
+      }
       let pickedSomething = false;
       if (legacy) {
         pickedSomething = gdjs.evtTools.linkedObjects.pickObjectsLinkedTo(
@@ -100,21 +130,63 @@ describe('gdjs.LinksManager', function () {
     runtimeScene.addObject(object2B);
     runtimeScene.addObject(object2C);
 
+    runtimeScene.addObject(object3A);
+    runtimeScene.addObject(object3B);
+    runtimeScene.addObject(object3C);
+
     it('can link two objects', function () {
       manager.linkObjects(object1A, object2A);
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object1A);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object1A,
+          [{ objectName: 'obj2', picked: [object2A, object2B, object2C] }]
+        );
         expect(pickedSomething).to.be(true);
-        expect(objectsLists.get('obj1').length).to.be(0);
         expect(objectsLists.get('obj2').length).to.be(1);
         expect(objectsLists.get('obj2')[0]).to.be(object2A);
       }
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object2A);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object2A,
+          [{ objectName: 'obj1', picked: [object1A, object1B, object1C] }]
+        );
         expect(pickedSomething).to.be(true);
         expect(objectsLists.get('obj1').length).to.be(1);
         expect(objectsLists.get('obj1')[0]).to.be(object1A);
-        expect(objectsLists.get('obj2').length).to.be(0);
+      }
+    });
+    it('can select from already picked objects', function () {
+      manager.linkObjects(object1A, object2B);
+      // object1A <--> object2A, object2B
+      {
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object1A,
+          // object2B was discarded from a parent condition
+          [{ objectName: 'obj2', picked: [object2A, object2C] }]
+        );
+        expect(pickedSomething).to.be(true);
+        expect(objectsLists.get('obj2').length).to.be(1);
+        expect(objectsLists.get('obj2')[0]).to.be(object2A);
+      }
+    });
+    // This is a local group. In an event function context,
+    // it can groups objects that are flatten scene groups.
+    it('can select from a group', function () {
+      manager.linkObjects(object1A, object3C);
+      // object1A <--> object2A, object2B, object3C
+      {
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object1A,
+          // This is a group of obj2 and obj3.
+          [
+            { objectName: 'obj2', picked: [object2A, object2B, object2C] },
+            { objectName: 'obj3', picked: [object3A, object3B, object3C] },
+          ]
+        );
+        expect(pickedSomething).to.be(true);
+        expect(objectsLists.get('obj2').length).to.be(2);
+        expect(objectsLists.get('obj3').length).to.be(1);
+        expect(objectsLists.get('obj3')[0]).to.be(object3C);
       }
     });
     it('can link more objects', function () {
@@ -122,73 +194,93 @@ describe('gdjs.LinksManager', function () {
       manager.linkObjects(object1A, object2B);
       manager.linkObjects(object1A, object2C);
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object1A);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object1A,
+          [{ objectName: 'obj2', picked: [object2A, object2B, object2C] }]
+        );
         expect(pickedSomething).to.be(true);
-        expect(objectsLists.get('obj1').length).to.be(0);
         expect(objectsLists.get('obj2').length).to.be(3);
       }
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object2C);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object2C,
+          [{ objectName: 'obj1', picked: [object1A, object1B, object1C] }]
+        );
         expect(pickedSomething).to.be(true);
         expect(objectsLists.get('obj1').length).to.be(1);
         expect(objectsLists.get('obj1')[0]).to.be(object1A);
-        expect(objectsLists.get('obj2').length).to.be(0);
       }
     });
     it('supports removing links', function () {
       manager.removeLinkBetween(object1A, object2B);
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object1A);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object1A,
+          [{ objectName: 'obj2', picked: [object2A, object2B, object2C] }]
+        );
         expect(pickedSomething).to.be(true);
-        expect(objectsLists.get('obj1').length).to.be(0);
         expect(objectsLists.get('obj2').length).to.be(2);
       }
-      manager.linkObjects(object2B, object2C);
+      manager.linkObjects(object1B, object2C);
       manager.removeAllLinksOf(object1A);
       manager.removeAllLinksOf(object1A);
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object1A);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object1A,
+          [{ objectName: 'obj2', picked: [object2A, object2B, object2C] }]
+        );
         expect(pickedSomething).to.be(false);
       }
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object2A);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object2A,
+          [{ objectName: 'obj1', picked: [object1A, object1B, object1C] }]
+        );
         expect(pickedSomething).to.be(false);
       }
       {
-        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(object2C);
+        const { pickedSomething, objectsLists } = pickObjectsLinkedTo(
+          object2C,
+          [{ objectName: 'obj1', picked: [object1A, object1B, object1C] }]
+        );
         expect(pickedSomething).to.be(true);
-        expect(objectsLists.get('obj1').length).to.be(0);
-        expect(objectsLists.get('obj2').length).to.be(1);
-        expect(objectsLists.get('obj2')[0]).to.be(object2B);
+        expect(objectsLists.get('obj1').length).to.be(1);
+        expect(objectsLists.get('obj1')[0]).to.be(object1B);
       }
     });
   };
 
+  // Following object names are the names of the object in the scene.
+  // The test cases are using local names 'obj1', 'obj2', 'obj3'.
   [
     {
       caseName: 'scene',
       // Called from a scene, no event function context.
+      // Scene names are the same as local names.
       eventsFunctionContext: null,
       object1Names: ['obj1', 'obj1', 'obj1'],
       object2Names: ['obj2', 'obj2', 'obj2'],
+      object3Names: ['obj3', 'obj3', 'obj3'],
     },
-    // In event functions, the names in the ObjectList are different than
-    // the names of the objects from that list.
+    // In event functions, the object names from the scene are different
+    // than the local names.
     {
       caseName: 'extension',
       // The implementation only check it exists.
       eventsFunctionContext: {},
       object1Names: ['SceneObj1', 'SceneObj1', 'SceneObj1'],
       object2Names: ['SceneObj2', 'SceneObj2', 'SceneObj2'],
+      object3Names: ['SceneObj3', 'SceneObj3', 'SceneObj3'],
     },
-    // When groups are passed in parameters, the lists can even contains
-    // objects with different names.
+    // When groups are passed in parameters, it's flattened as an object.
+    // So, the lists will contains objects with different names.
     {
       caseName: 'extension with groups',
       // The implementation only check it exists.
       eventsFunctionContext: {},
       object1Names: ['SceneObj1', 'SceneObj1p', 'SceneObj1p'],
       object2Names: ['SceneObj2p', 'SceneObj2p', 'SceneObj2'],
+      object3Names: ['SceneObj3p', 'SceneObj3p', 'SceneObj3'],
     },
   ].forEach((testCase) => {
     describe(testCase.caseName, function () {
@@ -198,6 +290,7 @@ describe('gdjs.LinksManager', function () {
             testCase.caseName,
             testCase.object1Names,
             testCase.object2Names,
+            testCase.object3Names,
             testCase.eventsFunctionContext,
             legacy
           );

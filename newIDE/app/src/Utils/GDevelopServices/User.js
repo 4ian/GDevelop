e2dc5747b865a87a085234c5a75c77920d2b4554
Clément Pasteau <clement.pasteau@gmail.com>
2021-09-21 11:42:44 +0200

    Allow AuthorIds to be saved with an Extension

---
+++ newIDE/app/src/Utils/GDevelopServices/User.js
@@ -0,0 +1,36 @@
+// @flow
+import axios from 'axios';
+import { GDevelopUserApi } from './ApiConfigs';
+
+export type UserPublicProfile = {
+  id: string,
+  username?: string,
+};
+
+export type UserPublicProfileByIds = {
+  [key: string]: UserPublicProfile,
+};
+
+export const searchUserPublicProfilesByUsername = (
+  searchString: string
+): Promise<Array<UserPublicProfile>> => {
+  return axios
+    .get(`${GDevelopUserApi.baseUrl}/user-public-profile/search`, {
+      params: {
+        username: searchString,
+      },
+    })
+    .then(response => response.data);
+};
+
+export const getUserPublicProfilesByIds = (
+  ids: Array<string>
+): Promise<UserPublicProfileByIds> => {
+  return axios
+    .get(`${GDevelopUserApi.baseUrl}/user-public-profile`, {
+      params: {
+        id: ids.join(','),
+      },
+    })
+    .then(response => response.data);
+};

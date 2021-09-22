33deca92e349027bd2ca0d0e849b4a4eedb11663
Florian Rival <Florian.rival@gmail.com>
2021-09-22 16:10:34 +0100

    Upgrade to Storybook v6 (#3078)
    * Add a dropdown to choose the theme in the stories, useful to quickly check a component with all themes.
    * Add support for build-storybook (to build a static version of the Storybook). Will be useful to share the storybook on PRs later or even do visual regression testing.
    
    Test: yarn && yarn start, yarn storybook and yarn build-storybook works
    Test: npm i && npm start, npm run storybook and npm run build-storybook works
    Test: npm run build in newIDE/app works
    
    Only show in developer changelog

---
+++ newIDE/app/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/stories/index.js'],
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      docs: false,
+    }
+  }],
+};
--- newIDE/app/.storybook/preview.js
+++ newIDE/app/.storybook/preview.js
@@ -1,3 +1,6 @@
+import { configure, addDecorator } from '@storybook/react';
+import i18nProviderDecorator from '../src/stories/I18nProviderDecorator';
+
 export const globalTypes = {
   themeName: {
     name: 'Theme',
@@ -6,7 +9,17 @@ export const globalTypes = {
     toolbar: {
       icon: 'circlehollow',
       // See theme names in ThemeRegistry.js
-      items: ['GDevelop default', 'Dark', 'Nord', 'Solarized Dark'],
+      items: ['GDevelop default', 'Dark', 'Nord', 'Solarized Dark', 'One Dark'],
+      showName: true,
     },
   },
 };
+
+export const parameters = {
+  // Disable controls and docs, included in @storybook/addon-essentials,
+  // that we don't use.
+  controls: { hideNoControlsWarning: true },
+  docs: { disable: true }
+};
+
+addDecorator(i18nProviderDecorator);

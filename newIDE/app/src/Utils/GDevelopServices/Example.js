1ea79137a1329ba4a912b765e825bb5fa47e84e1
Clément Pasteau <clement.pasteau@gmail.com>
2021-09-24 11:53:59 +0200

    Change Example fetching to use the object returned and avoid unnecessary call

--- newIDE/app/src/Utils/GDevelopServices/Example.js
+++ newIDE/app/src/Utils/GDevelopServices/Example.js
@@ -27,22 +27,21 @@ export type AllExamples = {|
   filters: Filters,
 |};
 
-export const listAllExamples = (): Promise<AllExamples> => {
-  return axios
-    .get(`${GDevelopAssetApi.baseUrl}/example`)
-    .then(response => response.data)
-    .then(({ exampleShortHeadersUrl, filtersUrl }) => {
-      if (!exampleShortHeadersUrl || !filtersUrl) {
-        throw new Error('Unexpected response from the example endpoint.');
-      }
-      return Promise.all([
-        axios.get(exampleShortHeadersUrl).then(response => response.data),
-        axios.get(filtersUrl).then(response => response.data),
-      ]).then(([exampleShortHeaders, filters]) => ({
-        exampleShortHeaders,
-        filters,
-      }));
-    });
+export const listAllExamples = async (): Promise<AllExamples> => {
+  const examplesResponse = await axios.get(
+    `${GDevelopAssetApi.baseUrl}/example`
+  );
+
+  const { examplesDatabase, filtersUrl } = examplesResponse.data;
+  if (!filtersUrl) {
+    throw new Error('Unexpected response from the example endpoint.');
+  }
+  const filtersResponse = await axios.get(filtersUrl);
+
+  return {
+    exampleShortHeaders: examplesDatabase,
+    filters: filtersResponse.data,
+  };
 };
 
 export const getExample = (
@@ -50,13 +49,5 @@ export const getExample = (
 ): Promise<Example> => {
   return axios
     .get(`${GDevelopAssetApi.baseUrl}/example/${exampleShortHeader.id}`)
-    .then(response => response.data)
-    .then(({ exampleUrl }) => {
-      if (!exampleUrl) {
-        throw new Error('Unexpected response from the example endpoint.');
-      }
-
-      return axios.get(exampleUrl);
-    })
-    .then(response => response.data);
+    .then(response => response.data.example);
 };

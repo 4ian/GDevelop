e463f352b701fcfe8ea6911b8ccf051498359a82
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2021-09-29 18:16:47 +0200

    Add several UI improvements on the IDE

--- newIDE/app/src/PropertiesEditor/index.js
+++ newIDE/app/src/PropertiesEditor/index.js
@@ -199,7 +199,7 @@ export default class PropertiesEditor extends React.Component<Props, {||}> {
       this.props.unsavedChanges.triggerUnsavedChanges();
     if (this.props.onInstancesModified)
       this.props.onInstancesModified(instances);
-    else this.forceUpdate();
+    this.forceUpdate();
   };
 
   _getFieldDescription = (instances: Instances, field: ValueField): ?string => {
@@ -506,6 +506,7 @@ export default class PropertiesEditor extends React.Component<Props, {||}> {
                     instances={this.props.instances}
                     mode="row"
                     unsavedChanges={unsavedChanges}
+                    onInstancesModified={this.props.onInstancesModified}
                   />
                 )}
               </UnsavedChangesContext.Consumer>
@@ -516,11 +517,17 @@ export default class PropertiesEditor extends React.Component<Props, {||}> {
             <div key={field.name}>
               <Subheader>{field.name}</Subheader>
               <div style={styles.subPropertiesEditorContainer}>
-                <PropertiesEditor
-                  schema={field.children}
-                  instances={this.props.instances}
-                  mode="column"
-                />
+                <UnsavedChangesContext.Consumer key={field.name}>
+                  {unsavedChanges => (
+                    <PropertiesEditor
+                      schema={field.children}
+                      instances={this.props.instances}
+                      mode="column"
+                      unsavedChanges={unsavedChanges}
+                      onInstancesModified={this.props.onInstancesModified}
+                    />
+                  )}
+                </UnsavedChangesContext.Consumer>
               </div>
             </div>
           );
---
+++ newIDE/app/src/UI/Tooltip.js
@@ -0,0 +1,3 @@
+// @flow
+
+export const tooltipEnterDelay: number = 300;

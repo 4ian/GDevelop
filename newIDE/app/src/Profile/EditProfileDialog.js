48a5b9cb5f4bf85b0dec9f47c862cbd4775ec9eb
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2021-10-01 10:01:19 +0200

    Display error if username contains non-ascii chars

--- newIDE/app/src/Profile/EditProfileDialog.js
+++ newIDE/app/src/Profile/EditProfileDialog.js
@@ -31,6 +31,8 @@ export const getUsernameErrorText = (error: ?AuthError) => {
 
   if (error.code === 'auth/username-used')
     return 'This username is already used, please pick another one.';
+  if (error.code === 'auth/malformed-username')
+    return 'Your username must have less than 30 characters and only from this list: a-z A-Z 0-9 _ -';
   return undefined;
 };
 

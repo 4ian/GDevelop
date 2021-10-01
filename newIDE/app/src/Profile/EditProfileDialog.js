567efafa70386ebf3509e195edd258d6c9ef56a8
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2021-10-01 11:41:59 +0200

    Display error if username is not valid

--- newIDE/app/src/Profile/EditProfileDialog.js
+++ newIDE/app/src/Profile/EditProfileDialog.js
@@ -31,6 +31,8 @@ export const getUsernameErrorText = (error: ?AuthError) => {
 
   if (error.code === 'auth/username-used')
     return 'This username is already used, please pick another one.';
+  if (error.code === 'auth/malformed-username')
+    return 'Please pick a short username with only alphanumeric characters as well as _ and -';
   return undefined;
 };
 

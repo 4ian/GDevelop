9bbbed71f0203a3c93e45aec1f3930b873d0cc5f
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-10-05 10:46:43 +0200

    Fix typing

--- newIDE/app/src/stories/componentStories/ProfileDetails.stories.js
+++ newIDE/app/src/stories/componentStories/ProfileDetails.stories.js
@@ -7,12 +7,14 @@ import paperDecorator from '../PaperDecorator';
 
 import ProfileDetails from '../../Profile/ProfileDetails';
 import { indieUserProfile } from '../../fixtures/GDevelopServicesTestData';
+import {type Profile as ProfileType} from '../../Utils/GDevelopServices/Authentication'
 
 export default {
   title: "ProfileDetails",
   component: ProfileDetails,
   decorators: [paperDecorator, muiDecorator],
   argTypes: {
+    onEditProfile: {action: 'edit profile'},
     profile: {
       control: { type: 'radio' },
       options: ['Complete profile', 'Without username nor bio'],
@@ -29,8 +31,13 @@ export default {
   },
 }
 
-export const Profile = (args) => <ProfileDetails {...args} onEditProfile={action('edit profile')} />;
-export const Loading = (args) => <ProfileDetails profile={null} onEditProfile={action('edit profile')} />;
+type ArgsTypes = {|
+  profile: ProfileType,
+  onEditProfile: () => void,
+|}
+
+export const Profile = (args: ArgsTypes) => <ProfileDetails {...args} />;
+export const Loading = (args: ArgsTypes) => <ProfileDetails {...args} profile={null} />;
 Loading.argTypes = {
   profile: {control: {disable: true}}
 }

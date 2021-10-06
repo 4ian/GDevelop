22b58c925e94fac4fdb641d318f7cb4ed7fe6db1
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-10-06 18:17:36 +0200

    Prettier

--- newIDE/app/src/Profile/PublicProfileContext.js
+++ newIDE/app/src/Profile/PublicProfileContext.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react';
 
-export type PublicProfileOpenerType = (string) => void;
+export type PublicProfileOpenerType = string => void;
 
 export const initialPublicProfileOpener = (userId: string) => {};
 

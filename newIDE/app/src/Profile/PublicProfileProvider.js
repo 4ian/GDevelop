53c38e0c9fe7a4d9226f5f23a9a78bb16378c61d
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-10-07 12:31:28 +0200

    Reduce PublicProfileContext rerenders

--- newIDE/app/src/Profile/PublicProfileProvider.js
+++ newIDE/app/src/Profile/PublicProfileProvider.js
@@ -3,7 +3,9 @@
 import * as React from 'react';
 
 import PublicProfileDialog from './PublicProfileDialog';
-import PublicProfileContext from './PublicProfileContext';
+import PublicProfileContext, {
+  type PublicProfileState,
+} from './PublicProfileContext';
 
 type Props = {|
   children: React.Node,
@@ -15,19 +17,30 @@ export default ({ children }: Props) => {
     setVisitedPublicProfileUserId,
   ] = React.useState<?string>(null);
 
-  const openUserPublicProfile = (userId: string): void => {
-    setVisitedPublicProfileUserId(userId);
-  };
+  const openUserPublicProfile = React.useCallback(
+    (userId: string): void => {
+      setVisitedPublicProfileUserId(userId);
+    },
+    [setVisitedPublicProfileUserId]
+  );
 
-  const closeUserPublicProfile = (): void => {
-    setVisitedPublicProfileUserId(null);
-  };
+  const closeUserPublicProfile = React.useCallback(
+    (): void => {
+      setVisitedPublicProfileUserId(null);
+    },
+    [setVisitedPublicProfileUserId]
+  );
+
+  const publicProfileState: PublicProfileState = React.useMemo(
+    () => ({
+      openUserPublicProfile: openUserPublicProfile,
+    }),
+    [openUserPublicProfile]
+  );
 
   return (
     <React.Fragment>
-      <PublicProfileContext.Provider
-        value={{ openUserPublicProfile: openUserPublicProfile }}
-      >
+      <PublicProfileContext.Provider value={publicProfileState}>
         {children}
       </PublicProfileContext.Provider>
       {visitedPublicProfileUserId && (

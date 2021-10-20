ec826d9d9ac4930b2f1f715e57a35e335930dcd2
Florian Rival <Florian.Rival@gmail.com>
2021-10-20 23:09:00 +0100

    Rework console rendering to avoid using MUI List at all + simplify a few things

--- newIDE/app/src/fixtures/ConsoleTestData.js
+++ newIDE/app/src/fixtures/ConsoleTestData.js
@@ -2,22 +2,31 @@ class MockLogsManager {
   logs = [
     {
       message: 'Only a message',
+      timestamp: 120000.1234,
+    },
+    {
+      message:
+        'This is a veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy long message',
+      timestamp: 120000.1,
     },
     {
       message: 'A warning!',
       type: 'warning',
+      timestamp: 120000,
     },
     {
-      message: 'An internal and',
+      message: 'An internal error message',
       group: 'Grouped error',
       type: 'error',
       internal: true,
+      timestamp: 42123.456789,
     },
     {
       message: 'A regular message',
       group: 'Regular group',
       internal: false,
       type: 'info',
+      timestamp: 20000,
     },
   ];
   groups = new Set(['Grouped error', 'Regular group']);

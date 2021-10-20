a3a6743498516bba77848b72f7783e2a3f9913cd
Florian Rival <Florian.Rival@gmail.com>
2021-10-20 23:09:00 +0100

    Rework console rendering to avoid using MUI List at all + simplify a few things

--- newIDE/app/src/fixtures/ConsoleTestData.js
+++ newIDE/app/src/fixtures/ConsoleTestData.js
@@ -1,28 +1,46 @@
-class MockLogsManager {
-  logs = [
+// @flow
+import { type Log, LogsManager } from '../Debugger/DebuggerConsole';
+
+class MockLogsManager extends LogsManager {
+  logs: Array<Log> = [
     {
       message: 'Only a message',
+      group: '',
+      type: 'info',
+      timestamp: 120000.1234,
+    },
+    {
+      message:
+        'This is a veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy veeeeeryy long message',
+      group: '',
+      type: 'info',
+      timestamp: 120000.1,
     },
     {
       message: 'A warning!',
+      group: '',
       type: 'warning',
+      timestamp: 120000,
     },
     {
-      message: 'An internal and',
+      message: 'An internal error message',
       group: 'Grouped error',
       type: 'error',
+      timestamp: 42123.456789,
       internal: true,
     },
     {
       message: 'A regular message',
       group: 'Regular group',
-      internal: false,
       type: 'info',
+      timestamp: 20000,
+      internal: false,
     },
   ];
-  groups = new Set(['Grouped error', 'Regular group']);
+  groups = new Set<string>(['Grouped error', 'Regular group']);
   on() {}
   off() {}
+  addLog() {}
 }
 
 export default new MockLogsManager();

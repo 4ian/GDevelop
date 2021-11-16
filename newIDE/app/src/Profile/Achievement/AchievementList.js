5e7f0485a3e7f8e8f7dc04ea5baedd437ec7ee0d
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-11-16 16:47:26 +0100

    Fetch achievements and display them

--- newIDE/app/src/Profile/Achievement/AchievementList.js
+++ newIDE/app/src/Profile/Achievement/AchievementList.js
@@ -1,29 +1,85 @@
 // @flow
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { DateFormat } from '@lingui/macro';
 
 import { Column, Line } from '../../UI/Grid';
 import Text from '../../UI/Text';
-import { type Badge } from '../../Utils/GDevelopServices/User';
+import {
+  type Badge,
+  type Achievement,
+} from '../../Utils/GDevelopServices/User';
 
 type Props = {|
   badges: Array<Badge>,
+  achievements: Array<Achievement>,
 |};
+
+const achievementBaseline = {
+  width: '100%',
+  padding: '0 20px',
+};
 
 const styles = {
   achievementsContainer: {
     maxHeight: 250,
     overflowY: 'scroll',
   },
+  unlockedAchievement: {
+    ...achievementBaseline,
+    color: 'green',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  lockedAchievement: {
+    ...achievementBaseline,
+    color: '#CCCCCC',
+  },
 };
 
-const AchievementList = ({ badges }: Props) => {
-  console.log(badges);
+const AchievementList = ({ badges, achievements }: Props) => {
+  const [formattedAchievements, setFormattedAchievements] = useState([]);
+  useEffect(
+    () => {
+      const badgeByAchievementId = badges.reduce((acc, badge) => {
+        acc[badge.achievementId] = badge;
+        return acc;
+      }, {});
+
+      setFormattedAchievements(
+        achievements.map(achievement => {
+          if (Object.keys(badgeByAchievementId).includes(achievement.id)) {
+            return {
+              ...achievement,
+              unlockedAt: badgeByAchievementId[achievement.id].unlockedAt,
+            };
+          } else {
+            return achievement;
+          }
+        })
+      );
+    },
+    [badges, achievements]
+  );
+
   return (
     <Column>
       <div style={styles.achievementsContainer}>
-        {badges.map(badge => (
-          <Line>
-            <Text>{badge.achievementId}</Text>
+        {formattedAchievements.map(achievement => (
+          <Line key={achievement.id}>
+            <div
+              style={
+                achievement.unlockedAt
+                  ? styles.unlockedAchievement
+                  : styles.lockedAchievement
+              }
+            >
+              <Text>{achievement.name}</Text>
+              {achievement.unlockedAt && (
+                <Text>
+                  <DateFormat>{achievement.unlockedAt}</DateFormat>
+                </Text>
+              )}
+            </div>
           </Line>
         ))}
       </div>

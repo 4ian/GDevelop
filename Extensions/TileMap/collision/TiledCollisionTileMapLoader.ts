6ba21b4a902fac03416ecd2e42b52133ac68124c
Davy Hélard <davy.helard@gmail.com>
2021-11-17 17:27:30 +0100

    Handle layers

--- Extensions/TileMap/collision/TiledCollisionTileMapLoader.ts
+++ Extensions/TileMap/collision/TiledCollisionTileMapLoader.ts
@@ -44,24 +44,26 @@ namespace gdjs {
           tiledMap.height,
           definitions
         );
-        // TODO handle multiple layer
-        const layer = tiledMap.layers[0];
 
-        if (layer.type === 'tilelayer') {
+        for (const tiledLayer of tiledMap.layers) {
+        if (tiledLayer.type === 'tilelayer') {
           let tileSlotIndex = 0;
           let layerData: integer[] | null = null;
 
-          if (layer.encoding === 'base64') {
-            layerData = gdjs.TileMap.decodeBase64LayerData(layer);
+          if (tiledLayer.encoding === 'base64') {
+            layerData = gdjs.TileMap.decodeBase64LayerData(tiledLayer);
             if (!layerData) {
               console.warn('Failed to uncompress layer.data');
             }
           } else {
-            layerData = layer.data as integer[];
+            layerData = tiledLayer.data as integer[];
           }
           if (layerData) {
-            for (let y = 0; y < layer.height; y++) {
-              for (let x = 0; x < layer.width; x++) {
+            const collisionTileLayer = collisionTileMap.addLayer(tiledLayer.id);
+            // TODO handle layer offset
+
+            for (let y = 0; y < tiledLayer.height; y++) {
+              for (let x = 0; x < tiledLayer.width; x++) {
                 // The "globalTileUid" is the tile UID with encoded
                 // bits about the flipping/rotation of the tile.
                 const globalTileUid = layerData[tileSlotIndex];
@@ -70,13 +72,14 @@ namespace gdjs {
                   globalTileUid
                   );
                 //console.log("globalTileUid: " + tileUid.id + " " + tileUid.flippedHorizontally + " " + tileUid.flippedVertically + " " + tileUid.flippedDiagonally);
-                collisionTileMap.set(x, y, tileUid.id - 1, tileUid.flippedHorizontally, tileUid.flippedVertically, tileUid.flippedDiagonally);
+                collisionTileLayer.set(x, y, tileUid.id - 1, tileUid.flippedHorizontally, tileUid.flippedVertically, tileUid.flippedDiagonally);
 
                 tileSlotIndex += 1;
               }
             }
           }
         }
+      }
 
         return collisionTileMap;
       }

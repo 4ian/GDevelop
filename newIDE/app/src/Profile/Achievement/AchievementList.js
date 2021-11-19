226bbf3ce31a3775efc3efbdff86bad525e20e51
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-11-19 16:30:25 +0100

    Simplify achievement formatting logic

--- newIDE/app/src/Profile/Achievement/AchievementList.js
+++ newIDE/app/src/Profile/Achievement/AchievementList.js
@@ -18,12 +18,19 @@ type Props = {|
   displayUnclaimedAchievements: boolean,
 |};
 
+type FormattedAchievement = {|
+  ...Achievement,
+  unlockedAt: ?Date,
+|};
+
 const AchievementList = ({
   badges,
   achievements,
   displayUnclaimedAchievements,
 }: Props) => {
-  const [formattedAchievements, setFormattedAchievements] = useState([]);
+  const [formattedAchievements, setFormattedAchievements] = useState<
+    Array<FormattedAchievement>
+  >([]);
 
   const styles = {
     achievementsContainer: {
@@ -37,35 +44,27 @@ const AchievementList = ({
 
   useEffect(
     () => {
-      let formattedAchievements = [];
-      if (displayUnclaimedAchievements) {
-        const badgeByAchievementId = badges.reduce((acc, badge) => {
-          acc[badge.achievementId] = badge;
-          return acc;
-        }, {});
+      const badgeByAchievementId = badges.reduce((acc, badge) => {
+        acc[badge.achievementId] = badge;
+        return acc;
+      }, {});
 
-        formattedAchievements = achievements.map(achievement => {
-          if (!!badgeByAchievementId[achievement.id]) {
-            return {
-              ...achievement,
-              unlockedAt: badgeByAchievementId[achievement.id].unlockedAt,
-            };
-          }
-          return achievement;
-        });
-      } else {
-        formattedAchievements = badges.map(badge => {
-          return {
-            ...achievements.find(
-              achievement => achievement.id === badge.achievementId
-            ),
-            unlockedAt: badge.unlockedAt,
-          };
-        });
-      }
-      formattedAchievements.sort((a, b) => {
-        if (a.unlockedAt && b.unlockedAt) {
-          return parseISO(b.unlockedAt) - parseISO(a.unlockedAt);
+      const achievementsWithDate = achievements.reduce((acc, achievement) => {
+        const badge = badgeByAchievementId[achievement.id];
+        const hasBadge = !!badge;
+        if (hasBadge || (!hasBadge && displayUnclaimedAchievements)) {
+          acc.push({
+            ...achievement,
+            unlockedAt: hasBadge ? parseISO(badge.unlockedAt) : null,
+          });
+        }
+
+        return acc;
+      }, []);
+
+      achievementsWithDate.sort((a, b) => {
+        if (b.unlockedAt && a.unlockedAt) {
+          return b.unlockedAt - a.unlockedAt;
         } else if (a.unlockedAt && !b.unlockedAt) {
           return -1;
         } else if (!a.unlockedAt && b.unlockedAt) {
@@ -74,7 +73,8 @@ const AchievementList = ({
           return 0;
         }
       });
-      setFormattedAchievements(formattedAchievements);
+
+      setFormattedAchievements(achievementsWithDate);
     },
     [badges, achievements, displayUnclaimedAchievements]
   );
@@ -84,28 +84,31 @@ const AchievementList = ({
       <I18n>
         {({ i18n }) => (
           <ScrollView style={styles.achievementsContainer}>
-            {formattedAchievements.map(achievement => (
-              <Line
-                key={achievement.id}
-                justifyContent="space-between"
-                padding="0 20px"
-              >
-                <Text
-                  style={
-                    achievement.unlockedAt
-                      ? styles.unlockedAchievement
-                      : styles.lockedAchievement
-                  }
-                >
-                  {achievement.name}
-                </Text>
-                {achievement.unlockedAt ? (
-                  <Text>{i18n.date(parseISO(achievement.unlockedAt))}</Text>
-                ) : (
-                  <Lock style={styles.lockedAchievement} />
-                )}
-              </Line>
-            ))}
+            {formattedAchievements.map(
+              achievement =>
+                achievement && (
+                  <Line
+                    key={achievement.id}
+                    justifyContent="space-between"
+                    padding="0 20px"
+                  >
+                    <Text
+                      style={
+                        achievement.unlockedAt
+                          ? styles.unlockedAchievement
+                          : styles.lockedAchievement
+                      }
+                    >
+                      {achievement.name}
+                    </Text>
+                    {achievement.unlockedAt ? (
+                      <Text>{i18n.date(achievement.unlockedAt)}</Text>
+                    ) : (
+                      <Lock style={styles.lockedAchievement} />
+                    )}
+                  </Line>
+                )
+            )}
           </ScrollView>
         )}
       </I18n>

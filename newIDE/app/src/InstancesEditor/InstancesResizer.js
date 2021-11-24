db05e98bc8989daf5228f06864d801618bccf1bd
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2021-11-24 17:12:01 +0100

    Improve object lock behavior in the editor
    * The objects are now selectable, to simplify unlocking them

--- newIDE/app/src/InstancesEditor/InstancesResizer.js
+++ newIDE/app/src/InstancesEditor/InstancesResizer.js
@@ -142,7 +142,13 @@ export default class InstancesResizer {
     this.totalDeltaX += deltaX;
     this.totalDeltaY += deltaY;
 
-    const initialSelectionAABB = this._getOrCreateSelectionAABB(instances);
+    const nonLockedInstances = instances.filter(
+      instance => !instance.isLocked()
+    );
+
+    const initialSelectionAABB = this._getOrCreateSelectionAABB(
+      nonLockedInstances
+    );
     if (!initialSelectionAABB) return;
 
     // Round the grabbed handle position on the grid.
@@ -212,7 +218,7 @@ export default class InstancesResizer {
           initialSelectionAABB.height()
         : flippedTotalDeltaY;
 
-    const hasRotatedInstance = areAnyInstancesNotStraight(instances);
+    const hasRotatedInstance = areAnyInstancesNotStraight(nonLockedInstances);
 
     // Applying a rotation then a scaling can result to
     // an affine transformation with a shear transformation - which we don't want.
@@ -258,8 +264,8 @@ export default class InstancesResizer {
       resizeGrabbingRelativePositions[grabbingLocation][1] *
         initialSelectionAABB.height();
 
-    for (let i = 0; i < instances.length; i++) {
-      const selectedInstance = instances[i];
+    for (let i = 0; i < nonLockedInstances.length; i++) {
+      const selectedInstance = nonLockedInstances[i];
 
       let initialUnrotatedInstanceAABB = this._getOrCreateUnrotatedInstanceAABB(
         selectedInstance

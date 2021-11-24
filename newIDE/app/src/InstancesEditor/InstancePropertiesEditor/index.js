98c1a93da5fea875ef93068591f4551e9241a5fe
Florian Rival <Florian.Rival@gmail.com>
2021-11-24 15:26:44 +0000

    Update a wording the instance properties editor
    Don't show in changelog

--- newIDE/app/src/InstancesEditor/InstancePropertiesEditor/index.js
+++ newIDE/app/src/InstancesEditor/InstancePropertiesEditor/index.js
@@ -94,14 +94,14 @@ export default class InstancePropertiesEditor extends React.Component<Props> {
         instance.setLayer(newValue),
     },
     {
-      name: this.props.i18n._(t`Custom size?`),
+      name: this.props.i18n._(t`Custom size`),
       valueType: 'boolean',
       getValue: (instance: gdInitialInstance) => instance.hasCustomSize(),
       setValue: (instance: gdInitialInstance, newValue: boolean) =>
         instance.setHasCustomSize(newValue),
     },
     {
-      name: this.props.i18n._(t`Custom size`),
+      name: 'custom-size-row',
       type: 'row',
       children: [
         {

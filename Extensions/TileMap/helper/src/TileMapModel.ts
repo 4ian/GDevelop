658331bc2502e1e5d1293d5f6e35b4d8df4bc31c
Davy Hélard <davy.helard@gmail.com>
2021-11-29 03:44:52 +0100

    TODO pass

--- Extensions/TileMap/helper/src/TileMapModel.ts
+++ Extensions/TileMap/helper/src/TileMapModel.ts
@@ -139,62 +139,104 @@ namespace gdjs {
       }
 
       getTileId(): integer {
-        return this.tileId & EditableTileMapLayer.tileIdMask;
+        return FlippingHelper.getTileId(this.tileId);
       }
 
       setFlippedHorizontally(flippedHorizontally: boolean) {
-        let tileId = this.tileId;
-        tileId &= ~EditableTileMapLayer.flippedHorizontallyFlag;
-        if (flippedHorizontally) {
-          tileId |= EditableTileMapLayer.flippedHorizontallyFlag;
-        }
-        this.tileId = tileId;
-      }
-
-      setFlippedVertically(flippedVertically: boolean) {
-        let tileId = this.tileId;
-        tileId &= ~EditableTileMapLayer.flippedVerticallyFlag;
-        if (flippedVertically) {
-          tileId |= EditableTileMapLayer.flippedVerticallyFlag;
-        }
-        this.tileId = tileId;
-      }
-
-      setFlippedDiagonally(flippedDiagonally: boolean) {
-        let tileId = this.tileId;
-        tileId &= ~EditableTileMapLayer.flippedDiagonallyFlag;
-        if (flippedDiagonally) {
-          tileId |= EditableTileMapLayer.flippedDiagonallyFlag;
-        }
-        this.tileId = tileId;
-      }
-
-      isFlippedHorizontally(): boolean {
-        return (
-          (this.tileId & EditableTileMapLayer.flippedHorizontallyFlag) !== 0
+        this.tileId = FlippingHelper.setFlippedHorizontally(
+          this.tileId,
+          flippedHorizontally
         );
       }
 
+      setFlippedVertically(flippedVertically: boolean) {
+        this.tileId = FlippingHelper.setFlippedVertically(
+          this.tileId,
+          flippedVertically
+        );
+      }
+
+      setFlippedDiagonally(flippedDiagonally: boolean) {
+        this.tileId = FlippingHelper.setFlippedDiagonally(
+          this.tileId,
+          flippedDiagonally
+        );
+      }
+
+      isFlippedHorizontally(): boolean {
+        return FlippingHelper.isFlippedHorizontally(this.tileId);
+      }
+
       isFlippedVertically(): boolean {
-        return (this.tileId & EditableTileMapLayer.flippedVerticallyFlag) !== 0;
+        return FlippingHelper.isFlippedVertically(this.tileId);
       }
 
       isFlippedDiagonally(): boolean {
-        return (this.tileId & EditableTileMapLayer.flippedDiagonallyFlag) !== 0;
+        return FlippingHelper.isFlippedDiagonally(this.tileId);
       }
     }
 
-    export class EditableTileMapLayer extends AbstractEditableLayer {
-      // TODO factorize flags handling?
+    class FlippingHelper {
       static readonly flippedHorizontallyFlag = 0x80000000;
       static readonly flippedVerticallyFlag = 0x40000000;
       static readonly flippedDiagonallyFlag = 0x20000000;
       static readonly tileIdMask = ~(
-        EditableTileMapLayer.flippedHorizontallyFlag |
-        EditableTileMapLayer.flippedVerticallyFlag |
-        EditableTileMapLayer.flippedDiagonallyFlag
+        FlippingHelper.flippedHorizontallyFlag |
+        FlippingHelper.flippedVerticallyFlag |
+        FlippingHelper.flippedDiagonallyFlag
       );
 
+      static getTileId(tileId: integer): integer {
+        return tileId & FlippingHelper.tileIdMask;
+      }
+
+      static setFlippedHorizontally(
+        tileId: integer,
+        flippedHorizontally: boolean
+      ): integer {
+        tileId &= ~FlippingHelper.flippedHorizontallyFlag;
+        if (flippedHorizontally) {
+          tileId |= FlippingHelper.flippedHorizontallyFlag;
+        }
+        return tileId;
+      }
+
+      static setFlippedVertically(
+        tileId: integer,
+        flippedVertically: boolean
+      ): integer {
+        tileId &= ~FlippingHelper.flippedVerticallyFlag;
+        if (flippedVertically) {
+          tileId |= FlippingHelper.flippedVerticallyFlag;
+        }
+        return tileId;
+      }
+
+      static setFlippedDiagonally(
+        tileId: integer,
+        flippedDiagonally: boolean
+      ): integer {
+        tileId &= ~FlippingHelper.flippedDiagonallyFlag;
+        if (flippedDiagonally) {
+          tileId |= FlippingHelper.flippedDiagonallyFlag;
+        }
+        return tileId;
+      }
+
+      static isFlippedHorizontally(tileId: integer): boolean {
+        return (tileId & FlippingHelper.flippedHorizontallyFlag) !== 0;
+      }
+
+      static isFlippedVertically(tileId: integer): boolean {
+        return (tileId & FlippingHelper.flippedVerticallyFlag) !== 0;
+      }
+
+      static isFlippedDiagonally(tileId: integer): boolean {
+        return (tileId & FlippingHelper.flippedDiagonallyFlag) !== 0;
+      }
+    }
+
+    export class EditableTileMapLayer extends AbstractEditableLayer {
       private readonly _tiles: Array<Int32Array>;
 
       constructor(tileMap: EditableTileMap, id: integer) {
@@ -206,15 +248,19 @@ namespace gdjs {
         }
       }
 
-      setTile(x: integer, y: integer, definitionIndex: integer) {
+      setTile(x: integer, y: integer, definitionIndex: integer): void {
         const definition = this.tileMap.getTileDefinition(definitionIndex);
         if (!definition) {
-          // The tile has no collision mask.
-          return;
+          throw new Error(`Invalid tile definition index: ${definitionIndex}`);
         }
         //console.log(x + " " + y + " set: " + definitionIndex);
         // +1 because 0 mean null
         this._tiles[y][x] = definitionIndex + 1;
+      }
+
+      removeTile(x: integer, y: integer): void {
+        // 0 mean null
+        this._tiles[y][x] = 0;
       }
 
       setFlippedHorizontally(
@@ -222,49 +268,48 @@ namespace gdjs {
         y: integer,
         flippedHorizontally: boolean
       ) {
-        let tileId = this._tiles[y][x];
-        tileId &= ~EditableTileMapLayer.flippedHorizontallyFlag;
-        if (flippedHorizontally) {
-          tileId |= EditableTileMapLayer.flippedHorizontallyFlag;
+        const tileId = this._tiles[y][x];
+        if (tileId === 0) {
+          return;
         }
-        this._tiles[y][x] = tileId;
+        this._tiles[y][x] = FlippingHelper.setFlippedHorizontally(
+          tileId,
+          flippedHorizontally
+        );
       }
 
       setFlippedVertically(x: integer, y: integer, flippedVertically: boolean) {
-        let tileId = this._tiles[y][x];
-        tileId &= ~EditableTileMapLayer.flippedVerticallyFlag;
-        if (flippedVertically) {
-          tileId |= EditableTileMapLayer.flippedVerticallyFlag;
+        const tileId = this._tiles[y][x];
+        if (tileId === 0) {
+          return;
         }
-        this._tiles[y][x] = tileId;
+        this._tiles[y][x] = FlippingHelper.setFlippedVertically(
+          tileId,
+          flippedVertically
+        );
       }
 
       setFlippedDiagonally(x: integer, y: integer, flippedDiagonally: boolean) {
-        let tileId = this._tiles[y][x];
-        tileId &= ~EditableTileMapLayer.flippedDiagonallyFlag;
-        if (flippedDiagonally) {
-          tileId |= EditableTileMapLayer.flippedDiagonallyFlag;
+        const tileId = this._tiles[y][x];
+        if (tileId === 0) {
+          return;
         }
-        this._tiles[y][x] = tileId;
+        this._tiles[y][x] = FlippingHelper.setFlippedDiagonally(
+          tileId,
+          flippedDiagonally
+        );
       }
 
       isFlippedHorizontally(x: integer, y: integer): boolean {
-        return (
-          (this._tiles[y][x] & EditableTileMapLayer.flippedHorizontallyFlag) !==
-          0
-        );
+        return FlippingHelper.isFlippedHorizontally(this._tiles[y][x]);
       }
 
       isFlippedVertically(x: integer, y: integer): boolean {
-        return (
-          (this._tiles[y][x] & EditableTileMapLayer.flippedVerticallyFlag) !== 0
-        );
+        return FlippingHelper.isFlippedVertically(this._tiles[y][x]);
       }
 
       isFlippedDiagonally(x: integer, y: integer): boolean {
-        return (
-          (this._tiles[y][x] & EditableTileMapLayer.flippedDiagonallyFlag) !== 0
-        );
+        return FlippingHelper.isFlippedDiagonally(this._tiles[y][x]);
       }
 
       get(x: integer, y: integer): integer | undefined {
@@ -273,7 +318,8 @@ namespace gdjs {
           return undefined;
         }
         // -1 because 0 is keep for null.
-        return (row[x] - 1) & EditableTileMapLayer.tileIdMask;
+        const tileId = FlippingHelper.getTileId(row[x] - 1);
+        return tileId;
       }
 
       dimX() {

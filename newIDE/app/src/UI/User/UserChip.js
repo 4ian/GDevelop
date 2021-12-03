d4eebcb1fb554898038dd5897395049586e5735e
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-03 10:57:19 +0100

    Create UserChip component

---
+++ newIDE/app/src/UI/User/UserChip.js
@@ -0,0 +1,36 @@
+// @flow
+import * as React from 'react';
+import { Trans } from '@lingui/macro';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
+import Avatar from '@material-ui/core/Avatar';
+import { type Profile } from '../../Utils/GDevelopServices/Authentication';
+import { getGravatarUrl } from '../GravatarUrl';
+
+type Props = {|
+  profile: ?Profile,
+  onClick: () => void,
+|};
+
+const UserChip = ({ profile, onClick }: Props) => {
+  return (
+    <Chip
+      variant="outlined"
+      avatar={profile ? (
+        <Avatar src={getGravatarUrl(profile.email || '', { size: 30 })} sx={{ width: 30, height: 30 }}/>
+      ) : (
+        <FaceIcon />
+      )}
+      label={
+        profile ? (
+          profile.username || profile.email
+        ) : (
+          <Trans>Click to connect</Trans>
+        )
+      }
+      onClick={onClick}
+    />
+  );
+};
+
+export default UserChip;

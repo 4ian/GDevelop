29740d9991bc35c278c3a56e1266fd9f40583663
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-07 11:45:30 +0100

    Use switch instead of if for showcased button rendering

--- newIDE/app/src/GamesShowcase/ShowcasedGameButtons.js
+++ newIDE/app/src/GamesShowcase/ShowcasedGameButtons.js
@@ -28,40 +28,40 @@ const getFullUrl = url =>
   url.startsWith('/') ? 'https://gdevelop-app.com' + url : url;
 
 const getLinkIconAndLabel = (url: string, type: string) => {
-  // Supported links:
-  if (type === '') {
-    return { icon: <PlayArrowIcon />, label: <Trans>Play or download</Trans> };
-  } else if (type === 'learn-more') {
-    return { icon: null, label: <Trans>Learn more</Trans> };
-  } else if (type === 'download' || type === 'download-win-mac-linux') {
-    return { icon: <GetAppIcon />, label: <Trans>Download</Trans> };
-  } else if (type === 'play') {
-    return { icon: <PlayArrowIcon />, label: <Trans>Play</Trans> };
+  switch (type) {
+    // Supported links:
+    case '':
+      return { icon: <PlayArrowIcon />, label: <Trans>Play or download</Trans> };
+    case 'learn-more':
+      return { icon: null, label: <Trans>Learn more</Trans> };
+    case 'download' || type === 'download-win-mac-linux':
+      return { icon: <GetAppIcon />, label: <Trans>Download</Trans> };
+    case 'play':
+      return { icon: <PlayArrowIcon />, label: <Trans>Play</Trans> };
+    // Officially supported stores/websites/social medias:
+    case 'App Store':
+      return { icon: <Apple />, label: 'iOS' };
+    case 'Play Store':
+      return { icon: <GooglePlay />, label: 'Android' };
+    case 'Steam':
+      return { icon: <Steam />, label: 'Steam' };
+    case 'Trailer':
+      return { icon: <YouTubeIcon />, label: <Trans>Trailer</Trans> };
+    case 'itch.io':
+      return { icon: <ItchIo />, label: 'itch.io' };
+    case 'Game Jolt':
+      return { icon: <FlashOnIcon />, label: 'Game Jolt' };
+    case 'Twitter':
+      return { icon: <Twitter />, label: 'Twitter' };
+    case 'Microsoft Store':
+      return { icon: <Microsoft />, label: 'Microsoft Store' };
+    case 'Instagram':
+      return { icon: <Instagram />, label: 'Instagram' };
+    case 'Twitch':
+      return { icon: <Twitch />, label: 'Twitch' };
+    default:
+      return { icon: null, label: type };
   }
-  // Officially supported stores/websites/social medias:
-  else if (type === 'App Store') {
-    return { icon: <Apple />, label: 'iOS' };
-  } else if (type === 'Play Store') {
-    return { icon: <GooglePlay />, label: 'Android' };
-  } else if (type === 'Steam') {
-    return { icon: <Steam />, label: 'Steam' };
-  } else if (type === 'Trailer') {
-    return { icon: <YouTubeIcon />, label: <Trans>Trailer</Trans> };
-  } else if (type === 'itch.io') {
-    return { icon: <ItchIo />, label: 'itch.io' };
-  } else if (type === 'Game Jolt') {
-    return { icon: <FlashOnIcon />, label: 'Game Jolt' };
-  } else if (type === 'Twitter') {
-    return { icon: <Twitter />, label: 'Twitter' };
-  } else if (type === 'Microsoft Store') {
-    return { icon: <Microsoft />, label: 'Microsoft Store' };
-  } else if (type === 'Instagram') {
-    return { icon: <Instagram />, label: 'Instagram' };
-  } else if (type === 'Twitch') {
-    return { icon: <Twitch />, label: 'Twitch' };
-  }
-
-  return { icon: null, label: type };
 };
 
 const LinkButton = ({

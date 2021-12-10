1f9d7860cf96ec50c241c735af7c5eff6c93c4fe
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-10 17:03:31 +0100

    Improve message

--- newIDE/app/src/Hints/index.js
+++ newIDE/app/src/Hints/index.js
@@ -152,5 +152,5 @@ export const getExtraInstructionInformation = (type: string): ?Hint => {
 
 export const getWarningAboutPlatformerCollisionMaskEditing = (): ?Hint => ({
   kind: 'warning',
-  message: t`You currently use a custom collision mask for an object that has the Platformer behavior. To prevent issues when flipping object horizontally while grabbing an edge, make sure your collision mask does not move when flipping it.`,
+  message: t`You currently use a custom collision mask for an object that has the Platformer behavior. A platformer with a custom collision mask could experience issues if it is flipped horizontally near a wall. To avoid that, make sure your collision mask does not move when flipping it by centering it.`,
 });

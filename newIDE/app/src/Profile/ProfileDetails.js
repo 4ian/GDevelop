9bb3f695a0da9c4c9b14417860371f9239eee7c8
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-15 16:46:38 +0100

    Add badge around achievement name in achievement list component

--- newIDE/app/src/Profile/ProfileDetails.js
+++ newIDE/app/src/Profile/ProfileDetails.js
@@ -107,6 +107,7 @@ const ProfileDetails = ({
             <UserAchievements
               badges={badges}
               displayUnclaimedAchievements={!!isAuthenticatedUserProfile}
+              displayNotifications={!!isAuthenticatedUserProfile}
             />
           )}
         </Column>

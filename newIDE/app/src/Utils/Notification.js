5869e31f8b044ca55b17cb93455bb1a699f1ac70
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-16 14:42:12 +0100

    Remove console log

--- newIDE/app/src/Utils/Notification.js
+++ newIDE/app/src/Utils/Notification.js
@@ -5,7 +5,6 @@ import { type AuthenticatedUser } from '../Profile/AuthenticatedUserContext';
 export const arePendingNotifications = (
   authenticatedUser: AuthenticatedUser
 ): boolean => {
-  console.log(authenticatedUser.authenticated)
   if (!authenticatedUser.authenticated) return false;
 
   const { badges } = authenticatedUser;

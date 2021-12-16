d56a530e9ac0c5d602a893897f060c85d273d309
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-16 10:36:16 +0100

    Add badge props necessary to positionning

--- newIDE/app/src/UI/Badge.js
+++ newIDE/app/src/UI/Badge.js
@@ -1,15 +1,28 @@
 // @flow
 import * as React from 'react';
+import ClassNameMap from '@material-ui/styles';
 import MuiBadge from '@material-ui/core/Badge';
 
 type Props = {|
   children: React.Node,
   badgeContent?: React.Node,
-  variant?: 'dot',
   invisible?: boolean,
   color: 'error' | 'primary' | 'secondary' | 'default',
+  variant?: 'dot',
+  overlap?: 'circle',
+  anchor?: 'topLeft',
+  classes?: ClassNameMap,
 |};
 
-const Badge = (props: Props) => <MuiBadge {...props} />;
+const Badge = ({ anchor, ...otherProps }: Props) => (
+  <MuiBadge
+    anchorOrigin={
+      anchor === 'topLeft'
+        ? { horizontal: 'left', vertical: 'top' }
+        : { horizontal: 'right', vertical: 'top' }
+    }
+    {...otherProps}
+  />
+);
 
 export default Badge;

c641e773e39d9ff8667adec3f17a3e48d190ed49
Davy Hélard <davy.helard@gmail.com>
2021-12-18 01:51:52 +0100

    Review changes on almost equal.

--- GDJS/Runtime/gd.ts
+++ GDJS/Runtime/gd.ts
@@ -520,21 +520,23 @@ namespace gdjs {
    * @returns true when a and b are within a relative margin error.
    */
   export const nearlyEqual = (a: float, b: float, epsilon: float): boolean => {
+    if (a === b) {
+      // also handles infinities
+      return true;
+    }
+
     const absA = Math.abs(a);
     const absB = Math.abs(b);
-    const diff = Math.abs(a - b);
+    const absDiff = Math.abs(a - b);
 
-    if (a === b) {
-      // shortcut, handles infinities
-      return true;
-    } else if (a == 0 || b == 0 || absA + absB < Number.EPSILON) {
+    if (a === 0 || b === 0 || absA + absB < Number.EPSILON) {
       // a or b is zero or both are extremely close to it
       // relative error is less meaningful here
-      return diff < epsilon * Number.EPSILON;
-    } else {
-      // use relative error
-      return diff / Math.min(absA + absB, Number.MAX_VALUE) < epsilon;
+      return absDiff < epsilon * Number.EPSILON;
     }
+
+    // use relative error
+    return absDiff / Math.min(absA + absB, Number.MAX_VALUE) < epsilon;
   };
 }
 

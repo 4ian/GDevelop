e162968f4bfcf286225e318e1f187828dffceee9
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-28 17:08:01 +0100

    Use onApply for SceneProperties dialog

--- newIDE/app/src/SceneEditor/ScenePropertiesDialog.js
+++ newIDE/app/src/SceneEditor/ScenePropertiesDialog.js
@@ -161,7 +161,7 @@ export default class ScenePropertiesDialog extends Component<Props, State> {
         ]}
         open={this.props.open}
         cannotBeDismissed={true}
-        canSubmitLastAction
+        onApply={this._onApply}
         onRequestClose={this.props.onClose}
         maxWidth="sm"
       >

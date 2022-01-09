6fe7f84dbbf2534a8aca83aa03041aec0a097d99
Leo_Red <midhilm457@gmail.com>
2022-01-09 15:41:58 +0530

    Update Step.js

--- newIDE/app/src/UI/CustomSvgIcons/Behaviors/Step.js
+++ newIDE/app/src/UI/CustomSvgIcons/Behaviors/Step.js
@@ -4,21 +4,25 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 export default props => (
   <SvgIcon {...props} viewBox="0 -6 24 24">
     <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-      <g id="Step" fillRule="nonzero">
+      <g id="Step">
         <polygon
           id="Shape"
-          fill="#2B3990"
-          points="7.5 4.5 3 0 3 3 0 3 0 6 3 6 3 9"
-        />
-        <path
-          fill="#27aae1"
-          d="m 12,1.5 a 3,3 0 0 0 -3,3 3,3 0 0 0 3,3 3,3 0 0 0 3,-3 3,3 0 0 0 -3,-3 z M 12,3 A 1.5,1.5 0 0 1 13.5,4.5 1.5,1.5 0 0 1 12,6 1.5,1.5 0 0 1 10.5,4.5 1.5,1.5 0 0 1 12,3 Z"
-          id="Oval"
+          points="0 3 0 6 3 6 3 9 7.5 4.5 3 0 3 3"
+          fill="#2b3990"
         />
         <polygon
-          id="Shape"
-          fill="#2B3990"
-          points="24 4.5 19.5 0 19.5 3 16.5 3 16.5 6 19.5 6 19.5 9"
+          id="polygon6"
+          points="16.5 3 16.5 6 19.5 6 19.5 9 24 4.5 19.5 0 19.5 3"
+          fill="#2b3990"
+        />
+        <circle
+          id="path825"
+          cx="12"
+          cy="4.5"
+          r="2.2509"
+          fill="none"
+          stroke="#27aae1"
+          strokeWidth="1.5"
         />
       </g>
     </g>

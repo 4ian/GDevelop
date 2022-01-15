aaca9dd41249904dbcad08771cbca602567206d5
Florian Rival <Florian.Rival@gmail.com>
2022-01-15 14:08:37 +0000

    [WIP] Switch to a container with no overflow and no scroll

--- Extensions/TextInput/textinputruntimeobject-pixi-renderer.ts
+++ Extensions/TextInput/textinputruntimeobject-pixi-renderer.ts
@@ -1,4 +1,14 @@
 namespace gdjs {
+  const userFriendlyToHtmlInputTypes = {
+    text: 'text',
+    email: 'email',
+    password: 'password',
+    number: 'number',
+    'telephone number': 'tel',
+    url: 'url',
+    search: 'search',
+  };
+
   class TextInputRuntimeObjectPixiRenderer {
     private _object: gdjs.TextInputRuntimeObject;
     private _input: HTMLInputElement;
@@ -9,18 +19,31 @@ namespace gdjs {
       this._object = runtimeObject;
       this._runtimeScene = runtimeObject.getRuntimeScene();
       this._runtimeGame = this._runtimeScene.getGame();
+
       this._input = document.createElement('input');
       this._input.style.border = '1px solid black';
-      this._input.style.background = 'transparent';
+      this._input.style.borderRadius = '0px';
+      this._input.style.background = 'white';
+      this._input.style.position = 'absolute';
 
       this._input.addEventListener('input', () => {
-        this._object.setValue(this._input.value);
+        this._object.onRendererInputValueChanged(this._input.value);
       });
-      this._input.style.position = 'absolute';
+      this._input.addEventListener('touchstart', () => {
+        // Focus directly when touching the input on touchscreens.
+        this._input.focus();
+      });
 
       this.updateValue();
       this.updateFont();
-      document.body.appendChild(this._input);
+      this.updatePlaceholder();
+      this.updateOpacity();
+      this.updateInputType();
+
+      this._runtimeGame
+        .getRenderer()
+        .getDomElementContainer()!
+        .appendChild(this._input);
     }
 
     onDestroy() {
@@ -43,58 +66,68 @@ namespace gdjs {
         0
       );
 
-      // Clamp the area to be inside the game screen.
-      if (topLeftCanvasCoordinates[0] < 0) topLeftCanvasCoordinates[0] = 0;
-      if (topLeftCanvasCoordinates[1] < 0) topLeftCanvasCoordinates[1] = 0;
-      if (
-        bottomRightCanvasCoordinates[0] > runtimeGame.getGameResolutionWidth()
-      )
-        bottomRightCanvasCoordinates[0] = runtimeGame.getGameResolutionWidth();
-      if (
-        bottomRightCanvasCoordinates[1] > runtimeGame.getGameResolutionHeight()
-      )
-        bottomRightCanvasCoordinates[1] = runtimeGame.getGameResolutionHeight();
-
-      if (bottomRightCanvasCoordinates[0] < topLeftCanvasCoordinates[0] ||
-        bottomRightCanvasCoordinates[1] < topLeftCanvasCoordinates[1]) {
+      // Hide the input entirely if not visible at all
+      const isOutsideCanvas =
+        bottomRightCanvasCoordinates[0] < 0 ||
+        bottomRightCanvasCoordinates[1] < 0 ||
+        topLeftCanvasCoordinates[0] > runtimeGame.getGameResolutionWidth() ||
+        topLeftCanvasCoordinates[1] > runtimeGame.getGameResolutionHeight();
+      if (isOutsideCanvas) {
         this._input.style.display = 'none';
         return;
       }
       this._input.style.display = 'initial';
 
-      // Position the input on the HTML page:
+      // Position the input on the container on top of the canvas
       const topLeftPageCoordinates =
-        runtimeGameRenderer.convertCanvasToPageCoords(topLeftCanvasCoordinates);
+        runtimeGameRenderer.convertCanvasToDomElementContainerCoords(
+          topLeftCanvasCoordinates
+        );
       const bottomRightPageCoordinates =
-        runtimeGameRenderer.convertCanvasToPageCoords(
+        runtimeGameRenderer.convertCanvasToDomElementContainerCoords(
           bottomRightCanvasCoordinates
         );
 
-      const widthInPage =
+      const widthInContainer =
         bottomRightPageCoordinates[0] - topLeftPageCoordinates[0];
-      const heightInPage =
+      const heightInContainer =
         bottomRightPageCoordinates[1] - topLeftPageCoordinates[1];
 
       this._input.style.left = topLeftPageCoordinates[0] + 'px';
       this._input.style.top = topLeftPageCoordinates[1] + 'px';
-      this._input.style.width = widthInPage + 'px';
-      this._input.style.height = heightInPage + 'px';
+      this._input.style.width = widthInContainer + 'px';
+      this._input.style.height = heightInContainer + 'px';
 
-      // if (this._object.getAutomaticFontSize()) // TODO
-      {
-        this._input.style.fontSize = heightInPage - 5 + 'px';
-      }
+      // Automatically adjust the font size to follow the game scale.
+      this._input.style.fontSize =
+        this._object.getFontSize() *
+          runtimeGameRenderer.getCanvasToDomElementContainerHeightScale() +
+        'px';
     }
 
     updateValue() {
       this._input.value = this._object.getValue();
     }
 
+    updatePlaceholder() {
+      this._input.placeholder = this._object.getPlaceholder();
+    }
+
     updateFont() {
       this._input.style.fontFamily = this._runtimeScene
         .getGame()
         .getFontManager()
-        .getFontFamily(this._object.getFont());
+        .getFontFamily(this._object.getFontResourceName());
+    }
+
+    updateOpacity() {
+      this._input.style.opacity = '' + this._object.getOpacity() / 255;
+    }
+
+    updateInputType() {
+      const newType =
+        userFriendlyToHtmlInputTypes[this._object.getInputType()] || 'text';
+      this._input.setAttribute('type', newType);
     }
   }
 

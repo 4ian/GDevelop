f93ce55125b522d95c360375e2b31579f4f06b99
D8H <Davy.Helard@gmail.com>
2022-01-23 15:47:34 +0100

    Add actions to abort a jump and change the fall speed immediately of a platformer object. (#3537)
    * This is useful to allow to "dash" toward the ground.

--- Extensions/PlatformBehavior/JsExtension.cpp
+++ Extensions/PlatformBehavior/JsExtension.cpp
@@ -108,6 +108,9 @@ class PlatformBehaviorJsExtension : public gd::PlatformExtension {
           .SetFunctionName("setJumpSustainTime")
           .SetGetter("getJumpSustainTime");
       autExpressions["JumpSustainTime"].SetFunctionName("getJumpSustainTime");
+      autActions["PlatformBehavior::PlatformerObjectBehavior::SetCurrentFallSpeed"]
+          .SetFunctionName("setCurrentFallSpeed")
+          .SetGetter("getCurrentFallSpeed");
       autConditions["PlatformBehavior::CurrentFallSpeed"].SetFunctionName(
           "getCurrentFallSpeed");
       autExpressions["CurrentFallSpeed"].SetFunctionName("getCurrentFallSpeed");
@@ -124,6 +127,7 @@ class PlatformBehaviorJsExtension : public gd::PlatformExtension {
       autExpressions["CurrentJumpSpeed"].SetFunctionName("getCurrentJumpSpeed");
       autActions["PlatformBehavior::SetCanJump"].SetFunctionName("setCanJump");
       autActions["PlatformBehavior::PlatformerObjectBehavior::SetCanNotAirJump"].SetFunctionName("setCanNotAirJump");
+      autActions["PlatformBehavior::PlatformerObjectBehavior::AbortJump"].SetFunctionName("abortJump");
       autConditions["PlatformBehavior::CanJump"].SetFunctionName(
           "canJump");
       autActions["PlatformBehavior::SimulateLeftKey"].SetFunctionName(

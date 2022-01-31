74ad2832f00066f78d73025b50eac073a4994e71
Davy Hélard <davy.helard@gmail.com>
2022-01-31 19:28:20 +0100

    [PathFinding] Remove the dependency from PathfindingRuntimeBehavior to BehaviorRBushAABB as its an internal usage that PathfindingObstacleRuntimeBehavior should not expose.

--- Extensions/PathfindingBehavior/pathfindingobstacleruntimebehavior.ts
+++ Extensions/PathfindingBehavior/pathfindingobstacleruntimebehavior.ts
@@ -70,7 +70,7 @@ namespace gdjs {
       x: float,
       y: float,
       radius: float,
-      result: gdjs.BehaviorRBushAABB<gdjs.PathfindingObstacleRuntimeBehavior>[]
+      result: gdjs.PathfindingObstacleRuntimeBehavior[]
     ): any {
       const searchArea = gdjs.staticObject(
         PathfindingObstaclesManager.prototype.getAllObstaclesAround
@@ -83,9 +83,13 @@ namespace gdjs {
       searchArea.maxX = x + radius;
       // @ts-ignore
       searchArea.maxY = y + radius;
-      const nearbyPlatforms = this._obstaclesRBush.search(searchArea);
+      const nearbyPlatforms: gdjs.BehaviorRBushAABB<
+        gdjs.PathfindingObstacleRuntimeBehavior
+      >[] = this._obstaclesRBush.search(searchArea);
       result.length = 0;
-      result.push.apply(result, nearbyPlatforms);
+      nearbyPlatforms.forEach((nearbyPlatform) =>
+        result.push(nearbyPlatform.behavior)
+      );
     }
   }
 

2a0cb9b66fec4022d622769c746de9c8c44de1d5
Davy Hélard <davy.helard@gmail.com>
2022-02-01 18:28:21 +0100

    Tried to build with rollup, but the pixi dep makes errors.

--- Extensions/TileMap/helper/tiled/TiledTileMapLoader.d.ts
+++
@@ -1,9 +0,0 @@
-import { EditableTileMap } from "../TileMapModel";
-import { TiledMap } from "./Tiled";
-/**
- * It creates a {@link EditableTileMap} from a Tiled JSON.
- */
-export declare class TiledTileMapLoader {
-    static load(pako: any, tiledMap: TiledMap): EditableTileMap | null;
-}
-//# sourceMappingURL=TiledTileMapLoader.d.ts.map

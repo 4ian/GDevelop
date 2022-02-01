cce6ce325dffa757c0d8e5b30b824d71cb1d471f
Davy Hélard <davy.helard@gmail.com>
2022-02-01 18:28:21 +0100

    Tried to build with rollup, but the pixi dep makes errors.

--- SharedLibs/TileMapHelper/src/ResourceCache.ts
+++ SharedLibs/TileMapHelper/src/ResourceCache.ts
@@ -1,49 +1,49 @@
-    export class ResourceCache<T> {
-      private _cachedValues: Map<string, T>;
+export class ResourceCache<T> {
+  private _cachedValues: Map<string, T>;
 
-      private _callbacks: Map<string, Array<(value: T | null) => void>>;
+  private _callbacks: Map<string, Array<(value: T | null) => void>>;
 
-      /**
-       *
-       */
-      constructor() {
-        this._cachedValues = new Map<string, T>();
-        this._callbacks = new Map<string, Array<(value: T | null) => void>>();
-      }
+  /**
+   *
+   */
+  constructor() {
+    this._cachedValues = new Map<string, T>();
+    this._callbacks = new Map<string, Array<(value: T | null) => void>>();
+  }
 
-      getOrLoad(
-        key: string,
-        load: (callback: (value: T | null) => void) => void,
-        callback: (value: T | null) => void
-      ): void {
-        // Check if the value is in the cache.
-        {
-          const value = this._cachedValues.get(key);
-          if (value) {
-            callback(value);
-            return;
-          }
-        }
-        // Check if the value is being loading.
-        {
-          const callbacks = this._callbacks.get(key);
-          if (callbacks) {
-            callbacks.push(callback);
-            return;
-          } else {
-            this._callbacks.set(key, [callback]);
-          }
-        }
-
-        load((value) => {
-          if (value) {
-            this._cachedValues.set(key, value);
-          }
-          const callbacks = this._callbacks.get(key)!;
-          this._callbacks.delete(key);
-          for (const callback of callbacks) {
-            callback(value);
-          }
-        });
+  getOrLoad(
+    key: string,
+    load: (callback: (value: T | null) => void) => void,
+    callback: (value: T | null) => void
+  ): void {
+    // Check if the value is in the cache.
+    {
+      const value = this._cachedValues.get(key);
+      if (value) {
+        callback(value);
+        return;
       }
     }
+    // Check if the value is being loading.
+    {
+      const callbacks = this._callbacks.get(key);
+      if (callbacks) {
+        callbacks.push(callback);
+        return;
+      } else {
+        this._callbacks.set(key, [callback]);
+      }
+    }
+
+    load((value) => {
+      if (value) {
+        this._cachedValues.set(key, value);
+      }
+      const callbacks = this._callbacks.get(key)!;
+      this._callbacks.delete(key);
+      for (const callback of callbacks) {
+        callback(value);
+      }
+    });
+  }
+}

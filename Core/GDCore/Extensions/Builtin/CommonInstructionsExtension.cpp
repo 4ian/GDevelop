4ian/GDevelop

commit 5d2e0786ee290ec77e03e616509f7a5252a94173

Sort actions/conditions in categories add icons to their groups (#3583)
* This should make it easier to identify and find actions/conditions. Categories give more clarity and icons help to identify what's available without reading everything.

Core/GDCore/Extensions/Builtin/CommonInstructionsExtension.cpp
@@ -24,19 +24,35 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
   extension
       .SetExtensionInformation(
           "BuiltinCommonInstructions",
-          _("Builtin events"),
+          _("Events and control flow"),
           "GDevelop comes with a set of events and conditions that allow to "
           "express the game logic and rules.",
           "Florian Rival",
           "Open source (MIT License)")
+      .SetCategory("Advanced")
       .SetExtensionHelpPath("/all-features/advanced-conditions");
+  extension.AddInstructionOrExpressionGroupMetadata(_("Events and control flow"))
+      .SetIcon("res/conditions/toujours24.png");
+
+  extension
+      .AddCondition("Toujours",
+                    _("Always"),
+                    _("This condition always returns true (or always false, if "
+                      "the condition is inverted)."),
+                    _("Always"),
+                    "",
+                    "res/conditions/toujours24.png",
+                    "res/conditions/toujours.png")
+      .SetHelpPath("/all-features/advanced-conditions")
+      .AddCodeOnlyParameter("conditionInverted", "")
+      .MarkAsAdvanced();
 
   extension
       .AddCondition("Or",
                     _("Or"),
                     _("Check if one of the sub conditions is true"),
                     _("If one of these conditions is true:"),
-                    _("Advanced"),
+                    "",
                     "res/conditions/or24.png",
                     "res/conditions/or.png")
       .SetCanHaveSubInstructions()
@@ -47,7 +63,7 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
                     _("And"),
                     _("Check if all sub conditions are true"),
                     _("If all of these conditions are true:"),
-                    _("Advanced"),
+                    "",
                     "res/conditions/and24.png",
                     "res/conditions/and.png")
       .SetCanHaveSubInstructions()
@@ -59,7 +75,7 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
           _("Not"),
           _("Return the contrary of the result of the sub conditions"),
           _("Invert the logical result of these conditions:"),
-          _("Advanced"),
+          "",
           "res/conditions/not24.png",
           "res/conditions/not.png")
       .SetCanHaveSubInstructions()
@@ -70,9 +86,37 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
       _("Trigger once while true"),
       _("Run actions only once, for each time the conditions have been met."),
       _("Trigger once"),
-      _("Advanced"),
+      "",
       "res/conditions/once24.png",
       "res/conditions/once.png");
+
+  extension
+      .AddCondition("Egal",
+                    _("Compare two numbers"),
+                    _("Compare the two numbers."),
+                    _("_PARAM0_ _PARAM1_ _PARAM2_"),
+                    "",
+                    "res/conditions/egal24.png",
+                    "res/conditions/egal.png")
+      .SetHelpPath("/all-features/advanced-conditions")
+      .AddParameter("expression", _("First expression"))
+      .AddParameter("relationalOperator", _("Sign of the test"), "number")
+      .AddParameter("expression", _("Second expression"))
+      .MarkAsAdvanced();
+
+  extension
+      .AddCondition("StrEqual",
+                    _("Compare two strings"),
+                    _("Compare the two strings."),
+                    _("_PARAM0_ _PARAM1_ _PARAM2_"),
+                    "",
+                    "res/conditions/egal24.png",
+                    "res/conditions/egal.png")
+      .SetHelpPath("/all-features/advanced-conditions")
+      .AddParameter("string", _("First string expression"))
+      .AddParameter("relationalOperator", _("Sign of the test"), "string")
+      .AddParameter("string", _("Second string expression"))
+      .MarkAsAdvanced();
 
   extension.AddEvent(
       "Standard",

Core/GDCore/Extensions/PlatformExtension.cpp
@@ -754,7 +754,7 @@ void PlatformExtension::StripUnimplementedInstructionsAndExpressions() {
 }
 #endif
 
-PlatformExtension::PlatformExtension() : deprecated(false) {}
+PlatformExtension::PlatformExtension() : deprecated(false), category(_("General")) {}
 
 PlatformExtension::~PlatformExtension() {}
 

Core/GDCore/Extensions/PlatformExtension.h
@@ -5,8 +5,7 @@
  * project is released under the MIT License.
  */
 
-#ifndef GDCORE_PLATFORMEXTENSION_H
-#define GDCORE_PLATFORMEXTENSION_H
+#pragma once
 #include <map>
 #include <memory>
 #include <vector>
@@ -16,6 +15,7 @@
 #include "GDCore/Extensions/Metadata/DependencyMetadata.h"
 #include "GDCore/Extensions/Metadata/EffectMetadata.h"
 #include "GDCore/Extensions/Metadata/EventMetadata.h"
+#include "GDCore/Extensions/Metadata/InstructionOrExpressionGroupMetadata.h"
 #include "GDCore/Extensions/Metadata/ObjectMetadata.h"
 #include "GDCore/Project/PropertyDescriptor.h"
 #include "GDCore/String.h"
@@ -106,9 +106,17 @@ class GD_CORE_API PlatformExtension {
   }
 
   /**
+   * \brief Set the category of the extension.
+   */
+  PlatformExtension& SetCategory(const gd::String& category_) {
+    category = category_;
+    return *this;
+  }
+
+  /**
    * \brief Set the path to the help, relative to the GDevelop documentation
    * root. For example, "/all-features/collisions" for
-   * "http://wiki.compilgames.net/doku.php/gdevelop5/all-features/collisions".
+   * "https://wiki.gdevelop.io/gdevelop5/all-features/collisions".
    *
    * The instructions, objects and behaviors will have this help path set by
    * default, unless you call SetHelpPath on them.
@@ -271,7 +279,6 @@ class GD_CORE_API PlatformExtension {
                               const gd::String& smallicon_,
                               std::shared_ptr<gd::BaseEvent> instance);
 
-#if defined(GD_IDE_ONLY)
   /**
    * \brief Create a new action which is the duplicate of the specified one.
    *
@@ -315,7 +322,15 @@ class GD_CORE_API PlatformExtension {
   gd::PropertyDescriptor& RegisterProperty(const gd::String& name) {
     return extensionPropertiesMetadata[name];
   };
-#endif
+
+  /**
+   * \brief Add some metadata (icon, etc...) for a group used for instructions
+   * or expressions.
+   */
+  InstructionOrExpressionGroupMetadata& AddInstructionOrExpressionGroupMetadata(
+      const gd::String& name) {
+    return instructionOrExpressionGroupMetadata[name];
+  }
 
   /**
    * \brief Delete all instructions having no function name or custom code
@@ -338,6 +353,11 @@ class GD_CORE_API PlatformExtension {
    * \brief Return the name of the extension
    */
   const gd::String& GetName() const { return name; }
+
+  /**
+   * \brief Return the category of the extension
+   */
+  const gd::String& GetCategory() const { return category; }
 
   /**
    * \brief Return a description of the extension
@@ -452,7 +472,6 @@ class GD_CORE_API PlatformExtension {
    */
   std::map<gd::String, gd::EventMetadata>& GetAllEvents();
 
-#if defined(GD_IDE_ONLY)
   /**
    * \brief Return a reference to a map containing the names of the actions
    * (as keys) and the metadata associated with (as values).
@@ -537,8 +556,16 @@ class GD_CORE_API PlatformExtension {
   std::map<gd::String, gd::PropertyDescriptor>& GetAllProperties() {
     return extensionPropertiesMetadata;
   }
+
+  /**
+   * \brief Get the metadata (icon, etc...) for groups used for instructions
+   * or expressions.
+   */
+  const std::map<gd::String, InstructionOrExpressionGroupMetadata>&
+  GetAllInstructionOrExpressionGroupMetadata() const {
+    return instructionOrExpressionGroupMetadata;
+  }
   ///@}
-#endif
 
   /**
    * \brief Return the name of all the extensions which are considered provided
@@ -566,6 +593,7 @@ class GD_CORE_API PlatformExtension {
                   ///< actions/conditions/expressions/objects/behavior/event.
   gd::String fullname;      ///< Name displayed to users in the editor.
   gd::String informations;  ///< Description displayed to users in the editor.
+  gd::String category;
   gd::String author;        ///< Author displayed to users in the editor.
   gd::String license;       ///< License name displayed to users in the editor.
   bool deprecated;  ///< true if the extension is deprecated and shouldn't be
@@ -577,7 +605,6 @@ class GD_CORE_API PlatformExtension {
   std::map<gd::String, gd::ObjectMetadata> objectsInfos;
   std::map<gd::String, gd::BehaviorMetadata> behaviorsInfo;
   std::map<gd::String, gd::EffectMetadata> effectsMetadata;
-#if defined(GD_IDE_ONLY)
   std::map<gd::String, gd::InstructionMetadata> conditionsInfos;
   std::map<gd::String, gd::InstructionMetadata> actionsInfos;
   std::map<gd::String, gd::ExpressionMetadata> expressionsInfos;
@@ -585,12 +612,12 @@ class GD_CORE_API PlatformExtension {
   std::vector<gd::DependencyMetadata> extensionDependenciesMetadata;
   std::map<gd::String, gd::EventMetadata> eventsInfos;
   std::map<gd::String, gd::PropertyDescriptor> extensionPropertiesMetadata;
-#endif
+  std::map<gd::String, InstructionOrExpressionGroupMetadata>
+      instructionOrExpressionGroupMetadata;
 
   ObjectMetadata badObjectMetadata;
   BehaviorMetadata badBehaviorMetadata;
   EffectMetadata badEffectMetadata;
-#if defined(GD_IDE_ONLY)
   static std::map<gd::String, gd::InstructionMetadata>
       badConditionsMetadata;  ///< Used when a condition is not found in the
                               ///< extension
@@ -600,12 +627,10 @@ class GD_CORE_API PlatformExtension {
   static std::map<gd::String, gd::ExpressionMetadata>
       badExpressionsMetadata;  ///< Used when an expression is not found in the
                                ///< extension
-#endif
 };
 
 }  // namespace gd
 
-#if defined(GD_IDE_ONLY)
 /** \brief Macro used by extensions in their constructor to declare how they
  * have been compiled. \see gd::CompilationInfo
  */
@@ -619,23 +644,5 @@ class GD_CORE_API PlatformExtension {
   compilationInfo.gccMinorVersion = __GNUC_MINOR__;     \
   compilationInfo.gccPatchLevel = __GNUC_PATCHLEVEL__;  \
   compilationInfo.informationCompleted = true;
-#else
-/** \brief Macro used by extensions in their constructor to declare how they
- * have been compiled. \see gd::CompilationInfo
- */
-#define GD_COMPLETE_EXTENSION_COMPILATION_INFORMATION() \
-  compilationInfo.runtimeOnly = true;                   \
-  compilationInfo.sfmlMajorVersion = 2;                 \
-  compilationInfo.sfmlMinorVersion = 0;                 \
-  compilationInfo.gdCoreVersion = GD_VERSION_STRING;    \
-  compilationInfo.sizeOfpInt = sizeof(int*);            \
-  compilationInfo.gccMajorVersion = __GNUC__;           \
-  compilationInfo.gccMinorVersion = __GNUC_MINOR__;     \
-  compilationInfo.gccPatchLevel = __GNUC_PATCHLEVEL__;  \
-  compilationInfo.informationCompleted = true;
-
-#endif
 
 #include "GDCore/Extensions/PlatformExtension.inl"
-
-#endif  // GDCORE_PLATFORMEXTENSION_H

GDJS/GDJS/Extensions/Builtin/CommonInstructionsExtension.cpp
@@ -40,6 +40,86 @@ CommonInstructionsExtension::CommonInstructionsExtension() {
   gd::BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
       *this);
 
+  GetAllConditions()["Toujours"].SetFunctionName(
+      "gdjs.evtTools.common.logicalNegation");
+
+  GetAllConditions()["Egal"].codeExtraInformation.SetCustomCodeGenerator(
+      [](gd::Instruction& instruction,
+         gd::EventsCodeGenerator& codeGenerator,
+         gd::EventsCodeGenerationContext& context) {
+        gd::String value1Code =
+            gd::ExpressionCodeGenerator::GenerateExpressionCode(
+                codeGenerator,
+                context,
+                "number",
+                instruction.GetParameters()[0].GetPlainString());
+
+        gd::String value2Code =
+            gd::ExpressionCodeGenerator::GenerateExpressionCode(
+                codeGenerator,
+                context,
+                "number",
+                instruction.GetParameters()[2].GetPlainString());
+
+        gd::String resultingBoolean =
+            codeGenerator.GenerateBooleanFullName("conditionTrue", context) +
+            ".val";
+
+        if (instruction.GetParameters()[1].GetPlainString() == "=" ||
+            instruction.GetParameters()[1].GetPlainString().empty())
+          return resultingBoolean + " = (" + value1Code + " == " + value2Code +
+                 ");\n";
+        else if (instruction.GetParameters()[1].GetPlainString() == ">")
+          return resultingBoolean + " = (" + value1Code + " > " + value2Code +
+                 ");\n";
+        else if (instruction.GetParameters()[1].GetPlainString() == "<")
+          return resultingBoolean + " = (" + value1Code + " < " + value2Code +
+                 ");\n";
+        else if (instruction.GetParameters()[1].GetPlainString() == "<=")
+          return resultingBoolean + " = (" + value1Code + " <= " + value2Code +
+                 ");\n";
+        else if (instruction.GetParameters()[1].GetPlainString() == ">=")
+          return resultingBoolean + " = (" + value1Code + " >= " + value2Code +
+                 ");\n";
+        else if (instruction.GetParameters()[1].GetPlainString() == "!=")
+          return resultingBoolean + " = (" + value1Code + " != " + value2Code +
+                 ");\n";
+
+        return gd::String("");
+      });
+
+  GetAllConditions()["StrEqual"].codeExtraInformation.SetCustomCodeGenerator(
+      [](gd::Instruction& instruction,
+         gd::EventsCodeGenerator& codeGenerator,
+         gd::EventsCodeGenerationContext& context) {
+        gd::String value1Code =
+            gd::ExpressionCodeGenerator::GenerateExpressionCode(
+                codeGenerator,
+                context,
+                "string",
+                instruction.GetParameters()[0].GetPlainString());
+
+        gd::String value2Code =
+            gd::ExpressionCodeGenerator::GenerateExpressionCode(
+                codeGenerator,
+                context,
+                "string",
+                instruction.GetParameters()[2].GetPlainString());
+
+        gd::String resultingBoolean =
+            codeGenerator.GenerateBooleanFullName("conditionTrue", context) +
+            ".val";
+
+        if (instruction.GetParameters()[1].GetPlainString() == "=")
+          return resultingBoolean + " = (" + value1Code + " == " + value2Code +
+                 ");\n";
+        else if (instruction.GetParameters()[1].GetPlainString() == "!=")
+          return resultingBoolean + " = (" + value1Code + " != " + value2Code +
+                 ");\n";
+
+        return gd::String("");
+      });
+
   GetAllEvents()["BuiltinCommonInstructions::Link"]
       .SetCodeGenerator([](gd::BaseEvent& event_,
                            gd::EventsCodeGenerator& codeGenerator,

commit 1115b2d1be4de312eac754ef1693aaf2f89968a0

Fix compatibility with instructions broken in #3583

Core/GDCore/Extensions/Builtin/CommonInstructionsExtension.cpp
@@ -4,7 +4,6 @@
  * reserved. This project is released under the MIT License.
  */
 #include "AllBuiltinExtensions.h"
-#include "GDCore/Tools/Localization.h"
 #include "GDCore/Events/Builtin/CommentEvent.h"
 #include "GDCore/Events/Builtin/ForEachChildVariableEvent.h"
 #include "GDCore/Events/Builtin/ForEachEvent.h"
@@ -14,6 +13,7 @@
 #include "GDCore/Events/Builtin/StandardEvent.h"
 #include "GDCore/Events/Builtin/WhileEvent.h"
 #include "GDCore/Events/Event.h"
+#include "GDCore/Tools/Localization.h"
 
 using namespace std;
 namespace gd {
@@ -31,11 +31,12 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
           "Open source (MIT License)")
       .SetCategory("Advanced")
       .SetExtensionHelpPath("/all-features/advanced-conditions");
-  extension.AddInstructionOrExpressionGroupMetadata(_("Events and control flow"))
+  extension
+      .AddInstructionOrExpressionGroupMetadata(_("Events and control flow"))
       .SetIcon("res/conditions/toujours24.png");
 
   extension
-      .AddCondition("Toujours",
+      .AddCondition("Always",
                     _("Always"),
                     _("This condition always returns true (or always false, if "
                       "the condition is inverted)."),
@@ -46,6 +47,13 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
       .SetHelpPath("/all-features/advanced-conditions")
       .AddCodeOnlyParameter("conditionInverted", "")
       .MarkAsAdvanced();
+
+  // Compatibility with GD <= 5.0.127
+  extension
+      .AddDuplicatedCondition(
+          "Toujours", "BuiltinCommonInstructions::Always", {.unscoped = true})
+      .SetHidden();
+  // end of compatibility code
 
   extension
       .AddCondition("Or",
@@ -91,7 +99,7 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
       "res/conditions/once.png");
 
   extension
-      .AddCondition("Egal",
+      .AddCondition("CompareNumbers",
                     _("Compare two numbers"),
                     _("Compare the two numbers."),
                     _("_PARAM0_ _PARAM1_ _PARAM2_"),
@@ -104,8 +112,15 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
       .AddParameter("expression", _("Second expression"))
       .MarkAsAdvanced();
 
+  // Compatibility with GD <= 5.0.127
   extension
-      .AddCondition("StrEqual",
+      .AddDuplicatedCondition(
+          "Egal", "BuiltinCommonInstructions::CompareNumbers", {.unscoped = true})
+      .SetHidden();
+  // end of compatibility code
+
+  extension
+      .AddCondition("CompareStrings",
                     _("Compare two strings"),
                     _("Compare the two strings."),
                     _("_PARAM0_ _PARAM1_ _PARAM2_"),
@@ -117,6 +132,13 @@ BuiltinExtensionsImplementer::ImplementsCommonInstructionsExtension(
       .AddParameter("relationalOperator", _("Sign of the test"), "string")
       .AddParameter("string", _("Second string expression"))
       .MarkAsAdvanced();
+
+  // Compatibility with GD <= 5.0.127
+  extension
+      .AddDuplicatedCondition(
+          "StrEqual", "BuiltinCommonInstructions::CompareStrings", {.unscoped = true})
+      .SetHidden();
+  // end of compatibility code
 
   extension.AddEvent(
       "Standard",

Core/GDCore/Extensions/PlatformExtension.cpp
@@ -372,9 +372,13 @@ gd::InstructionMetadata& PlatformExtension::AddDuplicatedAction(
 }
 
 gd::InstructionMetadata& PlatformExtension::AddDuplicatedCondition(
-    const gd::String& newConditionName, const gd::String& copiedConditionName) {
-  gd::String newNameWithNamespace = GetNameSpace() + newConditionName;
-  gd::String copiedNameWithNamespace = GetNameSpace() + copiedConditionName;
+    const gd::String& newConditionName,
+    const gd::String& copiedConditionName,
+    gd::DuplicatedInstructionOptions options) {
+  gd::String newNameWithNamespace =
+      (options.unscoped ? "" : GetNameSpace()) + newConditionName;
+  gd::String copiedNameWithNamespace =
+      (options.unscoped ? "" : GetNameSpace()) + copiedConditionName;
 
   auto copiedCondition = conditionsInfos.find(copiedNameWithNamespace);
   if (copiedCondition == conditionsInfos.end()) {
@@ -512,7 +516,8 @@ PlatformExtension::GetAllStrExpressionsForBehavior(gd::String autoType) {
   return badExpressionsMetadata;
 }
 
-gd::BaseEventSPtr PlatformExtension::CreateEvent(const gd::String& eventType) const {
+gd::BaseEventSPtr PlatformExtension::CreateEvent(
+    const gd::String& eventType) const {
   if (eventsInfos.find(eventType) != eventsInfos.end()) {
     if (eventsInfos.find(eventType)->second.instance ==
         std::shared_ptr<BaseEvent>()) {
@@ -754,7 +759,8 @@ void PlatformExtension::StripUnimplementedInstructionsAndExpressions() {
 }
 #endif
 
-PlatformExtension::PlatformExtension() : deprecated(false), category(_("General")) {}
+PlatformExtension::PlatformExtension()
+    : deprecated(false), category(_("General")) {}
 
 PlatformExtension::~PlatformExtension() {}
 

Core/GDCore/Extensions/PlatformExtension.h
@@ -71,6 +71,10 @@ class GD_CORE_API CompilationInfo {
   int sizeOfpInt;
 };
 
+struct GD_CORE_API DuplicatedInstructionOptions {
+  bool unscoped;
+};
+
 /**
  * \brief Base class for implementing platform's extensions.
  *
@@ -295,7 +299,8 @@ class GD_CORE_API PlatformExtension {
    */
   gd::InstructionMetadata& AddDuplicatedCondition(
       const gd::String& newConditionName,
-      const gd::String& copiedConditionName);
+      const gd::String& copiedConditionName,
+      gd::DuplicatedInstructionOptions options = {.unscoped = false});
   /**
    * \brief Create a new expression which is the duplicate of the specified one.
    *
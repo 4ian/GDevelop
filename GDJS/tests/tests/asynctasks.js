5a8dc40f49b8c047137077ab8fa0ab077cb1ef1e
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2022-02-12 21:39:51 +0100

    Add IIFE to test
    To not leak createMockCallback into the global scope

--- GDJS/tests/tests/asynctasks.js
+++ GDJS/tests/tests/asynctasks.js
@@ -1,119 +1,121 @@
-function createMockCallback() {
-  let callCount = 0;
-  function call() {
-    callCount++;
-  }
-  call.expectToHaveBeenCalled = () => {
-    expect(callCount).to.not.be(0);
-  };
-  call.expectToHaveBeenCalledOnce = () => {
-    expect(callCount).to.be(1);
-  };
-  call.expectToNotHaveBeenCalled = () => {
-    expect(callCount).to.be(0);
-  };
-  return call;
-}
-
-describe('gdjs.AsyncTaskManager', function () {
-  class ResolvingTask extends gdjs.AsyncTask {
-    update() {
-      return true;
+(() => {
+  function createMockCallback() {
+    let callCount = 0;
+    function call() {
+      callCount++;
     }
+    call.expectToHaveBeenCalled = () => {
+      expect(callCount).to.not.be(0);
+    };
+    call.expectToHaveBeenCalledOnce = () => {
+      expect(callCount).to.be(1);
+    };
+    call.expectToNotHaveBeenCalled = () => {
+      expect(callCount).to.be(0);
+    };
+    return call;
   }
 
-  class NeverResolvingTask extends gdjs.AsyncTask {
-    update() {
-      return false;
+  describe('gdjs.AsyncTaskManager', function () {
+    class ResolvingTask extends gdjs.AsyncTask {
+      update() {
+        return true;
+      }
     }
-  }
 
-  const runtimeGame = new gdjs.RuntimeGame({
-    variables: [],
-    // @ts-ignore TODO: make a function to create an empty game and use it across tests.
-    properties: { windowWidth: 800, windowHeight: 600 },
-    resources: { resources: [] },
+    class NeverResolvingTask extends gdjs.AsyncTask {
+      update() {
+        return false;
+      }
+    }
+
+    const runtimeGame = new gdjs.RuntimeGame({
+      variables: [],
+      // @ts-ignore TODO: make a function to create an empty game and use it across tests.
+      properties: { windowWidth: 800, windowHeight: 600 },
+      resources: { resources: [] },
+    });
+    const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
+    const asyncTasksManager = runtimeScene.getAsyncTasksManager();
+
+    this.beforeEach(() => asyncTasksManager.clearTasks());
+
+    it('should call a resolved callback', function () {
+      const cb = createMockCallback();
+      asyncTasksManager.addTask(new ResolvingTask(), cb);
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks();
+      cb.expectToHaveBeenCalled();
+    });
+
+    it('should not call callbacks twice', function () {
+      const cb = createMockCallback();
+      asyncTasksManager.addTask(new ResolvingTask(), cb);
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks();
+      asyncTasksManager.processTasks();
+      cb.expectToHaveBeenCalledOnce();
+    });
+
+    it('should not call an unresolved callback', function () {
+      const cb = createMockCallback();
+      asyncTasksManager.addTask(new NeverResolvingTask(), cb);
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks();
+      cb.expectToNotHaveBeenCalled();
+    });
   });
-  const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
-  const asyncTasksManager = runtimeScene.getAsyncTasksManager();
 
-  this.beforeEach(() => asyncTasksManager.clearTasks());
+  describe('gdjs.PromiseTask', function () {
+    const runtimeGame = new gdjs.RuntimeGame({
+      variables: [],
+      // @ts-ignore TODO: make a function to create an empty game and use it across tests.
+      properties: { windowWidth: 800, windowHeight: 600 },
+      resources: { resources: [] },
+    });
+    const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
+    const asyncTasksManager = runtimeScene.getAsyncTasksManager();
 
-  it('should call a resolved callback', function () {
-    const cb = createMockCallback();
-    asyncTasksManager.addTask(new ResolvingTask(), cb);
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks();
-    cb.expectToHaveBeenCalled();
+    it('Should resolve with Promise', async () => {
+      const promise = new Promise((r) => queueMicrotask(r));
+      const cb = createMockCallback();
+      asyncTasksManager.addTask(new gdjs.PromiseTask(promise), cb);
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks(runtimeScene);
+      cb.expectToNotHaveBeenCalled();
+      await promise;
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks(runtimeScene);
+      cb.expectToHaveBeenCalled();
+    });
   });
 
-  it('should not call callbacks twice', function () {
-    const cb = createMockCallback();
-    asyncTasksManager.addTask(new ResolvingTask(), cb);
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks();
-    asyncTasksManager.processTasks();
-    cb.expectToHaveBeenCalledOnce();
-  });
+  describe('gdjs.WaitTask', () => {
+    const runtimeGame = new gdjs.RuntimeGame({
+      variables: [],
+      // @ts-ignore TODO: make a function to create an empty game and use it across tests.
+      properties: { windowWidth: 800, windowHeight: 600 },
+      resources: { resources: [] },
+    });
+    const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
+    const asyncTasksManager = runtimeScene.getAsyncTasksManager();
+    const timeManager = runtimeScene.getTimeManager();
 
-  it('should not call an unresolved callback', function () {
-    const cb = createMockCallback();
-    asyncTasksManager.addTask(new NeverResolvingTask(), cb);
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks();
-    cb.expectToNotHaveBeenCalled();
+    it('Should resolve when the time has elapsed', async () => {
+      const cb = createMockCallback();
+      asyncTasksManager.addTask(
+        new gdjs.evtTools.runtimeScene.WaitTask(1000),
+        cb
+      );
+      cb.expectToNotHaveBeenCalled();
+      timeManager.update(500, 0);
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks(runtimeScene);
+      cb.expectToNotHaveBeenCalled();
+      timeManager.update(500, 0);
+      cb.expectToNotHaveBeenCalled();
+      asyncTasksManager.processTasks(runtimeScene);
+      cb.expectToHaveBeenCalled();
+    });
   });
-});
-
-describe('gdjs.PromiseTask', function () {
-  const runtimeGame = new gdjs.RuntimeGame({
-    variables: [],
-    // @ts-ignore TODO: make a function to create an empty game and use it across tests.
-    properties: { windowWidth: 800, windowHeight: 600 },
-    resources: { resources: [] },
-  });
-  const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
-  const asyncTasksManager = runtimeScene.getAsyncTasksManager();
-
-  it('Should resolve with Promise', async () => {
-    const promise = new Promise((r) => queueMicrotask(r));
-    const cb = createMockCallback();
-    asyncTasksManager.addTask(new gdjs.PromiseTask(promise), cb);
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks(runtimeScene);
-    cb.expectToNotHaveBeenCalled();
-    await promise;
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks(runtimeScene);
-    cb.expectToHaveBeenCalled();
-  });
-});
-
-describe('gdjs.WaitTask', () => {
-  const runtimeGame = new gdjs.RuntimeGame({
-    variables: [],
-    // @ts-ignore TODO: make a function to create an empty game and use it across tests.
-    properties: { windowWidth: 800, windowHeight: 600 },
-    resources: { resources: [] },
-  });
-  const runtimeScene = new gdjs.RuntimeScene(runtimeGame);
-  const asyncTasksManager = runtimeScene.getAsyncTasksManager();
-  const timeManager = runtimeScene.getTimeManager();
-
-  it('Should resolve when the time has elapsed', async () => {
-    const cb = createMockCallback();
-    asyncTasksManager.addTask(
-      new gdjs.evtTools.runtimeScene.WaitTask(1000),
-      cb
-    );
-    cb.expectToNotHaveBeenCalled();
-    timeManager.update(500, 0);
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks(runtimeScene);
-    cb.expectToNotHaveBeenCalled();
-    timeManager.update(500, 0);
-    cb.expectToNotHaveBeenCalled();
-    asyncTasksManager.processTasks(runtimeScene);
-    cb.expectToHaveBeenCalled();
-  });
-});
+})();

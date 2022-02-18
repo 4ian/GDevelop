67d221e7091d6dfd560e07255f9110e2fd7a866b
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-02-18 11:19:06 +0100

    Trigger onInstancesAdded callback when pasting instances

--- newIDE/app/src/SceneEditor/index.js
+++ newIDE/app/src/SceneEditor/index.js
@@ -902,6 +902,7 @@ export default class SceneEditor extends React.Component<Props, State> {
         instance.delete();
         return newInstance;
       });
+    this._onInstancesAdded(newInstances)
     this.instancesSelection.clearSelection();
     this.instancesSelection.selectInstances(newInstances, true);
   };

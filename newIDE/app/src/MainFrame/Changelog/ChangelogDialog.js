c5dabf9b6e250ad592aa1ac8ee82d5bbde881de0
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-03-02 10:24:36 +0100

    Optimise onboarding flow to show to all web users

--- newIDE/app/src/MainFrame/Changelog/ChangelogDialog.js
+++ newIDE/app/src/MainFrame/Changelog/ChangelogDialog.js
@@ -6,50 +6,47 @@ import Dialog from '../../UI/Dialog';
 import FlatButton from '../../UI/FlatButton';
 import Changelog from '.';
 import Text from '../../UI/Text';
+import useForceUpdate from '../../Utils/UseForceUpdate';
 
 type Props = {|
   open: boolean,
   onClose: () => void,
 |};
 
-type State = {||};
-
-export default class ChangelogDialog extends React.Component<Props, State> {
-  render() {
-    const { open, onClose } = this.props;
-    if (!open) {
-      // Don't render anything, to avoid in particular sending useless requests.
-      return null;
-    }
-
-    const actions = [
-      <FlatButton
-        key="close"
-        label={<Trans>Close</Trans>}
-        primary={true}
-        onClick={onClose}
-      />,
-    ];
-
-    return (
-      <Dialog
-        title={<Trans>What's new in GDevelop?</Trans>}
-        actions={actions}
-        open={open}
-        onRequestClose={onClose}
-        cannotBeDismissed={false}
-      >
-        <Text>
-          <Trans>
-            GDevelop was upgraded to a new version! Check out the changes.
-          </Trans>
-        </Text>
-        <Changelog
-          onUpdated={() => {
-            this.forceUpdate(); /*Force update to ensure dialog is properly positionned*/
-          }}
-        />
-      </Dialog>
-    );
+const ChangelogDialog = ({ open, onClose }: Props) => {
+  const forceUpdate = useForceUpdate();
+  if (!open) {
+    // Don't render anything, to avoid in particular sending useless requests.
+    return null;
   }
-}
+
+  const actions = [
+    <FlatButton
+      key="close"
+      label={<Trans>Close</Trans>}
+      primary={true}
+      onClick={onClose}
+    />,
+  ];
+
+  return (
+    <Dialog
+      title={<Trans>What's new in GDevelop?</Trans>}
+      actions={actions}
+      open={open}
+      onRequestClose={onClose}
+      cannotBeDismissed={false}
+    >
+      <Text>
+        <Trans>
+          GDevelop was upgraded to a new version! Check out the changes.
+        </Trans>
+      </Text>
+      <Changelog
+        onUpdated={forceUpdate} // Force update to ensure dialog is properly positionned
+      />
+    </Dialog>
+  );
+};
+
+export default ChangelogDialog;

d2086d481477b7e54e1f41bcda17894e580223a2
Oxey405 <Oxey405@gmail.com>
2022-03-11 19:27:51 +0100

    fixed format

--- GDJS/Runtime/events-tools/soundtools.ts
+++ GDJS/Runtime/events-tools/soundtools.ts
@@ -436,7 +436,7 @@ namespace gdjs {
         timeOfFade: float /*In seconds and converted to ms*/
       ) => {
         const sound = runtimeScene.getSoundManager().getSoundOnChannel(channel);
-        if (sound) sound.fade(sound.getVolume(), toVolume, timeOfFade*1000);
+        if (sound) sound.fade(sound.getVolume(), toVolume, timeOfFade * 1000);
         else {
           logger.error(
             `Cannot fade the volume of a non-existing sound on channel ${channel}.`

251c4abd1266c65460c63e0e312ee7a48c96e06a
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-03-14 18:46:23 +0100

    Initialize leaderboard context

---
+++ newIDE/app/src/Leaderboard/LeaderboardContext.js
@@ -0,0 +1,43 @@
+// @flow
+import * as React from 'react';
+import {
+  type Leaderboard,
+  type LeaderboardSortOption,
+  type LeaderboardDisplayData,
+} from '../Utils/GDevelopServices/Play';
+
+export type LeaderboardState = {|
+  leaderboards: ?Array<Leaderboard>,
+  currentLeaderboardId: ?string,
+  browsing: ?{|
+    entries: Array<LeaderboardDisplayData>,
+    currentUrl: string,
+    nextUrl: ?string,
+  |},
+  createLeaderboard: ({|
+    name: string,
+    sort: LeaderboardSortOption,
+  |}) => Promise<?Leaderboard>,
+  listLeaderboards: () => Promise<void>,
+  selectLeaderboard: (id: string) => void,
+  updateLeaderboard: ({|
+    name?: string,
+    sort?: LeaderboardSortOption,
+  |}) => Promise<void>,
+|};
+
+export const initialLeaderboardState = {
+  leaderboards: null,
+  currentLeaderboardId: null,
+  browsing: null,
+  createLeaderboard: async () => null,
+  listLeaderboards: async () => {},
+  selectLeaderboard: () => {},
+  updateLeaderboard: async () => {},
+};
+
+const LeaderboardContext = React.createContext<LeaderboardState>(
+  initialLeaderboardState
+);
+
+export default LeaderboardContext;

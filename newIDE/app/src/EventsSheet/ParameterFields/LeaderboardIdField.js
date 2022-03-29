cf4ee206978e036085f4551c415b40923ecce495
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-03-29 11:09:59 +0200

    Add ref to leaderboardId field when setting leaderboard id as an expression

--- newIDE/app/src/EventsSheet/ParameterFields/LeaderboardIdField.js
+++ newIDE/app/src/EventsSheet/ParameterFields/LeaderboardIdField.js
@@ -4,7 +4,10 @@ import { Trans, t } from '@lingui/macro';
 import { I18n } from '@lingui/react';
 import OpenInNew from '@material-ui/icons/OpenInNew';
 import { type ParameterInlineRendererProps } from './ParameterInlineRenderer.flow';
-import { type ParameterFieldProps } from './ParameterFieldCommons';
+import {
+  type ParameterFieldProps,
+  type ParameterFieldInterface,
+} from './ParameterFieldCommons';
 import SelectField from '../../UI/SelectField';
 import SelectOption from '../../UI/SelectOption';
 import { TextFieldWithButtonLayout } from '../../UI/Layout';
@@ -46,122 +49,135 @@ const useFetchLeaderboards = () => {
   return leaderboards;
 };
 
-export function LeaderboardIdField(props: ParameterFieldProps) {
-  const leaderboards = useFetchLeaderboards();
-  const [isAdminOpen, setIsAdminOpen] = React.useState(false);
+export default React.forwardRef<ParameterFieldProps, ParameterFieldInterface>(
+  function LeaderboardIdField(props, ref) {
+    const leaderboards = useFetchLeaderboards();
+    const [isAdminOpen, setIsAdminOpen] = React.useState(false);
+    const genericExpressionFieldRef = React.useRef<?GenericExpressionField>(
+      null
+    );
+    React.useImperativeHandle(ref, () => ({
+      focus: () => {
+        if (genericExpressionFieldRef.current) {
+          genericExpressionFieldRef.current.focus();
+        }
+      },
+    }));
 
-  const isCurrentValueInLeaderboardList =
-    leaderboards &&
-    !!leaderboards.find(leaderboard => `"${leaderboard.id}"` === props.value);
+    const isCurrentValueInLeaderboardList =
+      leaderboards &&
+      !!leaderboards.find(leaderboard => `"${leaderboard.id}"` === props.value);
 
-  const [isTextInput, setIsTextInput] = React.useState(
-    !leaderboards || (!!props.value && !isCurrentValueInLeaderboardList)
-  );
+    const [isTextInput, setIsTextInput] = React.useState(
+      !leaderboards || (!!props.value && !isCurrentValueInLeaderboardList)
+    );
 
-  const onChangeSelectValue = (event, value) => {
-    props.onChange(event.target.value);
-  };
+    const onChangeSelectValue = (event, value) => {
+      props.onChange(event.target.value);
+    };
 
-  const onChangeTextValue = (value: string) => {
-    props.onChange(value);
-  };
+    const onChangeTextValue = (value: string) => {
+      props.onChange(value);
+    };
 
-  const fieldLabel = props.parameterMetadata
-    ? props.parameterMetadata.getDescription()
-    : undefined;
+    const fieldLabel = props.parameterMetadata
+      ? props.parameterMetadata.getDescription()
+      : undefined;
 
-  return (
-    <I18n>
-      {({ i18n }) => (
-        <>
-          <TextFieldWithButtonLayout
-            renderTextField={() =>
-              !isTextInput ? (
-                <SelectField
-                  value={props.value}
-                  onChange={onChangeSelectValue}
-                  margin={props.isInline ? 'none' : 'dense'}
-                  fullWidth
-                  floatingLabelText={fieldLabel}
-                  hintText={t`Choose a leaderboard`}
-                  helperMarkdownText={
-                    leaderboards && leaderboards.length === 0
-                      ? i18n._(
-                          t`There are currently no leaderboards created for this game. Open the leaderboards manager to create one.`
-                        )
-                      : null
-                  }
-                >
-                  {leaderboards && !!leaderboards.length
-                    ? leaderboards.map(leaderboard => (
-                        <SelectOption
-                          key={leaderboard.id}
-                          value={`"${leaderboard.id}"`}
-                          primaryText={`${leaderboard.name} ${
-                            leaderboard.id
-                              ? `(${breakUuid(leaderboard.id.substring(0, 8))})`
-                              : ''
-                          }`}
-                        />
-                      ))
-                    : [
-                        <SelectOption
-                          disabled
-                          key="empty"
-                          value="empty"
-                          primaryText={''}
-                        />,
-                      ]}
-                </SelectField>
-              ) : (
-                <GenericExpressionField
-                  expressionType="string"
-                  {...props}
-                  onChange={onChangeTextValue}
-                  onExtractAdditionalErrors={(
-                    currentExpression: string,
-                    currentExpressionNode: gdExpressionNode
-                  ) => {
-                    if (!leaderboards)
-                      return `Unable to fetch leaderboards as you are offline.`;
-                  }}
+    return (
+      <I18n>
+        {({ i18n }) => (
+          <>
+            <TextFieldWithButtonLayout
+              renderTextField={() =>
+                !isTextInput ? (
+                  <SelectField
+                    value={props.value}
+                    onChange={onChangeSelectValue}
+                    margin={props.isInline ? 'none' : 'dense'}
+                    fullWidth
+                    floatingLabelText={fieldLabel}
+                    hintText={t`Choose a leaderboard`}
+                    helperMarkdownText={
+                      leaderboards && leaderboards.length === 0
+                        ? i18n._(
+                            t`There are currently no leaderboards created for this game. Open the leaderboards manager to create one.`
+                          )
+                        : null
+                    }
+                  >
+                    {leaderboards && !!leaderboards.length
+                      ? leaderboards.map(leaderboard => (
+                          <SelectOption
+                            key={leaderboard.id}
+                            value={`"${leaderboard.id}"`}
+                            primaryText={`${leaderboard.name} ${
+                              leaderboard.id
+                                ? `(${breakUuid(
+                                    leaderboard.id.substring(0, 8)
+                                  )})`
+                                : ''
+                            }`}
+                          />
+                        ))
+                      : [
+                          <SelectOption
+                            disabled
+                            key="empty"
+                            value="empty"
+                            primaryText={''}
+                          />,
+                        ]}
+                  </SelectField>
+                ) : (
+                  <GenericExpressionField
+                    ref={genericExpressionFieldRef}
+                    expressionType="string"
+                    {...props}
+                    onChange={onChangeTextValue}
+                    onExtractAdditionalErrors={(
+                      currentExpression: string,
+                      currentExpressionNode: gdExpressionNode
+                    ) => {
+                      if (!leaderboards)
+                        return `Unable to fetch leaderboards as you are offline.`;
+                    }}
+                  />
+                )
+              }
+              renderButton={style => (
+                <RaisedButtonWithSplitMenu
+                  icon={<OpenInNew />}
+                  style={style}
+                  primary
+                  onClick={() => setIsAdminOpen(true)}
+                  buildMenuTemplate={i18n => [
+                    {
+                      label: isTextInput
+                        ? i18n._(t`Select the leaderboard from a list`)
+                        : i18n._(
+                            t`Enter the leaderboard id as a text or an expression`
+                          ),
+                      disabled: !leaderboards,
+                      click: () => setIsTextInput(!isTextInput),
+                    },
+                  ]}
                 />
-              )
-            }
-            renderButton={style => (
-              <RaisedButtonWithSplitMenu
-                icon={<OpenInNew />}
-                style={style}
-                primary
-                onClick={() => setIsAdminOpen(true)}
-                buildMenuTemplate={i18n => [
-                  {
-                    label: isTextInput
-                      ? i18n._(t`Select the leaderboard from a list`)
-                      : i18n._(
-                          t`Enter the leaderboard id as a text or an expression`
-                        ),
-                    disabled: !leaderboards,
-                    click: () => setIsTextInput(!isTextInput),
-                  },
-                ]}
+              )}
+            />
+            {isAdminOpen && !!props.project && (
+              <LeaderboardDialog
+                onClose={() => setIsAdminOpen(false)}
+                open={isAdminOpen}
+                project={props.project}
               />
             )}
-          />
-          {isAdminOpen && !!props.project && (
-            <LeaderboardDialog
-              onClose={() => setIsAdminOpen(false)}
-              open={isAdminOpen}
-              project={props.project}
-            />
-          )}
-        </>
-      )}
-    </I18n>
-  );
-}
-
-export default React.forwardRef<ParameterFieldProps, {||}>(LeaderboardIdField);
+          </>
+        )}
+      </I18n>
+    );
+  }
+);
 
 const InlineLeaderboardIdField = ({
   value,

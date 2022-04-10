828cbe769958ff780f23489e8ffe95d1a9733093
Davy Hélard <davy.helard@gmail.com>
2022-04-10 22:07:46 +0200

    Make the lib easier to build.

--- SharedLibs/TileMapHelper/rollup.config.js
+++ SharedLibs/TileMapHelper/rollup.config.js
@@ -1,43 +1,44 @@
-import pkg from "./package.json";
-import typescript from "@rollup/plugin-typescript";
+//import pkg from "./package.json";
+//import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+//import commonjs from '@rollup/plugin-commonjs';
+
+// export default {
+//     input: "src/index.ts",
+//     output: {
+//       name: "TileMapHelper",
+//       format: "umd",
+//       file: "../../Extensions/TileMap/helper/TileMapHelper.js",
+//       sourcemap: true,
+//     },
+//     external: ["pixi.js"],
+//     plugins: [
+//       typescript({ tsconfig: './tsconfig.json' }),
+
+//       // It doesn't seem necessary
+//       // /**
+//       //  * https://github.com/bigtimebuddy/pixi-rollup-example/blob/main/rollup.config.js
+//       //  * 
+//       //  * Required!
+//       //  *
+//       //  * `preferBuiltins` is required to not confuse Rollup with
+//       //  * the 'url' dependence that is used by PixiJS utils
+//       //  */
+//       // resolve({
+//       //   preferBuiltins: false,
+//       // }),
+//       // /**
+//       //  * Required!
+//       //  *
+//       //  * PixiJS third-party dependencies use CommonJS exports
+//       //  * and do not have modules bundles available
+//       //  */
+//       // commonjs(),
+//     ],
+// }
 
 export default [
-  // {
-  //   input: "src/index.ts",
-  //   output: {
-  //     name: "TileMapHelper",
-  //     format: "umd",
-  //     file: "../../Extensions/TileMap/helper/TileMapHelper.js",
-  //     sourcemap: true,
-  //   },
-  //   plugins: [
-  //     //typescript(),
-
-  //     // It doesn't seem necessary
-  //     // /**
-  //     //  * https://github.com/bigtimebuddy/pixi-rollup-example/blob/main/rollup.config.js
-  //     //  * 
-  //     //  * Required!
-  //     //  *
-  //     //  * `preferBuiltins` is required to not confuse Rollup with
-  //     //  * the 'url' dependence that is used by PixiJS utils
-  //     //  */
-  //     // resolve({
-  //     //   preferBuiltins: false,
-  //     // }),
-  //     // /**
-  //     //  * Required!
-  //     //  *
-  //     //  * PixiJS third-party dependencies use CommonJS exports
-  //     //  * and do not have modules bundles available
-  //     //  */
-  //     // commonjs(),
-  //   ],
-  //   external: ["pixi.js"],
-  // },
   //To try without plugin-typescript
   {
   	input: './dist/index.js',//'../../Extensions/TileMap/helper/index.js',//

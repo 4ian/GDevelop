c3788c4db99db48d40c03fe2548a61806e742374
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-04-14 18:42:25 +0200

    Forward ref for image resource field

--- newIDE/app/src/EventsSheet/ParameterFields/ImageResourceField.js
+++ newIDE/app/src/EventsSheet/ParameterFields/ImageResourceField.js
@@ -4,14 +4,24 @@ import { Trans } from '@lingui/macro';
 import * as React from 'react';
 import ResourceSelector from '../../ResourcesList/ResourceSelector';
 import ResourcesLoader from '../../ResourcesLoader';
-import { type ParameterFieldProps } from './ParameterFieldCommons';
+import {
+  type ParameterFieldProps,
+  type ParameterFieldInterface,
+} from './ParameterFieldCommons';
 
-const ImageResourceField = (props: ParameterFieldProps) => {
+const ImageResourceField = React.forwardRef<
+  ParameterFieldProps,
+  ParameterFieldInterface
+>((props, ref) => {
   const fieldRef = React.useRef<?ResourceSelector>(null);
 
   const focus = (selectAll: boolean = false) => {
     if (fieldRef.current) fieldRef.current.focus(selectAll);
   };
+
+  React.useImperativeHandle(ref, () => ({
+    focus,
+  }));
 
   if (
     !props.resourceSources ||
@@ -43,6 +53,6 @@ const ImageResourceField = (props: ParameterFieldProps) => {
       ref={fieldRef}
     />
   );
-};
+});
 
 export default ImageResourceField;

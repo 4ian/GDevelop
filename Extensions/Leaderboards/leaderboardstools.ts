38d75bcb261e827e1ad7e444fe0c8975dae7ed50
Fannie Yan <fannie@gdevelop.io>
2022-04-22 15:49:23 +0200

    Format

--- Extensions/Leaderboards/leaderboardstools.ts
+++ Extensions/Leaderboards/leaderboardstools.ts
@@ -562,8 +562,9 @@ namespace gdjs {
 
               resetLeaderboardDisplayErrorTimeout(runtimeScene);
 
-              _leaderboardViewIframe =
-                computeLeaderboardDisplayingIframe(targetUrl);
+              _leaderboardViewIframe = computeLeaderboardDisplayingIframe(
+                targetUrl
+              );
               if (typeof window !== 'undefined') {
                 _leaderboardViewClosingCallback = (event: MessageEvent) => {
                   receiveMessageFromLeaderboardView(

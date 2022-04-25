905f56f99cf05740eddff39f6d6f5756e23a82a0
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-04-25 18:34:22 +0200

    Add duration formatter

---
+++ newIDE/app/src/Utils/DurationFormatter.js
@@ -0,0 +1,54 @@
+// @flow
+
+type DurationFormattingOption = 'hour' | 'minute' | 'second' | 'millisecond';
+
+const labelToDivider = {
+  hour: 3600000,
+  minute: 60000,
+  second: 1000,
+  millisecond: 1,
+};
+const labelToNextSeparator = {
+  hour: ':',
+  minute: ':',
+  second: '.',
+  millisecond: '',
+};
+
+const shouldDisplaySeparator = (
+  currentUnit: DurationFormattingOption,
+  options: DurationFormattingOption[]
+): boolean => {
+  switch (currentUnit) {
+    case 'hour':
+      return options.includes('minute');
+    case 'minute':
+      return options.includes('second');
+    case 'second':
+      return options.includes('millisecond');
+    default:
+      return false;
+  }
+};
+
+export const formatDuration = (
+  durationInSecond: number,
+  options: DurationFormattingOption[]
+): string => {
+  let formattedDuration = '';
+  let durationInMs = Math.round(durationInSecond * 1000);
+  for (const unit of ['hour', 'minute', 'second', 'millisecond']) {
+    if (options.includes(unit)) {
+      const divider = labelToDivider[unit];
+      const remainder = Math.round((durationInMs % divider) * 1000) / 1000;
+      const quotient = (durationInMs - remainder) / divider;
+      formattedDuration += `${quotient
+        .toString()
+        .padStart(unit === 'millisecond' ? 3 : 2, '0')}${
+        shouldDisplaySeparator(unit, options) ? labelToNextSeparator[unit] : ''
+      }`;
+      durationInMs = remainder;
+    }
+  }
+  return formattedDuration;
+};
---
+++ newIDE/app/src/Utils/DurationFormatter.spec.js
@@ -0,0 +1,103 @@
+// @flow
+
+const { it } = require('date-fns/locale');
+const { formatDuration } = require('./DurationFormatter');
+
+describe('Duration', () => {
+  describe('formatDuration', () => {
+    test('it correctly formats whole seconds', () => {
+      expect(formatDuration(81, ['second'])).toEqual('81');
+      expect(formatDuration(15, ['second'])).toEqual('15');
+      expect(formatDuration(8, ['second'])).toEqual('08');
+    });
+    test('it correctly formats seconds without milliseconds', () => {
+      expect(formatDuration(81.29, ['second'])).toEqual('81');
+      expect(formatDuration(15.677, ['second'])).toEqual('15');
+      expect(formatDuration(8.045, ['second'])).toEqual('08');
+    });
+    test('it correctly formats seconds with milliseconds', () => {
+      expect(formatDuration(81.29, ['second', 'millisecond'])).toEqual(
+        '81.290'
+      );
+      expect(formatDuration(15.677, ['second', 'millisecond'])).toEqual(
+        '15.677'
+      );
+      expect(formatDuration(8.045, ['second', 'millisecond'])).toEqual(
+        '08.045'
+      );
+    });
+
+    test('it correctly formats whole hours', () => {
+      expect(formatDuration(39 * 3600, ['hour'])).toEqual('39');
+      expect(formatDuration(15 * 3600, ['hour'])).toEqual('15');
+      expect(formatDuration(8 * 3600, ['hour'])).toEqual('08');
+    });
+    test('it correctly formats hours without lower units', () => {
+      expect(
+        formatDuration(39 * 3600 + Math.random() * 15 * 60, ['hour'])
+      ).toEqual('39');
+      expect(
+        formatDuration(15 * 3600 + Math.random() * 15 * 60, ['hour'])
+      ).toEqual('15');
+      expect(
+        formatDuration(8 * 3600 + Math.random() * 15 * 60, ['hour'])
+      ).toEqual('08');
+    });
+    test('it correctly formats hours with minutes', () => {
+      expect(
+        formatDuration(39 * 3600 + 13 * 60 + 6 + 0.134, ['hour', 'minute'])
+      ).toEqual('39:13');
+      expect(
+        formatDuration(15 * 3600 + 45 * 60 + 56 + 0.98332, ['hour', 'minute'])
+      ).toEqual('15:45');
+      expect(
+        formatDuration(8 * 3600 + 9 * 60 + 0.05, ['hour', 'minute'])
+      ).toEqual('08:09');
+    });
+    test('it correctly formats hours with minutes and seconds', () => {
+      expect(
+        formatDuration(39 * 3600 + 13 * 60 + 6 + 0.134, [
+          'hour',
+          'minute',
+          'second',
+        ])
+      ).toEqual('39:13:06');
+      expect(
+        formatDuration(15 * 3600 + 45 * 60 + 56 + 0.98332, [
+          'hour',
+          'minute',
+          'second',
+        ])
+      ).toEqual('15:45:56');
+      expect(
+        formatDuration(8 * 3600 + 9 * 60 + 0.05, ['hour', 'minute', 'second'])
+      ).toEqual('08:09:00');
+    });
+    test('it correctly formats hours with minutes, seconds and milliseconds', () => {
+      expect(
+        formatDuration(39 * 3600 + 13 * 60 + 6 + 0.134, [
+          'hour',
+          'minute',
+          'second',
+          'millisecond',
+        ])
+      ).toEqual('39:13:06.134');
+      expect(
+        formatDuration(15 * 3600 + 45 * 60 + 56 + 0.98352, [
+          'hour',
+          'minute',
+          'second',
+          'millisecond',
+        ])
+      ).toEqual('15:45:56.984');
+      expect(
+        formatDuration(8 * 3600 + 9 * 60 + 0.05, [
+          'hour',
+          'minute',
+          'second',
+          'millisecond',
+        ])
+      ).toEqual('08:09:00.050');
+    });
+  });
+});

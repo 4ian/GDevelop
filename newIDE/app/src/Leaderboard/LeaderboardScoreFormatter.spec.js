eb72f8b08f1105dfeddff2a154aa215108d70356
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-04-26 09:18:50 +0200

    Move file in leaderboard dedicated folder

--- newIDE/app/src/Leaderboard/LeaderboardScoreFormatter.spec.js
+++ newIDE/app/src/Leaderboard/LeaderboardScoreFormatter.spec.js
@@ -1,7 +1,7 @@
 // @flow
 
 const { it } = require('date-fns/locale');
-const { formatDuration } = require('./DurationFormatter');
+const { formatDuration } = require('./LeaderboardScoreFormatter');
 
 describe('Duration', () => {
   describe('formatDuration', () => {

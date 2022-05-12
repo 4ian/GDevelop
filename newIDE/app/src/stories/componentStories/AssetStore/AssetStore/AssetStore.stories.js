50f5949a65ab9b0a2cf9abd3e8f02df70177d3f4
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-05-12 16:16:01 +0200

    Fix unnecessary fixed height

--- newIDE/app/src/stories/componentStories/AssetStore/AssetStore/AssetStore.stories.js
+++ newIDE/app/src/stories/componentStories/AssetStore/AssetStore/AssetStore.stories.js
@@ -5,7 +5,6 @@ import { action } from '@storybook/addon-actions';
 import muiDecorator from '../../../ThemeDecorator';
 import paperDecorator from '../../../PaperDecorator';
 import { testProject } from '../../../GDevelopJsInitializerDecorator';
-import FixedHeightFlexContainer from '../../../FixedHeightFlexContainer';
 import { AssetStoreStateProvider } from '../../../../AssetStore/AssetStoreContext';
 import { AssetStore } from '../../../../AssetStore';
 
@@ -16,14 +15,12 @@ export default {
 };
 
 export const Default = () => (
-  <FixedHeightFlexContainer height={400}>
-    <AssetStoreStateProvider>
-      <AssetStore
-        onOpenDetails={action('onOpenDetails')}
-        events={testProject.testLayout.getEvents()}
-        project={testProject.project}
-        objectsContainer={testProject.testLayout}
-      />
-    </AssetStoreStateProvider>
-  </FixedHeightFlexContainer>
+  <AssetStoreStateProvider>
+    <AssetStore
+      onOpenDetails={action('onOpenDetails')}
+      events={testProject.testLayout.getEvents()}
+      project={testProject.project}
+      objectsContainer={testProject.testLayout}
+    />
+  </AssetStoreStateProvider>
 );

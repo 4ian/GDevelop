77e606bebf993fcb5554255ec1d2f29e0514aa0d
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-05-13 14:15:52 +0200

    Use large spacer where useful

--- newIDE/app/src/VariablesList/VariableTypeSelector.js
+++ newIDE/app/src/VariablesList/VariableTypeSelector.js
@@ -8,7 +8,7 @@ import VariableNumberIcon from './Icons/VariableNumberIcon';
 import VariableBooleanIcon from './Icons/VariableBooleanIcon';
 import VariableArrayIcon from './Icons/VariableArrayIcon';
 import VariableStructureIcon from './Icons/VariableStructureIcon';
-import { Spacer } from '../UI/Grid';
+import { LargeSpacer } from '../UI/Grid';
 import GDevelopThemeContext from '../UI/Theme/ThemeContext';
 const gd = global.gd;
 
@@ -48,9 +48,7 @@ const VariableTypeSelector = (props: Props) => {
             : undefined
         }
       />
-      <Spacer />
-      <Spacer />
-      <Spacer />
+      <LargeSpacer />
       <SelectField
         value={props.variableType}
         margin="none"

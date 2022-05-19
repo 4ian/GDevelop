1ffa365e13a84a80c8b6324c3f0dd630a7373f5e
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-05-19 18:28:53 +0200

    WIP rework Asset Store details page and navigation

--- newIDE/app/src/AssetStore/index.js
+++ newIDE/app/src/AssetStore/index.js
@@ -8,7 +8,16 @@ import DoubleChevronArrow from '../UI/CustomSvgIcons/DoubleChevronArrow';
 import { Column, Line, Spacer } from '../UI/Grid';
 import Background from '../UI/Background';
 import ScrollView from '../UI/ScrollView';
-import { sendAssetPackOpened } from '../Utils/Analytics/EventSender';
+import {
+  sendAssetAddedToProject,
+  sendAssetOpened,
+  sendAssetPackOpened,
+} from '../Utils/Analytics/EventSender';
+import {
+  type ResourceSource,
+  type ChooseResourceFunction,
+} from '../ResourcesList/ResourceSource';
+import { type ResourceExternalEditor } from '../ResourcesList/ResourceExternalEditor.flow';
 import { type AssetShortHeader } from '../Utils/GDevelopServices/Asset';
 import { BoxSearchResults } from '../UI/Search/BoxSearchResults';
 import { type SearchBarInterface } from '../UI/SearchBar';
@@ -22,6 +31,12 @@ import { AssetsHome } from './AssetsHome';
 import FlatButton from '../UI/FlatButton';
 import Text from '../UI/Text';
 import IconButton from '../UI/IconButton';
+import { AssetDetails } from './AssetDetails';
+import EventsFunctionsExtensionsContext from '../EventsFunctionsExtensionsLoader/EventsFunctionsExtensionsContext';
+import { installAsset } from './InstallAsset';
+import { useResourceFetcher } from '../ProjectsStorage/ResourceFetcher';
+import { showErrorBox } from '../UI/Messages/MessageBox';
+import PlaceholderLoader from '../UI/PlaceholderLoader';
 
 const styles = {
   searchBar: {
@@ -34,16 +49,24 @@ type Props = {
   project: gdProject,
   objectsContainer: gdObjectsContainer,
   events: gdEventsList,
-  onOpenDetails: AssetShortHeader => void,
   focusOnMount?: boolean,
+  onObjectAddedFromAsset: (object: gdObject) => void,
+  layout: ?gdLayout,
+  resourceSources: Array<ResourceSource>,
+  resourceExternalEditors: Array<ResourceExternalEditor>,
+  onChooseResource: ChooseResourceFunction,
 };
 
 export const AssetStore = ({
   project,
   objectsContainer,
   events,
-  onOpenDetails,
   focusOnMount,
+  onObjectAddedFromAsset,
+  layout,
+  resourceSources,
+  resourceExternalEditors,
+  onChooseResource,
 }: Props) => {
   const {
     filters,
@@ -65,8 +88,104 @@ export const AssetStore = ({
 
   const searchBar = React.useRef<?SearchBarInterface>(null);
   const shouldAutofocusSearchbar = useShouldAutofocusSearchbar();
-  const [isOnHomePage, setIsOnHomePage] = React.useState(true);
+  const [openedAssetPack, setOpenedAssetPack] = React.useState(null);
+  const [
+    openedAssetShortHeader,
+    setOpenedAssetShortHeader,
+  ] = React.useState<?AssetShortHeader>(null);
   const [isFiltersOpen, setIsFiltersOpen] = React.useState(false);
+
+  const [
+    assetBeingInstalled,
+    setAssetBeingInstalled,
+  ] = React.useState<?AssetShortHeader>(null);
+
+  const eventsFunctionsExtensionsState = React.useContext(
+    EventsFunctionsExtensionsContext
+  );
+
+  const resourcesFetcher = useResourceFetcher();
+
+  const onInstallAsset = React.useCallback(
+    (assetShortHeader: AssetShortHeader) => {
+      setAssetBeingInstalled(assetShortHeader);
+      (async () => {
+        try {
+          const installOutput = await installAsset({
+            assetShortHeader,
+            eventsFunctionsExtensionsState,
+            project,
+            objectsContainer,
+            events,
+          });
+          sendAssetAddedToProject({
+            id: assetShortHeader.id,
+            name: assetShortHeader.name,
+          });
+          console.log('Asset successfully installed.');
+
+          installOutput.createdObjects.forEach(object => {
+            onObjectAddedFromAsset(object);
+          });
+
+          await resourcesFetcher.ensureResourcesAreFetched(project);
+        } catch (error) {
+          console.error('Error while installing the asset:', error);
+          showErrorBox({
+            message: `There was an error while installing the asset "${
+              assetShortHeader.name
+            }". Verify your internet connection or try again later.`,
+            rawError: error,
+            errorId: 'install-asset-error',
+          });
+        }
+
+        setAssetBeingInstalled(null);
+      })();
+    },
+    [
+      resourcesFetcher,
+      eventsFunctionsExtensionsState,
+      project,
+      objectsContainer,
+      events,
+      onObjectAddedFromAsset,
+    ]
+  );
+
+  const isOnHomePage =
+    !openedAssetPack && !openedAssetShortHeader && !searchText;
+
+  const resetAssetSelection = () => {
+    setOpenedAssetPack(null);
+    setOpenedAssetShortHeader(null);
+  };
+
+  const selectPack = (tag: string) => {
+    if (!assetPacks) return;
+
+    sendAssetPackOpened(tag);
+    setOpenedAssetPack(tag);
+
+    const chosenCategory = {
+      node: { name: tag, allChildrenTags: [], children: [] },
+      parentNodes: [],
+    };
+    filtersState.setChosenCategory(chosenCategory);
+
+    setIsFiltersOpen(true);
+  };
+
+  const selectTag = (tag: string) => {
+    setOpenedAssetShortHeader(null);
+    setOpenedAssetPack(tag);
+    const chosenCategory = {
+      node: { name: tag, allChildrenTags: [], children: [] },
+      parentNodes: [],
+    };
+    filtersState.setChosenCategory(chosenCategory);
+    setIsFiltersOpen(true);
+  };
 
   React.useEffect(
     () => {
@@ -78,123 +197,170 @@ export const AssetStore = ({
   );
 
   return (
-    <ResponsiveWindowMeasurer>
-      {windowWidth => (
-        <Column expand noMargin useFullHeight>
-          <SearchBar
-            placeholder={t`Enter your Search`}
-            value={searchText}
-            onChange={setSearchText}
-            onRequestSearch={() => setIsOnHomePage(false)}
-            style={styles.searchBar}
-            ref={searchBar}
-            id="asset-store-search-bar"
-          />
-          {!isOnHomePage && <Spacer />}
-          <Line justifyContent="left" noMargin={isOnHomePage}>
-            {isOnHomePage ? (
-              <Column>
-                <Text size="title">
-                  <Trans>Discover</Trans>
-                </Text>
-              </Column>
-            ) : (
-              <FlatButton
-                icon={<ArrowBack />}
-                label={<Trans>Back to discover</Trans>}
-                primary={false}
-                onClick={() => {
-                  filtersState.setChosenCategory(null);
-                  setIsFiltersOpen(false);
-                  setIsOnHomePage(true);
-                }}
+    <>
+      <ResponsiveWindowMeasurer>
+        {windowWidth => (
+          <>
+            <Column expand noMargin useFullHeight>
+              <SearchBar
+                placeholder={t`Enter your Search`}
+                value={searchText}
+                onChange={setSearchText}
+                onRequestSearch={resetAssetSelection}
+                style={styles.searchBar}
+                ref={searchBar}
+                id="asset-store-search-bar"
               />
-            )}
-          </Line>
-          <Line
-            expand
-            overflow={
-              'hidden' /* Somehow required on Chrome/Firefox to avoid children growing (but not on Safari) */
-            }
-          >
-            <Background
-              noFullHeight
-              noExpand
-              width={!isFiltersOpen ? 50 : windowWidth === 'small' ? 150 : 250}
-            >
-              {!isFiltersOpen ? (
-                <Line justifyContent="center">
-                  <IconButton onClick={() => setIsFiltersOpen(true)}>
-                    <Tune />
-                  </IconButton>
-                </Line>
-              ) : (
-                <ScrollView>
-                  <Line justifyContent="space-between" alignItems="center">
-                    <Column>
-                      <Line alignItems="center">
-                        <Tune />
-                        <Subheader>
-                          <Trans>Categories</Trans>
-                        </Subheader>
-                      </Line>
-                    </Column>
-                    <IconButton onClick={() => setIsFiltersOpen(false)}>
-                      <DoubleChevronArrow />
-                    </IconButton>
-                  </Line>
-                  <>
-                    <CategoryChooser
-                      allItemsLabel={<Trans>All assets</Trans>}
-                      allFilters={filters}
-                      filtersState={filtersState}
-                      onChoiceChange={() => setIsOnHomePage(false)}
-                      error={error}
-                    />
-                    <Subheader>
-                      <Trans>Filters</Trans>
-                    </Subheader>
-                    <FiltersChooser
-                      allFilters={filters}
-                      filtersState={filtersState}
-                      error={error}
-                    />
-                  </>
-                </ScrollView>
-              )}
-            </Background>
-            {isOnHomePage && assetPacks ? (
-              <AssetsHome
-                assetPacks={assetPacks}
-                onPackSelection={tag => {
-                  const chosenCategory = {
-                    node: { name: tag, allChildrenTags: [], children: [] },
-                    parentNodes: [],
-                  };
-                  sendAssetPackOpened(tag);
-                  filtersState.setChosenCategory(chosenCategory);
-                  setIsFiltersOpen(true);
-                  setIsOnHomePage(false);
-                }}
-              />
-            ) : (
-              <BoxSearchResults
-                baseSize={128}
-                onRetry={fetchAssetsAndFilters}
-                error={error}
-                searchItems={searchResults}
-                renderSearchItem={(assetShortHeader, size) => (
-                  <AssetCard
-                    size={size}
-                    onOpenDetails={() => onOpenDetails(assetShortHeader)}
-                    assetShortHeader={assetShortHeader}
+              {!isOnHomePage && <Spacer />}
+              <Line justifyContent="left" noMargin>
+                {isOnHomePage ? (
+                  <Column>
+                    <Text size="title">
+                      <Trans>Discover</Trans>
+                    </Text>
+                  </Column>
+                ) : (
+                  <FlatButton
+                    icon={<ArrowBack />}
+                    label={
+                      openedAssetShortHeader ? (
+                        <Trans>Back</Trans>
+                      ) : (
+                        <Trans>Back to discover</Trans>
+                      )
+                    }
+                    primary={false}
+                    onClick={() => {
+                      if (openedAssetShortHeader) {
+                        // Going back from Asset page to search.
+                        setOpenedAssetShortHeader(null);
+                      } else {
+                        // Going back from search to home.
+                        setSearchText('');
+                        filtersState.setChosenCategory(null);
+                        setIsFiltersOpen(false);
+                        resetAssetSelection();
+                      }
+                    }}
                   />
                 )}
-              />
-            )}
-          </Line>
-        </Column>
-      )}
-    </ResponsiveWindowMeasurer>
+              </Line>
+              <Line
+                expand
+                overflow={
+                  'hidden' /* Somehow required on Chrome/Firefox to avoid children growing (but not on Safari) */
+                }
+              >
+                {!openedAssetShortHeader && ( // Don't show filters on asset page.
+                  <Background
+                    noFullHeight
+                    noExpand
+                    width={
+                      !isFiltersOpen ? 50 : windowWidth === 'small' ? 150 : 250
+                    }
+                  >
+                    {!isFiltersOpen ? (
+                      <Line justifyContent="center">
+                        <IconButton onClick={() => setIsFiltersOpen(true)}>
+                          <Tune />
+                        </IconButton>
+                      </Line>
+                    ) : (
+                      <ScrollView>
+                        <Line
+                          justifyContent="space-between"
+                          alignItems="center"
+                        >
+                          <Column>
+                            <Line alignItems="center">
+                              <Tune />
+                              <Subheader>
+                                <Trans>Categories</Trans>
+                              </Subheader>
+                            </Line>
+                          </Column>
+                          <IconButton onClick={() => setIsFiltersOpen(false)}>
+                            <DoubleChevronArrow />
+                          </IconButton>
+                        </Line>
+                        <>
+                          <CategoryChooser
+                            allItemsLabel={<Trans>All assets</Trans>}
+                            allFilters={filters}
+                            filtersState={filtersState}
+                            onChoiceChange={category => {
+                              if (category) {
+                                setOpenedAssetPack(category.node.name);
+                              }
+                            }}
+                            error={error}
+                          />
+                          <Subheader>
+                            <Trans>Filters</Trans>
+                          </Subheader>
+                          <FiltersChooser
+                            allFilters={filters}
+                            filtersState={filtersState}
+                            error={error}
+                          />
+                        </>
+                      </ScrollView>
+                    )}
+                  </Background>
+                )}
+                {isOnHomePage && !assetPacks && <PlaceholderLoader />}
+                {isOnHomePage && assetPacks && (
+                  <AssetsHome
+                    assetPacks={assetPacks}
+                    onPackSelection={selectPack}
+                  />
+                )}
+                {(openedAssetPack || searchText) && !openedAssetShortHeader && (
+                  <BoxSearchResults
+                    baseSize={128}
+                    onRetry={fetchAssetsAndFilters}
+                    error={error}
+                    searchItems={searchResults}
+                    renderSearchItem={(assetShortHeader, size) => (
+                      <AssetCard
+                        size={size}
+                        onOpenDetails={() => {
+                          sendAssetOpened({
+                            id: assetShortHeader.id,
+                            name: assetShortHeader.name,
+                          });
+                          setOpenedAssetShortHeader(assetShortHeader);
+                          setIsFiltersOpen(false);
+                        }}
+                        assetShortHeader={assetShortHeader}
+                      />
+                    )}
+                  />
+                )}
+                {openedAssetShortHeader && (
+                  <AssetDetails
+                    project={project}
+                    layout={layout}
+                    objectsContainer={objectsContainer}
+                    resourceSources={resourceSources}
+                    resourceExternalEditors={resourceExternalEditors}
+                    onTagSelection={selectTag}
+                    assetShortHeader={openedAssetShortHeader}
+                    onAdd={() => onInstallAsset(openedAssetShortHeader)}
+                    onClose={() => setOpenedAssetShortHeader(null)}
+                    canInstall={!assetBeingInstalled}
+                    isBeingInstalled={
+                      !!assetBeingInstalled &&
+                      assetBeingInstalled.id === openedAssetShortHeader.id
+                    }
+                  />
+                )}
+              </Line>
+            </Column>
+            {resourcesFetcher.renderResourceFetcherDialog()}
+          </>
+        )}
+      </ResponsiveWindowMeasurer>
+    </>
   );
 };

b12970920ace00084250736fb95281e950062080
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-05-20 17:11:24 +0200

    Add some spacing

--- newIDE/app/src/EventsSheet/ParameterFields/GenericExpressionField/ExpressionParametersEditor.js
+++ newIDE/app/src/EventsSheet/ParameterFields/GenericExpressionField/ExpressionParametersEditor.js
@@ -4,6 +4,7 @@ import { type EventsScope } from '../../../InstructionOrExpression/EventsScope.f
 import * as React from 'react';
 import { mapFor } from '../../../Utils/MapFor';
 import EmptyMessage from '../../../UI/EmptyMessage';
+import { Spacer } from '../../../UI/Grid';
 
 export type ParameterValues = Array<string>;
 
@@ -74,20 +75,23 @@ const ExpressionParametersEditor = ({
 
         if (parameterMetadata.isCodeOnly()) return null;
         return (
-          <ParameterComponent
-            expressionMetadata={expressionMetadata}
-            expression={expression}
-            parameterMetadata={parameterMetadata}
-            parameterIndex={i}
-            value={parameterValues[i]}
-            onChange={value => onChangeParameter(i, value)}
-            project={project}
-            scope={scope}
-            globalObjectsContainer={globalObjectsContainer}
-            objectsContainer={objectsContainer}
-            key={i}
-            parameterRenderingService={parameterRenderingService}
-          />
+          <>
+            {i > 0 && <Spacer />}
+            <ParameterComponent
+              expressionMetadata={expressionMetadata}
+              expression={expression}
+              parameterMetadata={parameterMetadata}
+              parameterIndex={i}
+              value={parameterValues[i]}
+              onChange={value => onChangeParameter(i, value)}
+              project={project}
+              scope={scope}
+              globalObjectsContainer={globalObjectsContainer}
+              objectsContainer={objectsContainer}
+              key={i}
+              parameterRenderingService={parameterRenderingService}
+            />
+          </>
         );
       })}
       {!hasNonCodeOnlyParameters(expressionMetadata) && (

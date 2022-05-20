88e1c5a9cdaf0b51112ee9eb5cdb12be8a1a6a2f
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-05-20 17:10:56 +0200

    Transform ExpressionParametersEditor into functional component

--- newIDE/app/src/EventsSheet/ParameterFields/GenericExpressionField/ExpressionParametersEditor.js
+++ newIDE/app/src/EventsSheet/ParameterFields/GenericExpressionField/ExpressionParametersEditor.js
@@ -28,7 +28,6 @@ type Props = {|
     getParameterComponent: (type: string) => any,
   },
 |};
-type State = {||};
 
 export const hasNonCodeOnlyParameters = (
   expressionMetadata: gdExpressionMetadata
@@ -38,70 +37,66 @@ export const hasNonCodeOnlyParameters = (
     return !parameterMetadata.isCodeOnly();
   }).filter(isVisible => isVisible).length !== 0;
 
-export default class ExpressionParametersEditor extends React.Component<
-  Props,
-  State
-> {
-  render() {
-    const {
-      expressionMetadata,
-      parameterValues,
-      project,
-      scope,
-      globalObjectsContainer,
-      objectsContainer,
-      parameterRenderingService,
-    } = this.props;
-
-    if (!parameterRenderingService) {
-      console.error(
-        'Missing parameterRenderingService for ExpressionParametersEditor'
-      );
-      return null;
-    }
-
-    // Create an object mimicking Instruction interface so that it can be used by
-    // ParameterFields components.
-    const parametersCount = expressionMetadata.getParametersCount();
-    const expression = {
-      getParametersCount: () => parametersCount,
-      getParameter: index => {
-        return parameterValues[index] || '';
-      },
-    };
-
-    return (
-      <div style={styles.container}>
-        {mapFor(0, expressionMetadata.getParametersCount(), i => {
-          const parameterMetadata = expressionMetadata.getParameter(i);
-          const ParameterComponent = parameterRenderingService.getParameterComponent(
-            parameterMetadata.getType()
-          );
-
-          if (parameterMetadata.isCodeOnly()) return null;
-          return (
-            <ParameterComponent
-              expressionMetadata={expressionMetadata}
-              expression={expression}
-              parameterMetadata={parameterMetadata}
-              parameterIndex={i}
-              value={parameterValues[i]}
-              onChange={value => this.props.onChangeParameter(i, value)}
-              project={project}
-              scope={scope}
-              globalObjectsContainer={globalObjectsContainer}
-              objectsContainer={objectsContainer}
-              key={i}
-              parameterRenderingService={parameterRenderingService}
-            />
-          );
-        })}
-        {!hasNonCodeOnlyParameters(expressionMetadata) && (
-          <EmptyMessage>
-            <Trans>There is nothing to configure.</Trans>
-          </EmptyMessage>
-        )}
-      </div>
+const ExpressionParametersEditor = ({
+  expressionMetadata,
+  parameterValues,
+  project,
+  scope,
+  globalObjectsContainer,
+  objectsContainer,
+  parameterRenderingService,
+  onChangeParameter,
+}: Props) => {
+  if (!parameterRenderingService) {
+    console.error(
+      'Missing parameterRenderingService for ExpressionParametersEditor'
     );
+    return null;
   }
-}
+
+  // Create an object mimicking Instruction interface so that it can be used by
+  // ParameterFields components.
+  const parametersCount = expressionMetadata.getParametersCount();
+  const expression = {
+    getParametersCount: () => parametersCount,
+    getParameter: index => {
+      return parameterValues[index] || '';
+    },
+  };
+
+  return (
+    <div style={styles.container}>
+      {mapFor(0, expressionMetadata.getParametersCount(), i => {
+        const parameterMetadata = expressionMetadata.getParameter(i);
+        const ParameterComponent = parameterRenderingService.getParameterComponent(
+          parameterMetadata.getType()
+        );
+
+        if (parameterMetadata.isCodeOnly()) return null;
+        return (
+          <ParameterComponent
+            expressionMetadata={expressionMetadata}
+            expression={expression}
+            parameterMetadata={parameterMetadata}
+            parameterIndex={i}
+            value={parameterValues[i]}
+            onChange={value => onChangeParameter(i, value)}
+            project={project}
+            scope={scope}
+            globalObjectsContainer={globalObjectsContainer}
+            objectsContainer={objectsContainer}
+            key={i}
+            parameterRenderingService={parameterRenderingService}
+          />
+        );
+      })}
+      {!hasNonCodeOnlyParameters(expressionMetadata) && (
+        <EmptyMessage>
+          <Trans>There is nothing to configure.</Trans>
+        </EmptyMessage>
+      )}
+    </div>
+  );
+};
+
+export default ExpressionParametersEditor;

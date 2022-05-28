7453896c89442f843f8e033b3fdad1829061fdfd
Florian Rival <Florian.Rival@gmail.com>
2022-05-28 16:02:18 +0200

    Reduce size of events in JSON by not writing empty events/sub-instructions arrays
    Only show in developer changelog

--- Core/GDCore/Events/Serialization.cpp
+++ Core/GDCore/Events/Serialization.cpp
@@ -4,6 +4,7 @@
  * reserved. This project is released under the MIT License.
  */
 #include "GDCore/Events/Serialization.h"
+
 #include "GDCore/CommonTools.h"
 #include "GDCore/Events/Event.h"
 #include "GDCore/Events/EventsList.h"
@@ -232,7 +233,8 @@ void EventsListSerialization::SerializeEventsTo(const EventsList& list,
     const gd::BaseEvent& event = list.GetEvent(j);
     SerializerElement& eventElem = events.AddChild("event");
 
-    if (event.IsDisabled()) eventElem.SetAttribute("disabled", event.IsDisabled());
+    if (event.IsDisabled())
+      eventElem.SetAttribute("disabled", event.IsDisabled());
     if (event.IsFolded()) eventElem.SetAttribute("folded", event.IsFolded());
     eventElem.AddChild("type").SetValue(event.GetType());
 
@@ -340,9 +342,10 @@ void gd::EventsListSerialization::SerializeInstructionsTo(
   instructions.ConsiderAsArrayOf("instruction");
   for (std::size_t k = 0; k < list.size(); k++) {
     SerializerElement& instruction = instructions.AddChild("instruction");
-    instruction.AddChild("type")
-        .SetAttribute("value", list[k].GetType())
-        .SetAttribute("inverted", list[k].IsInverted());
+    instruction.AddChild("type").SetAttribute("value", list[k].GetType());
+
+    if (list[k].IsInverted())
+      instruction.GetChild("type").SetAttribute("inverted", true);
 
     // Parameters
     SerializerElement& parameters = instruction.AddChild("parameters");
@@ -352,9 +355,10 @@ void gd::EventsListSerialization::SerializeInstructionsTo(
           .SetValue(list[k].GetParameter(l).GetPlainString());
 
     // Sub instructions
-    SerializerElement& subInstructions =
-        instruction.AddChild("subInstructions");
-    SerializeInstructionsTo(list[k].GetSubInstructions(), subInstructions);
+    if (!list[k].GetSubInstructions().empty()) {
+      SerializeInstructionsTo(list[k].GetSubInstructions(),
+                              instruction.AddChild("subInstructions"));
+    }
   }
 }
 

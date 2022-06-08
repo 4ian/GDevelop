bcdeaada0fe767e478cdad5c0d9affa160fc2ade
Sebastian Krzyszkowiak <dos@dosowisko.net>
2022-06-08 08:12:14 +0200

    Add an ability to preload a sound file without decoding
    PR #2006 made the preloaded sounds be decoded right away,
    bringing back old memory consumption issues from before #431.
    For games that want to download all their assets during
    the loading screen, but don't want to actually decode all sounds
    right away because of memory consumption concerns, being able to
    dynamically load/unload Howler objects via events is not enough,
    as the application may already go out-of-memory during loading,
    and not doing that will cause the game to download additional
    assets during gameplay. This change adds a new property,
    "preloadInCache", which makes GDJS request the sound asset via XHR.
    This puts the file into browser cache, so it can be reasonably
    expected to be decoded later without issuing network requests.
    
    While at it, add user visible descriptions for all preload options.

--- Core/GDCore/Project/ResourcesManager.cpp
+++ Core/GDCore/Project/ResourcesManager.cpp
@@ -184,10 +184,16 @@ std::map<gd::String, gd::PropertyDescriptor> AudioResource::GetProperties()
     const {
   std::map<gd::String, gd::PropertyDescriptor> properties;
   properties[_("Preload as sound")]
+      .SetDescription(_("Loads the fully decoded file into cache, so it can be played right away as Sound with no further delays."))
       .SetValue(preloadAsSound ? "true" : "false")
       .SetType("Boolean");
   properties[_("Preload as music")]
+      .SetDescription(_("Prepares the file for immediate streaming as Music (does not wait for complete download)."))
       .SetValue(preloadAsMusic ? "true" : "false")
+      .SetType("Boolean");
+  properties[_("Preload in cache")]
+      .SetDescription(_("Loads the complete file into cache, but does not decode it into memory until requested."))
+      .SetValue(preloadInCache ? "true" : "false")
       .SetType("Boolean");
 
   return properties;
@@ -199,6 +205,8 @@ bool AudioResource::UpdateProperty(const gd::String& name,
     preloadAsSound = value == "1";
   else if (name == _("Preload as music"))
     preloadAsMusic = value == "1";
+  else if (name == _("Preload in cache"))
+    preloadInCache = value == "1";
 
   return true;
 }

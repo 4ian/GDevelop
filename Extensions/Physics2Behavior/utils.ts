22386258082d6c9f1ddaf65deee83c9c88d2884a
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-06-15 14:23:50 +0200

    Greatly simplify util (why do simple when you can do hard)

--- Extensions/Physics2Behavior/utils.ts
+++ Extensions/Physics2Behavior/utils.ts
@@ -5,16 +5,10 @@ namespace gdjs {
       started: Array<T>,
       ended: Array<T>
     ): void => {
-      const endedDuplicate = [...ended];
       started.forEach((startedItem) => {
-        const isAlsoEndedIndex = endedDuplicate.indexOf(startedItem);
-        if (isAlsoEndedIndex !== -1) {
-          endedDuplicate.splice(isAlsoEndedIndex, 1);
-        } else {
-          current.push(startedItem);
-        }
+        current.push(startedItem);
       });
-      endedDuplicate.forEach((endedItem) => {
+      ended.forEach((endedItem) => {
         const index = current.indexOf(endedItem);
         if (index !== -1) {
           current.splice(index, 1);

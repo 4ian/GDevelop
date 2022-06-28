54c106c625fdc212a84fa922ca24fe13c11930e5
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-06-28 09:22:37 +0200

    Revert changes that won't be usefull in the MVP

--- newIDE/app/src/ProjectCreation/CreateProjectDialog.js
+++ newIDE/app/src/ProjectCreation/CreateProjectDialog.js
@@ -10,7 +10,6 @@ import { Column } from '../UI/Grid';
 import { type StorageProvider, type FileMetadata } from '../ProjectsStorage';
 import { type ExampleShortHeader } from '../Utils/GDevelopServices/Example';
 import ProjectPreCreationDialog from './ProjectPreCreationDialog';
-import { type AuthenticatedUser } from '../Profile/AuthenticatedUserContext';
 
 export type OnOpenProjectAfterCreationFunction = ({|
   project?: gdProject,
@@ -30,8 +29,6 @@ export type CreateProjectDialogWithComponentsProps = {|
 export type ProjectCreationSettings = {|
   projectName: string,
   outputPath?: string,
-  // TODO: Added for tests - TBD if this is the right way
-  authenticatedUser?: AuthenticatedUser,
 |};
 
 export type OnCreateBlankFunction = ({|

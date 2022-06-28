247b3e4836633781a9983a6080924a18d85b00bd
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-06-28 11:59:57 +0200

    Unzip project when opening it

--- newIDE/app/src/ProjectsStorage/CloudStorageProvider/CloudProjectOpener.js
+++ newIDE/app/src/ProjectsStorage/CloudStorageProvider/CloudProjectOpener.js
@@ -3,11 +3,27 @@
 import {
   getCloudProject,
   getCredentialsForProject,
-  getProjectFileAsJson,
+  getProjectFileAsZipBlob,
 } from '../../Utils/GDevelopServices/Project';
 import { type AuthenticatedUser } from '../../Profile/AuthenticatedUserContext';
 
 import { type FileMetadata } from '../index';
+import { initializeZipJs } from '../../Utils/Zip.js';
+
+const unzipProject = async (zippedProject: any) => {
+  const zipJs: ZipJs = await initializeZipJs();
+
+  return new Promise((resolve, reject) => {
+    zipJs.createReader(new zipJs.BlobReader(zippedProject), zipReader => {
+      zipReader.getEntries(entries => {
+        // Reading only the first entry since the zip should only contain the project json file
+        entries[0].getData(new zipJs.TextWriter(), result => {
+          resolve(result);
+        });
+      });
+    });
+  });
+};
 
 export const onOpenWithPicker = () => {};
 
@@ -22,9 +38,11 @@ export const generateOnOpen = (authenticatedUser: AuthenticatedUser) => async (
   if (!cloudProject) throw new Error("Cloud project couldn't be fetched.");
 
   await getCredentialsForProject(authenticatedUser, cloudProjectId);
-  const projectFileContent = getProjectFileAsJson(cloudProject);
+  const zippedSerializedProject = await getProjectFileAsZipBlob(cloudProject);
+  const serializedProject = await unzipProject(zippedSerializedProject);
+
   return {
-    content: projectFileContent,
+    content: JSON.parse(serializedProject),
   };
 };
 

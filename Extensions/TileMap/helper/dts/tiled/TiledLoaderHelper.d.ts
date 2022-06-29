4c7579ed69b65e8531525d7ad9e9d42601682f71
Davy Hélard <davy.helard@gmail.com>
2022-06-29 19:27:36 +0200

    Format

--- Extensions/TileMap/helper/dts/tiled/TiledLoaderHelper.d.ts
+++ Extensions/TileMap/helper/dts/tiled/TiledLoaderHelper.d.ts
@@ -1,5 +1,5 @@
-import { integer } from "../CommonTypes";
-import { TiledLayer } from "./Tiled";
+import { integer } from '../CommonTypes';
+import { TiledLayer } from './Tiled';
 /**
  * Decodes a layer data, which can sometimes be store as a compressed base64 string
  * by Tiled.
@@ -8,17 +8,22 @@ import { TiledLayer } from "./Tiled";
  * @param layer The layer data from a Tiled JSON.
  * @returns The decoded layer data.
  */
-export declare const decodeBase64LayerData: (pako: any, layer: TiledLayer) => number[];
+export declare const decodeBase64LayerData: (
+  pako: any,
+  layer: TiledLayer
+) => number[];
 export declare type TiledGID = {
-    id: integer;
-    flippedHorizontally: boolean;
-    flippedVertically: boolean;
-    flippedDiagonally: boolean;
+  id: integer;
+  flippedHorizontally: boolean;
+  flippedVertically: boolean;
+  flippedDiagonally: boolean;
 };
 /**
  * Extract information about the rotation of a tile from the tile id.
  * @param globalTileUid
  * @returns The tile identifier and orientation.
  */
-export declare const extractTileUidFlippedStates: (globalTileUid: integer) => TiledGID;
+export declare const extractTileUidFlippedStates: (
+  globalTileUid: integer
+) => TiledGID;
 //# sourceMappingURL=TiledLoaderHelper.d.ts.map
--- Extensions/TileMap/helper/dts/tiled/TiledTileMapLoader.d.ts
+++ Extensions/TileMap/helper/dts/tiled/TiledTileMapLoader.d.ts
@@ -1,9 +1,9 @@
-import { EditableTileMap } from "../TileMapModel";
-import { TiledMap } from "./Tiled";
+import { EditableTileMap } from '../TileMapModel';
+import { TiledMap } from './Tiled';
 /**
  * It creates a {@link EditableTileMap} from a Tiled JSON.
  */
 export declare class TiledTileMapLoader {
-    static load(pako: any, tiledMap: TiledMap): EditableTileMap | null;
+  static load(pako: any, tiledMap: TiledMap): EditableTileMap | null;
 }
 //# sourceMappingURL=TiledTileMapLoader.d.ts.map

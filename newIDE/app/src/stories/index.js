23a03c7578d8978bd9265007fb90be5037858184
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-06-29 15:34:02 +0200

    Move stories related to storage providers in dedicated files

--- newIDE/app/src/stories/index.js
+++ newIDE/app/src/stories/index.js
@@ -153,10 +153,6 @@ import EditorMosaicPlayground from './EditorMosaicPlayground';
 import EditorNavigator from '../UI/EditorMosaic/EditorNavigator';
 import ChooseEventsFunctionsExtensionEditor from '../EventsFunctionsExtensionEditor/ChooseEventsFunctionsExtensionEditor';
 import PropertiesEditor from '../PropertiesEditor';
-import OpenFromStorageProviderDialog from '../ProjectsStorage/OpenFromStorageProviderDialog';
-import GoogleDriveStorageProvider from '../ProjectsStorage/GoogleDriveStorageProvider';
-import LocalFileStorageProvider from '../ProjectsStorage/LocalFileStorageProvider';
-import GoogleDriveSaveAsDialog from '../ProjectsStorage/GoogleDriveStorageProvider/GoogleDriveSaveAsDialog';
 import { OpenConfirmDialog } from '../ProjectsStorage/OpenConfirmDialog';
 import CreateAccountDialog from '../Profile/CreateAccountDialog';
 import BrowserPreviewErrorDialog from '../Export/BrowserExporters/BrowserS3PreviewLauncher/BrowserPreviewErrorDialog';
@@ -2473,70 +2469,6 @@ storiesOf('Project Creation/CreateProjectDialog', module)
         onCreateFromExampleShortHeader={() => action('create from example')}
       />
     </ExampleStoreStateProvider>
-  ));
-
-storiesOf('OpenFromStorageProviderDialog', module)
-  .addDecorator(muiDecorator)
-  .add('default', () => (
-    <OpenFromStorageProviderDialog
-      storageProviders={[GoogleDriveStorageProvider, LocalFileStorageProvider]}
-      onChooseProvider={action('onChooseProvider')}
-      onClose={action('onClose')}
-    />
-  ));
-
-storiesOf(
-  'StorageProviders/GoogleDriveStorageProvider/GoogleDriveSaveAsDialog',
-  module
-)
-  .addDecorator(muiDecorator)
-  .add('default, fake picked file, save working', () => (
-    <GoogleDriveSaveAsDialog
-      onShowFilePicker={() =>
-        Promise.resolve({
-          type: 'FILE',
-          id: 'fake-id',
-          name: 'Fake Google Drive file',
-          parentId: 'fake-parent-id',
-        })
-      }
-      onCancel={action('cancel')}
-      onSave={() => Promise.resolve()}
-    />
-  ))
-  .add('default, fake picked folder, save working', () => (
-    <GoogleDriveSaveAsDialog
-      onShowFilePicker={() =>
-        Promise.resolve({
-          type: 'FOLDER',
-          id: 'fake-id',
-          name: 'Fake Google Drive file',
-        })
-      }
-      onCancel={action('cancel')}
-      onSave={() => Promise.resolve()}
-    />
-  ))
-  .add('default, error when picking file/folder', () => (
-    <GoogleDriveSaveAsDialog
-      onShowFilePicker={() => Promise.reject(new Error('fake-error'))}
-      onCancel={action('cancel')}
-      onSave={() => Promise.resolve()}
-    />
-  ))
-  .add('default, error while saving', () => (
-    <GoogleDriveSaveAsDialog
-      onShowFilePicker={() =>
-        Promise.resolve({
-          type: 'FILE',
-          id: 'fake-id',
-          name: 'Fake Google Drive file',
-          parentId: 'fake-parent-id',
-        })
-      }
-      onCancel={action('cancel')}
-      onSave={() => Promise.reject(new Error('fake-error'))}
-    />
   ));
 
 storiesOf('OpenConfirmDialog', module)

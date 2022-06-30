b0a4f2eadf2ed790a019bf35cca2c6e261754312
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-06-30 17:44:06 +0200

    Add missing snack message when starting to save as

--- newIDE/app/src/MainFrame/index.js
+++ newIDE/app/src/MainFrame/index.js
@@ -1766,6 +1766,8 @@ const MainFrame = (props: Props) => {
       if (!currentProject) return;
 
       saveUiSettings(state.editorTabs);
+      // TODO: Add condition on when to display the snack message
+      _showSnackMessage(i18n._(t`Saving...`));
 
       const storageProviderOperations = getStorageProviderOperations();
 

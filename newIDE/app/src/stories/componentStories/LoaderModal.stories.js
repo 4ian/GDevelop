4990c2bc49253c08c691d68dcda1d9fb6227dfe2
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-06-30 16:21:44 +0200

    Add possiblity to add progress value and messages to loader modal

---
+++ newIDE/app/src/stories/componentStories/LoaderModal.stories.js
@@ -0,0 +1,37 @@
+// @flow
+
+import * as React from 'react';
+
+import { useTimeout } from '../../Utils/UseTimeout';
+import muiDecorator from '../ThemeDecorator';
+import LoaderModal from '../../UI/LoaderModal';
+
+export default {
+  title: 'UI Building Blocks/LoaderModal',
+  component: LoaderModal,
+  decorators: [muiDecorator],
+};
+
+export const Default = () => <LoaderModal show />;
+
+const STEP_COUNT = 4;
+
+export const WithProgress = () => {
+  const [step, setStep] = React.useState<number>(0);
+  useTimeout(() => {
+    if (step < STEP_COUNT) setStep(step + 1);
+  }, 2000);
+  const messages = [
+    'Hello',
+    "it's gonna be fun!",
+    'Just wait a bit',
+    'Drumrolls',
+  ];
+  return (
+    <LoaderModal
+      show
+      message={messages[Math.min(step, messages.length - 1)]}
+      progress={(step / STEP_COUNT) * 100}
+    />
+  );
+};

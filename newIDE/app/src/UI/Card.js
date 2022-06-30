0a713053779277d6e43cd4ce89bfe228f51313da
Fannie Yan <fannie@gdevelop.io>
2022-06-30 10:17:57 +0200

    Use useResponsiveWindowWidth hook instead of component

--- newIDE/app/src/UI/Card.js
+++ newIDE/app/src/UI/Card.js
@@ -5,7 +5,19 @@ import MUICard from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import GDevelopThemeContext from './Theme/ThemeContext';
 import { Column, Line } from './Grid';
-import { ResponsiveWindowMeasurer } from './Reponsive/ResponsiveWindowMeasurer';
+import { useResponsiveWindowWidth } from './Reponsive/ResponsiveWindowMeasurer';
+
+const styles = {
+  headerContainer: {
+    minWidth: 0,
+    flex: 1,
+  },
+  cardContent: {
+    paddingBottom: 32,
+    paddingTop: 0,
+    minWidth: 0,
+  },
+};
 
 type Props = {|
   children: React.Node,
@@ -22,50 +34,49 @@ const Card = ({
   cardCornerAction,
   style,
 }: Props) => {
+  const windowWidth = useResponsiveWindowWidth();
   const gdevelopTheme = React.useContext(GDevelopThemeContext);
   return (
-    <ResponsiveWindowMeasurer>
-      {width => (
-        <MUICard
+    <MUICard
+      style={{
+        opacity: style && style.opacity ? style.opacity : 1,
+        ...(isHighlighted
+          ? {
+              borderLeftWidth: 4,
+              borderLeftColor: gdevelopTheme.palette.primary,
+            }
+          : {}),
+      }}
+      variant="outlined"
+    >
+      <Column expand noMargin>
+        <Line justifyContent="space-between" expand>
+          <div
+            style={{
+              ...styles.headerContainer,
+              paddingLeft: windowWidth === 'small' ? 8 : 32,
+              paddingRight: cardCornerAction
+                ? windowWidth === 'small'
+                  ? 8
+                  : 32
+                : 0,
+            }}
+          >
+            {header}
+          </div>
+          <Column>{cardCornerAction}</Column>
+        </Line>
+        <CardContent
           style={{
-            opacity: style && style.opacity ? style.opacity : 1,
-            ...(isHighlighted
-              ? {
-                  borderLeftWidth: 4,
-                  borderLeftColor: gdevelopTheme.palette.primary,
-                }
-              : {}),
+            ...styles.cardContent,
+            paddingRight: windowWidth === 'small' ? 8 : 32,
+            paddingLeft: windowWidth === 'small' ? 8 : 32,
           }}
-          variant="outlined"
         >
-          <Column expand noMargin>
-            <Line justifyContent="space-between" expand>
-              <div
-                style={{
-                  paddingLeft: width === 'small' ? 8 : 32,
-                  minWidth: 0,
-                  flex: 1,
-                }}
-              >
-                {header}
-              </div>
-              <Column>{cardCornerAction}</Column>
-            </Line>
-            <CardContent
-              style={{
-                paddingRight: width === 'small' ? 8 : 32,
-                paddingLeft: width === 'small' ? 8 : 32,
-                paddingBottom: 32,
-                paddingTop: 0,
-                minWidth: 0,
-              }}
-            >
-              {children}
-            </CardContent>
-          </Column>
-        </MUICard>
-      )}
-    </ResponsiveWindowMeasurer>
+          {children}
+        </CardContent>
+      </Column>
+    </MUICard>
   );
 };
 

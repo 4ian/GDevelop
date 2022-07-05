d305f4b6f7f255368819fdff574e076a908b4865
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-07-05 15:43:02 +0200

    Improve slightly all commmunity icon colors

--- newIDE/app/src/UI/CustomSvgIcons/Messages.js
+++ newIDE/app/src/UI/CustomSvgIcons/Messages.js
@@ -4,7 +4,7 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 export default React.memo(props => (
   <SvgIcon {...props}>
     <path
-      fill="#643EE2"
+      fill="#6A3BEB"
       d="M17,12V3A1,1 0 0,0 16,2H3A1,1 0 0,0 2,3V17L6,13H16A1,1 0 0,0 17,12M21,6H19V15H6V17A1,1 0 0,0 7,18H18L22,22V7A1,1 0 0,0 21,6Z"
     />
   </SvgIcon>

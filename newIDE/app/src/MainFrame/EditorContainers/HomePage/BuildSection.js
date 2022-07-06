e4aa661a5aa29517a60941175ec924ad84a30684
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-07-06 17:57:16 +0200

    Add cloud projects to the list of recent projects in build section

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/BuildSection.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/BuildSection.js
@@ -1,20 +1,24 @@
 // @flow
 import * as React from 'react';
 import { Trans } from '@lingui/macro';
-import AddIcon from '@material-ui/icons/Add';
 import { I18n } from '@lingui/react';
-import { Line, Column, Spacer } from '../../../UI/Grid';
-import RaisedButton from '../../../UI/RaisedButton';
-import { LineStackLayout, ResponsiveLineStackLayout } from '../../../UI/Layout';
-import PreferencesContext from '../../Preferences/PreferencesContext';
-import { type FileMetadataAndStorageProviderName } from '../../../ProjectsStorage';
-import TextButton from '../../../UI/TextButton';
-import Text from '../../../UI/Text';
-import { useResponsiveWindowWidth } from '../../../UI/Reponsive/ResponsiveWindowMeasurer';
-import SectionContainer, { SectionRow } from './SectionContainer';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+
+import AddIcon from '@material-ui/icons/Add';
+import Text from '../../../UI/Text';
+import TextButton from '../../../UI/TextButton';
+import RaisedButton from '../../../UI/RaisedButton';
+import { Line, Column, Spacer } from '../../../UI/Grid';
+import { useResponsiveWindowWidth } from '../../../UI/Reponsive/ResponsiveWindowMeasurer';
+import { LineStackLayout, ResponsiveLineStackLayout } from '../../../UI/Layout';
+
+import { type FileMetadataAndStorageProviderName } from '../../../ProjectsStorage';
+import PreferencesContext from '../../Preferences/PreferencesContext';
+import AuthenticatedUserContext from '../../../Profile/AuthenticatedUserContext';
+import SectionContainer, { SectionRow } from './SectionContainer';
 
 const styles = {
   listItem: {
@@ -42,11 +46,37 @@ const BuildSection = ({
   onOpenRecentFile,
 }: Props) => {
   const { getRecentProjectFiles } = React.useContext(PreferencesContext);
+  const authenticatedUser = React.useContext(AuthenticatedUserContext);
+
   const windowWidth = useResponsiveWindowWidth();
 
-  const recentProjectFiles = getRecentProjectFiles().filter(
+  let projectFiles: Array<FileMetadataAndStorageProviderName> = getRecentProjectFiles().filter(
     file => file.fileMetadata
   );
+
+  const { cloudProjects } = authenticatedUser;
+
+  if (cloudProjects) {
+    projectFiles = projectFiles.concat(
+      cloudProjects.map(cloudProject => {
+        const file: FileMetadataAndStorageProviderName = {
+          storageProviderName: 'Cloud',
+          fileMetadata: {
+            fileIdentifier: cloudProject.id,
+            lastModifiedDate: Date.parse(cloudProject.lastModifiedAt),
+            name: cloudProject.name,
+          },
+        };
+        return file;
+      })
+    );
+  }
+
+  projectFiles.sort((a, b) => {
+    if (!a.fileMetadata.lastModifiedDate) return 1;
+    if (!b.fileMetadata.lastModifiedDate) return -1;
+    return b.fileMetadata.lastModifiedDate - a.fileMetadata.lastModifiedDate;
+  });
 
   return (
     <I18n>
@@ -84,65 +114,85 @@ const BuildSection = ({
                 )}
               </ResponsiveLineStackLayout>
             </Line>
-            {recentProjectFiles && recentProjectFiles.length > 0 && (
-              <Line>
-                <Column noMargin expand>
-                  {windowWidth !== 'small' && (
-                    <LineStackLayout justifyContent="space-between">
-                      <Column expand noMargin>
-                        <Text color="secondary">
-                          <Trans>File name</Trans>
-                        </Text>
-                      </Column>
-                      <Column expand noMargin>
-                        <Text color="secondary">
-                          <Trans>Last edited</Trans>
-                        </Text>
-                      </Column>
-                    </LineStackLayout>
-                  )}
-                  <List>
-                    {recentProjectFiles.map(file => (
-                      <ListItem
-                        button
-                        key={file.fileMetadata.fileIdentifier}
-                        onClick={() => {
-                          onOpenRecentFile(file);
-                        }}
-                        style={styles.listItem}
-                      >
-                        {windowWidth !== 'small' ? (
-                          <LineStackLayout justifyContent="flex-start" expand>
-                            <Column expand noMargin>
-                              <Text noMargin>
-                                {file.fileMetadata.fileIdentifier}
-                              </Text>
-                            </Column>
-                            <Column expand noMargin>
-                              {file.fileMetadata.lastModifiedDate && (
+            {authenticatedUser.loginState === 'loading' ? (
+              <Column
+                useFullHeight
+                expand
+                noMargin
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <CircularProgress />
+                <Text>
+                  <Trans>Loading user projects...</Trans>
+                </Text>
+              </Column>
+            ) : (
+              projectFiles &&
+              projectFiles.length > 0 && (
+                <Line>
+                  <Column noMargin expand>
+                    {windowWidth !== 'small' && (
+                      <LineStackLayout justifyContent="space-between">
+                        <Column expand noMargin>
+                          <Text color="secondary">
+                            <Trans>File name</Trans>
+                          </Text>
+                        </Column>
+                        <Column expand noMargin>
+                          <Text color="secondary">
+                            <Trans>Last edited</Trans>
+                          </Text>
+                        </Column>
+                      </LineStackLayout>
+                    )}
+                    <List>
+                      {projectFiles.map(file => (
+                        <ListItem
+                          button
+                          key={file.fileMetadata.fileIdentifier}
+                          onClick={() => {
+                            onOpenRecentFile(file);
+                          }}
+                          style={styles.listItem}
+                        >
+                          {windowWidth !== 'small' ? (
+                            <LineStackLayout justifyContent="flex-start" expand>
+                              <Column expand noMargin>
                                 <Text noMargin>
-                                  {i18n.date(
-                                    file.fileMetadata.lastModifiedDate
-                                  )}
+                                  {file.fileMetadata.name
+                                    ? file.fileMetadata.name
+                                    : file.fileMetadata.fileIdentifier}
                                 </Text>
-                              )}
-                            </Column>
-                          </LineStackLayout>
-                        ) : (
-                          <ListItemText
-                            primary={file.fileMetadata.fileIdentifier}
-                            secondary={
-                              file.fileMetadata.lastModifiedDate
-                                ? i18n.date(file.fileMetadata.lastModifiedDate)
-                                : undefined
-                            }
-                          />
-                        )}
-                      </ListItem>
-                    ))}
-                  </List>
-                </Column>
-              </Line>
+                              </Column>
+                              <Column expand noMargin>
+                                {file.fileMetadata.lastModifiedDate && (
+                                  <Text noMargin>
+                                    {i18n.date(
+                                      file.fileMetadata.lastModifiedDate
+                                    )}
+                                  </Text>
+                                )}
+                              </Column>
+                            </LineStackLayout>
+                          ) : (
+                            <ListItemText
+                              primary={file.fileMetadata.fileIdentifier}
+                              secondary={
+                                file.fileMetadata.lastModifiedDate
+                                  ? i18n.date(
+                                      file.fileMetadata.lastModifiedDate
+                                    )
+                                  : undefined
+                              }
+                            />
+                          )}
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Column>
+                </Line>
+              )
             )}
           </SectionRow>
         </SectionContainer>

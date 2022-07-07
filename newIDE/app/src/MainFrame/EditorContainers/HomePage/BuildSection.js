d73a822f54596b391e5088f4d595daa1bb6f1d35
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-07-07 10:30:12 +0200

    Add context menu on recent project lines with only open item so far

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/BuildSection.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/BuildSection.js
@@ -1,7 +1,8 @@
 // @flow
 import * as React from 'react';
-import { Trans } from '@lingui/macro';
+import { Trans, t } from '@lingui/macro';
 import { I18n } from '@lingui/react';
+import { type I18n as I18nType } from '@lingui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -19,6 +20,10 @@ import { type FileMetadataAndStorageProviderName } from '../../../ProjectsStorag
 import PreferencesContext from '../../Preferences/PreferencesContext';
 import AuthenticatedUserContext from '../../../Profile/AuthenticatedUserContext';
 import SectionContainer, { SectionRow } from './SectionContainer';
+import ContextMenu, {
+  type ContextMenuInterface,
+} from '../../../UI/Menu/ContextMenu';
+import { type MenuItemTemplate } from '../../../UI/Menu/Menu.flow';
 
 const styles = {
   listItem: {
@@ -47,6 +52,11 @@ const BuildSection = ({
 }: Props) => {
   const { getRecentProjectFiles } = React.useContext(PreferencesContext);
   const authenticatedUser = React.useContext(AuthenticatedUserContext);
+  const contextMenu = React.useRef<?ContextMenuInterface>(null);
+  const [
+    selectedFile,
+    setSelectedFile,
+  ] = React.useState<?FileMetadataAndStorageProviderName>(null);
 
   const windowWidth = useResponsiveWindowWidth();
 
@@ -78,129 +88,168 @@ const BuildSection = ({
     return b.fileMetadata.lastModifiedDate - a.fileMetadata.lastModifiedDate;
   });
 
+  const openContextMenu = (
+    event: MouseEvent,
+    file: FileMetadataAndStorageProviderName
+  ) => {
+    setSelectedFile(file);
+    if (contextMenu.current) {
+      contextMenu.current.open(event.clientX, event.clientY);
+    }
+  };
+
+  const buildContextMenu = (
+    i18n: I18nType,
+    file: ?FileMetadataAndStorageProviderName
+  ): Array<MenuItemTemplate> => {
+    if (!file) return [];
+    return [
+      { label: i18n._(t`Open`), click: () => onOpenRecentFile(file) },
+      // { label: i18n._(t`Duplicate`), click: () => console.log('duplicate') },
+      { type: 'separator' },
+      // { label: i18n._(t`Manage game`), click: () => console.log('Manage game') },
+      { label: i18n._(t`Delete`), click: () => console.log('Delete') },
+    ];
+  };
+
   return (
     <I18n>
       {({ i18n }) => (
-        <SectionContainer title={<Trans>My projects</Trans>}>
-          <SectionRow>
-            <Line noMargin>
-              <ResponsiveLineStackLayout
-                noColumnMargin
-                justifyContent="start"
-                alignItems="center"
-              >
-                <RaisedButton
-                  primary
-                  label={<Trans>Create a project</Trans>}
-                  onClick={onCreateProject}
-                  icon={<AddIcon />}
-                  id="home-create-project-button"
-                />
-                {canOpen && (
-                  <>
-                    {windowWidth !== 'small' && (
-                      <>
-                        <Text>
-                          <Trans>or</Trans>
-                        </Text>
-                        <Spacer />
-                      </>
-                    )}
-                    <TextButton
-                      primary
-                      label={<Trans>Open an existing project</Trans>}
-                      onClick={onOpen}
-                    />
-                  </>
-                )}
-              </ResponsiveLineStackLayout>
-            </Line>
-            {authenticatedUser.loginState === 'loading' ? (
-              <Column
-                useFullHeight
-                expand
-                noMargin
-                justifyContent="flex-start"
-                alignItems="center"
-              >
-                <CircularProgress />
-                <Text>
-                  <Trans>Loading user projects...</Trans>
-                </Text>
-              </Column>
-            ) : (
-              projectFiles &&
-              projectFiles.length > 0 && (
-                <Line>
-                  <Column noMargin expand>
-                    {windowWidth !== 'small' && (
-                      <LineStackLayout justifyContent="space-between">
-                        <Column expand>
-                          <Text color="secondary">
-                            <Trans>File name</Trans>
+        <>
+          <SectionContainer title={<Trans>My projects</Trans>}>
+            <SectionRow>
+              <Line noMargin>
+                <ResponsiveLineStackLayout
+                  noColumnMargin
+                  justifyContent="start"
+                  alignItems="center"
+                >
+                  <RaisedButton
+                    primary
+                    label={<Trans>Create a project</Trans>}
+                    onClick={onCreateProject}
+                    icon={<AddIcon />}
+                    id="home-create-project-button"
+                  />
+                  {canOpen && (
+                    <>
+                      {windowWidth !== 'small' && (
+                        <>
+                          <Text>
+                            <Trans>or</Trans>
                           </Text>
-                        </Column>
-                        <Column expand>
-                          <Text color="secondary">
-                            <Trans>Last edited</Trans>
-                          </Text>
-                        </Column>
-                      </LineStackLayout>
-                    )}
-                    <List>
-                      {projectFiles.map(file => (
-                        <ListItem
-                          button
-                          key={file.fileMetadata.fileIdentifier}
-                          onClick={() => {
-                            onOpenRecentFile(file);
-                          }}
-                          style={styles.listItem}
-                        >
-                          {windowWidth !== 'small' ? (
-                            <LineStackLayout justifyContent="flex-start" expand>
-                              <Column expand>
-                                <Text noMargin>
-                                  {file.fileMetadata.name ||
-                                    file.fileMetadata.fileIdentifier}
-                                </Text>
-                              </Column>
-                              <Column expand>
-                                {file.fileMetadata.lastModifiedDate && (
+                          <Spacer />
+                        </>
+                      )}
+                      <TextButton
+                        primary
+                        label={<Trans>Open an existing project</Trans>}
+                        onClick={onOpen}
+                      />
+                    </>
+                  )}
+                </ResponsiveLineStackLayout>
+              </Line>
+              {authenticatedUser.loginState === 'loading' ? (
+                <Column
+                  useFullHeight
+                  expand
+                  noMargin
+                  justifyContent="flex-start"
+                  alignItems="center"
+                >
+                  <CircularProgress />
+                  <Text>
+                    <Trans>Loading projects...</Trans>
+                  </Text>
+                </Column>
+              ) : (
+                projectFiles &&
+                projectFiles.length > 0 && (
+                  <Line>
+                    <Column noMargin expand>
+                      {windowWidth !== 'small' && (
+                        <LineStackLayout justifyContent="space-between">
+                          <Column expand>
+                            <Text color="secondary">
+                              <Trans>File name</Trans>
+                            </Text>
+                          </Column>
+                          <Column expand>
+                            <Text color="secondary">
+                              <Trans>Last edited</Trans>
+                            </Text>
+                          </Column>
+                        </LineStackLayout>
+                      )}
+                      <List>
+                        {projectFiles.map(file => (
+                          <ListItem
+                            button
+                            key={file.fileMetadata.fileIdentifier}
+                            onClick={() => {
+                              onOpenRecentFile(file);
+                            }}
+                            style={styles.listItem}
+                            onContextMenu={event =>
+                              openContextMenu(event, file)
+                            }
+                          >
+                            {windowWidth !== 'small' ? (
+                              <LineStackLayout
+                                justifyContent="flex-start"
+                                expand
+                              >
+                                <Column expand>
                                   <Text noMargin>
-                                    {file.fileMetadata.name
-                                      ? file.fileMetadata.name
-                                      : file.fileMetadata.fileIdentifier}
+                                    {file.fileMetadata.name ||
+                                      file.fileMetadata.fileIdentifier}
                                   </Text>
-                                )}
-                              </Column>
-                            </LineStackLayout>
-                          ) : (
-                            <Column>
-                              <ListItemText
-                                primary={
-                                  file.fileMetadata.name ||
-                                  file.fileMetadata.fileIdentifier
-                                }
-                                secondary={
-                                  file.fileMetadata.lastModifiedDate
-                                    ? i18n.date(
+                                </Column>
+                                <Column expand>
+                                  {file.fileMetadata.lastModifiedDate && (
+                                    <Text noMargin>
+                                      {i18n.date(
                                         file.fileMetadata.lastModifiedDate
-                                      )
-                                    : undefined
-                                }
-                              />
-                            </Column>
-                          )}
-                        </ListItem>
-                      ))}
-                    </List>
-                  </Column>
-                </Line>
-              )
-            )}
-          </SectionRow>
-        </SectionContainer>
+                                      )}
+                                    </Text>
+                                  )}
+                                </Column>
+                              </LineStackLayout>
+                            ) : (
+                              <Column>
+                                <ListItemText
+                                  primary={
+                                    file.fileMetadata.name ||
+                                    file.fileMetadata.fileIdentifier
+                                  }
+                                  secondary={
+                                    file.fileMetadata.lastModifiedDate
+                                      ? i18n.date(
+                                          file.fileMetadata.lastModifiedDate
+                                        )
+                                      : undefined
+                                  }
+                                  onContextMenu={event =>
+                                    openContextMenu(event, file)
+                                  }
+                                />
+                              </Column>
+                            )}
+                          </ListItem>
+                        ))}
+                      </List>
+                    </Column>
+                  </Line>
+                )
+              )}
+            </SectionRow>
+          </SectionContainer>
+          <ContextMenu
+            ref={contextMenu}
+            buildMenuTemplate={_i18n => buildContextMenu(_i18n, selectedFile)}
+          />
+        </>
       )}
     </I18n>
   );

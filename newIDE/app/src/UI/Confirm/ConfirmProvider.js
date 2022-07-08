de1d492eadf084de0d126ed77928db3776630cd7
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-07-08 11:16:42 +0200

    Gather confirm delete dialog in confirm context provider

--- newIDE/app/src/UI/Confirm/ConfirmProvider.js
+++ newIDE/app/src/UI/Confirm/ConfirmProvider.js
@@ -2,38 +2,91 @@
 import * as React from 'react';
 import ConfirmContext from './ConfirmContext';
 import ConfirmDialog from './ConfirmDialog';
+import ConfirmDeleteDialog from './ConfirmDeleteDialog';
+import {
+  type ShowConfirmDeleteDialogOptionsWithCallback,
+  type ShowConfirmDialogOptionsWithCallback,
+} from './ConfirmContext';
 
 type Props = {| children: React.Node |};
 
 function ConfirmProvider({ children }: Props) {
-  const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
-  const [dialogConfig, setDialogConfig] = React.useState({});
+  const [confirmDialogOpen, setConfirmDialogOpen] = React.useState<boolean>(
+    false
+  );
+  const [
+    confirmDeleteDialogOpen,
+    setConfirmDeleteDialogOpen,
+  ] = React.useState<boolean>(false);
+  const [
+    confirmDialogConfig,
+    setConfirmDialogConfig,
+  ] = React.useState<?ShowConfirmDialogOptionsWithCallback>(null);
+  const [
+    confirmDeleteDialogConfig,
+    setConfirmDeleteDialogConfig,
+  ] = React.useState<?ShowConfirmDeleteDialogOptionsWithCallback>(null);
 
-  const openDialog = ({ title, message, actionCallback }) => {
-    setDialogOpen(true);
-    setDialogConfig({ title, message, actionCallback });
+  const openConfirmDialog = (options: ShowConfirmDialogOptionsWithCallback) => {
+    setConfirmDialogOpen(true);
+    setConfirmDialogConfig(options);
   };
 
-  const onConfirm = () => {
-    setDialogOpen(false);
-    dialogConfig.actionCallback(true);
+  const openConfirmDeleteDialog = (
+    options: ShowConfirmDeleteDialogOptionsWithCallback
+  ) => {
+    setConfirmDeleteDialogOpen(true);
+    setConfirmDeleteDialogConfig(options);
   };
 
-  const onDismiss = () => {
-    setDialogOpen(false);
-    dialogConfig.actionCallback(false);
+  const onConfirm = ({ deleteDialog }: { deleteDialog: boolean }) => {
+    if (deleteDialog) {
+      setConfirmDeleteDialogOpen(false);
+      confirmDeleteDialogConfig && confirmDeleteDialogConfig.callback(true);
+    } else {
+      setConfirmDialogOpen(false);
+      confirmDialogConfig && confirmDialogConfig.callback(true);
+    }
+  };
+
+  const onDismiss = ({ deleteDialog }: { deleteDialog: boolean }) => {
+    if (deleteDialog) {
+      setConfirmDeleteDialogOpen(false);
+      confirmDeleteDialogConfig && confirmDeleteDialogConfig.callback(false);
+    } else {
+      setConfirmDialogOpen(false);
+      confirmDialogConfig && confirmDialogConfig.callback(false);
+    }
   };
 
   return (
-    <ConfirmContext.Provider value={{ showConfirmDialog: openDialog }}>
+    <ConfirmContext.Provider
+      value={{
+        showConfirmDialog: openConfirmDialog,
+        showConfirmDeleteDialog: openConfirmDeleteDialog,
+      }}
+    >
       {children}
-      <ConfirmDialog
-        open={dialogOpen}
-        onConfirm={onConfirm}
-        onDismiss={onDismiss}
-        title={dialogConfig.title}
-        message={dialogConfig.message}
-      />
+      {confirmDialogConfig && (
+        <ConfirmDialog
+          open={confirmDialogOpen}
+          onConfirm={() => onConfirm({ deleteDialog: false })}
+          onDismiss={() => onDismiss({ deleteDialog: false })}
+          title={confirmDialogConfig.title}
+          message={confirmDialogConfig.message}
+        />
+      )}
+      {confirmDeleteDialogConfig && (
+        <ConfirmDeleteDialog
+          open={confirmDeleteDialogOpen}
+          onConfirm={() => onConfirm({ deleteDialog: true })}
+          onDismiss={() => onDismiss({ deleteDialog: true })}
+          title={confirmDeleteDialogConfig.title}
+          message={confirmDeleteDialogConfig.message}
+          fieldMessage={confirmDeleteDialogConfig.fieldMessage}
+          confirmText={confirmDeleteDialogConfig.confirmText}
+        />
+      )}
     </ConfirmContext.Provider>
   );
 }

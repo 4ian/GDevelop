30898058a4f5c7b07d7e7c3a745b8ed7e7c46235
D8H <Davy.Helard@gmail.com>
2022-07-25 18:53:30 +0200

    Fix layer names in drop-down lists were translated even though they are defined by users (#4130)

--- newIDE/app/src/LayersList/EnumerateLayers.js
+++ newIDE/app/src/LayersList/EnumerateLayers.js
@@ -1,10 +1,13 @@
 import { mapFor } from '../Utils/MapFor';
 
-const enumerateLayers = layersContainer =>
+const enumerateLayers = (
+  layersContainer: any
+): {| value: string, label: string, labelIsUserDefined: boolean |} =>
   mapFor(0, layersContainer.getLayersCount(), i => {
     return {
       value: layersContainer.getLayerAt(i).getName(),
       label: layersContainer.getLayerAt(i).getName() || 'Base layer',
+      labelIsUserDefined: !!layersContainer.getLayerAt(i).getName(),
     };
   });
 

cdda15cc68619443804730738fc86d2d3317b562
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-07-25 10:39:24 +0200

    Fix package URLs crashing the app on web

--- newIDE/app/src/Utils/BlobDownloadUrlHolder.js
+++ newIDE/app/src/Utils/BlobDownloadUrlHolder.js
@@ -8,27 +8,31 @@ type Props = {
 
 export const BlobDownloadUrlHolder = ({ blob, children }: Props) => {
   const [blobDownloadUrl, setBlobDownloadUrl] = React.useState('');
+  const [stateBlob, setStateBlob] = React.useState(null);
   React.useEffect(
     () => {
-      // Release the existing blob URL, if any.
-      if (blobDownloadUrl) {
-        URL.revokeObjectURL(blobDownloadUrl);
-      }
-
-      if (blob) {
+      // This effect function does not look at the blobDownloadUrl, to avoid infinite loops.
+      // It is only in charge of updating the Url when the blob changes.
+      if (blob && blob !== stateBlob) {
         setBlobDownloadUrl(URL.createObjectURL(blob));
-      } else {
-        setBlobDownloadUrl('');
+        setStateBlob(blob);
       }
+    },
+    [blob, stateBlob]
+  );
 
+  React.useEffect(
+    () => {
+      // This cleanup is called both when the component is unmounted or when an update happens.
+      // This allows releasing the URL when a new one is generated.
+      // See https://reactjs.org/docs/hooks-effect.html#explanation-why-effects-run-on-each-update
       return () => {
-        // Release the blob URL if component is unmounted.
         if (blobDownloadUrl) {
           URL.revokeObjectURL(blobDownloadUrl);
         }
       };
     },
-    [blob, blobDownloadUrl]
+    [blobDownloadUrl]
   );
 
   return children(blobDownloadUrl);

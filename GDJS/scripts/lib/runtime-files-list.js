f62236788d6d7b039533dbdfa9a0251b1f040e1f
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2022-07-26 14:25:08 +0200

    Add various improvements to the P2P extension (#4145)
    - Updated PeerJS.
    - Fixes for Safari.
    - Improved typings.
    - New default TURN server.
    - Add a JavaScript API to get access to the raw connection.
      This allows to send custom messages from JavaScript. This allows to use a binary protocol from JavaScript.
    - Use Map instead of JavaScript objects in places where it makes sense (more performant and less error prone).
    - Avoid making a redundant get for every connection on the map containing the connections.
    
    Only show in developer changelog

--- GDJS/scripts/lib/runtime-files-list.js
+++ GDJS/scripts/lib/runtime-files-list.js
@@ -9,7 +9,7 @@ const extensionsRuntimePath = path.join(gdevelopRootPath, 'Extensions');
 const gdjsRuntimePath = path.join(gdjsRootPath, 'Runtime');
 
 // The extensions to be included in the bundled Runtime (will be built with esbuild or copied).
-const allowedExtensions = ['.js', '.ts', '.html', '.json', '.xml'];
+const allowedExtensions = ['.js', '.ts', '.html', '.json', '.xml', '.map'];
 
 // These extensions will be built with esbuild (the other will be copied).
 const transformIncludedExtensions = ['.js', '.ts'];
@@ -78,9 +78,7 @@ const isTestDirectory = (fileOrDirectoryPath, stats) => {
  * @param {string} filePath
  */
 const isJsExtensionDeclaration = (filePath) => {
-  return (
-    path.basename(filePath) === 'JsExtension.js'
-  );
+  return path.basename(filePath) === 'JsExtension.js';
 };
 
 /** @typedef {{inPath: string; outPath: string;}} InOutPath */

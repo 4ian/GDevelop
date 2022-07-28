e90fc1036107d24089661411df042d9e94db3a1f
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-07-28 15:11:46 +0200

    change open error message for cloud provider

--- newIDE/app/src/ProjectsStorage/CloudStorageProvider/index.js
+++ newIDE/app/src/ProjectsStorage/CloudStorageProvider/index.js
@@ -39,7 +39,7 @@ export default ({
     ),
     onChangeProjectProperty: generateOnChangeProjectProperty(authenticatedUser),
     getOpenErrorMessage: (error: Error): MessageDescriptor => {
-      return t`Check that the file exists, that this file is a proper game created with GDevelop and that you have the authorization to open it.`;
+      return t`An error occurred when opening the project. Check that your internet connection is working and that your browser allows the use of cookies.`;
     },
   }),
 }: StorageProvider);

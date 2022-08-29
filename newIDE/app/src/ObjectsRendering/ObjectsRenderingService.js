f472133849faac2b87d2307b39f9ff4fedfa502e
Davy Hélard <davy.helard@gmail.com>
2022-08-29 12:18:59 +0200

    Custom object renderer.

--- newIDE/app/src/ObjectsRendering/ObjectsRenderingService.js
+++ newIDE/app/src/ObjectsRendering/ObjectsRenderingService.js
@@ -7,6 +7,7 @@ import RenderedTextInstance from './Renderers/RenderedTextInstance';
 import RenderedShapePainterInstance from './Renderers/RenderedShapePainterInstance';
 import RenderedTextEntryInstance from './Renderers/RenderedTextEntryInstance';
 import RenderedParticleEmitterInstance from './Renderers/RenderedParticleEmitterInstance';
+import RenderedCustomObjectInstance from './Renderers/RenderedCustomObjectInstance';
 import PixiResourcesLoader from './PixiResourcesLoader';
 import ResourcesLoader from '../ResourcesLoader';
 import RenderedInstance from './Renderers/RenderedInstance';
@@ -77,6 +78,17 @@ const ObjectsRenderingService = {
         pixiRenderer
       );
     else {
+      if (project.hasEventsBasedObject(objectType)) {
+        return new RenderedCustomObjectInstance(
+          project,
+          layout,
+          instance,
+          associatedObjectConfiguration,
+          pixiContainer,
+          PixiResourcesLoader
+        );
+      }
+
       console.warn(
         `Object with type ${objectType} has no instance renderer registered. Please use registerInstanceRenderer to register your renderer.`
       );

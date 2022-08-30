bb88f3ae4da7be0c8e8b9b4805ffd172b1b3ce8e
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2022-08-30 13:52:10 +0200

    Add button to release notes in about dialog
    Do not show in changelog

--- newIDE/app/src/MainFrame/AboutDialog.js
+++ newIDE/app/src/MainFrame/AboutDialog.js
@@ -28,10 +28,13 @@ import { ColumnStackLayout } from '../UI/Layout';
 import optionalRequire from '../Utils/OptionalRequire';
 const electron = optionalRequire('electron');
 
-type Props = {
+type Props = {|
+  open?: boolean,
   onClose: () => void,
   updateStatus: ElectronUpdateStatus,
-};
+|};
+
+type TabName = 'about' | 'changelog' | 'contributors';
 
 const styles = {
   logo: {
@@ -250,13 +253,16 @@ export default function AboutDialog({ onClose, updateStatus }: Props) {
     Window.openExternalURL('https://gdevelop.io/page/contribute/');
   }, []);
 
+  const openReleaseNote = () =>
+    Window.openExternalURL('https://github.com/4ian/GDevelop/releases');
+
   const openLink = React.useCallback((link: string) => {
     if (!link) return;
 
     Window.openExternalURL(link);
   }, []);
 
-  const [currentTab, setCurrentTab] = React.useState('about');
+  const [currentTab, setCurrentTab] = React.useState<TabName>('about');
 
   const { checkUpdates } = React.useContext(PreferencesContext);
 
@@ -287,6 +293,18 @@ export default function AboutDialog({ onClose, updateStatus }: Props) {
           onClick={onClose}
         />,
       ]}
+      secondaryActions={
+        currentTab === 'changelog'
+          ? [
+              <FlatButton
+                key="see-all"
+                label={<Trans>See all release notes</Trans>}
+                primary={false}
+                onClick={openReleaseNote}
+              />,
+            ]
+          : undefined
+      }
       onRequestClose={onClose}
       open
       maxWidth="sm"

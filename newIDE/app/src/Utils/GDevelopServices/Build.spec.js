079547a4d38c863c3bf52898514de4984f0e6b00
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-09-05 11:31:33 +0200

    Add more complex special characters regex

--- newIDE/app/src/Utils/GDevelopServices/Build.spec.js
+++ newIDE/app/src/Utils/GDevelopServices/Build.spec.js
@@ -10,6 +10,14 @@ describe('Build service', () => {
         })
       ).toEqual('Super game-1_0_0');
     });
+    test('it formats correctly a name with parenthesis', () => {
+      expect(
+        getBuildExtensionlessFilename({
+          gameName: 'Super game (bis)',
+          gameVersion: '1.0.0',
+        })
+      ).toEqual('Super game (bis)-1_0_0');
+    });
     test('it formats correctly a name with / and \\', () => {
       expect(
         getBuildExtensionlessFilename({
@@ -17,6 +25,14 @@ describe('Build service', () => {
           gameVersion: '1.0.0',
         })
       ).toEqual('Super_game_yo-1_0_0');
+    });
+    test('it formats correctly classic special characters', () => {
+      expect(
+        getBuildExtensionlessFilename({
+          gameName: 'Super #"game\'[ahah]@12.2$*',
+          gameVersion: '1.0.0',
+        })
+      ).toEqual('Super __game__ahah__12_2_*-1_0_0');
     });
   });
 });

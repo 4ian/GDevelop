cd9be3fa07721d68eef3dbe4ba0906a3fa3bca61
Davy Hélard <davy.helard@gmail.com>
2022-09-08 10:46:47 +0200

    Move up updateObjectsForces.

--- GDJS/Runtime/RuntimeInstancesContainer.ts
+++ GDJS/Runtime/RuntimeInstancesContainer.ts
@@ -587,5 +587,26 @@ namespace gdjs {
 
       return 0;
     }
+    
+    /**
+     * Update the objects positions according to their forces
+     */
+     updateObjectsForces(): void {
+      for (const name in this._instances.items) {
+        if (this._instances.items.hasOwnProperty(name)) {
+          const list = this._instances.items[name];
+          for (let j = 0, listLen = list.length; j < listLen; ++j) {
+            const obj = list[j];
+            if (!obj.hasNoForces()) {
+              const averageForce = obj.getAverageForce();
+              const elapsedTimeInSeconds = obj.getElapsedTime(this) / 1000;
+              obj.setX(obj.getX() + averageForce.getX() * elapsedTimeInSeconds);
+              obj.setY(obj.getY() + averageForce.getY() * elapsedTimeInSeconds);
+              obj.updateForces(elapsedTimeInSeconds);
+            }
+          }
+        }
+      }
+    }
   }
 }

9532a8f6de02d89880fd0fc20d0fb95b9d0a505f
D8H <Davy.Helard@gmail.com>
2022-09-15 15:43:28 +0200

    Fix extension upgrades that was leaving duplicated behaviors. (#4301)
    - Don't show in changelogs (the regression is between the 140 and the 141)

--- Core/GDCore/Project/EventsFunctionsExtension.cpp
+++ Core/GDCore/Project/EventsFunctionsExtension.cpp
@@ -168,24 +168,10 @@ void EventsFunctionsExtension::UnserializeExtensionImplementationFrom(
     gd::Project& project,
     const SerializerElement& element) {
   UnserializeEventsFunctionsFrom(project, element.GetChild("eventsFunctions"));
-  auto &behaviorsElement = element.GetChild("eventsBasedBehaviors");
-  behaviorsElement.ConsiderAsArrayOf("eventsBasedBehavior");
-  for (std::size_t i = 0; i < behaviorsElement.GetChildrenCount(); ++i) {
-    const gd::String &behaviorName =
-        behaviorsElement.GetChild(i).GetStringAttribute("name");
-    if (eventsBasedBehaviors.Has(behaviorName)) {
-      eventsBasedBehaviors.Get(behaviorName).UnserializeFrom(project, behaviorsElement.GetChild(i));
-    }
-  }
-  auto &objectsElement = element.GetChild("eventsBasedObjects");
-  objectsElement.ConsiderAsArrayOf("eventsBasedObject");
-  for (std::size_t i = 0; i < objectsElement.GetChildrenCount(); ++i) {
-    const gd::String &objectName =
-        objectsElement.GetChild(i).GetStringAttribute("name");
-    if (eventsBasedObjects.Has(objectName)) {
-      eventsBasedObjects.Get(objectName).UnserializeFrom(project, objectsElement.GetChild(i));
-    }
-  }
+  eventsBasedBehaviors.UnserializeElementsFrom(
+      "eventsBasedBehavior", project, element.GetChild("eventsBasedBehaviors"));
+  eventsBasedObjects.UnserializeElementsFrom(
+      "eventsBasedObject", project, element.GetChild("eventsBasedObjects"));
 }
 
 bool EventsFunctionsExtension::IsExtensionLifecycleEventsFunction(

11265883938e9f0cb179d9103bba47ede8c106be
Davy Hélard <davy.helard@gmail.com>
2022-09-21 18:55:51 +0200

    Fix tiles rotation that was no longer applied in tile maps.

--- Extensions/TileMap/helper/dts/render/TileTextureCache.d.ts
+++ Extensions/TileMap/helper/dts/render/TileTextureCache.d.ts
@@ -20,22 +20,28 @@ export declare class TileTextureCache {
     texture: PIXI.Texture
   ): void;
   /**
+   * Return the texture to use for the tile with the specified id.
+   *
+   * @param tileId The tile identifier
+   * @returns The texture for the given tile identifier.
+   */
+  findTileTexture(tileId: integer): PIXI.Texture | undefined;
+  /**
    * Return the texture to use for the tile with the specified uid, which can contains
    * information about rotation in bits 32, 31 and 30
    * (see https://doc.mapeditor.org/en/stable/reference/tmx-map-format/).
    *
-   * @param tileId The tile identifier
    * @param flippedHorizontally true if the tile is flipped horizontally.
    * @param flippedVertically true if the tile is flipped vertically.
    * @param flippedDiagonally true if the tile is flipped diagonally.
-   * @returns The texture for the given tile identifier and orientation.
+   * @returns the rotation "D8" number used by Pixi.
+   * @see https://pixijs.io/examples/#/textures/texture-rotate.js
    */
-  findTileTexture(
-    tileId: integer,
+  getPixiRotate(
     flippedHorizontally: boolean,
     flippedVertically: boolean,
     flippedDiagonally: boolean
-  ): PIXI.Texture | undefined;
+  ): number;
   /**
    * @return the Tiled tile global uniq identifier.
    */

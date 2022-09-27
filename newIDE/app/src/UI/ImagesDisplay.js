b9dfff84adc4bc954105c120ec4b914df6fff63a
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-09-27 10:46:54 +0200

    Split ImagesDisplay in 2 different component depending on mobile or desktop

--- newIDE/app/src/UI/ImagesDisplay.js
+++ newIDE/app/src/UI/ImagesDisplay.js
@@ -25,8 +25,16 @@ const styles = {
     aspectRatio: '16 / 9',
     display: 'block',
   },
+  mobileImageCarouselItem: {
+    height: 200,
+    aspectRatio: '16 / 9',
+  },
   grid: {
     overflowX: 'scroll',
+    overflowY: 'hidden',
+  },
+  mobileGrid: {
+    overflowX: 'hidden',
     overflowY: 'hidden',
   },
 };
@@ -54,6 +62,93 @@ const ImagesDisplay = ({ imagesUrls }: Props) => {
 
   const classesForGridContainer = useStylesForGridContainer();
 
+  const mobileGrid = React.useRef<?HTMLDivElement>(null);
+  const isTouching = React.useRef<boolean>(false);
+
+  const [startDragX, setStartDragX] = React.useState(null);
+  const [positionX, setPositionX] = React.useState(0);
+  const [
+    mobileGridScrollXBeforeTouch,
+    setMobileGridScrollXBeforeTouch,
+  ] = React.useState<number>(0);
+
+  const handleTouchStart = (event: TouchEvent) => {
+    isTouching.current = true;
+    setStartDragX(event.changedTouches[0].clientX);
+  };
+
+  const handleTouchEnd = (event: TouchEvent) => {
+    isTouching.current = false;
+
+    if (!startDragX) return;
+    const endDragX = event.changedTouches[0].clientX;
+    const newIndex =
+      startDragX - endDragX > 50
+        ? Math.min(selectedImageIndex + 1, imagesUrls.length - 1)
+        : startDragX - endDragX < -50
+        ? Math.max(selectedImageIndex - 1, 0)
+        : selectedImageIndex;
+
+    setSelectedImageIndex(newIndex);
+    const { current } = mobileGrid;
+    if (!current) return;
+    const step = current.scrollWidth / imagesUrls.length;
+    current.scrollTo({
+      left: newIndex * step,
+      behavior: 'smooth',
+    });
+    setMobileGridScrollXBeforeTouch(newIndex * step);
+  };
+
+  const handleTouchMove = (event: TouchEvent) => {
+    if (!startDragX) return;
+    const positionX = event.changedTouches[0].clientX;
+    setPositionX(positionX - startDragX - mobileGridScrollXBeforeTouch);
+  };
+
+  React.useEffect(
+    () => {
+      if (!mobileGrid.current || !isTouching.current) return;
+      mobileGrid.current.scrollTo(-positionX, 0);
+    },
+    [positionX]
+  );
+
+  if (windowWidth === 'small') {
+    return (
+      <Column noMargin>
+        <Grid
+          classes={classesForGridContainer}
+          container
+          spacing={1}
+          wrap="nowrap"
+          style={styles.mobileGrid}
+          onTouchMove={handleTouchMove}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
+          ref={mobileGrid}
+        >
+          {imagesUrls.map((url, index) => (
+            <Grid item key={url}>
+              <CardMedia>
+                <CorsAwareImage
+                  src={url}
+                  style={styles.mobileImageCarouselItem}
+                  alt={``}
+                />
+              </CardMedia>
+            </Grid>
+          ))}
+        </Grid>
+        <Line justifyContent="center">
+          <Text noMargin size="body2">
+            {selectedImageIndex + 1}/{imagesUrls.length}
+          </Text>
+        </Line>
+      </Column>
+    );
+  }
+
   return (
     <Column noMargin>
       <CorsAwareImage
@@ -61,52 +156,46 @@ const ImagesDisplay = ({ imagesUrls }: Props) => {
         src={imagesUrls[selectedImageIndex]}
         alt={``}
       />
-      <Line justifyContent="center">
-        {windowWidth === 'small' ? (
-          <Text noMargin size="body2">
-            {selectedImageIndex + 1}/{imagesUrls.length}
-          </Text>
-        ) : (
-          <Grid
-            classes={classesForGridContainer}
-            container
-            spacing={1}
-            wrap="nowrap"
-            style={styles.grid}
-          >
-            {imagesUrls.map((url, index) => (
-              <Grid
-                item
-                key={url}
-                tabIndex={0}
-                onKeyPress={(
-                  event: SyntheticKeyboardEvent<HTMLLIElement>
-                ): void => {
-                  if (shouldValidate(event)) {
-                    setSelectedImageIndex(index);
-                  }
+      <Line>
+        <Grid
+          classes={classesForGridContainer}
+          container
+          spacing={1}
+          wrap="nowrap"
+          style={styles.grid}
+        >
+          {imagesUrls.map((url, index) => (
+            <Grid
+              item
+              key={url}
+              tabIndex={0}
+              onKeyPress={(
+                event: SyntheticKeyboardEvent<HTMLLIElement>
+              ): void => {
+                if (shouldValidate(event)) {
+                  setSelectedImageIndex(index);
+                }
+              }}
+            >
+              <CardMedia
+                onClick={() => setSelectedImageIndex(index)}
+                style={{
+                  ...styles.carouselItem,
+                  outline:
+                    index === selectedImageIndex
+                      ? 'solid 1px white'
+                      : undefined,
                 }}
               >
-                <CardMedia
-                  onClick={() => setSelectedImageIndex(index)}
-                  style={{
-                    ...styles.carouselItem,
-                    outline:
-                      index === selectedImageIndex
-                        ? 'solid 1px white'
-                        : undefined,
-                  }}
-                >
-                  <CorsAwareImage
-                    src={url}
-                    style={styles.imageCarouselItem}
-                    alt={``}
-                  />
-                </CardMedia>
-              </Grid>
-            ))}
-          </Grid>
-        )}
+                <CorsAwareImage
+                  src={url}
+                  style={styles.imageCarouselItem}
+                  alt={``}
+                />
+              </CardMedia>
+            </Grid>
+          ))}
+        </Grid>
       </Line>
     </Column>
   );

b10b74b9bb18455c63ffd91438c69014e3d07309
Davy Hélard <davy.helard@gmail.com>
2022-10-03 17:01:44 +0200

    Remove some call depth.

--- GDJS/Runtime/layer.ts
+++ GDJS/Runtime/layer.ts
@@ -415,8 +415,8 @@ namespace gdjs {
      * demand for efficiency reasons.
      */
     private _forceDimensionUpdate(): void {
-      this.getWidth();
-      this.getHeight();
+      // This will update dimensions.
+      this._runtimeScene.getViewportWidth();
     }
 
     /**

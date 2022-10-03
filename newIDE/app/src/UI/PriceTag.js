cd64d9385fbacb27c0e4359d1ec8b459be36fa25
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-03 13:23:36 +0200

    Improve color handling for different use of component

--- newIDE/app/src/UI/PriceTag.js
+++ newIDE/app/src/UI/PriceTag.js
@@ -4,25 +4,34 @@ import * as React from 'react';
 import { I18n } from '@lingui/react';
 
 import Text from './Text';
+import { makeStyles } from '@material-ui/core';
 
-type Props = {| value: number |};
+type Props = {| value: number, transparentBackground: boolean |};
 
-const styles = {
-  container: {
-    backdropFilter: 'brightness(80%)',
-    borderRadius: 4,
-    padding: '2px 4px',
-  },
-};
+const useStyles = makeStyles(theme => {
+  return {
+    container: {
+      borderRadius: 4,
+      padding: '2px 4px',
+      backdropFilter: props =>
+        props.transparentBackground ? 'brightness(40%)' : undefined,
+      backgroundColor: props =>
+        props.transparentBackground
+          ? undefined
+          : theme.palette.background.alternate,
+    },
+  };
+});
 
-function PriceTag(props: Props) {
+function PriceTag({ value, transparentBackground }: Props) {
+  const classes = useStyles({ transparentBackground });
   return (
     <I18n>
       {({ i18n }) => (
-        <div style={styles.container}>
+        <div className={classes.container}>
           <Text noMargin size="sub-title">
             {i18n
-              .number(props.value / 100, {
+              .number(value / 100, {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
               })

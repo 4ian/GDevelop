765ce31666188736e89b9141b2307995d3899751
Paulo <paulo.castro48@gmail.com>
2022-10-07 11:29:02 +0000

    add type declaration to fix build errors, untested in app

--- Extensions/GooglePlayGameServices/tools.ts
+++ Extensions/GooglePlayGameServices/tools.ts
@@ -6,18 +6,25 @@ namespace gdjs {
   export namespace googlePlayGameServices {
 
     export namespace android {
+
+      interface extensionDataInterface {
+        AndroidReady: boolean;
+        runtimeSceneContainer: gdjs.RuntimeScene;
+      }
+
       // You can store global information, data, etc... directly in the namespace of your extension:
       let extensionData = {
           AndroidReady: false
-      };
+      } as extensionDataInterface;
 
       /**
        * In **rare cases** you may want to run code at the start of the scene. You can define a callback
        * that will be called at this moment.
        */
-      gdjs.registerRuntimeSceneLoadedCallback(function () {
+      gdjs.registerRuntimeSceneLoadedCallback(function (runtimeScene: gdjs.RuntimeScene) {
         document.addEventListener('deviceready', ()=>{
             extensionData.AndroidReady = true;
+            extensionData.runtimeSceneContainer = runtimeScene;
         }, false);
       });
 
@@ -27,19 +34,19 @@ namespace gdjs {
 
       export const googlePlayServicesAPI = function (method: string, resultSceneVariable: string) {
         cordova.plugins.playGamesServices[method](function() {
-            gdjs.runtimeScene.getVariables().get(resultSceneVariable).fromJSObject(arguments);
+            extensionData.runtimeSceneContainer.getVariables().get(resultSceneVariable).fromJSObject(arguments);
         },function() {
-            gdjs.runtimeScene.getVariables().get(resultSceneVariable).fromJSObject(arguments);
+            extensionData.runtimeSceneContainer.getVariables().get(resultSceneVariable).fromJSObject(arguments);
         });
       };
 
       export const googlePlayServicesAPIwParameters = function (method: string, resultSceneVariable: string, parameterSceneVariable: string) {
-        const parameters = gdjs.runtimeScene.getVariables().get(parameterSceneVariable).toJSObject();
+        const parameters = extensionData.runtimeSceneContainer.getVariables().get(parameterSceneVariable).toJSObject();
 
         cordova.plugins.playGamesServices[method](parameters, function() {
-            gdjs.runtimeScene.getVariables().get(resultSceneVariable).fromJSObject(arguments);
+            extensionData.runtimeSceneContainer.getVariables().get(resultSceneVariable).fromJSObject(arguments);
         },function() {
-            gdjs.runtimeScene.getVariables().get(resultSceneVariable).fromJSObject(arguments);
+            extensionData.runtimeSceneContainer.getVariables().get(resultSceneVariable).fromJSObject(arguments);
         });
       };
     }

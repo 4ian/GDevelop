02c9bede5817cc2b3998a06d08f3b225d46b6076
Florian Rival <Florian.rival@gmail.com>
2022-10-09 11:55:47 +0200

    Add test for local file > cloud resources moving

--- newIDE/app/src/ProjectsStorage/ResourceMover/LocalResourceMover.js
+++ newIDE/app/src/ProjectsStorage/ResourceMover/LocalResourceMover.js
@@ -19,8 +19,8 @@ import {
   type UploadedProjectResourceFiles,
 } from '../../Utils/GDevelopServices/Project';
 import { processByChunk } from '../../Utils/ProcessByChunk';
+import { readLocalFileToFile } from '../../Utils/LocalFileUploader';
 const path = optionalRequire('path');
-const fs = optionalRequire('fs');
 
 const gd: libGDevelop = global.gd;
 
@@ -44,25 +44,6 @@ const isBlobURL = (filename: string) => {
   return filename.startsWith('blob:');
 };
 
-const readLocalFileToArrayBuffer = async (filePath: string): Promise<ArrayBuffer> => {
-  const buffer: Buffer = await new Promise((resolve, reject) => {
-    fs.readFile(filePath, function(err, buffer) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(buffer);
-    });
-  });
-
-  // See https://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer:
-  // The slice and offset stuff is required because small Buffers (less than 4 kB by default, half the pool size)
-  // can be views on a shared ArrayBuffer. Without slicing, you can end up with an ArrayBuffer containing data from another Buffer.
-  const arrayBuffer = buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);;
-  return arrayBuffer;
-}
-
 type ResourceAndFile = {|
   resource: gdResource,
   file: File,
@@ -73,10 +54,6 @@ export const moveAllLocalResourcesToCloudResources = async ({
   authenticatedUser,
   oldFileMetadata,
   newFileMetadata,
-  oldStorageProvider,
-  oldStorageProviderOperations,
-  newStorageProvider,
-  newStorageProviderOperations,
   onProgress,
 }: MoveAllProjectResourcesOptions): Promise<MoveAllProjectResourcesResult> => {
   const result: MoveAllProjectResourcesResult = {
@@ -137,12 +114,9 @@ export const moveAllLocalResourcesToCloudResources = async ({
       );
 
       try {
-        const arrayBuffer = await readLocalFileToArrayBuffer(resourceAbsolutePath);
         return {
           resource,
-          file: new File([arrayBuffer], path.basename(resourceAbsolutePath), {
-            type: 'image/png', // TODO: read extension and get mime type from it.
-          }),
+          file: await readLocalFileToFile(resourceAbsolutePath),
         };
       } catch (error) {
         result.erroredResources.push({
@@ -155,7 +129,7 @@ export const moveAllLocalResourcesToCloudResources = async ({
       }
     },
     isChunkTooBig: (resourceAndFilesChunk: Array<ResourceAndFile | null>) => {
-      if (resourceAndFilesChunk.length > 20) return true;
+      if (resourceAndFilesChunk.length > 70) return true;
 
       const allBufferSize = resourceAndFilesChunk
         .filter(Boolean)

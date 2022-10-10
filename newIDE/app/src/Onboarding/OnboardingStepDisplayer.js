5ebe6586fa757cfa1a4902dede9d02da986af495
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-10 16:17:41 +0200

    Add logic to go to next step

--- newIDE/app/src/Onboarding/OnboardingStepDisplayer.js
+++ newIDE/app/src/Onboarding/OnboardingStepDisplayer.js
@@ -7,7 +7,24 @@ type Props = {|
   step: OnboardingFlowStep,
 |};
 
+const ELEMENT_QUERY_FREQUENCY = 500;
+
 function OnboardingStepDisplayer({ step }: Props) {
+  const [elementQueryCounter, setElementQueryCounter] = React.useState<number>(
+    0
+  );
+
+  React.useEffect(
+    () => {
+      const timeout = setTimeout(
+        () => setElementQueryCounter(elementQueryCounter + 1),
+        ELEMENT_QUERY_FREQUENCY
+      );
+      return () => clearTimeout(timeout);
+    },
+    [elementQueryCounter]
+  );
+
   const elementToHighlight = document.querySelector(step.elementToHighlightId);
 
   return (

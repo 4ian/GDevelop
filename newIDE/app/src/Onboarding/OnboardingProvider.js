3c595e7803ef518878c1b82623f0ad0904b09417
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-10 15:50:51 +0200

    Add mutation observer to onboarding provider to detect dom changes

--- newIDE/app/src/Onboarding/OnboardingProvider.js
+++ newIDE/app/src/Onboarding/OnboardingProvider.js
@@ -1,9 +1,40 @@
 // @flow
 import * as React from 'react';
+import { useDebounce } from '../Utils/UseDebounce';
 import OnboardingContext from './OnboardingContext';
 type Props = {| children: React.Node |};
 
 const OnboardingProvider = (props: Props) => {
+  const handleDomMutation = useDebounce(
+    React.useCallback(() => {
+      console.log('DOM change');
+    }, []),
+    200
+  );
+  const domObserverRef = React.useRef<?MutationObserver>(null);
+
+  React.useEffect(
+    () => {
+      const appContainer = document.querySelector('#root');
+      if (!appContainer) return;
+      const observer = new MutationObserver(handleDomMutation);
+      observer.observe(appContainer, {
+        childList: true,
+        attributes: true,
+        subtree: true,
+        characterData: true,
+      });
+      domObserverRef.current = observer;
+      return () => {
+        if (domObserverRef.current) {
+          domObserverRef.current.disconnect();
+          domObserverRef.current = null;
+        }
+      };
+    },
+    [handleDomMutation]
+  );
+
   return (
     <OnboardingContext.Provider
       value={{

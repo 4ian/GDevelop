89afa95c322aa1863610b06e129fc0f872aa13d6
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-10-10 19:44:48 +0200

    Wip improve scrollbars

--- newIDE/app/src/InstancesEditor/ViewPosition.js
+++ newIDE/app/src/InstancesEditor/ViewPosition.js
@@ -6,47 +6,47 @@ import { type InstancesEditorSettings } from './InstancesEditorSettings';
 type Props = {|
   initialViewX: number,
   initialViewY: number,
-  width: number,
-  height: number,
+  initialWidth: number,
+  initialHeight: number,
   instancesEditorSettings: InstancesEditorSettings,
 |};
 
 export default class ViewPosition {
   viewX: number = 0;
   viewY: number = 0;
-  _width: number;
-  _height: number;
+  width: number;
+  height: number;
   instancesEditorSettings: InstancesEditorSettings;
   _pixiContainer = new PIXI.Container();
 
   constructor({
     initialViewX,
     initialViewY,
-    width,
-    height,
+    initialWidth,
+    initialHeight,
     instancesEditorSettings,
   }: Props) {
     this.viewX = initialViewX;
     this.viewY = initialViewY;
     this.instancesEditorSettings = instancesEditorSettings;
-    this.resize(width, height);
+    this.resize(initialWidth, initialHeight);
   }
 
   setInstancesEditorSettings(instancesEditorSettings: InstancesEditorSettings) {
     this.instancesEditorSettings = instancesEditorSettings;
   }
 
-  resize(width: number, height: number) {
-    this._width = width;
-    this._height = height;
+  resize(newWidth: number, newHeight: number) {
+    this.width = newWidth;
+    this.height = newHeight;
   }
 
   getWidth() {
-    return this._width;
+    return this.width;
   }
 
   getHeight() {
-    return this._height;
+    return this.height;
   }
 
   /**
@@ -54,8 +54,8 @@ export default class ViewPosition {
    * "world" coordinates.
    */
   toSceneCoordinates = (x: number, y: number): [number, number] => {
-    x -= this._width / 2;
-    y -= this._height / 2;
+    x -= this.width / 2;
+    y -= this.height / 2;
     x /= Math.abs(this.instancesEditorSettings.zoomFactor);
     y /= Math.abs(this.instancesEditorSettings.zoomFactor);
 
@@ -91,7 +91,7 @@ export default class ViewPosition {
     x *= Math.abs(this.instancesEditorSettings.zoomFactor);
     y *= Math.abs(this.instancesEditorSettings.zoomFactor);
 
-    return [x + this._width / 2, y + this._height / 2];
+    return [x + this.width / 2, y + this.height / 2];
   };
 
   scrollBy(x: number, y: number) {
@@ -111,8 +111,8 @@ export default class ViewPosition {
   fitToRectangle(rectangle: Rectangle): number {
     this.viewX = rectangle.centerX();
     this.viewY = rectangle.centerY();
-    const idealZoomOnX = this._width / rectangle.width();
-    const idealZoomOnY = this._height / rectangle.height();
+    const idealZoomOnX = this.width / rectangle.width();
+    const idealZoomOnY = this.height / rectangle.height();
 
     return Math.min(idealZoomOnX, idealZoomOnY) * 0.95; // Add margin so that the object doesn't feel cut
   }
@@ -134,8 +134,8 @@ export default class ViewPosition {
       -this.viewX * this.instancesEditorSettings.zoomFactor;
     this._pixiContainer.position.y =
       -this.viewY * this.instancesEditorSettings.zoomFactor;
-    this._pixiContainer.position.x += this._width / 2;
-    this._pixiContainer.position.y += this._height / 2;
+    this._pixiContainer.position.x += this.width / 2;
+    this._pixiContainer.position.y += this.height / 2;
     this._pixiContainer.scale.x = this.instancesEditorSettings.zoomFactor;
     this._pixiContainer.scale.y = this.instancesEditorSettings.zoomFactor;
   }

f2e72e47dc98203726b70e7d713a13ee4b0dad2f
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-12 09:16:37 +0200

    Hide tooltip when element cannot be seen

--- newIDE/app/src/Onboarding/OnboardingTooltipDisplayer.js
+++ newIDE/app/src/Onboarding/OnboardingTooltipDisplayer.js
@@ -8,6 +8,7 @@ import { ColumnStackLayout } from '../UI/Layout';
 import Text from '../UI/Text';
 import { getDisplayZIndexForHighlighter } from './HTMLUtils';
 import { type OnboardingTooltip } from './OnboardingContext';
+import useIsElementVisibleInScroll from '../Utils/UseIsElementVisibleInScroll';
 
 type Props = {|
   anchorElement: HTMLElement,
@@ -15,10 +16,21 @@ type Props = {|
 |};
 
 function OnboardingTooltipDisplayer({ anchorElement, tooltip }: Props) {
+  const [show, setShow] = React.useState<boolean>(false);
+
+  const updateVisibility = React.useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      setShow(entries[0].isIntersecting);
+    },
+    []
+  );
+
+  useIsElementVisibleInScroll(anchorElement, updateVisibility);
+
   return (
     <Popper
       id="onboarding-tooltip-displayer"
-      open
+      open={show}
       anchorEl={anchorElement}
       transition
       placement={tooltip.placement}

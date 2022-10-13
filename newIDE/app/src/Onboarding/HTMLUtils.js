c96a23205c0b74a69079e94e279c5bcfa6a39474
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-13 10:16:24 +0200

    Check periodically if element is behind another dialog to hide highlighter and tooltip

--- newIDE/app/src/Onboarding/HTMLUtils.js
+++ newIDE/app/src/Onboarding/HTMLUtils.js
@@ -9,7 +9,7 @@ const getOverflowAttributes = (element: Element) =>
   getStyleAttribute(element, 'overflow-y') +
   getStyleAttribute(element, 'overflow-x');
 
-const getElementAncestry = (element: Element, elementPath: Array<Element>) => {
+export const getElementAncestry = (element: Element, elementPath: Array<Element>) => {
   if (!element.parentElement) {
     return elementPath;
   }

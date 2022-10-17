65931717f66714d339d96f2f760023076456a323
Davy Hélard <davy.helard@gmail.com>
2022-10-17 13:11:26 +0200

    Test CustomObjectConfiguration copy.

--- Core/tests/ObjectSerialization.cpp
+++ Core/tests/ObjectSerialization.cpp
@@ -12,11 +12,12 @@
 #include "GDCore/Events/Event.h"
 #include "GDCore/Events/EventsList.h"
 #include "GDCore/Events/Serialization.h"
+#include "GDCore/Extensions/Builtin/SpriteExtension/SpriteObject.h"
 #include "GDCore/Extensions/Platform.h"
+#include "GDCore/Project/CustomObjectConfiguration.h"
 #include "GDCore/Project/EventsFunctionsExtension.h"
 #include "GDCore/Project/Layout.h"
 #include "GDCore/Project/Object.h"
-#include "GDCore/Extensions/Builtin/SpriteExtension/SpriteObject.h"
 #include "GDCore/Project/ObjectsContainer.h"
 #include "GDCore/Project/Project.h"
 #include "GDCore/Project/Variable.h"
@@ -29,14 +30,7 @@ using namespace gd;
 
 namespace {
 
-void SetupProject(gd::Project &project, gd::Platform &platform) {
-  SetupProjectWithDummyPlatform(project, platform);
-
-  gd::Layout &layout = project.InsertNewLayout("Scene", 0);
-  gd::Object &object =
-      layout.InsertNewObject(project, "MyExtension::Sprite", "MyObject", 0);
-
-  auto &configuration = object.GetConfiguration();
+void SetupSpriteConfiguration(gd::ObjectConfiguration &configuration) {
   auto *spriteConfiguration = dynamic_cast<gd::SpriteObject *>(&configuration);
   REQUIRE(spriteConfiguration != nullptr);
   gd::Animation animation;
@@ -44,11 +38,30 @@ void SetupProject(gd::Project &project, gd::Platform &platform) {
   spriteConfiguration->AddAnimation(animation);
 };
 
-void CheckSpriteConfiguration(
-    SerializerElement &objectContainerElement) {
+gd::Object &SetupProjectWithSprite(gd::Project &project,
+                                   gd::Platform &platform) {
+  SetupProjectWithDummyPlatform(project, platform);
+
+  gd::Layout &layout = project.InsertNewLayout("Scene", 0);
+  gd::Object &object =
+      layout.InsertNewObject(project, "MyExtension::Sprite", "MyObject", 0);
+  SetupSpriteConfiguration(object.GetConfiguration());
+
+  return object;
 };
 
-void CheckSpriteConfigurationInElement(SerializerElement &projectElement) {
+void CheckSpriteConfigurationInObjectElement(SerializerElement &objectElement) {
+  REQUIRE(objectElement.HasChild("animations"));
+  auto &animationsElement = objectElement.GetChild("animations");
+  animationsElement.ConsiderAsArrayOf("animation");
+  REQUIRE(animationsElement.GetChildrenCount() == 1);
+  auto &animationElement = animationsElement.GetChild(0);
+
+  REQUIRE(animationElement.GetStringAttribute("name") == "Idle");
+};
+
+void CheckSpriteConfigurationInProjectElement(
+    SerializerElement &projectElement) {
   auto &layoutsElement = projectElement.GetChild("layouts");
   layoutsElement.ConsiderAsArrayOf("layout");
   REQUIRE(layoutsElement.GetChildrenCount() == 1);
@@ -64,30 +77,98 @@ void CheckSpriteConfigurationInElement(SerializerElement &projectElement) {
 
   REQUIRE(objectElement.GetStringAttribute("name") == "MyObject");
   REQUIRE(objectElement.GetStringAttribute("type") == "MyExtension::Sprite");
-
-  REQUIRE(objectElement.HasChild("animations"));
-  auto &animationsElement = objectElement.GetChild("animations");
-  animationsElement.ConsiderAsArrayOf("animation");
-  REQUIRE(animationsElement.GetChildrenCount() == 1);
-  auto &animationElement = animationsElement.GetChild(0);
-
-  REQUIRE(animationElement.GetStringAttribute("name") ==
-          "Idle");
+  CheckSpriteConfigurationInObjectElement(objectElement);
 };
 
-void CheckSpriteConfiguration(gd::Project &project) {
-  auto &layout = project.GetLayout("Scene");
-  auto &object = layout.GetObject("MyObject");
-  REQUIRE(object.GetName() == "MyObject");
-  REQUIRE(object.GetType() == "MyExtension::Sprite");
-
-  auto &configuration = object.GetConfiguration();
+void CheckSpriteConfiguration(gd::ObjectConfiguration &configuration) {
   auto *spriteConfiguration = dynamic_cast<gd::SpriteObject *>(&configuration);
   REQUIRE(spriteConfiguration);
   REQUIRE(spriteConfiguration->GetAnimationsCount() == 1);
 
   auto &animation = spriteConfiguration->GetAnimation(0);
   REQUIRE(animation.GetName() == "Idle");
+};
+
+void CheckSpriteConfiguration(gd::Object &object) {
+  REQUIRE(object.GetName() == "MyObject");
+  REQUIRE(object.GetType() == "MyExtension::Sprite");
+
+  CheckSpriteConfiguration(object.GetConfiguration());
+};
+
+void CheckSpriteConfiguration(gd::Project &project) {
+  auto &layout = project.GetLayout("Scene");
+  auto &object = layout.GetObject("MyObject");
+  CheckSpriteConfiguration(object);
+};
+
+gd::Object &SetupProjectWithCustomObject(gd::Project &project,
+                                         gd::Platform &platform) {
+  SetupProjectWithDummyPlatform(project, platform);
+
+  auto &eventsExtension =
+      project.InsertNewEventsFunctionsExtension("MyEventsExtension", 0);
+  auto &eventsBasedObject = eventsExtension.GetEventsBasedObjects().InsertNew(
+      "MyEventsBasedObject", 0);
+  eventsBasedObject.SetFullName("My events based object");
+  eventsBasedObject.SetDescription("An events based object for test");
+  eventsBasedObject.InsertNewObject(project, "MyExtension::Sprite", "MyChild",
+                                    0);
+
+  gd::Layout &layout = project.InsertNewLayout("Scene", 0);
+  gd::Object &object = layout.InsertNewObject(
+      project, "MyEventsExtension::MyEventsBasedObject", "MyObject", 0);
+  auto &configuration = object.GetConfiguration();
+  auto *customObjectConfiguration =
+      dynamic_cast<gd::CustomObjectConfiguration *>(&configuration);
+  auto &spriteConfiguration =
+      customObjectConfiguration->GetChildObjectConfiguration("MyChild");
+  SetupSpriteConfiguration(spriteConfiguration);
+
+  return object;
+};
+
+void CheckCustomObjectConfigurationInProjectElement(
+    SerializerElement &projectElement) {
+  auto &layoutsElement = projectElement.GetChild("layouts");
+  layoutsElement.ConsiderAsArrayOf("layout");
+  REQUIRE(layoutsElement.GetChildrenCount() == 1);
+  auto &layoutElement = layoutsElement.GetChild(0);
+
+  REQUIRE(layoutElement.GetStringAttribute("name") == "Scene");
+  REQUIRE(layoutElement.HasChild("objects"));
+
+  auto &objectsElement = layoutElement.GetChild("objects");
+  objectsElement.ConsiderAsArrayOf("object");
+  REQUIRE(objectsElement.GetChildrenCount() == 1);
+  auto &objectElement = objectsElement.GetChild(0);
+
+  REQUIRE(objectElement.GetStringAttribute("name") == "MyObject");
+  REQUIRE(objectElement.GetStringAttribute("type") ==
+          "MyEventsExtension::MyEventsBasedObject");
+  auto &childrenContentElement = objectElement.GetChild("childrenContent");
+
+  REQUIRE(childrenContentElement.HasChild("MyChild"));
+  auto &childElement = childrenContentElement.GetChild("MyChild");
+  CheckSpriteConfigurationInObjectElement(childElement);
+};
+
+void CheckCustomObjectConfiguration(gd::Object &object) {
+  REQUIRE(object.GetName() == "MyObject");
+  REQUIRE(object.GetType() == "MyEventsExtension::MyEventsBasedObject");
+
+  auto &configuration = object.GetConfiguration();
+  auto *customObjectConfiguration =
+      dynamic_cast<gd::CustomObjectConfiguration *>(&configuration);
+  auto &spriteConfiguration =
+      customObjectConfiguration->GetChildObjectConfiguration("MyChild");
+  CheckSpriteConfiguration(spriteConfiguration);
+};
+
+void CheckCustomObjectConfiguration(gd::Project &project) {
+  auto &layout = project.GetLayout("Scene");
+  auto &object = layout.GetObject("MyObject");
+  CheckCustomObjectConfiguration(object);
 };
 } // namespace
 
@@ -96,16 +177,52 @@ TEST_CASE("ObjectSerialization", "[common]") {
   SECTION("Save and load a project with a sprite configuration") {
     gd::Platform platform;
     gd::Project writtenProject;
-    SetupProject(writtenProject, platform);
+    SetupProjectWithSprite(writtenProject, platform);
     CheckSpriteConfiguration(writtenProject);
 
     SerializerElement projectElement;
     writtenProject.SerializeTo(projectElement);
-    CheckSpriteConfigurationInElement(projectElement);
+    CheckSpriteConfigurationInProjectElement(projectElement);
 
     gd::Project readProject;
     readProject.AddPlatform(platform);
     readProject.UnserializeFrom(projectElement);
     CheckSpriteConfiguration(readProject);
+  }
+
+  SECTION("Clone a sprite object") {
+    gd::Platform platform;
+    gd::Project project;
+    auto &object = SetupProjectWithSprite(project, platform);
+    CheckSpriteConfiguration(object);
+
+    auto clonedObject = object.Clone();
+    CheckSpriteConfiguration(*(clonedObject.get()));
+  }
+
+  SECTION("Save and load a project with a custom object configuration") {
+    gd::Platform platform;
+    gd::Project writtenProject;
+    SetupProjectWithCustomObject(writtenProject, platform);
+    CheckCustomObjectConfiguration(writtenProject);
+
+    SerializerElement projectElement;
+    writtenProject.SerializeTo(projectElement);
+    CheckCustomObjectConfigurationInProjectElement(projectElement);
+
+    gd::Project readProject;
+    readProject.AddPlatform(platform);
+    readProject.UnserializeFrom(projectElement);
+    CheckCustomObjectConfiguration(readProject);
+  }
+
+  SECTION("Clone a custom object") {
+    gd::Platform platform;
+    gd::Project project;
+    auto &object = SetupProjectWithCustomObject(project, platform);
+    CheckCustomObjectConfiguration(object);
+
+    auto clonedObject = object.Clone();
+    CheckCustomObjectConfiguration(*(clonedObject.get()));
   }
 }

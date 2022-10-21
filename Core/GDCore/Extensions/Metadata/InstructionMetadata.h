a7cb3fc5a2f126f0e9d5d34e0e6ea9df767e2a80
Davy Hélard <davy.helard@gmail.com>
2022-10-21 20:19:03 +0200

    Add some comment about parameter types.

--- Core/GDCore/Extensions/Metadata/InstructionMetadata.h
+++ Core/GDCore/Extensions/Metadata/InstructionMetadata.h
@@ -222,13 +222,19 @@ class GD_CORE_API InstructionMetadata {
 
   /**
    * \brief Add the default parameters for an instruction manipulating the
-   * specified type ("string", "number") with the default operators.
+   * specified type ("string", "number", "boolean") with the default operators.
+   * 
+   * \note The type "string" can be declined in several subtypes.
+   * \see ParameterMetadata
    */
   InstructionMetadata &UseStandardOperatorParameters(const gd::String &type);
 
   /**
    * \brief Add the default parameters for an instruction comparing the
    * specified type ("string", "number") with the default relational operators.
+   * 
+   * \note The type "string" can be declined in several subtypes.
+   * \see ParameterMetadata
    */
   InstructionMetadata &UseStandardRelationalOperatorParameters(
       const gd::String &type);

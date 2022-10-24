84fc09dcd8d871c0f3452ecd7a4ae4cbc2d2de2b
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-24 17:35:26 +0200

    Deprecate variable tween actions with starting value

--- Extensions/TweenBehavior/JsExtension.js
+++ Extensions/TweenBehavior/JsExtension.js
@@ -118,11 +118,38 @@ module.exports = {
       .addParameter('expression', _('Final value'), '', false)
       .addParameter('expression', _('Duration'), '', false)
       .addParameter('stringWithSelector', _('Easing'), easingChoices, false)
+      .setHidden()
       .getCodeExtraInformation()
       .setIncludeFile('Extensions/TweenBehavior/shifty.js')
       .addIncludeFile('Extensions/TweenBehavior/shifty_setup.js')
       .addIncludeFile('Extensions/TweenBehavior/tweentools.js')
       .setFunctionName('gdjs.evtTools.tween.tweenVariableNumber');
+
+    extension
+      .addAction(
+        'TweenSceneVariableNumber2',
+        _('Tween a number in a scene variable'),
+        _(
+          "Tweens a scene variable's numeric value from current value to another."
+        ),
+        _(
+          'Tween variable _PARAM2_ to _PARAM3_ for _PARAM4_ms with easing _PARAM5_ as _PARAM1_'
+        ),
+        _('Scene Tweens'),
+        'JsPlatform/Extensions/tween_behavior24.png',
+        'JsPlatform/Extensions/tween_behavior32.png'
+      )
+      .addCodeOnlyParameter('currentScene', '')
+      .addParameter('identifier', _('Tween Identifier'), 'sceneTween')
+      .addParameter('scenevar', _('The variable to tween'), '', false)
+      .addParameter('expression', _('Final value'), '', false)
+      .addParameter('expression', _('Duration'), '', false)
+      .addParameter('stringWithSelector', _('Easing'), easingChoices, false)
+      .getCodeExtraInformation()
+      .setIncludeFile('Extensions/TweenBehavior/shifty.js')
+      .addIncludeFile('Extensions/TweenBehavior/shifty_setup.js')
+      .addIncludeFile('Extensions/TweenBehavior/tweentools.js')
+      .setFunctionName('gdjs.evtTools.tween.tweenVariableNumber2');
 
     extension
       .addAction(
@@ -393,8 +420,39 @@ module.exports = {
         false
       )
       .setDefaultValue('no')
+      .setHidden()
       .getCodeExtraInformation()
       .setFunctionName('addVariableTween');
+
+    behavior
+      .addAction(
+        'AddObjectVariableTween2',
+        _('Add object variable tween'),
+        _('Add a tween animation for an object variable.'),
+        _(
+          'Tween the variable _PARAM3_ of _PARAM0_ to _PARAM4_ with easing _PARAM5_ over _PARAM6_ms as _PARAM2_'
+        ),
+        _('Variables'),
+        'JsPlatform/Extensions/tween_behavior24.png',
+        'JsPlatform/Extensions/tween_behavior32.png'
+      )
+      .addParameter('object', _('Object'), '', false)
+      .addParameter('behavior', _('Behavior'), 'TweenBehavior', false)
+      .addParameter('identifier', _('Tween Identifier'), 'objectTween')
+      .addParameter('objectvar', _('Object variable'), '', false)
+      .addParameter('expression', _('To value'), '', false)
+      .addParameter('stringWithSelector', _('Easing'), easingChoices, false)
+      .setDefaultValue('linear')
+      .addParameter('expression', _('Duration, in milliseconds'), '', false)
+      .addParameter(
+        'yesorno',
+        _('Destroy this object when tween finishes'),
+        '',
+        false
+      )
+      .setDefaultValue('no')
+      .getCodeExtraInformation()
+      .setFunctionName('addVariableTween2');
 
     behavior
       .addAction(

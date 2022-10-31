f1dbb595bfb3a27b89154c210ebb55b287559040
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-10-31 17:45:21 +0100

    Add logic to fetch in app tutorial at start

--- newIDE/app/src/Utils/GDevelopServices/InAppTutorial.js
+++ newIDE/app/src/Utils/GDevelopServices/InAppTutorial.js
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { GDevelopAssetApi } from './ApiConfigs';
+import { type InAppTutorial } from '../../InAppTutorial/InAppTutorialContext';
 
 export type InAppTutorialShortHeader = {| id: string, contentUrl: string |};
 
@@ -11,5 +12,12 @@ export const fetchInAppTutorialShortHeaders = async (): Promise<
   const response = await axios.get(
     `${GDevelopAssetApi.baseUrl}/in-app-tutorial-short-header`
   );
+  return response.data;
+};
+
+export const fetchInAppTutorial = async (
+  shortHeader: InAppTutorialShortHeader
+): Promise<InAppTutorial> => {
+  const response = await axios.get(shortHeader.contentUrl);
   return response.data;
 };

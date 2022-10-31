69436241a7995b279e0161d7006e1c688dc6cbd8
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-10-31 12:59:21 +0100

    Update React Markdown to fix rendering issue

--- newIDE/app/src/UI/MarkdownText.js
+++ newIDE/app/src/UI/MarkdownText.js
@@ -64,7 +64,7 @@ export const MarkdownText = (props: Props) => {
       {({ i18n }) => (
         <ReactMarkdown
           escapeHtml
-          source={
+          children={
             props.translatableSource
               ? i18n._(props.translatableSource)
               : props.source

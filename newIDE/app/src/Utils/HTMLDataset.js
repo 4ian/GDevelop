ea1d0de826988a6e1600855fdc863fdb7c719d0a
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-11-14 15:49:54 +0100

    Add possibility to set html dataset for key components

---
+++ newIDE/app/src/Utils/HTMLDataset.js
@@ -0,0 +1,13 @@
+// @flow
+
+import { toKebabCase } from './StringHelpers';
+
+export type HTMLDataset = {| [key: string]: string |};
+
+export const dataObjectToProps = (object: ?HTMLDataset) =>
+  object
+    ? Object.entries(object).reduce((acc, [key, value]) => {
+        acc[`data-${toKebabCase(key)}`] = value;
+        return acc;
+      }, {})
+    : undefined;

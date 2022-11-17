ce107534054f1276931c31d2ea928558671503e1
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-11-17 15:40:59 +0100

    Add possibility to set add condition button

--- newIDE/app/src/EventsSheet/EventsTree/InstructionsList.js
+++ newIDE/app/src/EventsSheet/EventsTree/InstructionsList.js
@@ -41,6 +41,7 @@ type Props = {
   onParameterClick: ParameterContext => void,
   selection: any,
   addButtonLabel?: React.Node,
+  addButtonId?: string,
   className?: string,
   style?: Object,
   disabled: boolean,
@@ -58,6 +59,7 @@ const DropTarget = makeDropTarget<{
 }>(reactDndInstructionType);
 
 export default function InstructionsList({
+  addButtonId,
   addButtonLabel,
   areConditions,
   className,
@@ -220,7 +222,7 @@ export default function InstructionsList({
                 }}
                 {...longTouchForContextMenuProps}
                 ref={addButton}
-                id={`${
+                id={addButtonId || `${
                   areConditions ? 'add-condition-button' : 'add-action-button'
                 }${instructions.length === 0 ? '-empty' : ''}`}
               >

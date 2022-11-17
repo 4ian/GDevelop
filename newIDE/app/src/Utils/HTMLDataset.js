56ccfc14eadd23031d3644aececf8a6d7a069317
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-11-17 18:02:45 +0100

    Allow undefined in html dataset type

--- newIDE/app/src/Utils/HTMLDataset.js
+++ newIDE/app/src/Utils/HTMLDataset.js
@@ -2,12 +2,14 @@
 
 import { toKebabCase } from './StringHelpers';
 
-export type HTMLDataset = {| [key: string]: string |};
+export type HTMLDataset = {| [key: string]: ?string |};
 
 export const dataObjectToProps = (object: ?HTMLDataset) =>
   object
     ? Object.entries(object).reduce((acc, [key, value]) => {
-        acc[`data-${toKebabCase(key)}`] = value;
+        if (value) {
+          acc[`data-${toKebabCase(key)}`] = value;
+        }
         return acc;
       }, {})
     : undefined;

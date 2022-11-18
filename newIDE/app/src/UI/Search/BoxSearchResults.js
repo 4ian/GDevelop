aee15424f2c2d5e8cf6dd50bad52bbbe44582490
D8H <Davy.Helard@gmail.com>
2022-11-18 17:06:10 +0100

    Save the scroll position of the asset store (#4528)

--- newIDE/app/src/UI/Search/BoxSearchResults.js
+++ newIDE/app/src/UI/Search/BoxSearchResults.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import PlaceholderLoader from '../PlaceholderLoader';
 import PlaceholderError from '../PlaceholderError';
 import ErrorBoundary from '../ErrorBoundary';
-import { AutoSizer, Grid } from 'react-virtualized';
+import { AutoSizer, Grid, type ScrollParams } from 'react-virtualized';
 import EmptyMessage from '../EmptyMessage';
 
 type Props<SearchItem> = {|
@@ -24,86 +24,129 @@ const styles = {
   grid: { overflowX: 'hidden' },
 };
 
-export const BoxSearchResults = <SearchItem>({
-  searchItems,
-  renderSearchItem,
-  error,
-  onRetry,
-  baseSize,
-  noResultPlaceholder,
-  noScroll,
-}: Props<SearchItem>) => {
-  if (!searchItems) {
-    if (!error) return <PlaceholderLoader />;
-    else {
+export type BoxSearchResultsInterface = {|
+  getScrollPosition: () => number,
+  scrollToPosition: (y: number) => void,
+|};
+
+export const BoxSearchResults = React.forwardRef<
+  // $FlowFixMe The generic type can't pass through.
+  Props<SearchItem>,
+  BoxSearchResultsInterface
+>(
+  (
+    {
+      searchItems,
+      renderSearchItem,
+      error,
+      onRetry,
+      baseSize,
+      noResultPlaceholder,
+      noScroll,
+    }: Props<SearchItem>,
+    ref
+  ) => {
+    const grid = React.useRef<?Grid>(null);
+    React.useImperativeHandle(ref, () => ({
+      /**
+       * Return the scroll position.
+       */
+      getScrollPosition: () => {
+        const scrollViewElement = grid.current;
+        if (!scrollViewElement) return 0;
+
+        // TODO Find a clean way to get the scroll position.
+        // Using the internal state of a component is hacky.
+        // Grid probably doesn't expose the scroll position
+        // because it became irrelevant when the dimensions change.
+        // Though, it's easier to use it and the chance that the Grid is
+        // resized is low.
+        return scrollViewElement.state.scrollTop;
+      },
+      scrollToPosition: (y: number) => {
+        const scrollViewElement = grid.current;
+        if (!scrollViewElement) return;
+
+        scrollViewElement.scrollToPosition({ scrollLeft: 0, scrollTop: y });
+      },
+    }));
+
+    if (!searchItems) {
+      if (!error) return <PlaceholderLoader />;
+      else {
+        return (
+          <PlaceholderError onRetry={onRetry}>
+            <Trans>
+              Can't load the results. Verify your internet connection or retry
+              later.
+            </Trans>
+          </PlaceholderError>
+        );
+      }
+    } else if (searchItems.length === 0) {
       return (
-        <PlaceholderError onRetry={onRetry}>
-          <Trans>
-            Can't load the results. Verify your internet connection or retry
-            later.
-          </Trans>
-        </PlaceholderError>
+        noResultPlaceholder || (
+          <EmptyMessage>
+            <Trans>
+              No results returned for your search. Try something else, browse
+              the categories or create your object from scratch!
+            </Trans>
+          </EmptyMessage>
+        )
       );
     }
-  } else if (searchItems.length === 0) {
+
     return (
-      noResultPlaceholder || (
-        <EmptyMessage>
-          <Trans>
-            No results returned for your search. Try something else, browse the
-            categories or create your object from scratch!
-          </Trans>
-        </EmptyMessage>
-      )
-    );
-  }
+      <ErrorBoundary>
+        <div style={styles.container}>
+          <AutoSizer>
+            {({ width, height }) => {
+              const columnCount = Math.max(
+                Math.floor((width - 5) / baseSize),
+                1
+              );
+              const columnWidth = Math.max(Math.floor(width / columnCount), 30);
+              const rowCount = Math.max(
+                1,
+                Math.ceil(searchItems.length / columnCount)
+              );
+              const rowHeight = columnWidth; // Square items.
+              const gridHeight = noScroll ? rowHeight * rowCount : height;
+              const gridWidth = width;
 
-  return (
-    <ErrorBoundary>
-      <div style={styles.container}>
-        <AutoSizer>
-          {({ width, height }) => {
-            const columnCount = Math.max(Math.floor((width - 5) / baseSize), 1);
-            const columnWidth = Math.max(Math.floor(width / columnCount), 30);
-            const rowCount = Math.max(
-              1,
-              Math.ceil(searchItems.length / columnCount)
-            );
-            const rowHeight = columnWidth; // Square items.
-            const gridHeight = noScroll ? rowHeight * rowCount : height;
-            const gridWidth = width;
+              function cellRenderer({ columnIndex, key, rowIndex, style }) {
+                const indexInList = rowIndex * columnCount + columnIndex;
+                const searchItem =
+                  indexInList < searchItems.length
+                    ? searchItems[indexInList]
+                    : null;
 
-            function cellRenderer({ columnIndex, key, rowIndex, style }) {
-              const indexInList = rowIndex * columnCount + columnIndex;
-              const searchItem =
-                indexInList < searchItems.length
-                  ? searchItems[indexInList]
-                  : null;
+                return (
+                  <div key={key} style={style}>
+                    {searchItem
+                      ? renderSearchItem(searchItem, columnWidth)
+                      : null}
+                  </div>
+                );
+              }
 
               return (
-                <div key={key} style={style}>
-                  {searchItem
-                    ? renderSearchItem(searchItem, columnWidth)
-                    : null}
-                </div>
+                <Grid
+                  ref={grid}
+                  width={gridWidth}
+                  height={gridHeight}
+                  columnCount={columnCount}
+                  columnWidth={columnWidth}
+                  rowHeight={rowHeight}
+                  rowCount={rowCount}
+                  cellRenderer={cellRenderer}
+                  style={styles.grid}
+                />
               );
-            }
-
-            return (
-              <Grid
-                width={gridWidth}
-                height={gridHeight}
-                columnCount={columnCount}
-                columnWidth={columnWidth}
-                rowHeight={rowHeight}
-                rowCount={rowCount}
-                cellRenderer={cellRenderer}
-                style={styles.grid}
-              />
-            );
-          }}
-        </AutoSizer>
-      </div>
-    </ErrorBoundary>
-  );
-};
+            }}
+          </AutoSizer>
+        </div>
+      </ErrorBoundary>
+    );
+  }
+);

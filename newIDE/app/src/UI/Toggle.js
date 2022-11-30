5cad2be194ea962d407a2af9a9963f7b8aa2d20c
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-11-30 15:23:43 +0100

    Improve Toggle styling (#4602)

--- newIDE/app/src/UI/Toggle.js
+++ newIDE/app/src/UI/Toggle.js
@@ -3,8 +3,10 @@ import * as React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import { makeStyles } from '@material-ui/core';
+import GDevelopThemeContext from './Theme/ThemeContext';
+import { type GDevelopTheme } from './Theme';
 
-const useStyles = makeStyles({
+const useFormStyles = makeStyles({
   root: {
     cursor: 'default',
   },
@@ -12,15 +14,41 @@ const useStyles = makeStyles({
   label: { flex: 1 },
 });
 
+const useSwitchStyles = ({
+  theme,
+  toggled,
+  disabled,
+}: {
+  theme: GDevelopTheme,
+  toggled: boolean,
+  disabled: boolean,
+}) =>
+  makeStyles({
+    thumb: {
+      backgroundColor: disabled
+        ? theme.switch.thumbColor.disabled
+        : toggled
+        ? theme.switch.thumbColor.toggled
+        : theme.switch.thumbColor.default,
+    },
+    track: {
+      backgroundColor: disabled
+        ? theme.switch.trackColor.disabled
+        : toggled
+        ? theme.switch.trackColor.toggled
+        : theme.switch.trackColor.default,
+      opacity: '1 !important', // MUI overrides the opacity to 0.5, prevent this.
+    },
+  })();
+
 // We support a subset of the props supported by Material-UI v0.x Toggle
 // They should be self descriptive - refer to Material UI docs otherwise.
 type Props = {|
-  label?: ?React.Node,
+  label: React.Node,
   toggled: boolean,
   onToggle: (e: {||}, toggled: boolean) => void,
   disabled?: boolean,
   labelPosition: 'right' | 'left',
-  size?: 'small',
 
   style?: {|
     marginTop?: number,
@@ -30,25 +58,36 @@ type Props = {|
 /**
  * A text field based on Material-UI text field.
  */
-const Toggle = (props: Props) => {
-  const classes = useStyles();
+const Toggle = ({
+  label,
+  toggled,
+  onToggle,
+  disabled,
+  labelPosition,
+  style,
+}: Props) => {
+  const formClasses = useFormStyles();
+  const gdevelopTheme = React.useContext(GDevelopThemeContext);
+  const switchClasses = useSwitchStyles({
+    theme: gdevelopTheme,
+    toggled,
+    disabled: !!disabled,
+  });
   return (
     <FormControlLabel
       control={
         <Switch
-          checked={props.toggled}
-          onChange={event => props.onToggle(event, event.target.checked)}
-          color="secondary"
-          size={props.size}
+          checked={toggled}
+          onChange={event => onToggle(event, event.target.checked)}
+          classes={switchClasses}
+          color="default"
         />
       }
-      labelPlacement={props.labelPosition === 'right' ? 'end' : 'start'}
-      label={props.label}
-      disabled={props.disabled}
-      classes={classes}
-      style={{
-        ...props.style,
-      }}
+      labelPlacement={labelPosition === 'right' ? 'end' : 'start'}
+      label={label}
+      disabled={disabled}
+      classes={formClasses}
+      style={style}
     />
   );
 };

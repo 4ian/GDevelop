c50984b958a2eecbd649f45f8a5bc310400c9661
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-12-02 11:39:12 +0100

    Fix a few things

--- newIDE/app/src/InAppTutorial/InAppTutorialOrchestrator.js
+++ newIDE/app/src/InAppTutorial/InAppTutorialOrchestrator.js
@@ -182,7 +182,7 @@ const interpolateElementId = (
   ) {
     const splittedElementId = elementId.split(':');
     const sceneKey = splittedElementId[1];
-    return `#project-manager [id^="scene-item"][data-scene="${
+    return `div[role="presentation"]:not([aria-hidden=true]) #project-manager [id^="scene-item"][data-scene="${
       data[sceneKey]
     }"]`;
   } else if (

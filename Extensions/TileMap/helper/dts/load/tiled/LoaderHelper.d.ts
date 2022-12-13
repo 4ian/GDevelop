6e13f63e4be258303fae2d949f0953a9999a8d60
Baptiste Augrain <daiyam@zokugun.org>
2022-12-13 19:35:34 +0100

    style: format generated codes

--- Extensions/TileMap/helper/dts/load/tiled/LoaderHelper.d.ts
+++ Extensions/TileMap/helper/dts/load/tiled/LoaderHelper.d.ts
@@ -1,5 +1,5 @@
-import { integer } from "../../types/commons";
-import { TiledLayer } from "../../types/Tiled";
+import { integer } from '../../types/commons';
+import { TiledLayer } from '../../types/Tiled';
 /**
  * Decodes a layer data, which can sometimes be store as a compressed base64 string
  * by Tiled.
@@ -8,23 +8,30 @@ import { TiledLayer } from "../../types/Tiled";
  * @param layer The layer data from a Tiled JSON.
  * @returns The decoded layer data.
  */
-export declare const decodeBase64LayerData: (pako: any, layer: TiledLayer) => number[];
+export declare const decodeBase64LayerData: (
+  pako: any,
+  layer: TiledLayer
+) => number[];
 export type TiledGID = {
-    id: integer;
-    flippedHorizontally: boolean;
-    flippedVertically: boolean;
-    flippedDiagonally: boolean;
+  id: integer;
+  flippedHorizontally: boolean;
+  flippedVertically: boolean;
+  flippedDiagonally: boolean;
 };
 /**
  * Extract information about the rotation of a tile from the tile id.
  * @param globalTileUid The Tiled tile global uniq identifier.
  * @returns The tile identifier and orientation.
  */
-export declare const extractTileUidFlippedStates: (globalTileUid: integer) => TiledGID;
+export declare const extractTileUidFlippedStates: (
+  globalTileUid: integer
+) => TiledGID;
 /**
  * Tiled use 0 as null, we do too but it's black boxed.
  * This is why the id needs to be decremented.
  * @return the tile identifier.
  */
-export declare function getTileIdFromTiledGUI(tiledGUI: number | undefined): number | undefined;
+export declare function getTileIdFromTiledGUI(
+  tiledGUI: number | undefined
+): number | undefined;
 //# sourceMappingURL=LoaderHelper.d.ts.map

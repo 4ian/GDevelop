664d9cf7f02a62a54626845b125447cad32d0c71
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2022-12-15 12:41:00 +0100

    Fix image preview overflowing dialog

--- newIDE/app/src/ObjectEditor/Editors/SpriteEditor/AnimationPreview.js
+++ newIDE/app/src/ObjectEditor/Editors/SpriteEditor/AnimationPreview.js
@@ -2,7 +2,7 @@
 import { Trans } from '@lingui/macro';
 
 import React from 'react';
-import { Column, Line } from '../../../UI/Grid';
+import { Column } from '../../../UI/Grid';
 import { LineStackLayout, ResponsiveLineStackLayout } from '../../../UI/Layout';
 import ImagePreview from '../../../ResourcesList/ResourcePreview/ImagePreview';
 import Replay from '@material-ui/icons/Replay';
@@ -21,6 +21,7 @@ const styles = {
     position: 'relative',
     display: 'flex',
     flex: 1,
+    height: 'calc(100% - 80px)', // 80px are allocated to the space the play pause button line can take once the responsive line stack layout is collapsed
   },
   loaderContainer: {
     position: 'absolute',
@@ -238,98 +239,109 @@ const AnimationPreview = ({
 
   return (
     <Column expand noOverflowParent noMargin>
-      <Line noMargin expand>
-        <div style={styles.imageContainer}>
-          <ImagePreview
-            resourceName={resourceName}
-            imageResourceSource={getImageResourceSource(resourceName)}
-            isImageResourceSmooth={isImageResourceSmooth(resourceName)}
-            initialZoom={initialZoom}
-            project={project}
-            hideCheckeredBackground={hideCheckeredBackground}
-            hideControls={hideControls}
-            fixedHeight={fixedHeight}
-            fixedWidth={fixedWidth}
-            onImageLoaded={onImageLoaded}
-            isImagePrivate={isAssetPrivate}
-            hideLoader // Handled by the animation preview, important to let the browser cache the image.
-          />
-          {!hideAnimationLoader && isStillLoadingResources && (
-            <div style={styles.loaderContainer}>
-              <PlaceholderLoader />
-            </div>
-          )}
-        </div>
-      </Line>
+      <div style={styles.imageContainer}>
+        <ImagePreview
+          resourceName={resourceName}
+          imageResourceSource={getImageResourceSource(resourceName)}
+          isImageResourceSmooth={isImageResourceSmooth(resourceName)}
+          initialZoom={initialZoom}
+          project={project}
+          hideCheckeredBackground={hideCheckeredBackground}
+          hideControls={hideControls}
+          fixedHeight={fixedHeight}
+          fixedWidth={fixedWidth}
+          onImageLoaded={onImageLoaded}
+          isImagePrivate={isAssetPrivate}
+          hideLoader // Handled by the animation preview, important to let the browser cache the image.
+        />
+        {!hideAnimationLoader && isStillLoadingResources && (
+          <div style={styles.loaderContainer}>
+            <PlaceholderLoader />
+          </div>
+        )}
+      </div>
       {!hideControls && (
-        <ResponsiveLineStackLayout alignItems="center">
-          <LineStackLayout alignItems="center" justifyContent="center" noMargin>
-            <Text>
-              <Trans>FPS:</Trans>
-            </Text>
-            <TextField
-              margin="none"
-              value={fps}
-              onChange={(e, text) => {
-                const fps = parseFloat(text);
-                if (fps > 0) {
-                  setFps(fps);
-                  const newTimeBetweenFrames = parseFloat((1 / fps).toFixed(4));
-                  timeBetweenFramesRef.current = newTimeBetweenFrames;
-                  if (onChangeTimeBetweenFrames) {
-                    onChangeTimeBetweenFrames(newTimeBetweenFrames);
+        // Column used to not have the expand behavior when responsive line stack layout is a column
+        <Column noMargin>
+          <ResponsiveLineStackLayout alignItems="center">
+            <LineStackLayout
+              alignItems="center"
+              justifyContent="center"
+              noMargin
+            >
+              <Text>
+                <Trans>FPS:</Trans>
+              </Text>
+              <TextField
+                margin="none"
+                value={fps}
+                onChange={(e, text) => {
+                  const fps = parseFloat(text);
+                  if (fps > 0) {
+                    setFps(fps);
+                    const newTimeBetweenFrames = parseFloat(
+                      (1 / fps).toFixed(4)
+                    );
+                    timeBetweenFramesRef.current = newTimeBetweenFrames;
+                    if (onChangeTimeBetweenFrames) {
+                      onChangeTimeBetweenFrames(newTimeBetweenFrames);
+                    }
+                    replay();
                   }
-                  replay();
-                }
-              }}
-              id="direction-time-between-frames"
-              type="number"
-              step={1}
-              min={1}
-              max={100}
-              style={styles.timeField}
-              autoFocus={true}
-            />
-            <Timer style={styles.timeIcon} />
-            <TextField
-              margin="none"
-              value={timeBetweenFramesRef.current}
-              onChange={(e, text) => {
-                const time = parseFloat(text);
-                if (time > 0) {
-                  setFps(Math.round(1 / time));
-                  timeBetweenFramesRef.current = time;
-                  if (onChangeTimeBetweenFrames) {
-                    onChangeTimeBetweenFrames(time);
+                }}
+                id="direction-time-between-frames"
+                type="number"
+                step={1}
+                min={1}
+                max={100}
+                style={styles.timeField}
+                autoFocus={true}
+              />
+              <Timer style={styles.timeIcon} />
+              <TextField
+                margin="none"
+                value={timeBetweenFramesRef.current}
+                onChange={(e, text) => {
+                  const time = parseFloat(text);
+                  if (time > 0) {
+                    setFps(Math.round(1 / time));
+                    timeBetweenFramesRef.current = time;
+                    if (onChangeTimeBetweenFrames) {
+                      onChangeTimeBetweenFrames(time);
+                    }
+                    replay();
                   }
-                  replay();
-                }
-              }}
-              id="direction-time-between-frames"
-              type="number"
-              step={0.005}
-              precision={2}
-              min={0.01}
-              max={5}
-              style={styles.timeField}
-            />
-          </LineStackLayout>
-          <LineStackLayout alignItems="center" justifyContent="center" noMargin>
-            <FlatButton
-              leftIcon={<Replay />}
-              label={<Trans>Replay</Trans>}
-              onClick={replay}
-            />
-            <FlatButton
-              leftIcon={!!pausedRef.current ? <PlayArrow /> : <Pause />}
-              label={!!pausedRef.current ? 'Play' : 'Pause'}
-              onClick={() => {
-                pausedRef.current = !pausedRef.current;
-                forceUdpate();
-              }}
-            />
-          </LineStackLayout>
-        </ResponsiveLineStackLayout>
+                }}
+                id="direction-time-between-frames"
+                type="number"
+                step={0.005}
+                precision={2}
+                min={0.01}
+                max={5}
+                style={styles.timeField}
+              />
+            </LineStackLayout>
+            <LineStackLayout
+              alignItems="center"
+              justifyContent="center"
+              noMargin
+            >
+              <FlatButton
+                leftIcon={<Replay />}
+                label={<Trans>Replay</Trans>}
+                onClick={replay}
+              />
+              <FlatButton
+                leftIcon={!!pausedRef.current ? <PlayArrow /> : <Pause />}
+                label={!!pausedRef.current ? 'Play' : 'Pause'}
+                onClick={() => {
+                  pausedRef.current = !pausedRef.current;
+                  forceUdpate();
+                }}
+              />
+            </LineStackLayout>
+          </ResponsiveLineStackLayout>
+        </Column>
       )}
     </Column>
   );

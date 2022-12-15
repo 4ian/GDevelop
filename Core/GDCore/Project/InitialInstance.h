c6e09b0f1fb46c3f5683c9a5c98454ed3390ba19
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2022-12-15 10:28:05 +0100

    Improve object number properties by hiding unnecessary decimals

--- Core/GDCore/Project/InitialInstance.h
+++ Core/GDCore/Project/InitialInstance.h
@@ -7,13 +7,14 @@
 #ifndef GDCORE_INITIALINSTANCE_H
 #define GDCORE_INITIALINSTANCE_H
 #include <map>
+
 #include "GDCore/Project/VariablesContainer.h"
 #include "GDCore/String.h"
 namespace gd {
 class PropertyDescriptor;
 class Project;
 class Layout;
-}
+}  // namespace gd
 
 namespace gd {
 
@@ -127,7 +128,8 @@ class GD_CORE_API InitialInstance {
   void SetCustomHeight(double height_) { height = height_; }
 
   /**
-   * \brief Return true if the instance is locked and cannot be moved in the IDE.
+   * \brief Return true if the instance is locked and cannot be moved in the
+   * IDE.
    */
   bool IsLocked() const { return locked; };
 
@@ -240,7 +242,7 @@ class GD_CORE_API InitialInstance {
    * \brief Set the value of a string property stored in the instance.
    */
   void SetRawStringProperty(const gd::String& name, const gd::String& value);
-///@}
+  ///@}
 
   /** \name Saving and loading
    * Members functions related to serialization.
@@ -272,18 +274,19 @@ class GD_CORE_API InitialInstance {
       stringProperties;  ///< More data which can be used by the object
 
   gd::String objectName;  ///< Object name
-  double x;                ///< Object initial X position
-  double y;                ///< Object initial Y position
-  double angle;            ///< Object initial angle
+  double x;               ///< Object initial X position
+  double y;               ///< Object initial Y position
+  double angle;           ///< Object initial angle
   int zOrder;             ///< Object initial Z order
   gd::String layer;       ///< Object initial layer
   bool personalizedSize;  ///< True if object has a custom size
-  double width;            ///< Object custom width
-  double height;           ///< Object custom height
+  double width;           ///< Object custom width
+  double height;          ///< Object custom height
   gd::VariablesContainer initialVariables;  ///< Instance specific variables
   bool locked;                              ///< True if the instance is locked
   bool sealed;                              ///< True if the instance is sealed
-  mutable gd::String persistentUuid; ///< A persistent random version 4 UUID, useful for hot reloading.
+  mutable gd::String persistentUuid;  ///< A persistent random version 4 UUID,
+                                      ///< useful for hot reloading.
 
   static gd::String*
       badStringProperyValue;  ///< Empty string returned by GetRawStringProperty

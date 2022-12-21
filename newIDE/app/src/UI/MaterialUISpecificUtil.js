dbfef27d2edef38338210a10a1e7608dd02cdfa7
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2022-12-21 11:08:24 +0100

    Fix leaderboard admin closing when trying to remove a leaderboard (#4691)

--- newIDE/app/src/UI/MaterialUISpecificUtil.js
+++ newIDE/app/src/UI/MaterialUISpecificUtil.js
@@ -49,3 +49,19 @@ export const getMuiCheckboxValue = (element: HTMLElement): boolean => {
   }
   return true;
 };
+
+export const doesPathContainDialog = (path: Array<Element>): boolean => {
+  // Dialogs root elements are directly placed in the body element.
+  // So the path is global > document > html > body > dialog.
+  try {
+    return isElementADialog(path[path.length - 5], { isVisible: true });
+  } catch (error) {
+    console.error(
+      `An error occurred when determining if path ${path.join(
+        ' > '
+      )} leads to a dialog`,
+      error
+    );
+    return false;
+  }
+};

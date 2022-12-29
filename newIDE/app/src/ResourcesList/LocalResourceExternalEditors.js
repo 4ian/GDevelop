bcd3009e70d59352af82efeb3200030d80ecfc50
Florian Rival <Florian.Rival@gmail.com>
2022-12-29 12:35:53 +0100

    Refactor external editors to support cloud projects

--- newIDE/app/src/ResourcesList/LocalResourceExternalEditors.js
+++ newIDE/app/src/ResourcesList/LocalResourceExternalEditors.js
@@ -1,11 +1,261 @@
 // @flow
-import { openPiskel } from './LocalPiskelBridge';
-import { openJfxr } from './LocalJfxrBridge';
-import { openYarn } from './LocalYarnBridge';
-import { type ResourceExternalEditor } from './ResourceExternalEditor.flow';
+import {
+  type ResourceExternalEditor,
+  type ExternalEditorInput,
+  type ExternalEditorOutput,
+  type ExternalEditorBase64Resource,
+} from './ResourceExternalEditor.flow';
 import { sendExternalEditorOpened } from '../Utils/Analytics/EventSender';
 import { t } from '@lingui/macro';
-import Window from '../Utils/Window';
+import optionalRequire from '../Utils/OptionalRequire';
+import { isBlobURL, isURL } from './ResourceUtils';
+import { downloadUrlsToBlobs, type ItemResult } from '../Utils/BlobDownloader';
+import { createNewResource, type ResourceKind } from './ResourceSource';
+import newNameGenerator from '../Utils/NewNameGenerator';
+const path = optionalRequire('path');
+const fs = optionalRequire('fs');
+const electron = optionalRequire('electron');
+const ipcRenderer = electron ? electron.ipcRenderer : null;
+
+/**
+ * Ask Electron main process to open a window for the specified editor,
+ * then wait for the editor to be closed and pass the results.
+ */
+const openAndWaitForExternalEditorWindow = async (
+  editorName: 'piskel' | 'yarn' | 'jfxr',
+  externalEditorInput: ExternalEditorInput
+): Promise<ExternalEditorOutput> => {
+  if (!ipcRenderer) throw new Error('Not supported.');
+  return new Promise(resolve => {
+    ipcRenderer.removeAllListeners(`${editorName}-closed`);
+    ipcRenderer.on(`${editorName}-closed`, (event, externalEditorData) => {
+      resolve(externalEditorData);
+    });
+
+    ipcRenderer.send(`${editorName}-load`, externalEditorInput);
+  });
+};
+
+// TODO: move in some utils?
+function convertBlobToDataURL(blob: Blob): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    // $FlowFixMe - it's guaranted for reader.result to be a string.
+    reader.onload = _e => resolve(reader.result);
+    reader.onerror = _e => reject(reader.error);
+    reader.onabort = _e => reject(new Error('Read aborted'));
+    reader.readAsDataURL(blob);
+  });
+}
+
+// TODO: move in some utils?
+function convertDataURLtoBlob(dataUrl: string): ?Blob {
+  const arr = dataUrl.split(',');
+  if (arr.length < 2) return null;
+
+  const mimeMatchResults = arr[0].match(/:(.*?);/);
+  const mime = mimeMatchResults
+    ? mimeMatchResults[1]
+    : 'application/octet-stream';
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], { type: mime });
+}
+
+// TODO: quickly test this.
+const downloadAndPrepareExternalEditorBase64Resources = async ({
+  project,
+  resourceNames,
+}: {|
+  project: gdProject,
+  resourceNames: Array<string>,
+|}) => {
+  type ResourceToDownload = {|
+    resourceName: string,
+    url: string,
+  |};
+
+  type ResourceToReadLocally = {|
+    resourceName: string,
+    localFilePath: string,
+  |};
+
+  const projectPath = path.dirname(project.getProjectFile());
+  const urlsToDownload: Array<ResourceToDownload> = [];
+  const filesToRead: Array<ResourceToReadLocally> = [];
+  const resourcesManager = project.getResourcesManager();
+  resourceNames.forEach(resourceName => {
+    if (!resourcesManager.hasResource(resourceName)) return;
+
+    const resource = resourcesManager.getResource(resourceName);
+    const url = resource.getFile();
+    if (isURL(url)) {
+      if (isBlobURL(url)) {
+        console.error('Unsupported blob URL for a resource - ignoring it.');
+      } else {
+        urlsToDownload.push({
+          url,
+          resourceName,
+        });
+      }
+    } else {
+      // This resource is a local file.
+      filesToRead.push({
+        localFilePath: url,
+        resourceName,
+      });
+    }
+  });
+
+  const downloadedBlobs: Array<
+    ItemResult<ResourceToDownload>
+  > = await downloadUrlsToBlobs({
+    urlContainers: urlsToDownload,
+    onProgress: (count, total) => {},
+  });
+
+  const resourcesToDataUrl = new Map<
+    string,
+    {|
+      dataUrl: string,
+      localFilePath?: string,
+    |}
+  >();
+  await Promise.all(
+    downloadedBlobs.map(async ({ error, blob, item }) => {
+      if (blob) {
+        try {
+          resourcesToDataUrl.set(item.resourceName, {
+            dataUrl: await convertBlobToDataURL(blob),
+          });
+        } catch (error) {
+          console.error(
+            `Unable to read data from resource "${
+              item.resourceName
+            }" - ignoring it.`,
+            error
+          );
+        }
+      }
+    })
+  );
+  await Promise.all(
+    filesToRead.map(async ({ localFilePath, resourceName }) => {
+      try {
+        const content = await fs.promises.readFile(
+          path.resolve(projectPath, localFilePath)
+        );
+
+        resourcesToDataUrl.set(resourceName, {
+          localFilePath,
+          dataUrl: 'data:text/plain;base64,' + content.toString('base64'),
+        });
+      } catch (error) {
+        console.error(
+          `Unable to read local file "${localFilePath}" - ignoring it.`,
+          error
+        );
+      }
+    })
+  );
+
+  return resourceNames.map(resourceName => {
+    const resourceData = resourcesToDataUrl.get(resourceName);
+    if (!resourceData)
+      return {
+        name: resourceName,
+        dataUrl: '',
+      };
+
+    const { localFilePath, dataUrl } = resourceData;
+    return {
+      name: resourceName,
+      dataUrl,
+      localFilePath,
+    };
+  });
+};
+
+const saveBlobUrlsFromExternalEditorBase64Resources = async ({
+  project,
+  baseNameForNewResources,
+  resources,
+  resourceKind,
+}: {|
+  project: gdProject,
+  baseNameForNewResources: string,
+  resources: Array<ExternalEditorBase64Resource>,
+  resourceKind: ResourceKind,
+|}): Promise<
+  Array<{|
+    name: string,
+    blobUrl: string,
+    originalIndex?: ?number,
+  |}>
+> => {
+  const resourcesManager = project.getResourcesManager();
+  const blobs = resources.map(
+    ({ name, dataUrl, localFilePath, extension, originalIndex }) => {
+      const blob = convertDataURLtoBlob(dataUrl);
+      const blobUrl = blob ? URL.createObjectURL(blob) : '';
+
+      if (!name || !resourcesManager.hasResource(name)) {
+        // Insert a new resource.
+        const name = newNameGenerator(baseNameForNewResources, name =>
+          resourcesManager.hasResource(name)
+        );
+        console.info('Creating new resource ' + name + '...');
+        const newResource = createNewResource(resourceKind);
+        if (newResource) {
+          newResource.setName(name);
+          newResource.setFile(blobUrl);
+          newResource.setMetadata(
+            JSON.stringify({
+              localFilePath,
+              extension,
+            })
+          );
+          resourcesManager.addResource(newResource);
+          newResource.delete();
+        } else {
+          console.error(
+            'Could not create a resource for kind: ' + resourceKind
+          );
+        }
+
+        return {
+          name,
+          originalIndex,
+          blobUrl,
+        };
+      } else {
+        console.info('Updating resource ' + name + '.');
+        const resource = resourcesManager.getResource(name);
+        resource.setFile(blobUrl);
+
+        // TODO: avoid overwriting the rest of the metadata?
+        resource.setMetadata(
+          JSON.stringify({
+            localFilePath,
+            extension,
+          })
+        );
+
+        return {
+          name,
+          originalIndex,
+          blobUrl,
+        };
+      }
+    }
+  );
+
+  return blobs;
+};
 
 /**
  * This is the list of editors that can be used to edit resources
@@ -17,17 +267,60 @@ const editors: Array<ResourceExternalEditor> = [
     createDisplayName: t`Create with Piskel`,
     editDisplayName: t`Edit with Piskel`,
     kind: 'image',
-    edit: options => {
-      const storageProvider = options.getStorageProvider();
-      if (storageProvider.internalName !== 'LocalFile') {
-        Window.showMessageBox(
-          'Piskel is only supported when your project is saved locally. It will be available for Cloud projects in a future version. To use Piskel, save your project on your computer first.'
+    edit: async options => {
+      sendExternalEditorOpened('piskel');
+      const { project, resourceNames, resourceManagementProps } = options;
+
+      const resources = await downloadAndPrepareExternalEditorBase64Resources({
+        project,
+        resourceNames,
+      });
+      const externalEditorInput: ExternalEditorInput = {
+        // TODO: remove "extraOptions".
+        singleFrame: options.singleFrame,
+        // TODO: adapt to read metadata and merge.
+        externalEditorData: options.extraOptions.externalEditorData,
+        fps: options.extraOptions.fps,
+        isLooping: options.extraOptions.isLooping,
+        name: options.extraOptions.name || resourceNames[0] || 'New resource',
+        resources,
+      };
+      const externalEditorOutput: ExternalEditorOutput = await openAndWaitForExternalEditorWindow(
+        'piskel',
+        externalEditorInput
+      );
+      const outputResources = await saveBlobUrlsFromExternalEditorBase64Resources(
+        {
+          baseNameForNewResources: externalEditorOutput.baseNameForNewResources,
+          project,
+          resources: externalEditorOutput.resources,
+          resourceKind: 'image',
+        }
+      );
+      try {
+        await resourceManagementProps.onFetchNewlyAddedResources();
+      } catch (error) {
+        console.error(
+          'An error happened while fetching the newly added resources:',
+          error
         );
-        return;
       }
 
-      sendExternalEditorOpened('piskel');
-      return openPiskel(options);
+      outputResources.forEach(({ blobUrl }) => {
+        // Free the blob urls that were created just to allow the resources
+        // to be "fetched" to their final destination by `onFetchNewlyAddedResources`.
+        URL.revokeObjectURL(blobUrl);
+
+        // TODO: put again/add metadata to the resources.
+      });
+
+      return {
+        resources: outputResources.map(({ name, originalIndex }) => ({
+          name,
+          originalIndex,
+        })),
+        externalEditorData: externalEditorOutput.externalEditorData,
+      };
     },
   },
   {
@@ -35,17 +328,59 @@ const editors: Array<ResourceExternalEditor> = [
     createDisplayName: t`Create with Jfxr`,
     editDisplayName: t`Edit with Jfxr`,
     kind: 'audio',
-    edit: options => {
-      const storageProvider = options.getStorageProvider();
-      if (storageProvider.internalName !== 'LocalFile') {
-        Window.showMessageBox(
-          'Jfxr is only supported when your project is saved locally. It will be available for Cloud projects in a future version. To use Jfxr, save your project on your computer first.'
+    edit: async options => {
+      sendExternalEditorOpened('jfxr');
+      const { project, resourceNames, resourceManagementProps } = options;
+
+      const resources = await downloadAndPrepareExternalEditorBase64Resources({
+        project,
+        resourceNames,
+      });
+      const externalEditorInput: ExternalEditorInput = {
+        // TODO: remove "extraOptions".
+        singleFrame: options.singleFrame,
+        // TODO: adapt to read metadata and merge.
+        externalEditorData: options.extraOptions.externalEditorData,
+        fps: options.extraOptions.fps,
+        isLooping: options.extraOptions.isLooping,
+        name: options.extraOptions.name || resourceNames[0] || 'New resource',
+        resources,
+      };
+      const externalEditorOutput = await openAndWaitForExternalEditorWindow(
+        'jfxr',
+        externalEditorInput
+      );
+      const outputResources = await saveBlobUrlsFromExternalEditorBase64Resources(
+        {
+          baseNameForNewResources: externalEditorOutput.baseNameForNewResources,
+          project,
+          resources: externalEditorOutput.resources,
+          resourceKind: 'audio',
+        }
+      );
+      try {
+        await resourceManagementProps.onFetchNewlyAddedResources();
+      } catch (error) {
+        console.error(
+          'An error happened while fetching the newly added resources:',
+          error
         );
-        return;
       }
 
-      sendExternalEditorOpened('jfxr');
-      return openJfxr(options);
+      outputResources.forEach(({ blobUrl }) => {
+        // Free the blob urls that were created just to allow the resources
+        // to be "fetched" to their final destination by `onFetchNewlyAddedResources`.
+        URL.revokeObjectURL(blobUrl);
+
+        // TODO: put again/add metadata to the resources.
+      });
+      return {
+        resources: outputResources.map(({ name, originalIndex }) => ({
+          name,
+          originalIndex,
+        })),
+        externalEditorData: externalEditorOutput.externalEditorData,
+      };
     },
   },
   {
@@ -53,17 +388,59 @@ const editors: Array<ResourceExternalEditor> = [
     createDisplayName: t`Create with Yarn`,
     editDisplayName: t`Edit with Yarn`,
     kind: 'json',
-    edit: options => {
-      const storageProvider = options.getStorageProvider();
-      if (storageProvider.internalName !== 'LocalFile') {
-        Window.showMessageBox(
-          'Yarn is only supported when your project is saved locally. It will be available for Cloud projects in a future version. To use Yarn, save your project on your computer first.'
+    edit: async options => {
+      sendExternalEditorOpened('yarn');
+      const { project, resourceNames, resourceManagementProps } = options;
+
+      const resources = await downloadAndPrepareExternalEditorBase64Resources({
+        project,
+        resourceNames,
+      });
+      const externalEditorInput: ExternalEditorInput = {
+        // TODO: remove "extraOptions".
+        singleFrame: options.singleFrame,
+        // TODO: adapt to read metadata and merge.
+        externalEditorData: options.extraOptions.externalEditorData,
+        fps: options.extraOptions.fps,
+        isLooping: options.extraOptions.isLooping,
+        name: options.extraOptions.name || resourceNames[0] || 'New resource',
+        resources,
+      };
+      const externalEditorOutput = await openAndWaitForExternalEditorWindow(
+        'yarn',
+        externalEditorInput
+      );
+      const outputResources = await saveBlobUrlsFromExternalEditorBase64Resources(
+        {
+          baseNameForNewResources: externalEditorOutput.baseNameForNewResources,
+          project,
+          resources: externalEditorOutput.resources,
+          resourceKind: 'json',
+        }
+      );
+      try {
+        await resourceManagementProps.onFetchNewlyAddedResources();
+      } catch (error) {
+        console.error(
+          'An error happened while fetching the newly added resources:',
+          error
         );
-        return;
       }
 
-      sendExternalEditorOpened('yarn');
-      return openYarn(options);
+      outputResources.forEach(({ blobUrl }) => {
+        // Free the blob urls that were created just to allow the resources
+        // to be "fetched" to their final destination by `onFetchNewlyAddedResources`.
+        URL.revokeObjectURL(blobUrl);
+
+        // TODO: put again/add metadata to the resources.
+      });
+      return {
+        resources: outputResources.map(({ name, originalIndex }) => ({
+          name,
+          originalIndex,
+        })),
+        externalEditorData: externalEditorOutput.externalEditorData,
+      };
     },
   },
 ];

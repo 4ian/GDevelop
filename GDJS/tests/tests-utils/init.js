3f5b82a2f4c0ae6a949e703716036f7293ad3eb5
Florian Rival <Florian.Rival@gmail.com>
2023-01-10 10:33:35 +0100

    Add two expressions to replace one or all occurrences of a text in another

--- GDJS/tests/tests-utils/init.js
+++ GDJS/tests/tests-utils/init.js
@@ -4,3 +4,4 @@
 // Disable a few logs that are too verbose:
 gdjs.Logger.getDefaultConsoleLoggerOutput().discardGroup('Firebase (setup)');
 gdjs.Logger.getDefaultConsoleLoggerOutput().discardGroup('RuntimeScene (setup warnings)');
+gdjs.Logger.getDefaultConsoleLoggerOutput().discardGroup('Player Authentication');

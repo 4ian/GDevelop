debcd3045f83ed7a3820b78e0ee6b2827d7e3d6a
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2023-01-16 14:37:46 +0100

    Add autocompletions for some fields that can contain object groups (#4831)
    * Effect name
    * Effect parameter name
    * Sprite point name

--- newIDE/app/src/EventsSheet/ParameterFields/ObjectEffectNameField.js
+++ newIDE/app/src/EventsSheet/ParameterFields/ObjectEffectNameField.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import intersection from 'lodash/intersection';
 import GenericExpressionField from './GenericExpressionField';
 import {
   type ParameterFieldProps,
@@ -9,6 +10,8 @@ import { type ExpressionAutocompletion } from '../../ExpressionAutocompletion';
 import getObjectByName from '../../Utils/GetObjectByName';
 import { getLastObjectParameterValue } from './ParameterMetadataTools';
 import { enumerateEffectNames } from '../../EffectsList/EnumerateEffects';
+import getObjectGroupByName from '../../Utils/GetObjectGroupByName';
+import { mapVector } from '../../Utils/MapFor';
 
 export default React.forwardRef<ParameterFieldProps, ParameterFieldInterface>(
   function ObjectEffectNameField(props: ParameterFieldProps, ref) {
@@ -30,28 +33,51 @@ export default React.forwardRef<ParameterFieldProps, ParameterFieldInterface>(
     } = props;
 
     const getEffectNames = (): Array<ExpressionAutocompletion> => {
-      const objectName = getLastObjectParameterValue({
+      const objectOrGroupName = getLastObjectParameterValue({
         instructionMetadata,
         instruction,
         expressionMetadata,
         expression,
         parameterIndex,
       });
-      if (!objectName || !project) {
+      if (!objectOrGroupName || !project) {
         return [];
       }
 
-      const object = getObjectByName(project, scope.layout, objectName);
-      if (!object) {
-        return [];
+      const object = getObjectByName(project, scope.layout, objectOrGroupName);
+      if (object) {
+        return enumerateEffectNames(object.getEffects())
+          .sort()
+          .map(effectName => ({
+            kind: 'Text',
+            completion: `"${effectName}"`,
+          }));
+      }
+      const group = getObjectGroupByName(
+        project,
+        scope.layout,
+        objectOrGroupName
+      );
+      if (group) {
+        const effectsNamesByObject: string[][] = mapVector(
+          group.getAllObjectsNames(),
+          objectName => {
+            const object = getObjectByName(project, scope.layout, objectName);
+            if (!object) {
+              return null;
+            }
+            return enumerateEffectNames(object.getEffects());
+          }
+        ).filter(Boolean);
+        return intersection(...effectsNamesByObject)
+          .sort()
+          .map(effectName => ({
+            kind: 'Text',
+            completion: `"${effectName}"`,
+          }));
       }
 
-      return enumerateEffectNames(object.getEffects())
-        .sort()
-        .map(effectName => ({
-          kind: 'Text',
-          completion: `"${effectName}"`,
-        }));
+      return [];
     };
 
     return (
--- newIDE/app/src/EventsSheet/ParameterFields/ObjectPointNameField.js
+++ newIDE/app/src/EventsSheet/ParameterFields/ObjectPointNameField.js
@@ -1,11 +1,14 @@
 // @flow
 import React, { Component } from 'react';
+import intersection from 'lodash/intersection';
 import GenericExpressionField from './GenericExpressionField';
 import { type ParameterFieldProps } from './ParameterFieldCommons';
 import { type ExpressionAutocompletion } from '../../ExpressionAutocompletion';
 import getObjectByName from '../../Utils/GetObjectByName';
 import { getLastObjectParameterValue } from './ParameterMetadataTools';
 import { getAllPointNames } from '../../ObjectEditor/Editors/SpriteEditor/Utils/SpriteObjectHelper';
+import getObjectGroupByName from '../../Utils/GetObjectGroupByName';
+import { mapVector } from '../../Utils/MapFor';
 
 const gd: libGDevelop = global.gd;
 
@@ -30,32 +33,64 @@ export default class ObjectPointNameField extends Component<
       parameterIndex,
     } = this.props;
 
-    const objectName = getLastObjectParameterValue({
+    const objectOrGroupName = getLastObjectParameterValue({
       instructionMetadata,
       instruction,
       expressionMetadata,
       expression,
       parameterIndex,
     });
-    if (!objectName || !project) {
+    if (!objectOrGroupName || !project) {
       return [];
     }
 
-    const object = getObjectByName(project, scope.layout, objectName);
-    if (!object) {
-      return [];
-    }
-
-    if (object.getType() === 'Sprite') {
+    const object = getObjectByName(project, scope.layout, objectOrGroupName);
+    if (object && object.getType() === 'Sprite') {
       const spriteConfiguration = gd.asSpriteConfiguration(
         object.getConfiguration()
       );
 
       return getAllPointNames(spriteConfiguration)
-        .map(spriteObjectName =>
-          spriteObjectName.length > 0 ? spriteObjectName : null
-        )
+        .map(pointName => (pointName.length > 0 ? pointName : null))
         .filter(Boolean)
+        .sort()
+        .map(pointName => ({
+          kind: 'Text',
+          completion: `"${pointName}"`,
+        }));
+    }
+
+    const group = getObjectGroupByName(
+      project,
+      scope.layout,
+      objectOrGroupName
+    );
+    if (group) {
+      // If the instruction targets a group, we check that every object of the
+      // group is a sprite and get the points that they all have in common.
+      const pointsNamesByObject = mapVector(
+        group.getAllObjectsNames(),
+        objectName => {
+          const object = getObjectByName(project, scope.layout, objectName);
+          if (!object || object.getType() !== 'Sprite') {
+            return null;
+          }
+          const spriteConfiguration = gd.asSpriteConfiguration(
+            object.getConfiguration()
+          );
+
+          return getAllPointNames(spriteConfiguration)
+            .map(pointName => (pointName.length > 0 ? pointName : null))
+            .filter(Boolean);
+        }
+      );
+
+      if (pointsNamesByObject.some(pointsNames => !pointsNames)) return [];
+
+      // Flow fears that pointsNamesByObject contains null values but this
+      // possibility should be handled above.
+      // $FlowExpectedError[incompatible-call]
+      return intersection<string>(...pointsNamesByObject)
         .sort()
         .map(pointName => ({
           kind: 'Text',

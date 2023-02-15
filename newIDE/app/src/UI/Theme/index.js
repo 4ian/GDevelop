de50dc7967d96e0089a243f0604465d981bbf893
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2023-02-15 15:39:58 +0100

    Use safe area to correctly display a few elements (#4959)
    Don't show in changelog

--- newIDE/app/src/UI/Theme/index.js
+++ newIDE/app/src/UI/Theme/index.js
@@ -10,6 +10,7 @@ import 'react-virtualized/styles.css';
 // Styles
 import './Global/Animation.css';
 import './Global/EventsSheet.css';
+import './Global/Snackbar.css';
 import './Global/Markdown.css';
 import './Global/Scrollbar.css';
 import './Global/Mosaic.css';

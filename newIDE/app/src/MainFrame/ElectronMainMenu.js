a6e7dd6160101df11d8d48ae87386ed25923fb1c
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2023-03-10 11:33:55 +0100

    Add possibility to close preview window with Cmd/Ctrl + W (#5088)

--- newIDE/app/src/MainFrame/ElectronMainMenu.js
+++ newIDE/app/src/MainFrame/ElectronMainMenu.js
@@ -9,18 +9,58 @@ import {
   type MainMenuEvent,
 } from './MainMenu';
 const electron = optionalRequire('electron');
+const remote = optionalRequire('@electron/remote');
+const app = remote ? remote.app : null;
 const ipcRenderer = electron ? electron.ipcRenderer : null;
 
 // Custom hook to register and deregister IPC listener
-const useIPCEventListener = (ipcEvent: MainMenuEvent, func) => {
+const useIPCEventListener = ({
+  ipcEvent,
+  callback,
+  shouldApply,
+}: {
+  ipcEvent: MainMenuEvent,
+  callback: Function,
+  shouldApply: boolean,
+}) => {
   React.useEffect(
     () => {
-      if (!ipcRenderer) return;
-      const handler = (event, ...eventArgs) => func(...eventArgs);
+      if (!ipcRenderer || !shouldApply) return;
+
+      const handler = (event, ...eventArgs) => callback(...eventArgs);
       ipcRenderer.on(ipcEvent, handler);
       return () => ipcRenderer.removeListener(ipcEvent, handler);
     },
-    [ipcEvent, func]
+    [ipcEvent, callback, shouldApply]
+  );
+};
+
+const useAppEventListener = ({
+  event,
+  callback,
+}: {
+  event: string,
+  callback: Function,
+}) => {
+  React.useEffect(
+    () => {
+      if (!app) return;
+      const handler = (event, ...eventArgs) => callback(...eventArgs);
+      app.on(event, handler);
+      return () => app.removeListener(event, handler);
+    },
+    [event, callback]
+  );
+};
+
+const isMainWindow = (windowTitle: string): boolean => {
+  if (!windowTitle) return false;
+  const lowercaseTitle = windowTitle.toLowerCase();
+  return (
+    lowercaseTitle.startsWith('gdevelop') &&
+    lowercaseTitle !== 'gdevelop dialogue tree editor (yarn)' &&
+    lowercaseTitle !== 'gdevelop sound effects editor (jfxr)' &&
+    lowercaseTitle !== 'gdevelop image editor (piskel)'
   );
 };
 
@@ -36,36 +76,115 @@ const ElectronMainMenu = ({
 |}) => {
   const { i18n, project, recentProjectFiles, shortcutMap } = props;
   const language = i18n.language;
+  const [
+    isFocusedOnMainWindow,
+    setIsFocusedOnMainWindow,
+  ] = React.useState<boolean>(true);
+  useAppEventListener({
+    event: 'browser-window-focus',
+    callback: window => {
+      setIsFocusedOnMainWindow(isMainWindow(window.title));
+    },
+  });
+  useAppEventListener({
+    event: 'browser-window-blur',
+    callback: window => {
+      setIsFocusedOnMainWindow(!isMainWindow(window.title));
+    },
+  });
 
   // We could use a for loop, but for safety let's write every hook one by
   // one to avoid any change at runtime which would break the rules of hooks.
-  useIPCEventListener('main-menu-open', callbacks.onChooseProject);
-  useIPCEventListener('main-menu-open-recent', callbacks.onOpenRecentFile);
-  useIPCEventListener('main-menu-save', callbacks.onSaveProject);
-  useIPCEventListener('main-menu-save-as', callbacks.onSaveProjectAs);
-  useIPCEventListener('main-menu-close', callbacks.onCloseProject);
-  useIPCEventListener('main-menu-close-app', callbacks.onCloseApp);
-  useIPCEventListener('main-menu-export', callbacks.onExportProject);
-  useIPCEventListener('main-menu-create-template', callbacks.onCreateProject);
-  useIPCEventListener('main-menu-create-blank', callbacks.onCreateBlank);
-  useIPCEventListener(
-    'main-menu-open-project-manager',
-    callbacks.onOpenProjectManager
-  );
-  useIPCEventListener('main-menu-open-home-page', callbacks.onOpenHomePage);
-  useIPCEventListener('main-menu-open-debugger', callbacks.onOpenDebugger);
-  useIPCEventListener('main-menu-open-about', callbacks.onOpenAbout);
-  useIPCEventListener(
-    'main-menu-open-preferences',
-    callbacks.onOpenPreferences
-  );
-  useIPCEventListener('main-menu-open-language', callbacks.onOpenLanguage);
-  useIPCEventListener('main-menu-open-profile', callbacks.onOpenProfile);
-  useIPCEventListener(
-    'main-menu-open-games-dashboard',
-    callbacks.onOpenGamesDashboard
-  );
-  useIPCEventListener('update-status', callbacks.setElectronUpdateStatus);
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open',
+    callback: callbacks.onChooseProject,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-recent',
+    callback: callbacks.onOpenRecentFile,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-save',
+    callback: callbacks.onSaveProject,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-save-as',
+    callback: callbacks.onSaveProjectAs,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-close',
+    callback: callbacks.onCloseProject,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-close-app',
+    callback: callbacks.onCloseApp,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-export',
+    callback: callbacks.onExportProject,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-create-template',
+    callback: callbacks.onCreateProject,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-create-blank',
+    callback: callbacks.onCreateBlank,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-project-manager',
+    callback: callbacks.onOpenProjectManager,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-home-page',
+    callback: callbacks.onOpenHomePage,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-debugger',
+    callback: callbacks.onOpenDebugger,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-about',
+    callback: callbacks.onOpenAbout,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-preferences',
+    callback: callbacks.onOpenPreferences,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-language',
+    callback: callbacks.onOpenLanguage,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-profile',
+    callback: callbacks.onOpenProfile,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'main-menu-open-games-dashboard',
+    callback: callbacks.onOpenGamesDashboard,
+    shouldApply: isFocusedOnMainWindow,
+  });
+  useIPCEventListener({
+    ipcEvent: 'update-status',
+    callback: callbacks.setElectronUpdateStatus,
+    shouldApply: true, // Keep logic around app update even if on preview window
+  });
 
   React.useEffect(
     () => {

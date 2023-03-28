878414a2378e9c3e069a7480fb67c6d026f4a695
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2023-03-28 12:02:06 +0200

    Fix typo

--- newIDE/app/src/AssetStore/ExampleStore/ExampleStoreContext.js
+++ newIDE/app/src/AssetStore/ExampleStore/ExampleStoreContext.js
@@ -91,7 +91,7 @@ export const ExampleStoreStateProvider = ({
 
           console.info(
             `Loaded ${
-              exampleShortHeaders.length ? exampleShortHeaders.length : 0
+              exampleShortHeaders ? exampleShortHeaders.length : 0
             } examples from the example store.`
           );
           setExampleShortHeadersById(exampleShortHeadersById);

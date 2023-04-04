37c765758434aeeaf08c7b31bf77ef13c00b83bd
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-04-04 12:04:52 +0200

    Open or download resource file of cloud project depending on the platform

--- newIDE/app/src/ProjectsStorage/CloudStorageProvider/CloudProjectResourcesHandler.js
+++ newIDE/app/src/ProjectsStorage/CloudStorageProvider/CloudProjectResourcesHandler.js
@@ -2,20 +2,56 @@
 import { t } from '@lingui/macro';
 import { type AuthenticatedUser } from '../../Profile/AuthenticatedUserContext';
 import { type ResourcesActionsProps } from '../../ProjectsStorage';
+import optionalRequire from '../../Utils/OptionalRequire';
+import { getCredentialsForCloudProject } from '../../Utils/GDevelopServices/Project';
+import { downloadUrlsToLocalFiles } from '../../Utils/LocalFileDownloader';
+import { checkIfIsGDevelopCloudBucketUrl } from '../../Utils/CrossOrigin';
+import Window from '../../Utils/Window';
+import ResourcesLoader from '../../ResourcesLoader';
+
+const path = optionalRequire('path');
+const remote = optionalRequire('@electron/remote');
+const app = remote ? remote.app : null;
 
 export const generateGetResourceActions = ({
   authenticatedUser,
 }: {
   authenticatedUser: AuthenticatedUser,
-}) => ({ project, resource, i18n }: ResourcesActionsProps) => {
+}) => ({ project, resource, i18n, fileMetadata }: ResourcesActionsProps) => {
+  const openLabel = app && path ? t`Download` : t`Open resource in navigator`;
   return [
     {
-      label: i18n._(t`Open resource in navigator`),
+      label: i18n._(openLabel),
       click: async () => {
-        // Get token
-        // Get resource url
-        // Append token to url
-        // Open external url
+        const resourceUrl = ResourcesLoader.getResourceFullUrl(
+          project,
+          resource.getName(),
+          {}
+        );
+        if (checkIfIsGDevelopCloudBucketUrl(resourceUrl)) {
+          await getCredentialsForCloudProject(
+            authenticatedUser,
+            fileMetadata.fileIdentifier
+          );
+        }
+        if (app && path) {
+          const targetPath = path.join(
+            app.getPath('downloads'),
+            resource.getName()
+          );
+          await downloadUrlsToLocalFiles({
+            urlContainers: [
+              {
+                url: resourceUrl,
+                filePath: targetPath,
+              },
+            ],
+            onProgress: () => {},
+            throwIfAnyError: false,
+          });
+        } else {
+          Window.openExternalURL(resourceUrl);
+        }
       },
     },
   ];

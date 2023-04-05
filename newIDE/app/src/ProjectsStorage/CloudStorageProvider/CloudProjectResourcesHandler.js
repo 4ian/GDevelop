655bc0fe79bcde21a84523e1918d60c38c43cb2c
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-04-05 17:42:41 +0200

    Add second option to download resource under a user chosen path

--- newIDE/app/src/ProjectsStorage/CloudStorageProvider/CloudProjectResourcesHandler.js
+++ newIDE/app/src/ProjectsStorage/CloudStorageProvider/CloudProjectResourcesHandler.js
@@ -27,47 +27,77 @@ export const generateGetResourceActions = ({
   fileMetadata,
   informUser,
 }: ResourcesActionsProps) => {
-  const openLabel = app && path ? t`Download` : t`Open resource in browser`;
-  return [
+  const openLabel =
+    app && path
+      ? t`Download under "Downloads" folder`
+      : t`Open resource in browser`;
+
+  const openOrDownloadResource = async ({
+    askUserForDownloadDestination,
+  }: {
+    askUserForDownloadDestination: boolean,
+  }) => {
+    const resourceUrl = ResourcesLoader.getResourceFullUrl(
+      project,
+      resource.getName(),
+      {}
+    );
+    if (checkIfIsGDevelopCloudBucketUrl(resourceUrl)) {
+      await getCredentialsForCloudProject(
+        authenticatedUser,
+        fileMetadata.fileIdentifier
+      );
+    }
+    if (app && path && electron) {
+      const defaultPath = path.join(
+        app.getPath('downloads'),
+        resource.getName()
+      );
+      let targetPath;
+      if (askUserForDownloadDestination) {
+        targetPath = remote.dialog.showSaveDialogSync(null, {
+          defaultPath: defaultPath,
+          properties: ['createDirectory', 'showOverwriteConfirmation'],
+        });
+        if (!targetPath) return;
+      } else {
+        targetPath = defaultPath;
+      }
+      await downloadUrlsToLocalFiles({
+        urlContainers: [
+          {
+            url: resourceUrl,
+            filePath: targetPath,
+          },
+        ],
+        onProgress: () => {},
+        throwIfAnyError: false,
+      });
+      informUser({
+        actionLabel: <Trans>Open folder</Trans>,
+        message: <Trans>The resource has been downloaded</Trans>,
+        onActionClick: () =>
+          electron.shell.showItemInFolder(path.resolve(targetPath)),
+      });
+    } else {
+      Window.openExternalURL(resourceUrl);
+    }
+  };
+
+  const actions = [
     {
       label: i18n._(openLabel),
-      click: async () => {
-        const resourceUrl = ResourcesLoader.getResourceFullUrl(
-          project,
-          resource.getName(),
-          {}
-        );
-        if (checkIfIsGDevelopCloudBucketUrl(resourceUrl)) {
-          await getCredentialsForCloudProject(
-            authenticatedUser,
-            fileMetadata.fileIdentifier
-          );
-        }
-        if (app && path && electron) {
-          const targetPath = path.join(
-            app.getPath('downloads'),
-            resource.getName()
-          );
-          await downloadUrlsToLocalFiles({
-            urlContainers: [
-              {
-                url: resourceUrl,
-                filePath: targetPath,
-              },
-            ],
-            onProgress: () => {},
-            throwIfAnyError: false,
-          });
-          informUser({
-            actionLabel: <Trans>Open folder</Trans>,
-            message: <Trans>The resource has been downloaded</Trans>,
-            onActionClick: () =>
-              electron.shell.showItemInFolder(path.resolve(targetPath)),
-          });
-        } else {
-          Window.openExternalURL(resourceUrl);
-        }
-      },
+      click: () =>
+        openOrDownloadResource({ askUserForDownloadDestination: false }),
     },
   ];
+
+  if (app && path) {
+    actions.push({
+      label: i18n._(t`Download under...`),
+      click: () =>
+        openOrDownloadResource({ askUserForDownloadDestination: true }),
+    });
+  }
+  return actions;
 };

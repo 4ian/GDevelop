db2950f02eaea4cbd0f82edd5a658fb27b339abd
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2023-04-06 15:52:01 +0200

    Rework external layout and animation fields to have selects

--- newIDE/app/src/EventsSheet/ParameterFields/ObjectAnimationNameField.js
+++ newIDE/app/src/EventsSheet/ParameterFields/ObjectAnimationNameField.js
@@ -1,21 +1,31 @@
 // @flow
 import * as React from 'react';
+import { Trans, t } from '@lingui/macro';
 import GenericExpressionField from './GenericExpressionField';
 import {
   type ParameterFieldProps,
   type ParameterFieldInterface,
   type FieldFocusFunction,
 } from './ParameterFieldCommons';
-import { type ExpressionAutocompletion } from '../../ExpressionAutocompletion';
 import getObjectByName from '../../Utils/GetObjectByName';
 import { getLastObjectParameterValue } from './ParameterMetadataTools';
 import { mapFor } from '../../Utils/MapFor';
+import SelectField, { type SelectFieldInterface } from '../../UI/SelectField';
+import SelectOption from '../../UI/SelectOption';
+import { TextFieldWithButtonLayout } from '../../UI/Layout';
+import FlatButton from '../../UI/FlatButton';
+import RaisedButton from '../../UI/RaisedButton';
+import Functions from '@material-ui/icons/Functions';
+import TypeCursorSelect from '../../UI/CustomSvgIcons/TypeCursorSelect';
 
 const gd: libGDevelop = global.gd;
 
 export default React.forwardRef<ParameterFieldProps, ParameterFieldInterface>(
   function ObjectAnimationNameField(props: ParameterFieldProps, ref) {
-    const field = React.useRef<?GenericExpressionField>(null);
+    const field = React.useRef<?(
+      | GenericExpressionField
+      | SelectFieldInterface
+    )>(null);
     const focus: FieldFocusFunction = options => {
       if (field.current) field.current.focus(options);
     };
@@ -23,65 +33,159 @@ export default React.forwardRef<ParameterFieldProps, ParameterFieldInterface>(
       focus,
     }));
 
-    const getAnimationNames = (): Array<ExpressionAutocompletion> => {
-      const {
+    const {
+      project,
+      scope,
+      instructionMetadata,
+      instruction,
+      expressionMetadata,
+      expression,
+      parameterIndex,
+    } = props;
+
+    const animationNames = React.useMemo(
+      () => {
+        const objectName = getLastObjectParameterValue({
+          instructionMetadata,
+          instruction,
+          expressionMetadata,
+          expression,
+          parameterIndex,
+        });
+
+        if (!objectName || !project) {
+          return [];
+        }
+
+        const object = getObjectByName(project, scope.layout, objectName);
+        if (!object) {
+          return [];
+        }
+
+        if (object.getType() === 'Sprite') {
+          const spriteConfiguration = gd.asSpriteConfiguration(
+            object.getConfiguration()
+          );
+
+          return mapFor(0, spriteConfiguration.getAnimationsCount(), index => {
+            const animationName = spriteConfiguration
+              .getAnimation(index)
+              .getName();
+            return animationName.length > 0 ? animationName : null;
+          })
+            .filter(Boolean)
+            .sort();
+        }
+
+        return [];
+      },
+      [
         project,
-        scope,
+        scope.layout,
         instructionMetadata,
         instruction,
         expressionMetadata,
         expression,
         parameterIndex,
-      } = props;
+      ]
+    );
 
-      const objectName = getLastObjectParameterValue({
-        instructionMetadata,
-        instruction,
-        expressionMetadata,
-        expression,
-        parameterIndex,
-      });
-      if (!objectName || !project) {
-        return [];
-      }
+    const isCurrentValueInAnimationNamesList = !!animationNames.find(
+      animationName => `"${animationName}"` === props.value
+    );
 
-      const object = getObjectByName(project, scope.layout, objectName);
-      if (!object) {
-        return [];
-      }
+    // If the current value is not in the list of animation names, display an expression field.
+    const [isExpressionField, setIsExpressionField] = React.useState(
+      !!props.value && !isCurrentValueInAnimationNamesList
+    );
 
-      if (object.getType() === 'Sprite') {
-        const spriteConfiguration = gd.asSpriteConfiguration(
-          object.getConfiguration()
-        );
-
-        return mapFor(0, spriteConfiguration.getAnimationsCount(), index => {
-          const animationName = spriteConfiguration
-            .getAnimation(index)
-            .getName();
-          return animationName.length > 0 ? animationName : null;
-        })
-          .filter(Boolean)
-          .sort()
-          .map(animationName => ({
-            kind: 'Text',
-            completion: `"${animationName}"`,
-          }));
-      }
-
-      return [];
+    const switchFieldType = () => {
+      setIsExpressionField(!isExpressionField);
     };
 
+    const onChangeSelectValue = (event, value) => {
+      props.onChange(event.target.value);
+    };
+
+    const onChangeTextValue = (value: string) => {
+      props.onChange(value);
+    };
+
+    const fieldLabel = props.parameterMetadata
+      ? props.parameterMetadata.getDescription()
+      : undefined;
+
+    const selectOptions = animationNames.map(animationName => {
+      return (
+        <SelectOption
+          key={animationName}
+          value={`"${animationName}"`}
+          label={animationName}
+          shouldNotTranslate
+        />
+      );
+    });
+
     return (
-      <GenericExpressionField
-        expressionType="string"
-        onGetAdditionalAutocompletions={expression =>
-          getAnimationNames().filter(
-            ({ completion }) => completion.indexOf(expression) === 0
+      <TextFieldWithButtonLayout
+        renderTextField={() =>
+          !isExpressionField ? (
+            <SelectField
+              ref={field}
+              id={
+                props.parameterIndex !== undefined
+                  ? `parameter-${props.parameterIndex}-animation-name-field`
+                  : undefined
+              }
+              value={props.value}
+              onChange={onChangeSelectValue}
+              margin={props.isInline ? 'none' : 'dense'}
+              fullWidth
+              floatingLabelText={fieldLabel}
+              translatableHintText={t`Choose an animation`}
+              helperMarkdownText={
+                (props.parameterMetadata &&
+                  props.parameterMetadata.getLongDescription()) ||
+                null
+              }
+            >
+              {selectOptions}
+            </SelectField>
+          ) : (
+            <GenericExpressionField
+              ref={field}
+              id={
+                props.parameterIndex !== undefined
+                  ? `parameter-${props.parameterIndex}-animation-name-field`
+                  : undefined
+              }
+              expressionType="string"
+              {...props}
+              onChange={onChangeTextValue}
+            />
           )
         }
-        ref={field}
-        {...props}
+        renderButton={style =>
+          isExpressionField ? (
+            <FlatButton
+              id="switch-expression-select"
+              leftIcon={<TypeCursorSelect />}
+              style={style}
+              primary
+              label={<Trans>Select an animation</Trans>}
+              onClick={switchFieldType}
+            />
+          ) : (
+            <RaisedButton
+              id="switch-expression-select"
+              icon={<Functions />}
+              style={style}
+              primary
+              label={<Trans>Use an Expression</Trans>}
+              onClick={switchFieldType}
+            />
+          )
+        }
       />
     );
   }

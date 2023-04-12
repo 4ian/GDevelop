475e0e504deffcbaf9a47f671650ee8cb6ca6cd8
Davy Hélard <davy.helard@gmail.com>
2023-04-12 17:17:43 +0200

    Fix collision mask zoom bounds

--- newIDE/app/src/Utils/ZoomUtils.js
+++ newIDE/app/src/Utils/ZoomUtils.js
@@ -1,11 +1,11 @@
 // @flow
 
-export const zoomStepBasePower = 1 / 16;
+const zoomStepBasePower = 1 / 4;
 
-const stepZoomFactor = Math.pow(2, 2 * zoomStepBasePower);
+const stepZoomFactor = Math.pow(2, zoomStepBasePower);
 export const zoomInFactor = stepZoomFactor;
 export const zoomOutFactor = Math.pow(stepZoomFactor, -1);
-const wheelStepZoomFactor = Math.pow(1.7, zoomStepBasePower);
+const wheelStepZoomFactor = Math.pow(2, zoomStepBasePower);
 
 // TODO: Use absolute value of signal that should represent either:
 // - Mouse sensitivity
@@ -19,19 +19,3 @@ const instancesEditorMinZoom = 1 / 128;
 
 export const clampInstancesEditorZoom = (zoom: number): number =>
   Math.max(Math.min(zoom, instancesEditorMaxZoom), instancesEditorMinZoom);
-
-export const imagePreviewMaxZoom = 16;
-export const imagePreviewMinZoom = 1 / 16;
-
-export const clampImagePreviewZoom = (zoom: number): number =>
-  Math.max(Math.min(zoom, imagePreviewMaxZoom), imagePreviewMinZoom);
-
-export const willZoomChange = (
-  currentZoom: number,
-  multiplier: number
-): boolean => {
-  return !(
-    (currentZoom >= imagePreviewMaxZoom && multiplier > 1) ||
-    (currentZoom <= imagePreviewMinZoom && multiplier < 1)
-  );
-};

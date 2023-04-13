670dc1b4fff86c4debc25a654df8a80c8fa75ee7
Davy Hélard <davy.helard@gmail.com>
2023-04-13 11:45:41 +0200

    Revert zoom speed changes to handle it better in another PR.

--- newIDE/app/src/Utils/ZoomUtils.js
+++ newIDE/app/src/Utils/ZoomUtils.js
@@ -1,11 +1,11 @@
 // @flow
 
-const zoomStepBasePower = 1 / 4;
+export const zoomStepBasePower = 1 / 16;
 
-const stepZoomFactor = Math.pow(2, zoomStepBasePower);
+const stepZoomFactor = Math.pow(2, 2 * zoomStepBasePower);
 export const zoomInFactor = stepZoomFactor;
 export const zoomOutFactor = Math.pow(stepZoomFactor, -1);
-const wheelStepZoomFactor = Math.pow(2, zoomStepBasePower);
+const wheelStepZoomFactor = Math.pow(1.7, zoomStepBasePower);
 
 // TODO: Use absolute value of signal that should represent either:
 // - Mouse sensitivity

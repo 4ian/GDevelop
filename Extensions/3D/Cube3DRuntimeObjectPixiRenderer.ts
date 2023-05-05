c4f88bddd3507ff7b3320419f25c29bf35991c03
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-05-05 14:34:44 +0200

    Prettier

--- Extensions/3D/Cube3DRuntimeObjectPixiRenderer.ts
+++ Extensions/3D/Cube3DRuntimeObjectPixiRenderer.ts
@@ -147,13 +147,11 @@ namespace gdjs {
      */
     updateTextureUvMapping(faceIndex?: number) {
       // @ts-ignore - position is stored as a Float32BufferAttribute
-      const pos: THREE.BufferAttribute = this._boxMesh.geometry.getAttribute(
-        'position'
-      );
+      const pos: THREE.BufferAttribute =
+        this._boxMesh.geometry.getAttribute('position');
       // @ts-ignore - uv is stored as a Float32BufferAttribute
-      const uvMapping: THREE.BufferAttribute = this._boxMesh.geometry.getAttribute(
-        'uv'
-      );
+      const uvMapping: THREE.BufferAttribute =
+        this._boxMesh.geometry.getAttribute('uv');
       const startIndex =
         faceIndex === undefined ? 0 : faceIndexToMaterialIndex[faceIndex] * 4;
       const endIndex =
@@ -175,9 +173,10 @@ namespace gdjs {
           continue;
         }
 
-        const shouldRepeatTexture = this._object.shouldRepeatTextureOnFaceAtIndex(
-          materialIndexToFaceIndex[materialIndex]
-        );
+        const shouldRepeatTexture =
+          this._object.shouldRepeatTextureOnFaceAtIndex(
+            materialIndexToFaceIndex[materialIndex]
+          );
 
         const shouldOrientateFacesTowardsY =
           this._object.getFacesOrientation() === 'Y';
@@ -206,12 +205,10 @@ namespace gdjs {
               if (shouldOrientateFacesTowardsY) {
                 [x, y] = noRepeatTextureVertexIndexToUvMapping[vertexIndex % 4];
               } else {
-                [
-                  x,
-                  y,
-                ] = noRepeatTextureVertexIndexToUvMappingForLeftAndRightFacesTowardsZ[
-                  vertexIndex % 4
-                ];
+                [x, y] =
+                  noRepeatTextureVertexIndexToUvMappingForLeftAndRightFacesTowardsZ[
+                    vertexIndex % 4
+                  ];
               }
             }
             break;
@@ -237,12 +234,10 @@ namespace gdjs {
               if (shouldOrientateFacesTowardsY) {
                 [x, y] = noRepeatTextureVertexIndexToUvMapping[vertexIndex % 4];
               } else {
-                [
-                  x,
-                  y,
-                ] = noRepeatTextureVertexIndexToUvMappingForLeftAndRightFacesTowardsZ[
-                  vertexIndex % 4
-                ];
+                [x, y] =
+                  noRepeatTextureVertexIndexToUvMappingForLeftAndRightFacesTowardsZ[
+                    vertexIndex % 4
+                  ];
                 x = -x;
                 y = -y;
               }

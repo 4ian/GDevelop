77eda24c30ff749eb68f29c57d4e48e730e26a1b
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-05-22 16:02:48 +0200

    Add unit tests for game slug cleaning function

---
+++ newIDE/app/src/GameDashboard/PublicGameProperties.spec.js
@@ -0,0 +1,22 @@
+// @flow
+
+import { cleanUpGameSlug } from './PublicGameProperties';
+
+describe('cleanUpGameSlug', () => {
+  it('should replace space characters with dashes and lowercase everything', () => {
+    expect(cleanUpGameSlug('Turtle Mutant Ninja Turtle')).toEqual(
+      'turtle-mutant-ninja-turtle'
+    );
+  });
+  it('should cut off to a maximal length', () => {
+    expect(
+      cleanUpGameSlug('turtle-mutant-ninja-turtle-the-return-of-the-rat')
+    ).toEqual('turtle-mutant-ninja-turtle-the');
+  });
+  it('should replace cyrillic characters with latin ones', () => {
+    expect(cleanUpGameSlug('БГДЖЗИЙЛПФЦЧШЩЫЭЮЯбвгджзийклмн')).toEqual(
+      'bgdzhzijlpftschshshchyeyuyabvg'
+    );
+    expect(cleanUpGameSlug('птфцчшщыэюя')).toEqual('ptftschshshchyeyuya');
+  });
+});

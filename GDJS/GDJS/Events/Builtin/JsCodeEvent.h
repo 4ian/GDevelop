9a82acbd0b828fda0cf505f5428061a5bdd94477
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-07-13 15:52:42 +0200

    Add javascript event object expression to list of event object expressions

--- GDJS/GDJS/Events/Builtin/JsCodeEvent.h
+++ GDJS/GDJS/Events/Builtin/JsCodeEvent.h
@@ -50,6 +50,12 @@ class JsCodeEvent : public gd::BaseEvent {
   bool IsEventsSheetExpanded() const { return eventsSheetExpanded; }
   void SetEventsSheetExpanded(bool enable) { eventsSheetExpanded = enable; };
 
+  std::vector<gd::Expression*> GetAllObjectExpressions() override {
+    std::vector<gd::Expression*> allObjectExpressions;
+    allObjectExpressions.push_back(&parameterObjects);
+    return allObjectExpressions;
+  }
+
  private:
   void Init(const JsCodeEvent& event);
 

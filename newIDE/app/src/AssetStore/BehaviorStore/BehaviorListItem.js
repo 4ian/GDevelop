8969c9af8c358fc5302e84a5eb1c6b2df793bdd7
D8H <Davy.Helard@gmail.com>
2023-07-24 17:50:39 +0200

    Show extensions from the store at the end of the behavior list (#5476)

---
+++ newIDE/app/src/AssetStore/BehaviorStore/BehaviorListItem.js
@@ -0,0 +1,160 @@
+// @flow
+import * as React from 'react';
+import { type BehaviorShortHeader } from '../../Utils/GDevelopServices/Extension';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Text from '../../UI/Text';
+import { Trans } from '@lingui/macro';
+import { Column } from '../../UI/Grid';
+import { IconContainer } from '../../UI/IconContainer';
+import HighlightedText from '../../UI/Search/HighlightedText';
+import { type SearchMatch } from '../../UI/Search/UseSearchStructuredItem';
+import Chip from '../../UI/Chip';
+import { LineStackLayout } from '../../UI/Layout';
+import { type SearchableBehaviorMetadata } from './BehaviorStoreContext';
+
+const styles = {
+  button: { width: '100%' },
+  container: {
+    display: 'flex',
+    textAlign: 'left',
+    overflow: 'hidden',
+    width: '100%',
+  },
+  icon: {
+    paddingTop: 4,
+  },
+};
+
+type Props = {|
+  id?: string,
+  objectType: string,
+  objectBehaviorsTypes: Array<string>,
+  behaviorShortHeader: BehaviorShortHeader | SearchableBehaviorMetadata,
+  matches: ?Array<SearchMatch>,
+  onChoose: () => void,
+  onHeightComputed: number => void,
+|};
+
+export const BehaviorListItem = ({
+  id,
+  objectType,
+  objectBehaviorsTypes,
+  behaviorShortHeader,
+  matches,
+  onChoose,
+  onHeightComputed,
+}: Props) => {
+  const alreadyAdded = objectBehaviorsTypes.includes(behaviorShortHeader.type);
+  // An empty object type means the base object, i.e: any object.
+  const isObjectIncompatible =
+    behaviorShortHeader.objectType &&
+    objectType !== behaviorShortHeader.objectType;
+
+  // Report the height of the item once it's known.
+  const containerRef = React.useRef<?HTMLDivElement>(null);
+  React.useLayoutEffect(() => {
+    if (containerRef.current)
+      onHeightComputed(containerRef.current.getBoundingClientRect().height);
+  });
+
+  const renderField = (field: 'description' | 'fullName') => {
+    const originalField = behaviorShortHeader[field];
+
+    if (!matches) return originalField;
+    const nameMatches = matches.filter(match => match.key === field);
+    if (nameMatches.length === 0) return originalField;
+
+    return (
+      <HighlightedText
+        text={originalField}
+        matchesCoordinates={nameMatches[0].indices}
+      />
+    );
+  };
+
+  const isEnabled = !alreadyAdded && !isObjectIncompatible;
+
+  const chooseBehavior = React.useCallback(
+    () => {
+      if (isEnabled) {
+        onChoose();
+      }
+    },
+    [isEnabled, onChoose]
+  );
+
+  return (
+    <ButtonBase
+      id={id}
+      onClick={chooseBehavior}
+      focusRipple
+      style={styles.button}
+    >
+      <div
+        style={
+          isEnabled ? styles.container : { ...styles.container, opacity: 0.384 }
+        }
+        ref={containerRef}
+      >
+        <LineStackLayout>
+          <div style={styles.icon}>
+            <IconContainer
+              alt={behaviorShortHeader.fullName}
+              src={behaviorShortHeader.previewIconUrl}
+              size={32}
+            />
+          </div>
+          <Column expand>
+            <LineStackLayout noMargin alignItems="baseline">
+              <Text
+                noMargin
+                allowBrowserAutoTranslate={false}
+                displayInlineAsSpan // Important to avoid the text to use a "p" which causes crashes with automatic translation tools with the highlighted text.
+              >
+                {renderField('fullName')}
+              </Text>
+              {alreadyAdded && (
+                <Chip
+                  size="small"
+                  label={<Trans>Already added</Trans>}
+                  color="secondary"
+                  variant="outlined"
+                />
+              )}
+              {isObjectIncompatible && (
+                <Chip
+                  size="small"
+                  label={<Trans>Incompatible with the object</Trans>}
+                  color="secondary"
+                  variant="outlined"
+                />
+              )}
+              {behaviorShortHeader.tier === 'community' && (
+                <Chip
+                  size="small"
+                  label={<Trans>Community-made</Trans>}
+                  color="primary"
+                />
+              )}
+              {behaviorShortHeader.isDeprecated && (
+                <Chip
+                  size="small"
+                  label={<Trans>Deprecated</Trans>}
+                  color="primary"
+                />
+              )}
+            </LineStackLayout>
+            <Text
+              noMargin
+              size="body2"
+              allowBrowserAutoTranslate={false}
+              displayInlineAsSpan // Important to avoid the text to use a "p" which causes crashes with automatic translation tools with the highlighted text.
+            >
+              {renderField('description')}
+            </Text>
+          </Column>
+        </LineStackLayout>
+      </div>
+    </ButtonBase>
+  );
+};
--- newIDE/app/src/BehaviorsEditor/EnumerateBehaviorsMetadata.js
+++ newIDE/app/src/BehaviorsEditor/EnumerateBehaviorsMetadata.js
@@ -10,7 +10,8 @@ export type EnumeratedBehaviorMetadata = {|
   defaultName: string,
   fullName: string,
   description: string,
-  iconFilename: string,
+  previewIconUrl: string,
+  category: string,
 |};
 
 export const enumerateBehaviorsMetadata = (
@@ -44,8 +45,9 @@ export const enumerateBehaviorsMetadata = (
           defaultName: behaviorMetadata.getDefaultName(),
           fullName: behaviorMetadata.getFullName(),
           description: behaviorMetadata.getDescription(),
-          iconFilename: behaviorMetadata.getIconFilename(),
+          previewIconUrl: behaviorMetadata.getIconFilename(),
           objectType: behaviorMetadata.getObjectType(),
+          category: extension.getCategory(),
         }));
     })
   );

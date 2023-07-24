16c78b07a466a00c5c3ce08665c328a8647ad1d1
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2023-07-24 18:46:23 +0200

    Prevent installing more than 100 assets at a time

--- newIDE/app/src/AssetStore/AssetPackInstallDialog.js
+++ newIDE/app/src/AssetStore/AssetPackInstallDialog.js
@@ -36,6 +36,8 @@ import {
 } from './NewObjectDialog';
 import { type InstallAssetOutput } from './InstallAsset';
 
+const MAX_ASSETS_TO_INSTALL = 100;
+
 type Props = {|
   assetPack: PublicAssetPack | PrivateAssetPack | null,
   assetShortHeaders: Array<AssetShortHeader>,
@@ -68,6 +70,8 @@ const AssetPackInstallDialog = ({
   const noAssetsInstalled =
     !allAssetsInstalled &&
     missingAssetShortHeaders.length === assetShortHeaders.length;
+  const isInstallingTooManyAssets =
+    assetShortHeaders.length > MAX_ASSETS_TO_INSTALL;
 
   const [
     areAssetsBeingInstalled,
@@ -140,6 +144,7 @@ const AssetPackInstallDialog = ({
   const onInstallAssets = React.useCallback(
     async (assetShortHeaders: Array<AssetShortHeader>) => {
       if (!assetShortHeaders || !assetShortHeaders.length) return;
+      if (assetShortHeaders.length > MAX_ASSETS_TO_INSTALL) return;
 
       setAreAssetsBeingInstalled(true);
       try {
@@ -244,6 +249,30 @@ const AssetPackInstallDialog = ({
                 <Trans>
                   You need to save this project as a cloud project to install
                   premium assets. Please save your project and try again.
+                </Trans>
+              </Text>
+            </AlertMessage>
+          ),
+        }
+      : isInstallingTooManyAssets
+      ? {
+          actionButton: (
+            <RaisedButton
+              key="continue"
+              label={<Trans>Add the assets</Trans>}
+              primary
+              disabled
+              onClick={() => {}}
+            />
+          ),
+          onApply: () => {},
+          content: (
+            <AlertMessage kind="warning">
+              <Text>
+                <Trans>
+                  You can only install up to {MAX_ASSETS_TO_INSTALL} assets at
+                  once. Try filtering the assets you would like to install, or
+                  install them one by one.
                 </Trans>
               </Text>
             </AlertMessage>

3ecee7960a066cc272fe8650a76bc4439ccc3842
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-07-27 09:12:08 +0200

    Add detection of file picking dialog closing

--- newIDE/app/src/EventsFunctionsExtensionsLoader/Storage/BrowserEventsFunctionsExtensionOpener.js
+++ newIDE/app/src/EventsFunctionsExtensionsLoader/Storage/BrowserEventsFunctionsExtensionOpener.js
@@ -12,6 +12,33 @@ export default class BrowserEventsFunctionsExtensionOpener {
         return resolve(file);
       };
 
+      // There is no built-in way to know if the user closed the file picking dialog
+      // with the cancel button. What follows is an implementation that follows
+      // https://stackoverflow.com/questions/71435515/how-can-i-detect-that-the-cancel-button-has-been-clicked-on-a-input-type-file.
+
+      const onFocusBackWindow = () => {
+        window.removeEventListener('focus', onFocusBackWindow);
+        if (document.body) {
+          document.body.addEventListener(
+            'pointermove',
+            onFilePickingDialogFinishedClosing
+          );
+        }
+      };
+      const onFilePickingDialogFinishedClosing = () => {
+        if (document.body) {
+          document.body.removeEventListener(
+            'pointermove',
+            onFilePickingDialogFinishedClosing
+          );
+        }
+        if (!adhocInput.files.length) {
+          console.log('No file selected.');
+          resolve(null);
+        }
+      };
+      window.addEventListener('focus', onFocusBackWindow);
+
       adhocInput.click();
     });
   };

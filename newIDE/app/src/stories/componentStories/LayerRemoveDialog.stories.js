44b95522d69f584991a48dd9a64b7ddf1c00877e
Davy Hélard <davy.helard@gmail.com>
2023-08-10 12:20:35 +0200

    Typo

--- newIDE/app/src/stories/componentStories/LayerRemoveDialog.stories.js
+++ newIDE/app/src/stories/componentStories/LayerRemoveDialog.stories.js
@@ -40,7 +40,7 @@ export const LayerWithInstances = () => {
     return () => {
       project.delete();
     };
-    // Delete layout on unmount
+    // Delete project on unmount
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -64,7 +64,7 @@ export const LayerWithoutInstances = () => {
     return () => {
       project.delete();
     };
-    // Delete layout on unmount
+    // Delete project on unmount
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (

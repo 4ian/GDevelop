84396b8583806177a7311f233196063119d9ec95
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-09-05 14:05:22 +0200

    Reduce number of needed ref

--- newIDE/app/src/ResourcesList/ResourcePreview/index.js
+++ newIDE/app/src/ResourcesList/ResourcePreview/index.js
@@ -7,7 +7,6 @@ import FontDownload from '@material-ui/icons/FontDownload';
 import File from '../../UI/CustomSvgIcons/File';
 import Video from '../../UI/CustomSvgIcons/Video';
 import Music from '../../UI/CustomSvgIcons/Music';
-import useForceUpdate from '../../Utils/UseForceUpdate';
 
 type Props = {|
   project: gdProject,
@@ -16,62 +15,50 @@ type Props = {|
   onSize?: (number, number) => void,
 |};
 
-export type ResourcePreviewInterface = {| forceUpdate: () => void |};
-
 /**
  * Display the right preview for any given resource of a project
  */
-const ResourcePreview = React.forwardRef<Props, ResourcePreviewInterface>(
-  (props, ref) => {
-    const forceUpdate = useForceUpdate();
-    React.useImperativeHandle(ref, () => ({ forceUpdate }));
-    const { project, resourceName } = props;
-    const resourcesManager = project.getResourcesManager();
-    const resourceKind = resourcesManager.hasResource(resourceName)
-      ? resourcesManager.getResource(resourceName).getKind()
-      : null;
+const ResourcePreview = (props: Props) => {
+  const { project, resourceName } = props;
+  const resourcesManager = project.getResourcesManager();
+  const resourceKind = resourcesManager.hasResource(resourceName)
+    ? resourcesManager.getResource(resourceName).getKind()
+    : null;
 
-    switch (resourceKind) {
-      case 'image':
-        return (
-          <ImagePreview
-            resourceName={props.resourceName}
-            imageResourceSource={props.resourcesLoader.getResourceFullUrl(
-              props.project,
-              props.resourceName,
-              {}
-            )}
-            isImageResourceSmooth={isProjectImageResourceSmooth(
-              props.project,
-              props.resourceName
-            )}
-            onSize={props.onSize}
-          />
-        );
-      case 'audio':
-        return (
-          <GenericIconPreview renderIcon={props => <Music {...props} />} />
-        );
-      case 'json':
-      case 'tilemap':
-      case 'tileset':
-      case 'model3D':
-        return <GenericIconPreview renderIcon={props => <File {...props} />} />;
-      case 'video':
-        return (
-          <GenericIconPreview renderIcon={props => <Video {...props} />} />
-        );
-      case 'font':
-      case 'bitmapFont':
-        return (
-          <GenericIconPreview
-            renderIcon={props => <FontDownload {...props} />}
-          />
-        );
-      default:
-        return null;
-    }
+  switch (resourceKind) {
+    case 'image':
+      return (
+        <ImagePreview
+          resourceName={props.resourceName}
+          imageResourceSource={props.resourcesLoader.getResourceFullUrl(
+            props.project,
+            props.resourceName,
+            {}
+          )}
+          isImageResourceSmooth={isProjectImageResourceSmooth(
+            props.project,
+            props.resourceName
+          )}
+          onSize={props.onSize}
+        />
+      );
+    case 'audio':
+      return <GenericIconPreview renderIcon={props => <Music {...props} />} />;
+    case 'json':
+    case 'tilemap':
+    case 'tileset':
+    case 'model3D':
+      return <GenericIconPreview renderIcon={props => <File {...props} />} />;
+    case 'video':
+      return <GenericIconPreview renderIcon={props => <Video {...props} />} />;
+    case 'font':
+    case 'bitmapFont':
+      return (
+        <GenericIconPreview renderIcon={props => <FontDownload {...props} />} />
+      );
+    default:
+      return null;
   }
-);
+};
 
 export default ResourcePreview;

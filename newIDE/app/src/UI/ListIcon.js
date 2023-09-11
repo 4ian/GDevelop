42b38db0477cbad3202d5081ee9a5fcf2fd8f12f
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-09-11 15:08:51 +0200

    Remove offset of image in ListIcon component relative to parent

--- newIDE/app/src/UI/ListIcon.js
+++ newIDE/app/src/UI/ListIcon.js
@@ -77,6 +77,7 @@ function ListIcon(props: Props) {
   return (
     <div
       style={{
+        display: 'flex',
         width: iconWidth,
         height: iconHeight,
         lineHeight: `${iconHeight}px`, // Vertical centering

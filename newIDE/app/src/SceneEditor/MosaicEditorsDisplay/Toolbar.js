5ac7582b8d776be66b3bbb84490a2acd6525e2ad
Florian Rival <Florian.Rival@gmail.com>
2023-09-23 13:50:21 +0200

    Avoid some useless rendering of toolbars
    Don't show in changelog

--- newIDE/app/src/SceneEditor/MosaicEditorsDisplay/Toolbar.js
+++ newIDE/app/src/SceneEditor/MosaicEditorsDisplay/Toolbar.js
@@ -7,7 +7,6 @@ import ToolbarSeparator from '../../UI/ToolbarSeparator';
 import IconButton from '../../UI/IconButton';
 import ElementWithMenu from '../../UI/Menu/ElementWithMenu';
 import ToolbarCommands from '../ToolbarCommands';
-import InstancesSelection from '../../InstancesEditor/InstancesSelection';
 import { type MenuItemTemplate } from '../../UI/Menu/Menu.flow';
 import ObjectIcon from '../../UI/CustomSvgIcons/Object';
 import ObjectGroupIcon from '../../UI/CustomSvgIcons/ObjectGroup';
@@ -33,14 +32,14 @@ type Props = {|
   redo: () => void,
   canRedo: boolean,
   deleteSelection: () => void,
-  instancesSelection: InstancesSelection,
+  selectedInstancesCount: number,
   toggleInstancesList: () => void,
   isInstancesListShown: boolean,
   toggleLayersList: () => void,
   isLayersListShown: boolean,
-  isWindowMaskShown: () => boolean,
+  isWindowMaskShown: boolean,
   toggleWindowMask: () => void,
-  isGridShown: () => boolean,
+  isGridShown: boolean,
   toggleGrid: () => void,
   openSetupGrid: () => void,
   getContextMenuZoomItems: I18nType => Array<MenuItemTemplate>,
@@ -51,7 +50,7 @@ type Props = {|
   onRenameObject: () => void,
 |};
 
-const Toolbar = (props: Props) => {
+const Toolbar = React.memo<Props>(function Toolbar(props) {
   return (
     <>
       <ToolbarCommands
@@ -68,9 +67,7 @@ const Toolbar = (props: Props) => {
         toggleWindowMask={props.toggleWindowMask}
         toggleGrid={props.toggleGrid}
         setupGrid={props.openSetupGrid}
-        canDeleteSelection={
-          props.instancesSelection.getSelectedInstances().length !== 0
-        }
+        canDeleteSelection={props.selectedInstancesCount !== 0}
         canRenameObject={props.canRenameObject}
         onRenameObject={props.onRenameObject}
       />
@@ -159,13 +156,13 @@ const Toolbar = (props: Props) => {
             {
               type: 'checkbox',
               label: i18n._(t`Show Mask`),
-              checked: props.isWindowMaskShown(),
+              checked: props.isWindowMaskShown,
               click: () => props.toggleWindowMask(),
             },
             {
               type: 'checkbox',
               label: i18n._(t`Show grid`),
-              checked: props.isGridShown(),
+              checked: props.isGridShown,
               click: () => props.toggleGrid(),
             },
             { type: 'separator' },
@@ -221,7 +218,7 @@ const Toolbar = (props: Props) => {
           size="small"
           color="default"
           onClick={props.deleteSelection}
-          disabled={!props.instancesSelection.getSelectedInstances().length}
+          disabled={!props.selectedInstancesCount}
           tooltip={t`Delete the selected instances from the scene`}
         >
           <TrashIcon />
@@ -240,6 +237,6 @@ const Toolbar = (props: Props) => {
       </ToolbarGroup>
     </>
   );
-};
+});
 
 export default Toolbar;

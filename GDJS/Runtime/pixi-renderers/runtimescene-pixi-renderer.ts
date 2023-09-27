d26d92f58d4ab1e0fee4104f4721d1927e6380d0
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2023-09-27 23:09:25 +0200

    Apply review comments

--- GDJS/Runtime/pixi-renderers/runtimescene-pixi-renderer.ts
+++ GDJS/Runtime/pixi-renderers/runtimescene-pixi-renderer.ts
@@ -281,9 +281,21 @@ namespace gdjs {
     }
 
     /**
+     * Unless you know what you are doing, use the VR extension instead of this function directly.
+     *
      * In VR, only 3D elements can be rendered, 2D cannot.
      * This rendering method skips over all 2D layers and elements, and simply renders the 3D content.
-     * This method is to be called by the XRSession's requestAnimationFrame for rendering to
+     * This method is to be called by the XRSession's `requestAnimationFrame` for rendering to
+     * the headset whenever the headset requests the screen to be drawn. Note that while an XRSession
+     * is in progress, the regular `requestAnimationFrame` will be disabled. Make sure that whenever you
+     * enter an XRSession, you:
+     * - Call this function first and foremost when the XRSession's requestAnimationFrame fires,
+     *   as it is necessary to draw asap as the headset will eventually stop waiting and just draw the
+     *   framebuffer as it is to maintain a constant screen refresh rate, which can be in the middle of
+     *   or even before rendering if we aren't fast enough, leading to screen flashes and bugs.
+     * - Call GDevelop's step function to give the scene a chance to step after having drawn to the screen
+     *   to allow the game to actually progress, since GDevelop will no longer step by itself with
+     *   `requestAnimationFrame` disabled.
      *
      * Note to engine developers: `threeRenderer.resetState()` may NOT be called in this function,
      * as WebXR modifies the WebGL state in a way that resetting it will cause an improper render
@@ -295,7 +307,8 @@ namespace gdjs {
 
       const threeRenderer = this._threeRenderer;
       // VR rendering relies on ThreeJS
-      if (!threeRenderer) return;
+      if (!threeRenderer)
+        throw new Error('Cannot render a scene with no 3D elements in VR!');
 
       // Render each layer one by one.
       let isFirstRender = true;
@@ -311,7 +324,7 @@ namespace gdjs {
         )
           continue;
 
-        // Render a layer with 3D rendering, and possibly some 2D rendering too.
+        // Render a layer with 3D rendering, and no 2D rendering at all for VR.
         const threeScene = runtimeLayerRenderer.getThreeScene();
         const threeCamera = runtimeLayerRenderer.getThreeCamera();
         if (!threeScene || !threeCamera) continue;

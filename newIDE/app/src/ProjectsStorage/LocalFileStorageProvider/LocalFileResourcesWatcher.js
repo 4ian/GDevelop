0cd90b47073317d1719192ec9b46c90473a826fc
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-10-06 16:30:29 +0200

    Debounce callback file-wise

--- newIDE/app/src/ProjectsStorage/LocalFileStorageProvider/LocalFileResourcesWatcher.js
+++ newIDE/app/src/ProjectsStorage/LocalFileStorageProvider/LocalFileResourcesWatcher.js
@@ -3,12 +3,31 @@ import path from 'path-browserify';
 import optionalRequire from '../../Utils/OptionalRequire';
 import { type FileMetadata } from '..';
 import debounce from 'lodash/debounce';
+import wrap from 'lodash/wrap';
+import memoize from 'lodash/memoize';
 
 const fileWatcher = optionalRequire('chokidar');
 
 export const setupResourcesWatcher =
   fileWatcher && path
     ? (fileMetadata: FileMetadata, callback: (resourceInfo: any) => void) => {
+        // We can't just debounce the whole callback, it has to be done file-wise,
+        // otherwise we would miss all the debounced calls but the last one.
+        // See https://stackoverflow.com/questions/28787436/debounce-a-function-with-argument
+        const debouncedCallback = wrap(
+          memoize(() =>
+            debounce(
+              filePath => {
+                const relativePath = path.relative(folderPath, filePath);
+
+                callback({ path: relativePath });
+              },
+              200,
+              { leading: false, trailing: true }
+            )
+          ),
+          (getMemoizedFunc, obj) => getMemoizedFunc(obj)(obj)
+        );
         const folderPath = path.dirname(fileMetadata.fileIdentifier);
         const gameFile = path.basename(fileMetadata.fileIdentifier);
         const watcher = fileWatcher
@@ -20,15 +39,7 @@ export const setupResourcesWatcher =
             // TODO: Is it safe to let it like that since the OS could for some reason
             // do never-ending operations on the folder or its children, making the debounce
             // never ending.
-            debounce(
-              filePath => {
-                const relativePath = path.relative(folderPath, filePath);
-
-                callback({ path: relativePath });
-              },
-              200,
-              { leading: false, trailing: true }
-            )
+            debouncedCallback
           );
         return () => watcher.unwatch(folderPath);
       }

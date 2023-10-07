ecefd68db27320f41d55387bef09639c877fce1c
Davy Hélard <davy.helard@gmail.com>
2023-10-07 17:20:30 +0200

    Better test life cycle.

--- Extensions/TweenBehavior/tests/TweenBehavior.spec.js
+++ Extensions/TweenBehavior/tests/TweenBehavior.spec.js
@@ -1,11 +1,11 @@
 // @ts-check
-describe('gdjs.TweenRuntimeBehavior', function () {
+describe('gdjs.TweenRuntimeBehavior', () => {
   const behaviorName = 'Tween';
 
   const createScene = (timeDelta = 1000 / 60) => {
     const runtimeGame = gdjs.getPixiRuntimeGame();
     const runtimeScene = new gdjs.TestRuntimeScene(runtimeGame);
-    runtimeScene._timeManager.getElapsedTime = function () {
+    runtimeScene._timeManager.getElapsedTime = () => {
       return timeDelta;
     };
     return runtimeScene;
@@ -36,7 +36,7 @@ describe('gdjs.TweenRuntimeBehavior', function () {
   let object;
   /** @type {gdjs.TweenRuntimeBehavior} */
   let behavior;
-  beforeEach(function () {
+  beforeEach(() => {
     runtimeScene = createScene();
     runtimeScene.getLayer('').setTimeScale(1.5);
     object = addObject(runtimeScene);
@@ -44,8 +44,9 @@ describe('gdjs.TweenRuntimeBehavior', function () {
     behavior = object.getBehavior(behaviorName);
   });
 
-  it('can play a tween till the end', function () {
+  it('can play a tween till the end', () => {
     object.setPosition(200, 300);
+
     behavior.addObjectPositionXTween2('MyTween', 600, 'linear', 0.25, false);
 
     // Tween actions don't change the value directly.
@@ -53,10 +54,20 @@ describe('gdjs.TweenRuntimeBehavior', function () {
     expect(behavior.getValue('MyTween')).to.be(200);
     expect(behavior.getProgress('MyTween')).to.be(0);
 
+    let oldX;
+    let oldValue;
+    let oldProgress;
     for (let i = 0; i < 10; i++) {
+      oldX = object.getX();
+      oldValue = behavior.getValue('MyTween');
+      oldProgress = behavior.getProgress('MyTween');
+
       runtimeScene.renderAndStep(1000 / 60);
       expect(behavior.isPlaying('MyTween')).to.be(true);
       expect(behavior.hasFinished('MyTween')).to.be(false);
+      expect(object.getX()).to.be.above(oldX);
+      expect(behavior.getValue('MyTween')).to.be.above(oldValue);
+      expect(behavior.getProgress('MyTween')).to.be.above(oldProgress);
     }
     // The tween reaches the end
     runtimeScene.renderAndStep(1000 / 60);
@@ -81,7 +92,7 @@ describe('gdjs.TweenRuntimeBehavior', function () {
     expect(object.getX()).to.be(123);
   });
 
-  it('can pause and resume a tween', function () {
+  it('can pause and resume a tween', () => {
     object.setPosition(200, 300);
     behavior.addObjectPositionXTween2('MyTween', 600, 'linear', 0.25, false);
 
@@ -100,8 +111,8 @@ describe('gdjs.TweenRuntimeBehavior', function () {
       runtimeScene.renderAndStep(1000 / 60);
       expect(behavior.isPlaying('MyTween')).to.be(false);
       expect(behavior.hasFinished('MyTween')).to.be(false);
+      expect(object.getX()).to.be(400);
     }
-    expect(object.getX()).to.be(400);
 
     // The value is not overridden during the pause.
     object.setX(123);
@@ -119,7 +130,7 @@ describe('gdjs.TweenRuntimeBehavior', function () {
     expect(object.getX()).to.be(440);
   });
 
-  it('can stop and restart a tween', function () {
+  it('can stop and restart a tween', () => {
     object.setPosition(200, 300);
 
     // Start the tween
@@ -138,10 +149,10 @@ describe('gdjs.TweenRuntimeBehavior', function () {
       runtimeScene.renderAndStep(1000 / 60);
       expect(behavior.isPlaying('MyTween')).to.be(false);
       expect(behavior.hasFinished('MyTween')).to.be(true);
+      expect(object.getX()).to.be(400);
     }
-    expect(object.getX()).to.be(400);
 
-    // The value is not overridden during the pause.
+    // The value is not overridden by a stopped tween.
     object.setX(123);
     runtimeScene.renderAndStep(1000 / 60);
     expect(object.getX()).to.be(123);
@@ -167,7 +178,62 @@ describe('gdjs.TweenRuntimeBehavior', function () {
     expect(object.getX()).to.be(373);
   });
 
-  it('can tween the position on X axis', function () {
+  it('can remove and recreate a tween', () => {
+    object.setPosition(200, 300);
+
+    // Start the tween
+    expect(behavior.exists('MyTween')).to.be(false);
+    behavior.addObjectPositionXTween2('MyTween', 600, 'linear', 0.25, false);
+    expect(behavior.exists('MyTween')).to.be(true);
+    for (let i = 0; i < 5; i++) {
+      runtimeScene.renderAndStep(1000 / 60);
+      expect(behavior.isPlaying('MyTween')).to.be(true);
+      expect(behavior.hasFinished('MyTween')).to.be(false);
+    }
+    expect(object.getX()).to.be(400);
+
+    // Remove the tween
+    expect(behavior.exists('MyTween')).to.be(true);
+    behavior.removeTween('MyTween', false);
+    expect(behavior.exists('MyTween')).to.be(false);
+    expect(object.getX()).to.be(400);
+    for (let i = 0; i < 5; i++) {
+      runtimeScene.renderAndStep(1000 / 60);
+      expect(behavior.isPlaying('MyTween')).to.be(false);
+      expect(behavior.hasFinished('MyTween')).to.be(false);
+      expect(object.getX()).to.be(400);
+    }
+
+    // The value is not overridden after the tween has been removed.
+    object.setX(123);
+    runtimeScene.renderAndStep(1000 / 60);
+    expect(object.getX()).to.be(123);
+
+    // A removed tween can't be resumed.
+    behavior.resumeTween('MyTween');
+    expect(behavior.isPlaying('MyTween')).to.be(false);
+    expect(behavior.hasFinished('MyTween')).to.be(false);
+    expect(object.getX()).to.be(123);
+
+    runtimeScene.renderAndStep(1000 / 60);
+    expect(object.getX()).to.be(123);
+    expect(behavior.isPlaying('MyTween')).to.be(false);
+    expect(behavior.hasFinished('MyTween')).to.be(false);
+
+    // Recreate the tween
+    expect(behavior.exists('MyTween')).to.be(false);
+    behavior.addObjectPositionXTween2('MyTween', 623, 'linear', 0.25, false);
+    expect(behavior.exists('MyTween')).to.be(true);
+    for (let i = 0; i < 5; i++) {
+      runtimeScene.renderAndStep(1000 / 60);
+      expect(behavior.isPlaying('MyTween')).to.be(true);
+      expect(behavior.hasFinished('MyTween')).to.be(false);
+    }
+    expect(object.getX()).to.be(373);
+    expect(behavior.exists('MyTween')).to.be(true);
+  });
+
+  it('can tween the position on X axis', () => {
     object.setPosition(200, 300);
     behavior.addObjectPositionXTween2('MyTween', 600, 'linear', 0.25, false);
 

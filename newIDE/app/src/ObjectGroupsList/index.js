345bad98767c4fea6efad27aeadae88e94a00836
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2023-10-25 11:17:54 +0200

    Clean forgotten import (#5831)
    Don't show in changelog

--- newIDE/app/src/ObjectGroupsList/index.js
+++ newIDE/app/src/ObjectGroupsList/index.js
@@ -25,7 +25,6 @@ import { type EmptyPlaceholder } from '../ObjectsList';
 import TreeView, { type TreeViewInterface } from '../UI/TreeView';
 import useForceUpdate from '../Utils/UseForceUpdate';
 import useAlertDialog from '../UI/Alert/useAlertDialog';
-import { useResponsiveWindowWidth } from '../UI/Reponsive/ResponsiveWindowMeasurer';
 
 export const groupWithContextReactDndType = 'GD_GROUP_WITH_CONTEXT';
 
@@ -127,8 +126,6 @@ const ObjectGroupsList = React.forwardRef<Props, ObjectGroupsListInterface>(
       showConfirmation,
       showAlert,
     } = useAlertDialog();
-    const windowWidth = useResponsiveWindowWidth();
-    const isMobileScreen = windowWidth === 'small';
 
     React.useImperativeHandle(ref, () => ({ forceUpdate }));
 

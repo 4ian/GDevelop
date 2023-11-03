26588b90024a2488fef1ddba62dbff554a3d574f
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2023-11-03 14:34:53 +0100

    Improve issue template and link to creating issue

--- newIDE/app/src/UI/ErrorBoundary.js
+++ newIDE/app/src/UI/ErrorBoundary.js
@@ -102,37 +102,31 @@ export const ErrorFallbackComponent = ({
           label={<Trans>Report the issue on GitHub</Trans>}
           primary
           onClick={() => {
-            const body = `
-=> Please write here a short description of when the error occurred and how to reproduce it.
+            const templateFile = '--bug-report.yml';
+            const title = 'Crash while using an editor';
+            const description =
+              error && error.stack
+                ? `${error.stack.slice(0, 600)}...`
+                : 'No error found';
+            const platform = getPlatformName();
+            const gdevelopVersion = getIDEVersionWithHash();
+            const platformInfo = `System Version: ${getSystemVersion()}, Arch: ${getArch()}, User Agent: ${getUserAgent()}`;
+            const additionalContext = componentStack
+              ? `${componentStack.slice(0, 600)}...`
+              : 'No component stack found';
 
-When you're ready, click on "Submit new issue". Don't change the rest of the message. Thanks!
-
-## Error stack (don't write anything here)
-\`\`\`
-${error && error.stack ? `${error.stack.slice(0, 600)}...` : 'No error found'}
-\`\`\`
-
-## Component stack (don't write anything here)
-\`\`\`
-${
-              componentStack
-                ? `${componentStack.slice(0, 600)}...`
-                : 'No component stack found'
-            }
-\`\`\`
-
-## Other details
-* IDE version: ${getIDEVersionWithHash()}
-* Arch: ${getArch()},
-* Platform Name: ${getPlatformName()},
-* System Version: ${getSystemVersion()},
-* User Agent: ${getUserAgent()},
-        `;
-            Window.openExternalURL(
-              `https://github.com/4ian/GDevelop/issues/new?body=${encodeURIComponent(
-                body
-              )}&title=Crash%20while%20using%20an%20editor`
+            const baseUrl = new URL(
+              'https://github.com/4ian/GDevelop/issues/new'
             );
+            baseUrl.searchParams.set('template', templateFile);
+            baseUrl.searchParams.set('title', title);
+            baseUrl.searchParams.set('description', description);
+            baseUrl.searchParams.set('platform', platform);
+            baseUrl.searchParams.set('gdevelop_version', gdevelopVersion);
+            baseUrl.searchParams.set('platform_info', platformInfo);
+            baseUrl.searchParams.set('additional_context', additionalContext);
+
+            Window.openExternalURL(baseUrl.href);
           }}
         />
       </Line>

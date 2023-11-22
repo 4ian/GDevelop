b69b83f522d7524e4755a876d992ee070d6ebbe3
Vladyslav Pohorielov <vpohorielov@playtika.com>
2023-11-22 17:35:51 +0200

    update install-spine script with shelljs and error logs

--- GDJS/scripts/install-spine.js
+++ GDJS/scripts/install-spine.js
@@ -1,14 +1,27 @@
-const fs = require('fs');
 const path = require('path');
+const shell = require('shelljs');
 
-const format = 'utf8';
+const readContent = (path, testErrorMessage) => {
+    if (!shell.test('-f', path)) {
+        shell.echo(`❌ ${testErrorMessage} Should exist in ${path} folder.`);
+        shell.exit(1);
+    }
+
+    const readingResult = shell.cat(path);
+
+    if (readingResult.stderr) {
+        shell.echo(`❌ Error during ${path} reading: ${readingResult.stderr}`);
+        shell.exit(1);
+    }
+
+    return readingResult.toString();
+}
 
 const originalSpineDir = path.resolve('node_modules/pixi-spine');
-const originalSpinePackage = JSON.parse(fs.readFileSync(path.join(originalSpineDir, 'package.json'), format));
-const originalSpinePath = path.join(originalSpineDir, originalSpinePackage.extensionConfig.bundle);
-const originalSpine = fs.readFileSync(originalSpinePath, format);
+const originalSpinePackage = JSON.parse(readContent(path.join(originalSpineDir, 'package.json'), 'Cannot find pixi-spine package.json file.'));
+const originalSpineContent = readContent(path.join(originalSpineDir, originalSpinePackage.extensionConfig.bundle), 'Cannot find pixi-spine bundle.');
 
 const varSpineExport = '\nvar pixi_spine = this.PIXI.spine;\n';
 const runtimeSpinePath = 'Runtime/pixi-renderers/pixi-spine.js';
+new shell.ShellString(originalSpineContent + varSpineExport).to(runtimeSpinePath);
 
-fs.writeFileSync(runtimeSpinePath, originalSpine + varSpineExport, format);

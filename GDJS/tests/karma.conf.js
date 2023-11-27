5be090629317938e37210d34496704470885fb2d
Gleb Volkov <glebusheg@gmail.com>
2023-11-27 19:04:05 +0100

    Add missing `jsonmanger` dependency in tests

--- GDJS/tests/karma.conf.js
+++ GDJS/tests/karma.conf.js
@@ -49,6 +49,7 @@ module.exports = function (config) {
       './newIDE/app/resources/GDJS/Runtime/fontfaceobserver-font-manager/fontfaceobserver.js',
       './newIDE/app/resources/GDJS/Runtime/fontfaceobserver-font-manager/fontfaceobserver-font-manager.js',
       './newIDE/app/resources/GDJS/Runtime/Model3DManager.js',
+      './newIDE/app/resources/GDJS/Runtime/jsonmanager.js',
       './newIDE/app/resources/GDJS/Runtime/ResourceLoader.js',
       './newIDE/app/resources/GDJS/Runtime/ResourceCache.js',
       './newIDE/app/resources/GDJS/Runtime/timemanager.js',

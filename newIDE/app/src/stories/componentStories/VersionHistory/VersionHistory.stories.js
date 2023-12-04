f6b8768e07743d72228107760078753925405ad0
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-12-04 17:06:16 +0100

    Group versions by day

--- newIDE/app/src/stories/componentStories/VersionHistory/VersionHistory.stories.js
+++ newIDE/app/src/stories/componentStories/VersionHistory/VersionHistory.stories.js
@@ -22,23 +22,51 @@ export default {
 const projectId = 'fb4d878a-1935-4916-b681-f9235475d354';
 const versions: Array<FilledCloudProjectVersion> = [
   {
+    id: 'dddbe02b-be5e-4008-aff4-90ab32e3315a',
+    projectId,
+    createdAt: '2023-12-04T17:21:26.729Z',
+    previousVersion: '0b43b267-ae5a-4822-a926-af14bf52f06d',
+    userId: 'a9bc54be-07e1-4f29-9739-5fbec2b04da7'
+  },
+  {
+    id: '0b43b267-ae5a-4822-a926-af14bf52f06d',
+    projectId,
+    createdAt: '2023-12-04T16:02:26.729Z',
+    previousVersion: '8e067d2d-6f08-4f93-ad2d-f3ad5ca3c69c',
+    userId: 'a9bc54be-07e1-4f29-9739-5fbec2b04da7'
+  },
+  {
+    id: 'dddbe02b-be5e-4008-aff4-90ab32e3315a',
+    projectId,
+    createdAt: '2023-08-01T07:21:26.729Z',
+    previousVersion: '0b43b267-ae5a-4822-a926-af14bf52f06d',
+    userId: 'c73c4d69-86a2-441b-a8b7-afe6b8fce810'
+  },
+  {
+    id: '0b43b267-ae5a-4822-a926-af14bf52f06d',
+    projectId,
+    createdAt: '2023-07-28T19:52:26.729Z',
+    previousVersion: '8e067d2d-6f08-4f93-ad2d-f3ad5ca3c69c',
+    userId: 'c73c4d69-86a2-441b-a8b7-afe6b8fce810'
+  },
+  {
     id: '8e067d2d-6f08-4f93-ad2d-f3ad5ca3c69c',
     projectId,
-    createdAt: '2022-12-14T10:11:49.305Z',
+    createdAt: '2022-11-14T10:11:49.305Z',
     previousVersion: '9f9f50a3-1bb2-41c3-9ddb-feaf9be45648',
     userId: 'a9bc54be-07e1-4f29-9739-5fbec2b04da7'
   },
   {
     id: '9f9f50a3-1bb2-41c3-9ddb-feaf9be45648',
     projectId,
-    createdAt: '2022-12-13T10:11:49.305Z',
+    createdAt: '2022-11-13T10:11:49.305Z',
     previousVersion: '5280e344-bd36-4662-9948-cb0d18928d03',
     userId: 'c73c4d69-86a2-441b-a8b7-afe6b8fce810',
   },
   {
     id: '5280e344-bd36-4662-9948-cb0d18928d03',
     projectId,
-    createdAt: '2022-12-12T10:11:49.305Z',
+    createdAt: '2022-10-12T10:11:49.305Z',
     previousVersion: null,
   },
 ];

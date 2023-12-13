0f5e803b035d390992d5b8c4de4566bbeee439d0
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-12-13 17:11:40 +0100

    Update current filemetadata lastModifiedDate when saving cloud project name

--- newIDE/app/src/ProjectsStorage/index.js
+++ newIDE/app/src/ProjectsStorage/index.js
@@ -146,7 +146,7 @@ export type StorageProviderOperations = {|
     project: gdProject,
     fileMetadata: FileMetadata,
     properties: {| name?: string, gameId?: string |} // In order to synchronize project and cloud project names.
-  ) => Promise<null | {| version: string |}>,
+  ) => Promise<null | {| version: string, lastModifiedDate: number |}>,
 
   // Project auto saving:
   onAutoSaveProject?: (

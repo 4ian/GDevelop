a26933cde8e54b818f5ce19cd2d700a26c442887
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2023-12-15 17:19:31 +0100

    Adapt status chip content to status

--- newIDE/app/src/VersionHistory/OpenedVersionStatusChip.js
+++ newIDE/app/src/VersionHistory/OpenedVersionStatusChip.js
@@ -2,13 +2,13 @@
 
 import * as React from 'react';
 import { I18n } from '@lingui/react';
+import { t } from '@lingui/macro';
 
 import Text from '../UI/Text';
 import GDevelopThemeContext from '../UI/Theme/GDevelopThemeContext';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Cross from '../UI/CustomSvgIcons/Cross';
-import History from '../UI/CustomSvgIcons/History';
 import { Spacer } from '../UI/Grid';
 import { textEllipsisStyle } from '../UI/TextEllipsis';
 import { type OpenedVersionStatus } from '.';
@@ -60,13 +60,14 @@ const OpenedVersionStatusChip = ({
             ),
           }}
         >
-          <History />
-          <Spacer />
           <Text noMargin color="inherit">
-            {i18n.date(Date.parse(openedVersionStatus.version.createdAt), {
-              dateStyle: 'medium',
-              timeStyle: 'short',
-            })}
+            {openedVersionStatus.status === 'saving'
+              ? i18n._(t`Saving...`)
+              : i18n.date(Date.parse(openedVersionStatus.version.createdAt), {
+                  dateStyle: 'medium',
+                  timeStyle: 'short',
+                }) +
+                (openedVersionStatus.status === 'unsavedChanges' ? '*' : '')}
           </Text>
           <Spacer />
           <ButtonBase classes={classes} onClick={onClickClose}>

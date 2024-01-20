866455f823f8176702a4f272c2c2e9431f4bcb34
Florian Rival <Florian.rival@gmail.com>
2024-01-20 13:21:31 +0100

    [WIP] Use eSigner CKA Tool to codesign on Windows

---
+++ newIDE/electron-app/electron-builder-config.js
@@ -0,0 +1,91 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  appId: 'com.gdevelop-app.ide',
+  extraResources: [
+    {
+      from: '../app/resources/in-app-tutorials',
+      to: 'in-app-tutorials',
+    },
+    {
+      from: '../app/resources/GDJS',
+      to: 'GDJS',
+    },
+    {
+      from: '../app/resources/preview_node_modules',
+      to: 'preview_node_modules',
+    },
+  ],
+  linux: {
+    target: [
+      {
+        target: 'AppImage',
+        arch: ['x64', 'arm64'],
+      },
+      {
+        target: 'zip',
+        arch: ['x64', 'arm64'],
+      },
+      {
+        target: 'deb',
+        arch: ['x64', 'arm64'],
+      },
+    ],
+  },
+  mac: {
+    category: 'public.app-category.developer-tools',
+    hardenedRuntime: true,
+    entitlements: './build/entitlements.mac.inherit.plist',
+    target: {
+      target: 'default',
+      arch: ['universal'],
+    },
+  },
+  win: {
+    executableName: 'GDevelop',
+  },
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true,
+  },
+  appx: {
+    publisherDisplayName: 'GDevelop game engine',
+    displayName: 'GDevelop',
+    publisher: 'CN=B13CB8D3-97AA-422C-A394-0EE51B9ACAD3',
+    identityName: 'GDevelopgameengine.GDevelop',
+    backgroundColor: '#524F9C',
+    languages: [
+      'EN-US',
+      'ZH-HANS',
+      'DE',
+      'IT',
+      'JA',
+      'PT-BR',
+      'RU',
+      'ES',
+      'FR',
+      'SL',
+    ],
+  },
+  afterSign: 'scripts/electron-builder-after-sign.js',
+  publish: [
+    {
+      provider: 'github',
+    },
+  ],
+};
+
+if (
+  process.env.GD_SIGNTOOL_SUBJECT_NAME &&
+  process.env.GD_SIGNTOOL_THUMBPRINT
+) {
+  config.win.certificateSubjectName = process.env.GD_SIGNTOOL_SUBJECT_NAME;
+  config.win.certificateSha1 = process.env.GD_SIGNTOOL_THUMBPRINT;
+  console.log('ℹ️ Set Windows build signing options:', config.win);
+} else {
+  console.log('ℹ️ No Windows build signing options set.');
+}
+
+module.exports = config;

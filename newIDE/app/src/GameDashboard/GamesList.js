63570fd3c8bf7713b79f9465ae821f33c9be05b6
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2024-02-27 12:39:06 +0100

    Paginate list of games (#6387)

--- newIDE/app/src/GameDashboard/GamesList.js
+++ newIDE/app/src/GameDashboard/GamesList.js
@@ -1,5 +1,5 @@
 // @flow
-import { t } from '@lingui/macro';
+import { Trans, t } from '@lingui/macro';
 import * as React from 'react';
 import AuthenticatedUserContext from '../Profile/AuthenticatedUserContext';
 import { type Game, registerGame } from '../Utils/GDevelopServices/Game';
@@ -17,28 +17,46 @@ import {
   getFuseSearchQueryForSimpleArray,
   sharedFuseConfiguration,
 } from '../UI/Search/UseSearchStructuredItem';
+import IconButton from '../UI/IconButton';
+import ChevronArrowLeft from '../UI/CustomSvgIcons/ChevronArrowLeft';
+import ChevronArrowRight from '../UI/CustomSvgIcons/ChevronArrowRight';
+import { Column, Line } from '../UI/Grid';
+import Text from '../UI/Text';
+import Paper from '../UI/Paper';
+import BackgroundText from '../UI/BackgroundText';
+
+const pageSize = 10;
+
+const styles = { noGameMessageContainer: { padding: 10 } };
 
 const getGamesToDisplay = ({
   project,
   games,
   searchText,
   searchClient,
+  currentPage,
 }: {|
   project: ?gdProject,
   games: Array<Game>,
   searchText: string,
   searchClient: Fuse,
+  currentPage: number,
 |}): Array<Game> => {
+  if (searchText) {
+    const searchResults = searchClient.search(
+      getFuseSearchQueryForSimpleArray(searchText)
+    );
+    return searchResults.map(result => result.item);
+  }
   const projectUuid = project ? project.getProjectUuid() : null;
   const thisGame = games.find(game => !!projectUuid && game.id === projectUuid);
   const orderedGames = thisGame
     ? [thisGame, ...games.filter(game => game.id !== thisGame.id)]
     : games;
-  if (!searchText) return orderedGames;
-  const searchResults = searchClient.search(
-    getFuseSearchQueryForSimpleArray(searchText)
+  return orderedGames.slice(
+    currentPage * pageSize,
+    (currentPage + 1) * pageSize
   );
-  return searchResults.map(result => result.item);
 };
 
 type Props = {|
@@ -70,6 +88,7 @@ const GamesList = ({
   const [displayedGames, setDisplayedGames] = React.useState<Array<Game>>(
     games
   );
+  const [currentPage, setCurrentPage] = React.useState<number>(0);
 
   const searchClient = React.useMemo(
     () =>
@@ -168,24 +187,33 @@ const GamesList = ({
     ]
   );
 
-  const getGamesToDisplayDebounced = useDebounce(() => {
-    setDisplayedGames(
-      getGamesToDisplay({
-        project,
-        games,
-        searchText,
-        searchClient,
-      })
-    );
-  }, 250);
+  const getGamesToDisplayDebounced = useDebounce(
+    () => {
+      setDisplayedGames(
+        getGamesToDisplay({
+          project,
+          games,
+          searchText,
+          searchClient,
+          currentPage,
+        })
+      );
+    },
+    // Use debounce when searching for a game.
+    // Keep a lower debounce when changing pages so that the UI does not refresh until the
+    // user stops changing pages, giving a sense of rapidity.
+    searchText ? 250 : 150
+  );
 
   // Refresh games to display when:
   // - search text changes (user input)
   // - games change (refresh following an update for instance)
+  // - user changes page
   React.useEffect(getGamesToDisplayDebounced, [
     getGamesToDisplayDebounced,
     searchText,
     games,
+    currentPage,
   ]);
 
   const projectUuid = project ? project.getProjectUuid() : null;
@@ -199,14 +227,38 @@ const GamesList = ({
           onGameRegistered={onRefreshGames}
         />
       )}
-      <SearchBar
-        value={searchText}
-        onChange={setSearchText}
-        // Search is triggered on each search text change
-        onRequestSearch={() => {}}
-        placeholder={t`Search by name`}
-      />
-      {displayedGames &&
+      <Line noMargin alignItems="center">
+        <Column noMargin expand>
+          <SearchBar
+            value={searchText}
+            onChange={setSearchText}
+            // Search is triggered on each search text change
+            onRequestSearch={() => {}}
+            placeholder={t`Search by name`}
+          />
+        </Column>
+        <IconButton
+          tooltip={t`Previous page`}
+          onClick={() => setCurrentPage(currentPage => currentPage - 1)}
+          disabled={!!searchText || currentPage === 0}
+        >
+          <ChevronArrowLeft />
+        </IconButton>
+        <Text noMargin style={{ opacity: searchText ? 0.6 : 1 }}>
+          {searchText ? 1 : currentPage + 1}
+        </Text>
+        <IconButton
+          tooltip={t`Next page`}
+          onClick={() => setCurrentPage(currentPage => currentPage + 1)}
+          disabled={
+            !!searchText || (currentPage + 1) * pageSize >= games.length
+          }
+        >
+          <ChevronArrowRight />
+        </IconButton>
+      </Line>
+
+      {displayedGames.length > 0 ? (
         displayedGames.map(game => (
           <GameCard
             key={game.id}
@@ -218,7 +270,20 @@ const GamesList = ({
             }}
             onUpdateGame={onRefreshGames}
           />
-        ))}
+        ))
+      ) : !!searchText ? (
+        <Column expand noMargin>
+          <Paper
+            variant="outlined"
+            background="dark"
+            style={styles.noGameMessageContainer}
+          >
+            <BackgroundText>
+              <Trans>No game matching your search.</Trans>
+            </BackgroundText>
+          </Paper>
+        </Column>
+      ) : null}
     </ColumnStackLayout>
   );
 };

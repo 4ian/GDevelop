3d3d36e08b3ecd0b95af9c40165fe81f8fd858e5
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-03-26 17:51:22 +0100

    Replace many stories with a single story with arguments

--- newIDE/app/src/stories/componentStories/Profile/Subscription/SubscriptionDialog.stories.js
+++ newIDE/app/src/stories/componentStories/Profile/Subscription/SubscriptionDialog.stories.js
@@ -4,17 +4,12 @@ import { action } from '@storybook/addon-actions';
 
 import muiDecorator from '../../../ThemeDecorator';
 import paperDecorator from '../../../PaperDecorator';
-import AuthenticatedUserContext, {
-  type AuthenticatedUser,
-} from '../../../../Profile/AuthenticatedUserContext';
+import AuthenticatedUserContext from '../../../../Profile/AuthenticatedUserContext';
 import {
   fakeAuthenticatedUserLoggingIn,
   fakeSilverAuthenticatedUser,
-  fakeSilverButCancelAtPeriodEndAuthenticatedUser,
   fakeAuthenticatedUserWithNoSubscription,
   fakeNotAuthenticatedUser,
-  fakeAuthenticatedUserWithValidSilverRedemptionCode,
-  fakeAuthenticatedUserWithExpiredSilverRedemptionCode,
   fakeAuthenticatedUserWithLegacyIndieSubscription,
   fakeGoldAuthenticatedUser,
   fakeAuthenticatedUserWithLegacyProSubscription,
@@ -23,7 +18,6 @@ import {
   subscriptionPlansWithPricingSystems,
 } from '../../../../fixtures/GDevelopServicesTestData';
 import SubscriptionDialog from '../../../../Profile/Subscription/SubscriptionDialog';
-import { type SubscriptionType } from '../../../../Profile/Subscription/SubscriptionSuggestionContext';
 import AlertProvider from '../../../../UI/Alert/AlertProvider';
 import { getAvailableSubscriptionPlansWithPrices } from '../../../../Utils/UseSubscriptionPlans';
 
@@ -31,15 +25,142 @@ export default {
   title: 'Subscription/SubscriptionDialog',
   component: SubscriptionDialog,
   decorators: [paperDecorator, muiDecorator],
+  argTypes: {
+    authenticated: {
+      options: ['no', 'loading', 'yes'],
+      control: { type: 'radio' },
+    },
+    willCancelAtPeriodEndOrIsExpired: {
+      control: { type: 'boolean' },
+    },
+    filter: {
+      options: ['none', 'individual', 'team', 'education'],
+      control: { type: 'radio' },
+    },
+    userSubscriptionId: {
+      options: [
+        'none',
+        'gdevelop_silver',
+        'gdevelop_gold',
+        'gdevelop_startup',
+        'gdevelop_education',
+        'gdevelop_indie',
+        'gdevelop_pro',
+      ],
+      control: { type: 'radio' },
+    },
+    pricingSystem: {
+      options: [
+        'monthly',
+        'yearly',
+        'redeemed',
+        'team member',
+        'manually added',
+      ],
+      control: { type: 'radio' },
+    },
+  },
 };
 
-const SubscriptionDialogWrapper = ({
-  authenticatedUser,
+export const Default = ({
+  authenticated,
+  willCancelAtPeriodEndOrIsExpired,
   filter,
-}: {
-  authenticatedUser: AuthenticatedUser,
-  filter?: ?SubscriptionType,
-}) => {
+  userSubscriptionId,
+  pricingSystem,
+}: {|
+  authenticated?: 'no' | 'loading' | 'yes',
+  willCancelAtPeriodEndOrIsExpired?: boolean,
+  filter?: 'none' | 'individual' | 'team' | 'education',
+  userSubscriptionId?:
+    | 'none'
+    | 'gdevelop_silver'
+    | 'gdevelop_gold'
+    | 'gdevelop_startup'
+    | 'gdevelop_education'
+    | 'gdevelop_indie'
+    | 'gdevelop_pro',
+  pricingSystem?:
+    | 'monthly'
+    | 'yearly'
+    | 'redeemed'
+    | 'team member'
+    | 'manually added',
+|}) => {
+  console.log(authenticated)
+  const authenticatedUser =
+    authenticated === 'no'
+      ? fakeNotAuthenticatedUser
+      : authenticated === 'loading'
+      ? fakeAuthenticatedUserLoggingIn
+      : userSubscriptionId === 'none'
+      ? fakeAuthenticatedUserWithNoSubscription
+      : userSubscriptionId === 'gdevelop_silver'
+      ? fakeSilverAuthenticatedUser
+      : userSubscriptionId === 'gdevelop_gold'
+      ? fakeGoldAuthenticatedUser
+      : userSubscriptionId === 'gdevelop_startup'
+      ? fakeStartupAuthenticatedUser
+      : userSubscriptionId === 'gdevelop_education'
+      ? fakeAuthenticatedUserWithEducationPlan
+      : userSubscriptionId === 'gdevelop_indie'
+      ? fakeAuthenticatedUserWithLegacyIndieSubscription
+      : userSubscriptionId === 'gdevelop_pro'
+      ? fakeAuthenticatedUserWithLegacyProSubscription
+      : fakeNotAuthenticatedUser;
+
+  if (authenticatedUser.subscription) {
+    if (pricingSystem === 'redeemed') {
+      authenticatedUser.subscription.pricingSystemId = 'REDEMPTION_CODE';
+      authenticatedUser.subscription.redemptionCode = 'test-123-code';
+      // $FlowIgnore
+      authenticatedUser.subscription.redemptionCodeValidUntil =
+        Date.now() +
+        (!!willCancelAtPeriodEndOrIsExpired ? -1 : 1) * 7 * 24 * 3600 * 1000;
+    } else if (pricingSystem === 'team member') {
+      authenticatedUser.subscription.pricingSystemId = 'TEAM_MEMBER';
+    } else if (pricingSystem === 'manually added') {
+      authenticatedUser.subscription.pricingSystemId = 'MANUALLY_ADDED';
+    } else {
+      authenticatedUser.subscription.cancelAtPeriodEnd = !!willCancelAtPeriodEndOrIsExpired;
+      if (userSubscriptionId === 'gdevelop_silver') {
+        if (pricingSystem === 'yearly') {
+          authenticatedUser.subscription.pricingSystemId =
+            'silver_1year_3599EUR';
+        } else {
+          authenticatedUser.subscription.pricingSystemId =
+            'silver_1month_499EUR';
+        }
+      } else if (userSubscriptionId === 'gdevelop_gold') {
+        if (pricingSystem === 'yearly') {
+          authenticatedUser.subscription.pricingSystemId = 'gold_1year_7199EUR';
+        } else {
+          authenticatedUser.subscription.pricingSystemId = 'gold_1month_999EUR';
+        }
+      } else if (userSubscriptionId === 'gdevelop_startup') {
+        if (pricingSystem === 'yearly') {
+          authenticatedUser.subscription.pricingSystemId =
+            'startup_1year_30900EUR';
+        } else {
+          authenticatedUser.subscription.pricingSystemId =
+            'startup_1month_3000EUR';
+        }
+      } else if (userSubscriptionId === 'gdevelop_education') {
+        if (pricingSystem === 'yearly') {
+          authenticatedUser.subscription.pricingSystemId =
+            'education_1year_2999EUR';
+        } else {
+          authenticatedUser.subscription.pricingSystemId =
+            'education_1month_299EUR';
+        }
+      } else if (userSubscriptionId === 'gdevelop_indie') {
+        authenticatedUser.subscription.pricingSystemId = 'indie_1month';
+      } else if (userSubscriptionId === 'gdevelop_pro') {
+        authenticatedUser.subscription.pricingSystemId = 'pro_1month';
+      }
+    }
+  }
+
   const { subscription: userSubscription } = authenticatedUser;
   const userLegacySubscriptionPlanWithPricingSystem = userSubscription
     ? subscriptionPlansWithPricingSystems.find(
@@ -62,94 +183,9 @@ const SubscriptionDialogWrapper = ({
           }
           onClose={() => action('on close')()}
           analyticsMetadata={{ reason: 'Debugger' }}
-          filter={filter}
+          filter={filter === 'none' ? undefined : filter}
         />
       </AuthenticatedUserContext.Provider>
     </AlertProvider>
   );
 };
-
-export const NotAuthenticated = () => (
-  <SubscriptionDialogWrapper authenticatedUser={fakeNotAuthenticatedUser} />
-);
-
-export const AuthenticatedButLoading = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserLoggingIn}
-  />
-);
-
-export const WithNoSubscription = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithNoSubscription}
-  />
-);
-
-export const FilteredOnIndividual = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithNoSubscription}
-    filter="individual"
-  />
-);
-
-export const FilteredOnTeam = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithNoSubscription}
-    filter="team"
-  />
-);
-
-export const FilteredOnEducation = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithNoSubscription}
-    filter="education"
-  />
-);
-
-export const WithSilverSubscription = () => (
-  <SubscriptionDialogWrapper authenticatedUser={fakeSilverAuthenticatedUser} />
-);
-
-export const WithSilverButCancelAtPeriodEndSubscription = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeSilverButCancelAtPeriodEndAuthenticatedUser}
-  />
-);
-
-export const WithGoldSubscription = () => (
-  <SubscriptionDialogWrapper authenticatedUser={fakeGoldAuthenticatedUser} />
-);
-
-export const WithStartupSubscription = () => (
-  <SubscriptionDialogWrapper authenticatedUser={fakeStartupAuthenticatedUser} />
-);
-
-export const WithEducationPlan = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithEducationPlan}
-  />
-);
-
-export const WithValidSilverRedemptionCodeSubscription = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithValidSilverRedemptionCode}
-  />
-);
-
-export const WithExpiredSilverRedemptionCodeSubscription = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithExpiredSilverRedemptionCode}
-  />
-);
-
-export const WithLegacyIndieSubscription = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithLegacyIndieSubscription}
-  />
-);
-
-export const WithLegacyProSubscription = () => (
-  <SubscriptionDialogWrapper
-    authenticatedUser={fakeAuthenticatedUserWithLegacyProSubscription}
-  />
-);

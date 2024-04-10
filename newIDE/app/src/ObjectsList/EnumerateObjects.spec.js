9652b1e292ba1fd0218e8fca35e6db09307488b0
Alex <alexandre@gdevelop.io>
2024-04-10 16:01:33 +0200

    Update test

--- newIDE/app/src/ObjectsList/EnumerateObjects.spec.js
+++ newIDE/app/src/ObjectsList/EnumerateObjects.spec.js
@@ -17,9 +17,9 @@ describe('EnumerateObjects', () => {
       allObjectsList,
     } = enumerateObjects(project, testLayout);
 
-    expect(containerObjectsList).toHaveLength(20);
+    expect(containerObjectsList).toHaveLength(21);
     expect(projectObjectsList).toHaveLength(2);
-    expect(allObjectsList).toHaveLength(22);
+    expect(allObjectsList).toHaveLength(23);
   });
 
   it('can enumerate objects with a filter on object type', () => {

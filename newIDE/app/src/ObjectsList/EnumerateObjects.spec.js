447da79dcc1db1640677d140f4b5bfac1c1564f5
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-04-12 10:52:16 +0200

    Fix tests

--- newIDE/app/src/ObjectsList/EnumerateObjects.spec.js
+++ newIDE/app/src/ObjectsList/EnumerateObjects.spec.js
@@ -17,9 +17,9 @@ describe('EnumerateObjects', () => {
       allObjectsList,
     } = enumerateObjects(project, testLayout);
 
-    expect(containerObjectsList).toHaveLength(21);
+    expect(containerObjectsList).toHaveLength(22);
     expect(projectObjectsList).toHaveLength(2);
-    expect(allObjectsList).toHaveLength(23);
+    expect(allObjectsList).toHaveLength(24);
   });
 
   it('can enumerate objects with a filter on object type', () => {

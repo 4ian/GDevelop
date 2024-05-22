b90f39b48d3908da086351cbf16756ff65d31f86
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-05-22 18:25:25 +0200

    Add form

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/EducationMarketingSection/index.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/EducationMarketingSection/index.js
@@ -7,10 +7,16 @@ import Text from '../../../../UI/Text';
 import { CardWidget } from '../CardWidget';
 import {
   ColumnStackLayout,
+  LineStackLayout,
   ResponsiveLineStackLayout,
 } from '../../../../UI/Layout';
 import RaisedButton from '../../../../UI/RaisedButton';
 import { SubscriptionSuggestionContext } from '../../../../Profile/Subscription/SubscriptionSuggestionContext';
+import Form from '../../../../UI/Form';
+import TextField from '../../../../UI/TextField';
+import { emailRegex } from '../../../../Profile/ForgotPasswordDialog';
+import { Line } from '../../../../UI/Grid';
+import { useResponsiveWindowSize } from '../../../../UI/Responsive/ResponsiveWindowMeasurer';
 
 export type EducationForm = {|
   firstName: string,
@@ -31,6 +37,8 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
   },
+  postersImage: { maxWidth: 300 },
+  mobilePostersImage: { width: '100%' },
 };
 
 type Props = {|
@@ -38,7 +46,9 @@ type Props = {|
   onChangeForm: EducationForm => void,
 |};
 
-const EducationMarketingSection = (props: Props) => {
+const EducationMarketingSection = ({ form, onChangeForm }: Props) => {
+  const { isMobile } = useResponsiveWindowSize();
+  const [isEmailValid, setIsEmailValid] = React.useState<boolean>(true);
   const { openSubscriptionDialog } = React.useContext(
     SubscriptionSuggestionContext
   );
@@ -91,6 +101,104 @@ const EducationMarketingSection = (props: Props) => {
             it’s a helpful guide to your STEM related courses.
           </Trans>
         </Text>
+        <ResponsiveLineStackLayout noColumnMargin useLargeSpacer>
+          <img
+            src="res/education-posters.png"
+            alt="GDevelop education posters preview"
+            style={isMobile ? styles.mobilePostersImage : styles.postersImage}
+          />
+          <Form
+            onSubmit={() => console.log('submit')}
+            autoComplete="on"
+            name="education-form"
+          >
+            <ColumnStackLayout noMargin>
+              <Text size="block-title" noMargin>
+                <Trans>Get a free sample in your email</Trans>
+              </Text>
+              <LineStackLayout noMargin>
+                <TextField
+                  value={form.firstName}
+                  floatingLabelText={<Trans>First name</Trans>}
+                  fullWidth
+                  type="text"
+                  required
+                  onChange={(e, value) => {
+                    onChangeForm({ ...form, firstName: value });
+                  }}
+                  onBlur={event => {
+                    onChangeForm({
+                      ...form,
+                      firstName: event.currentTarget.value.trim(),
+                    });
+                  }}
+                />
+                <TextField
+                  value={form.lastName}
+                  floatingLabelText={<Trans>Last name</Trans>}
+                  fullWidth
+                  type="text"
+                  required
+                  onChange={(e, value) => {
+                    onChangeForm({ ...form, lastName: value });
+                  }}
+                  onBlur={event => {
+                    onChangeForm({
+                      ...form,
+                      lastName: event.currentTarget.value.trim(),
+                    });
+                  }}
+                />
+              </LineStackLayout>
+              <TextField
+                required
+                value={form.email}
+                floatingLabelText={<Trans>Email</Trans>}
+                onChange={(e, value) => {
+                  if (!isEmailValid) setIsEmailValid(true);
+                  onChangeForm({ ...form, email: value });
+                }}
+                errorText={
+                  !isEmailValid ? (
+                    <Trans>Invalid email address.</Trans>
+                  ) : (
+                    undefined
+                  )
+                }
+                type="email"
+                fullWidth
+                onBlur={event => {
+                  const trimmedEmail = event.currentTarget.value.trim();
+                  onChangeForm({ ...form, email: trimmedEmail });
+                  setIsEmailValid(emailRegex.test(trimmedEmail));
+                }}
+              />
+              <TextField
+                value={form.schoolName}
+                floatingLabelText={<Trans>School name</Trans>}
+                fullWidth
+                type="text"
+                required
+                onChange={(e, value) => {
+                  onChangeForm({ ...form, schoolName: value });
+                }}
+                onBlur={event => {
+                  onChangeForm({
+                    ...form,
+                    schoolName: event.currentTarget.value.trim(),
+                  });
+                }}
+              />
+              <Line noMargin justifyContent="flex-end">
+                <RaisedButton
+                  primary
+                  label={<Trans>Send</Trans>}
+                  onClick={() => console.log('send')}
+                />
+              </Line>
+            </ColumnStackLayout>
+          </Form>
+        </ResponsiveLineStackLayout>
       </SectionRow>
     </SectionContainer>
   );

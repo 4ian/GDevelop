54401a9720567d38ba2f4c387a3ad9e3fdbd589f
Davy Hélard <davy.helard@gmail.com>
2024-05-27 16:52:05 +0200

    Header date

--- Core/GDCore/IDE/Events/EventsVariableInstructionTypeSwitcher.h
+++ Core/GDCore/IDE/Events/EventsVariableInstructionTypeSwitcher.h
@@ -1,6 +1,6 @@
 /*
  * GDevelop Core
- * Copyright 2008-2016 Florian Rival (Florian.Rival@gmail.com). All rights
+ * Copyright 2008-2024 Florian Rival (Florian.Rival@gmail.com). All rights
  * reserved. This project is released under the MIT License.
  */
 #pragma once

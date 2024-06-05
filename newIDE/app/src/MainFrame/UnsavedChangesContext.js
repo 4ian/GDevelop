0aeb7fdd1953610c8dc6f9b79c76c2497c386303
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-06-05 11:20:15 +0200

    Add changes count and last save time to unsaved changes context

--- newIDE/app/src/MainFrame/UnsavedChangesContext.js
+++ newIDE/app/src/MainFrame/UnsavedChangesContext.js
@@ -5,12 +5,16 @@ export type UnsavedChanges = {|
   hasUnsavedChanges: boolean,
   sealUnsavedChanges: () => void,
   triggerUnsavedChanges: () => void,
+  changesCount: number,
+  lastSaveTime: number | null,
 |};
 
 const initialState: UnsavedChanges = {
   hasUnsavedChanges: false,
   sealUnsavedChanges: () => {},
   triggerUnsavedChanges: () => {},
+  changesCount: 0,
+  lastSaveTime: null,
 };
 
 const UnsavedChangesContext = React.createContext<UnsavedChanges>(initialState);
@@ -25,11 +29,17 @@ export const UnsavedChangesContextProvider = (props: Props) => {
   const [hasUnsavedChanges, setHasUnsavedChanges] = React.useState<boolean>(
     false
   );
+  const [changesCount, setChangesCount] = React.useState<number>(0);
+  const [lastSaveTime, setLastSaveTime] = React.useState<number | null>(null);
+
   const triggerUnsavedChanges = (): void => {
+    setChangesCount(changesCount_ => changesCount_ + 1);
     if (!hasUnsavedChanges) setHasUnsavedChanges(true);
   };
 
   const sealUnsavedChanges = (): void => {
+    setChangesCount(0);
+    setLastSaveTime(Date.now());
     if (hasUnsavedChanges) setHasUnsavedChanges(false);
   };
 
@@ -39,6 +49,8 @@ export const UnsavedChangesContextProvider = (props: Props) => {
         hasUnsavedChanges,
         triggerUnsavedChanges: triggerUnsavedChanges,
         sealUnsavedChanges: sealUnsavedChanges,
+        changesCount,
+        lastSaveTime,
       }}
     >
       {props.children}

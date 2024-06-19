0decf49a125c9c7adcb5fa262957996d036e0766
Aurélien Vivet <bouh.vivez@gmail.com>
2024-06-19 19:05:50 +0200

    coucou clem

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/InAppTutorials/Icons/FireAbullet.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/InAppTutorials/Icons/FireAbullet.js
@@ -10,50 +10,50 @@ export default React.memo(props => (
     fill="none"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M21.6799 84.2797C21.9531 84.4687 22.3125 84.4032 22.5015 84.13L22.2111 87.9229L30.6625 87.5767L31.5081 83.046C31.2981 83.3495 31.394 83.7299 31.6671 83.9189C31.9403 84.1078 32.2997 84.0423 32.5097 83.7388L34.6931 80.5824C36.1418 78.4882 35.6179 75.6129 33.5237 74.1642C31.4295 72.7156 28.5542 73.2395 27.1055 75.3336L21.4999 83.4372C21.3109 83.7103 21.4068 84.0908 21.6799 84.2797Z"
       fill="#F84848"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M13.8304 102.633C16.7744 104.669 20.7793 103.94 22.8159 100.996L24.8524 98.0517C25.0413 97.7786 24.9968 97.3888 24.7237 97.1999C24.4505 97.0109 24.0398 97.0858 23.8508 97.3589L25.2668 95.3768L16.6473 89.4142L15.2313 91.3963C15.4203 91.1232 15.3758 90.7334 15.1026 90.5445C14.8295 90.3555 14.4187 90.4303 14.2298 90.7035L12.1932 93.6475C10.1567 96.5915 10.9168 100.617 13.8304 102.633Z"
       fill="#A19DAE"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M64.3828 113.82C61.1417 115.975 60.3303 111.959 61.198 104.347C60.9248 104.158 60.829 103.778 61.039 103.474L63.2224 100.318C64.6711 98.2235 67.5464 97.6996 69.6406 99.1482C71.7348 100.597 72.289 103.493 70.8404 105.587L65.2347 113.691C65.0458 113.964 64.656 114.009 64.3828 113.82Z"
       fill="#F84848"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M49.9473 127.617C47.0337 125.601 46.2736 121.575 48.3101 118.632L50.3466 115.688C50.5356 115.414 50.9463 115.34 51.2195 115.528C51.4926 115.717 51.5675 116.128 51.3785 116.401L53.674 113.278L62.2935 119.24L59.9677 122.343C60.1566 122.07 60.5674 121.995 60.8405 122.184C61.1137 122.373 61.1885 122.784 60.9996 123.057L58.9631 126.001C56.9266 128.945 52.8913 129.653 49.9473 127.617Z"
       fill="#A19DAE"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M65.993 119.017L59.3896 99.3715C51.9144 101.29 49.125 106.945 47.183 112.736L28.7603 99.9921C36.0605 98.1315 40.3044 93.6182 36.8464 83.9566L38.0648 83.9468C38.4032 83.9117 38.6738 83.6501 38.6597 83.2814C38.6549 82.9639 38.363 82.6723 38.0246 82.7074L15.8702 83.4019C15.6858 83.409 15.4898 83.4978 15.3848 83.6495L11.7107 88.9608C11.5217 89.234 11.5966 89.6448 11.8697 89.8337L62.6459 124.958C62.919 125.147 63.3298 125.072 63.5187 124.799L67.1929 119.488C67.2978 119.336 67.3118 119.121 67.2533 118.946L60.1733 97.8494C60.0565 97.4993 59.711 97.3501 59.3819 97.4365C59.0832 97.544 58.8826 97.8989 59.0204 98.2186L65.993 119.017Z"
       fill="#CDCDCD"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M45.8014 113.306L28.6838 101.465C28.4713 101.318 28.3639 101.019 28.4501 100.765C31.7236 91.3621 37.3404 82.0099 43.8778 75.0893C47.4545 71.2812 51.1482 68.4066 54.9076 66.4749C59.0287 64.3447 63.1082 63.4423 67.0436 63.7862C67.2373 63.8305 67.4194 63.9565 67.5595 64.1432C69.2694 67.7043 69.8638 71.8399 69.324 76.4475C68.8121 80.6257 67.425 85.0959 65.1534 89.8068C60.9834 98.3649 54.2132 106.919 46.5389 113.278C46.3313 113.448 46.0442 113.474 45.8014 113.306Z"
       fill="#E4E4E4"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M41.4916 110.325L32.9936 104.446C32.7204 104.257 32.6759 103.868 32.8649 103.594L45.2309 85.7181C47.0575 83.0776 50.6516 82.4227 53.2618 84.2283C55.9022 86.0548 56.5571 89.649 54.7305 92.2895L42.3645 110.166C42.1756 110.439 41.7648 110.514 41.4916 110.325Z"
       fill="#B9B9B9"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M69.324 76.4476C69.8638 71.84 69.2694 67.7043 67.5595 64.1432C67.4194 63.9565 67.2373 63.8305 67.0436 63.7863C63.1083 63.4423 59.0287 64.3448 54.9076 66.475C54.4945 66.6828 54.1328 66.8814 53.7198 67.0893C55.9052 69.1844 58.3591 71.1511 61.0299 72.9986C63.7007 74.8462 66.4368 76.4697 69.1378 77.7547C69.217 77.3159 69.2752 76.9074 69.324 76.4476Z"
       fill="#F8485E"
     />
@@ -88,48 +88,48 @@ export default React.memo(props => (
       <path
         d="M74.4037 93.3096L78.7656 85.9839"
         stroke="#00F0FF"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </g>
     <g filter="url(#filter1_dd_153_2)">
       <path
         d="M38 68.7202L42.8935 61.7383"
         stroke="#00F0FF"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </g>
     <g filter="url(#filter2_dd_153_2)">
       <path
         d="M81.819 80.856L86.181 73.5303"
         stroke="#00F0FF"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </g>
     <g filter="url(#filter3_dd_153_2)">
       <path
         d="M46.3189 56.851L51.2124 49.8691"
         stroke="#00F0FF"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </g>
     <g filter="url(#filter4_dd_153_2)">
       <path
         d="M89.2344 68.4023L93.5963 61.0766"
         stroke="#00F0FF"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </g>
     <g filter="url(#filter5_dd_153_2)">
       <path
         d="M54.6378 44.9819L59.5313 38"
         stroke="#00F0FF"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </g>
     <defs>
@@ -140,9 +140,9 @@ export default React.memo(props => (
         width="16.4113"
         height="19.2759"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
@@ -193,9 +193,9 @@ export default React.memo(props => (
         width="16.9466"
         height="18.9277"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
@@ -246,9 +246,9 @@ export default React.memo(props => (
         width="16.4113"
         height="19.2759"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
@@ -299,9 +299,9 @@ export default React.memo(props => (
         width="16.9466"
         height="18.9277"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
@@ -352,9 +352,9 @@ export default React.memo(props => (
         width="16.4113"
         height="19.2759"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
@@ -405,9 +405,9 @@ export default React.memo(props => (
         width="16.9466"
         height="18.9277"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"

e1a4f88ba6fd23dedf97d90c1721c41756751f5b
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2024-07-31 11:48:07 +0200

    Implement new design for education marketing form (#6829)

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/EducationMarketingSection/index.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/EducationMarketingSection/index.js
@@ -15,12 +15,16 @@ import { SubscriptionSuggestionContext } from '../../../../Profile/Subscription/
 import Form from '../../../../UI/Form';
 import TextField from '../../../../UI/TextField';
 import { emailRegex } from '../../../../Profile/ForgotPasswordDialog';
-import { Line } from '../../../../UI/Grid';
+import { Column, Line, Spacer } from '../../../../UI/Grid';
 import { useResponsiveWindowSize } from '../../../../UI/Responsive/ResponsiveWindowMeasurer';
 import type { EducationFormStatus, EducationForm } from '../UseEducationForm';
 import PlaceholderLoader from '../../../../UI/PlaceholderLoader';
 import FlatButton from '../../../../UI/FlatButton';
 import PlaceholderError from '../../../../UI/PlaceholderError';
+import ShinyCrown from '../../../../UI/CustomSvgIcons/ShinyCrown';
+import Education from '../../../../Profile/Subscription/Icons/Education';
+import Window from '../../../../Utils/Window';
+import GDevelopThemeContext from '../../../../UI/Theme/GDevelopThemeContext';
 
 const styles = {
   banner: {
@@ -34,12 +38,20 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
   },
+  educationIcon: { width: 100, height: 100 },
   postersImage: { height: 260, aspectRatio: '1.15' },
   mobilePostersImage: { width: '100%' },
   rightPartContainer: { flex: 2, maxWidth: 500, display: 'flex' },
   mobileFooter: {
     // Leave space below form
     height: 150,
+  },
+  freeTag: {
+    alignSelf: 'flex-start',
+    padding: '4px 6px',
+    border: '1px solid white',
+    borderRadius: 6,
+    color: 'white',
   },
 };
 
@@ -69,6 +81,7 @@ const EducationMarketingSection = ({
   onLogin,
 }: Props) => {
   const { isMobile } = useResponsiveWindowSize();
+  const gdevelopTheme = React.useContext(GDevelopThemeContext);
   const [isEmailValid, setIsEmailValid] = React.useState<boolean>(true);
   const { openSubscriptionDialog } = React.useContext(
     SubscriptionSuggestionContext
@@ -203,53 +216,88 @@ const EducationMarketingSection = ({
       </ColumnStackLayout>
     </Form>
   );
+
   return (
     <SectionContainer title={<Trans>Classrooms</Trans>}>
       <SectionRow>
         <CardWidget size="banner">
           <div style={styles.banner}>
-            <ResponsiveLineStackLayout noColumnMargin>
-              <img
-                src="res/hero-playing-with-kids.svg"
-                alt="Red hero playing with kids"
-              />
-              <ColumnStackLayout alignItems="flex-start">
+            <ResponsiveLineStackLayout noColumnMargin alignItems="center">
+              <Column noMargin alignItems="center">
+                <Education style={styles.educationIcon} />
+              </Column>
+              <ColumnStackLayout
+                expand
+                alignItems={isMobile ? 'stretch' : 'flex-start'}
+              >
                 <Text size="block-title" noMargin>
-                  <Trans>The best way to teach and learn</Trans>
+                  <Trans>Purchase the Education subscription</Trans>
                 </Text>
                 <Text noMargin align="left">
                   <Trans>
-                    Discover the best way to teach students the principles of
-                    game design, and critical thinking for game development.{' '}
+                    The Education subscription gives access to GDevelop's Game
+                    Development curriculum. Co-created with teachers and
+                    institutions, it’s a ready-to-use, proven way to implement
+                    STEM in your classroom.
                   </Trans>
                 </Text>
+                <Spacer />
+                <ResponsiveLineStackLayout noColumnMargin noMargin>
+                  <div style={styles.buttonContainer}>
+                    <FlatButton
+                      primary
+                      fullWidth
+                      onClick={() => {
+                        Window.openExternalURL('https://gdevelop.io/education');
+                      }}
+                      label={<Trans>Learn more</Trans>}
+                    />
+                  </div>
+                  <div style={styles.buttonContainer}>
+                    <RaisedButton
+                      primary
+                      fullWidth
+                      icon={<ShinyCrown fontSize="small" />}
+                      onClick={() => {
+                        openSubscriptionDialog({
+                          filter: 'education',
+                          analyticsMetadata: {
+                            reason: 'Callout in Classroom tab',
+                          },
+                        });
+                      }}
+                      label={<Trans>Subscribe to Edu</Trans>}
+                    />
+                  </div>
+                </ResponsiveLineStackLayout>
               </ColumnStackLayout>
-              <div style={styles.buttonContainer}>
-                <RaisedButton
-                  primary
-                  fullWidth
-                  onClick={() => {
-                    openSubscriptionDialog({
-                      filter: 'education',
-                      analyticsMetadata: { reason: 'Callout in Classroom tab' },
-                    });
-                  }}
-                  label={<Trans>Get the Edu subscription</Trans>}
-                />
-              </div>
             </ResponsiveLineStackLayout>
           </div>
         </CardWidget>
       </SectionRow>
       <SectionRow>
+        <div
+          style={{
+            ...styles.freeTag,
+            color: gdevelopTheme.statusIndicator.success,
+            borderColor: gdevelopTheme.statusIndicator.success,
+          }}
+        >
+          <Text noMargin color="inherit">
+            <Trans>Free</Trans>
+          </Text>
+        </div>
         <Text size="section-title">
-          <Trans>Teaching resources included!</Trans>
+          <Trans>Get our teaching resources</Trans>
         </Text>
         <Text>
           <Trans>
-            The Education subscription gives access to GDevelop’s Game
-            Development curriculum. Co-created with teachers and institutions,
-            it’s a helpful guide to your STEM related courses.
+            We’ll send to your email:
+            <li>An extract of our ready-to-teach Curriculum</li>
+            <li>
+              A poster with GDevelop's core concepts to use in your classroom
+            </li>
+            <li>“Game Development as an Educational wonder” PDF</li>
           </Trans>
         </Text>
         <ResponsiveLineStackLayout noColumnMargin useLargeSpacer>

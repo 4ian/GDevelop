95ff677cc0847de5508f32148a664c6926d16b0e
Vladyslav Pohorielov <pohorielovvladislav@gmail.com>
2024-08-13 19:20:34 +0300

    chore: update comments

--- GDevelop.js/scripts/sync-versions.js
+++ GDevelop.js/scripts/sync-versions.js
@@ -5,6 +5,7 @@ const gdVersion = require('../../newIDE/electron-app/app/package.json').version;
 const gdVersionArray = gdVersion.split('.');
 const versionPrivContent = 
 `// This file is automatically synchronizing with newIDE/electron-app/app/package.json version
+// Check GDevelop/scripts/sync-versions.js for more details
 #define GD_VERSION_STRING "${gdVersion}-0-release"
 #define GD_VERSION_RC ${gdVersionArray.join(',')},0-0-release
 #define GD_VERSION_RC_STRING "${gdVersionArray.join(', ')}, 0-0-release\\0"

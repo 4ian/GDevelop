3969b89cefa52516cf3b666bccda8a93b0e1c489
Vladyslav Pohorielov <pohorielovvladislav@gmail.com>
2024-08-14 17:11:11 +0300

    fix: remove unused gd version defines and date data, sync version with grunt

--- GDevelop.js/scripts/sync-versions.js
+++ GDevelop.js/scripts/sync-versions.js
@@ -1,15 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 
-const gdVersion = require('../../newIDE/electron-app/app/package.json').version;
-const gdVersionArray = gdVersion.split('.');
+const [gdVersion, prerelease] = require('../../newIDE/electron-app/app/package.json').version.split('-');
 const versionPrivContent = 
 `// This file is automatically synchronizing with newIDE/electron-app/app/package.json version
 // Check GDevelop/scripts/sync-versions.js for more details
-#define GD_VERSION_STRING "${gdVersion}-0-release"
-#define GD_VERSION_RC ${gdVersionArray.join(',')},0-0-release
-#define GD_VERSION_RC_STRING "${gdVersionArray.join(', ')}, 0-0-release\\0"
-#define GD_DATE_STRING __DATE__
+#define GD_VERSION_STRING "${gdVersion}-${prerelease || 0}"
 `;
 
 fs.writeFileSync(path.join(__dirname, "../../Core/GDCore/Tools/VersionPriv.h"), versionPrivContent, "utf8");

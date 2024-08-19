047e5ac8156cb7c88aa2bd486cdf338bfa1548d4
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2024-08-19 15:10:40 +0200

    Bump GD version

--- Core/GDCore/Tools/VersionPriv.h
+++ Core/GDCore/Tools/VersionPriv.h
@@ -1,3 +1,3 @@
 // This file is automatically synchronizing with newIDE/electron-app/app/package.json version
 // Check GDevelop/scripts/sync-versions.js for more details
-#define GD_VERSION_STRING "4.0.99-0"
+#define GD_VERSION_STRING "5.4.208-0"

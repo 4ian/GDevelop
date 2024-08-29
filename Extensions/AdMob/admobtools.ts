a476d5cecb02b3c1eedd44741780f1d49f0afc50
devgiordane <bruno_giordane@outlook.com>
2024-08-29 18:20:06 -0300

    Set Banner offset to 0 for just overlapping the banner on top of the content.

--- Extensions/AdMob/admobtools.ts
+++ Extensions/AdMob/admobtools.ts
@@ -316,12 +316,7 @@ namespace gdjs {
      * Set up a banner that can then be displayed by calling `showBanner`.
      * If a banner is already set up, it will be hidden and replaced by the new one.
      */
-    export const setupBanner = async (
-      androidAdUnitId,
-      iosAdUnitId,
-      atTop,
-      hasOffset
-    ) => {
+    export const setupBanner = async (androidAdUnitId, iosAdUnitId, atTop) => {
       if (!(await checkIfAdMobIsAvailable())) return;
       const adUnitId = getAdUnitId(androidAdUnitId, iosAdUnitId, 'banner');
       if (!adUnitId) return;
@@ -338,7 +333,7 @@ namespace gdjs {
         adUnitId,
         position: atTop ? 'top' : 'bottom',
         size: bannerRequestedAdSizeType,
-        ...(hasOffset && { offset: 0 }),
+        offset: 0,
       });
 
       banner.on('load', () => {

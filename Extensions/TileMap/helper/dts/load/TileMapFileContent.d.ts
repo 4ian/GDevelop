63e6e9cfe3959f22d653dd532790e06d15e320ae
Florian Rival <Florian.rival@gmail.com>
2024-09-05 15:05:20 +0200

    Fix tilemap instance crash when no image set

--- Extensions/TileMap/helper/dts/load/TileMapFileContent.d.ts
+++ Extensions/TileMap/helper/dts/load/TileMapFileContent.d.ts
@@ -1,6 +1,6 @@
 import { LDtkTileMap } from '../load/ldtk/LDtkFormat';
 import { TiledTileMap } from '../load/tiled/TiledFormat';
-export declare type TileMapFileContent =
+export type TileMapFileContent =
   | {
       kind: 'tiled';
       data: TiledTileMap;

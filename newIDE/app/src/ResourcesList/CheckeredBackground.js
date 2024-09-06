6f32a7593f6c0c019631babe41e90353d276f606
Aurélien Vivet <bouh.vivez@gmail.com>
2024-09-06 12:11:22 +0200

    Update CheckeredBackground.js

--- newIDE/app/src/ResourcesList/CheckeredBackground.js
+++ newIDE/app/src/ResourcesList/CheckeredBackground.js
@@ -26,7 +26,6 @@ const CheckeredBackground = () => {
     left: 0,
     width: '100%',
     height: '100%',
-    zIndex: -1,
 
     // Apply a theme-defined CSS filter on static checkered background
     background: 'url("res/transparentback.png") repeat',

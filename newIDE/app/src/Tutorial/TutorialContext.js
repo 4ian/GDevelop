0d1bdc5bfff93a1b5fc576d66ff770b5e1ca420e
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-09-13 17:16:49 +0200

    Remove mock data

--- newIDE/app/src/Tutorial/TutorialContext.js
+++ newIDE/app/src/Tutorial/TutorialContext.js
@@ -44,38 +44,7 @@ export const TutorialStateProvider = ({
           console.info(
             `Loaded ${allTutorials ? allTutorials.length : 0} tutorials.`
           );
-          setTutorials([
-            {
-              id: 'education-curriculum-flappy-cat',
-              titleByLocale: { en: 'Flappy Cat' },
-              descriptionByLocale: {
-                en:
-                  "It's time to make your first game! And what a way to learn than by making your own version of this mobile sensation. Tap tap tap, make sure the cat doesn't touch the ground or the walls!",
-              },
-              isPrivateTutorial: true,
-              type: 'pdf-tutorial',
-              category: 'education-curriculum',
-              linkByLocale: {
-                en: 'https://api-dev.gdevelop.io/pdf-tutorial/flappy-cat',
-              },
-              thumbnailUrlByLocale: {
-                en:
-                  'https://resources.gdevelop-app.com/tutorials/images/flappy-cat.jpg',
-              },
-              redeemHintByLocale: {
-                en:
-                  'This tutorial is made for education and accessible to teachers and subscribers to the GDevelop Education plan only.',
-              },
-              redeemLinkByLocale: {
-                en: 'https://gdevelop.io/pricing/education',
-              },
-              tagsByLocale: [{ en: 'Single player' }, { en: 'Beginner' }],
-              sectionByLocale: { en: 'Practical lessons' },
-              gameLink: 'https://gd.games/gdevelop/flappy-cat',
-              templateUrl:
-                'https://resources.gdevelop-app.com/tutorials/templates/flappy-cat/game.json',
-            },
-          ]);
+          setTutorials(allTutorials);
         } catch (error) {
           console.error(`Unable to load the tutorials:`, error);
           setError(error);

72709405b7a7fbf12e46d256da6fe8e75fe491ff
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-10-18 14:21:52 +0200

    Fix React warnings

--- newIDE/app/src/UI/CustomSvgIcons/TopTextAlignment.js
+++ newIDE/app/src/UI/CustomSvgIcons/TopTextAlignment.js
@@ -4,14 +4,14 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 export default React.memo(props => (
   <SvgIcon {...props} width="24" height="24" viewBox="0 0 24 24" fill="none">
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M14.2303 12.2803C13.9374 12.5732 13.4626 12.5732 13.1697 12.2803L12.7 11.8107L12.7 18C12.7 18.4142 12.3642 18.75 11.95 18.75C11.5358 18.75 11.2 18.4142 11.2 18V11.8107L10.7303 12.2803C10.4374 12.5732 9.96256 12.5732 9.66967 12.2803C9.37677 11.9874 9.37677 11.5126 9.66967 11.2197L11.4197 9.46967C11.7126 9.17678 12.1874 9.17678 12.4803 9.46967L14.2303 11.2197C14.5232 11.5126 14.5232 11.9874 14.2303 12.2803Z"
       fill="currentColor"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M3.25 6C3.25 5.58579 3.58579 5.25 4 5.25L20 5.25C20.4142 5.25 20.75 5.58579 20.75 6C20.75 6.41421 20.4142 6.75 20 6.75L4 6.75C3.58579 6.75 3.25 6.41421 3.25 6Z"
       fill="currentColor"
     />

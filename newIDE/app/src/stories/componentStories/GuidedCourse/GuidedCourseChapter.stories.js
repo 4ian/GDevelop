be88b5f68aac2c8bc5350e8379509af4d4e3a473
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-11-28 17:18:55 +0100

    Update images urls with new sub folder

--- newIDE/app/src/stories/componentStories/GuidedCourse/GuidedCourseChapter.stories.js
+++ newIDE/app/src/stories/componentStories/GuidedCourse/GuidedCourseChapter.stories.js
@@ -20,7 +20,7 @@ const guidedCourseChapter: GuidedCourseChapter = {
       text:
         'Complete the six tasks below by dragging the premade objects from the object list in to the scene’s default view window to match the image of the scene shown below.',
       imageUrls: [
-        'https://public-resources.gdevelop.io/staging/course/6900ffbe-4a0e-4f1c-9ad8-b9aa495daf17.png',
+        'https://public-resources.gdevelop.io/staging/course/images/6900ffbe-4a0e-4f1c-9ad8-b9aa495daf17.png',
       ],
     },
     {
@@ -31,7 +31,7 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(A tiled sprite will repeat the image used in the object instead of stretching when it is scaled. It is also the only object that can be adjusted with an X/Y offset, making it ideal for scrolling backgrounds or repeating images in your game.)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/40c07f00-8c66-4b6c-b092-fa4a8e00a3d6.png',
+          'https://public-resources.gdevelop.io/staging/course/images/40c07f00-8c66-4b6c-b092-fa4a8e00a3d6.png',
         ],
       },
     },
@@ -43,7 +43,7 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(A sprite is the only object that supports multiple image frames for animations. These animations can be played, paused, or switched during gameplay. Each animation can be given a name, but are automatically given a number based on its position in the list (e.g., 0, 1, 2, 3, etc.).)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/22338211-c869-45d2-9d15-0868d9588142.png',
+          'https://public-resources.gdevelop.io/staging/course/images/22338211-c869-45d2-9d15-0868d9588142.png',
         ],
       },
     },
@@ -54,11 +54,11 @@ const guidedCourseChapter: GuidedCourseChapter = {
       hint:
         '(A panel sprite(9 patch) is used for text boxes and any UI elements with repeating patterns and a rectangular border. They’re called 9-patch because images are broken into 9 “patches” that have different rules applied to them when the object is stretched.)',
       imageUrls: [
-        'https://public-resources.gdevelop.io/staging/course/74933b88-cbd9-4690-87e4-b3dfe583a93e.png',
+        'https://public-resources.gdevelop.io/staging/course/images/74933b88-cbd9-4690-87e4-b3dfe583a93e.png',
       ],
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/ffb596da-2200-45f4-ad14-de2c0929f3ab.png',
+          'https://public-resources.gdevelop.io/staging/course/images/ffb596da-2200-45f4-ad14-de2c0929f3ab.png',
         ],
       },
     },
@@ -70,7 +70,7 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(There are three different types of text objects in the engine, each serving a different purpose, and they can all have their text changed while the game is running with the use of events. Which you’ll learn more about in later chapters.)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/a73da7b8-dc84-4130-917e-7341ed263f5a.png',
+          'https://public-resources.gdevelop.io/staging/course/images/a73da7b8-dc84-4130-917e-7341ed263f5a.png',
         ],
       },
     },
@@ -82,7 +82,7 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(A panel sprite button is useful because it makes it easy to check if the player has clicked on the button, and automatically handles the animations and state of the button.)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/01138252-6128-459c-9cce-c5b728d46479.png',
+          'https://public-resources.gdevelop.io/staging/course/images/01138252-6128-459c-9cce-c5b728d46479.png',
         ],
       },
     },
@@ -93,13 +93,13 @@ const guidedCourseChapter: GuidedCourseChapter = {
       hint:
         '(When you come across a text field for a color, it will always be presented with a color picker tool next to it. You can pick any color by clicking/tapping the color box, and it will automatically change the text field for you.)',
       imageUrls: [
-        'https://public-resources.gdevelop.io/staging/course/0c7a1d3b-b2ca-45ad-a832-d51649a5b08d.png',
+        'https://public-resources.gdevelop.io/staging/course/images/0c7a1d3b-b2ca-45ad-a832-d51649a5b08d.png',
       ],
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/46aab784-344b-46bb-bd04-3017036a010f.png',
-          'https://public-resources.gdevelop.io/staging/course/48e8ea8b-8fcd-45c3-8a5e-5b4f43cc8461.png',
-          'https://public-resources.gdevelop.io/staging/course/6ef17077-50f7-46b6-8c03-17dd797d7ffe.png',
+          'https://public-resources.gdevelop.io/staging/course/images/46aab784-344b-46bb-bd04-3017036a010f.png',
+          'https://public-resources.gdevelop.io/staging/course/images/48e8ea8b-8fcd-45c3-8a5e-5b4f43cc8461.png',
+          'https://public-resources.gdevelop.io/staging/course/images/6ef17077-50f7-46b6-8c03-17dd797d7ffe.png',
         ],
       },
     },
@@ -111,7 +111,7 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(Z-order values determine which elements are in front or behind of others on the same layer, which is useful for games where the player character needs to be in front of certain elements but behind others.)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/f699f117-23e5-4856-9412-5cad48573a60.png',
+          'https://public-resources.gdevelop.io/staging/course/images/f699f117-23e5-4856-9412-5cad48573a60.png',
         ],
       },
     },
@@ -123,8 +123,8 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(Take note that this doesn’t need to be done for every object in your game; typically, you only change an object’s origin point when it is required for positioning, rotation, or z-ordering based on the origin point’s position.)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/7a74918f-ebab-4ff2-9263-5628565747fc.png',
-          'https://public-resources.gdevelop.io/staging/course/17acd5ef-246e-4137-a104-dde205c9dc50.png',
+          'https://public-resources.gdevelop.io/staging/course/images/7a74918f-ebab-4ff2-9263-5628565747fc.png',
+          'https://public-resources.gdevelop.io/staging/course/images/17acd5ef-246e-4137-a104-dde205c9dc50.png',
         ],
       },
     },
@@ -136,8 +136,8 @@ const guidedCourseChapter: GuidedCourseChapter = {
         '(Take note that only sprite and tilemap objects have adjustable collision masks. Most other objects will use their entire height and width as their collision mask, and some objects, like particles, have no collision mask at all.)',
       answer: {
         imageUrls: [
-          'https://public-resources.gdevelop.io/staging/course/8e0a7b37-d8f9-448c-9eeb-9d73eac97b07.png',
-          'https://public-resources.gdevelop.io/staging/course/547562c4-4c7e-47fd-bf04-0710fd1a4f57.png',
+          'https://public-resources.gdevelop.io/staging/course/images/8e0a7b37-d8f9-448c-9eeb-9d73eac97b07.png',
+          'https://public-resources.gdevelop.io/staging/course/images/547562c4-4c7e-47fd-bf04-0710fd1a4f57.png',
         ],
       },
     },

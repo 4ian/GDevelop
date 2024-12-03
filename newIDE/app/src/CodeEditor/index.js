1b0c088f71720f7cb68b9425db0cce9e3f324e82
Florian Rival <Florian.rival@gmail.com>
2024-12-03 18:25:25 +0100

    Add error display for some obvious semantic/type errors in JavaScript code blocks

--- newIDE/app/src/CodeEditor/index.js
+++ newIDE/app/src/CodeEditor/index.js
@@ -70,13 +70,25 @@ export class CodeEditor extends React.Component<Props, State> {
     if (!monacoCompletionsInitialized) {
       monacoCompletionsInitialized = true;
 
+      // Enable type checking of JavaScript files
       monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-        // noLib: true,
+        ...monaco.languages.typescript.javascriptDefaults.getCompilerOptions(),
         target: monaco.languages.typescript.ScriptTarget.ES6,
         allowNonTsExtensions: true,
         allowJs: true,
         checkJs: true,
       });
+
+      // Activate checks - though this won't work for .d.ts classes/funcions for
+      // some reason. See:
+      // https://microsoft.github.io/monaco-editor/playground.html?source=v0.52.0#XQAAAALxBQAAAAAAAABBqQkHQ5NjdMjwa-jY7SIQ9S7DNlzs5W-mwj0fe1ZCDRFc9ws9XQE0SJE1jc2VKxhaLFIw9vEWSxW3yscw90T9lfa8wm3bKzPN7npWhYaA4x4rxLXXBbB2_OnRSToJ21hOUR_hS9pG2m6stSYmFVtipRHVTrIB91DOr8dFz_2f6wKJhg_ghHcsBt-hKnlu-qt2H4NuwdaqfbMd6vqkrLY5fzdHCB8FZIGGUDanilP6QXPTpV0rme8fvINUj2BklEotLylw-HHch53nHQMCabAMh3iNqg8S_rHB5OZg2IxLB-8POziUzSwFrTcfwxftX7gi3ZIWNPyf4vJOFtRKVr3hJLIKUGJew4RN0hYb1SnPUm1x6B8hb8I5-9WqAgbYz6E0ntlpOU4jvw6zty4iIJ4GQFuyQ1ys6LtVNKef1bnBs7fDr3xFb3LOW7E4RMonCTxapW3_DQvpQ_x4psM1GYFKMZZXr0lCRvrFs4PBf0uXMwEeTfC1PurLOeoNNEEZFLutUE1ojQVjkTgxZC_HCheas9sKTtZRsJ0i6qDWJh3PbNocPFIbvPQ5la2NXuQIph1oaGrDjqgoirRoexyKumFnXVLkvKVoTdglfgXLwEaoheNdItwJfWKdPqmHh18GTsk8Df_mr8zt3r-pLUtNvB6220ZAKaswAMPR0yDrfHfz_7vWaBfDy6yykp-ROV0Ckt2qV83DVNrHW9HNm4e0WC1ByFoDOB8AdJzjQye8YD3aCwy8ft4sSOGvSeNo2FOPoqd2TU-Sr58fhP09rWes4Vgrv1MhMlUtZ5zBNSL_Mpb1R32H8hrNShgxxaT0r048_u6-nFbkNKq-VCjTlFuUoTW-3y8b1UrYxSmTkjmp-KCc9ObqKgzbRV5tPIX_sqYqVCJFOZN0Nndp4s6xm7qmVPi8SMcDfCD4zfPWBpanAohwTcIIbXPJqggntHxMUFvH7h85mh3AZoJ5FPz2v387LTrxdqOZZW7kTGqSl-Y8NHsa_znjlaNDOU-qywr5DypPbH3_wOm2XMeQNg0jX1bBtIoh_PG5BWMQNlJmQRlDnmDtueZf_nTzQw
+      monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+        ...monaco.languages.typescript.javascriptDefaults.getDiagnosticsOptions(),
+        noSemanticValidation: false,
+        noSuggestionDiagnostics: false,
+        noSyntaxValidation: false,
+      });
+
       setupAutocompletions(monaco);
     }
 

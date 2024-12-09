5a5ac112dfe0b252da469546a4eb37a0313b99b7
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2024-12-09 14:57:16 +0100

    Add possibility to have linear progress with success color

--- newIDE/app/src/UI/LinearProgress.js
+++ newIDE/app/src/UI/LinearProgress.js
@@ -2,16 +2,31 @@
 import * as React from 'react';
 
 import MuiLinearProgress from '@material-ui/core/LinearProgress';
+import GDevelopThemeContext from './Theme/GDevelopThemeContext';
+import type { GDevelopTheme } from './Theme';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = (color?: 'success', gdevelopTheme: GDevelopTheme) =>
+  makeStyles({
+    barColorSecondary: {
+      backgroundColor: gdevelopTheme.statusIndicator.success,
+    },
+  })();
 
 type Props = {|
   variant?: 'indeterminate' | 'determinate',
+  color?: 'success',
   value?: ?number,
   style?: {| height?: number, borderRadius?: number, width?: number |},
 |};
 
 function LinearProgress(props: Props) {
+  const gdevelopTheme = React.useContext(GDevelopThemeContext);
+  const classes = useStyles(props.color, gdevelopTheme);
+
   return (
     <MuiLinearProgress
+      classes={classes}
       color="secondary"
       style={{ flex: 1, ...props.style }}
       variant={props.variant}

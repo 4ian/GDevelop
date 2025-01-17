50ac59a277d8dabc37600472e9bb027cb240b968
Aurélien Vivet <bouh.vivez@gmail.com>
2025-01-17 18:40:54 +0100

    Change icon chevron by arrow

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/LearnSection/MainPage.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/LearnSection/MainPage.js
@@ -29,7 +29,7 @@ import GridListTile from '@material-ui/core/GridListTile';
 import ImageTileRow from '../../../../UI/ImageTileRow';
 import { formatTutorialToImageTileComponent, TUTORIAL_CATEGORY_TEXTS } from '.';
 import GuidedLessons from '../InAppTutorials/GuidedLessons';
-import ChevronArrowRight from '../../../../UI/CustomSvgIcons/ChevronArrowRight';
+import ArrowRight from '../../../../UI/CustomSvgIcons/ArrowRight';
 import Upload from '../../../../UI/CustomSvgIcons/Upload';
 import FlingGame from '../InAppTutorials/FlingGame';
 import AuthenticatedUserContext from '../../../../Profile/AuthenticatedUserContext';
@@ -129,7 +129,7 @@ export const TutorialsRow = ({
             })
           )}
         onShowAll={() => onSelectCategory(category)}
-        showAllIcon={<ChevronArrowRight fontSize="small" />}
+        showAllIcon={<ArrowRight fontSize="small" />}
         getColumnsFromWindowSize={getTutorialsColumnsFromWidth}
         getLimitFromWindowSize={getTutorialsColumnsFromWidth}
       />

1b1377a19284126205028ee432d9f4d7fa6b1075
Davy Hélard <davy.helard@gmail.com>
2025-04-13 12:27:33 +0200

    Fix thumbnails

--- newIDE/app/src/ObjectsRendering/Renderers/RenderedCustomObjectInstance.js
+++ newIDE/app/src/ObjectsRendering/Renderers/RenderedCustomObjectInstance.js
@@ -353,10 +353,16 @@ export default class RenderedCustomObjectInstance extends Rendered3DInstance
     const childObjects = variant.getObjects();
     for (let i = 0; i < childObjects.getObjectsCount(); i++) {
       const childObject = childObjects.getObjectAt(i);
-      const childObjectConfiguration = variant
-        .getObjects()
-        .getObject(childObject.getName())
-        .getConfiguration();
+      const childObjectConfiguration =
+        customObjectConfiguration.isForcedToOverrideEventsBasedObjectChildrenConfiguration() ||
+        customObjectConfiguration.isMarkedAsOverridingEventsBasedObjectChildrenConfiguration()
+          ? customObjectConfiguration.getChildObjectConfiguration(
+              childObject.getName()
+            )
+          : variant
+              .getObjects()
+              .getObject(childObject.getName())
+              .getConfiguration();
       const childType = childObjectConfiguration.getType();
       if (
         childType === 'Sprite' ||

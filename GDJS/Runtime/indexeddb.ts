a01507ac5d7a324b8a32a6b1c98121f37ea96bf6
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2025-04-30 15:11:43 +0200

    Use const variable

--- GDJS/Runtime/indexeddb.ts
+++ GDJS/Runtime/indexeddb.ts
@@ -10,48 +10,48 @@ namespace gdjs {
     key: string
   ): Promise<any> {
     return new Promise((resolve, reject) => {
-      let request: IDBOpenDBRequest;
-
       try {
-        request = indexedDB.open(dbName, 1);
+        const request = indexedDB.open(dbName, 1);
+        request.onupgradeneeded = function () {
+          const db = request.result;
+          if (!db.objectStoreNames.contains(objectStoreName)) {
+            db.createObjectStore(objectStoreName);
+          }
+        };
+
+        request.onsuccess = function () {
+          const db = request.result;
+
+          const tx = db.transaction(objectStoreName, 'readonly');
+          const store = tx.objectStore(objectStoreName);
+          const getRequest = store.get(key);
+
+          getRequest.onsuccess = function () {
+            if (getRequest.result !== undefined) {
+              resolve(getRequest.result);
+            } else {
+              resolve(null);
+            }
+          };
+
+          getRequest.onerror = function () {
+            console.error(
+              'Error loading data from IndexedDB:',
+              getRequest.error
+            );
+            reject(getRequest.error);
+          };
+        };
+
+        request.onerror = function () {
+          console.error('Error opening IndexedDB:', request.error);
+          reject(request.error);
+        };
       } catch (err) {
         console.error('Exception thrown while opening IndexedDB:', err);
         reject(err);
         return;
       }
-
-      request.onupgradeneeded = function () {
-        const db = request.result;
-        if (!db.objectStoreNames.contains(objectStoreName)) {
-          db.createObjectStore(objectStoreName);
-        }
-      };
-
-      request.onsuccess = function () {
-        const db = request.result;
-
-        const tx = db.transaction(objectStoreName, 'readonly');
-        const store = tx.objectStore(objectStoreName);
-        const getRequest = store.get(key);
-
-        getRequest.onsuccess = function () {
-          if (getRequest.result !== undefined) {
-            resolve(getRequest.result);
-          } else {
-            resolve(null);
-          }
-        };
-
-        getRequest.onerror = function () {
-          console.error('Error loading game from IndexedDB:', getRequest.error);
-          reject(getRequest.error);
-        };
-      };
-
-      request.onerror = function () {
-        console.error('Error opening IndexedDB:', request.error);
-        reject(request.error);
-      };
     });
   };
 
@@ -62,41 +62,39 @@ namespace gdjs {
     data: any
   ): Promise<void> {
     return new Promise((resolve, reject) => {
-      let request: IDBOpenDBRequest;
       try {
-        request = indexedDB.open(dbName, 1);
+        const request = indexedDB.open(dbName, 1);
+        request.onupgradeneeded = function (event) {
+          const db = request.result;
+          if (!db.objectStoreNames.contains(objectStoreName)) {
+            db.createObjectStore(objectStoreName);
+          }
+        };
+        request.onsuccess = function () {
+          const db = request.result;
+          const tx = db.transaction(objectStoreName, 'readwrite');
+          const store = tx.objectStore(objectStoreName);
+          const putRequest = store.put(data, key);
+
+          putRequest.onsuccess = function () {
+            resolve();
+          };
+
+          putRequest.onerror = function () {
+            console.error('Error saving data to IndexedDB:', putRequest.error);
+            reject(putRequest.error);
+          };
+        };
+
+        request.onerror = function () {
+          console.error('Error opening IndexedDB:', request.error);
+          reject(request.error);
+        };
       } catch (err) {
         console.error('Exception thrown while opening IndexedDB:', err);
         reject(err);
         return;
       }
-
-      request.onupgradeneeded = function (event) {
-        const db = request.result;
-        if (!db.objectStoreNames.contains(objectStoreName)) {
-          db.createObjectStore(objectStoreName);
-        }
-      };
-      request.onsuccess = function () {
-        const db = request.result;
-        const tx = db.transaction(objectStoreName, 'readwrite');
-        const store = tx.objectStore(objectStoreName);
-        const putRequest = store.put(data, key);
-
-        putRequest.onsuccess = function () {
-          resolve();
-        };
-
-        putRequest.onerror = function () {
-          console.error('Error saving game in IndexedDB:', putRequest.error);
-          reject(putRequest.error);
-        };
-      };
-
-      request.onerror = function () {
-        console.error('Error opening IndexedDB:', request.error);
-        reject(request.error);
-      };
     });
   };
 }

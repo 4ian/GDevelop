a6e795a05ff9285310ab9c3c423e767a4bff32e8
Neyl <assassinloup@gmail.com>
2025-05-05 10:48:01 +0200

    format

--- GDJS/Runtime/pixi-renderers/runtimescene-pixi-renderer.ts
+++ GDJS/Runtime/pixi-renderers/runtimescene-pixi-renderer.ts
@@ -26,10 +26,8 @@ namespace gdjs {
       this._runtimeGameRenderer = runtimeGameRenderer;
       this._runtimeScene = runtimeScene;
       this._pixiContainer = new PIXI.Container();
-      
       // Contains the layers of the scene (and, optionally, debug PIXI objects).
       this._pixiContainer.sortableChildren = true;
-      
       this._threeRenderer = this._runtimeGameRenderer
         ? this._runtimeGameRenderer.getThreeRenderer()
         : null;

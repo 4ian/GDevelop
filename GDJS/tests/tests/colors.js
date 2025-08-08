6af635c5ab03d19cfae817179861b63b27f86872
Davy Hélard <davy.helard@gmail.com>
2025-08-08 15:33:00 +0200

    Typo

--- GDJS/tests/tests/colors.js
+++ GDJS/tests/tests/colors.js
@@ -48,7 +48,6 @@ describe('gdjs', function () {
         expect(gdjs.rgbOrHexToRGBColor('19819830803')).to.eql([0, 0, 0]);
         expect(gdjs.rgbOrHexToRGBColor('Infinity')).to.eql([0, 0, 0]);
         expect(gdjs.rgbOrHexToRGBColor('-4564')).to.eql([0, 0, 0]);
-        expect(gdjs.rgbOrHexToRGBColor('9999;12;6')).to.eql([255, 12, 6]);
       });
     });
   });

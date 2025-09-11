feb6cc4d8e30b71d7667647c20f3d8f0f1c18c1a
Davy Hélard <davy.helard@gmail.com>
2025-09-11 14:10:59 +0200

    Add a link to an help page for asynchronous functions

--- newIDE/app/src/EventsFunctionsExtensionEditor/EventsFunctionConfigurationEditor/EventsFunctionPropertiesEditor.js
+++ newIDE/app/src/EventsFunctionsExtensionEditor/EventsFunctionConfigurationEditor/EventsFunctionPropertiesEditor.js
@@ -553,12 +553,21 @@ export const EventsFunctionPropertiesEditor = ({
                     }}
                   />
                   {eventsFunction.isAsync() && (
-                    <AlertMessage kind="info">
+                    <AlertMessage
+                      kind="info"
+                      renderRightButton={() => (
+                        <HelpButton
+                          helpPagePath={'/functions/asynchronous-functions'}
+                        />
+                      )}
+                    >
                       <Trans>
                         This is an asynchronous action, meaning that the actions
                         and sub-events following it will wait for it to end.
-                        Don't forget to use the action "End asynchronous
-                        function" to mark the end of the action.
+                        This function events are only executed once, you should
+                        use other async actions like "wait" to schedule your
+                        actions and don't forget to use the action "End
+                        asynchronous function" to mark the end of the action.
                       </Trans>
                     </AlertMessage>
                   )}

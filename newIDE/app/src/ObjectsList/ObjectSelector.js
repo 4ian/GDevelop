fc19b6d67efcfb68b26bcb685e9e1e55b517bb7d
Davy Hélard <davy.helard@gmail.com>
2025-09-15 18:40:51 +0200

    Underline groups with missing behaviors

--- newIDE/app/src/ObjectsList/ObjectSelector.js
+++ newIDE/app/src/ObjectsList/ObjectSelector.js
@@ -146,10 +146,8 @@ const getMissingBehaviors = ({
     return [];
   }
 
-  if (!objectsContainersList.hasObjectNamed(objectName)) {
-    // Either the object does not exist or it's a group - not a problem because:
-    // - if the object does not exist, we can't know its capabilities, we assume it has all.
-    // - a group is assumed to have all the capabilities.
+  if (!objectsContainersList.hasObjectOrGroupNamed(objectName)) {
+    // The parser already gives errors for objects which don't exist.
     return [];
   }
   return requiredBehaviorTypes.filter(

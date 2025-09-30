1cc56eb37b0eb61245d89a4a1d56c147f4670260
danvervlad <danvervlad@gmail.com>
2025-09-30 17:59:33 +0300

    add static isHitBoxesUpdateDisabled for SpineRuntimeObject

--- Extensions/Spine/spineruntimeobject.ts
+++ Extensions/Spine/spineruntimeobject.ts
@@ -53,6 +53,8 @@ namespace gdjs {
 
     readonly spineResourceName: string;
 
+    static isHitBoxesUpdateDisabled = false;
+
     /**
      * @param instanceContainer The container the object belongs to.
      * @param objectData The object data used to initialize the object
@@ -73,7 +75,10 @@ namespace gdjs {
       );
       this.setAnimationIndex(0);
       this._renderer.updateAnimation(0);
-      this.hitBoxes.length = 0;
+
+      if (SpineRuntimeObject.isHitBoxesUpdateDisabled) {
+          this.hitBoxes.length = 0;
+      }
 
       // *ALWAYS* call `this.onCreated()` at the very end of your object constructor.
       this.onCreated();
@@ -185,6 +190,11 @@ namespace gdjs {
     }
 
     updateHitBoxes(): void {
+        if (SpineRuntimeObject.isHitBoxesUpdateDisabled) {
+            return;
+        }
+
+        super.updateHitBoxes();
     }
 
     extraInitializationFromInitialInstance(

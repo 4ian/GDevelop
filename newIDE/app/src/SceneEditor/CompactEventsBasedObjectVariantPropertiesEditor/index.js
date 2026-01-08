56f62cd2527e8c249b5fa47c131ed9654306fee7
D8H <Davy.Helard@gmail.com>
2026-01-08 12:09:38 +0100

    Fix Schema imports (#8112)
    Don't show in changelog

--- newIDE/app/src/SceneEditor/CompactEventsBasedObjectVariantPropertiesEditor/index.js
+++ newIDE/app/src/SceneEditor/CompactEventsBasedObjectVariantPropertiesEditor/index.js
@@ -4,7 +4,7 @@ import { Trans } from '@lingui/macro';
 import { type I18n as I18nType } from '@lingui/core';
 
 import CompactPropertiesEditor from '../../CompactPropertiesEditor';
-import { type Schema } from '../../CompactPropertiesEditor';
+import { type Schema } from '../../PropertiesEditor/PropertiesEditorSchema';
 import { Column, Spacer, marginsSize } from '../../UI/Grid';
 import { type UnsavedChanges } from '../../MainFrame/UnsavedChangesContext';
 import ScrollView, { type ScrollViewInterface } from '../../UI/ScrollView';

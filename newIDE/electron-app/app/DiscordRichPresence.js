3407493f450b4f85bb1399cf96d4af27136b9bda
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2026-01-28 11:24:29 +0100

    Try using other discord package

--- newIDE/electron-app/app/DiscordRichPresence.js
+++ newIDE/electron-app/app/DiscordRichPresence.js
@@ -1,9 +1,23 @@
-const discordClient = require('discord-rich-presence')('718194112775454720');
+const { Client } = require('@xhayper/discord-rpc');
+
+const discordClient = new Client({
+  clientId: '718194112775454720',
+});
+
 discordClient.on('error', _ => true); // Ignore error: They happen only when the discord client is not installed
+
+// Connect to Discord
+discordClient.login().catch(() => {
+  // Ignore connection errors - Discord client might not be installed
+});
 
 module.exports = function(ipc) {
   // Set-up Discord Rich Presence event:
-  ipc.on('update-discord-rich-presence', (event, config) =>
-    discordClient.updatePresence(config)
-  );
+  ipc.on('update-discord-rich-presence', (event, config) => {
+    if (discordClient.user) {
+      discordClient.setActivity(config).catch(() => {
+        // Ignore errors silently
+      });
+    }
+  });
 };

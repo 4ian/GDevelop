0b28f0ca42a903ad2814c7be07f92ef3c4dc1517
Aurélien Vivet <bouh.vivez@gmail.com>
2026-02-10 16:20:24 +0100

    Dispose EventListener for the debugger when game closed

--- GDJS/Runtime/runtimegame.ts
+++ GDJS/Runtime/runtimegame.ts
@@ -1270,6 +1270,9 @@ namespace gdjs {
       if (this._inGameEditor) {
         this._inGameEditor.dispose();
       }
+      if (this._debuggerClient) {
+        this._debuggerClient.dispose();
+      }
       this._renderer.stopGameLoop();
       this._sceneStack.dispose();
       this._renderer.dispose(removeCanvas);

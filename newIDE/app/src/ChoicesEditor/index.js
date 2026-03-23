831a4b0f8a76b87fd4f2916938ecedc824c756f6
D8H <Davy.Helard@gmail.com>
2026-03-23 15:22:20 +0100

    Redesign the object/behavior configuration editor in extensions to be more compact (#8425)

--- newIDE/app/src/ChoicesEditor/index.js
+++ newIDE/app/src/ChoicesEditor/index.js
@@ -1,6 +1,7 @@
 // @flow
 import { Trans } from '@lingui/macro';
 import { t } from '@lingui/macro';
+import { I18n } from '@lingui/react';
 import * as React from 'react';
 import {
   ResponsiveLineStackLayout,
@@ -8,11 +9,12 @@ import {
   ColumnStackLayout,
 } from '../UI/Layout';
 import { Line } from '../UI/Grid';
-import SemiControlledTextField from '../UI/SemiControlledTextField';
 import { IconButton } from '@material-ui/core';
 import RaisedButton from '../UI/RaisedButton';
 import Add from '../UI/CustomSvgIcons/Add';
 import Trash from '../UI/CustomSvgIcons/Trash';
+import CompactPropertiesEditorRowField from '../CompactPropertiesEditor/CompactPropertiesEditorRowField';
+import CompactSemiControlledTextField from '../UI/CompactSemiControlledTextField';
 
 export type Choice = {|
   value: string,
@@ -37,76 +39,93 @@ const ChoicesEditor = ({
   const updateChoices = () => setChoices(choices);
 
   return (
-    <ResponsiveLineStackLayout noOverflowParent>
-      <ColumnStackLayout
-        justifyContent="flex-end"
-        expand
-        noOverflowParent
-        noMargin
-      >
-        {choices.map((item, index) => (
-          <LineStackLayout
-            key={index}
+    <I18n>
+      {({ i18n }) => (
+        <ResponsiveLineStackLayout noOverflowParent>
+          <ColumnStackLayout
             justifyContent="flex-end"
             expand
-            alignItems="center"
+            noOverflowParent
             noMargin
           >
-            <SemiControlledTextField
-              disabled={disabled}
-              commitOnBlur
-              floatingLabelText={<Trans>Value</Trans>}
-              value={item.value}
-              onChange={text => {
-                choices[index].value = text;
-                updateChoices();
-              }}
-              fullWidth
-            />
-            {!hideLabels && (
-              <SemiControlledTextField
+            {choices.map((item, index) => (
+              <LineStackLayout
+                key={index}
+                justifyContent="flex-end"
+                expand
+                alignItems="center"
+                noMargin
+              >
+                <CompactPropertiesEditorRowField
+                  label={i18n._(t`Value`)}
+                  labelMaxWidth={60}
+                  field={
+                    <CompactSemiControlledTextField
+                      disabled={disabled}
+                      commitOnBlur
+                      value={item.value}
+                      onChange={text => {
+                        choices[index].value = text;
+                        updateChoices();
+                      }}
+                    />
+                  }
+                />
+                {!hideLabels && (
+                  <CompactPropertiesEditorRowField
+                    label={i18n._(t`Label`)}
+                    labelMaxWidth={60}
+                    field={
+                      <CompactSemiControlledTextField
+                        disabled={disabled}
+                        commitOnBlur
+                        placeholder={i18n._(t`Label displayed in editor`)}
+                        value={item.label}
+                        onChange={text => {
+                          choices[index].label = text;
+                          updateChoices();
+                        }}
+                      />
+                    }
+                  />
+                )}
+                <IconButton
+                  disabled={disabled}
+                  tooltip={t`Delete option`}
+                  onClick={() => {
+                    choices.splice(index, 1);
+                    updateChoices();
+                  }}
+                  size="small"
+                >
+                  <Trash />
+                </IconButton>
+              </LineStackLayout>
+            ))}
+
+            <Line justifyContent="flex-end" expand>
+              <RaisedButton
                 disabled={disabled}
-                commitOnBlur
-                floatingLabelText={<Trans>Label displayed in editor</Trans>}
-                value={item.label}
-                onChange={text => {
-                  choices[index].label = text;
+                primary
+                onClick={() => {
+                  choices.push(
+                    isNumber
+                      ? {
+                          value: choices.length.toString(),
+                          label: 'New Option',
+                        }
+                      : { value: 'New Option', label: '' }
+                  );
                   updateChoices();
                 }}
-                fullWidth
+                label={<Trans>Add a new option</Trans>}
+                icon={<Add />}
               />
-            )}
-            <IconButton
-              disabled={disabled}
-              tooltip={t`Delete option`}
-              onClick={() => {
-                choices.splice(index, 1);
-                updateChoices();
-              }}
-            >
-              <Trash />
-            </IconButton>
-          </LineStackLayout>
-        ))}
-
-        <Line justifyContent="flex-end" expand>
-          <RaisedButton
-            disabled={disabled}
-            primary
-            onClick={() => {
-              choices.push(
-                isNumber
-                  ? { value: choices.length.toString(), label: 'New Option' }
-                  : { value: 'New Option', label: '' }
-              );
-              updateChoices();
-            }}
-            label={<Trans>Add a new option</Trans>}
-            icon={<Add />}
-          />
-        </Line>
-      </ColumnStackLayout>
-    </ResponsiveLineStackLayout>
+            </Line>
+          </ColumnStackLayout>
+        </ResponsiveLineStackLayout>
+      )}
+    </I18n>
   );
 };
 

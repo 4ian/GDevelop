232443328daa0b8f615c1115bc967eb80bdbccc0
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2026-05-05 18:29:21 +0200

    Fix some visual glitches in the Events Sheet (#8551)

--- newIDE/app/src/EventsSheet/EventsTree/EventHeightsCache.js
+++ newIDE/app/src/EventsSheet/EventsTree/EventHeightsCache.js
@@ -6,15 +6,24 @@
  * the size of each event - which we only know after the event has been rendered.
  */
 export default class EventHeightsCache {
-  // $FlowFixMe[missing-local-annot]
-  eventHeights = {};
-  updateTimeoutId: ?TimeoutID = null;
+  eventHeights: { [number]: number } = {};
+  version: number = 0;
+  _onHeightsChanged: ?() => void = null;
 
   setEventHeight(event: gdBaseEvent, height: number) {
+    if (this.eventHeights[event.ptr] === height) return;
     this.eventHeights[event.ptr] = height;
+    this.version++;
+    if (this._onHeightsChanged) {
+      this._onHeightsChanged();
+    }
   }
 
   getEventHeight(event: gdBaseEvent): number {
     return this.eventHeights[event.ptr] || 0;
+  }
+
+  setOnHeightsChanged(callback: ?() => void) {
+    this._onHeightsChanged = callback;
   }
 }

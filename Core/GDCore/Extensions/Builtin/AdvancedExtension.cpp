2573d9123134e224c1fd45a1008192e4deda0d7b
LuniMoon <103995399+LuniMoon@users.noreply.github.com>
2026-05-06 09:42:13 +0200

    Replace helpPath
    Updating to correct URL suggested by Davy
    
    Co-authored-by: D8H <Davy.Helard@gmail.com>

--- Core/GDCore/Extensions/Builtin/AdvancedExtension.cpp
+++ Core/GDCore/Extensions/Builtin/AdvancedExtension.cpp
@@ -33,7 +33,7 @@ void GD_CORE_API BuiltinExtensionsImplementer::ImplementsAdvancedExtension(
           "",
           "res/functions/expression_black.svg",
           "res/functions/expression_black.svg")
-      .SetHelpPath("/all-features/advanced/reference/#actions")
+      .SetHelpPath("events/functions/#return-a-value-from-a-function")
       .AddParameter("expression", _("The number to be returned"))
       .SetRelevantForFunctionEventsOnly()
       .MarkAsAdvanced();

8501c45577c095c5e6854f5866e34f7a9613842d
tianheng2017 <tianheng2017@qq.com>
2026-05-16 17:48:02 +0800

    Cover runtime resource URLs with hashes

--- newIDE/app/src/ResourcesList/ResourceUrlEncoding.spec.js
+++ newIDE/app/src/ResourcesList/ResourceUrlEncoding.spec.js
@@ -3,7 +3,6 @@ import path from 'path';
 import { pathToFileURL } from 'url';
 import { getLocalResourceFullPath } from './ResourceUtils';
 import ResourcesLoader from '../ResourcesLoader';
-import optionalRequire from '../Utils/OptionalRequire';
 
 jest.mock('../Utils/OptionalRequire');
 jest.mock('../Utils/CrossOrigin', () => ({
@@ -11,17 +10,30 @@ jest.mock('../Utils/CrossOrigin', () => ({
 }));
 
 describe('ResourceUrlEncoding', () => {
-  it('encodes hashes in local resource urls and decodes them back', () => {
-    const project = {
-      ptr: 1,
-      getProjectFile: () => 'E:\\GDevelop\\Game\\game.json',
-      getResourcesManager: () => ({
-        hasResource: () => true,
-        getResource: () => ({
-          getFile: () => 'Weekend Jam #1/Parts/hero.png',
-        }),
+  let dateNowSpy;
+
+  const makeProject = ptr => ({
+    ptr,
+    getProjectFile: () => 'E:\\GDevelop\\Game\\game.json',
+    getResourcesManager: () => ({
+      hasResource: () => true,
+      getResource: () => ({
+        getFile: () => 'Weekend Jam #1/Parts/hero.png',
       }),
-    };
+    }),
+  });
+
+  beforeEach(() => {
+    ResourcesLoader.burstAllUrlsCache();
+    dateNowSpy = jest.spyOn(Date, 'now').mockReturnValue(1234);
+  });
+
+  afterEach(() => {
+    dateNowSpy.mockRestore();
+  });
+
+  it('encodes hashes in local resource urls and decodes them back', () => {
+    const project = makeProject(1);
 
     const fullUrl = ResourcesLoader.getResourceFullUrl(
       project,
@@ -38,5 +50,21 @@ describe('ResourceUrlEncoding', () => {
     expect(
       getLocalResourceFullPath(project, 'Weekend Jam #1/Parts/hero.png')
     ).toBe(path.resolve('E:\\GDevelop\\Game', 'Weekend Jam #1/Parts/hero.png'));
+  });
+
+  it('adds cache busting after the encoded local resource url', () => {
+    const project = makeProject(2);
+
+    const fullUrl = ResourcesLoader.getResourceFullUrl(
+      project,
+      'Weekend Jam #1/Parts/hero.png',
+      {}
+    );
+
+    expect(fullUrl).toBe(
+      pathToFileURL(
+        path.resolve('E:\\GDevelop\\Game', 'Weekend Jam #1/Parts/hero.png')
+      ).toString() + '?cache=1234'
+    );
   });
 });

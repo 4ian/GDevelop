56dc9b77ad98fec956ea78f683e82694f0536fdc
Florian Rival <Florian.rival@gmail.com>
2026-06-08 17:48:06 +0200

    Add tile scale property to 3D Box object (#8685)
    * It defaults to 1 (same rendering as a Tiled Sprite in 2D). Useful for 3D games which can set it to a lower value like 0.2 to avoid textures being too big and so low quality when the camera is near 3D Box instances.

--- Extensions/3D/JsExtension.js
+++ Extensions/3D/JsExtension.js
@@ -964,6 +964,12 @@ module.exports = {
         objectContent[propertyName] = parseFloat(newValue);
         return true;
       }
+      if (propertyName === 'tileScale') {
+        const newTileScale = parseFloat(newValue);
+        objectContent.tileScale =
+          isNaN(newTileScale) || newTileScale <= 0 ? 1 : newTileScale;
+        return true;
+      }
       if (propertyName === 'facesOrientation') {
         const normalizedValue = newValue.toUpperCase();
         if (normalizedValue === 'Y' || normalizedValue === 'Z') {
@@ -1194,6 +1200,18 @@ module.exports = {
         .setGroup(_('Textures'));
 
       objectProperties
+        .getOrCreate('tileScale')
+        .setValue((objectContent.tileScale || 1).toString())
+        .setType('number')
+        .setLabel(_('Tile scale'))
+        .setDescription(
+          _(
+            'The scale applied to tiled textures. A value of 1 displays them at the same size as in 2D.'
+          )
+        )
+        .setGroup(_('Textures'));
+
+      objectProperties
         .getOrCreate('frontFaceVisible')
         .setValue(objectContent.frontFaceVisible ? 'true' : 'false')
         .setType('boolean')
@@ -1294,6 +1312,7 @@ module.exports = {
       rightFaceResourceRepeat: false,
       topFaceResourceRepeat: false,
       bottomFaceResourceRepeat: false,
+      tileScale: 1,
       materialType: 'StandardWithoutMetalness',
       tint: '255;255;255',
       isCastingShadow: true,
@@ -2823,6 +2842,7 @@ module.exports = {
       _faceResourceNames = new Array(6).fill(null);
       _faceVisibilities = new Array(6).fill(null);
       _shouldRepeatTextureOnFace = new Array(6).fill(null);
+      _tileScale = 1;
       _facesOrientation = 'Y';
       _backFaceUpThroughWhichAxisRotation = 'X';
       _shouldUseTransparentTexture = false;
@@ -3029,6 +3049,12 @@ module.exports = {
           uvMappingDirty = true;
         }
 
+        const tileScale = object.content.tileScale || 1;
+        if (tileScale !== this._tileScale) {
+          this._tileScale = tileScale;
+          uvMappingDirty = true;
+        }
+
         const backFaceUpThroughWhichAxisRotation =
           object.content.backFaceUpThroughWhichAxisRotation || 'X';
         if (
@@ -3078,6 +3104,7 @@ module.exports = {
         const uvMapping = this._threeObject.geometry.getAttribute('uv');
         const startIndex = 0;
         const endIndex = 23;
+        const tileScale = this._tileScale || 1;
         for (
           let vertexIndex = startIndex;
           vertexIndex <= endIndex;
@@ -3268,6 +3295,10 @@ module.exports = {
               break;
             default:
               [x, y] = noRepeatTextureVertexIndexToUvMapping[vertexIndex % 4];
+          }
+          if (shouldRepeatTexture && tileScale !== 1) {
+            x /= tileScale;
+            y /= tileScale;
           }
           uvMapping.setXY(vertexIndex, x, y);
         }

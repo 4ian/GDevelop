ebde0175a43178584c97897cd5977192932ce455
Davy Hélard <davy.helard@gmail.com>
2026-06-09 20:00:18 +0200

    Add drag and drop decorator

--- newIDE/app/src/stories/componentStories/LayoutEditor/CompactObjectGroupPropertiesEditor.stories.js
+++ newIDE/app/src/stories/componentStories/LayoutEditor/CompactObjectGroupPropertiesEditor.stories.js
@@ -8,6 +8,7 @@ import { testProject } from '../../GDevelopJsInitializerDecorator';
 import fakeResourceManagementProps from '../../FakeResourceManagement';
 
 import paperDecorator from '../../PaperDecorator';
+import DragAndDropContextProvider from '../../../UI/DragAndDrop/DragAndDropContextProvider';
 import { CompactObjectGroupPropertiesEditor } from '../../../ObjectGroupEditor/CompactObjectGroupPropertiesEditor';
 
 export default {
@@ -17,58 +18,64 @@ export default {
 };
 
 export const Default = (): React.Node => (
-  <CompactObjectGroupPropertiesEditor
-    project={testProject.project}
-    projectScopedContainersAccessor={
-      testProject.testSceneProjectScopedContainersAccessor
-    }
-    globalObjectsContainer={testProject.project.getObjects()}
-    objectsContainer={testProject.testLayout.getObjects()}
-    initialInstances={testProject.testLayout.getInitialInstances()}
-    objectGroup={testProject.group2}
-    isVariableListLocked={false}
-    isObjectListLocked={false}
-    eventsFunctionsExtension={null}
-    eventsBasedObject={null}
-    onEditObjectGroup={action('onEditObjectGroup')}
-    resourceManagementProps={fakeResourceManagementProps}
-  />
+  <DragAndDropContextProvider>
+    <CompactObjectGroupPropertiesEditor
+      project={testProject.project}
+      projectScopedContainersAccessor={
+        testProject.testSceneProjectScopedContainersAccessor
+      }
+      globalObjectsContainer={testProject.project.getObjects()}
+      objectsContainer={testProject.testLayout.getObjects()}
+      initialInstances={testProject.testLayout.getInitialInstances()}
+      objectGroup={testProject.group2}
+      isVariableListLocked={false}
+      isObjectListLocked={false}
+      eventsFunctionsExtension={null}
+      eventsBasedObject={null}
+      onEditObjectGroup={action('onEditObjectGroup')}
+      resourceManagementProps={fakeResourceManagementProps}
+    />
+  </DragAndDropContextProvider>
 );
 
 export const WithLongObjectNames = (): React.Node => (
-  <CompactObjectGroupPropertiesEditor
-    project={testProject.project}
-    projectScopedContainersAccessor={
-      testProject.testSceneProjectScopedContainersAccessor
-    }
-    globalObjectsContainer={testProject.project.getObjects()}
-    objectsContainer={testProject.testLayout.getObjects()}
-    initialInstances={testProject.testLayout.getInitialInstances()}
-    objectGroup={testProject.group4WithLongsNames}
-    isVariableListLocked={false}
-    isObjectListLocked={false}
-    eventsFunctionsExtension={null}
-    eventsBasedObject={null}
-    onEditObjectGroup={action('onEditObjectGroup')}
-    resourceManagementProps={fakeResourceManagementProps}
-  />
+  <DragAndDropContextProvider>
+    <CompactObjectGroupPropertiesEditor
+      project={testProject.project}
+      projectScopedContainersAccessor={
+        testProject.testSceneProjectScopedContainersAccessor
+      }
+      globalObjectsContainer={testProject.project.getObjects()}
+      objectsContainer={testProject.testLayout.getObjects()}
+      initialInstances={testProject.testLayout.getInitialInstances()}
+      objectGroup={testProject.group4WithLongsNames}
+      isVariableListLocked={false}
+      isObjectListLocked={false}
+      eventsFunctionsExtension={null}
+      eventsBasedObject={null}
+      onEditObjectGroup={action('onEditObjectGroup')}
+      resourceManagementProps={fakeResourceManagementProps}
+    />
+  </DragAndDropContextProvider>
 );
 
 export const Empty = (): React.Node => (
-  <CompactObjectGroupPropertiesEditor
-    project={testProject.project}
-    projectScopedContainersAccessor={
-      testProject.testSceneProjectScopedContainersAccessor
-    }
-    globalObjectsContainer={testProject.project.getObjects()}
-    objectsContainer={testProject.testLayout.getObjects()}
-    initialInstances={testProject.testLayout.getInitialInstances()}
-    objectGroup={testProject.emptyGroup}
-    isVariableListLocked={false}
-    isObjectListLocked={false}
-    eventsFunctionsExtension={null}
-    eventsBasedObject={null}
-    onEditObjectGroup={action('onEditObjectGroup')}
-    resourceManagementProps={fakeResourceManagementProps}
-  />
+  <DragAndDropContextProvider>
+    <CompactObjectGroupPropertiesEditor
+      project={testProject.project}
+      projectScopedContainersAccessor={
+        testProject.testSceneProjectScopedContainersAccessor
+      }
+      globalObjectsContainer={testProject.project.getObjects()}
+      objectsContainer={testProject.testLayout.getObjects()}
+      initialInstances={testProject.testLayout.getInitialInstances()}
+      objectGroup={testProject.emptyGroup}
+      isVariableListLocked={false}
+      isObjectListLocked={false}
+      eventsFunctionsExtension={null}
+      eventsBasedObject={null}
+      onEditObjectGroup={action('onEditObjectGroup')}
+      resourceManagementProps={fakeResourceManagementProps}
+    />
+  </DragAndDropContextProvider>
 );

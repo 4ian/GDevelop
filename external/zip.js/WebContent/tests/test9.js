68f874d3af44fa7efd86a1071d534306604c3434
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2026-06-12 17:39:13 +0200

    Updates

---
+++ external/zip.js/WebContent/tests/test9.js
@@ -0,0 +1,33 @@
+var URL = "lorem.zip";
+
+var zipFs = new zip.fs.FS();
+
+function onerror(message) {
+	console.error(message);
+}
+
+function zipImportedZip(callback) {
+	var directory = zipFs.root.addDirectory("import");
+	directory.importHttpContent(URL, false, function() {
+		zipFs.exportBlob(callback);
+	}, onerror);
+}
+
+function unzipBlob(blob, callback) {
+	zipFs.importBlob(blob, function() {
+		var directory = zipFs.root.getChildByName("import");
+		var firstEntry = directory.children[0];
+		firstEntry.getText(callback);
+	}, onerror);
+}
+
+function logText(text) {
+	console.log(text);
+	console.log("--------------");
+}
+
+zipImportedZip(function(zippedBlob) {
+	unzipBlob(zippedBlob, function(unzippedText) {
+		logText(unzippedText);
+	});
+});

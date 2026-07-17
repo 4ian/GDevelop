c648ba54c846d89643c8746ee1b737a090a4849e
Davy Hélard <davy.helard@gmail.com>
2026-07-17 16:44:58 +0200

    Remove deprecated attribute

--- newIDE/app/src/ResourcesList/ResourcePreview/Resource3DPreview.worker.js
+++ newIDE/app/src/ResourcesList/ResourcePreview/Resource3DPreview.worker.js
@@ -51,7 +51,6 @@ const initRenderer = () => {
     antialias: true,
     alpha: true,
   });
-  renderer.useLegacyLights = true; // Use legacy lights as in the editor.
 
   renderer.setSize(width, height, false);
 

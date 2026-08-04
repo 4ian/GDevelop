eaf32ac7ce1a48f62b373e26a6177a361357ecd3
Davy Hélard <davy.helard@gmail.com>
2026-08-04 16:16:41 +0200

    Fix missing translations for the 3D character behavior

--- Extensions/Physics3DBehavior/JsExtension.js
+++ Extensions/Physics3DBehavior/JsExtension.js
@@ -2052,7 +2052,7 @@ module.exports = {
 
         behaviorProperties
           .getOrCreate('slopeMaxAngle')
-          .setLabel('Slope max. angle')
+          .setLabel(_('Slope max. angle'))
           .setGroup(_('Walk'))
           .setType('Number')
           .setMeasurementUnit(gd.MeasurementUnit.getDegreeAngle())
@@ -2070,7 +2070,7 @@ module.exports = {
         }
         behaviorProperties
           .getOrCreate('stairHeightMax')
-          .setLabel('Max. stair height')
+          .setLabel(_('Max. stair height'))
           .setGroup(_('Walk'))
           .setType('Number')
           .setMeasurementUnit(gd.MeasurementUnit.getPixel())
@@ -2085,7 +2085,7 @@ module.exports = {
 
         behaviorProperties
           .getOrCreate('shouldBindObjectAndForwardAngle')
-          .setLabel('Keep object angle and forward direction the same')
+          .setLabel(_('Keep object angle and forward direction the same'))
           .setGroup(_('Walk'))
           .setType('Boolean')
           .setValue(
@@ -2103,7 +2103,7 @@ module.exports = {
         }
         behaviorProperties
           .getOrCreate('canBePushed')
-          .setLabel('Can be pushed by other characters')
+          .setLabel(_('Can be pushed by other characters'))
           .setGroup(_('Walk'))
           .setType('Boolean')
           .setValue(

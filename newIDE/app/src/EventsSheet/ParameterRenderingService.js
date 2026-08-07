accc32ee48c2c97dbd12d4abd28c9aaa1607b097
Aurélien Vivet <bouh.vivez@gmail.com>
2026-08-07 12:52:00 +0200

    Add preview of color parameters in the event sheets (#8927)

--- newIDE/app/src/EventsSheet/ParameterRenderingService.js
+++ newIDE/app/src/EventsSheet/ParameterRenderingService.js
@@ -62,7 +62,9 @@ import JsonResourceField from './ParameterFields/JsonResourceField';
 import SpineResourceField from './ParameterFields/SpineResourceField';
 import BitmapFontResourceField from './ParameterFields/BitmapFontResourceField';
 import FontResourceField from './ParameterFields/FontResourceField';
-import ColorExpressionField from './ParameterFields/ColorExpressionField';
+import ColorExpressionField, {
+  renderInlineColor,
+} from './ParameterFields/ColorExpressionField';
 import ForceMultiplierField, {
   renderInlineForceMultiplier,
 } from './ParameterFields/ForceMultiplierField';
@@ -172,6 +174,7 @@ const inlineRenderers: { [string]: ParameterInlineRenderer } = {
   operator: renderInlineOperator,
   relationalOperator: renderInlineRelationalOperator,
   leaderboardId: renderInlineLeaderboardIdField,
+  color: renderInlineColor,
 };
 const userFriendlyTypeName: { [string]: MessageDescriptor } = {
   mouse: t`Mouse button`,

f11d73ef7341afba4e252170693d2031e3c715d2
Davy Hélard <davy.helard@gmail.com>
2026-08-11 16:42:07 +0200

    Fix a regression on Physics3D extension declaration

--- Extensions/Physics3DBehavior/JsExtension.js
+++ Extensions/Physics3DBehavior/JsExtension.js
@@ -358,7 +358,7 @@ module.exports = {
           .setType('resource')
           .addExtraInfo('model3D')
           .setLabel(_("Simplified 3D model"))
-          .setDescription(_("Leave empty to use object's one"));
+          .setDescription(_("Leave empty to use object's one"))
           // Hidden as required to be changed in the full editor.
           .setHidden(true)
           .setHasImpactOnOtherProperties(true);

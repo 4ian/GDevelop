0aabf5fc9c7c29f1ab6b2cdfad388a6dc27f1000
Florian Rival <Florian.rival@gmail.com>
2026-08-18 13:41:41 +0200

    Revert document.h

--- Core/GDCore/Serialization/rapidjson/document.h
+++ Core/GDCore/Serialization/rapidjson/document.h
@@ -322,7 +322,7 @@ struct GenericStringRef {
     operator const Ch *() const { return s; }
 
     const Ch* const s; //!< plain CharType pointer
-    SizeType length; //!< length of the string (excluding the trailing NULL terminator)
+    const SizeType length; //!< length of the string (excluding the trailing NULL terminator)
 
 private:
     //! Disallow construction from non-const array

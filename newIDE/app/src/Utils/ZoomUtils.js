22323733776346d604901e7b49962393d36287f2
Mykyta Bozhdohan <50903186+Raiguri@users.noreply.github.com>
2026-08-24 13:08:46 +0300

    Update ZoomUtils.js

--- newIDE/app/src/Utils/ZoomUtils.js
+++ newIDE/app/src/Utils/ZoomUtils.js
@@ -2,18 +2,21 @@
 
 const zoomStepBasePower = 1 / 16;
 
-// See same factors in `GDJS/Runtime/InGameEditor/InGameEditor.tsx`.
 const stepZoomFactor: number = Math.pow(2, 2 * zoomStepBasePower);
 export const zoomInFactor = stepZoomFactor;
 export const zoomOutFactor: number = Math.pow(stepZoomFactor, -1);
-const wheelStepZoomFactor = Math.pow(1.7, zoomStepBasePower);
 
-// TODO: Use absolute value of signal that should represent either:
-// - Mouse sensitivity
-// - MacOS scroll acceleration
-// Signal is usually WheelEvent.deltaY
-export const getWheelStepZoomFactor = (deltaY: number): number =>
-  Math.pow(wheelStepZoomFactor, Math.sign(deltaY));
+// Sensitivity constant mapping wheel/trackpad delta to zoom speed.
+const WHEEL_SENSITIVITY = 0.0015;
+
+/**
+ * Calculates a continuous exponential zoom factor proportional to scroll magnitude.
+ * Replaces discrete `Math.sign(deltaY)` steps with smooth exponentiation.
+ */
+export const getWheelStepZoomFactor = (deltaY: number): number => {
+  // Standard wheel deltaY > 0 is scroll down (zoom out), deltaY < 0 is scroll up (zoom in).
+  return Math.exp(deltaY * WHEEL_SENSITIVITY);
+};
 
 const instancesEditorMaxZoom = 128;
 const instancesEditorMinZoom = 1 / 128;

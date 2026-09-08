896ea2f60c3a35fe55d2caecabe6f96a4b273dab
Davy Hélard <davy.helard@gmail.com>
2026-09-08 17:55:40 +0200

    Fix extension editor side panel section order

--- newIDE/app/src/EventsFunctionsList/index.js
+++ newIDE/app/src/EventsFunctionsList/index.js
@@ -1556,29 +1556,6 @@ const EventsFunctionsList = React.forwardRef<
           {
             isRoot: true,
             content: new LabelTreeViewItemContent(
-              extensionTestsRootFolderId,
-              i18n._(t`Gameplay tests`),
-              {
-                icon: <Add />,
-                label: i18n._(t`Add a gameplay test`),
-                click: addNewGameplayTest,
-              }
-            ),
-            getChildren(i18n: I18nType): ?Array<TreeViewItem> {
-              return gameplayTestTreeViewItems.length === 0
-                ? [
-                    new PlaceHolderTreeViewItem(
-                      extensionTestsEmptyPlaceholderId,
-                      i18n._(t`Start by adding a new gameplay test.`)
-                    ),
-                  ]
-                : // $FlowFixMe[incompatible-type]
-                  gameplayTestTreeViewItems;
-            },
-          },
-          {
-            isRoot: true,
-            content: new LabelTreeViewItemContent(
               extensionFunctionsRootFolderId,
               i18n._(t`Functions`),
               {
@@ -1646,6 +1623,29 @@ const EventsFunctionsList = React.forwardRef<
                   functionTreeViewItemProps: freeFunctionProps,
                 });
               });
+            },
+          },
+          {
+            isRoot: true,
+            content: new LabelTreeViewItemContent(
+              extensionTestsRootFolderId,
+              i18n._(t`Gameplay tests`),
+              {
+                icon: <Add />,
+                label: i18n._(t`Add a gameplay test`),
+                click: addNewGameplayTest,
+              }
+            ),
+            getChildren(i18n: I18nType): ?Array<TreeViewItem> {
+              return gameplayTestTreeViewItems.length === 0
+                ? [
+                    new PlaceHolderTreeViewItem(
+                      extensionTestsEmptyPlaceholderId,
+                      i18n._(t`Start by adding a new gameplay test.`)
+                    ),
+                  ]
+                : // $FlowFixMe[incompatible-type]
+                  gameplayTestTreeViewItems;
             },
           },
         ].filter(Boolean);

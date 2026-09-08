68ce08bcd449f98c9fd9996f4dc803aefc445535
Davy Hélard <davy.helard@gmail.com>
2026-09-08 12:27:22 +0200

    Fix deletion of folders not to leave empty sub-folders.

--- newIDE/app/src/ObjectsList/ObjectFolderTreeViewItemContent.js
+++ newIDE/app/src/ObjectsList/ObjectFolderTreeViewItemContent.js
@@ -24,6 +24,7 @@ import { type MessageDescriptor } from '../Utils/i18n/MessageDescriptor.flow';
 import type { ObjectWithContext } from '../ObjectsList/EnumerateObjects';
 import { type HTMLDataset } from '../Utils/HTMLDataset';
 import { exceptionallyGuardAgainstDeadObject } from '../Utils/IsNullPtr';
+import { removeSubFolders } from '../Utils/Folders';
 
 const gd: libGDevelop = global.gd;
 
@@ -370,6 +371,7 @@ export class ObjectFolderTreeViewItemContent implements TreeViewItemContent {
     if (objectsToDelete.length === 0) {
       // Folder is empty or contains only empty folders.
       selectObjectFolderOrObjectWithContext(null);
+      removeSubFolders(this.objectFolder);
       this.objectFolder.getParent().removeFolderChild(this.objectFolder);
       onObjectModified(true);
       return true;
@@ -417,6 +419,7 @@ export class ObjectFolderTreeViewItemContent implements TreeViewItemContent {
         });
       }
 
+      removeSubFolders(folderToDelete);
       folderToDelete.getParent().removeFolderChild(folderToDelete);
       forceUpdateList();
 
